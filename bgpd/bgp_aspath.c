efa9f8300965d53d11b8a24efbab4b3644af7ff5
paul <paul>
2002-12-13 21:47:59 +0000

    change log message fron signed to unsigned decimal

--- bgpd/bgp_aspath.c
+++ bgpd/bgp_aspath.c
@@ -1170,7 +1170,7 @@ aspath_show_all_iterator (struct hash_backet *backet, struct vty *vty)
 
   as = (struct aspath *) backet->data;
 
-  vty_out (vty, "[%p:%d] (%ld) ", backet, backet->key, as->refcnt);
+  vty_out (vty, "[%p:%u] (%ld) ", backet, backet->key, as->refcnt);
   vty_out (vty, "%s%s", as->str, VTY_NEWLINE);
 }
 
