9a76e2ddb6c4dc303ce7ed937c0cfa9430c3ed27
paul <paul>
2004-01-19 21:25:17 +0000

    2004-01-19 Paul Jakma <paul@dishone.st>
            * tests/test-sig.c: New file, regression test for sigevents.

---
+++ tests/test-sig.c
@@ -0,0 +1,52 @@
+
+#include <zebra.h>
+#include <sigevent.h>
+
+void
+sighup (void)
+{
+  printf ("processed hup\n");
+}
+
+void
+sigusr1 (void)
+{
+  printf ("processed usr1\n");
+}
+
+void
+sigusr2 (void)
+{
+  printf ("processed usr2\n");
+}
+
+struct quagga_signal_t sigs[] = 
+{
+  {
+    .signal = SIGHUP,
+    .handler = &sighup,
+  },
+  {
+    .signal = SIGUSR1,
+    .handler = &sigusr1,
+  },
+  {
+    .signal = SIGUSR2,
+    .handler = &sigusr2,
+  }
+};
+
+struct thread_master *master;
+struct thread t;
+
+int
+main (void)
+{
+  master = thread_master_create ();
+  signal_init (master, Q_SIGC(sigs), sigs);
+
+  while (thread_fetch (master, &t))
+    thread_call (&t);
+
+  exit (0);
+}
