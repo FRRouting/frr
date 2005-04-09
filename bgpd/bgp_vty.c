a8a80d532f3248567b6926b5dbd12509eadd6b43
hasso <hasso>
2005-04-09 13:07:47 +0000

    * bgp_vty.c: Make "exit-address-family" work in IPv4 unicast address 	  family node.
    	[backport candidate]

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -3954,7 +3954,8 @@ DEFUN (exit_address_family,
        "exit-address-family",
        "Exit from Address Family configuration mode\n")
 {
-  if (vty->node == BGP_IPV4M_NODE
+  if (vty->node == BGP_IPV4_NODE
+      || vty->node == BGP_IPV4M_NODE
       || vty->node == BGP_VPNV4_NODE
       || vty->node == BGP_IPV6_NODE)
     vty->node = BGP_NODE;
