7e4bcdc1d6d0e0338fe5cac6103dd485332e7b51
Paul Jakma <paul.jakma@sun.com>
2006-08-04 06:14:13 +0000

    [zebra] add more stuff to misc_null to avoid compile breakages
    2006-08-04 Paul Jakma <paul.jakma@sun.com>
    
    	* misc_null.c: Add ifstat_update_sysctl, add another required
    	  header.

--- zebra/misc_null.c
+++ zebra/misc_null.c
@@ -3,7 +3,9 @@
 #include "prefix.h"
 #include "zebra/rtadv.h"
 #include "zebra/irdp.h"
+#include "zebra/interface.h"
 
 void ifstat_update_proc (void) { return; }
 #pragma weak rtadv_config_write = ifstat_update_proc
 #pragma weak irdp_config_write = ifstat_update_proc
+#pragma weak ifstat_update_sysctl = ifstat_update_proc
