d1a8316149fb2d9f6e79c80d87b483bd744046e4
Greg Troxel <gdt@fnord.ir.bbn.com>
2007-02-02 17:01:58 +0000

    Provide 'standard interface', even if it just invokes autoreconf.

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This file exists to document the proper way to initialize autotools,
+# and so that those used to the presence of bootstrap.sh or autogen.sh
+# will have an eaiser time.
+
+autoreconf
