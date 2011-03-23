aad356a49d6b00590b21751ea560c54f321c73c9
Christian Hammers <ch@debian.org>
2011-03-23 13:07:55 +0300

    lib: fix more format warnings (#637)
    The following patch was also neccessary to compile.
    
    * command.c: (config_logmsg_cmd) use "%s" format spec
    * if.c: (connected_log) ditto

--- lib/if.c
+++ lib/if.c
@@ -664,7 +664,7 @@ connected_log (struct connected *connected, char *str)
       strncat (logbuf, inet_ntop (p->family, &p->u.prefix, buf, BUFSIZ),
 	       BUFSIZ - strlen(logbuf));
     }
-  zlog (NULL, LOG_INFO, logbuf);
+  zlog (NULL, LOG_INFO, "%s", logbuf);
 }
 
 /* If two connected address has same prefix return 1. */
