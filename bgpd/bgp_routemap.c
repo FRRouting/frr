4a2035fd71990848017237838fe11bc1aeb41641
Paul Jakma <paul@quagga.net>
2011-04-01 15:58:27 +0100

    Revert "bgpd: "Intern" communities in route maps"
    This reverts commit 2c9fd7e07283b8904ef20030c9dadb032e999b12.

--- bgpd/bgp_routemap.c
+++ bgpd/bgp_routemap.c
@@ -1297,7 +1297,7 @@ route_set_community (void *rule, struct prefix *prefix,
 	new = community_dup (rcs->com);
       
       /* will be interned by caller if required */
-      attr->community = community_intern (new);
+      attr->community = new;
 
       attr->flag |= ATTR_FLAG_BIT (BGP_ATTR_COMMUNITIES);
     }
@@ -1311,7 +1311,6 @@ route_set_community_compile (const char *arg)
 {
   struct rmap_com_set *rcs;
   struct community *com = NULL;
-  struct community *comint;
   char *sp;
   int additive = 0;
   int none = 0;
@@ -1338,9 +1337,8 @@ route_set_community_compile (const char *arg)
 	return NULL;
     }
   
-  comint = community_intern (com);
   rcs = XCALLOC (MTYPE_ROUTE_MAP_COMPILED, sizeof (struct rmap_com_set));
-  rcs->com = comint;
+  rcs->com = com;
   rcs->additive = additive;
   rcs->none = none;
   
@@ -1403,7 +1401,7 @@ route_set_community_delete (void *rule, struct prefix *prefix,
 	    }
 	  else
 	    {
-	      binfo->attr->community = community_intern (new);
+	      binfo->attr->community = new;
 	      binfo->attr->flag |= ATTR_FLAG_BIT (BGP_ATTR_COMMUNITIES);
 	    }
 	}
