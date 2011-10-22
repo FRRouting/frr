12da1a188a5e5bfdfecb98c48ecca48ed38e2d89
Denis Ovsienko <infrastation@yandex.ru>
2011-10-22 22:11:31 +0400

    bgpd: check AGGREGATOR attr flags (BZ#678)
    * bgp_attr.c
      * bgp_attr_aggregator(): check Optional/Transitive flag bits

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -1103,6 +1103,27 @@ bgp_attr_aggregator (struct peer *peer, bgp_size_t length,
   bgp_size_t total;
   
   total = length + (CHECK_FLAG (flag, BGP_ATTR_FLAG_EXTLEN) ? 4 : 3);
+  /* Flags check. */
+  if (! CHECK_FLAG (flag, BGP_ATTR_FLAG_OPTIONAL))
+  {
+    zlog (peer->log, LOG_ERR,
+          "AGGREGATOR attribute must be flagged as \"optional\" (%u)", flag);
+    bgp_notify_send_with_data (peer,
+                               BGP_NOTIFY_UPDATE_ERR,
+                               BGP_NOTIFY_UPDATE_ATTR_FLAG_ERR,
+                               startp, total);
+    return -1;
+  }
+  if (! CHECK_FLAG (flag, BGP_ATTR_FLAG_TRANS))
+  {
+    zlog (peer->log, LOG_ERR,
+          "AGGREGATOR attribute must be flagged as \"transitive\" (%u)", flag);
+    bgp_notify_send_with_data (peer,
+                               BGP_NOTIFY_UPDATE_ERR,
+                               BGP_NOTIFY_UPDATE_ATTR_FLAG_ERR,
+                               startp, total);
+    return -1;
+  }
   /* peer with AS4 will send 4 Byte AS, peer without will send 2 Byte */
   if ( CHECK_FLAG (peer->cap, PEER_CAP_AS4_RCV ) )
     wantedlen = 8;
