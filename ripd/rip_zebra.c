9c58fbd7d7658055c2b3074489c6d338448d88e9
Matthieu Boutier <boutier@pps.jussieu.fr>
2012-02-09 21:51:17 +0100

    ripd: add babel for redistribution.

--- ripd/rip_zebra.c
+++ ripd/rip_zebra.c
@@ -207,7 +207,8 @@ static struct {
   {ZEBRA_ROUTE_CONNECT, 1, "connected"},
   {ZEBRA_ROUTE_STATIC,  1, "static"},
   {ZEBRA_ROUTE_OSPF,    1, "ospf"},
-  {ZEBRA_ROUTE_BGP,     1, "bgp"},
+  {ZEBRA_ROUTE_BGP,     2, "bgp"},
+  {ZEBRA_ROUTE_BABEL,   2, "babel"},
   {0, 0, NULL}
 };
 
