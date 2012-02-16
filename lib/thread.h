e854095932260b3e6187902aa9a7baa0e96b9428
Everton Marques <everton.marques@gmail.com>
2012-02-16 06:14:54 +0100

    lib: add THREAD_TIMER_MSEC_ON()
     * lib/thread.h: new timer macro, millisecond precision
    
    (this was cherrypicked from pimd to isolate all non-contained changes)
    
    From: Everton Marques <everton.marques@gmail.com>
    Signed-off-by: David Lamparter <equinox@diac24.net>

--- lib/thread.h
+++ lib/thread.h
@@ -1,5 +1,6 @@
 /* Thread management routine header.
  * Copyright (C) 1998 Kunihiro Ishiguro
+ * Portions Copyright (c) 2008 Everton da Silva Marques <everton.marques@gmail.com>
  *
  * This file is part of GNU Zebra.
  *
@@ -135,6 +136,12 @@ enum quagga_clkid {
   do { \
     if (! thread) \
       thread = thread_add_timer (master, func, arg, time); \
+  } while (0)
+
+#define THREAD_TIMER_MSEC_ON(master,thread,func,arg,time) \
+  do { \
+    if (! thread) \
+      thread = thread_add_timer_msec (master, func, arg, time); \
   } while (0)
 
 #define THREAD_OFF(thread) \
