d92a2f39b46f1990052d2db046b47edf7bb21ebb
Jorge Boncompte [DTI2] <jorge@dti2.net>
2013-07-31 16:36:08 +0000

    bgpd: use ATTR_FLAG_BIT() for BGP_ATTR_ values
    * bgp_attr.c: this UNSET_FLAG()s are bogus. I did a quick review and
      I think that they could not cause any bug anyway.
    
    Signed-off-by: Jorge Boncompte [DTI2] <jorge@dti2.net>
    Acked-by: Feng Lu <lu.feng@6wind.com>
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -645,21 +645,21 @@ bgp_attr_unintern_sub (struct attr *attr)
   /* aspath refcount shoud be decrement. */
   if (attr->aspath)
     aspath_unintern (&attr->aspath);
-  UNSET_FLAG(attr->flag, BGP_ATTR_AS_PATH);
+  UNSET_FLAG(attr->flag, ATTR_FLAG_BIT (BGP_ATTR_AS_PATH));
   
   if (attr->community)
     community_unintern (&attr->community);
-  UNSET_FLAG(attr->flag, BGP_ATTR_COMMUNITIES);
+  UNSET_FLAG(attr->flag, ATTR_FLAG_BIT (BGP_ATTR_COMMUNITIES));
   
   if (attr->extra)
     {
       if (attr->extra->ecommunity)
         ecommunity_unintern (&attr->extra->ecommunity);
-      UNSET_FLAG(attr->flag, BGP_ATTR_EXT_COMMUNITIES);
+      UNSET_FLAG(attr->flag, ATTR_FLAG_BIT (BGP_ATTR_EXT_COMMUNITIES));
       
       if (attr->extra->cluster)
         cluster_unintern (attr->extra->cluster);
-      UNSET_FLAG(attr->flag, BGP_ATTR_CLUSTER_LIST);
+      UNSET_FLAG(attr->flag, ATTR_FLAG_BIT (BGP_ATTR_CLUSTER_LIST));
       
       if (attr->extra->transit)
         transit_unintern (attr->extra->transit);
