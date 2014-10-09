618969eaf056b29010cd6b6c0e22ef39cfad62aa
Olivier Cochard-Labbé <olivier@cochard.me>
2014-10-09 10:28:21 +0100

    FreeBSD has changed its SOCK_RAW for being truly raw.
    (cherry picked from commit f6444e4f6e1664f49f7552f894c8c94e45dd3c35)

--- lib/zebra.h
+++ lib/zebra.h
@@ -358,7 +358,8 @@ struct in_pktinfo
  * OpenBSD: network byte order, apart from older versions which are as per 
  *          *BSD
  */
-#if defined(__NetBSD__) || defined(__FreeBSD__) \
+#if defined(__NetBSD__) \
+   || (defined(__FreeBSD__) && (__FreeBSD_version < 1100030)) \
    || (defined(__OpenBSD__) && (OpenBSD < 200311)) \
    || (defined(__APPLE__)) \
    || (defined(SUNOS_5) && defined(WORDS_BIGENDIAN))
