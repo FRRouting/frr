31eba040c282554cb8cb68f27344e9d83e8e8aad
Donald Sharp <sharpd@cumulusnetworks.com>
2015-05-19 18:03:43 -0700

    If a BGP path has an unreachable nexthop display that path as invalid

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -6015,7 +6015,8 @@ route_vty_short_status_out (struct vty *vty, struct bgp_info *binfo)
     vty_out (vty, "S");
   else if (binfo->extra && binfo->extra->suppress)
     vty_out (vty, "s");
-  else if (! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
+  else if (CHECK_FLAG (binfo->flags, BGP_INFO_VALID) &&
+           ! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
     vty_out (vty, "*");
   else
     vty_out (vty, " ");
@@ -6484,7 +6485,9 @@ route_vty_out_detail (struct vty *vty, struct bgp *bgp, struct prefix *p,
       if (attr->extra && attr->extra->tag != 0)
         vty_out (vty, ", tag %d", attr->extra->tag);
 	
-      if (! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
+      if (! CHECK_FLAG (binfo->flags, BGP_INFO_VALID))
+	vty_out (vty, ", invalid");
+      else if (! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
 	vty_out (vty, ", valid");
 
       if (binfo->peer != bgp->peer_self)
