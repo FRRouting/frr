b5790b703d534c5df2fc8c9a90a14be185e6e5db
Donald Sharp <sharpd@cumulusnetworks.com>
2015-10-01 12:40:51 -0400

    pimd: Add knowledge of different packet types
    There are several additional packet types that pimd is unaware of
    Add code to allow pim to understand them in the future.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pim_pim.h
+++ pimd/pim_pim.h
@@ -41,8 +41,14 @@
 #define PIM_DEFAULT_T_PERIODIC                   (60)   /* RFC 4601: 4.11.  Timer Values */
 
 #define PIM_MSG_TYPE_HELLO      (0)
+#define PIM_MSG_TYPE_REGISTER   (1)
+#define PIM_MSG_TYPE_REG_STOP   (2)
 #define PIM_MSG_TYPE_JOIN_PRUNE (3)
+#define PIM_MSG_TYPE_BOOTSTRAP  (4)
 #define PIM_MSG_TYPE_ASSERT     (5)
+#define PIM_MSG_TYPE_GRAFT      (6)
+#define PIM_MSG_TYPE_GRAFT_ACK  (7)
+#define PIM_MSG_TYPE_CANDIDATE  (8)
 
 #define PIM_MSG_HDR_OFFSET_VERSION(pim_msg) (pim_msg)
 #define PIM_MSG_HDR_OFFSET_TYPE(pim_msg) (pim_msg)
