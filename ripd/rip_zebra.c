b3556ea327bf3abc4bd183a4b5b8ad1a15d15035
Donald Sharp <sharpd@cumulusnetworks.com>
2016-02-11 08:29:36 -0800

    ripd: Fix crash when a default route is passed to rip
    When rip is not configured and zebra decides to send
    a default route to it, check to make sure that rip is
    configured before accepting anything from zebra.
    
    Ticket: CM-9270
    Reviewed-by: Don Slice
    Tested: OSPF test case that was erroring out no longer
    happens
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- ripd/rip_zebra.c
+++ ripd/rip_zebra.c
@@ -106,7 +106,10 @@ rip_zebra_read_ipv4 (int command, struct zclient *zclient, zebra_size_t length,
   unsigned long ifindex;
   struct in_addr nexthop;
   struct prefix_ipv4 p;
-  
+
+  if (!rip)
+    return 0;
+
   s = zclient->ibuf;
   ifindex = 0;
   nexthop.s_addr = 0;
