FRRouting/frr

commit 3e66be2ee6db028d808a304586e0b24d5374914e

Zebra: Ignore status change for VRF (temporary)
Temporary change to ignore status change for a VRF device as it is
incorrectly implemented now. When VRF is also supported as an
interface, the status change will be handled for the interface.

Ticket:
Reviewed By:
Testing Done:

zebra/rt_netlink.c
@@ -540,6 +540,19 @@ netlink_vrf_change (struct nlmsghdr *h, struct rtattr *tb, const char *name)
         zlog_debug ("%s: RTM_NEWLINK for VRF(%s) index %u, table %u", __func__,
                     name, ifi->ifi_index, nl_table_id);
 
+      /* If VRF already exists, we just return; status changes are ignored
+       * for now.
+       * TODO: Status changes will be handled against the VRF "interface".
+       */
+      vrf = vrf_lookup ((vrf_id_t)ifi->ifi_index);
+      if (vrf)
+        {
+          if (IS_ZEBRA_DEBUG_KERNEL)
+            zlog_debug ("%s: RTM_NEWLINK status for VRF(%s) index %u - ignored",
+                        __func__, name, ifi->ifi_index);
+          return;
+        }
+
       vrf = vrf_get((vrf_id_t)ifi->ifi_index, name); // It would create vrf
       if (!vrf)
         {

commit e786b6015e51953fde6404cfa18a4e879a8d7cac

Zebra: Fix ignoring status for VRF device
The earlier change to ignore status for VRF device was not quite perfect. As
defect CM-9437 illustrates, there are situations when Quagga may get a VRF
member interface (that refers to the VRF id of the VRF device) before it gets
the VRF device itself. The code has some logic to handle this, creating a
VRF structure which is partly initialized. The initialization is completed
with some additional incorrect status processing when the VRF is learnt. The
fix done earlier completely ignored the VRF message treating it as a status
change because the VRF is already present, but this left the VRF structure
not fully initialized in Quagga. The fix is to do some additional checks
to handle this scenario.

Fixes: 3e66be2ee6db028d808a304586e0b24d5374914e

Ticket: CM-9437
Reviewed By: None
Testing Done: Reproduced problem, verified fix.

zebra/rt_netlink.c
@@ -545,7 +545,7 @@ netlink_vrf_change (struct nlmsghdr *h, struct rtattr *tb, const char *name)
        * TODO: Status changes will be handled against the VRF "interface".
        */
       vrf = vrf_lookup ((vrf_id_t)ifi->ifi_index);
-      if (vrf)
+      if (vrf && vrf->info)
         {
           if (IS_ZEBRA_DEBUG_KERNEL)
             zlog_debug ("%s: RTM_NEWLINK status for VRF(%s) index %u - ignored",