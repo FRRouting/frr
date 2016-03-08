3914b418c57118dbeb5acc88303ea5786e3c61ed
Daniel Walton <dwalton@cumulusnetworks.com>
2016-03-08 16:14:39 +0000

    Exclamation points are missing at the end of an address-family sub-context
    Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>
    Reviewed-by:   Don Slice <dslice@cumulusnetworks.com>
    
    Ticket: CM-9268

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -6842,7 +6842,7 @@ bgp_config_write_family_header (struct vty *vty, afi_t afi, safi_t safi,
   if (*write)
     return;
 
-  vty_out (vty, "!%s address-family ", VTY_NEWLINE);
+  vty_out (vty, " !%s address-family ", VTY_NEWLINE);
 
   if (afi == AFI_IP)
     {
