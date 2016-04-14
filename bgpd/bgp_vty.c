0c412461ba38ba60d0b8d48aac100f12f09d8b4c
Daniel Walton <dwalton@cumulusnetworks.com>
2016-04-14 20:34:43 +0000

    BGP: cannot "no neighbor ISL timers 3 10" for peer-group
    Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>
    Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>
    
    Ticket: CM-10438

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -4769,7 +4769,7 @@ peer_timers_unset_vty (struct vty *vty, const char *ip_str)
   int ret;
   struct peer *peer;
 
-  peer = peer_lookup_vty (vty, ip_str);
+  peer = peer_and_group_lookup_vty (vty, ip_str);
   if (! peer)
     return CMD_WARNING;
 
