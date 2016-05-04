dfa857a5c0a284129e45accd9600773df4a280a1
Donald Sharp <sharpd@cumulusnetworks.com>
2016-05-04 13:05:37 +0000

    lib: Swap show run ordering of interfaces and vrf's
    When doing a 'show run' display vrf information
    first to allow later commands to just
    work correctly for vrf cli read in.
    
    Ticket: CM-10139
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>
    Reviewed-by: Vivek Venkatraman <vivek@cumulusnetworks.com>
    Reviewed-by: Don Slice <dslice@cumulusnetworks.com>

--- lib/command.h
+++ lib/command.h
@@ -74,8 +74,8 @@ enum node_type
   AAA_NODE,			/* AAA node. */
   KEYCHAIN_NODE,		/* Key-chain node. */
   KEYCHAIN_KEY_NODE,		/* Key-chain key node. */
-  INTERFACE_NODE,		/* Interface mode node. */
   VRF_NODE,		        /* VRF mode node. */
+  INTERFACE_NODE,		/* Interface mode node. */
   ZEBRA_NODE,			/* zebra connection node. */
   TABLE_NODE,			/* rtm_table selection node. */
   RIP_NODE,			/* RIP protocol mode node. */ 
