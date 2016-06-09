Quagga/quagga

ospfd: Fix for 'no' + 'debug command' does not disable 'debug command'
"no debug ospf packet all detail" does not cancel "debug ospf packet all detail"
due to the code inconsistency in setting/unsetting debug flags.

* ospf_dump.c: added missing flags.
Acked-by: Donald Sharp <sharpd@cumulusnetworks.com>

ospfd/ospf_dump.c
@@ -902,7 +902,7 @@ DEFUN (no_debug_ospf_packet,
       else if (strncmp (argv[1], "r", 1) == 0)
 	flag = OSPF_DEBUG_RECV | OSPF_DEBUG_DETAIL;
       else if (strncmp (argv[1], "d", 1) == 0)
-	flag = OSPF_DEBUG_DETAIL;
+	flag = OSPF_DEBUG_SEND | OSPF_DEBUG_RECV | OSPF_DEBUG_DETAIL;
     }
 
   /* detail. */

FRRouting/frr

(cherry picked from commit b075e8728f650b0850e20fdf8dde467440a24b28)

ospfd/ospf_dump.c
@@ -955,7 +955,7 @@ no_debug_ospf_packet_common (struct vty *vty, int arg_base, int argc,
       else if (strncmp (argv[arg_base + 1], "r", 1) == 0)
 	flag = OSPF_DEBUG_RECV | OSPF_DEBUG_DETAIL;
       else if (strncmp (argv[arg_base + 1], "d", 1) == 0)
-	flag = OSPF_DEBUG_DETAIL;
+	flag = OSPF_DEBUG_DETAIL | OSPF_DEBUG_RECV | OSPF_DEBUG_DETAIL;
     }
 
   /* detail. */