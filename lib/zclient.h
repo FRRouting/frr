donaldsharp/quagga

zebra: implement per-route mtu handling
This commits allow overriding MTU using netlink attributes on
per-route basis. This is useful for routing protocols that can
advertice prefix specific MTUs between routers (e.g. NHRP).

Signed-off-by: Timo Teräs <timo.teras@iki.fi>

lib/zclient.h
@@ -96,6 +96,7 @@ struct zclient
 #define ZAPI_MESSAGE_IFINDEX  0x02
 #define ZAPI_MESSAGE_DISTANCE 0x04
 #define ZAPI_MESSAGE_METRIC   0x08
+#define ZAPI_MESSAGE_MTU      0x10
 
 /* Zserv protocol message header */
 struct zserv_header
@@ -130,6 +131,8 @@ struct zapi_ipv4
   u_char distance;
 
   u_int32_t metric;
+
+  u_int32_t mtu;
 
   vrf_id_t vrf_id;
 };
@@ -203,6 +206,8 @@ struct zapi_ipv6
   u_char distance;
 
   u_int32_t metric;
+
+  u_int32_t mtu;
 
   vrf_id_t vrf_id;
 };

FRRouting/frr

(cherry picked from commit b11f3b54c842117e22e2f5cf1561ea34eee8dfcc)

lib/zclient.h
@@ -116,6 +116,7 @@ struct zclient
 #define ZAPI_MESSAGE_DISTANCE 0x04
 #define ZAPI_MESSAGE_METRIC   0x08
 #define ZAPI_MESSAGE_TAG      0x10
+#define ZAPI_MESSAGE_MTU      0x20
 
 /* Zserv protocol message header */
 struct zserv_header
@@ -153,6 +154,8 @@ struct zapi_ipv4
   u_int32_t metric;
 
   u_short tag;
+
+  u_int32_t mtu;
 
   vrf_id_t vrf_id;
 };
@@ -236,6 +239,8 @@ struct zapi_ipv6
   u_int32_t metric;
 
   u_short tag;
+
+  u_int32_t mtu;
 
   vrf_id_t vrf_id;
 };