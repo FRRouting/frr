16d7c05093f0f3a24d09f9780afc1ea114e73f94
Quentin Young <qlyoung@cumulusnetworks.com>
2016-09-01 21:27:33 +0000

    lib: Generalize graph to work for any data type
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

---
+++ lib/graph.c
@@ -0,0 +1,113 @@
+/*
+ * Graph data structure.
+ *
+ * --
+ * Copyright (C) 2016 Cumulus Networks, Inc.
+ *
+ * This file is part of GNU Zebra.
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Zebra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Zebra; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+#include <zebra.h>
+#include "command_graph.h"
+#include "memory.h"
+
+
+struct graph_node *
+graph_new_node (struct graph *graph, void *data, void (*del) (void*))
+{
+  struct graph_node *node =
+     XCALLOC(MTYPE_CMD_TOKENS, sizeof(struct graph_node));
+
+  node->from = vector_init(VECTOR_MIN_SIZE);
+  node->to   = vector_init(VECTOR_MIN_SIZE);
+  node->data = data;
+  node->del  = del;
+
+  vector_set (graph->nodes, node);
+
+  return node;
+}
+
+void
+graph_delete_node (struct graph *graph, struct graph_node *node)
+{
+  if (!node) return;
+
+  // an adjacent node
+  struct graph_node *adj;
+
+  // for all nodes that have an edge to us, remove us from their ->to
+  for (int i = 0; i < vector_active (node->from); i++)
+    {
+      adj = vector_slot (node->from, i);
+      for (int j = 0; j < vector_active (adj->to); j++)
+        if (vector_slot (adj->to, j) == node)
+          vector_unset (adj->to, j);
+
+      // if the node is orphaned, delete it
+      if (vector_active (adj->to) == 0 && vector_active (adj->from) == 0)
+        graph_delete_node (adj);
+    }
+
+  // for all nodes that we have an edge to, remove us from their ->from
+  for (int i = 0; i < vector_active (node->to); i++)
+    {
+      adj = vector_slot (node->to, i);
+      for (int j = 0; j < vector_active (adj->from); j++)
+        if (vector_slot (adj->from, j) == node)
+          vector_unset (adj->from, j);
+
+      // if the node is orphaned, delete it
+      if (vector_active (adj->to) == 0 && vector_active (adj->from) == 0)
+        graph_delete_node (adj);
+    }
+
+  // if there is a deletion callback, call it!
+  if (node->del && node->data)
+    (*node->del) (node->data);
+
+  // free adjacency lists
+  vector_free (node->to);
+  vector_free (node->from);
+
+  // remove node from graph->nodes
+  for (int i = 0; i < vector_active (graph->nodes); i++)
+    if (vector_slot (graph->nodes, i) == node)
+      vector_unset (graph->nodes, i);
+
+  // free the node itself
+  free (node);
+}
+
+struct graph_node *
+graph_add_edge (struct graph_node *from, struct graph_node *to)
+{
+  vector_set (from->to, to);
+  vector_set (to->from, from);
+  return to;
+}
+
+void
+graph_delete_graph (struct graph *graph)
+{
+  // delete each node in the graph
+  for (int i = 0; i < vector_active (graph->nodes); i++)
+    graph_delete_node (vector_slot (graph->nodes, i));
+
+  vector_free (graph->nodes);
+  free (graph);
+}
---
+++ lib/graph.h
@@ -0,0 +1,99 @@
+/*
+ * Graph data structure.
+ *
+ * --
+ * Copyright (C) 2016 Cumulus Networks, Inc.
+ *
+ * This file is part of GNU Zebra.
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Zebra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Zebra; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef _ZEBRA_COMMAND_GRAPH_H
+#define _ZEBRA_COMMAND_GRAPH_H
+
+#include "vector.h"
+
+/**
+ * Graph structure.
+ */
+struct graph
+{
+  vector nodes; // all nodes in the graph
+}
+
+/**
+ * Graph node / vertex.
+ */
+struct graph_node
+{
+  vector from;                  // nodes which have edges to this node
+  vector to;                    // nodes which this node has edges to
+
+  void *data;                   // node data
+  void (*del) (void *data)      // deletion callback
+};
+
+/**
+ * Creates a new node.
+ *
+ * @struct graph the graph this node exists in
+ * @param[in] data this node's data
+ * @param[in] del data deletion callback
+ * @return the new node
+ */
+struct graph_node *
+graph_new_node (struct graph *graph, void *data, void (*del) (void*));
+
+/**
+ * Deletes a node.
+ *
+ * Before deletion, this function removes all edges to and from this node from
+ * any neighbor nodes.
+ *
+ * If, as a result of this operation, any neighbor node has no edges either to
+ * or from itself, that node will be deleted as well. If the graph topology is
+ * e.g. a star this will result in the deletion of all nodes.
+ *
+ * If *data and *del are non-null, the following call is made:
+ *   (*node->del) (node->data);
+ *
+ * @param[out] node pointer to node to delete
+ */
+void
+graph_delete_node (struct graph *graph, struct graph_node *node);
+
+/**
+ * Makes a directed edge between two nodes.
+ *
+ * @param[in] from
+ * @param[in] to
+ * @return to
+ */
+struct graph_node *
+graph_add_edge (struct graph_node *from, struct graph_node *to);
+
+/**
+ * Deletes a graph.
+ *
+ * Calls graph_delete_node on each node before freeing the graph struct itself.
+ *
+ * @param graph the graph to delete
+ */
+void
+graph_delete_graph (struct graph *graph);
+
+#endif /* _ZEBRA_COMMAND_GRAPH_H */
