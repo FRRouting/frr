1d808091d34785df0d8e62f5b388a25af48f9a93
Donald Sharp <sharpd@cumulusnetworks.com>
2016-09-09 11:02:54 -0400

    bgpd: Allow connections with no v4|6 addr's in some conditions
    When compiling/running in with --enable-bgp-standalone=yes allow
    v4 sessions to be established with no v4 address configured.
    Additionally allow v6 connections with no v6 addresses
    configured.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -1163,10 +1163,12 @@ bgp_open_receive (struct peer *peer, bgp_size_t size)
     {
       if (!peer->nexthop.v4.s_addr)
         {
+#if !defined (HAVE_BGP_STANDALONE)
           zlog_err ("%s: No local IPv4 addr resetting connection, fd %d",
                     peer->host, peer->fd);
           bgp_notify_send (peer, BGP_NOTIFY_CEASE, BGP_NOTIFY_SUBCODE_UNSPECIFIC);
           return -1;
+#endif
         }
     }
   if (peer->afc_nego[AFI_IP6][SAFI_UNICAST] ||
@@ -1176,10 +1178,12 @@ bgp_open_receive (struct peer *peer, bgp_size_t size)
     {
       if (IN6_IS_ADDR_UNSPECIFIED (&peer->nexthop.v6_global))
         {
+#if !defined (HAVE_BGP_STANDALONE)
           zlog_err ("%s: No local IPv6 addr resetting connection, fd %d",
                     peer->host, peer->fd);
           bgp_notify_send (peer, BGP_NOTIFY_CEASE, BGP_NOTIFY_SUBCODE_UNSPECIFIC);
           return -1;
+#endif
         }
     }
   peer->rtt = sockopt_tcp_rtt (peer->fd);
