5b0747d71df6006835ead8e6354f70b26f7bca80
Donald Sharp <sharpd@cumulusnetworks.com>
2016-11-18 11:41:53 -0500

    ospf6d: Allow unconfig of unknown lsa's
    When we are receiving the 'no debug ospf6 lsa unknown ...'
    Allow the removal of the originate examine and flooding
    keywords.
    
    Ticket: CM-12805
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- ospf6d/ospf6_lsa.c
+++ ospf6d/ospf6_lsa.c
@@ -872,7 +872,17 @@ ALIAS (debug_ospf6_lsa_type,
        DEBUG_STR
        OSPF6_STR
        "Debug Link State Advertisements (LSAs)\n"
-       "Specify LS type as Hexadecimal\n"
+       "Router LSA's\n"
+       "Network LSA's\n"
+       "Inter-Prefix LSA's\n"
+       "Inter-Router LSA's\n"
+       "AS-External LSA's\n"
+       "Link LSA's\n"
+       "Intra-Prefix LSA's\n"
+       "Unknown LSA's\n"
+       "Originate\n"
+       "Examine\n"
+       "Flooding\n"
       )
 
 DEFUN (no_debug_ospf6_lsa_type,
@@ -928,12 +938,22 @@ DEFUN (no_debug_ospf6_lsa_type,
 
 ALIAS (no_debug_ospf6_lsa_type,
        no_debug_ospf6_lsa_hex_detail_cmd,
-       "no debug ospf6 lsa (router|network|inter-prefix|inter-router|as-external|link|intra-prefix) (originate|examine|flooding)",
+       "no debug ospf6 lsa (router|network|inter-prefix|inter-router|as-external|link|intra-prefix|unknown) (originate|examine|flooding)",
        NO_STR
        DEBUG_STR
        OSPF6_STR
        "Debug Link State Advertisements (LSAs)\n"
-       "Specify LS type as Hexadecimal\n"
+       "Router LSA's\n"
+       "Network LSA's\n"
+       "Inter-Prefix LSA's\n"
+       "Inter-Router LSA's\n"
+       "AS-External LSA's\n"
+       "Link LSA's\n"
+       "Intra-Prefix LSA's\n"
+       "Unknown LSA's\n"
+       "Originate\n"
+       "Examine\n"
+       "Flooding\n"
       )
 
 void
