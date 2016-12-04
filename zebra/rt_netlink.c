40a678a4212dc4963f3d471bf831e65200417110
Donald Sharp <sharpd@cumulusnetworks.com>
2016-12-04 14:41:20 -0500

    zebra: LastUsed needs to be updated properly.
    The change to allow 12.04 to work killed S,G age out.
    
    Put it back.
    
    Ticket: CM-13879
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -77,6 +77,10 @@
 #define RTA_ENCAP	22
 #endif
 
+#ifndef RTA_EXPIRES
+#define RTA_EXPIRES     23
+#endif
+
 #ifndef LWTUNNEL_ENCAP_MPLS
 #define LWTUNNEL_ENCAP_MPLS  1
 #endif
@@ -552,7 +556,7 @@ netlink_route_change_read_multicast (struct sockaddr_nl *snl, struct nlmsghdr *h
   if (tb[RTA_DST])
     sg.grp = *(struct in_addr *)RTA_DATA (tb[RTA_DST]);
 
-  if (tb[RTA_EXPIRES])
+  if ((RTA_EXPIRES <= RTA_MAX) && tb[RTA_EXPIRES])
     lastused = *(unsigned long long *)RTA_DATA (tb[RTA_EXPIRES]);
 
   if (tb[RTA_MULTIPATH])
