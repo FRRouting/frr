8793176b5a180bff8e637ce7cba6005f880e8915
David Lamparter <equinox@opensourcerouting.org>
2016-12-05 19:08:00 +0100

    build: number sections in COMMUNITY.md HTML
    adds some CSS to get section numbers.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- render_md.py
+++ render_md.py
@@ -4,11 +4,14 @@ import sys, markdown
 
 template = '''<html><head><meta charset="UTF-8"><style type="text/css">
 body { max-width: 45em; margin: auto; margin-top: 2em; margin-bottom: 2em;
-    font-family:Fira Sans,sans-serif; text-align: justify; }
+    font-family:Fira Sans,sans-serif; text-align: justify;
+    counter-reset: ch2; }
 pre, code { font-family:Fira Mono,monospace; }
 pre > code { display: block; padding:0.5em; border:1px solid black;
     background-color:#eee; color:#000; }
-h2 { clear: both; margin-top: 3em; text-decoration: underline; }
+h2:before { content: counter(ch2) ". "; counter-increment: ch2; }
+h2 { clear: both; margin-top: 3em; text-decoration: underline; counter-reset: ch3; }
+h3:before { content: counter(ch2) "." counter(ch3) ". "; counter-increment: ch3; }
 h3 { clear: both; margin-top: 2em; font-weight: normal; font-style: italic; }
 h4 { font-weight: normal; font-style: italic; }
 img[alt~="float-right"] { float:right; margin-left:2em; margin-bottom:2em; }
