opensourcerouting/quagga

zebra: receive ZAPI IPv6 source prefix
Check and read the IPv6 source prefix on ZAPI messages, and pass it down
to the RIB functions (which do nothing with it yet.)  Since the RIB
functions now all have a new extra argument, this also updates the
kernel route read functions to supply NULL.

Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

lib/zclient.h
@@ -89,6 +89,7 @@ struct zclient
 #define ZAPI_MESSAGE_IFINDEX  0x02
 #define ZAPI_MESSAGE_DISTANCE 0x04
 #define ZAPI_MESSAGE_METRIC   0x08
+#define ZAPI_MESSAGE_SRCPFX   0x10
 
 /* Zserv protocol message header */
 struct zserv_header

FRRouting/frr

lib/zclient.h
@@ -114,6 +114,7 @@ struct zclient
 #define ZAPI_MESSAGE_METRIC   0x08
 #define ZAPI_MESSAGE_TAG      0x10
 #define ZAPI_MESSAGE_MTU      0x20
+#define ZAPI_MESSAGE_SRCPFX   0x40
 
 /* Zserv protocol message header */
 struct zserv_header