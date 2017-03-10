fa4b84c58940c1cfda9f70a9e4f2cdf75ae8db2f
Donald Sharp <sharpd@cumulusnetworks.com>
2017-03-10 08:04:39 -0500

    bgpd: Use appropriate Attribute number for labels
    The draft-ietf-idr-bgp-prefix-sid-04 specifies the label attribute
    should be 40 not 30.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -972,8 +972,8 @@ struct bgp_nlri
 #define BGP_ATTR_AS4_AGGREGATOR                 18
 #define BGP_ATTR_AS_PATHLIMIT                   21
 #define BGP_ATTR_ENCAP                          23
-#define BGP_ATTR_LABEL_INDEX                    30
 #define BGP_ATTR_LARGE_COMMUNITIES              32
+#define BGP_ATTR_LABEL_INDEX                    40
 #if ENABLE_BGP_VNC
 #define BGP_ATTR_VNC                           255
 #endif
