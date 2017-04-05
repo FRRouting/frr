52884837196a26d75150b55a88d768b3a27eab8b
Donald Sharp <sharpd@cumulusnetworks.com>
2017-04-04 20:18:24 -0400

    *: Clean up some wrong references
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_evpn.h
+++ bgpd/bgp_evpn.h
@@ -1,20 +1,20 @@
 /* E-VPN header for packet handling
    Copyright (C) 2016 6WIND
 
-This file is part of Free Range Routing.
+This file is part of FRRouting.
 
-Free Range Routing is free software; you can redistribute it and/or modify it
+FRRouting is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation; either version 2, or (at your option) any
 later version.
 
-Free Range Routing is distributed in the hope that it will be useful, but
+FRRouting is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Free Range Routing; see the file COPYING.  If not, write to the Free
+along with FRRouting; see the file COPYING.  If not, write to the Free
 Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 02111-1307, USA.  */
 
