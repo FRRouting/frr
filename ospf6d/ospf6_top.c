FRRouting/frr

commit 16cedbb01f6d59b28c704278efc72afbfb5abfd2

all: Fix underfull doc strings, part 1
Add missing docstrings and separating \n.
Also eat some low-hanging refactoring fruit.

Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

ospf6d/ospf6_top.c
@@ -305,16 +305,9 @@ DEFUN (no_router_ospf6,
        no_router_ospf6_cmd,
        "no router ospf6",
        NO_STR
-       OSPF6_ROUTER_STR)
+       ROUTER_STR
+       OSPF6_STR)
 {
-  if (ospf6 == NULL)
-    vty_out (vty, "OSPFv3 is not configured%s", VNL);
-  else
-    {
-      ospf6_delete (ospf6);
-      ospf6 = NULL;
-    }
-
   /* return to config node . */
   vty->node = CONFIG_NODE;
   vty->index = NULL;

bgpd/bgp_route.c
@@ -5502,7 +5502,8 @@ DEFUN (no_ipv6_aggregate_address,
        "no aggregate-address X:X::X:X/M [summary-only]",
        NO_STR
        "Configure BGP aggregate entries\n"
-       "Aggregate prefix\n")
+       "Aggregate prefix\n"
+       "Filter more specific routes from updates\n")
 {
   int idx = 0;
   argv_find (argv, argc, "X:X::X:X/M", &idx);
@@ -10112,7 +10113,11 @@ DEFUN (bgp_damp_unset,
        "no bgp dampening [(1-45) [(1-20000) (1-20000) (1-255)]]",
        NO_STR
        "BGP Specific commands\n"
-       "Enable route-flap dampening\n")
+       "Enable route-flap dampening\n"
+       "Half-life time for the penalty\n"
+       "Value to start reusing a route\n"
+       "Value to start suppressing a route\n"
+       "Maximum duration to suppress a stable route\n")
 {
   struct bgp *bgp;
 

bgpd/bgp_vty.c
@@ -2387,7 +2387,9 @@ DEFUN (bgp_listen_range,
        "BGP specific commands\n"
        "Configure BGP Dynamic Neighbors\n"
        "add a listening range for Dynamic Neighbors\n"
-       LISTEN_RANGE_ADDR_STR)
+       NEIGHBOR_ADDR_STR
+       "Member of the peer-group\n"
+       "Peer-group name\n")
 {
   int idx_ipv4_ipv6_prefixlen = 3;
   int idx_word = 5;
@@ -2762,7 +2764,7 @@ DEFUN (neighbor_interface_config,
        "Interface name or neighbor tag\n"
        "Enable BGP on interface\n"
        "Member of the peer-group\n"
-       "peer-group name\n")
+       "Peer-group name\n")
 {
   int idx_word = 1;
   int idx_peer_group_word = 4;
@@ -2783,7 +2785,7 @@ DEFUN (neighbor_interface_config_v6only,
        "Enable BGP on interface\n"
        "Enable BGP with v6 link-local only\n"
        "Member of the peer-group\n"
-       "peer-group name\n")
+       "Peer-group name\n")
 {
   int idx_word = 1;
   int idx_peer_group_word = 5;
@@ -2920,7 +2922,8 @@ DEFUN (no_neighbor_interface_config,
        "Configure BGP on interface\n"
        "Enable BGP with v6 link-local only\n"
        "Member of the peer-group\n"
-       "peer-group name\n"
+       "Peer-group name\n"
+       "Specify remote AS\n"
        AS_STR)
 {
   int idx_word = 2;
@@ -3155,7 +3158,8 @@ DEFUN (no_neighbor_password,
        NO_STR
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
-       "Set a password\n")
+       "Set a password\n"
+       "The password\n")
 {
   int idx_peer = 2;
   struct peer *peer;
@@ -3222,7 +3226,7 @@ DEFUN (neighbor_set_peer_group,
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
        "Member of the peer-group\n"
-       "peer-group name\n")
+       "Peer-group name\n")
 {
   int idx_peer = 1;
   int idx_word = 3;
@@ -3290,7 +3294,7 @@ DEFUN (no_neighbor_set_peer_group,
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
        "Member of the peer-group\n"
-       "peer-group name\n")
+       "Peer-group name\n")
 {
   int idx_peer = 2;
   int idx_word = 4;
@@ -3708,7 +3712,7 @@ DEFUN (neighbor_remove_private_as_all_replace_as,
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
        "Remove private ASNs in outbound updates\n"
-       "Apply to all AS numbers"
+       "Apply to all AS numbers\n"
        "Replace private ASNs with our ASN in outbound updates\n")
 {
   int idx_peer = 1;
@@ -3738,7 +3742,7 @@ DEFUN (no_neighbor_remove_private_as_all,
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
        "Remove private ASNs in outbound updates\n"
-       "Apply to all AS numbers")
+       "Apply to all AS numbers\n")
 {
   int idx_peer = 2;
   return peer_af_flag_unset_vty (vty, argv[idx_peer]->arg, bgp_node_afi (vty),
@@ -3768,7 +3772,7 @@ DEFUN (no_neighbor_remove_private_as_all_replace_as,
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
        "Remove private ASNs in outbound updates\n"
-       "Apply to all AS numbers"
+       "Apply to all AS numbers\n"
        "Replace private ASNs with our ASN in outbound updates\n")
 {
   int idx_peer = 2;
@@ -4771,7 +4775,7 @@ DEFUN (no_neighbor_interface,
        "no neighbor <A.B.C.D|X:X::X:X|WORD> interface WORD",
        NO_STR
        NEIGHBOR_STR
-       NEIGHBOR_ADDR_STR
+       NEIGHBOR_ADDR_STR2
        "Interface\n"
        "Interface name\n")
 {
@@ -5311,11 +5315,11 @@ DEFUN (neighbor_maximum_prefix_threshold_restart,
        "neighbor <A.B.C.D|X:X::X:X|WORD> maximum-prefix (1-4294967295) (1-100) restart (1-65535)",
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
-       "Maximum number of prefix accept from this peer\n"
+       "Maximum number of prefixes to accept from this peer\n"
        "maximum no. of prefix limit\n"
        "Threshold value (%) at which to generate a warning msg\n"
        "Restart bgp connection after limit is exceeded\n"
-       "Restart interval in minutes")
+       "Restart interval in minutes\n")
 {
   int idx_peer = 1;
   int idx_number = 3;
@@ -5331,11 +5335,11 @@ DEFUN (no_neighbor_maximum_prefix,
        NO_STR
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
-       "Maximum number of prefix accept from this peer\n"
+       "Maximum number of prefixes to accept from this peer\n"
        "maximum no. of prefix limit\n"
        "Threshold value (%) at which to generate a warning msg\n"
        "Restart bgp connection after limit is exceeded\n"
-       "Restart interval in minutes"
+       "Restart interval in minutes\n"
        "Only give warning message when limit is exceeded\n")
 {
   int idx_peer = 2;
@@ -5409,6 +5413,7 @@ DEFUN (neighbor_ttl_security,
        "neighbor <A.B.C.D|X:X::X:X|WORD> ttl-security hops (1-254)",
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
+       "BGP ttl-security parameters\n"
        "Specify the maximum number of hops to the BGP peer\n")
 {
   int idx_peer = 1;
@@ -5441,6 +5446,7 @@ DEFUN (no_neighbor_ttl_security,
        NO_STR
        NEIGHBOR_STR
        NEIGHBOR_ADDR_STR2
+       "BGP ttl-security parameters\n"
        "Specify the maximum number of hops to the BGP peer\n")
 {
   int idx_peer = 2;
@@ -5885,6 +5891,7 @@ DEFUN (clear_bgp_ipv6_safi_prefix,
        BGP_STR
        "Address Family\n"
        "Address Family Modifier\n"
+       "Address Family Modifier\n"
        "Clear bestpath and re-advertise\n"
        "IPv6 prefix\n")
 {
@@ -5903,6 +5910,7 @@ DEFUN (clear_bgp_instance_ipv6_safi_prefix,
        BGP_STR
        BGP_INSTANCE_HELP_STR
        "Address Family\n"
+       "Address Family Modifier\n"
        "Address Family Modifier\n"
        "Clear bestpath and re-advertise\n"
        "IPv6 prefix\n")

isisd/isis_vty.c
@@ -1394,6 +1394,7 @@ DEFUN (set_attached_bit,
 DEFUN (no_set_attached_bit,
        no_set_attached_bit_cmd,
        "no set-attached-bit",
+       NO_STR
        "Reset attached bit\n")
 {
   VTY_DECLVAR_CONTEXT (isis_area, area);
@@ -1807,68 +1808,44 @@ area_max_lsp_lifetime_set(struct vty *vty, int level,
 
 DEFUN (max_lsp_lifetime,
        max_lsp_lifetime_cmd,
-       "max-lsp-lifetime (350-65535)",
-       "Maximum LSP lifetime\n"
+       "max-lsp-lifetime [<level-1|level-2>] (350-65535)",
+       "Maximum LSP lifetime for Level 1 only\n"
+       "Maximum LSP lifetime for Level 2 only\n"
        "LSP lifetime in seconds\n")
 {
-  int idx_number = 1;
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_1_AND_2, atoi(argv[idx_number]->arg));
+  int idx = 0;
+  unsigned int level = IS_LEVEL_1_AND_2;
+
+  if (argv_find (argv, argc, "level-1", &idx))
+    level = IS_LEVEL_1;
+  else if (argv_find (argv, argc, "level-2", &idx))
+    level = IS_LEVEL_2;
+
+  argv_find (argv, argc, "(350-65535)", &idx);
+  int lifetime = atoi(argv[idx]->arg);
+
+  return area_max_lsp_lifetime_set(vty, level, lifetime);
 }
 
 
 DEFUN (no_max_lsp_lifetime,
        no_max_lsp_lifetime_cmd,
-       "no max-lsp-lifetime [(350-65535)]",
+       "no max-lsp-lifetime [<level-1|level-2>] [(350-65535)]",
        NO_STR
-       "Maximum LSP lifetime\n"
+       "Maximum LSP lifetime for Level 1 only\n"
+       "Maximum LSP lifetime for Level 2 only\n"
        "LSP lifetime in seconds\n")
 {
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_1_AND_2,
-				   DEFAULT_LSP_LIFETIME);
+  int idx = 0;
+  unsigned int level = IS_LEVEL_1_AND_2;
+
+  if (argv_find (argv, argc, "level-1", &idx))
+    level = IS_LEVEL_1;
+  else if (argv_find (argv, argc, "level-2", &idx))
+    level = IS_LEVEL_2;
+
+  return area_max_lsp_lifetime_set(vty, level, DEFAULT_LSP_LIFETIME);
 }
-
-
-DEFUN (max_lsp_lifetime_l1,
-       max_lsp_lifetime_l1_cmd,
-       "max-lsp-lifetime level-1 (350-65535)",
-       "Maximum LSP lifetime for Level 1 only\n"
-       "LSP lifetime for Level 1 only in seconds\n")
-{
-  int idx_number = 2;
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_1, atoi(argv[idx_number]->arg));
-}
-
-
-DEFUN (no_max_lsp_lifetime_l1,
-       no_max_lsp_lifetime_l1_cmd,
-       "no max-lsp-lifetime level-1 [(350-65535)]",
-       NO_STR
-       "LSP lifetime for Level 1 only in seconds\n")
-{
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_1, DEFAULT_LSP_LIFETIME);
-}
-
-
-DEFUN (max_lsp_lifetime_l2,
-       max_lsp_lifetime_l2_cmd,
-       "max-lsp-lifetime level-2 (350-65535)",
-       "Maximum LSP lifetime for Level 2 only\n"
-       "LSP lifetime for Level 2 only in seconds\n")
-{
-  int idx_number = 2;
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_2, atoi(argv[idx_number]->arg));
-}
-
-
-DEFUN (no_max_lsp_lifetime_l2,
-       no_max_lsp_lifetime_l2_cmd,
-       "no max-lsp-lifetime level-2 [(350-65535)]",
-       NO_STR
-       "LSP lifetime for Level 2 only in seconds\n")
-{
-  return area_max_lsp_lifetime_set(vty, IS_LEVEL_2, DEFAULT_LSP_LIFETIME);
-}
-
 
 static int
 area_lsp_refresh_interval_set(struct vty *vty, int level, uint16_t interval)
@@ -1910,72 +1887,44 @@ area_lsp_refresh_interval_set(struct vty *vty, int level, uint16_t interval)
 
 DEFUN (lsp_refresh_interval,
        lsp_refresh_interval_cmd,
-       "lsp-refresh-interval (1-65235)",
+       "lsp-refresh-interval [<level-1|level-2>] (1-65235)",
        "LSP refresh interval\n"
+       "LSP refresh interval for Level 1 only\n"
+       "LSP refresh interval for Level 2 only\n"
        "LSP refresh interval in seconds\n")
 {
-  int idx_number = 1;
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_1_AND_2, atoi(argv[idx_number]->arg));
-}
+  int idx = 0;
+  unsigned int level = IS_LEVEL_1_AND_2;
+  unsigned int interval = 0;
 
+  if (argv_find (argv, argc, "level-1", &idx))
+    level = IS_LEVEL_1;
+  else if (argv_find (argv, argc, "level-2", &idx))
+    level = IS_LEVEL_2;
+
+  interval = atoi(argv[argc-1]->arg);
+  return area_lsp_refresh_interval_set(vty, level, interval);
+}
 
 DEFUN (no_lsp_refresh_interval,
        no_lsp_refresh_interval_cmd,
-       "no lsp-refresh-interval [(1-65235)]",
+       "no lsp-refresh-interval [<level-1|level-2>] [(1-65235)]",
        NO_STR
        "LSP refresh interval\n"
+       "LSP refresh interval for Level 1 only\n"
+       "LSP refresh interval for Level 2 only\n"
        "LSP refresh interval in seconds\n")
 {
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_1_AND_2,
-				       DEFAULT_MAX_LSP_GEN_INTERVAL);
+  int idx = 0;
+  unsigned int level = IS_LEVEL_1_AND_2;
+
+  if (argv_find (argv, argc, "level-1", &idx))
+    level = IS_LEVEL_1;
+  else if (argv_find (argv, argc, "level-2", &idx))
+    level = IS_LEVEL_2;
+
+  return area_lsp_refresh_interval_set(vty, level, DEFAULT_MAX_LSP_GEN_INTERVAL);
 }
-
-
-DEFUN (lsp_refresh_interval_l1,
-       lsp_refresh_interval_l1_cmd,
-       "lsp-refresh-interval level-1 (1-65235)",
-       "LSP refresh interval for Level 1 only\n"
-       "LSP refresh interval for Level 1 only in seconds\n")
-{
-  int idx_number = 2;
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_1, atoi(argv[idx_number]->arg));
-}
-
-
-DEFUN (no_lsp_refresh_interval_l1,
-       no_lsp_refresh_interval_l1_cmd,
-       "no lsp-refresh-interval level-1 [(1-65235)]",
-       NO_STR
-       "LSP refresh interval for Level 1 only\n"
-       "LSP refresh interval for Level 1 only in seconds\n")
-{
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_1,
-				       DEFAULT_MAX_LSP_GEN_INTERVAL);
-}
-
-
-DEFUN (lsp_refresh_interval_l2,
-       lsp_refresh_interval_l2_cmd,
-       "lsp-refresh-interval level-2 (1-65235)",
-       "LSP refresh interval for Level 2 only\n"
-       "LSP refresh interval for Level 2 only in seconds\n")
-{
-  int idx_number = 2;
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_2, atoi(argv[idx_number]->arg));
-}
-
-
-DEFUN (no_lsp_refresh_interval_l2,
-       no_lsp_refresh_interval_l2_cmd,
-       "no lsp-refresh-interval level-2 [(1-65235)]",
-       NO_STR
-       "LSP refresh interval for Level 2 only\n"
-       "LSP refresh interval for Level 2 only in seconds\n")
-{
-  return area_lsp_refresh_interval_set(vty, IS_LEVEL_2,
-				       DEFAULT_MAX_LSP_GEN_INTERVAL);
-}
-
 
 static int
 area_passwd_set(struct vty *vty, int level,
@@ -2192,17 +2141,9 @@ isis_vty_init (void)
 
   install_element (ISIS_NODE, &max_lsp_lifetime_cmd);
   install_element (ISIS_NODE, &no_max_lsp_lifetime_cmd);
-  install_element (ISIS_NODE, &max_lsp_lifetime_l1_cmd);
-  install_element (ISIS_NODE, &no_max_lsp_lifetime_l1_cmd);
-  install_element (ISIS_NODE, &max_lsp_lifetime_l2_cmd);
-  install_element (ISIS_NODE, &no_max_lsp_lifetime_l2_cmd);
 
   install_element (ISIS_NODE, &lsp_refresh_interval_cmd);
   install_element (ISIS_NODE, &no_lsp_refresh_interval_cmd);
-  install_element (ISIS_NODE, &lsp_refresh_interval_l1_cmd);
-  install_element (ISIS_NODE, &no_lsp_refresh_interval_l1_cmd);
-  install_element (ISIS_NODE, &lsp_refresh_interval_l2_cmd);
-  install_element (ISIS_NODE, &no_lsp_refresh_interval_l2_cmd);
 
   install_element (ISIS_NODE, &area_passwd_md5_cmd);
   install_element (ISIS_NODE, &area_passwd_clear_cmd);

isisd/isisd.c
@@ -907,6 +907,7 @@ DEFUN (debug_isis_adj,
 DEFUN (no_debug_isis_adj,
        no_debug_isis_adj_cmd,
        "no debug isis adj-packets",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS Adjacency related packets\n")
@@ -933,6 +934,7 @@ DEFUN (debug_isis_csum,
 DEFUN (no_debug_isis_csum,
        no_debug_isis_csum_cmd,
        "no debug isis checksum-errors",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS LSP checksum errors\n")
@@ -959,6 +961,7 @@ DEFUN (debug_isis_lupd,
 DEFUN (no_debug_isis_lupd,
        no_debug_isis_lupd_cmd,
        "no debug isis local-updates",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS local update packets\n")
@@ -985,6 +988,7 @@ DEFUN (debug_isis_err,
 DEFUN (no_debug_isis_err,
        no_debug_isis_err_cmd,
        "no debug isis protocol-errors",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS LSP protocol errors\n")
@@ -1011,6 +1015,7 @@ DEFUN (debug_isis_snp,
 DEFUN (no_debug_isis_snp,
        no_debug_isis_snp_cmd,
        "no debug isis snp-packets",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS CSNP/PSNP packets\n")
@@ -1037,6 +1042,7 @@ DEFUN (debug_isis_upd,
 DEFUN (no_debug_isis_upd,
        no_debug_isis_upd_cmd,
        "no debug isis update-packets",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS Update related packets\n")
@@ -1063,6 +1069,7 @@ DEFUN (debug_isis_spfevents,
 DEFUN (no_debug_isis_spfevents,
        no_debug_isis_spfevents_cmd,
        "no debug isis spf-events",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS Shortest Path First Events\n")
@@ -1089,6 +1096,7 @@ DEFUN (debug_isis_spfstats,
 DEFUN (no_debug_isis_spfstats,
        no_debug_isis_spfstats_cmd,
        "no debug isis spf-statistics",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS SPF Timing and Statistic Data\n")
@@ -1115,6 +1123,7 @@ DEFUN (debug_isis_spftrigg,
 DEFUN (no_debug_isis_spftrigg,
        no_debug_isis_spftrigg_cmd,
        "no debug isis spf-triggers",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS SPF triggering events\n")
@@ -1141,6 +1150,7 @@ DEFUN (debug_isis_rtevents,
 DEFUN (no_debug_isis_rtevents,
        no_debug_isis_rtevents_cmd,
        "no debug isis route-events",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS Route related events\n")
@@ -1167,6 +1177,7 @@ DEFUN (debug_isis_events,
 DEFUN (no_debug_isis_events,
        no_debug_isis_events_cmd,
        "no debug isis events",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS Events\n")
@@ -1193,6 +1204,7 @@ DEFUN (debug_isis_packet_dump,
 DEFUN (no_debug_isis_packet_dump,
        no_debug_isis_packet_dump_cmd,
        "no debug isis packet-dump",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS packet dump\n")
@@ -1219,6 +1231,7 @@ DEFUN (debug_isis_lsp_gen,
 DEFUN (no_debug_isis_lsp_gen,
        no_debug_isis_lsp_gen_cmd,
        "no debug isis lsp-gen",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS generation of own LSPs\n")
@@ -1245,6 +1258,7 @@ DEFUN (debug_isis_lsp_sched,
 DEFUN (no_debug_isis_lsp_sched,
        no_debug_isis_lsp_sched_cmd,
        "no debug isis lsp-sched",
+       NO_STR
        UNDEBUG_STR
        "IS-IS information\n"
        "IS-IS scheduling of LSP generation\n")

lib/command.h
@@ -363,7 +363,6 @@ struct cmd_element
 #define IFNAME_STR "Interface name(e.g. ep0)\n"
 #define IP6_STR "IPv6 Information\n"
 #define OSPF6_STR "Open Shortest Path First (OSPF) for IPv6\n"
-#define OSPF6_ROUTER_STR "Enable a routing process\n"
 #define OSPF6_INSTANCE_STR "(1-65535) Instance ID\n"
 #define SECONDS_STR "(1-65535) Seconds\n"
 #define ROUTE_STR "Routing Table\n"
@@ -391,13 +390,6 @@ struct cmd_element
 #else
 #define NEIGHBOR_ADDR_STR  "Neighbor address\n"
 #define NEIGHBOR_ADDR_STR2 "Neighbor address\nNeighbor tag\n"
-#endif /* HAVE_IPV6 */
-
-/* Dynamic neighbor (listen range) configuration */
-#ifdef HAVE_IPV6
-#define LISTEN_RANGE_ADDR_STR "Neighbor address\nNeighbor IPv6 address\n"
-#else
-#define LISTEN_RANGE_ADDR_STR "Neighbor address\n"
 #endif /* HAVE_IPV6 */
 
 /* Prototypes. */

ospf6d/ospf6_lsa.c
@@ -866,8 +866,15 @@ DEFUN (no_debug_ospf6_lsa_type,
        DEBUG_STR
        OSPF6_STR
        "Debug Link State Advertisements (LSAs)\n"
-       "Specify LS type as Hexadecimal\n"
-      )
+       "Display Router LSAs\n"
+       "Display Network LSAs\n"
+       "Display Inter-Area-Prefix LSAs\n"
+       "Display As-External LSAs\n"
+       "Display Link LSAs\n"
+       "Display Intra-Area-Prefix LSAs\n"
+       "Display details of LSAs\n"
+       "Dump LSAs\n"
+       "Display LSA's internal information\n")
 {
   int idx_lsa = 4;
   int idx_type = 5;

ospf6d/ospf6_route.c
@@ -1567,8 +1567,8 @@ DEFUN (debug_ospf6_route,
        "debug ospf6 route <table|intra-area|inter-area|memory>",
        DEBUG_STR
        OSPF6_STR
+       "Debug routes\n"
        "Debug route table calculation\n"
-       "Debug detail\n"
        "Debug intra-area route calculation\n"
        "Debug inter-area route calculation\n"
        "Debug route memory use\n"
@@ -1595,8 +1595,10 @@ DEFUN (no_debug_ospf6_route,
        NO_STR
        DEBUG_STR
        OSPF6_STR
+       "Debug routes\n"
        "Debug route table calculation\n"
        "Debug intra-area route calculation\n"
+       "Debug inter-area route calculation\n"
        "Debug route memory use\n")
 {
   int idx_type = 4;

commit 34288970c42bcfaebc6d101e500c0fdad6420ff5

ospf6d: fix "no router ospf6"
The "no router ospf6" command wasn't working.

Regression introduced by commit 16cedbb.

Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

ospf6d/ospf6_top.c
@@ -315,6 +315,14 @@ DEFUN (no_router_ospf6,
        ROUTER_STR
        OSPF6_STR)
 {
+  if (ospf6 == NULL)
+    vty_out (vty, "OSPFv3 is not configured%s", VNL);
+  else
+    {
+      ospf6_delete (ospf6);
+      ospf6 = NULL;
+    }
+
   /* return to config node . */
   VTY_PUSH_CONTEXT_NULL(CONFIG_NODE);
 