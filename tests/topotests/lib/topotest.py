c63b6f86f07516f95cacc8fd07b31b67b777a6b9
Martin Winter <mwinter@opensourcerouting.org>
2017-04-26 13:22:34 -0700

    lib: Fix version check in previous kernel compare
    Signed-off-by: Martin Winter <mwinter@opensourcerouting.org>

--- tests/topotests/lib/topotest.py
+++ tests/topotests/lib/topotest.py
@@ -160,8 +160,8 @@ class Router(Node):
             kernel_version = re.search(r'([0-9]+)\.([0-9]+).*', platform.release())
 
             if kernel_version:
-                if float(kernel_version.group(1)) < 4 or
-                   (float(kernel_version.group(1)) == 4 and float(kernel.version.group(2)) < 5):
+                if (float(kernel_version.group(1)) < 4 or
+                   (float(kernel_version.group(1)) == 4 and float(kernel.version.group(2)) < 5)):
                     print("LDP Test need Linux Kernel 4.5 minimum")
                     return "LDP Test need Linux Kernel 4.5 minimum"
         # Add mpls modules to kernel if we use LDP
@@ -249,9 +249,10 @@ class Router(Node):
         if not os.path.isfile('/usr/lib/%s/%s' % (self.routertype, daemon)):
             return False
         if (daemon == 'ldpd'):
-            kernel_version = re.search(r'([0-9]+\.[0-9]+).*', platform.release())
+            kernel_version = re.search(r'([0-9]+)\.([0-9]+).*', platform.release())
             if kernel_version:
-                if float(kernel_version.group(1)) < 4.5:
+                if (float(kernel_version.group(1)) < 4 or
+                   (float(kernel_version.group(1)) == 4 and float(kernel.version.group(2)) < 5)):
                     return False
             else:
                 return False
