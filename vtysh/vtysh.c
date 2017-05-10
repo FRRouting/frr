08c369bda98a28e9bfb148f25749319648f61f1b
Donald Sharp <sharpd@cumulusnetworks.com>
2017-05-10 11:09:24 -0400

    *: Allow configure to control VTYSH_PAGER used
    Signed-off-by: Donald Sharp

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -264,7 +264,7 @@ vtysh_pager_init (void)
   if (pager_defined)
     vtysh_pager_name = strdup (pager_defined);
   else
-    vtysh_pager_name = strdup ("more");
+    vtysh_pager_name = strdup (VTYSH_PAGER);
 }
 
 /* Command execution over the vty interface. */
