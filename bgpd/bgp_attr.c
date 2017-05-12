3eefc7345412a9cfb13b6442549573c961561aa0
Renato Westphal <renato@opensourcerouting.org>
2017-05-12 11:36:14 -0300

    bgpd: remove unused bgp_attr_default_intern() function
    This function is not used anymore since commit 137446f9.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -968,26 +968,6 @@ bgp_attr_default_set (struct attr *attr, u_char origin)
   return attr;
 }
 
-
-/* Make network statement's attribute. */
-struct attr *
-bgp_attr_default_intern (u_char origin)
-{
-  struct attr attr;
-  struct attr *new;
-
-  memset (&attr, 0, sizeof (struct attr));
-  bgp_attr_extra_get (&attr);
-
-  bgp_attr_default_set(&attr, origin);
-
-  new = bgp_attr_intern (&attr);
-  bgp_attr_extra_free (&attr);
-  
-  aspath_unintern (&new->aspath);
-  return new;
-}
-
 /* Create the attributes for an aggregate */
 struct attr *
 bgp_attr_aggregate_intern (struct bgp *bgp, u_char origin,
