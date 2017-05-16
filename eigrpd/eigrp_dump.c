7298b8ccd59827de1f0777ed97532f5e29f01904
Donald Sharp <sharpd@cumulusnetworks.com>
2017-05-15 20:46:06 -0400

    eigrpd: Fix arm compile issue with some gcc's
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- eigrpd/eigrp_dump.c
+++ eigrpd/eigrp_dump.c
@@ -313,7 +313,7 @@ show_ip_eigrp_prefix_entry (struct vty *vty, struct eigrp_prefix_entry *tn)
   vty_out (vty, "%-3c",(tn->state > 0) ? 'A' : 'P');
   vty_out (vty, "%s/%u, ",inet_ntoa (tn->destination_ipv4->prefix),tn->destination_ipv4->prefixlen);
   vty_out (vty, "%u successors, ",eigrp_topology_get_successor(tn)->count);
-  vty_out (vty, "FD is %u, serno: %lu %s",tn->fdistance, tn->serno, VTY_NEWLINE);
+  vty_out (vty, "FD is %u, serno: %" PRIu64 " %s",tn->fdistance, tn->serno, VTY_NEWLINE);
 }
 
 void
