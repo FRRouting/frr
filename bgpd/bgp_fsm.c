vincentbernat/quagga

bgpd: Fix ADJCHANGE message to include more info
When bgp logs ADJCHANGE messages include the
hostname and vrf that this change is being made
in.

Ticket: CM-10922
Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

bgpd/bgp_fsm.c
@@ -1023,8 +1023,14 @@ bgp_stop (struct peer *peer)
 
       /* bgp log-neighbor-changes of neighbor Down */
       if (bgp_flag_check (peer->bgp, BGP_FLAG_LOG_NEIGHBOR_CHANGES))
-	zlog_info ("%%ADJCHANGE: neighbor %s Down %s", peer->host,
-                   peer_down_str [(int) peer->last_reset]);
+	{
+	  struct vrf *vrf = vrf_lookup (peer->bgp->vrf_id);
+	  zlog_info ("%%ADJCHANGE: neighbor %s(%s) in vrf %s Down %s",
+		     peer->host,
+		     (peer->hostname) ? peer->hostname : "Unknown",
+		     (vrf->vrf_id != VRF_DEFAULT) ? vrf->name : "Default",
+		     peer_down_str [(int) peer->last_reset]);
+	}
 
       /* graceful restart */
       if (peer->t_gr_stale)
@@ -1461,8 +1467,13 @@ bgp_establish (struct peer *peer)
 
   /* bgp log-neighbor-changes of neighbor Up */
   if (bgp_flag_check (peer->bgp, BGP_FLAG_LOG_NEIGHBOR_CHANGES))
-    zlog_info ("%%ADJCHANGE: neighbor %s Up", peer->host);
-
+    {
+      struct vrf *vrf = vrf_lookup (peer->bgp->vrf_id);
+      zlog_info ("%%ADJCHANGE: neighbor %s(%s) in vrf %s Up",
+		 peer->host,
+		 (peer->hostname) ? peer->hostname : "Unknown",
+		 (vrf->vrf_id != VRF_DEFAULT ) ? vrf->name : "Default");
+    }
   /* assign update-group/subgroup */
   update_group_adjust_peer_afs(peer);
 

FRRouting/frr

bgpd/bgp_fsm.c
@@ -1021,8 +1021,14 @@ bgp_stop (struct peer *peer)
 
       /* bgp log-neighbor-changes of neighbor Down */
       if (bgp_flag_check (peer->bgp, BGP_FLAG_LOG_NEIGHBOR_CHANGES))
-	zlog_info ("%%ADJCHANGE: neighbor %s Down %s", peer->host,
-                   peer_down_str [(int) peer->last_reset]);
+	{
+	  struct vrf *vrf = vrf_lookup_by_id (peer->bgp->vrf_id);
+	  zlog_info ("%%ADJCHANGE: neighbor %s(%s) in vrf %s Down %s",
+		     peer->host,
+		     (peer->hostname) ? peer->hostname : "Unknown",
+		     (vrf->vrf_id != VRF_DEFAULT) ? vrf->name : "Default",
+		     peer_down_str [(int) peer->last_reset]);
+	}
 
       /* graceful restart */
       if (peer->t_gr_stale)
@@ -1457,8 +1463,13 @@ bgp_establish (struct peer *peer)
 
   /* bgp log-neighbor-changes of neighbor Up */
   if (bgp_flag_check (peer->bgp, BGP_FLAG_LOG_NEIGHBOR_CHANGES))
-    zlog_info ("%%ADJCHANGE: neighbor %s Up", peer->host);
-
+    {
+      struct vrf *vrf = vrf_lookup_by_id (peer->bgp->vrf_id);
+      zlog_info ("%%ADJCHANGE: neighbor %s(%s) in vrf %s Up",
+		 peer->host,
+		 (peer->hostname) ? peer->hostname : "Unknown",
+		 (vrf->vrf_id != VRF_DEFAULT ) ? vrf->name : "Default");
+    }
   /* assign update-group/subgroup */
   update_group_adjust_peer_afs(peer);
 