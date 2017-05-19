f509d94116a761ff38b2382ce806985d98f429d8
Donald Sharp <sharpd@cumulusnetworks.com>
2017-05-19 11:14:28 -0400

    pimd: Fix mistake in uplifting to new thread model
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pim_mroute.c
+++ pimd/pim_mroute.c
@@ -679,7 +679,7 @@ done:
 
 static void mroute_read_on(struct pim_instance *pim)
 {
-	thread_add_read(master, mroute_read, 0, pimg->mroute_socket,
+	thread_add_read(master, mroute_read, pim, pim->mroute_socket,
 			&pim->thread);
 }
 
