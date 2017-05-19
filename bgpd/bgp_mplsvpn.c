d81e2370014b3b0fbf93d4c298aa639c3c635914
Renato Westphal <renato@opensourcerouting.org>
2017-05-19 10:49:24 -0300

    bgpd: remove unused str2tag() function
    This function is not used anymore since commit fb1d2a2d9.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -381,31 +381,6 @@ out:
   return lret;
 }
 
-int
-str2tag (const char *str, u_char *tag)
-{
-  unsigned long l;
-  char *endptr;
-  u_int32_t t;
-
-  if (*str == '-')
-    return 0;
-  
-  errno = 0;
-  l = strtoul (str, &endptr, 10);
-
-  if (*endptr != '\0' || errno || l > UINT32_MAX)
-    return 0;
-
-  t = (u_int32_t) l;
-  
-  tag[0] = (u_char)(t >> 12);
-  tag[1] = (u_char)(t >> 4);
-  tag[2] = (u_char)(t << 4);
-
-  return 1;
-}
-
 char *
 prefix_rd2str (struct prefix_rd *prd, char *buf, size_t size)
 {
