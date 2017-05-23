a5b82e0ced69da8f9bc744fb27a2eb2e0fd062ff
Jafar Al-Gharaibeh <jafar@atcorp.com>
2017-05-23 13:23:04 -0500

    vtysh: fix a compile error when user/group are disabled
    If FRR is configured explicilty with --disable-user and
    --disable-group, FRR_USER and FRR_GROUP will not be defined
    and can be safely skipped in vtysh.
    
    Signed-off-by: Jafar Al-Gharaibeh <jafar@atcorp.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -2628,6 +2628,7 @@ vtysh_write_config_integrated(void)
       err++;
     }
 
+#ifdef FRR_USER
   pwentry = getpwnam (FRR_USER);
   if (pwentry)
     uid = pwentry->pw_uid;
@@ -2636,7 +2637,8 @@ vtysh_write_config_integrated(void)
       printf ("%% Warning: could not look up user \"%s\"\n", FRR_USER);
       err++;
     }
-
+#endif
+#ifdef FRR_GROUP
   grentry = getgrnam (FRR_GROUP);
   if (grentry)
     gid = grentry->gr_gid;
@@ -2645,6 +2647,7 @@ vtysh_write_config_integrated(void)
       printf ("%% Warning: could not look up group \"%s\"\n", FRR_GROUP);
       err++;
     }
+#endif
 
   if (!fstat (fd, &st))
     {
