ea2e48899bb921dac10da1d8273a167f4a63678e
vivek <vivek@cumulusnetworks.com>
2017-05-23 22:52:53 -0700

    bgpd: Fix route detailed show for EVPN
    Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -7184,7 +7184,8 @@ route_vty_out_detail (struct vty *vty, struct bgp *bgp, struct prefix *p,
 	  
       /* Line2 display Next-hop, Neighbor, Router-id */
       /* Display the nexthop */
-      if (p->family == AF_INET &&
+      if ((p->family == AF_INET ||
+           p->family == AF_ETHERNET) &&
           (safi == SAFI_MPLS_VPN ||
            safi == SAFI_ENCAP ||
            safi == SAFI_EVPN ||
