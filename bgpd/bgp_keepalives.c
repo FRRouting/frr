5c0c651c0a021675e8551b10b563e5dd721df6c2
Quentin Young <qlyoung@cumulusnetworks.com>
2017-06-09 19:22:34 +0000

    bgpd: restyle bgp_keepalives.[ch]
    And update copyright header.
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- bgpd/bgp_keepalives.c
+++ bgpd/bgp_keepalives.c
@@ -1,27 +1,25 @@
-/*
-  BGP Keepalives.
-
-  Implements a producer thread to generate BGP keepalives for peers.
-  ----------------------------------------
-  Copyright (C) 2017 Cumulus Networks, Inc.
-  Quentin Young
-
-  This file is part of FRRouting.
-
-  FRRouting is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free
-  Software Foundation; either version 2, or (at your option) any later
-  version.
-
-  FRRouting is distributed in the hope that it will be useful, but WITHOUT ANY
-  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-  details.
-
-  You should have received a copy of the GNU General Public License along with
-  FRRouting; see the file COPYING.  If not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+/* BGP Keepalives.
+ * Implements a producer thread to generate BGP keepalives for peers.
+ * Copyright (C) 2017 Cumulus Networks, Inc.
+ * Quentin Young
+ *
+ * This file is part of FRRouting.
+ *
+ * FRRouting is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
+ *
+ * FRRouting is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 #include <zebra.h>
 #include <signal.h>
 #include <sys/time.h>
--- bgpd/bgp_keepalives.h
+++ bgpd/bgp_keepalives.h
@@ -1,38 +1,30 @@
 /* BGP Keepalives.
- *
- * Implemented server-style in a pthread.
- * --------------------------------------
+ * Implements a producer thread to generate BGP keepalives for peers.
  * Copyright (C) 2017 Cumulus Networks, Inc.
+ * Quentin Young
  *
- * This file is part of Free Range Routing.
+ * This file is part of FRRouting.
  *
- * Free Range Routing is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any later
+ * FRRouting is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
  * version.
  *
- * Free Range Routing is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * FRRouting is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GN5U General Public License along
- * with Free Range Routing; see the file COPYING.  If not, write to the Free
- * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 #ifndef _BGP_KEEPALIVES_H_
 #define _BGP_KEEPALIVES_H_
 
 #include "frr_pthread.h"
 #include "bgpd.h"
-
-/* Thread control flag.
- *
- * Setting this flag to 'false' while the thread is running will result in
- * thread termination.
- */
-extern bool bgp_keepalives_thread_run;
 
 /* Turns on keepalives for a peer.
  *
@@ -88,7 +80,9 @@ extern void *bgp_keepalives_start(void *arg);
  */
 extern void bgp_keepalives_wake(void);
 
-/* stop function */
+/**
+ * Stops the thread and blocks until it terminates.
+ */
 int bgp_keepalives_stop(void **result, struct frr_pthread *fpt);
 
 #endif /* _BGP_KEEPALIVES_H */
