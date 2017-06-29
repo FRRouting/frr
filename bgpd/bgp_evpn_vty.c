5bc2ed52f0b55615a24d788e9529ddb630fd9bc8
Mitesh Kanjariya <mitesh@marvel-07.cumulusnetworks.com>
2017-06-29 14:16:49 -0700

    bgpd: fix crash while configuring rt
    Ticket: CM-16410
    Review: CCR-6409
    Unit-test: Manual
    
    Signed-off-by: Mitesh Kanjariya <mitesh@cumulusnetworks.com>

--- bgpd/bgp_evpn_vty.c
+++ bgpd/bgp_evpn_vty.c
@@ -2563,11 +2563,11 @@ DEFUN (bgp_evpn_vni_rt,
 	if (rt_type == RT_TYPE_BOTH || rt_type == RT_TYPE_IMPORT) {
 		ecomadd = ecommunity_str2com(argv[2]->arg,
 					     ECOMMUNITY_ROUTE_TARGET, 0);
-		ecommunity_str(ecomadd);
 		if (!ecomadd) {
 			vty_out(vty, "%% Malformed Route Target list\n");
 			return CMD_WARNING;
 		}
+		ecommunity_str(ecomadd);
 
 		/* Do nothing if we already have this import route-target */
 		if (!bgp_evpn_rt_matches_existing(vpn->import_rtl, ecomadd))
@@ -2578,11 +2578,11 @@ DEFUN (bgp_evpn_vni_rt,
 	if (rt_type == RT_TYPE_BOTH || rt_type == RT_TYPE_EXPORT) {
 		ecomadd = ecommunity_str2com(argv[2]->arg,
 					     ECOMMUNITY_ROUTE_TARGET, 0);
-		ecommunity_str(ecomadd);
 		if (!ecomadd) {
 			vty_out(vty, "%% Malformed Route Target list\n");
 			return CMD_WARNING;
 		}
+		ecommunity_str(ecomadd);
 
 		/* Do nothing if we already have this export route-target */
 		if (!bgp_evpn_rt_matches_existing(vpn->export_rtl, ecomadd))
@@ -2645,11 +2645,11 @@ DEFUN (no_bgp_evpn_vni_rt,
 	}
 
 	ecomdel = ecommunity_str2com(argv[3]->arg, ECOMMUNITY_ROUTE_TARGET, 0);
-	ecommunity_str(ecomdel);
 	if (!ecomdel) {
 		vty_out(vty, "%% Malformed Route Target list\n");
 		return CMD_WARNING;
 	}
+	ecommunity_str(ecomdel);
 
 	if (rt_type == RT_TYPE_IMPORT) {
 		if (!bgp_evpn_rt_matches_existing(vpn->import_rtl, ecomdel)) {
