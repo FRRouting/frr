FRRouting/frr

commit 4d5f445750e01467898eee47796e80d808500d56

lib: add vty_outln()
Like *.println() in some other unspeakable languages

Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

lib/vty.c
@@ -92,28 +92,23 @@ char integrate_default[] = SYSCONFDIR INTEGRATE_DEFAULT_CONFIG;
 
 static int do_log_commands = 0;
 
-/* VTY standard output function. */
-int
-vty_out (struct vty *vty, const char *format, ...)
+static int
+vty_out_variadic (struct vty *vty, const char *format, va_list args)
 {
-  va_list args;
   int len = 0;
   int size = 1024;
   char buf[1024];
   char *p = NULL;
+  va_list cp;
 
   if (vty_shell (vty))
-    {
-      va_start (args, format);
-      vprintf (format, args);
-      va_end (args);
-    }
+    vprintf (format, args);
   else
     {
       /* Try to write to initial buffer.  */
-      va_start (args, format);
+      va_copy (cp, args);
       len = vsnprintf (buf, sizeof(buf), format, args);
-      va_end (args);
+      va_end (cp);
 
       /* Initial buffer is not enough.  */
       if (len < 0 || len >= size)
@@ -129,9 +124,7 @@ vty_out (struct vty *vty, const char *format, ...)
               if (! p)
                 return -1;
 
-              va_start (args, format);
               len = vsnprintf (p, size, format, args);
-              va_end (args);
 
               if (len > -1 && len < size)
                 break;
@@ -151,6 +144,32 @@ vty_out (struct vty *vty, const char *format, ...)
     }
 
   return len;
+}
+/* VTY standard output function. */
+int
+vty_out (struct vty *vty, const char *format, ...)
+{
+  int len;
+  va_list args;
+
+  va_start (args, format);
+  len = vty_out_variadic (vty, format, args);
+  va_end (args);
+
+  return len;
+}
+
+int
+vty_outln (struct vty *vty, const char *format, ...)
+{
+  int len;
+  va_list args;
+
+  va_start (args, format);
+  len = vty_out_variadic (vty, format, args);
+  va_end (args);
+
+  return len + vty_out (vty, "%s", VTY_NEWLINE);
 }
 
 static int

lib/vty.h
@@ -215,6 +215,7 @@ extern void vty_reset (void);
 extern struct vty *vty_new (void);
 extern struct vty *vty_stdio (void (*atclose)(void));
 extern int vty_out (struct vty *, const char *, ...) PRINTF_ATTRIBUTE(2, 3);
+extern int vty_outln (struct vty *, const char *, ...) PRINTF_ATTRIBUTE(2, 3);
 extern void vty_read_config (const char *, char *);
 extern void vty_time_print (struct vty *, int);
 extern void vty_serv_sock (const char *, unsigned short, const char *);

commit ecf3d1b9d5ff9bf11a9e79a8e4efb63eff4eb10e

lib: fix vty_out with >1024 bytes of output
Consuming va_args modifies its internal bits, hence the need to copy
it... but the copying wasn't quite right just yet.

Fixes: 4d5f445 ("lib: add vty_outln()")
Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

lib/vty.c
@@ -107,7 +107,7 @@ vty_out_variadic (struct vty *vty, const char *format, va_list args)
     {
       /* Try to write to initial buffer.  */
       va_copy (cp, args);
-      len = vsnprintf (buf, sizeof(buf), format, args);
+      len = vsnprintf (buf, sizeof(buf), format, cp);
       va_end (cp);
 
       /* Initial buffer is not enough.  */
@@ -124,7 +124,9 @@ vty_out_variadic (struct vty *vty, const char *format, va_list args)
               if (! p)
                 return -1;
 
-              len = vsnprintf (p, size, format, args);
+              va_copy (cp, args);
+              len = vsnprintf (p, size, format, cp);
+              va_end (cp);
 
               if (len > -1 && len < size)
                 break;