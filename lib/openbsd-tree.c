FRRouting/frr

commit 8f942af9026f306e2eeca322b019e2c3c17d8000

lib: fix a possible NULL deference
Silences a warning generated by clang.

lib/openbsd-tree.c
@@ -222,6 +222,10 @@ rbe_remove_color(const struct rb_type *t, struct rb_tree *rbt,
 {
 	struct rb_entry *tmp;
 
+	/* Silence clang possible NULL deference warning. */
+	if (parent == NULL)
+		return;
+
 	while ((rbe == NULL || RBE_COLOR(rbe) == RB_BLACK) &&
 	    rbe != RBH_ROOT(rbt)) {
 		if (RBE_LEFT(parent) == rbe) {

commit 145b205177ab47860a78b68329dd4b0ac0d19036

lib: fix corrupted RB trees
Commit 8f942af90 introduced a bug while silencing a clang warning. Silence
the warning in a different way to fix our red-black tree implementation.

Fixes #841.

Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

lib/openbsd-tree.c
@@ -222,12 +222,8 @@ rbe_remove_color(const struct rb_type *t, struct rbt_tree *rbt,
 {
 	struct rb_entry *tmp;
 
-	/* Silence clang possible NULL deference warning. */
-	if (parent == NULL)
-		return;
-
 	while ((rbe == NULL || RBE_COLOR(rbe) == RB_BLACK) &&
-	    rbe != RBH_ROOT(rbt)) {
+	    rbe != RBH_ROOT(rbt) && parent) {
 		if (RBE_LEFT(parent) == rbe) {
 			tmp = RBE_RIGHT(parent);
 			if (RBE_COLOR(tmp) == RB_RED) {