cd49efdac87a4373c78adeb5170ca5298b6c8385
Renato Westphal <renato@opensourcerouting.org>
2017-07-24 21:39:55 -0300

    ldpd: restore correct defaults on "no discovery ... interval"
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- ldpd/ldp_vty_conf.c
+++ ldpd/ldp_vty_conf.c
@@ -582,11 +582,12 @@ ldp_vty_disc_interval(struct vty *vty, int disable, const char *hello_type_str,
 		if (disable) {
 			switch (hello_type) {
 			case HELLO_LINK:
-				vty_conf->lhello_interval = LINK_DFLT_HOLDTIME;
+				vty_conf->lhello_interval =
+				    DEFAULT_HELLO_INTERVAL;
 				break;
 			case HELLO_TARGETED:
 				vty_conf->thello_interval =
-				    TARGETED_DFLT_HOLDTIME;
+				    DEFAULT_HELLO_INTERVAL;
 				break;
 			}
 		} else {
