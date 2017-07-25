a83a533139f64434b68ebc3ae9a65a013b04cc34
Quentin Young <qlyoung@cumulusnetworks.com>
2017-07-25 17:28:19 -0400

    lib: add CLI node names
    Adds an array of descriptive names for each CLI node, plus a runtime
    check to make sure folks don't forget to update it.
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/command.h
+++ lib/command.h
@@ -68,7 +68,7 @@ struct host {
 	char *motdfile;
 };
 
-/* There are some command levels which called from command node. */
+/* List of CLI nodes. Please remember to update the names array below. */
 enum node_type {
 	AUTH_NODE,		/* Authentication mode of vty interface. */
 	VIEW_NODE,		/* View node. Default mode of vty interface. */
@@ -135,7 +135,10 @@ enum node_type {
 	VTY_NODE,		/* Vty node. */
 	LINK_PARAMS_NODE,       /* Link-parameters node */
 	BGP_EVPN_VNI_NODE,      /* BGP EVPN VNI */
+	NODE_TYPE_MAX,		/* maximum */
 };
+
+extern const char *node_names[];
 
 /* Node which has some commands and prompt string and configuration
    function pointer . */
