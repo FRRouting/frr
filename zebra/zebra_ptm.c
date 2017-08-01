c0320f438f9633488978d6556790692ecb0f0eff
radhika <radhika@cumulusnetworks.com>
2017-08-01 10:10:35 -0700

    Zebra: Fix ptm-enable config failure on internally created interfaces
    Ticket: CM-15658
    Reviewed By: CCR-6534
    Testing Done: Unit
    
    Issue: frr ptm-enable command not working for interfaces that have been created by frr as a place holder.
    
    Root Cause: The ptm-enable on interface configuration was not getting stored when the interface was internally created by frr.
    
    Fix: Store the ptm-enable configuration even if the interface is internally created.
    
    Signed-off-by: Radhika Mahankali <radhika@cumulusnetworks.com>

--- zebra/zebra_ptm.c
+++ zebra/zebra_ptm.c
@@ -302,6 +302,9 @@ DEFUN (zebra_ptm_enable_if,
 	int old_ptm_enable;
 	int send_linkdown = 0;
 
+	if_data = ifp->info;
+	if_data->ptm_enable = ZEBRA_IF_PTM_ENABLE_UNSPEC;
+
 	if (ifp->ifindex == IFINDEX_INTERNAL) {
 		return CMD_SUCCESS;
 	}
@@ -320,9 +323,6 @@ DEFUN (zebra_ptm_enable_if,
 			if_down(ifp);
 		}
 	}
-
-	if_data = ifp->info;
-	if_data->ptm_enable = ZEBRA_IF_PTM_ENABLE_UNSPEC;
 
 	return CMD_SUCCESS;
 }
