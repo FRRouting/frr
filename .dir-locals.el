86036536ef2720ab5722b6e498f64ab67fe9d14c
Lou Berger <lberger@labn.net>
2017-08-10 14:07:28 -0400

    Initial take on project emacs variables file and settings.  Sets tabs for c-mode.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;;; Match project coding conventions
+((c-mode
+  (indent-tabs-mode . t)
+  (c-basic-offset . 8)))
+
