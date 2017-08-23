67eb7a6c9910c121e7975e26eaae199cb03ff2b1
David Lamparter <equinox@opensourcerouting.org>
2017-08-23 17:33:17 +0200

    python/clippy: make output file writing less weird
    This should stomp out any parallel build issues in this regard.
    
    Fixes: #971
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- python/clidef.py
+++ python/clidef.py
@@ -257,4 +257,4 @@ if __name__ == '__main__':
     process_file(args.cfile, ofd, dumpfd, args.all_defun)
 
     if args.o is not None:
-        clippy.wrdiff(args.o, ofd)
+        clippy.wrdiff(args.o, ofd, [args.cfile, os.path.realpath(__file__)])
