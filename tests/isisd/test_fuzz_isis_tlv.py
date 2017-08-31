0fb07d51c0e800570d543f8ea40a984c391eda74
Christian Franke <chris@opensourcerouting.org>
2017-08-31 22:45:01 +0200

    tests: skip isis tlv fuzztest on SunOS

--- tests/isisd/test_fuzz_isis_tlv.py
+++ tests/isisd/test_fuzz_isis_tlv.py
@@ -1,6 +1,15 @@
 import frrtest
 
-class TestFuzzIsisTLV(frrtest.TestMultiOut):
-    program = './test_fuzz_isis_tlv'
+import pytest
+import platform
 
-TestFuzzIsisTLV.exit_cleanly()
+if platform.uname()[0] == 'SunOS':
+    class TestFuzzIsisTLV:
+        @pytest.mark.skipif(True, reason='Test unsupported on SunOS')
+        def test_exit_cleanly(self):
+            pass
+else:
+    class TestFuzzIsisTLV(frrtest.TestMultiOut):
+        program = './test_fuzz_isis_tlv'
+
+    TestFuzzIsisTLV.exit_cleanly()
