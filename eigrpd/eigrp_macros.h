12e5c4abf1c5dbb1968ad8af61b5bf5c66c9dd9f
Donald Sharp <sharpd@cumulusnetworks.com>
2017-10-02 21:29:08 -0400

    eigrpd: Remove unnecessary macro definitions
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- eigrpd/eigrp_macros.h
+++ eigrpd/eigrp_macros.h
@@ -28,24 +28,8 @@
 #ifndef _ZEBRA_EIGRP_MACROS_H_
 #define _ZEBRA_EIGRP_MACROS_H_
 
-#define DECLARE_IF_PARAM(T, P) T P; u_char P##__config:1
-#define IF_EIGRP_IF_INFO(I) ((struct eigrp_interface *)((I)->info))
-#define IF_OIFS(I)  (IF_EIGRP_IF_INFO (I)->eifs)
-#define IF_OIFS_PARAMS(I) (IF_EIGRP_IF_INFO (I)->eparams)
 
-#define SET_IF_PARAM(S, P) ((S)->P##__config) = 1
-#define IF_DEF_PARAMS(I) (IF_EIGRP_IF_INFO (I)->def_params)
-
-#define UNSET_IF_PARAM(S, P) ((S)->P##__config) = 0
-
-#define EIGRP_IF_PARAM_CONFIGURED(S, P) ((S) && (S)->P##__config)
-#define EIGRP_IF_PARAM(O, P)                                                   \
-	(EIGRP_IF_PARAM_CONFIGURED((O)->params, P)                             \
-		 ? (O)->params->P                                              \
-		 : IF_DEF_PARAMS((O)->ifp)->P)
-
-
-//------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------
 
 #define EIGRP_IF_STRING_MAXLEN  40
 #define IF_NAME(I)      eigrp_if_name_string ((I))
