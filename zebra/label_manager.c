7bfe377d63f41d21e0b7dece264bcf54a197ac13
ßingen <bingen@voltanet.io>
2017-11-13 21:41:58 +0100

    ldpd: Add privs to label manager
    Signed-off-by: ßingen <bingen@voltanet.io>

--- zebra/label_manager.c
+++ zebra/label_manager.c
@@ -41,6 +41,8 @@
 
 struct label_manager lbl_mgr;
 
+extern struct zebra_privs_t zserv_privs;
+
 DEFINE_MGROUP(LBL_MGR, "Label Manager");
 DEFINE_MTYPE_STATIC(LBL_MGR, LM_CHUNK, "Label Manager Chunk");
 
@@ -222,6 +224,7 @@ static void lm_zclient_init(char *lm_zserv_path)
 
 	/* Set default values. */
 	zclient = zclient_new_notify(zebrad.master, &zclient_options_default);
+	zclient->privs = &zserv_privs;
 	zclient->sock = -1;
 	zclient->t_connect = NULL;
 	lm_zclient_connect(NULL);
