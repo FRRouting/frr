47b7136955e81f2fc128d609cd09fd63669e6a9f
Mitesh Kanjariya <mitesh@marvel-07.cumulusnetworks.com>
2017-12-02 16:27:29 -0800

    bgpd: provide filter for ip prefix route in bgp l2vpn evpn route cmd
    Ticket: CM-19000
    Review: ccr-7008
    Testing: Manual
    
    Signed-off-by: Mitesh Kanjariya <mitesh@cumulusnetworks.com>

--- bgpd/bgp_evpn_vty.c
+++ bgpd/bgp_evpn_vty.c
@@ -2932,7 +2932,7 @@ DEFUN(show_bgp_l2vpn_evpn_summary,
  */
 DEFUN(show_bgp_l2vpn_evpn_route,
       show_bgp_l2vpn_evpn_route_cmd,
-      "show bgp l2vpn evpn route [type <macip|multicast>] [json]",
+      "show bgp l2vpn evpn route [type <macip|multicast|prefix>] [json]",
       SHOW_STR
       BGP_STR
       L2VPN_HELP_STR
@@ -2941,6 +2941,7 @@ DEFUN(show_bgp_l2vpn_evpn_route,
       "Specify Route type\n"
       "MAC-IP (Type-2) route\n"
       "Multicast (Type-3) route\n"
+      "Prefix route"
       JSON_STR)
 {
 	struct bgp *bgp;
@@ -2965,6 +2966,8 @@ DEFUN(show_bgp_l2vpn_evpn_route,
 			type = BGP_EVPN_MAC_IP_ROUTE;
 		else if (strncmp(argv[type_idx + 1]->arg, "mu", 2) == 0)
 			type = BGP_EVPN_IMET_ROUTE;
+		else if (strncmp(argv[type_idx + 1]->arg, "pr", 2) == 0)
+			type = BGP_EVPN_IP_PREFIX_ROUTE;
 		else
 			return CMD_WARNING;
 	}
