75ad2f88102bbedf9e4c187f00417b5ac3d821a8
Philippe Guibert <philippe.guibert@6wind.com>
2017-12-19 11:25:21 +0100

    bgpd: bgp instance field moved to 32 bits to host vrf_id_t
    In order to host the vrf identifier in bgp, the instance sub field is
    changed to 32 bits.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_zebra.h
+++ bgpd/bgp_zebra.h
@@ -41,8 +41,7 @@ extern void bgp_zebra_terminate_radv(struct bgp *bgp, struct peer *peer);
 extern void bgp_zebra_instance_register(struct bgp *);
 extern void bgp_zebra_instance_deregister(struct bgp *);
 
-extern struct bgp_redist *bgp_redist_lookup(struct bgp *, afi_t, u_char,
-					    u_short);
+extern struct bgp_redist *bgp_redist_lookup(struct bgp *, afi_t, u_char, vrf_id_t);
 extern struct bgp_redist *bgp_redist_add(struct bgp *, afi_t, u_char, u_short);
 extern int bgp_redistribute_set(struct bgp *, afi_t, int, u_short);
 extern int bgp_redistribute_resend(struct bgp *, afi_t, int, u_short);
