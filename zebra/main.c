50bf8e8f8d5f738f88177957191d9156df4e0347
Philippe Guibert <philippe.guibert@6wind.com>
2018-01-24 19:06:06 +0100

    zebra: fix assert mpls when terminating zebra
    The assert appears in zebra_mpls.c when checking default zebra_vrf.
    It appears that when the mpls entries are flushed, it gets the default
    vrf which is already flushed by vrf_terminate() function. In order to
    avoid that assert to trigger a crash, the mpls flush is called before
    vrf termination.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/main.c
+++ zebra/main.c
@@ -137,6 +137,8 @@ static void sigint(void)
 			if (zvrf)
 				SET_FLAG(zvrf->flags, ZEBRA_VRF_RETAIN);
 		}
+	if (zebrad.lsp_process_q)
+		work_queue_free(zebrad.lsp_process_q);
 	vrf_terminate();
 
 	ns_walk_func(zebra_ns_disabled);
@@ -147,8 +149,6 @@ static void sigint(void)
 
 	list_delete_and_null(&zebrad.client_list);
 	work_queue_free(zebrad.ribq);
-	if (zebrad.lsp_process_q)
-		work_queue_free(zebrad.lsp_process_q);
 	meta_queue_free(zebrad.mq);
 
 	frr_fini();
