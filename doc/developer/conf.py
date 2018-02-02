87e90b666414dab604270b483e11d331268ebc16
Quentin Young <qlyoung@cumulusnetworks.com>
2018-02-02 16:27:00 -0500

    doc: add logos
    Branding!
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- doc/developer/conf.py
+++ doc/developer/conf.py
@@ -154,12 +154,12 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../figures/frr-logo-medium.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '../figures/frr-logo-icon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -255,7 +255,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = '../figures/frr-logo-medium.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
