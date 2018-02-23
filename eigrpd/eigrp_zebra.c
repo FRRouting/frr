3252bd3e22e1090e6bda38a275ff6100179ccc74
Donald Sharp <sharpd@cumulusnetworks.com>
2018-02-23 14:40:46 -0500

    *: Modify notify_owner to route_notify_owner
    In the future we are going to have a rule_notify_owner
    so make the distinction between the two types of notification
    clearer.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- eigrpd/eigrp_zebra.c
+++ eigrpd/eigrp_zebra.c
@@ -94,8 +94,8 @@ static int eigrp_router_id_update_zebra(int command, struct zclient *zclient,
 	return 0;
 }
 
-static int eigrp_zebra_notify_owner(int command, struct zclient *zclient,
-				    zebra_size_t length, vrf_id_t vrf_id)
+static int eigrp_zebra_route_notify_owner(int command, struct zclient *zclient,
+					  zebra_size_t length, vrf_id_t vrf_id)
 {
 	struct prefix p;
 	enum zapi_route_notify_owner note;
@@ -129,7 +129,7 @@ void eigrp_zebra_init(void)
 	zclient->interface_address_delete = eigrp_interface_address_delete;
 	zclient->redistribute_route_add = eigrp_zebra_read_route;
 	zclient->redistribute_route_del = eigrp_zebra_read_route;
-	zclient->notify_owner = eigrp_zebra_notify_owner;
+	zclient->route_notify_owner = eigrp_zebra_route_notify_owner;
 }
 
 
