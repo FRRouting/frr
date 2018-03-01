839ce99725a2f2334eeb914df41ece3f865459fd
Mitesh Kanjariya <mitesh@marvel-07.cumulusnetworks.com>
2018-03-01 01:47:28 -0800

    bgpd: enable neighbor-nexthop-self for l2vpn evpn address family
    In the FRR implementation of EVPN,
    eBGP leaf-spine peering for EVPN is fully supported by allowing
    the next hop to be propagated and not rewritten at each hop.
    There are other changes also related to route import to facilitate this.
    However, propagating the next hop is not correct in some cases.
    Specifically, if the DC is comprised of multiple PODs
    with distinct intra-POD and inter-POD VxLAN tunnels,
    EVPN routes received from an adjacent POD by a border/exit leaf
    must be propagated into the local POD with the next hop rewritten (to self).
    
    Signed-off-by: Mitesh Kanjariya <mitesh@cumulusnetworks.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -1312,6 +1312,8 @@ static void subgroup_announce_reset_nhop(u_char family, struct attr *attr)
 		attr->nexthop.s_addr = 0;
 	if (family == AF_INET6)
 		memset(&attr->mp_nexthop_global, 0, IPV6_MAX_BYTELEN);
+	if (family == AF_EVPN)
+		memset(&attr->mp_nexthop_global_in, 0, BGP_ATTR_NHLEN_IPV4);
 }
 
 int subgroup_announce_check(struct bgp_node *rn, struct bgp_info *ri,
