86b286101b1b4eea804b4ffa3e0c47c23a664ed2
Lou Berger <lberger@labn.net>
2018-03-14 12:11:57 -0400

    vtysh: add -u/--user flag to run commands without enable
    Signed-off-by: Lou Berger <lberger@labn.net>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -305,12 +305,20 @@ static int vtysh_execute_func(const char *line, int pager)
 	int closepager = 0;
 	int tried = 0;
 	int saved_ret, saved_node;
+	extern int user_mode;
 
 	/* Split readline string up into the vector. */
 	vline = cmd_make_strvec(line);
 
 	if (vline == NULL)
 		return CMD_SUCCESS;
+
+	if (user_mode) {
+		if (strncmp("en", vector_slot(vline, 0), 2) == 0) {
+			fprintf(stdout, "%% Command not allowed: enable\n");
+			return CMD_WARNING;
+		}
+	}
 
 	saved_ret = ret = cmd_execute_command(vline, vty, &cmd, 1);
 	saved_node = vty->node;
@@ -385,13 +393,13 @@ static int vtysh_execute_func(const char *line, int pager)
 			fprintf(stdout, "Warning...\n");
 		break;
 	case CMD_ERR_AMBIGUOUS:
-		fprintf(stdout, "%% Ambiguous command.\n");
+		fprintf(stdout, "%% Ambiguous command: %s\n", line);
 		break;
 	case CMD_ERR_NO_MATCH:
-		fprintf(stdout, "%% Unknown command.\n");
+		fprintf(stdout, "%% Unknown command: %s\n", line);
 		break;
 	case CMD_ERR_INCOMPLETE:
-		fprintf(stdout, "%% Command incomplete.\n");
+		fprintf(stdout, "%% Command incomplete: %s\n", line);
 		break;
 	case CMD_SUCCESS_DAEMON: {
 		/*
