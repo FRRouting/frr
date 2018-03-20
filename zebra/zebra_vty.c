af5849b6628695a945f70eb195c2049d1bbb3dfb
Donald Sharp <sharpd@cumulusnetworks.com>
2018-03-20 18:41:00 -0400

    zebra: Fix memory leak on re-enter case
    When we have a case where the user re-enters the same
    ip route line, we need to delete the memory we just
    malloc'ed.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -249,6 +249,9 @@ static int zebra_static_route_holdem(struct zebra_vrf *zvrf,
 			return CMD_SUCCESS;
 		}
 
+		XFREE(MTYPE_STATIC_ROUTE, shr->nhvrf_name);
+		XFREE(MTYPE_STATIC_ROUTE, shr->vrf_name);
+		XFREE(MTYPE_STATIC_ROUTE, shr);
 		/*
 		 * If a person enters the same line again
 		 * we need to silently accept it
