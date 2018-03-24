5c4e963e4023e7b69ea7ac24fef6a70d4a9134ae
Christian Franke <nobody@nowhere.ws>
2018-03-24 13:51:40 +0100

    ospf6-topo1: make ip-route comparison more robust
    Signed-off-by: Christian Franke <nobody@nowhere.ws>

--- tests/topotests/ospf6-topo1/test_ospf6_topo1.py
+++ tests/topotests/ospf6-topo1/test_ospf6_topo1.py
@@ -332,16 +332,17 @@ def test_linux_ipv6_kernel_routingTable():
             for ll in linklocals:
                 actual = actual.replace(ll[1], "fe80::__(%s)__" % ll[0])
             # Mask out protocol name or number
-            actual = re.sub(r"[ ]+proto [0-9a-z]+ ", "  proto XXXX ", actual)
+            actual = re.sub(r"[ ]+proto [0-9a-z]+ +", "  proto XXXX ", actual)
             # Remove ff00::/8 routes (seen on some kernels - not from FRR)
             actual = re.sub(r'ff00::/8.*', '', actual)
 
             # Strip empty lines
             actual = actual.lstrip()
             actual = actual.rstrip()
+            actual = re.sub(r'  +', ' ', actual)
 
             # Fix newlines (make them all the same)
-            actual = ('\n'.join(actual.splitlines())).splitlines(1)
+            actual = ('\n'.join(sorted(actual.splitlines()))).splitlines(1)
 
             # Print Actual table
             # print("Router r%s table" % i)
