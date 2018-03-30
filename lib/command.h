3df99b560132ef89f2d9e1bf142ee980353b6f69
Quentin Young <qlyoung@cumulusnetworks.com>
2018-03-30 15:42:19 -0400

    bgpd: remove COMMUNITY_LIST_NODE
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/command.h
+++ lib/command.h
@@ -129,7 +129,6 @@ enum node_type {
 	ACCESS_MAC_NODE,	 /* MAC access list node*/
 	PREFIX_IPV6_NODE,	/* Prefix list node. */
 	AS_LIST_NODE,		 /* AS list node. */
-	COMMUNITY_LIST_NODE,     /* Community list node. */
 	RMAP_NODE,		 /* Route map node. */
 	SMUX_NODE,		 /* SNMP configuration node. */
 	DUMP_NODE,		 /* Packet dump node. */
