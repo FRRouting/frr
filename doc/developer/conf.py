cb3d8153a33f952b4c2e275a597c1b2a68ec2fbb
Quentin Young <qlyoung@cumulusnetworks.com>
2018-04-02 14:41:38 -0400

    doc: add more CLI documentation
    * Document modes & nodes
    * Document walkup behavior
    * Document struct cmd_node
    * Add graph of node relationships
    * Reorder sections to make more logical sense
    * Rewrap sections to 80 lines
    * Fix code block indent
    * Specify language for code blocks in order to get syntax highlighting
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- doc/developer/conf.py
+++ doc/developer/conf.py
@@ -32,7 +32,7 @@ rst_prolog = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
