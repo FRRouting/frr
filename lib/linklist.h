18e72c6609738b94b3216afce3be2d4e1997d433
Lou Berger <lberger@labn.net>
2018-04-03 13:44:46 -0400

    *: Only test CONFDATE when VERSION_TYPE_DEV defined
    Signed-off-by: Lou Berger <lberger@labn.net>

--- lib/linklist.h
+++ lib/linklist.h
@@ -84,7 +84,7 @@ extern void *listnode_head(struct list *);
  * and remove list_delete_original and the list_delete #define
  * Additionally remove list_free entirely
  */
-#if CONFDATE > 20181001
+#if defined(VERSION_TYPE_DEV) && CONFDATE > 20181001
 CPP_NOTICE("list_delete without double pointer is deprecated, please fixup")
 #endif
 extern void list_delete_and_null(struct list **);
