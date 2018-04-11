d86321ae94b1eda4582800198e511c784d053a53
G. Paul Ziemba <paulz@labn.net>
2018-04-11 11:46:35 -0700

    bgp_l3vpn_to_bgp_vrf: require frr 4.1
    Signed-off-by: G. Paul Ziemba <paulz@labn.net>

--- tests/topotests/bgp_l3vpn_to_bgp_vrf/test_bgp_l3vpn_to_bgp_vrf.py
+++ tests/topotests/bgp_l3vpn_to_bgp_vrf/test_bgp_l3vpn_to_bgp_vrf.py
@@ -33,36 +33,36 @@ def test_check_linux_vrf():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\', iproute2=\'4.9\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True, iproute2=\'4.9\')'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
     ltemplateTest('scripts/check_linux_vrf.py', False, CliOnFail, CheckFunc)
 
 def test_adjacencies():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True)'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True)'
     ltemplateTest('scripts/adjacencies.py', False, CliOnFail, CheckFunc)
 
 def SKIP_test_add_routes():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True)'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True)'
     ltemplateTest('scripts/add_routes.py', False, CliOnFail, CheckFunc)
 
 def test_check_routes():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True)'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True)'
     ltemplateTest('scripts/check_routes.py', False, CliOnFail, CheckFunc)
 
 #manual data path setup test - remove once have bgp/zebra vrf path working
@@ -70,18 +70,18 @@ def test_check_linux_mpls():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\', iproute2=\'4.9\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True, iproute2=\'4.9\')'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
     ltemplateTest('scripts/check_linux_mpls.py', False, CliOnFail, CheckFunc)
 
 def SKIP_test_cleanup_all():
     CliOnFail = None
     # For debugging, uncomment the next line
     #CliOnFail = 'tgen.mininet_cli'
-    CheckFunc = 'ltemplateVersionCheck(\'3.1\')'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\')'
     #uncomment next line to start cli *before* script is run
-    #CheckFunc = 'ltemplateVersionCheck(\'3.1\', cli=True)'
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True)'
     ltemplateTest('scripts/cleanup_all.py', False, CliOnFail, CheckFunc)
 
 if __name__ == '__main__':
