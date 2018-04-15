e9e41f8a87a6e17dc1b7fcc34440c53c9a20740e
Donald Sharp <sharpd@cumulusnetworks.com>
2018-04-15 10:57:19 -0400

    zebra: Fix crash with certain types of tunnels
    Zebra did not have a handler for tunnels in v6 for
    some reason.  Add code to handle the broadcast address
    for both addition and deletion.
    
    This appears to fix the crash.  There might still need
    to be some work to make the code `work` properly for
    this type of tunnel.
    
    Fixes: #2063
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/kernel_socket.c
+++ zebra/kernel_socket.c
@@ -771,10 +771,11 @@ int ifam_read(struct ifa_msghdr *ifam)
 
 		if (ifam->ifam_type == RTM_NEWADDR)
 			connected_add_ipv6(ifp, flags, &addr.sin6.sin6_addr,
+					   NULL,
 					   ip6_masklen(mask.sin6.sin6_addr),
 					   (isalias ? ifname : NULL));
 		else
-			connected_delete_ipv6(ifp, &addr.sin6.sin6_addr,
+			connected_delete_ipv6(ifp, &addr.sin6.sin6_addr, NULL,
 					      ip6_masklen(mask.sin6.sin6_addr));
 		break;
 	default:
