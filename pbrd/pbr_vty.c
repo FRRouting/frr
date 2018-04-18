66074ebd00c6c30147a42e68dced0e7254f6d26a
Donald Sharp <sharpd@cumulusnetworks.com>
2018-04-18 08:25:51 -0400

    pbrd: Do not display vrf name for show run for interfaces
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pbrd/pbr_vty.c
+++ pbrd/pbr_vty.c
@@ -1107,11 +1107,7 @@ static int pbr_interface_config_write(struct vty *vty)
 
 	RB_FOREACH (vrf, vrf_name_head, &vrfs_by_name) {
 		FOR_ALL_INTERFACES (vrf, ifp) {
-			if (vrf->vrf_id == VRF_DEFAULT)
-				vty_frame(vty, "interface %s\n", ifp->name);
-			else
-				vty_frame(vty, "interface %s vrf %s\n",
-					  ifp->name, vrf->name);
+			vty_frame(vty, "interface %s\n", ifp->name);
 
 			if (ifp->desc)
 				vty_out(vty, " description %s\n", ifp->desc);
