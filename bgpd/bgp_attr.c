23a2a47ecab55ca6de84b2078c012a18efa83921
Donald Sharp <sharpd@cumulusnetworks.com>
2018-04-23 15:24:26 -0400

    bgpd: Add some additional attribute information to dump
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -593,6 +593,9 @@ static void attr_show_all_iterator(struct hash_backet *backet, struct vty *vty)
 
 	vty_out(vty, "attr[%ld] nexthop %s\n", attr->refcnt,
 		inet_ntoa(attr->nexthop));
+	vty_out(vty, "\tflags: %" PRIu64 " med: %u local_pref: %u origin: %u weight: %u\n",
+		attr->flag, attr->med, attr->local_pref, attr->origin,
+		attr->weight);
 }
 
 void attr_show_all(struct vty *vty)
