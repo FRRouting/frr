6e10c490b1aca8891eedeca06507f84c13263993
Quentin Young <qlyoung@cumulusnetworks.com>
2018-04-24 11:36:25 -0400

    zserv: optimize zserv_read
    * Increase the maximum number of packets to read per read job
    * Store read packets in a local cached buffer to avoid mutex overhead
    * Only update last-read time / last-command if we actually read a packet
    * Add missing log line for corrupt header case
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- zebra/zserv.h
+++ zebra/zserv.h
@@ -183,8 +183,8 @@ struct zebra_t {
 	/* LSP work queue */
 	struct work_queue *lsp_process_q;
 
-#define ZEBRA_ZAPI_PACKETS_TO_PROCESS 10
-	uint32_t packets_to_process;
+#define ZEBRA_ZAPI_PACKETS_TO_PROCESS 100000
+	_Atomic uint32_t packets_to_process;
 };
 extern struct zebra_t zebrad;
 extern unsigned int multipath_num;
