dc94b5175d186b377975577bb05c50000f7c2aca
Philippe Guibert <philippe.guibert@6wind.com>
2018-05-03 08:56:42 +0200

    zebra: update comment when calling pbr southbound interface
    Some documentation is updated.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zebra_pbr.c
+++ zebra/zebra_pbr.c
@@ -443,7 +443,6 @@ void zebra_pbr_destroy_ipset(struct zebra_ns *zns,
 	lookup = hash_lookup(zns->ipset_hash, ipset);
 	/* TODO:
 	 * - Netlink destroy from kernel
-	 * - ?? destroy ipset entries before
 	 */
 	if (lookup) {
 		hash_release(zns->ipset_hash, lookup);
@@ -513,7 +512,6 @@ void zebra_pbr_add_ipset_entry(struct zebra_ns *zns,
 	(void)hash_get(zns->ipset_entry_hash, ipset,
 		       pbr_ipset_entry_alloc_intern);
 	/* TODO:
-	 * - attach to ipset list
 	 * - Netlink add to kernel
 	 */
 }
@@ -526,8 +524,6 @@ void zebra_pbr_del_ipset_entry(struct zebra_ns *zns,
 	lookup = hash_lookup(zns->ipset_entry_hash, ipset);
 	/* TODO:
 	 * - Netlink destroy
-	 * - detach from ipset list
-	 * - ?? if no more entres, delete ipset
 	 */
 	if (lookup) {
 		hash_release(zns->ipset_entry_hash, lookup);
@@ -556,7 +552,7 @@ void zebra_pbr_add_iptable(struct zebra_ns *zns,
 {
 	(void)hash_get(zns->iptable_hash, iptable,
 		       pbr_iptable_alloc_intern);
-	/* TODO call netlink layer */
+	/* TODO call ioctl layer */
 }
 
 void zebra_pbr_del_iptable(struct zebra_ns *zns,
@@ -566,8 +562,7 @@ void zebra_pbr_del_iptable(struct zebra_ns *zns,
 
 	lookup = hash_lookup(zns->iptable_hash, iptable);
 	/* TODO:
-	 * - call netlink layer
-	 * - detach from iptable list
+	 * - call ioctl layer
 	 */
 	if (lookup) {
 		struct listnode *node, *nnode;
