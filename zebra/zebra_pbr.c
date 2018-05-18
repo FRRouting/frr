a0146715fed4f848095f48eb97f275385aae2d72
Philippe Guibert <philippe.guibert@6wind.com>
2018-05-18 16:22:23 +0200

    zebra: add pbr objects fail_remove value into notification
    After PBR or BGP sends back a request for sending a rule/ipset/ipset
    entry/iptable delete, there may be issue in deleting it. A notification
    is sent back with a new value indicating that the removal failed.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zebra_pbr.c
+++ zebra/zebra_pbr.c
@@ -659,7 +659,7 @@ void kernel_pbr_rule_add_del_status(struct zebra_pbr_rule *rule,
 		zsend_rule_notify_owner(rule, ZAPI_RULE_REMOVED);
 		break;
 	case SOUTHBOUND_DELETE_FAILURE:
-		zsend_rule_notify_owner(rule, ZAPI_RULE_REMOVED);
+		zsend_rule_notify_owner(rule, ZAPI_RULE_FAIL_REMOVE);
 		break;
 	}
 }
@@ -681,7 +681,7 @@ void kernel_pbr_ipset_add_del_status(struct zebra_pbr_ipset *ipset,
 		zsend_ipset_notify_owner(ipset, ZAPI_IPSET_REMOVED);
 		break;
 	case SOUTHBOUND_DELETE_FAILURE:
-		zsend_ipset_notify_owner(ipset, ZAPI_IPSET_REMOVED);
+		zsend_ipset_notify_owner(ipset, ZAPI_IPSET_FAIL_REMOVE);
 		break;
 	}
 }
@@ -708,7 +708,7 @@ void kernel_pbr_ipset_entry_add_del_status(
 		break;
 	case SOUTHBOUND_DELETE_FAILURE:
 		zsend_ipset_entry_notify_owner(ipset,
-					       ZAPI_IPSET_ENTRY_REMOVED);
+					       ZAPI_IPSET_ENTRY_FAIL_REMOVE);
 		break;
 	}
 }
@@ -732,7 +732,7 @@ void kernel_pbr_iptable_add_del_status(struct zebra_pbr_iptable *iptable,
 		break;
 	case SOUTHBOUND_DELETE_FAILURE:
 		zsend_iptable_notify_owner(iptable,
-					   ZAPI_IPTABLE_REMOVED);
+					   ZAPI_IPTABLE_FAIL_REMOVE);
 		break;
 	}
 }
