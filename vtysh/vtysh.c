a127c68d52176a5d380f35eed372d2a0848e3565
LQchengdu <lqking7735@163.com>
2018-05-22 14:29:57 +0800

    Update vtysh.c

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -2754,8 +2754,7 @@ DEFUN (vtysh_output_file,
 {
 	const char *path = argv[argc - 1]->arg;
 
-	if (outputfile != stdout)
-	{
+	if (outputfile != stdout) {
 		fclose(outputfile);
 	}
 	outputfile = fopen(path, "a");
