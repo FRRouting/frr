24f8bd87e58abd7ac2463d4026dc84a48e0cd19f
Donald Sharp <sharpd@cumulusnetworks.com>
2018-05-30 09:50:22 -0400

    bgpd: Ignore return code for str2prefix
    The call to str2prefix_rd when we get to this point of the
    code cannot fail.  As such let's just ignore the return code.
    
    Found by Coverity SA.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_rd.c
+++ bgpd/bgp_rd.c
@@ -210,5 +210,5 @@ void form_auto_rd(struct in_addr router_id,
 	prd->family = AF_UNSPEC;
 	prd->prefixlen = 64;
 	sprintf(buf, "%s:%hu", inet_ntoa(router_id), rd_id);
-	str2prefix_rd(buf, prd);
+	(void)str2prefix_rd(buf, prd);
 }
