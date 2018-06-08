ce872c6adb26cb62051dcbbdaec8676516f936a8
paco <paco@voltanet.io>
2018-06-08 19:46:48 +0200

    spf6d: fix use after free (Coverity 1221459)
    Signed-off-by: F. Aragon <paco@voltanet.io>

--- ospf6d/ospf6_flood.c
+++ ospf6d/ospf6_flood.c
@@ -349,8 +349,9 @@ void ospf6_flood_interface(struct ospf6_neighbor *from, struct ospf6_lsa *lsa,
 						ospf6_lsa_unlock(req);
 						on->last_ls_req = NULL;
 					}
-					ospf6_lsdb_remove(req,
-							  on->request_list);
+					if (req)
+						ospf6_lsdb_remove(req,
+							on->request_list);
 					ospf6_check_nbr_loading(on);
 					/* fall through */
 				}
