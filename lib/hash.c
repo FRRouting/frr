dfc7fde51b9ff0d75b4762b570ee017bd7f48e1a
Donald Sharp <sharpd@cumulusnetworks.com>
2018-07-06 10:18:11 -0400

    Revert "lib: A small optimization for the hash iterate and walk functions"
    This reverts commit fc61644e440c875eefa222ab34d726c6281ca806.

--- lib/hash.c
+++ lib/hash.c
@@ -255,21 +255,15 @@ void hash_iterate(struct hash *hash, void (*func)(struct hash_backet *, void *),
 	unsigned int i;
 	struct hash_backet *hb;
 	struct hash_backet *hbnext;
-	uint32_t count = 0;
 
-	for (i = 0; i < hash->size; i++) {
+	for (i = 0; i < hash->size; i++)
 		for (hb = hash->index[i]; hb; hb = hbnext) {
 			/* get pointer to next hash backet here, in case (*func)
 			 * decides to delete hb by calling hash_release
 			 */
 			hbnext = hb->next;
 			(*func)(hb, arg);
-			count++;
-
 		}
-		if (count == hash->count)
-			return;
-	}
 }
 
 /* Iterator function for hash.  */
@@ -280,7 +274,6 @@ void hash_walk(struct hash *hash, int (*func)(struct hash_backet *, void *),
 	struct hash_backet *hb;
 	struct hash_backet *hbnext;
 	int ret = HASHWALK_CONTINUE;
-	uint32_t count = 0;
 
 	for (i = 0; i < hash->size; i++) {
 		for (hb = hash->index[i]; hb; hb = hbnext) {
@@ -291,10 +284,7 @@ void hash_walk(struct hash *hash, int (*func)(struct hash_backet *, void *),
 			ret = (*func)(hb, arg);
 			if (ret == HASHWALK_ABORT)
 				return;
-			count++;
 		}
-		if (count == hash->count)
-			return;
 	}
 }
 
