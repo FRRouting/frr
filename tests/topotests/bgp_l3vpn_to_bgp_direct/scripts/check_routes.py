064c807f63ba6d86b819c7b940b01e77dc1d3104
Lou Berger <lberger@labn.net>
2018-07-25 07:32:58 -0400

    bgp_l3vpn_to_bgp_direct: imports and more debug Allow for different timings on, and number of, import processing Collect more info for debugging
    Signed-off-by: Lou Berger <lberger@labn.net>

--- tests/topotests/bgp_l3vpn_to_bgp_direct/scripts/check_routes.py
+++ tests/topotests/bgp_l3vpn_to_bgp_direct/scripts/check_routes.py
@@ -6,7 +6,12 @@ luCommand('r1','vtysh -c "show bgp ipv4 uni"','7 routes and 9','pass','Unicast S
 luCommand('r2','vtysh -c "show bgp ipv4 uni"','No BGP prefixes displayed','pass','Unicast SAFI')
 luCommand('r3','vtysh -c "show bgp ipv4 uni"','7 routes and 9','pass','Unicast SAFI')
 luCommand('r4','vtysh -c "show bgp ipv4 uni"','7 routes and 9','pass','Unicast SAFI')
-luCommand('r1','vtysh -c "show bgp ipv4 vpn"','7 routes and 7','pass','VPN SAFI')
-luCommand('r2','vtysh -c "show bgp ipv4 vpn"','7 routes and 7','pass','VPN SAFI')
-luCommand('r3','vtysh -c "show bgp ipv4 vpn"','7 routes and 7','pass','VPN SAFI')
-luCommand('r4','vtysh -c "show bgp ipv4 vpn"','7 routes and 7','pass','VPN SAFI')
+have2ndImports = luCommand('r3','vtysh -c "show vnc registrations imported"','2 out of 2 imported','none','Imported Registrations',2)
+if have2ndImports:
+    num = '9 routes and 9'
+else:
+    num = '7 routes and 7'
+luCommand('r1','vtysh -c "show bgp ipv4 vpn"',num,'pass','VPN SAFI')
+luCommand('r2','vtysh -c "show bgp ipv4 vpn"',num,'pass','VPN SAFI')
+luCommand('r3','vtysh -c "show bgp ipv4 vpn"',num,'pass','VPN SAFI')
+luCommand('r4','vtysh -c "show bgp ipv4 vpn"',num,'pass','VPN SAFI')
