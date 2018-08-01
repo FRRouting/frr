7df407eda8b501b6319f9c563ee5f72f9199d8a6
Chirag Shah <chirag@cumulusnetworks.com>
2018-07-31 18:45:39 -0700

    bgpd: check existing l3vni for any l2vni creation
    Scan all bgp vrf instances and respective L3VNI against the VNI which is being configured.
    
    Ticket:CM-21859
    Testing Done:
    Configure l3vni,
    try to configure same vni as l2vni under router bgp, address-family
    l2vpn evpn.
    The configuration is rejected.
    
    show evpn vni
    VNI        Type VxLAN IF              # MACs   # ARPs   # Remote VTEPs Tenant VRF
    4001       L3   vx-4001               0        0        n/a vrf1
    
    TOR(config)# router bgp 5546
    TOR(config-router)# address-family l2vpn evpn
    TOR(config-router-af)# vni 4001
    % Failed to create VNI
    
    Signed-off-by: Chirag Shah <chirag@cumulusnetworks.com>

--- bgpd/bgp_evpn_vty.c
+++ bgpd/bgp_evpn_vty.c
@@ -1888,6 +1888,14 @@ static struct bgpevpn *evpn_create_update_vni(struct bgp *bgp, vni_t vni)
 
 	vpn = bgp_evpn_lookup_vni(bgp, vni);
 	if (!vpn) {
+		/* Check if this L2VNI is already configured as L3VNI */
+		if (bgp_evpn_lookup_l3vni_l2vni_table(vni)) {
+			flog_err(BGP_ERR_VNI,
+				 "%u: Failed to create L2VNI %u, it is configured as L3VNI",
+				 bgp->vrf_id, vni);
+			return NULL;
+		}
+
 		/* tenant vrf will be updated when we get local_vni_add from
 		 * zebra
 		 */
