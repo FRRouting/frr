9f134cdcfd7e811c998784e1d5a7170e1a88511c
F. Aragon <paco@voltanet.io>
2018-08-03 15:23:26 +0200

    bgpd: null check (Coverity 1472237)
    Signed-off-by: F. Aragon <paco@voltanet.io>

--- bgpd/bgp_table.c
+++ bgpd/bgp_table.c
@@ -144,9 +144,6 @@ void bgp_table_range_lookup(const struct bgp_table *table, struct prefix *p,
 	struct bgp_node *node = bgp_node_from_rnode(table->route_table->top);
 	struct bgp_node *matched = NULL;
 
-	if (node == NULL)
-		return;
-
 	while (node && node->p.prefixlen <= p->prefixlen
 	       && prefix_match(&node->p, p)) {
 		if (node->info && node->p.prefixlen == p->prefixlen) {
@@ -156,6 +153,9 @@ void bgp_table_range_lookup(const struct bgp_table *table, struct prefix *p,
 		node = bgp_node_from_rnode(node->link[prefix_bit(
 			&p->u.prefix, node->p.prefixlen)]);
 	}
+
+	if (node == NULL)
+		return;
 
 	if ((matched == NULL && node->p.prefixlen > maxlen) || !node->parent)
 		return;
