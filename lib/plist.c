0e411ce68160c3896d3b6f58906697e8eec7874c
Donald Sharp <sharpd@cumulusnetworks.com>
2018-08-20 10:47:00 -0400

    lib: Modify zlog_warn to vty_out
    The vty_prefix_list_install function was modifying the prefix to match the
    specified prefix length and warning in the log file.  Modify
    code to use zlog_info as that a warn implies that something has
    gone terribly wrong.  Additionally display to the terminal as
    well so that user can get immediate feedback from something
    that they can correct.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- lib/plist.c
+++ lib/plist.c
@@ -930,9 +930,11 @@ static int vty_prefix_list_install(struct vty *vty, afi_t afi, const char *name,
 		char buf_tmp[PREFIX2STR_BUFFER];
 		prefix2str(&p, buf, sizeof(buf));
 		prefix2str(&p_tmp, buf_tmp, sizeof(buf_tmp));
-		zlog_warn(
-			"Prefix-list %s prefix changed from %s to %s to match length",
+		vty_out(vty,
+			"%% Prefix-list %s prefix changed from %s to %s to match length\n",
 			name, buf, buf_tmp);
+		zlog_info("Prefix-list %s prefix changed from %s to %s to match length",
+			  name, buf, buf_tmp);
 		p = p_tmp;
 	}
 
