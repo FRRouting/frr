0eff58207eef2335b092380a0ffb7b12c3e07d91
Rafael Zalamena <rzalamena@opensourcerouting.org>
2018-08-27 13:48:45 -0300

    lib: fix `ip4_route_zebra` header removal code
    The `strip` function is actually a method of the String object.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- tests/topotests/lib/topotest.py
+++ tests/topotests/lib/topotest.py
@@ -428,8 +428,7 @@ def ip4_route_zebra(node, vrf_name=None):
 
     lines = output.splitlines()
     header_found = False
-    while lines and (not strip(lines[0])
-                        or not header_found):
+    while lines and (not lines[0].strip() or not header_found):
         if '> - selected route' in lines[0]:
             header_found = True
         lines = lines[1:]
