16872f5683b3595c682526a1bd37233cff6d1122
Lou Berger <lberger@labn.net>
2018-08-29 14:18:02 +0000

    bgp_rfapi_basic_sanity: added multi-path to timeout test
    Signed-off-by: Lou Berger <lberger@labn.net>

--- tests/topotests/bgp_rfapi_basic_sanity/scripts/add_routes.py
+++ tests/topotests/bgp_rfapi_basic_sanity/scripts/add_routes.py
@@ -7,6 +7,7 @@ else:
     to = "6"
     cost = "cost 50"
 luCommand('r1','vtysh -c "debug rfapi-dev open vn 10.0.0.1 un 1.1.1.1"','rfapi_set_response_cb: status 0', 'pass', 'Opened RFAPI')
+luCommand('r1','vtysh -c "debug rfapi-dev query vn 10.0.0.1 un 1.1.1.1 target 11.11.11.11"','rc=2', 'pass', 'Clean query')
 luCommand('r1','vtysh -c "debug rfapi-dev register vn 10.0.0.1 un 1.1.1.1 prefix 11.11.11.0/24 lifetime {}"'.format(to),'', 'none', 'Prefix registered')
 luCommand('r1','vtysh -c "show vnc registrations local"','1 out of 1','wait','Local registration')
 luCommand('r1','vtysh -c "debug rfapi-dev response-omit-self off"','.','none')
