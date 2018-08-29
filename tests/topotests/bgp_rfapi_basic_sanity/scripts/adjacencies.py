d1f4560e7d0e024ff1cfbdf229eca7adb2a8561a
Lou Berger <lberger@labn.net>
2018-08-29 19:28:13 +0000

    bgp_rfapi_basic_sanity_config2: allow for slow CI
    Signed-off-by: Lou Berger <lberger@labn.net>

--- tests/topotests/bgp_rfapi_basic_sanity/scripts/adjacencies.py
+++ tests/topotests/bgp_rfapi_basic_sanity/scripts/adjacencies.py
@@ -1,7 +1,7 @@
 luCommand('r1','ping 2.2.2.2 -c 1',' 0. packet loss','wait','PE->P2 (loopback) ping',60)
 luCommand('r3','ping 2.2.2.2 -c 1',' 0. packet loss','wait','PE->P2 (loopback) ping',60)
 luCommand('r4','ping 2.2.2.2 -c 1',' 0. packet loss','wait','PE->P2 (loopback) ping',60)
-luCommand('r2','vtysh -c "show bgp summary"',' 00:0.* 00:0.* 00:0','wait','Core adjacencies up')
+luCommand('r2','vtysh -c "show bgp summary"',' 00:0.* 00:0.* 00:0','wait','Core adjacencies up',30)
 luCommand('r1','vtysh -c "show bgp vrf all summary"',' 00:0','pass','All adjacencies up')
 luCommand('r3','vtysh -c "show bgp vrf all summary"',' 00:0','pass','All adjacencies up')
 luCommand('r4','vtysh -c "show bgp vrf all summary"',' 00:0','pass','All adjacencies up')
