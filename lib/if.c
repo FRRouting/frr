FRRouting/frr

commit b53686c52a592e5edf0d89f92ec9a2b83fc4e470

zebra: delete interface that disappeared
When moving interfaces to an other place, like other netns, the
remaining interface is still present, with inactive status.
Now, that interface is deleted from the list, if the interface appears
on an other netns. If not, the interface is kept.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/if_netlink.c
@@ -1008,6 +1008,54 @@ int netlink_interface_addr(struct sockaddr_nl *snl, struct nlmsghdr *h,
 	return 0;
 }
 
+/* helper function called by if_netlink_change
+ * to delete interfaces in case the interface moved
+ * to an other netns
+ */
+static void if_netlink_check_ifp_instance_consistency(uint16_t cmd,
+						     struct interface *ifp,
+						     ns_id_t ns_id)
+{
+	struct interface *old_ifp;
+
+	/*
+	 * look if interface name is also found on other netns
+	 * - only if vrf backend is netns
+	 * - do not concern lo interface
+	 * - then remove previous one
+	 * - for new link case, check found interface is not active
+	 */
+	if (!vrf_is_backend_netns() ||
+	    !strcmp(ifp->name, "lo"))
+		return;
+	old_ifp = if_lookup_by_name_not_ns(ns_id, ifp->name);
+	if (!old_ifp)
+		return;
+	if ((cmd == RTM_NEWLINK)
+	    && (CHECK_FLAG(old_ifp->status, ZEBRA_INTERFACE_ACTIVE)))
+		return;
+	if (IS_ZEBRA_DEBUG_KERNEL)
+		zlog_debug("%s %s(%u) %s VRF %u",
+			   cmd == RTM_DELLINK ?
+			   "RTM_DELLINK replaced by" :
+			   "RTM_NEWLINK replaces",
+			   ifp->name,
+			   old_ifp->ifindex,
+			   cmd == RTM_DELLINK ?
+			   "in" : "from",
+			   old_ifp->vrf_id);
+	/* the found interface replaces the current one
+	 * remove it
+	 */
+	if (cmd == RTM_DELLINK)
+		if_delete(ifp);
+	else
+		if_delete(old_ifp);
+	/* the found interface is replaced by the current one
+	 * suppress it
+	 */
+}
+
 int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			ns_id_t ns_id, int startup)
 {
@@ -1169,6 +1217,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp))
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
+			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
+								  ifp, ns_id);
 		} else if (ifp->vrf_id != vrf_id) {
 			/* VRF change for an interface. */
 			if (IS_ZEBRA_DEBUG_KERNEL)
@@ -1236,6 +1286,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp) || was_bridge_slave)
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
+			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
+								  ifp, ns_id);
 		}
 	} else {
 		/* Delete interface notification from kernel */
@@ -1259,6 +1311,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 
 		if (!IS_ZEBRA_IF_VRF(ifp))
 			if_delete_update(ifp);
+		if_netlink_check_ifp_instance_consistency(RTM_DELLINK,
+							  ifp, ns_id);
 	}
 
 	return 0;

zebra/interface.c
@@ -252,6 +252,30 @@ struct interface *if_lookup_by_name_per_ns(struct zebra_ns *ns,
 	return NULL;
 }
 
+/* this function must be used only if the vrf backend
+ * is a netns backend
+ */
+struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
+					   const char *ifname)
+{
+	struct interface *ifp;
+	struct ns *ns;
+
+	RB_FOREACH (ns, ns_head, &ns_tree) {
+		if (ns->ns_id == ns_id)
+			continue;
+		/* if_delete_update has removed interface
+		 * from zns->if_table
+		 * so to look for interface, use the vrf list
+		 */
+		ifp = if_lookup_by_name(ifname, (vrf_id_t)ns->ns_id);
+		if (!ifp)
+			continue;
+		return ifp;
+	}
+	return NULL;
+}
+
 const char *ifindex2ifname_per_ns(struct zebra_ns *zns, unsigned int ifindex)
 {
 	struct interface *ifp;

zebra/interface.h
@@ -318,6 +318,8 @@ extern struct interface *if_lookup_by_index_per_ns(struct zebra_ns *,
 						   u_int32_t);
 extern struct interface *if_lookup_by_name_per_ns(struct zebra_ns *,
 						  const char *);
+extern struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
+						  const char *ifname);
 extern struct interface *if_link_per_ns(struct zebra_ns *, struct interface *);
 extern const char *ifindex2ifname_per_ns(struct zebra_ns *, unsigned int);
 

commit ee2f2c23ca2778feafa398e773d24da07d6f174e

zebra: fix crash when interface vrf changes
This crash occurs only with netns implementation.
vrf meaning is different regarging its implementation (netns or
vrf-lite)

- With vrf-lite implementation vrf is a property of the interface that
  can be changed as the speed or the state (iproute2 command: "ip link
  set dev IF_NAME master VRF_NAME"). All interfaces of the system are in
  the same netns and so interface name is unique.
- With netns implementation vrf is a characteristic of the interface
  that CANNOT be changed: it is the id of the netns where the interface
  is located. To change the vrf of an interface (iproute2 command to
  move an interface "ip netns exec VRF_NAME1 ip link set dev IF_NAME
  netns VRF_NAME2") the interface is deleted from the old vrf and
  created in the new vrf.
  Interface name is not unique, the same name can be present in the
  different netns (typically the lo interface) and search of interface
  must be done by the tuple (interface name, netns id).

Current tests on the vrf implementation (vrf-lite or netns) are not
sufficient. In some cases (for example when an interface is moved from
a vrf X to the default vrf and then move back to VRF X) we can have a
corruption message and then a crash of zebra.

To avoid this corruption test on the vrf implementation, needed when an
interface changes, has been rewritten:
- For all interface changes except deletion the if_get_by_name function,
  that checks if an interface exists and creates or updates it if
  needed, is changed:
    * The vrf-lite implementation is unchanged: search of the interface
      is based only on the name and update the vrf-id if needed.
    * The netns implementation search of the interface is based on the
      (name, vrf-id) tuple and interface is created if not found, the
      vrf-id is never updated.
- deletion of an interface (reception of a RTM_DELLINK netlink message):
    * The vrf-lite implementation is unchanged: the interface
      information are cleared and the interface is moved to the default
      vrf if it does not belong to (to allow vrf deletion)
    * The netns implementation is changed: only the interface
      information are cleared and the interface stays in its vrf to
      avoid conflict with interface with the same name in the default
      vrf.

This implementation reverts (partially or totally):
commit 393ec5424e35 ("zebra: fix missing node attribute set in ifp")
commit e9e9b1150f0c ("lib: create interface even if name is the same")
commit 9373219c67e1 ("zebra: improve logs when replacing interface to an
other netns")
Fixes: b53686c52a59 ("zebra: delete interface that disappeared")

lib/if.c
@@ -371,37 +371,47 @@ struct interface *if_lookup_prefix(struct prefix *prefix, vrf_id_t vrf_id)
    one. */
 struct interface *if_get_by_name(const char *name, vrf_id_t vrf_id, int vty)
 {
-	struct interface *ifp;
+	struct interface *ifp = NULL;
 
-	ifp = if_lookup_by_name(name, vrf_id);
-	if (ifp)
-		return ifp;
-	/* Not Found on same VRF. If the interface command
-	 * was entered in vty without a VRF (passed as VRF_DEFAULT),
-	 * accept the ifp we found. If a vrf was entered and there is
-	 * a mismatch, reject it if from vty.
-	 */
-	ifp = if_lookup_by_name_all_vrf(name);
-	if (!ifp)
-		return if_create(name, vrf_id);
-	if (vty) {
-		if (vrf_id == VRF_DEFAULT)
+	if (vrf_is_mapped_on_netns(vrf_lookup_by_id(vrf_id))) {
+		ifp = if_lookup_by_name(name, vrf_id);
+		if (ifp)
 			return ifp;
-		return NULL;
-	}
-	/* if vrf backend uses NETNS, then
-	 * this should not be considered as an update
-	 * then create the new interface
-	 */
-	if (ifp->vrf_id != vrf_id && vrf_is_mapped_on_netns(
-					vrf_lookup_by_id(vrf_id)))
+		if (vty) {
+			/* If the interface command was entered in vty without a
+			 * VRF (passed as VRF_DEFAULT), search an interface with
+			 * this name in all VRs
+			 */
+			if (vrf_id == VRF_DEFAULT)
+				return if_lookup_by_name_all_vrf(name);
+			return NULL;
+		}
 		return if_create(name, vrf_id);
-	/* If it came from the kernel
-	 * or by way of zclient, believe it and update
-	 * the ifp accordingly.
-	 */
-	if_update_to_new_vrf(ifp, vrf_id);
-	return ifp;
+	} else {
+		ifp = if_lookup_by_name_all_vrf(name);
+		if (ifp) {
+			if (ifp->vrf_id == vrf_id)
+				return ifp;
+			/* Found a match on a different VRF. If the interface
+			 * command was entered in vty without a VRF (passed as
+			 * VRF_DEFAULT), accept the ifp we found. If a vrf was
+			 * entered and there is a mismatch, reject it if from
+			 * vty. If it came from the kernel or by way of zclient,
+			 * believe it and update the ifp accordingly.
+			 */
+			if (vty) {
+				if (vrf_id == VRF_DEFAULT)
+					return ifp;
+				return NULL;
+			}
+			/* If it came from the kernel or by way of zclient,
+			 * believe it and update the ifp accordingly.
+			 */
+			if_update_to_new_vrf(ifp, vrf_id);
+			return ifp;
+		}
+		return if_create(name, vrf_id);
+	}
 }
 
 void if_set_index(struct interface *ifp, ifindex_t ifindex)

zebra/if_netlink.c
@@ -1042,67 +1042,6 @@ int netlink_interface_addr(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 	return 0;
 }
 
-/* helper function called by if_netlink_change
- * to delete interfaces in case the interface moved
- * to an other netns
- */
-static void if_netlink_check_ifp_instance_consistency(uint16_t cmd,
-						     struct interface *ifp,
-						     ns_id_t ns_id)
-{
-	struct interface *other_ifp;
-
-	/*
-	 * look if interface name is also found on other netns
-	 * - only if vrf backend is netns
-	 * - do not concern lo interface
-	 * - then remove previous one
-	 * - for new link case, check found interface is not active
-	 */
-	if (!vrf_is_backend_netns() ||
-	    !strcmp(ifp->name, "lo"))
-		return;
-	other_ifp = if_lookup_by_name_not_ns(ns_id, ifp->name);
-	if (!other_ifp)
-		return;
-	/* because previous interface may be inactive,
-	 * interface is moved back to default vrf
-	 * then one may find the same pointer; ignore
-	 */
-	if (other_ifp == ifp)
-		return;
-	if ((cmd == RTM_NEWLINK)
-	    && (CHECK_FLAG(other_ifp->status, ZEBRA_INTERFACE_ACTIVE)))
-		return;
-	if (IS_ZEBRA_DEBUG_KERNEL && cmd == RTM_NEWLINK) {
-		zlog_debug("RTM_NEWLINK %s(%u, VRF %u) replaces %s(%u, VRF %u)\n",
-			   ifp->name,
-			   ifp->ifindex,
-			   ifp->vrf_id,
-			   other_ifp->name,
-			   other_ifp->ifindex,
-			   other_ifp->vrf_id);
-	} else	if (IS_ZEBRA_DEBUG_KERNEL && cmd == RTM_DELLINK) {
-		zlog_debug("RTM_DELLINK %s(%u, VRF %u) is replaced by %s(%u, VRF %u)\n",
-			   ifp->name,
-			   ifp->ifindex,
-			   ifp->vrf_id,
-			   other_ifp->name,
-			   other_ifp->ifindex,
-			   other_ifp->vrf_id);
-	}
-	/* the found interface replaces the current one
-	 * remove it
-	 */
-	if (cmd == RTM_DELLINK)
-		if_delete(ifp);
-	else
-		if_delete(other_ifp);
-	/* the found interface is replaced by the current one
-	 * suppress it
-	 */
-}
-
 int netlink_link_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 {
 	int len;
@@ -1276,8 +1215,6 @@ int netlink_link_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp))
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
-			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
-								  ifp, ns_id);
 		} else if (ifp->vrf_id != vrf_id) {
 			/* VRF change for an interface. */
 			if (IS_ZEBRA_DEBUG_KERNEL)
@@ -1351,8 +1288,6 @@ int netlink_link_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp) || was_bridge_slave)
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
-			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
-								  ifp, ns_id);
 		}
 	} else {
 		/* Delete interface notification from kernel */
@@ -1376,8 +1311,6 @@ int netlink_link_change(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 
 		if (!IS_ZEBRA_IF_VRF(ifp))
 			if_delete_update(ifp);
-		if_netlink_check_ifp_instance_consistency(RTM_DELLINK,
-							  ifp, ns_id);
 	}
 
 	return 0;

zebra/interface.c
@@ -202,7 +202,6 @@ struct interface *if_link_per_ns(struct zebra_ns *ns, struct interface *ifp)
 	if (rn->info) {
 		ifp = (struct interface *)rn->info;
 		route_unlock_node(rn); /* get */
-		ifp->node = rn;
 		return ifp;
 	}
 
@@ -250,30 +249,6 @@ struct interface *if_lookup_by_name_per_ns(struct zebra_ns *ns,
 			return (ifp);
 	}
 
-	return NULL;
-}
-
-/* this function must be used only if the vrf backend
- * is a netns backend
- */
-struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
-					   const char *ifname)
-{
-	struct interface *ifp;
-	struct ns *ns;
-
-	RB_FOREACH (ns, ns_head, &ns_tree) {
-		if (ns->ns_id == ns_id)
-			continue;
-		/* if_delete_update has removed interface
-		 * from zns->if_table
-		 * so to look for interface, use the vrf list
-		 */
-		ifp = if_lookup_by_name(ifname, (vrf_id_t)ns->ns_id);
-		if (!ifp)
-			continue;
-		return ifp;
-	}
 	return NULL;
 }
 
@@ -753,8 +728,12 @@ void if_delete_update(struct interface *ifp)
 	ifp->node = NULL;
 
 	/* if the ifp is in a vrf, move it to default so vrf can be deleted if
-	 * desired */
-	if (ifp->vrf_id)
+	 * desired. This operation is not done for netns implementation to avoid
+	 * collision with interface with the same name in the default vrf (can
+	 * occur with this implementation whereas it is not possible with
+	 * vrf-lite).
+	 */
+	if ((ifp->vrf_id) && !vrf_is_backend_netns())
 		if_handle_vrf_change(ifp, VRF_DEFAULT);
 
 	/* Reset some zebra interface params to default values. */

zebra/interface.h
@@ -324,8 +324,6 @@ extern void zebra_if_init(void);
 extern struct interface *if_lookup_by_index_per_ns(struct zebra_ns *, uint32_t);
 extern struct interface *if_lookup_by_name_per_ns(struct zebra_ns *,
 						  const char *);
-extern struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
-						  const char *ifname);
 extern struct interface *if_link_per_ns(struct zebra_ns *, struct interface *);
 extern const char *ifindex2ifname_per_ns(struct zebra_ns *, unsigned int);
 