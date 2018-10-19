c084724fbcf38c672a3c08528c6f41b37381e9c2
Renato Westphal <renato@opensourcerouting.org>
2018-10-19 15:55:08 -0300

    bgpd: remove wrong assert
    The vnc_direct_del_rn_group_rd() function can be called with the 'afi'
    parameter set to AFI_L2VPN on some specific cases. Remove the assert to
    fix the crash.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- bgpd/rfapi/vnc_export_bgp.c
+++ bgpd/rfapi/vnc_export_bgp.c
@@ -1401,7 +1401,6 @@ static void vnc_direct_bgp_del_group_afi(struct bgp *bgp,
 		return;
 	}
 
-	assert(afi == AFI_IP || afi == AFI_IP6);
 	rt = import_table->imported_vpn[afi];
 
 	if (!rfg->nves && rfg->type != RFAPI_GROUP_CFG_VRF) {
