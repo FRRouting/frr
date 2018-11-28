b77004d63213111525727ec6974899b441b6aca9
Donald Sharp <sharpd@cumulusnetworks.com>
2018-11-28 18:21:08 -0500

    bgpd: Fix crash in various 'show bgp neighbor json' commands
    bgp would crash with various `show bgp neighbor json` commands
    based upon whether or not it did a pretty print of the output
    or not.  This is because we were freeing the data 2 times.
    Cleanup so that we free the json data 1 time.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -11007,6 +11007,7 @@ static void bgp_show_all_instances_neighbors_vty(struct vty *vty,
 			bgp_show_neighbor(vty, bgp, show_all, NULL, NULL,
 					  use_json, json);
 		}
+		json_object_free(json);
 	}
 
 	if (use_json) {
