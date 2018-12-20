0ddfb198ce43fd52fa59e842ed3ef546572f3317
fatpelt <patrick.felt@sling.com>
2018-12-20 14:57:53 -0700

    zmqw: git clang-format cleanup pimd: git clang-format cleanup
    additional fixes provided by the git clang-format process
    
    Signed-off-by: fatpelt <patrick.felt@sling.com>

--- zmqw/zmqw.c
+++ zmqw/zmqw.c
@@ -25,9 +25,9 @@ static int pimd_igmpv3_add(struct igmp_group *group, struct in_addr src_addr)
 	json_object_string_add(out, "group", g);
 	json_object_string_add(out, "source", s);
 
-	jsonOut = XSTRDUP(MTYPE_TMP, 
-		json_object_to_json_string_ext(out, JSON_C_TO_STRING_PRETTY));
-	zmq_send(zmqsock, jsonOut, strlen(jsonOut)+1, 0);
+	jsonOut = XSTRDUP(MTYPE_TMP, json_object_to_json_string_ext(
+					     out, JSON_C_TO_STRING_PRETTY));
+	zmq_send(zmqsock, jsonOut, strlen(jsonOut) + 1, 0);
 	XFREE(MTYPE_TMP, jsonOut);
 	json_object_free(out);
 
@@ -47,9 +47,9 @@ static int pimd_igmpv3_del(struct igmp_group *group, struct in_addr src_addr)
 	json_object_string_add(out, "group", g);
 	json_object_string_add(out, "source", s);
 
-	jsonOut = XSTRDUP(MTYPE_TMP,
-		json_object_to_json_string_ext(out, JSON_C_TO_STRING_PRETTY));
-	zmq_send(zmqsock, jsonOut, strlen(jsonOut)+1, 0);
+	jsonOut = XSTRDUP(MTYPE_TMP, json_object_to_json_string_ext(
+					     out, JSON_C_TO_STRING_PRETTY));
+	zmq_send(zmqsock, jsonOut, strlen(jsonOut) + 1, 0);
 	XFREE(MTYPE_TMP, jsonOut);
 	json_object_free(out);
 
@@ -59,23 +59,28 @@ static int pimd_igmpv3_del(struct igmp_group *group, struct in_addr src_addr)
 static int zmqw_late_init(struct thread_master *tm)
 {
 	const char *format = THIS_MODULE->load_args;
-	/* c doesn't like initializers on dynamic length stack variables */
-	#define ZMQCONNECTSTRLEN 14
-	char zmqConnectStr[ZMQCONNECTSTRLEN] = {0,}; /* tcp:XX*:65536 */
+/* c doesn't like initializers on dynamic length stack variables */
+#define ZMQCONNECTSTRLEN 14
+	char zmqConnectStr[ZMQCONNECTSTRLEN] = {
+		0,
+	}; /* tcp:XX*:65536 */
 	vector formatVector = frrstr_split_vec(format, ";");
 
-	for( size_t x = 0; x < vector_active(formatVector); x++) {
-		vector parameterVector = frrstr_split_vec(vector_lookup(formatVector, x), "=");
+	for (size_t x = 0; x < vector_active(formatVector); x++) {
+		vector parameterVector =
+			frrstr_split_vec(vector_lookup(formatVector, x), "=");
 		char *parameter = vector_lookup(parameterVector, 0);
 		if (strcmp(parameter, "port") == 0) {
-			snprintf(zmqConnectStr, ZMQCONNECTSTRLEN, "tcp://*:%s", (char *)vector_lookup(parameterVector, 1));
+			snprintf(zmqConnectStr, ZMQCONNECTSTRLEN, "tcp://*:%s",
+				 (char *)vector_lookup(parameterVector, 1));
 		}
 	}
 
 	vector_free(formatVector);
 
 	if (*zmqConnectStr == '\0') {
-		zlog_err("could not parse the zmq port number off the command line.  please specify one.  will not initialize the hooks without it\n");
+		zlog_err(
+			"could not parse the zmq port number off the command line.  please specify one.  will not initialize the hooks without it\n");
 		return -1;
 	}
 
@@ -100,9 +105,5 @@ static int zmqw_init(void)
 	return 0;
 }
 
-FRR_MODULE_SETUP(
-	.name = "zmqw",
-	.version = "zmqw",
-	.description = "zmqw",
-	.init = zmqw_init,
-)
+FRR_MODULE_SETUP(.name = "zmqw", .version = "zmqw", .description = "zmqw",
+		 .init = zmqw_init, )
