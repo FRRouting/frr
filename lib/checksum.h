f3edd56ce9538fe06c8ab04f9a91c80815a1b070
Quentin Young <qlyoung@cumulusnetworks.com>
2019-01-22 22:39:47 +0000

    lib: add pseudoheader structs
    Add structures for IPv4 and IPv6 pseudoheaders.
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/checksum.h
+++ lib/checksum.h
@@ -1,4 +1,25 @@
+#include <stdint.h>
+#include <netinet/in.h>
+
 extern int in_cksum(void *, int);
 #define FLETCHER_CHECKSUM_VALIDATE 0xffff
 extern uint16_t fletcher_checksum(uint8_t *, const size_t len,
 				  const uint16_t offset);
+
+/* IPv4 pseudoheader */
+struct ipv4_ph {
+	struct in_addr src;
+	struct in_addr dst;
+	uint8_t rsvd;
+	uint8_t proto;
+	uint16_t len;
+} __attribute__((packed));
+
+/* IPv6 pseudoheader */
+struct ipv6_ph {
+	struct in6_addr src;
+	struct in6_addr dst;
+	uint32_t ulpl;
+	uint8_t zero[3];
+	uint8_t next_hdr;
+} __attribute__((packed));
