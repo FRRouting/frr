6e9529edd655a99e21672649c21621c0a27454bf
Quentin Young <qlyoung@cumulusnetworks.com>
2019-01-23 22:59:07 +0000

    vrrpd: disable multicast loopback
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- vrrpd/vrrp.c
+++ vrrpd/vrrp.c
@@ -664,6 +664,9 @@ static int vrrp_socket(struct vrrp_router *r)
 				r->vr->vrid);
 		}
 
+		/* Turn off multicast loop on Tx */
+		setsockopt_ipv4_multicast_loop(r->sock_tx, 0);
+
 		/* Bind Rx socket to exact interface */
 		vrrp_privs.change(ZPRIVS_RAISE);
 		{
@@ -750,6 +753,9 @@ static int vrrp_socket(struct vrrp_router *r)
 			failed = true;
 			goto done;
 		}
+
+		/* Turn off multicast loop on Tx */
+		setsockopt_ipv6_multicast_loop(r->sock_tx, 0);
 
 		/* Bind Rx socket to exact interface */
 		vrrp_privs.change(ZPRIVS_RAISE);
