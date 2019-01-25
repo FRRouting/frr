6c578835d210dca26edf563ec418f48e99b04cbc
Donald Sharp <sharpd@cumulusnetworks.com>
2019-01-24 19:16:24 -0500

    topotests: Remove unnused function
    The pytest_runtest_call() function is using a deprecated
    function call pytest.config.getoption.  Which is no longer
    available on newer versions of pytest, which is starting to
    show up on newer releases of linux.  Since this function is
    never used and I couldn't figure out the pytest one true way
    I just removed it.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- tests/topotests/conftest.py
+++ tests/topotests/conftest.py
@@ -15,21 +15,6 @@ def pytest_addoption(parser):
     parser.addoption('--topology-only', action='store_true',
                      help='Only set up this topology, don\'t run tests')
 
-def pytest_runtest_call():
-    """
-    This function must be run after setup_module(), it does standarized post
-    setup routines. It is only being used for the 'topology-only' option.
-    """
-    # pylint: disable=E1101
-    # Trust me, 'config' exists.
-    if pytest.config.getoption('--topology-only'):
-        tgen = get_topogen()
-        if tgen is not None:
-            # Allow user to play with the setup.
-            tgen.mininet_cli()
-
-        pytest.exit('the topology executed successfully')
-
 def pytest_assertrepr_compare(op, left, right):
     """
     Show proper assertion error message for json_cmp results.
