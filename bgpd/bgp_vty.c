c72d031488045316230d970e20550a0ccc64bc06
Philippe Guibert <philippe.guibert@6wind.com>
2019-01-30 17:09:04 +0100

    bgpd: increase bgp restart timer value from 1-3600 to 1-4095
    rfc of bgp graceful restart mechanism permits to increase the
    restart timer, since its value is encoded on 12 bit.
    So make available the possibility to extend it.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -1962,7 +1962,7 @@ DEFUN (bgp_graceful_restart_stalepath_time,
 
 DEFUN (bgp_graceful_restart_restart_time,
        bgp_graceful_restart_restart_time_cmd,
-       "bgp graceful-restart restart-time (1-3600)",
+       "bgp graceful-restart restart-time (1-4095)",
        "BGP specific commands\n"
        "Graceful restart capability parameters\n"
        "Set the time to wait to delete stale routes before a BGP open message is received\n"
@@ -1994,7 +1994,7 @@ DEFUN (no_bgp_graceful_restart_stalepath_time,
 
 DEFUN (no_bgp_graceful_restart_restart_time,
        no_bgp_graceful_restart_restart_time_cmd,
-       "no bgp graceful-restart restart-time [(1-3600)]",
+       "no bgp graceful-restart restart-time [(1-4095)]",
        NO_STR
        "BGP specific commands\n"
        "Graceful restart capability parameters\n"
