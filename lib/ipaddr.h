4f838de4201322e6a74e1a0bb0639f22f27d4772
Quentin Young <qlyoung@cumulusnetworks.com>
2019-02-01 21:23:02 +0000

    lib: add sizing macro to ipaddr.h
    Useful for getting the size of the relevant data in the `ip` field of
    struct ipaddr.
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/ipaddr.h
+++ lib/ipaddr.h
@@ -56,6 +56,9 @@ struct ipaddr {
 #define SET_IPADDR_V4(p)  (p)->ipa_type = IPADDR_V4
 #define SET_IPADDR_V6(p)  (p)->ipa_type = IPADDR_V6
 
+#define IPADDRSZ(p)                                                            \
+	IS_IPADDR_V4((p)) ? sizeof(struct in_addr) : sizeof(struct in6_addr)
+
 static inline int str2ipaddr(const char *str, struct ipaddr *ip)
 {
 	int ret;
