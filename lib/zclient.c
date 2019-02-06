c724258df75fb68054e3c2985fb5392e1260ded2
Philippe Guibert <philippe.guibert@6wind.com>
2019-02-06 18:02:19 +0100

    lib: new interface zebra hook looks for interface with vrf
    as it is not possible to receive hooks of interface creation
    before vrf creation and enable, then the vrf should be retrieved.
    Consequently, it is possible to look in the list of interfaces for pre
    configured interfaces.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Emmanuel Vize <emmanuel.vize@6wind.com>
    
    (cherry picked from commit 02cd56308bf2)
    Old-PR: 62623
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Emmanuel Vize <emmanuel.vize@6wind.com>
    (cherry picked from commit 0b1f68c5507dcd0f8e8bbefaaba555ae4fa9710e)
    
    Conflicts:
      lib/zclient.c
    
    PR=76507
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

--- lib/zclient.c
+++ lib/zclient.c
@@ -1979,20 +1979,23 @@ static int zclient_interface_add(struct zclient *zclient, vrf_id_t vrf_id)
 	struct interface *ifp;
 	char ifname_tmp[INTERFACE_NAMSIZ + 1] = {};
 	struct stream *s = zclient->ibuf;
+	struct vrf *vrf;
 
 	/* Read interface name. */
 	STREAM_GET(ifname_tmp, s, INTERFACE_NAMSIZ);
 
 	/* Lookup/create interface by name. */
-	if (!vrf_get(vrf_id, NULL)) {
+	vrf = vrf_get(vrf_id, NULL);
+	if (!vrf) {
 		zlog_debug(
 			"Rx'd interface add from Zebra, but VRF %u does not exist",
 			vrf_id);
 		return -1;
 	}
 
-	ifp = if_get_by_name(ifname_tmp, vrf_id);
-
+	ifp = if_get_by_name_vrf(ifname_tmp, vrf);
+	if (ifp->vrf_id == VRF_UNKNOWN)
+		ifp->vrf_id = vrf->vrf_id;
 	zebra_interface_if_set_value(s, ifp);
 
 	if_new_via_zapi(ifp);
