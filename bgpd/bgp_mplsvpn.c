b9969c861d5e7e0b7d5ecf1d00427282253cc513
Renato Westphal <renato@opensourcerouting.org>
2019-02-26 18:22:10 -0300

    Revert "bgpd: in case nexthop is ipv6, set nh attribute flag in mpls vpn case"
    BGP IPv6 routes should never contain the NEXT_HOP attribute
    (MP_REACH_NLRI should be used instead).
    
    This reverts commit 75cd35c6977137a14ac37347a465d4e8a91eb4c4.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -1088,8 +1088,6 @@ vpn_leak_to_vrf_update_onevrf(struct bgp *bgp_vrf,	    /* to */
 	 */
 	uint8_t nhfamily = NEXTHOP_FAMILY(path_vpn->attr->mp_nexthop_len);
 
-	if (nhfamily != AF_UNSPEC)
-		static_attr.flag |= ATTR_FLAG_BIT(BGP_ATTR_NEXT_HOP);
 	memset(&nexthop_orig, 0, sizeof(nexthop_orig));
 	nexthop_orig.family = nhfamily;
 
@@ -1109,6 +1107,7 @@ vpn_leak_to_vrf_update_onevrf(struct bgp *bgp_vrf,	    /* to */
 			static_attr.mp_nexthop_len =
 				path_vpn->attr->mp_nexthop_len;
 		}
+		static_attr.flag |= ATTR_FLAG_BIT(BGP_ATTR_NEXT_HOP);
 		break;
 	case AF_INET6:
 		/* save */
