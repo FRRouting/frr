7f6077d06322b2ac0d6741c33e644c4c4ec8a95c
Stephen Worley <sworley@cumulusnetworks.com>
2019-03-06 14:56:04 -0500

    zebra: Add a queued flag to nhg_hash_entry
    Added a NEXTHOP_GROUP_QUEUED flag to the nexthop
    group hash entry struct. This indicates when we have
    sent it to be installed to the kernel and are waiting
    for the dataplane provider to process it.
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

--- zebra/zebra_nhg.h
+++ zebra/zebra_nhg.h
@@ -50,6 +50,11 @@ struct nhg_hash_entry {
  * and it's possible usage by a route entry.
  */
 #define NEXTHOP_GROUP_INSTALLED 0x2
+/*
+ * Has the nexthop group been queued to be send to the FIB?
+ * The NEXTHOP_GROUP_VALID flag should also be set by this point.
+ */
+#define NEXTHOP_GROUP_QUEUED 0x4
 };
 
 void zebra_nhg_init(void);
