fb09579ef78f9bc2588658b5ca5c3d17f098477e
Donald Sharp <sharpd@cumulusnetworks.com>
2019-04-03 16:17:26 -0400

    bgpd, pimd: Remove undebug commands
    The undebug XXX commands were deprecated over a year ago now,
    time to nuke from on high.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pim_cmd.c
+++ pimd/pim_cmd.c
@@ -7414,14 +7414,6 @@ DEFUN (no_debug_msdp,
 	return CMD_SUCCESS;
 }
 
-#if CONFDATE > 20190402
-CPP_NOTICE("bgpd: time to remove undebug commands")
-#endif
-ALIAS_HIDDEN (no_debug_msdp,
-              undebug_msdp_cmd,
-              "undebug msdp",
-              UNDEBUG_STR DEBUG_MSDP_STR)
-
 DEFUN (debug_msdp_events,
        debug_msdp_events_cmd,
        "debug msdp events",
@@ -7445,16 +7437,6 @@ DEFUN (no_debug_msdp_events,
 	return CMD_SUCCESS;
 }
 
-#if CONFDATE > 20190402
-CPP_NOTICE("bgpd: time to remove undebug commands")
-#endif
-ALIAS_HIDDEN (no_debug_msdp_events,
-              undebug_msdp_events_cmd,
-              "undebug msdp events",
-              UNDEBUG_STR
-              DEBUG_MSDP_STR
-              DEBUG_MSDP_EVENTS_STR)
-
 DEFUN (debug_msdp_packets,
        debug_msdp_packets_cmd,
        "debug msdp packets",
@@ -7477,16 +7459,6 @@ DEFUN (no_debug_msdp_packets,
 	PIM_DONT_DEBUG_MSDP_PACKETS;
 	return CMD_SUCCESS;
 }
-
-#if CONFDATE > 20190402
-CPP_NOTICE("bgpd: time to remove undebug commands")
-#endif
-ALIAS_HIDDEN (no_debug_msdp_packets,
-              undebug_msdp_packets_cmd,
-              "undebug msdp packets",
-              UNDEBUG_STR
-              DEBUG_MSDP_STR
-              DEBUG_MSDP_PACKETS_STR)
 
 DEFUN (debug_mtrace,
        debug_mtrace_cmd,
@@ -8921,13 +8893,10 @@ void pim_cmd_init(void)
 	install_element(ENABLE_NODE, &no_debug_pim_zebra_cmd);
 	install_element(ENABLE_NODE, &debug_msdp_cmd);
 	install_element(ENABLE_NODE, &no_debug_msdp_cmd);
-	install_element(ENABLE_NODE, &undebug_msdp_cmd);
 	install_element(ENABLE_NODE, &debug_msdp_events_cmd);
 	install_element(ENABLE_NODE, &no_debug_msdp_events_cmd);
-	install_element(ENABLE_NODE, &undebug_msdp_events_cmd);
 	install_element(ENABLE_NODE, &debug_msdp_packets_cmd);
 	install_element(ENABLE_NODE, &no_debug_msdp_packets_cmd);
-	install_element(ENABLE_NODE, &undebug_msdp_packets_cmd);
 	install_element(ENABLE_NODE, &debug_mtrace_cmd);
 	install_element(ENABLE_NODE, &no_debug_mtrace_cmd);
 
@@ -8965,13 +8934,10 @@ void pim_cmd_init(void)
 	install_element(CONFIG_NODE, &no_debug_pim_zebra_cmd);
 	install_element(CONFIG_NODE, &debug_msdp_cmd);
 	install_element(CONFIG_NODE, &no_debug_msdp_cmd);
-	install_element(CONFIG_NODE, &undebug_msdp_cmd);
 	install_element(CONFIG_NODE, &debug_msdp_events_cmd);
 	install_element(CONFIG_NODE, &no_debug_msdp_events_cmd);
-	install_element(CONFIG_NODE, &undebug_msdp_events_cmd);
 	install_element(CONFIG_NODE, &debug_msdp_packets_cmd);
 	install_element(CONFIG_NODE, &no_debug_msdp_packets_cmd);
-	install_element(CONFIG_NODE, &undebug_msdp_packets_cmd);
 	install_element(CONFIG_NODE, &debug_mtrace_cmd);
 	install_element(CONFIG_NODE, &no_debug_mtrace_cmd);
 
