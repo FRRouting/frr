d3b9ece1c189a02f88eb88bacbd36033a1972b14
Donald Sharp <sharpd@cumulusnetworks.com>
2019-05-02 18:25:57 -0400

    pimd: Remove unnecessary boolean to pim_rpf_update
    We already log whether or not we add nht tracking, having
    an additional boolean to say to log another line is
    a bit over the top.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pim_rpf.c
+++ pimd/pim_rpf.c
@@ -195,8 +195,7 @@ static int nexthop_mismatch(const struct pim_nexthop *nh1,
 }
 
 enum pim_rpf_result pim_rpf_update(struct pim_instance *pim,
-				   struct pim_upstream *up, struct pim_rpf *old,
-				   uint8_t is_new)
+				   struct pim_upstream *up, struct pim_rpf *old)
 {
 	struct pim_rpf *rpf = &up->rpf;
 	struct pim_rpf saved;
@@ -216,14 +215,6 @@ enum pim_rpf_result pim_rpf_update(struct pim_instance *pim,
 	saved.source_nexthop = rpf->source_nexthop;
 	saved.rpf_addr = rpf->rpf_addr;
 
-	if (is_new && PIM_DEBUG_ZEBRA) {
-		char source_str[INET_ADDRSTRLEN];
-		pim_inet4_dump("<source?>", up->upstream_addr, source_str,
-			       sizeof(source_str));
-		zlog_debug("%s: NHT Register upstream %s addr %s with Zebra.",
-			   __PRETTY_FUNCTION__, up->sg_str, source_str);
-	}
-	/* Register addr with Zebra NHT */
 	nht_p.family = AF_INET;
 	nht_p.prefixlen = IPV4_MAX_BITLEN;
 	nht_p.u.prefix4.s_addr = up->upstream_addr.s_addr;
