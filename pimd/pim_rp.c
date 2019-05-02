ae14da4860df6bcffd848ee765d3db4750a4c23f
Donald Sharp <sharpd@cumulusnetworks.com>
2019-05-02 18:25:57 -0400

    pimd: Remove unnecessary boolean to pim_rpf_update
    We already log whether or not we add nht tracking, having
    an additional boolean to say to log another line is
    a bit over the top.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- pimd/pim_rp.c
+++ pimd/pim_rp.c
@@ -381,7 +381,7 @@ void pim_upstream_update(struct pim_instance *pim, struct pim_upstream *up)
 
 	old_rpf.source_nexthop.interface = up->rpf.source_nexthop.interface;
 
-	rpf_result = pim_rpf_update(pim, up, &old_rpf, 1);
+	rpf_result = pim_rpf_update(pim, up, &old_rpf);
 	if (rpf_result == PIM_RPF_FAILURE)
 		pim_mroute_del(up->channel_oil, __PRETTY_FUNCTION__);
 
