eea3c899cdb2aa58abea00c6d2e57b5bb0d74d49
Renato Westphal <renato@opensourcerouting.org>
2019-05-20 18:52:16 -0300

    lib: add missing extern "C" {} blocks to new headers
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- lib/typesafe.h
+++ lib/typesafe.h
@@ -23,6 +23,10 @@
 #include <assert.h>
 #include "compiler.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* generic macros for all list-like types */
 
 #define for_each(prefix, head, item)                                           \
@@ -849,6 +853,10 @@ extern void typesafe_skiplist_del(struct sskip_head *head,
 			const struct sskip_item *a,
 			const struct sskip_item *b));
 extern struct sskip_item *typesafe_skiplist_pop(struct sskip_head *head);
+
+#ifdef __cplusplus
+}
+#endif
 
 /* this needs to stay at the end because both files include each other.
  * the resolved order is typesafe.h before typerb.h
