f01f65cf025eb3dffc908529a5662ddfc79db08f
Philippe Guibert <philippe.guibert@6wind.com>
2019-05-28 13:49:53 +0200

    topotests: add static bfd vrf test with failover
    a static bfd entry is configured with 2 uplinks, where only one is valid
    at the same time. the static bfd entry, then will adapt to the uplink by
    being removed and/or created with appropriate local address of the
    uplink.
    Additional changes:
    - config : this test can be run in either vrf netns mode or l3vrf mode
    - config : align bfd config with new implementation
                     multi-hop must be explicit
                     source must be mentioned with multihop
    - rename folder bfd-mpath-topo5 to bfd_mpath_topo5
    - remove vrf0 reference
    - operational : show bfd peer vrf <vrf> <peer> becomes
    					show bfd vrf <vrf> peer <peer>
    
    Old-PR: 63625
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Emmanuel Vize <emmanuel.vize@6wind.com>
    (cherry picked from commit 67d8a4e58579f8d626364dc4f55ffe3657646787)
    PR=76508
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

---
+++ tests/topotests/bfd_vrf_mpath_topo5/test_bfd_vrf_mpath_topo5.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python
+
+#
+# test_bfd_vrf_mpath_topo5.py
+#
+# Copyright 2019 6WIND S.A.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted, provided
+# that the above copyright notice and this permission notice appear
+# in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND NETDEF DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL NETDEF BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THIS SOFTWARE.
+#
+
+"""
+test_bfd_vrf_mpath_topo5.py: Test the FRR/Quagga BFD daemon.
+"""
+
+import os
+import sys
+import json
+import platform
+from functools import partial
+import pytest
+
+# Save the Current Working Directory to find configuration files.
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(CWD, '../'))
+
+# pylint: disable=C0413
+# Import topogen and topotest helpers
+from lib import topotest
+from lib.topogen import Topogen, TopoRouter, get_topogen
+from lib.topolog import logger
+
+# Required to instantiate the topology builder class.
+from mininet.topo import Topo
+
+
+CustomizeVrfWithNetns = True
+
+class BFDTopo(Topo):
+    "Test topology builder"
+    def build(self, *_args, **_opts):
+        "Build function"
+        tgen = get_topogen(self)
+
+        # Create 4 routers
+        for routern in range(1, 5):
+            tgen.add_router('r{}'.format(routern))
+
+        switch = tgen.add_switch('s1')
+        switch.add_link(tgen.gears['r1'])
+        switch.add_link(tgen.gears['r2'])
+
+        switch = tgen.add_switch('s2')
+        switch.add_link(tgen.gears['r2'])
+        switch.add_link(tgen.gears['r3'])
+        switch.add_link(tgen.gears['r4'])
+
+        switch = tgen.add_switch('s3')
+        switch.add_link(tgen.gears['r3'])
+
+        switch = tgen.add_switch('s4')
+        switch.add_link(tgen.gears['r1'])
+
+        switch = tgen.add_switch('s5')
+        switch.add_link(tgen.gears['r1'])
+        switch.add_link(tgen.gears['r4'])
+        
+def setup_module(mod):
+    "Sets up the pytest environment"
+    global CustomizeVrfWithNetns
+
+    tgen = Topogen(BFDTopo, mod.__name__)
+    tgen.start_topology()
+
+    CustomizeVrfWithNetns = True
+    option_vrf_mode = os.getenv('VRF_MODE_PARAM', 'netns')
+    if option_vrf_mode == 'vrf-lite':
+        CustomizeVrfWithNetns = False
+
+    router_list = tgen.routers()
+
+    krel = platform.release()
+    l3mdev_accept = 0
+    if topotest.version_cmp(krel, '4.15') >= 0 and \
+       topotest.version_cmp(krel, '4.18') <= 0:
+        l3mdev_accept = 1
+
+    if topotest.version_cmp(krel, '5.0') >= 0:
+        l3mdev_accept = 1
+
+    if CustomizeVrfWithNetns:
+        if os.system('ip netns list') != 0:
+            return  pytest.skip('Skipping BFD Topo1 VRF NETNS Test. NETNS not available on System')
+
+    # in addition to move interfaces to appropriate netns
+    # - ipv6 forwarding is enabled for each namespace
+    # - ipv6 address are kept after link up / link down operations
+    if CustomizeVrfWithNetns:
+        logger.info('Testing with VRF Namespace support')
+        cmds = ['if [ -e /var/run/netns/{0}-cust1 ] ; then ip netns del {0}-cust1 ; fi',
+                'ip netns add {0}-cust1',
+                'ip link set dev {0}-eth0 netns {0}-cust1',
+                'ip netns exec {0}-cust1 ifconfig {0}-eth0 up',
+                'ip link set dev {0}-eth1 netns {0}-cust1',
+                'ip netns exec {0}-cust1 ifconfig {0}-eth1 up',
+                'ip netns exec {0}-cust1 ip li set dev lo up',
+                'ip netns exec {0}-cust1 sysctl net.ipv6.conf.all.forwarding=1',
+                'ip netns exec {0}-cust1 sysctl net.ipv6.conf.{0}-eth0.keep_addr_on_down=1',
+                'ip netns exec {0}-cust1 sysctl net.ipv6.conf.{0}-eth1.keep_addr_on_down=1']
+
+        cmds2 = ['ip link set dev {0}-eth2 netns {0}-cust1',
+                 'ip netns exec {0}-cust1 ifconfig {0}-eth2 up',
+                 'ip netns exec {0}-cust1 sysctl net.ipv6.conf.{0}-eth2.keep_addr_on_down=1']
+
+        cmds3 = ['ip link add loop1 type dummy',
+                 'ip link set dev loop1 netns {0}-cust1',
+                 'ip netns exec {0}-cust1 ifconfig loop1 up',
+                 'ip netns exec {0}-cust1 sysctl net.ipv6.conf.loop1.keep_addr_on_down=1',
+                 'ip link add loop2 type dummy',
+                 'ip link set dev loop2 netns {0}-cust1',
+                 'ip netns exec {0}-cust1 ifconfig loop2 up',
+                 'ip netns exec {0}-cust1 sysctl net.ipv6.conf.loop2.keep_addr_on_down=1']
+    else:
+        logger.info('setting net.ipv4.tcp_l3mdev_accept={}'.format(l3mdev_accept))
+        logger.info('setting net.ipv4.udp_l3mdev_accept={}'.format(l3mdev_accept))
+        cmds = ['sysctl -w net.ipv4.tcp_l3mdev_accept={}'.format(l3mdev_accept),
+                'sysctl -w net.ipv4.udp_l3mdev_accept={}'.format(l3mdev_accept),
+                'ip link add {0}-cust1 type vrf table 10',
+                'ip link set dev {0}-cust1 up',
+                'ip link set dev {0}-eth0 master {0}-cust1',
+                'ip link set dev {0}-eth1 master {0}-cust1',
+                'sysctl -w net.ipv6.conf.all.forwarding=1',
+                'sysctl net.ipv6.conf.{0}-eth0.keep_addr_on_down=1',
+                'sysctl net.ipv6.conf.{0}-eth1.keep_addr_on_down=1']
+
+        cmds2 = ['ip link set dev {0}-eth2 master {0}-cust1',
+                 'sysctl net.ipv6.conf.{0}-eth2.keep_addr_on_down=1']
+
+        cmds3 = ['ip link add loop1 type dummy',
+                 'ip link set dev loop1 master {0}-cust1',
+                 'sysctl net.ipv6.conf.loop1.keep_addr_on_down=1',
+                 'ip link add loop2 type dummy',
+                 'ip link set dev loop2 master {0}-cust1',
+                 'sysctl net.ipv6.conf.loop2.keep_addr_on_down=1']
+
+    for rname, router in router_list.iteritems():
+        for cmd in cmds:
+            cmd = cmd.format(rname)
+            output = tgen.net[rname].cmd(cmd.format(rname))
+            logger.info('output: '+output);
+        if rname == 'r1':
+            for cmd in cmds2:
+                cmd = cmd.format(rname)
+                output = tgen.net[rname].cmd(cmd.format(rname))
+                logger.info('output: '+output);
+        if rname == 'r3':
+            for cmd in cmds3:
+                cmd = cmd.format(rname)
+                output = tgen.net[rname].cmd(cmd.format(rname))
+                logger.info('output: '+output);
+
+    zebra_option = '--vrfwnetns' if CustomizeVrfWithNetns else ''
+    for rname, router in router_list.iteritems():
+        router.load_config(
+            TopoRouter.RD_ZEBRA,
+            os.path.join(CWD, '{}/zebra.conf'.format(rname)),
+            zebra_option
+        )
+        router.load_config(
+            TopoRouter.RD_BFD,
+            os.path.join(CWD, '{}/bfdd.conf'.format(rname))
+        )
+
+    # Initialize all routers.
+    tgen.start_router()
+
+    # Verify that we are using the proper version and that the BFD
+    # daemon exists.
+    for router in router_list.values():
+        # Check for Version
+        if router.has_version('<', '5.1'):
+            tgen.set_error('Unsupported FRR version')
+            break
+
+
+def teardown_module(_mod):
+    "Teardown the pytest environment"
+    global CustomizeVrfWithNetns
+
+    tgen = get_topogen()
+
+    if CustomizeVrfWithNetns:
+        cmds2 = ['ip netns exec {0}-cust1 ip link set {0}-eth2 netns 1']
+        cmds = ['ip netns exec {0}-cust1 ip link set {0}-eth1 netns 1',
+                'ip netns exec {0}-cust1 ip link set {0}-eth0 netns 1',
+                'ip netns delete {0}-cust1']
+    else:
+        cmds2 = ['ip link set {0}-eth2 nomaster']
+        cmds = ['ip link set dev {0}-eth1 nomaster',
+                'ip link set dev {0}-eth0 nomaster',
+                'ip link delete {0}-cust1']
+
+    router_list = tgen.routers()
+    for rname, router in router_list.iteritems():
+        if rname == 'r1':
+            for cmd in cmds2:
+                tgen.net[rname].cmd(cmd.format(rname))
+        for cmd in cmds:
+            tgen.net[rname].cmd(cmd.format(rname))
+    tgen.stop_topology()
+
+
+def check_bfd_ipv6_nominal_state():
+    tgen = get_topogen()
+    # check bfd entries
+    donna = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.5.1 multihop json', isjson=True)
+    assert donna['peer'] == '192.168.1.3', "r1, 192.168.1.3, bfd entry not present"
+    assert donna['local'] == '192.168.5.1', "r1, 192.168.1.3, bfd local address not 192.168.5.1"
+    assert donna['status'] == 'up', "r1, 192.168.1.3, bfd status not up"
+    assert donna['diagnostic'] == 'ok', "r1, 192.168.1.3, bfd diagnostic not ok"
+    donna = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 1001:1::3 local-address 1005:1::1 multihop json', isjson=True)
+    assert donna['peer'] == '1001:1::3', "r1, 1001:1::3, bfd entry not present"
+    assert donna['local'] == '1005:1::1', "r1, 1001:1::3, bfd local address not 1005:1::1"
+    assert donna['status'] == 'up', "r1, 1001:1::3, bfd status not up"
+    assert donna['diagnostic'] == 'ok', "r1, 1001:1::3, bfd diagnostic not ok"
+
+    # check routing entries
+    donna = tgen.gears['r1'].vtysh_cmd('show ip route vrf r1-cust1 192.168.3.0/24 json', isjson=True)
+    if '192.168.3.0/24' not in donna.keys():
+        assert 0, "r1, route 192.168.3.0/24 not present"
+    routeid = donna['192.168.3.0/24']
+    if 'selected' not in routeid[0].keys():
+        assert 0, "r1, route 192.168.3.0/24 found in BGP RIB is not selected"
+    assert routeid[0]["selected"] == True, "r1, route 192.168.3.0/24 not set to true"
+    if 'nexthops' not in routeid[0].keys():
+        assert 0, "r1, route 192.168.3.0/24 does not have nexthops"
+    nhop = routeid[0]["nexthops"]
+    if nhop[1]['ip'] == "192.168.5.4":
+        assert nhop[1]['interfaceName'] == "r1-eth2", "r1, nh 192.168.5.3 does not use r1-eth2"
+    else:
+        if nhop[0]['ip'] == "192.168.5.4":
+            assert nhop[0]['interfaceName'] == "r1-eth2", "r1, nh 192.168.5.3 does not use r1-eth2"
+        else:
+            assert 0, "r1, route 192.168.3.0/24, no valid nexthop found"
+    donna = tgen.gears['r1'].vtysh_cmd('show ipv6 route vrf r1-cust1 1003:1::/96 json', isjson=True)
+    if '1003:1::/96' not in donna.keys():
+        assert 0, "r1, route 1003:1::/96 not present"
+    routeid = donna['1003:1::/96']
+    if 'selected' not in routeid[0].keys():
+        assert 0, "r1, route 1003:1::/96 found in BGP RIB is not selected"
+    assert routeid[0]["selected"] == True, "r1, route 1003:1::/96 not set to true"
+    if 'nexthops' not in routeid[0].keys():
+        assert 0, "r1, route 1003:1::/96 does not have nexthops"
+    nhop = routeid[0]["nexthops"]
+    if nhop[1]['ip'] == "1005:1::4":
+        assert nhop[1]['interfaceName'] == "r1-eth2", "r1, nh 1003:1::/96 does not use r1-eth2"
+    else:
+        if nhop[0]['ip'] == "1005:1::4":
+            assert nhop[0]['interfaceName'] == "r1-eth2", "r1, nh 1003:1::/96 does not use r1-eth2"
+        else:
+            assert 0, "r1, route 1003:1::/96, no valid nexthop found"
+    
+def test_bfd_connection():
+    "Assert that the BFD peers can find themselves."
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    topotest.sleep(5, 'waiting that BFD initialises')
+    output = tgen.gears['r1'].vtysh_cmd('show running-config', isjson=False)
+    logger.info('==== result from show running-config')
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.5.1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.5.1 multihop')
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 1001:1::3 local-address 1005:1::1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 1001:1::3 local-address 1005:1::1 multihop')
+    logger.info(output)
+    logger.info('==== result from show ip route vrf r1-cust1 and show ipv6 route vrf r1-cust1')
+    output = tgen.gears['r1'].vtysh_cmd('show ip route vrf r1-cust1', isjson=False)
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show ipv6 route vrf r1-cust1', isjson=False)
+    logger.info(output)
+    check_bfd_ipv6_nominal_state()
+
+def test_bfd_fast_convergence():
+    """
+    Assert that BFD notices the link down after simulating network
+    failure.
+    """
+    global CustomizeVrfWithNetns
+
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    logger.info('=========== disabling r4 device')
+    logger.info('waiting for bfd multihop peers to go down')
+    # 
+    # Disable r4-eth0 and r4-eth1 link.
+    netns = 'r4-cust1' if CustomizeVrfWithNetns else None
+    tgen.gears['r4'].link_enable('r4-eth1', enabled=False, netns=netns)
+    tgen.gears['r4'].link_enable('r4-eth0', enabled=False, netns=netns)
+    topotest.sleep(5, 'waiting that BFD event propagates')
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.0.1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.0.1 multihop')
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 1001:1::3 local-address 1000:1::1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 1001:1::3 local-address 1000:1::1 multihop')
+    logger.info(output)
+    logger.info('==== result from show ip route vrf r1-cust1 and show ipv6 route vrf r1-cust1')
+    output = tgen.gears['r1'].vtysh_cmd('show ip route vrf r1-cust1', isjson=False)
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show ipv6 route vrf r1-cust1', isjson=False)
+    logger.info(output)
+
+    # check bfd entries
+    donna = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.0.1 multihop json', isjson=True)
+    assert donna['peer'] == '192.168.1.3', "r1, 192.168.1.3, bfd entry not present"
+    assert donna['local'] == '192.168.0.1', "r1, 192.168.1.3, bfd local address not 192.168.0.1"
+    assert donna['status'] == 'up', "r1, 192.168.1.3, bfd status not up"
+    assert donna['diagnostic'] == 'ok', "r1, 192.168.1.3, bfd diagnostic not ok"
+    donna = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 1001:1::3 local-address 1000:1::1 multihop json', isjson=True)
+    assert donna['peer'] == '1001:1::3', "r1, 1001:1::3, bfd entry not present"
+    assert donna['local'] == '1000:1::1', "r1, 1000:1::1, bfd local address not 1000:1::1"
+    assert donna['status'] == 'up', "r1, 1001:1::3, bfd status not up"
+    assert donna['diagnostic'] == 'ok', "r1, 1001:1::3, bfd diagnostic not ok"
+
+    # check routing entries
+    donna = tgen.gears['r1'].vtysh_cmd('show ip route vrf r1-cust1 192.168.3.0/24 json', isjson=True)
+    if '192.168.3.0/24' not in donna.keys():
+        assert 0, "r1, route 192.168.3.0/24 not present"
+    routeid = donna['192.168.3.0/24']
+    if 'selected' not in routeid[0].keys():
+        assert 0, "r1, route 192.168.3.0/24 found in BGP RIB is not selected"
+    assert routeid[0]["selected"] == True, "r1, route 192.168.3.0/24 not set to true"
+    if 'nexthops' not in routeid[0].keys():
+        assert 0, "r1, route 192.168.3.0/24 does not have nexthops"
+    nhop = routeid[0]["nexthops"]
+    if nhop[1]['ip'] == "192.168.0.2":
+        assert nhop[1]['interfaceName'] == "r1-eth0", "r1, nh 192.168.0.2 does not use r1-eth0"
+    else:
+        if nhop[0]['ip'] == "192.168.0.2":
+            assert nhop[0]['interfaceName'] == "r1-eth2", "r1, nh 192.168.0.2 does not use r1-eth0"
+        else:
+            assert 0, "r1, route 192.168.3.0/24, no valid nexthop found"
+    donna = tgen.gears['r1'].vtysh_cmd('show ipv6 route vrf r1-cust1 1003:1::/96 json', isjson=True)
+    if '1003:1::/96' not in donna.keys():
+        assert 0, "r1, route 1003:1::/96 not present"
+    routeid = donna['1003:1::/96']
+    if 'selected' not in routeid[0].keys():
+        assert 0, "r1, route 1003:1::/96 found in BGP RIB is not selected"
+    assert routeid[0]["selected"] == True, "r1, route 1003:1::/96 not set to true"
+    if 'nexthops' not in routeid[0].keys():
+        assert 0, "r1, route 1003:1::/96 does not have nexthops"
+    nhop = routeid[0]["nexthops"]
+    if nhop[1]['ip'] == "1000:1::2":
+        assert nhop[1]['interfaceName'] == "r1-eth0", "r1, nh 1003:1::/96 does not use r1-eth0"
+    else:
+        if nhop[0]['ip'] == "1000:1::2":
+            assert nhop[0]['interfaceName'] == "r1-eth0", "r1, nh 1003:1::/96 does not use r1-eth0"
+        else:
+            assert 0, "r1, route 1003:1::/96, no valid nexthop found"
+    
+    # expected = 2 mhop sessions should use 192.168.0.2 as gateway
+    # as well as r1-eth0 interface
+    logger.info('=========== enabling r4 device')
+    logger.info('waiting for bfd multihop peers to go up again')
+    tgen.gears['r4'].link_enable('r4-eth0', enabled=True, netns=netns)
+    tgen.gears['r4'].link_enable('r4-eth1', enabled=True, netns=netns)
+    topotest.sleep(5, 'waiting that BFD event propagates')
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.5.1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 192.168.1.3 local-address 192.168.5.1 multihop')
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show bfd vrf r1-cust1 peer 1001:1::3 local-address 1005:1::1 multihop', isjson=False)
+    logger.info('==== result from show bfd vrf r1-cust1 peer 1001:1::3 local-address 1005:1::1 multihop')
+    logger.info(output)
+    logger.info('==== result from show ip route vrf r1-cust1 and show ipv6 route vrf r1-cust1')
+    output = tgen.gears['r1'].vtysh_cmd('show ip route vrf r1-cust1', isjson=False)
+    logger.info(output)
+    output = tgen.gears['r1'].vtysh_cmd('show ipv6 route vrf r1-cust1', isjson=False)
+    logger.info(output)
+    check_bfd_ipv6_nominal_state()
+
+def test_memory_leak():
+    "Run the memory leak test and report results."
+    tgen = get_topogen()
+    if not tgen.is_memleak_enabled():
+        pytest.skip('Memory leak test/report is disabled')
+
+    tgen.report_memory_leaks()
+
+
+if __name__ == '__main__':
+    args = ["-s"] + sys.argv[1:]
+    sys.exit(pytest.main(args))
