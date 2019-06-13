f73126c31acdd496dcc88c69059a5e0103d6a2ff
Stephen Worley <sworley@cumulusnetworks.com>
2019-06-13 17:46:29 -0400

    lib,vtysh: Add vtysh commands for log-filter
    Add vtysh commands to add/del/clear/show filters across
    all daemons and independently on each one. Add automake and
    clippy boilerplate for those commands as well.
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

---
+++ lib/log_vty.h
@@ -0,0 +1,24 @@
+/*
+ * Logging - VTY library
+ * Copyright (C) 2019 Cumulus Networks, Inc.
+ *                    Stephen Worley
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef __LOG_VTY_H__
+#define __LOG_VTY_H__
+extern void log_filter_cmd_init(void);
+#endif /* __LOG_VTY_H__ */
