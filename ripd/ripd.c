100db763b70d7089d7222efa6aa698c611e4daa4
Philippe Guibert <philippe.guibert@6wind.com>
2019-06-20 15:02:15 +0200

    ripd: update the default vrf name by registering to callback
    if default vrf name is updated, then rip contexts based on that
    hypothetical vrfname, will be enabled.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- ripd/ripd.c
+++ ripd/ripd.c
@@ -3682,7 +3682,7 @@ static int rip_vrf_disable(struct vrf *vrf)
 void rip_vrf_init(void)
 {
 	vrf_init(rip_vrf_new, rip_vrf_enable, rip_vrf_disable, rip_vrf_delete,
-		 NULL);
+		 rip_vrf_enable);
 }
 
 void rip_vrf_terminate(void)
