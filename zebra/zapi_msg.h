e11d7c96d7de7f7117dc544f2e6a59a83335d185
Emanuele Di Pascale <emanuele@voltanet.io>
2019-06-27 10:11:35 +0200

    zebra: label manager refactor
    in order to both streamline the code and allow users to
    define their own specialized versions of the LM api handlers,
    define hooks for the 4 main primitives offered by the label
    manager (i.e. connect, disconnect, get_chunk and release_chunk),
    and have the existing code be run in response to a hook_call.
    Additionally, have the responses to the requesting daemon be
    callable from an external API.
    
    Note that the proxy version of the label manager was a source of
    issues and hardly used in practice. With the new hooks, users with
    more complex requirements can simply plug in their own code to
    handle label distribution remotely, so there is no longer a reason
    to maintain this code.
    
    Signed-off-by: Emanuele Di Pascale <emanuele@voltanet.io>

--- zebra/zapi_msg.h
+++ zebra/zapi_msg.h
@@ -29,6 +29,8 @@
 #include "zebra/zserv.h"
 #include "zebra/zebra_pbr.h"
 #include "zebra/zebra_errors.h"
+#include "zebra/label_manager.h"
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -90,6 +92,14 @@ extern void zserv_nexthop_num_warn(const char *caller, const struct prefix *p,
 				   const unsigned int nexthop_num);
 
 extern void zsend_capabilities_all_clients(void);
+extern int zsend_assign_label_chunk_response(struct zserv *client,
+					     vrf_id_t vrf_id, uint8_t proto,
+					     uint16_t instance,
+					     struct label_manager_chunk *lmc);
+extern int zsend_label_manager_connect_response(struct zserv *client,
+						vrf_id_t vrf_id,
+						unsigned short result);
+
 
 #ifdef __cplusplus
 }
