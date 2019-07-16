9fafbd158241f4c063af4ddbdc31cec5f4cc657f
Donald Sharp <sharpd@cumulusnetworks.com>
2019-07-16 08:27:31 -0400

    staticd: Track state of where we are and limit installs/updates to min
    Track the state of the route and how we have installed it or not.
    This commit limits the number of installs/updates/deletes to a
    minimum number instead of repeated sends to zebra.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- staticd/static_nht.h
+++ staticd/static_nht.h
@@ -34,4 +34,17 @@
  */
 extern void static_nht_update(struct prefix *sp, struct prefix *nhp,
 			      uint32_t nh_num, afi_t afi, vrf_id_t vrf_id);
+
+/*
+ * For the given tracked nexthop, nhp, mark all routes that use
+ * this route as in starting state again.
+ */
+extern void static_nht_reset_start(struct prefix *nhp, afi_t afi,
+				   vrf_id_t nh_vrf_id);
+
+/*
+ * For the given prefix, sp, mark it as in a particular state
+ */
+extern void static_nht_mark_state(struct prefix *sp, vrf_id_t vrf_id,
+				  enum static_install_states state);
 #endif
