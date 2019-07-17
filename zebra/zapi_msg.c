911a7ffd575544583a683fbc23f6a537b5f815bd
Donald Sharp <sharpd@cumulusnetworks.com>
2019-07-16 20:17:05 -0400

    zebra: Guard debug messages
    A bunch of debug code has snuck in that is unguarded.
    Fix this.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -1938,9 +1938,10 @@ static void zread_get_label_chunk(struct zserv *client, struct stream *msg,
 			"Unable to assign Label Chunk of size %u to %s instance %u",
 			size, zebra_route_string(proto), instance);
 	else
-		zlog_debug("Assigned Label Chunk %u - %u to %s instance %u",
-			   lmc->start, lmc->end,
-			   zebra_route_string(proto), instance);
+		if (IS_ZEBRA_DEBUG_PACKET)
+			zlog_debug("Assigned Label Chunk %u - %u to %s instance %u",
+				   lmc->start, lmc->end,
+				   zebra_route_string(proto), instance);
 
 stream_failure:
 	return;
