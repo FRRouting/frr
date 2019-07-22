c4db51ba63abf2f32b4d8199dd0ab77260ed0f14
Bug Fest <52962234+bugfest@users.noreply.github.com>
2019-07-22 22:59:24 +0200

    alpine: disable-ntop-override flag
    This new flag in the configure script disables libc-ntop override
    Skipping ntop override tests if FRR buils with "disable-ntop-override"
    flag enabled
    
    Signed-off-by: <52962234+bugfest@users.noreply.github.com>

--- tests/lib/test_ntop.py
+++ tests/lib/test_ntop.py
@@ -1,6 +1,10 @@
 import frrtest
+import pytest
 
 class TestNtop(frrtest.TestMultiOut):
     program = './test_ntop'
 
-TestNtop.exit_cleanly()
+    @pytest.mark.skipif('S["NTOP_NO_OVERRIDE_TRUE"]!=""\n' not in open('../config.status').readlines(),
+                        reason='NTOP is not overridden')
+    def test_exit_cleanly(self):
+        return super(TestNtop, self).test_exit_cleanly()
