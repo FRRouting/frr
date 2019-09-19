1f5d3fc3086791e7edfd9292caafa2f9329ee42e
Philippe Guibert <philippe.guibert@6wind.com>
2019-09-19 16:39:24 +0200

    bgpd: no rpki command available
    this command permits to stop and flush the rpki contexts for a specific
    vrf.
    
    Old-PR: 65330
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Emmanuel Vize <emmanuel.vize@6wind.com>
    (cherry picked from commit f88227fb8dac)
    
    Conflicts:
    	vtysh/vtysh.c
    
    PR=76591
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -1752,6 +1752,16 @@ DEFUNSH(VTYSH_BGPD, address_family_ipv6_labeled_unicast,
 }
 
 DEFUNSH(VTYSH_BGPD,
+	no_rpki,
+	no_rpki_cmd,
+	"no rpki",
+	NO_STR
+	"rpki\n")
+{
+	return CMD_SUCCESS;
+}
+
+DEFUNSH(VTYSH_BGPD,
 	rpki,
 	rpki_cmd,
 	"rpki",
@@ -4034,6 +4044,7 @@ void vtysh_init_vty(void)
 
 	install_node(&rpki_node);
 	install_element(CONFIG_NODE, &rpki_cmd);
+	install_element(CONFIG_NODE, &no_rpki_cmd);
 	install_element(RPKI_NODE, &rpki_exit_cmd);
 	install_element(RPKI_NODE, &rpki_quit_cmd);
 	install_element(RPKI_NODE, &vtysh_end_all_cmd);
@@ -4042,6 +4053,7 @@ void vtysh_init_vty(void)
 
 	install_node(&rpki_vrf_node);
 	install_element(VRF_NODE, &rpki_cmd);
+	install_element(VRF_NODE, &no_rpki_cmd);
 	install_element(RPKI_VRF_NODE, &rpki_exit_cmd);
 	install_element(RPKI_VRF_NODE, &rpki_quit_cmd);
 	install_element(RPKI_VRF_NODE, &vtysh_end_all_cmd);
