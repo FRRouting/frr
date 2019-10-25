3acc394bc5e5c225e9258fd0d57a6cebea0c0ccd
Philippe Guibert <philippe.guibert@6wind.com>
2019-10-25 14:25:00 +0200

    zebra: when parsing local entry against dad, retrieve config
    when duplicate address detection is observed, some incrementation,
    some timing mechanisms need to be done. For that the main evpn
    configuration is retrieved. Until now, the VRF that was storing the dad
    config parameters was the same VRF that hosted the VXLAN interface. With
    netns backend, this is not true, as the VXLAN interface is in the
    same VRF as the bridge interface. The modification takes same definition
    as in BGP, that is to say that there is a single bgp evpn instance, and
    this is that instance that will give the correct config settings.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zebra_vxlan.c
+++ zebra/zebra_vxlan.c
@@ -5680,7 +5680,7 @@ static void process_remote_macip_add(vni_t vni,
 		return;
 	}
 
-	zvrf = vrf_info_lookup(zvni->vxlan_if->vrf_id);
+	zvrf = zebra_vrf_get_evpn();
 	if (!zvrf)
 		return;
 
@@ -8189,11 +8189,10 @@ int zebra_vxlan_local_mac_add_update(struct interface *ifp,
 		return -1;
 	}
 
-	zvrf = vrf_info_lookup(zvni->vxlan_if->vrf_id);
+	zvrf = zebra_vrf_get_evpn();
 	if (!zvrf) {
 		if (IS_ZEBRA_DEBUG_VXLAN)
-			zlog_debug("        No Vrf found for vrf_id: %d",
-				   zvni->vxlan_if->vrf_id);
+			zlog_debug("        No Evpn Global Vrf found");
 		return -1;
 	}
 
