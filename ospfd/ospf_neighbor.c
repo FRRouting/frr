b4074a4762aafc67d92ecfde428e780a5d093a74
Quentin Young <qlyoung@cumulusnetworks.com>
2019-12-12 14:53:06 -0500

    ospfd allow fuzzing LSUPD, LSACK, LSREQ packets
    - Anything except HELLO wants a neighbor created, so do that
    - Skip some unnecessary stuff
    - Most stuff checks the LSDB and returns early, so skip those
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- ospfd/ospf_neighbor.c
+++ ospfd/ospf_neighbor.c
@@ -445,8 +445,10 @@ static struct ospf_neighbor *ospf_nbr_add(struct ospf_interface *oi,
 				nbr_nbma->nbr = nbr;
 				nbr->nbr_nbma = nbr_nbma;
 
+#ifndef FUZZING
 				if (nbr_nbma->t_poll)
 					OSPF_POLL_TIMER_OFF(nbr_nbma->t_poll);
+#endif
 
 				nbr->state_change = nbr_nbma->state_change + 1;
 			}
@@ -454,8 +456,10 @@ static struct ospf_neighbor *ospf_nbr_add(struct ospf_interface *oi,
 	}
 
 	/* New nbr, save the crypto sequence number if necessary */
+#ifndef FUZZING
 	if (ntohs(ospfh->auth_type) == OSPF_AUTH_CRYPTOGRAPHIC)
 		nbr->crypt_seqnum = ospfh->u.crypt.crypt_seqnum;
+#endif
 
 	/* Configure BFD if interface has it. */
 	ospf_neighbor_bfd_apply(nbr);
