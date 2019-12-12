4c1cb1a3ab57c82dcb9a30148ed61a3325fb09b2
Quentin Young <qlyoung@cumulusnetworks.com>
2019-12-12 14:52:20 -0500

    lib: disable thread scheduler funcs
    dont work when we skip threadmaster creation / init
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- lib/thread.h
+++ lib/thread.h
@@ -161,6 +161,8 @@ struct cpu_thread_history {
 
 #define debugargdef  const char *funcname, const char *schedfrom, int fromln
 
+#ifndef FUZZING
+
 #define thread_add_read(m,f,a,v,t) funcname_thread_add_read_write(THREAD_READ,m,f,a,v,t,#f,__FILE__,__LINE__)
 #define thread_add_write(m,f,a,v,t) funcname_thread_add_read_write(THREAD_WRITE,m,f,a,v,t,#f,__FILE__,__LINE__)
 #define thread_add_timer(m,f,a,v,t) funcname_thread_add_timer(m,f,a,v,t,#f,__FILE__,__LINE__)
@@ -170,6 +172,19 @@ struct cpu_thread_history {
 #define thread_execute(m,f,a,v) funcname_thread_execute(m,f,a,v,#f,__FILE__,__LINE__)
 #define thread_execute_name(m, f, a, v, n)				\
 	funcname_thread_execute(m, f, a, v, n, __FILE__, __LINE__)
+
+#else
+
+#define thread_add_read(m,f,a,v,t) 0
+#define thread_add_write(m,f,a,v,t) 0
+#define thread_add_timer(m,f,a,v,t) 0
+#define thread_add_timer_msec(m,f,a,v,t) 0
+#define thread_add_timer_tv(m,f,a,v,t) 0
+#define thread_add_event(m,f,a,v,t) 0
+#define thread_execute(m,f,a,v) 0
+#define thread_execute_name(m, f, a, v, n) 0
+
+#endif
 
 /* Prototypes. */
 extern struct thread_master *thread_master_create(const char *);
