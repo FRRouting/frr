6d097bf15c1bcc6cb71b01f980dfe9a2bdd93ce9
Quentin Young <qlyoung@cumulusnetworks.com>
2020-01-06 12:09:23 -0500

    zebra: free ptm message on error
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- zebra/zebra_ptm.c
+++ zebra/zebra_ptm.c
@@ -1431,6 +1431,7 @@ static void _zebra_ptm_reroute(struct zserv *zs, struct zebra_vrf *zvrf,
 	return;
 
 stream_failure:
+	stream_free(msgc);
 	zlog_err("%s:%d failed to registrate client pid", __FILE__, __LINE__);
 }
 
