FRRouting/frr

commit 0eb97b860dc94329cf9add9f8f3d3a2c7f539568

lib,zebra: use nhg_hash_entry pointer in route_entry
Replace the existing list of nexthops (via a nexthop_group
struct) in the route_entry with a direct pointer to zebra's
new shared group (from zebra_nhg.h). This allows more
direct access to that shared group and the info it carries.

Signed-off-by: Mark Stapp <mjs@voltanet.io>

lib/nexthop_group.c
@@ -233,8 +233,8 @@ void _nexthop_add(struct nexthop **target, struct nexthop *nexthop)
 	nexthop->prev = last;
 }
 
-void _nexthop_group_add_sorted(struct nexthop_group *nhg,
-			       struct nexthop *nexthop)
+void nexthop_group_add_sorted(struct nexthop_group *nhg,
+			      struct nexthop *nexthop)
 {
 	struct nexthop *position, *prev, *tail;
 

lib/nexthop_group.h
@@ -50,6 +50,8 @@ void copy_nexthops(struct nexthop **tnh, const struct nexthop *nh,
 uint32_t nexthop_group_hash_no_recurse(const struct nexthop_group *nhg);
 uint32_t nexthop_group_hash(const struct nexthop_group *nhg);
 void nexthop_group_mark_duplicates(struct nexthop_group *nhg);
+void nexthop_group_add_sorted(struct nexthop_group *nhg,
+			      struct nexthop *nexthop);
 
 /* The following for loop allows to iterate over the nexthop
  * structure of routes.

lib/nexthop_group_private.h
@@ -35,8 +35,6 @@ extern "C" {
 
 void _nexthop_add(struct nexthop **target, struct nexthop *nexthop);
 void _nexthop_del(struct nexthop_group *nhg, struct nexthop *nexthop);
-void _nexthop_group_add_sorted(struct nexthop_group *nhg,
-			       struct nexthop *nexthop);
 
 #ifdef __cplusplus
 }

zebra/redistribute.c
@@ -637,13 +637,15 @@ int zebra_add_import_table_entry(struct zebra_vrf *zvrf, struct route_node *rn,
 	struct route_entry *newre;
 	struct route_entry *same;
 	struct prefix p;
+	struct nexthop_group *ng;
 	route_map_result_t ret = RMAP_PERMITMATCH;
 	afi_t afi;
 
 	afi = family2afi(rn->p.family);
 	if (rmap_name)
 		ret = zebra_import_table_route_map_check(
-			afi, re->type, re->instance, &rn->p, re->ng->nexthop,
+			afi, re->type, re->instance, &rn->p,
+			re->nhe->nhg->nexthop,
 			zvrf->vrf->vrf_id, re->tag, rmap_name);
 
 	if (ret != RMAP_PERMITMATCH) {
@@ -678,10 +680,11 @@ int zebra_add_import_table_entry(struct zebra_vrf *zvrf, struct route_node *rn,
 	newre->table = zvrf->table_id;
 	newre->uptime = monotime(NULL);
 	newre->instance = re->table;
-	newre->ng = nexthop_group_new();
-	route_entry_copy_nexthops(newre, re->ng->nexthop);
 
-	rib_add_multipath(afi, SAFI_UNICAST, &p, NULL, newre);
+	ng = nexthop_group_new();
+	copy_nexthops(&ng->nexthop, re->nhe->nhg->nexthop, NULL);
+
+	rib_add_multipath(afi, SAFI_UNICAST, &p, NULL, newre, ng);
 
 	return 0;
 }
@@ -696,8 +699,9 @@ int zebra_del_import_table_entry(struct zebra_vrf *zvrf, struct route_node *rn,
 	prefix_copy(&p, &rn->p);
 
 	rib_delete(afi, SAFI_UNICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_TABLE,
-		   re->table, re->flags, &p, NULL, re->ng->nexthop, re->nhe_id,
-		   zvrf->table_id, re->metric, re->distance, false);
+		   re->table, re->flags, &p, NULL, re->nhe->nhg->nexthop,
+		   re->nhe_id, zvrf->table_id, re->metric, re->distance,
+		   false);
 
 	return 0;
 }

zebra/rib.h
@@ -88,10 +88,14 @@ struct route_entry {
 	/* Link list. */
 	struct re_list_item next;
 
-	/* Nexthop structure (from RIB) */
-	struct nexthop_group *ng;
+	/* Nexthop group, shared/refcounted, based on the nexthop(s)
+	 * provided by the owner of the route
+	 */
+	struct nhg_hash_entry *nhe;
 
-	/* Nexthop group from FIB (optional) */
+	/* Nexthop group from FIB (optional), reflecting what is actually
+	 * installed in the FIB if that differs.
+	 */
 	struct nexthop_group fib_ng;
 
 	/* Nexthop group hash entry ID */
@@ -308,31 +312,6 @@ typedef enum {
 	RIB_UPDATE_MAX
 } rib_update_event_t;
 
-extern struct nexthop *route_entry_nexthop_ifindex_add(struct route_entry *re,
-						       ifindex_t ifindex,
-						       vrf_id_t nh_vrf_id);
-extern struct nexthop *
-route_entry_nexthop_blackhole_add(struct route_entry *re,
-				  enum blackhole_type bh_type);
-extern struct nexthop *route_entry_nexthop_ipv4_add(struct route_entry *re,
-						    struct in_addr *ipv4,
-						    struct in_addr *src,
-						    vrf_id_t nh_vrf_id);
-extern struct nexthop *
-route_entry_nexthop_ipv4_ifindex_add(struct route_entry *re,
-				     struct in_addr *ipv4, struct in_addr *src,
-				     ifindex_t ifindex, vrf_id_t nh_vrf_id);
-extern void route_entry_nexthop_delete(struct route_entry *re,
-				       struct nexthop *nexthop);
-extern struct nexthop *route_entry_nexthop_ipv6_add(struct route_entry *re,
-						    struct in6_addr *ipv6,
-						    vrf_id_t nh_vrf_id);
-extern struct nexthop *
-route_entry_nexthop_ipv6_ifindex_add(struct route_entry *re,
-				     struct in6_addr *ipv6, ifindex_t ifindex,
-				     vrf_id_t nh_vrf_id);
-extern void route_entry_nexthop_add(struct route_entry *re,
-				    struct nexthop *nexthop);
 extern void route_entry_copy_nexthops(struct route_entry *re,
 				      struct nexthop *nh);
 int route_entry_update_nhe(struct route_entry *re, struct nhg_hash_entry *new);
@@ -370,7 +349,8 @@ extern int rib_add(afi_t afi, safi_t safi, vrf_id_t vrf_id, int type,
 		   uint32_t mtu, uint8_t distance, route_tag_t tag);
 
 extern int rib_add_multipath(afi_t afi, safi_t safi, struct prefix *p,
-			     struct prefix_ipv6 *src_p, struct route_entry *re);
+			     struct prefix_ipv6 *src_p, struct route_entry *re,
+			     struct nexthop_group *ng);
 
 extern void rib_delete(afi_t afi, safi_t safi, vrf_id_t vrf_id, int type,
 		       unsigned short instance, int flags, struct prefix *p,
@@ -535,7 +515,7 @@ static inline struct nexthop_group *rib_active_nhg(struct route_entry *re)
 	if (re->fib_ng.nexthop)
 		return &(re->fib_ng);
 	else
-		return re->ng;
+		return re->nhe->nhg;
 }
 
 extern void zebra_vty_init(void);

zebra/rt_netlink.c
@@ -390,7 +390,7 @@ parse_nexthop_unicast(ns_id_t ns_id, struct rtmsg *rtm, struct rtattr **tb,
 }
 
 static uint8_t parse_multipath_nexthops_unicast(ns_id_t ns_id,
-						struct route_entry *re,
+						struct nexthop_group *ng,
 						struct rtmsg *rtm,
 						struct rtnexthop *rtnh,
 						struct rtattr **tb,
@@ -406,8 +406,6 @@ static uint8_t parse_multipath_nexthops_unicast(ns_id_t ns_id,
 
 	int len = RTA_PAYLOAD(tb[RTA_MULTIPATH]);
 	vrf_id_t nh_vrf_id = vrf_id;
-
-	re->ng = nexthop_group_new();
 
 	for (;;) {
 		struct nexthop *nh = NULL;
@@ -454,21 +452,19 @@ static uint8_t parse_multipath_nexthops_unicast(ns_id_t ns_id,
 
 		if (gate && rtm->rtm_family == AF_INET) {
 			if (index)
-				nh = route_entry_nexthop_ipv4_ifindex_add(
-					re, gate, prefsrc, index, nh_vrf_id);
+				nh = nexthop_from_ipv4_ifindex(
+					gate, prefsrc, index, nh_vrf_id);
 			else
-				nh = route_entry_nexthop_ipv4_add(
-					re, gate, prefsrc, nh_vrf_id);
+				nh = nexthop_from_ipv4(gate, prefsrc,
+						       nh_vrf_id);
 		} else if (gate && rtm->rtm_family == AF_INET6) {
 			if (index)
-				nh = route_entry_nexthop_ipv6_ifindex_add(
-					re, gate, index, nh_vrf_id);
+				nh = nexthop_from_ipv6_ifindex(
+					gate, index, nh_vrf_id);
 			else
-				nh = route_entry_nexthop_ipv6_add(re, gate,
-								  nh_vrf_id);
+				nh = nexthop_from_ipv6(gate, nh_vrf_id);
 		} else
-			nh = route_entry_nexthop_ifindex_add(re, index,
-							     nh_vrf_id);
+			nh = nexthop_from_ifindex(index, nh_vrf_id);
 
 		if (nh) {
 			if (num_labels)
@@ -477,6 +473,9 @@ static uint8_t parse_multipath_nexthops_unicast(ns_id_t ns_id,
 
 			if (rtnh->rtnh_flags & RTNH_F_ONLINK)
 				SET_FLAG(nh->flags, NEXTHOP_FLAG_ONLINK);
+
+			/* Add to temporary list */
+			nexthop_group_add_sorted(ng, nh);
 		}
 
 		if (rtnh->rtnh_len == 0)
@@ -486,10 +485,7 @@ static uint8_t parse_multipath_nexthops_unicast(ns_id_t ns_id,
 		rtnh = RTNH_NEXT(rtnh);
 	}
 
-	uint8_t nhop_num = nexthop_group_nexthop_num(re->ng);
-
-	if (!nhop_num)
-		nexthop_group_delete(&re->ng);
+	uint8_t nhop_num = nexthop_group_nexthop_num(ng);
 
 	return nhop_num;
 }
@@ -737,6 +733,7 @@ static int netlink_route_change_read_unicast(struct nlmsghdr *h, ns_id_t ns_id,
 		} else {
 			/* This is a multipath route */
 			struct route_entry *re;
+			struct nexthop_group *ng = NULL;
 			struct rtnexthop *rtnh =
 				(struct rtnexthop *)RTA_DATA(tb[RTA_MULTIPATH]);
 
@@ -753,19 +750,30 @@ static int netlink_route_change_read_unicast(struct nlmsghdr *h, ns_id_t ns_id,
 			re->nhe_id = nhe_id;
 
 			if (!nhe_id) {
-				uint8_t nhop_num =
+				uint8_t nhop_num;
+
+				/* Use temporary list of nexthops; parse
+				 * message payload's nexthops.
+				 */
+				ng = nexthop_group_new();
+				nhop_num =
 					parse_multipath_nexthops_unicast(
-						ns_id, re, rtm, rtnh, tb,
+						ns_id, ng, rtm, rtnh, tb,
 						prefsrc, vrf_id);
 
 				zserv_nexthop_num_warn(
 					__func__, (const struct prefix *)&p,
 					nhop_num);
+
+				if (nhop_num == 0) {
+					nexthop_group_delete(&ng);
+					ng = NULL;
+				}
 			}
 
-			if (nhe_id || re->ng)
+			if (nhe_id || ng)
 				rib_add_multipath(afi, SAFI_UNICAST, &p,
-						  &src_p, re);
+						  &src_p, re, ng);
 			else
 				XFREE(MTYPE_RE, re);
 		}

zebra/zapi_msg.c
@@ -559,7 +559,8 @@ int zsend_redistribute_route(int cmd, struct zserv *client,
 		memcpy(&api.src_prefix, src_p, sizeof(api.src_prefix));
 	}
 
-	for (nexthop = re->ng->nexthop; nexthop; nexthop = nexthop->next) {
+	for (nexthop = re->nhe->nhg->nexthop;
+	     nexthop; nexthop = nexthop->next) {
 		if (!CHECK_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE))
 			continue;
 
@@ -666,7 +667,7 @@ static int zsend_ipv4_nexthop_lookup_mrib(struct zserv *client,
 		 * nexthop we are looking up. Therefore, we will just iterate
 		 * over the top chain of nexthops.
 		 */
-		for (nexthop = re->ng->nexthop; nexthop;
+		for (nexthop = re->nhe->nhg->nexthop; nexthop;
 		     nexthop = nexthop->next)
 			if (CHECK_FLAG(nexthop->flags, NEXTHOP_FLAG_ACTIVE))
 				num += zserv_encode_nexthop(s, nexthop);
@@ -1393,6 +1394,7 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 	struct prefix_ipv6 *src_p = NULL;
 	struct route_entry *re;
 	struct nexthop *nexthop = NULL;
+	struct nexthop_group *ng = NULL;
 	int i, ret;
 	vrf_id_t vrf_id = 0;
 	struct ipaddr vtep_ip;
@@ -1424,7 +1426,6 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 	re->flags = api.flags;
 	re->uptime = monotime(NULL);
 	re->vrf_id = vrf_id;
-	re->ng = nexthop_group_new();
 
 	if (api.tableid)
 		re->table = api.tableid;
@@ -1438,10 +1439,12 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 			  __func__, &api.prefix,
 			  zebra_route_string(client->proto));
 
-		nexthop_group_delete(&re->ng);
 		XFREE(MTYPE_RE, re);
 		return;
 	}
+
+	/* Use temporary list of nexthops */
+	ng = nexthop_group_new();
 
 	/*
 	 * TBD should _all_ of the nexthop add operations use
@@ -1457,8 +1460,8 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 
 		switch (api_nh->type) {
 		case NEXTHOP_TYPE_IFINDEX:
-			nexthop = route_entry_nexthop_ifindex_add(
-				re, api_nh->ifindex, api_nh->vrf_id);
+			nexthop = nexthop_from_ifindex(api_nh->ifindex,
+						       api_nh->vrf_id);
 			break;
 		case NEXTHOP_TYPE_IPV4:
 			if (IS_ZEBRA_DEBUG_RECV) {
@@ -1469,8 +1472,8 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 				zlog_debug("%s: nh=%s, vrf_id=%d", __func__,
 					   nhbuf, api_nh->vrf_id);
 			}
-			nexthop = route_entry_nexthop_ipv4_add(
-				re, &api_nh->gate.ipv4, NULL, api_nh->vrf_id);
+			nexthop = nexthop_from_ipv4(&api_nh->gate.ipv4,
+						    NULL, api_nh->vrf_id);
 			break;
 		case NEXTHOP_TYPE_IPV4_IFINDEX:
 
@@ -1486,8 +1489,8 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 					__func__, nhbuf, api_nh->vrf_id,
 					re->vrf_id, ifindex);
 			}
-			nexthop = route_entry_nexthop_ipv4_ifindex_add(
-				re, &api_nh->gate.ipv4, NULL, ifindex,
+			nexthop = nexthop_from_ipv4_ifindex(
+				&api_nh->gate.ipv4, NULL, ifindex,
 				api_nh->vrf_id);
 
 			/* Special handling for IPv4 routes sourced from EVPN:
@@ -1504,15 +1507,15 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 			}
 			break;
 		case NEXTHOP_TYPE_IPV6:
-			nexthop = route_entry_nexthop_ipv6_add(
-				re, &api_nh->gate.ipv6, api_nh->vrf_id);
+			nexthop = nexthop_from_ipv6(&api_nh->gate.ipv6,
+						    api_nh->vrf_id);
 			break;
 		case NEXTHOP_TYPE_IPV6_IFINDEX:
 			memset(&vtep_ip, 0, sizeof(struct ipaddr));
 			ifindex = api_nh->ifindex;
-			nexthop = route_entry_nexthop_ipv6_ifindex_add(
-				re, &api_nh->gate.ipv6, ifindex,
-				api_nh->vrf_id);
+			nexthop = nexthop_from_ipv6_ifindex(&api_nh->gate.ipv6,
+							    ifindex,
+							    api_nh->vrf_id);
 
 			/* Special handling for IPv6 routes sourced from EVPN:
 			 * the nexthop and associated MAC need to be installed.
@@ -1527,8 +1530,7 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 			}
 			break;
 		case NEXTHOP_TYPE_BLACKHOLE:
-			nexthop = route_entry_nexthop_blackhole_add(
-				re, api_nh->bh_type);
+			nexthop = nexthop_from_blackhole(api_nh->bh_type);
 			break;
 		}
 
@@ -1537,10 +1539,11 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 				EC_ZEBRA_NEXTHOP_CREATION_FAILED,
 				"%s: Nexthops Specified: %d but we failed to properly create one",
 				__PRETTY_FUNCTION__, api.nexthop_num);
-			nexthop_group_delete(&re->ng);
+			nexthop_group_delete(&ng);
 			XFREE(MTYPE_RE, re);
 			return;
 		}
+
 		if (api_nh->onlink)
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_ONLINK);
 
@@ -1563,6 +1566,9 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 					   api_nh->label_num,
 					   &api_nh->labels[0]);
 		}
+
+		/* Add new nexthop to temporary list */
+		nexthop_group_add_sorted(ng, nexthop);
 	}
 
 	if (CHECK_FLAG(api.message, ZAPI_MESSAGE_DISTANCE))
@@ -1579,14 +1585,14 @@ static void zread_route_add(ZAPI_HANDLER_ARGS)
 		flog_warn(EC_ZEBRA_RX_SRCDEST_WRONG_AFI,
 			  "%s: Received SRC Prefix but afi is not v6",
 			  __PRETTY_FUNCTION__);
-		nexthop_group_delete(&re->ng);
+		nexthop_group_delete(&ng);
 		XFREE(MTYPE_RE, re);
 		return;
 	}
 	if (CHECK_FLAG(api.message, ZAPI_MESSAGE_SRCPFX))
 		src_p = &api.src_prefix;
 
-	ret = rib_add_multipath(afi, api.safi, &api.prefix, src_p, re);
+	ret = rib_add_multipath(afi, api.safi, &api.prefix, src_p, re, ng);
 
 	/* Stats */
 	switch (api.prefix.family) {

zebra/zebra_dplane.c
@@ -1507,7 +1507,8 @@ static int dplane_ctx_route_init(struct zebra_dplane_ctx *ctx,
 	ctx->u.rinfo.zd_safi = info->safi;
 
 	/* Copy nexthops; recursive info is included too */
-	copy_nexthops(&(ctx->u.rinfo.zd_ng.nexthop), re->ng->nexthop, NULL);
+	copy_nexthops(&(ctx->u.rinfo.zd_ng.nexthop),
+		      re->nhe->nhg->nexthop, NULL);
 
 	/* Ensure that the dplane's nexthops flags are clear. */
 	for (ALL_NEXTHOPS(ctx->u.rinfo.zd_ng, nexthop))
@@ -1746,7 +1747,7 @@ static int dplane_ctx_pw_init(struct zebra_dplane_ctx *ctx,
 
 			if (re)
 				copy_nexthops(&(ctx->u.pw.nhg.nexthop),
-					      re->ng->nexthop, NULL);
+					      re->nhe->nhg->nexthop, NULL);
 
 			route_unlock_node(rn);
 		}
@@ -1842,7 +1843,7 @@ dplane_route_update_internal(struct route_node *rn,
 			 * We'll need these to do per-nexthop deletes.
 			 */
 			copy_nexthops(&(ctx->u.rinfo.zd_old_ng.nexthop),
-				      old_re->ng->nexthop, NULL);
+				      old_re->nhe->nhg->nexthop, NULL);
 #endif	/* !HAVE_NETLINK */
 		}
 

commit 3d7660fa859fe5460ef335fda1fc0c60aff3f053

tests: Test ospf unnumbered behavior and ensure that it works
Recent commit: 0eb97b860dc94329cf9add9f8f3d3a2c7f539568

Broke onlink behavior and as a result ospf unnumbered failed
to work.  This commit adds a small test to create 2 ospf routers,
connect them through ospf unlinked behavior and then ensure
that the routes are installed into the kernel as expected.

Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

tests/topotests/ospf-topo2/r1/ospfd.conf
@@ -1,0 +1,11 @@
+!
+interface r1-eth1
+  ip ospf network point-to-point
+!
+router ospf
+  ospf router-id 10.0.255.1
+  redistribute kernel
+  redistribute connected
+  redistribute static
+  network 0.0.0.0/0 area 0
+!

tests/topotests/ospf-topo2/r1/zebra.conf
@@ -1,0 +1,7 @@
+!
+interface r1-eth0
+ ip address 10.0.1.1/32
+!
+interface r1-eth1
+ ip address 10.0.3.4/32
+!

tests/topotests/ospf-topo2/r2/ospfd.conf
@@ -1,0 +1,11 @@
+!
+interface r2-eth1
+  ip ospf network point-to-point
+!
+router ospf
+  ospf router-id 10.0.255.2
+  redistribute kernel
+  redistribute connected
+  redistribute static
+  network 0.0.0.0/0 area 0
+!

tests/topotests/ospf-topo2/r2/zebra.conf
@@ -1,0 +1,7 @@
+!
+interface r2-eth0
+ ip address 10.0.20.1/32
+!
+interface r2-eth1
+ ip address 10.0.3.2/32
+!

tests/topotests/ospf-topo2/test_ospf_topo2.py
@@ -1,0 +1,165 @@
+#!/usr/bin/env python
+
+#
+# test_ospf_topo2.py
+#
+# Copyright (c) 2019 by
+# Cumulus Networks, Inc
+# Donald Sharp
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted, provided
+# that the above copyright notice and this permission notice appear
+# in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND NETDEF DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL NETDEF BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THIS SOFTWARE.
+#
+
+"""
+test_ospf_topo2.py: Test the OSPF unnumbered.
+"""
+
+import os
+import re
+import sys
+from functools import partial
+import pytest
+import json
+
+# Save the Current Working Directory to find configuration files.
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(CWD, '../'))
+
+# pylint: disable=C0413
+# Import topogen and topotest helpers
+from lib import topotest
+from lib.topogen import Topogen, TopoRouter, get_topogen
+from lib.topolog import logger
+
+# Required to instantiate the topology builder class.
+from mininet.topo import Topo
+
+class OSPFTopo(Topo):
+    "Test topology builder"
+    def build(self, *_args, **_opts):
+        "Build function"
+        tgen = get_topogen(self)
+
+        # Create 4 routers
+        for routern in range(1, 3):
+            tgen.add_router('r{}'.format(routern))
+
+        # Create a empty network for router 1
+        switch = tgen.add_switch('s1')
+        switch.add_link(tgen.gears['r1'])
+
+        # Create a empty network for router 2
+        switch = tgen.add_switch('s2')
+        switch.add_link(tgen.gears['r2'])
+
+        # Interconect router 1, 2
+        switch = tgen.add_switch('s3')
+        switch.add_link(tgen.gears['r1'])
+        switch.add_link(tgen.gears['r2'])
+
+
+def setup_module(mod):
+    "Sets up the pytest environment"
+    tgen = Topogen(OSPFTopo, mod.__name__)
+    tgen.start_topology()
+
+    router_list = tgen.routers()
+    for rname, router in router_list.iteritems():
+        router.load_config(
+            TopoRouter.RD_ZEBRA,
+            os.path.join(CWD, '{}/zebra.conf'.format(rname))
+        )
+        router.load_config(
+            TopoRouter.RD_OSPF,
+            os.path.join(CWD, '{}/ospfd.conf'.format(rname))
+        )
+
+	# What is this?  OSPF Unnumbered depends on the rp_filter
+	# being set appropriately( HA! )
+	# Effectively we are putting different /32's on the interface
+	# the multicast packet delivery is somewhat controlled by
+	# the rp_filter.  Setting it to '0' allows the OS to pass
+	# up the mcast packet not destined for the local routers
+	# network.
+	topotest.set_sysctl(tgen.net['r1'],
+			    'net.ipv4.conf.r1-eth1.rp_filter', 0)
+        topotest.set_sysctl(tgen.net['r1'],
+                            'net.ipv4.conf.all.rp_filter', 0)
+	topotest.set_sysctl(tgen.net['r2'],
+			    'net.ipv4.conf.r2-eth1.rp_filter', 0)
+        topotest.set_sysctl(tgen.net['r2'],
+                            'net.ipv4.conf.all.rp_filter', 0)
+
+    # Initialize all routers.
+    tgen.start_router()
+    #tgen.mininet_cli()
+
+def teardown_module(mod):
+    "Teardown the pytest environment"
+    tgen = get_topogen()
+    tgen.stop_topology()
+
+
+def test_ospf_convergence():
+    "Test OSPF daemon convergence and that we have received the ospf routes"
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip('skipped because of router(s) failure')
+
+    for router, rnode in tgen.routers().iteritems():
+        logger.info('Waiting for router "%s" convergence', router)
+
+        json_file = '{}/{}/ospf-route.json'.format(CWD, router)
+        expected = json.loads(open(json_file).read())
+
+        test_func = partial(topotest.router_json_cmp,
+                            rnode, 'show ip ospf route json', expected)
+        _, result = topotest.run_and_expect(test_func, None, count=160, wait=0.5)
+        assertmsg = '"{}" JSON output mismatches'.format(router)
+        assert result is None, assertmsg
+    #tgen.mininet_cli()
+
+def test_ospf_kernel_route():
+    "Test OSPF kernel route installation and we have the onlink success"
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip('skipped because of router(s) failure')
+
+    rlist = tgen.routers().values()
+    for router in rlist:
+        logger.info('Checking OSPF IPv4 kernel routes in "%s"', router.name)
+
+        json_file = '{}/{}/v4_route.json'.format(CWD, router.name)
+        expected = json.loads(open(json_file).read())
+
+        test_func = partial(topotest.router_json_cmp,
+                            router, 'show ip route json', expected)
+        _, result = topotest.run_and_expect(test_func, None, count=10, wait=.5)
+        assertmsg = '"{}" JSON output mistmatches'.format(router)
+        assert result is None, assertmsg
+    #tgen.mininet_cli()
+
+
+def test_memory_leak():
+    "Run the memory leak test and report results."
+    tgen = get_topogen()
+    if not tgen.is_memleak_enabled():
+        pytest.skip('Memory leak test/report is disabled')
+
+    tgen.report_memory_leaks()
+
+if __name__ == '__main__':
+    args = ["-s"] + sys.argv[1:]
+    sys.exit(pytest.main(args))