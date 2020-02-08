f2ebe24292cbb6b914a492c7e6ccf0c369d5f06c
Lou Berger <lberger@labn.net>
2020-02-08 10:36:49 -0500

    topotest: bgp_l3vpn_to_bgp_vrf - report unexpected BGP notifications
    Signed-off-by: Lou Berger <lberger@labn.net>

--- tests/topotests/bgp_l3vpn_to_bgp_vrf/test_bgp_l3vpn_to_bgp_vrf.py
+++ tests/topotests/bgp_l3vpn_to_bgp_vrf/test_bgp_l3vpn_to_bgp_vrf.py
@@ -47,6 +47,15 @@ def test_adjacencies():
     #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True)'
     ltemplateTest('scripts/adjacencies.py', False, CliOnFail, CheckFunc)
 
+def test_notification_check():
+    CliOnFail = None
+    # For debugging, uncomment the next line
+    #CliOnFail = 'tgen.mininet_cli'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
+    #uncomment next line to start cli *before* script is run
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
+    ltemplateTest('scripts/notification_check.py', False, CliOnFail, CheckFunc)
+
 def SKIP_test_add_routes():
     CliOnFail = None
     # For debugging, uncomment the next line
@@ -75,6 +84,15 @@ def test_check_linux_mpls():
     #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
     ltemplateTest('scripts/check_linux_mpls.py', False, CliOnFail, CheckFunc)
 
+def test_notification_check():
+    CliOnFail = None
+    # For debugging, uncomment the next line
+    #CliOnFail = 'tgen.mininet_cli'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
+    #uncomment next line to start cli *before* script is run
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
+    ltemplateTest('scripts/notification_check.py', False, CliOnFail, CheckFunc)
+
 def test_check_scale_up():
     CliOnFail = None
     # For debugging, uncomment the next line
@@ -84,6 +102,15 @@ def test_check_scale_up():
     #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
     ltemplateTest('scripts/scale_up.py', False, CliOnFail, CheckFunc)
 
+def test_notification_check():
+    CliOnFail = None
+    # For debugging, uncomment the next line
+    #CliOnFail = 'tgen.mininet_cli'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
+    #uncomment next line to start cli *before* script is run
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
+    ltemplateTest('scripts/notification_check.py', False, CliOnFail, CheckFunc)
+
 def test_check_scale_down():
     CliOnFail = None
     # For debugging, uncomment the next line
@@ -92,6 +119,15 @@ def test_check_scale_down():
     #uncomment next line to start cli *before* script is run
     #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
     ltemplateTest('scripts/scale_down.py', False, CliOnFail, CheckFunc)
+
+def test_notification_check():
+    CliOnFail = None
+    # For debugging, uncomment the next line
+    #CliOnFail = 'tgen.mininet_cli'
+    CheckFunc = 'ltemplateVersionCheck(\'4.1\', iproute2=\'4.9\')'
+    #uncomment next line to start cli *before* script is run
+    #CheckFunc = 'ltemplateVersionCheck(\'4.1\', cli=True, iproute2=\'4.9\')'
+    ltemplateTest('scripts/notification_check.py', False, CliOnFail, CheckFunc)
 
 def SKIP_test_cleanup_all():
     CliOnFail = None
