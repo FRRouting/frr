61d4a7557df4a79c47af888fe8aa99c86026842f
Stephen Worley <sworley@cumulusnetworks.com>
2020-02-11 15:50:29 -0500

    zebra: add for_each macros for top level NHEs/nexthops
    Add some for_each macros for iterating over the top level
    nexthops in an NHE. This means we do not iterate over
    the recursively resolved ones.
    
    It is expected that you are passing in a group (not a singleton).
    
    This is functionally simialr to this:
    
    for (nexthop = *; nexthop; nexthop = nexthop->next) {}
    
    Ex)
    	1:
    		-> 2:
    			-> 3
    		-> 4
    		-> 5:
    			-> 6
    		-> 7
    
    	for (2,4,5,7)
    
    Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

--- zebra/zebra_nhg.h
+++ zebra/zebra_nhg.h
@@ -110,6 +110,76 @@ struct nhg_hash_entry {
 
 #define zebra_nhg_nexthop(nhe) (nhe->nhg->nexthop)
 
+/* Macro for looping over top-level (non-recursive) NHEs
+ *
+ * EX)
+ * 	1:
+ * 		-> 2:
+ * 			-> 3
+ * 		-> 4
+ * 		-> 5:
+ * 			-> 6
+ * 		-> 7
+ *
+ * 	for (2,4,5,7)
+ *
+ * It will also handle the case of being a passed a singleton NHE (i.e. not a
+ * real group). If so, it just loops once over that singleton NHE.
+ *
+ * Ex)
+ * 	1: (recursive singleton)
+ * 		-> 2
+ *
+ * 	for (1)
+ *
+ *
+ * Note Well: These are non-safe macros (do not remove from list while
+ * iterating)
+ *
+ *
+ * Macro Explaination:
+ *
+ * init:
+ * 	- The first ternary is used to force iter as NULL since we use it in the
+ * loop condtional. It will always evaluate down the first branch since we are
+ * setting it to NULL.
+ * 	- Set rb_node_dep to the first depend of the tree.
+ *
+ * loop condition:
+ * 	- if something is set in rb_node_dep, it found an item in the tree and
+ * so we use that to set the iter.
+ *  	- otherwise, we may have been passed a singleton NHE. If thats the case,
+ * check to make sure we haven't iterated already (!iter). If we haven't check
+ * to see if the parent is indeed a group. If its not, then we will just be
+ * iterating once on the Parent's NHE so set iter to that.
+ *
+ * iteration:
+ * 	- get the next item in the tree. rb_node_dep may be NULL if we were
+ * passed a singleton NHE into this macro, so use the safe version.
+ *
+ */
+#define zebra_nhg_each(parent, iter)                                           \
+	for (struct nhg_connected *rb_node_dep =                               \
+		     (!(iter = NULL)                                           \
+			      ? nhg_connected_tree_first(&parent->nhg_depends) \
+			      : NULL);                                         \
+	     (rb_node_dep && (iter = rb_node_dep->nhe)                         \
+	      || (!iter && !zebra_nhg_is_group(parent) && (iter = parent)));   \
+	     rb_node_dep = nhg_connected_tree_next_safe(&parent->nhg_depends,  \
+							rb_node_dep))
+
+/* Same as above but loops over top-level nexthops */
+#define zebra_nhg_each_nexthop(parent, iter)                                   \
+	for (struct nhg_connected *rb_node_dep =                               \
+		     (!(iter = NULL)                                           \
+			      ? nhg_connected_tree_first(&parent->nhg_depends) \
+			      : NULL);                                         \
+	     (rb_node_dep && (iter = zebra_nhg_nexthop(rb_node_dep->nhe)))     \
+	     || (!iter && !zebra_nhg_is_group(parent)                          \
+		 && (iter = zebra_nhg_nexthop(parent)));                       \
+	     rb_node_dep = nhg_connected_tree_next_safe(&parent->nhg_depends,  \
+							rb_node_dep))
+
 enum nhg_ctx_op_e {
 	NHG_CTX_OP_NONE = 0,
 	NHG_CTX_OP_NEW,
