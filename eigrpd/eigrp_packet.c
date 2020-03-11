1dcdfaa4f0f4d2384eb3ddb332fc66ccfd43d6f3
Ruben Kerkhof <ruben@rubenkerkhof.com>
2020-03-11 13:31:13 +0100

    eigrpd: stop checking for ancient OpenBSD
    Signed-off-by: Ruben Kerkhof <ruben@rubenkerkhof.com>

--- eigrpd/eigrp_packet.c
+++ eigrpd/eigrp_packet.c
@@ -749,7 +749,7 @@ static struct stream *eigrp_recv_packet(struct eigrp *eigrp,
 
 	ip_len = iph->ip_len;
 
-#if !defined(GNU_LINUX) && (OpenBSD < 200311) && (__FreeBSD_version < 1000000)
+#if defined(__FreeBSD__) && (__FreeBSD_version < 1000000)
 	/*
 	 * Kernel network code touches incoming IP header parameters,
 	 * before protocol specific processing.
