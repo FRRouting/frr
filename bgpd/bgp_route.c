d69a76ac1a40ef13c707d814c8f3b95fe7b84627
vivek <vivek@cumulusnetworks.com>
2020-03-18 15:16:23 -0700

    bgpd: Reverse route-map check for consistency
    Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -2697,13 +2697,13 @@ static void bgp_process_main_one(struct bgp *bgp, struct bgp_node *rn,
 				ret = route_map_apply(
 					bgp->adv_cmd_rmap[afi][safi].map,
 					&rn->p, RMAP_BGP, new_select);
-				if (ret == RMAP_PERMITMATCH)
+				if (ret == RMAP_DENYMATCH)
+					bgp_evpn_withdraw_type5_route(
+						bgp, &rn->p, afi, safi);
+				else
 					bgp_evpn_advertise_type5_route(
 						bgp, &rn->p, new_select->attr,
 						afi, safi);
-				else
-					bgp_evpn_withdraw_type5_route(
-						bgp, &rn->p, afi, safi);
 			} else {
 				bgp_evpn_advertise_type5_route(bgp,
 							       &rn->p,
