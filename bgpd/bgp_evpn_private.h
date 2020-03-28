6845b9be827772eea931127d69db4ae9c89c4474
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-03-28 10:12:04 -0700

    bgpd: Type-2/MAC-IP SYNC route handling
    SYNC routes are paths rxed from a local-ES peer. These routes result in
    the installation of local dataplane entries i.e. with access port as
    destination (vs. the remote-VTEP destination that results in the packet
    being sent via the VxLAN overlay).
    
    If a SYNC path is selected as the best path it is always turned around
    into a local path which immediately lowers the status of the SYNC path
    to non-best. However we need to keep track of the highest MM seq-number
    and peer activity to continue advertising the local path. In order to
    do that we need information from the "second-best" SYNC path to be
    bubbled up to the local best path. This "SYNC" info is then consolidated
    and sent to zebra which is responsible for the MM handling and local
    path management.
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- bgpd/bgp_evpn_private.h
+++ bgpd/bgp_evpn_private.h
@@ -343,13 +343,15 @@ static inline void encode_mac_mobility_extcomm(int static_mac, uint32_t seq,
 }
 
 static inline void encode_na_flag_extcomm(struct ecommunity_val *eval,
-					  uint8_t na_flag)
+					  uint8_t na_flag, bool proxy)
 {
 	memset(eval, 0, sizeof(*eval));
 	eval->val[0] = ECOMMUNITY_ENCODE_EVPN;
 	eval->val[1] = ECOMMUNITY_EVPN_SUBTYPE_ND;
 	if (na_flag)
 		eval->val[2] |= ECOMMUNITY_EVPN_SUBTYPE_ND_ROUTER_FLAG;
+	if (proxy)
+		eval->val[2] |= ECOMMUNITY_EVPN_SUBTYPE_PROXY_FLAG;
 }
 
 static inline void ip_prefix_from_type5_prefix(const struct prefix_evpn *evp,
@@ -536,6 +538,14 @@ static inline bool bgp_evpn_is_svi_macip_enabled(struct bgpevpn *vpn)
 
 	return (bgp_evpn->evpn_info->advertise_svi_macip ||
 		vpn->advertise_svi_macip);
+}
+
+static inline bool bgp_evpn_is_path_local(struct bgp *bgp,
+		struct bgp_path_info *pi)
+{
+	return (pi->peer == bgp->peer_self
+			&& pi->type == ZEBRA_ROUTE_BGP
+			&& pi->sub_type == BGP_ROUTE_STATIC);
 }
 
 extern struct zclient *zclient;
