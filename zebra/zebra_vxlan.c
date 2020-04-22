c774f1a4d38eda5346e7f758ba96d3b7a7ed582b
Pat Ruddy <pat@voltanet.io>
2020-04-22 16:30:52 +0100

    [refactor 7/16] zebra: extract gateway mac add from zebra_vxlan.c
    extract mac_gateway add code from zevi_gw_macip_add and move it to
    a new generic function zebra_evpn_mac_gw_macip_add in zebra_evpn_mac.c
    
    Signed-off-by: Pat Ruddy <pat@voltanet.io>

--- zebra/zebra_vxlan.c
+++ zebra/zebra_vxlan.c
@@ -2250,25 +2250,10 @@ static int zevpn_gw_macip_add(struct interface *ifp, zebra_evpn_t *zevpn,
 
 	vxl = &zif->l2info.vxl;
 
-	mac = zebra_evpn_mac_lookup(zevpn, macaddr);
-	if (!mac) {
-		mac = zebra_evpn_mac_add(zevpn, macaddr);
-		if (!mac) {
-			flog_err(EC_ZEBRA_MAC_ADD_FAILED,
-				 "Failed to add MAC %s intf %s(%u) VID %u",
-				 prefix_mac2str(macaddr, buf, sizeof(buf)),
-				 ifp->name, ifp->ifindex, vxl->access_vlan);
-			return -1;
-		}
-	}
-
-	/* Set "local" forwarding info. */
-	SET_FLAG(mac->flags, ZEBRA_MAC_LOCAL);
-	SET_FLAG(mac->flags, ZEBRA_MAC_AUTO);
-	SET_FLAG(mac->flags, ZEBRA_MAC_DEF_GW);
-	memset(&mac->fwd_info, 0, sizeof(mac->fwd_info));
-	mac->fwd_info.local.ifindex = ifp->ifindex;
-	mac->fwd_info.local.vid = vxl->access_vlan;
+	if (zebra_evpn_mac_gw_macip_add(ifp, zevpn, ip, &mac, macaddr,
+					vxl->access_vlan)
+	    != 0)
+		return -1;
 
 	n = zevpn_neigh_lookup(zevpn, ip);
 	if (!n) {
