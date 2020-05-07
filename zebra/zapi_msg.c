4867bfe55b5e4702ecf89636dd38ea85cf5061d4
Philippe Guibert <philippe.guibert@6wind.com>
2020-05-07 11:21:16 +0200

    zebra: GRE_UPDATE message incomplete
    when gre information could not be retrieved because GRE interface has
    been deleted, a GRE_UPDATE message may be sent to NHRP. In that case,
    the gre values are reset. There was a missing tunnel destination value,
    which has been omitted.
    
    Old-PR: 66542
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Emmanuel Vize <emmanuel.vize@6wind.com>
    (cherry picked from commit d3b2c507cb907e2cd03b39742e5cf4f540fcdf35)
    
    Conflicts:
    	zebra/zapi_msg.c
    
    PR=76612
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -3365,6 +3365,7 @@ static inline void zebra_gre_get(ZAPI_HANDLER_ARGS)
 		stream_putl(s, IFINDEX_INTERNAL);
 		stream_putl(s, VRF_UNKNOWN);
 		stream_putl(s, 0);
+		stream_putl(s, 0);
 	}
 	/* Write packet size. */
 	stream_putw_at(s, 0, stream_get_endp(s));
