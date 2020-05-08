990f29a17da20a858b574d075ea5f92eb89fcd2f
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-05-08 16:53:25 -0700

    zebra: dplane APIs for programming evpn-mh access port attributes
    This includes -
    1. non-DF block filter
    2. List of es-peers that need to be blocked per-access port (for
    split horizon filtering)
    3. Backup nexthop group to failover local-es via the VxLAN overlay
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -2563,6 +2563,7 @@ void zebra_nhg_dplane_result(struct zebra_dplane_ctx *ctx)
 	case DPLANE_OP_NEIGH_DELETE:
 	case DPLANE_OP_VTEP_ADD:
 	case DPLANE_OP_VTEP_DELETE:
+	case DPLANE_OP_BR_PORT_UPDATE:
 	case DPLANE_OP_NONE:
 		break;
 	}
