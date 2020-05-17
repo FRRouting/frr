acffa256ba4f0726215db36f8ef553361f1ada8b
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-05-17 10:59:30 -0700

    zebra: add json output for zebra ES, ES-EVI and access vlan dumps
    1. ES
    root@torm-11:mgmt:~# vtysh -c "show evpn es 03:44:38:39:ff:ff:01:00:00:01 json"
    |python -m json.tool
    {
        "accessPort": "hostbond1",
        "dfPreference": 50000,
        "esi": "03:44:38:39:ff:ff:01:00:00:01",
        "flags": [
            "local",
            "remote",
            "readyForBgp",
            "bridgePort",
            "operUp",
            "nexthopGroupActive"
        ],
        "macCount": 10,
        "nexthopGroup": 536870913,
        "vniCount": 10,
        "vteps": [
            {
                "dfAlgorithm": "preference",
                "dfPreference": 32767,
                "nexthopId": 268435460,
                "vtep": "27.0.0.16"
            },
            {
                "dfAlgorithm": "preference",
                "dfPreference": 32767,
                "nexthopId": 268435463,
                "vtep": "27.0.0.17"
            }
        ]
    }
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    
    2. ES-EVI -
    root@torm-11:mgmt:~# vtysh -c "show evpn es-evi vni 1001 detail json" |python -m json.tool
    [
        {
            "esi": "03:44:38:39:ff:ff:01:00:00:01",
            "flags": [
                "local",
                "readyForBgp"
            ],
            "vni": 1001
        },
        {
            "esi": "03:44:38:39:ff:ff:01:00:00:02",
            "flags": [
                "local",
                "readyForBgp"
            ],
            "vni": 1001
        },
    ]
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    
    3. access-vlan
    root@torm-11:mgmt:~# vtysh -c "show evpn access-vlan 1001 json" |python -m json.
    tool
    {
        "memberIfCount": 4,
        "members": [
            {
                "ifName": "hostbond4"
            },
            {
                "ifName": "hostbond1"
            },
            {
                "ifName": "hostbond2"
            },
            {
                "ifName": "hostbond3"
            }
        ],
        "vlan": 1001,
        "vni": 1001,
        "vxlanIf": "vx-1001"
    }
    root@torm-11:mgmt:~#
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -2596,7 +2596,7 @@ DEFUN (show_evpn_global,
 
 DEFPY(show_evpn_es,
       show_evpn_es_cmd,
-      "show evpn es [NAME$esi_str] [json$json] [detail$detail]",
+      "show evpn es [NAME$esi_str|detail$detail] [json$json]",
       SHOW_STR
       "EVPN\n"
       "Ethernet Segment\n"
@@ -2625,14 +2625,14 @@ DEFPY(show_evpn_es,
 
 DEFPY(show_evpn_es_evi,
       show_evpn_es_evi_cmd,
-      "show evpn es-evi [vni (1-16777215)$vni] [json$json] [detail$detail]",
+      "show evpn es-evi [vni (1-16777215)$vni] [detail$detail] [json$json]",
       SHOW_STR
       "EVPN\n"
       "Ethernet Segment per EVI\n"
       "VxLAN Network Identifier\n"
       "VNI\n"
-      JSON_STR
-      "Detailed information\n")
+      "Detailed information\n"
+      JSON_STR)
 {
 	bool uj = !!json;
 	bool ud = !!detail;
@@ -2647,13 +2647,13 @@ DEFPY(show_evpn_es_evi,
 
 DEFPY(show_evpn_access_vlan,
       show_evpn_access_vlan_cmd,
-      "show evpn access-vlan [(1-4094)$vid] [json$json] [detail$detail]",
+      "show evpn access-vlan [(1-4094)$vid | detail$detail] [json$json]",
       SHOW_STR
       "EVPN\n"
       "Access VLANs\n"
       "VLAN ID\n"
-      JSON_STR
-      "Detailed information\n")
+      "Detailed information\n"
+      JSON_STR)
 {
 	bool uj = !!json;
 
