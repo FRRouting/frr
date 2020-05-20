38511fe33504d92404f9967af08cf102b44bc7de
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-05-20 14:56:36 -0700

    zebra: fixup pending items
    Cleanup some of the XXX added during development of MH.
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/zebra_evpn_mh.c
+++ zebra/zebra_evpn_mh.c
@@ -1637,7 +1637,6 @@ static int zebra_evpn_es_send_del_to_client(struct zebra_evpn_es *es)
 	return zserv_send_message(client, s);
 }
 
-/* XXX - call any time ZEBRA_EVPNES_LOCAL gets set or cleared */
 static void zebra_evpn_es_re_eval_send_to_client(struct zebra_evpn_es *es,
 		bool es_evi_re_reval)
 {
@@ -2021,7 +2020,9 @@ static int zebra_evpn_remote_es_del(esi_t *esi, struct in_addr vtep_ip)
 
 	es = zebra_evpn_es_find(esi);
 	if (!es) {
-		/* XXX - error log */
+		zlog_warn("remote es %s vtep %s del failed, es missing",
+				esi_to_str(esi, buf, sizeof(buf)),
+				inet_ntoa(vtep_ip));
 		return -1;
 	}
 
@@ -2065,7 +2066,9 @@ static int zebra_evpn_remote_es_add(esi_t *esi, struct in_addr vtep_ip,
 	if (!es) {
 		es = zebra_evpn_es_new(esi);
 		if (!es) {
-			/* XXX - error log */
+			zlog_warn("remote es %s vtep %s add failed, es missing",
+					esi_to_str(esi, buf, sizeof(buf)),
+					inet_ntoa(vtep_ip));
 			return -1;
 		}
 	}
