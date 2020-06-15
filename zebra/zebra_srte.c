9b0eec8c429de2ab11266b83a514fb5f53d40b16
GalaxyGorilla <sascha@netdef.org>
2020-06-15 11:12:45 +0000

    zebra: use stdint.h for BSD types
    Signed-off-by: GalaxyGorilla <sascha@netdef.org>

--- zebra/zebra_srte.c
+++ zebra/zebra_srte.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <sys/types.h>
+#include <stdint.h>
 
 #include "lib/zclient.h"
 #include "lib/lib_errors.h"
