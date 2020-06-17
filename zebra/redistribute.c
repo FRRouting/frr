daad19071cc40e4ef6913e674cc41fa35121c399
Philippe Guibert <philippe.guibert@6wind.com>
2020-06-17 14:11:35 +0200

    zebra: add redistribute table-direct support
    Redistributing routes from a specific routing table to a particular routing
    protocol necessitates copying route entries to the main routing table using the
    "ip import-table" command. Once copied, these routes are assigned a distinct
    "table" route type, which the "redistribute table" command of the routing
    protocol then picks up.
    
    For illustration, here is a configuration that showcases the use of
    "import-table" and "redistribute":
    
    > # show running-config
    > [..]
    > ip route 172.31.0.10/32 172.31.1.10 table 100
    > router bgp 65500
    >  address-family ipv4 unicast
    >   redistribute table 100
    >  exit-address-family
    > exit
    > ip import-table 100
    >
    > # show ip route vrf default
    > [..]
    > T[100]>* 172.31.0.10/32 [15/0] via 172.31.1.10, r2-eth1, weight 1, 00:00:05
    
    However, this method has inherent constraints:
    
    - The 'import-table' parameter only handles route table id up to 252. The
    253/254/255 table ids are reserved in the linux system, and adding other table
    IDs above 255 leads to a design issue, where the size of some tables is directly
    related to the maximum number of table ids to support.
    - Duplicated route entries might interfere with original default table routes,
    leading to potential conflicts. There is no guarantee that the zebra RIB will
    favor these duplicated entries during redistribution.
    - There are cases where the table ID can be checked independently of the default
    routing table, as seen in Linux where the "ip rule" command is able to divert
    traffic to that routing table. In that case, there is no need to duplicate route
    entries in the default routing table.
    
    To overcome these issues, a new redistribution type is proposed to redistribute
    route entries directly from a specified routing table, eliminating the need for
    an initial import into the default table.
    
    Add a 'ZEBRA_ROUTE_TABLE_DIRECT' type to the 'REDISTRIBUTE' ZAPI messages. It
    allows sending routes from a given non default table ID from zebra to a routing
    daemon. The destination routing protocol table must be the default table.
    The redistributed route inherit from the default distance value of 14: this is
    the distance value reserved for routes redistributed via ROUTE_TABLE_DIRECT.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/redistribute.c
+++ zebra/redistribute.c
@@ -77,9 +77,8 @@ static void zebra_redistribute_default(struct zserv *client, vrf_id_t vrf_id)
 
 		RNODE_FOREACH_RE (rn, newre) {
 			if (CHECK_FLAG(newre->flags, ZEBRA_FLAG_SELECTED))
-				zsend_redistribute_route(
-					ZEBRA_REDISTRIBUTE_ROUTE_ADD, client,
-					rn, newre);
+				zsend_redistribute_route(ZEBRA_REDISTRIBUTE_ROUTE_ADD,
+							 client, rn, newre, false);
 		}
 
 		route_unlock_node(rn);
@@ -88,14 +87,26 @@ static void zebra_redistribute_default(struct zserv *client, vrf_id_t vrf_id)
 
 /* Redistribute routes. */
 static void zebra_redistribute(struct zserv *client, int type,
-			       unsigned short instance, vrf_id_t vrf_id,
+			       unsigned short instance, struct zebra_vrf *zvrf,
 			       int afi)
 {
 	struct route_entry *newre;
 	struct route_table *table;
 	struct route_node *rn;
+	bool is_table_direct = false;
+	vrf_id_t vrf_id = zvrf_id(zvrf);
 
-	table = zebra_vrf_table(afi, SAFI_UNICAST, vrf_id);
+	if (type == ZEBRA_ROUTE_TABLE_DIRECT) {
+		if (vrf_id == VRF_DEFAULT) {
+			table = zebra_router_find_table(zvrf, instance, afi,
+							SAFI_UNICAST);
+			type = ZEBRA_ROUTE_ALL;
+			is_table_direct = true;
+		} else
+			return;
+	} else
+		table = zebra_vrf_table(afi, SAFI_UNICAST, vrf_id);
+
 	if (!table)
 		return;
 
@@ -124,9 +135,28 @@ static void zebra_redistribute(struct zserv *client, int type,
 			if (!zebra_check_addr(&rn->p))
 				continue;
 
+			if (type == ZEBRA_ROUTE_ADD && is_table_direct &&
+			    newre->vrf_id != VRF_DEFAULT)
+				continue;
+
 			zsend_redistribute_route(ZEBRA_REDISTRIBUTE_ROUTE_ADD,
-						 client, rn, newre);
+						 client, rn, newre, is_table_direct);
 		}
+}
+
+/*
+ * Function to return a valid table id value if table-direct is used
+ * return 0 otherwise
+ * This function can be called only if zebra_redistribute_check returns TRUE
+ */
+static bool zebra_redistribute_is_table_direct(const struct route_entry *re)
+{
+	struct zebra_vrf *zvrf;
+
+	zvrf = zebra_vrf_lookup_by_id(re->vrf_id);
+	if (re->vrf_id == VRF_DEFAULT && zvrf->table_id != re->table)
+		return true;
+	return false;
 }
 
 /*
@@ -146,8 +176,19 @@ static bool zebra_redistribute_check(const struct route_node *rn,
 
 	afi = family2afi(rn->p.family);
 	zvrf = zebra_vrf_lookup_by_id(re->vrf_id);
-	if (re->vrf_id == VRF_DEFAULT && zvrf->table_id != re->table)
+	if (re->vrf_id == VRF_DEFAULT && zvrf->table_id != re->table) {
+		if (re->table &&
+		    redist_check_instance(&client->mi_redist
+						   [afi][ZEBRA_ROUTE_TABLE_DIRECT],
+					  re->table)) {
+			/* table-direct redistribution only for route entries which
+			 * are on the default vrf, and that have table id different
+			 * from the default table.
+			 */
+			return true;
+		}
 		return false;
+	}
 
 	/* If default route and redistributed */
 	if (is_default_prefix(&rn->p) &&
@@ -185,6 +226,7 @@ void redistribute_update(const struct route_node *rn,
 {
 	struct listnode *node, *nnode;
 	struct zserv *client;
+	bool is_table_direct;
 
 	if (IS_ZEBRA_DEBUG_RIB)
 		zlog_debug(
@@ -210,11 +252,16 @@ void redistribute_update(const struct route_node *rn,
 					re->vrf_id, re->table, re->type,
 					re->distance, re->metric);
 			}
+			is_table_direct = zebra_redistribute_is_table_direct(re);
 			zsend_redistribute_route(ZEBRA_REDISTRIBUTE_ROUTE_ADD,
-						 client, rn, re);
-		} else if (zebra_redistribute_check(rn, prev_re, client))
+						 client, rn, re,
+						 is_table_direct);
+		} else if (zebra_redistribute_check(rn, prev_re, client)) {
+			is_table_direct = zebra_redistribute_is_table_direct(prev_re);
 			zsend_redistribute_route(ZEBRA_REDISTRIBUTE_ROUTE_DEL,
-						 client, rn, prev_re);
+						 client, rn, prev_re,
+						 is_table_direct);
+		}
 	}
 }
 
@@ -233,6 +280,7 @@ void redistribute_delete(const struct route_node *rn,
 	struct listnode *node, *nnode;
 	struct zserv *client;
 	vrf_id_t vrfid;
+	bool is_table_direct;
 
 	if (old_re)
 		vrfid = old_re->vrf_id;
@@ -285,9 +333,12 @@ void redistribute_delete(const struct route_node *rn,
 			continue;
 
 		/* Send a delete for the 'old' re to any subscribed client. */
-		if (zebra_redistribute_check(rn, old_re, client))
+		if (zebra_redistribute_check(rn, old_re, client)) {
+			is_table_direct = zebra_redistribute_is_table_direct(old_re);
 			zsend_redistribute_route(ZEBRA_REDISTRIBUTE_ROUTE_DEL,
-						 client, rn, old_re);
+						 client, rn, old_re,
+						 is_table_direct);
+		}
 	}
 }
 
@@ -326,8 +377,7 @@ void zebra_redistribute_add(ZAPI_HANDLER_ARGS)
 					   instance)) {
 			redist_add_instance(&client->mi_redist[afi][type],
 					    instance);
-			zebra_redistribute(client, type, instance,
-					   zvrf_id(zvrf), afi);
+			zebra_redistribute(client, type, instance, zvrf, afi);
 		}
 	} else {
 		if (!vrf_bitmap_check(&client->redist[afi][type],
@@ -339,7 +389,7 @@ void zebra_redistribute_add(ZAPI_HANDLER_ARGS)
 					zvrf_id(zvrf));
 			vrf_bitmap_set(&client->redist[afi][type],
 				       zvrf_id(zvrf));
-			zebra_redistribute(client, type, 0, zvrf_id(zvrf), afi);
+			zebra_redistribute(client, type, 0, zvrf, afi);
 		}
 	}
 
