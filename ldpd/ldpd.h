c734e0dd39930138d964c44d5a5d25628c60d491
Karen Schoener <karen@volta.io>
2020-09-23 17:38:23 -0400

    ldpd: Add support for read-only snmp mib objects.
    Add support for read-only snmp mib objects as described in RFC 3815.
    
    Signed-off-by: Lynne Morrison <lynne@voltanet.io>
    Signed-off-by: Karen Schoener <karen@voltanet.io>

--- ldpd/ldpd.h
+++ ldpd/ldpd.h
@@ -161,6 +161,7 @@ enum imsg_type {
 	IMSG_RLFA_REG,
 	IMSG_RLFA_UNREG_ALL,
 	IMSG_RLFA_LABELS,
+	IMSG_AGENTX_ENABLED,
 };
 
 struct ldpd_init {
@@ -434,6 +435,25 @@ struct ldp_stats {
 	uint32_t		 labelrel_rcvd;
 	uint32_t		 labelabreq_sent;
 	uint32_t		 labelabreq_rcvd;
+	uint32_t		 unknown_tlv;
+	uint32_t		 unknown_msg;
+
+};
+
+struct ldp_entity_stats {
+	uint32_t		 session_attempts;
+	uint32_t		 session_rejects_hello;
+	uint32_t		 session_rejects_ad;
+	uint32_t		 session_rejects_max_pdu;
+	uint32_t		 session_rejects_lr;
+	uint32_t		 bad_ldp_id;
+	uint32_t		 bad_pdu_len;
+	uint32_t		 bad_msg_len;
+	uint32_t		 bad_tlv_len;
+	uint32_t		 malformed_tlv;
+	uint32_t		 keepalive_timer_exp;
+	uint32_t		 shutdown_rcv_notify;
+	uint32_t		 shutdown_send_notify;
 };
 
 struct l2vpn_if {
@@ -562,6 +582,8 @@ struct ldpd_conf {
 	uint16_t		 trans_pref;
 	uint16_t		 wait_for_sync_interval;
 	int			 flags;
+	time_t			 config_change_time;
+	struct ldp_entity_stats  stats;
 	QOBJ_FIELDS
 };
 DECLARE_QOBJ_TYPE(ldpd_conf)
@@ -683,6 +705,8 @@ struct ctl_nbr {
 	int			 nbr_state;
 	struct ldp_stats	 stats;
 	int			 flags;
+	uint16_t		 max_pdu_len;
+	uint16_t		 hold_time_remaining;
 };
 
 struct ctl_rt {
@@ -890,5 +914,9 @@ int		 ldp_zebra_send_rlfa_labels(struct zapi_rlfa_response *
 	(IN6_IS_ADDR_MULTICAST(a) &&	\
 	(__IPV6_ADDR_MC_SCOPE(a) == __IPV6_ADDR_SCOPE_INTFACELOCAL))
 #endif
+
+DECLARE_HOOK(ldp_register_mib, (struct thread_master * tm), (tm))
+
+extern void ldp_agentx_enabled(void);
 
 #endif	/* _LDPD_H_ */
