26b6d034b0f3e0111b464b19c11026366f1e4192
Pat Ruddy <pat@voltanet.io>
2020-10-15 16:40:36 +0100

    vrrpd: Convert to using %pFX
    Signed-off-by: Pat Ruddy <pat@voltanet.io>

--- vrrpd/vrrp_zebra.c
+++ vrrpd/vrrp_zebra.c
@@ -56,8 +56,8 @@ static void vrrp_zebra_debug_if_dump_address(struct interface *ifp,
 	for (ALL_LIST_ELEMENTS_RO(ifp->connected, node, ifc)) {
 		struct prefix *p = ifc->address;
 
-		DEBUGD(&vrrp_dbg_zebra, "%s: interface %s address %s %s", func,
-		       ifp->name, inet_ntoa(p->u.prefix4),
+		DEBUGD(&vrrp_dbg_zebra, "%s: interface %s address %pFX %s",
+		       func, ifp->name, p,
 		       CHECK_FLAG(ifc->flags, ZEBRA_IFA_SECONDARY) ? "secondary"
 								   : "primary");
 	}
