5c3375847a20f94acabd5f46fe7a0b9b4914ccb5
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-10-20 09:26:51 -0700

    bgpd: fix crash in the MH cleanup handling
    The MH datastructures were being released before the paths that were
    referencing them. Fix is to do the MH cleanup last.
    
    The MH finish function has also been stripped down to only do a
    datastructure cleanup i.e. avoid sending route updates etc.
    
    Ticket: 31376
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- bgpd/bgp_main.c
+++ bgpd/bgp_main.c
@@ -60,6 +60,7 @@
 #include "bgpd/bgp_keepalives.h"
 #include "bgpd/bgp_network.h"
 #include "bgpd/bgp_errors.h"
+#include "bgpd/bgp_evpn_mh.h"
 
 #ifdef ENABLE_BGP_VNC
 #include "bgpd/rfapi/rfapi_backend.h"
@@ -204,6 +205,8 @@ static __attribute__((__noreturn__)) void bgp_exit(int status)
 		bgp_delete(bgp_evpn);
 	if (bgp_default)
 		bgp_delete(bgp_default);
+
+	bgp_evpn_mh_finish();
 
 	/* reverse bgp_dump_init */
 	bgp_dump_finish();
