b66ec22ca9b227c4b7f78f71918be7eff30fb552
Donald Sharp <sharpd@nvidia.com>
2020-10-29 06:59:42 -0400

    ospf6d: ifindex should not be a pointer for ospf6_sendmsg
    Let's cleanup the ospf6_sendmsg api and not pass in a pointer
    to the ifindex to use.  It's an integer.
    
    Also remove the assert(*ifindex);  We never use ifindex of 0
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- ospf6d/ospf6_network.h
+++ ospf6d/ospf6_network.h
@@ -29,7 +29,7 @@ extern void ospf6_serv_close(int *ospf6_sock);
 extern int ospf6_sso(ifindex_t ifindex, struct in6_addr *group, int option);
 
 extern int ospf6_sendmsg(struct in6_addr *src, struct in6_addr *dst,
-			 ifindex_t *ifindex, struct iovec *message,
+			 ifindex_t ifindex, struct iovec *message,
 			 int ospf6_sock);
 extern int ospf6_recvmsg(struct in6_addr *src, struct in6_addr *dst,
 			 ifindex_t *ifindex, struct iovec *message,
