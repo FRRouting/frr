c3eed7b479de3f1678c2e4bafc642c57ed7fd329
Andre Russ <russ@gmx.de>
2020-11-20 13:11:50 +0100

    fix python error object vs. value compare

--- tools/frr-reload.py
+++ tools/frr-reload.py
@@ -829,7 +829,7 @@ def check_for_exit_vrf(lines_to_add, lines_to_del):
                 add_exit_vrf = False
 
         if ctx_keys[0].startswith("vrf") and line:
-            if line is not "exit-vrf":
+            if line != "exit-vrf":
                 add_exit_vrf = True
                 prior_ctx_key = ctx_keys[0]
             else:
