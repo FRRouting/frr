09fe9033524b2506d6f75381e970151899be73d3
Emanuele Di Pascale <emanuele@voltanet.io>
2020-12-01 18:24:46 +0100

    zebra: fix show ip route vrf X summary
    The lookup for non default VRFs was always using a tableId; if not
    provided, we were defaulting to RT_TABLE_MAIN. This is fine for the
    default VRF but not for others. As a result, the command was silently
    failing for non-default VRFs unless we also specified the correct tableId.
    
    Fix this by only performing the lookup using the tableId if it is
    provided; else use zebra_vrf_table.
    
    Signed-off-by: Emanuele Di Pascale <emanuele@voltanet.io>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -1909,9 +1909,6 @@ DEFPY (show_route_summary,
 	struct route_table *table;
 	bool uj = use_json(argc, argv);
 
-	if (table_id == 0)
-		table_id = RT_TABLE_MAIN;
-
 	if (vrf_all) {
 		struct vrf *vrf;
 		struct zebra_vrf *zvrf;
@@ -1920,8 +1917,13 @@ DEFPY (show_route_summary,
 			if ((zvrf = vrf->info) == NULL)
 				continue;
 
-			table = zebra_vrf_lookup_table_with_table_id(
-				afi, SAFI_UNICAST, zvrf->vrf->vrf_id, table_id);
+			if (table_id == 0)
+				table = zebra_vrf_table(afi, SAFI_UNICAST,
+							zvrf->vrf->vrf_id);
+			else
+				table = zebra_vrf_lookup_table_with_table_id(
+					afi, SAFI_UNICAST, zvrf->vrf->vrf_id,
+					table_id);
 
 			if (!table)
 				continue;
@@ -1938,8 +1940,11 @@ DEFPY (show_route_summary,
 		if (vrf_name)
 			VRF_GET_ID(vrf_id, vrf_name, false);
 
-		table = zebra_vrf_lookup_table_with_table_id(afi, SAFI_UNICAST,
-							     vrf_id, table_id);
+		if (table_id == 0)
+			table = zebra_vrf_table(afi, SAFI_UNICAST, vrf_id);
+		else
+			table = zebra_vrf_lookup_table_with_table_id(
+				afi, SAFI_UNICAST, vrf_id, table_id);
 		if (!table)
 			return CMD_SUCCESS;
 
