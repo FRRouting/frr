9fd80543fe9ad6011b5e186cf09990a37fd42c85
Donald Sharp <sharpd@nvidia.com>
2020-12-17 13:19:52 -0500

    tests: unicode to frr_unicode
    Let's standardize on the internal to frr unicode function.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- tests/topotests/ospf_basic_functionality/test_ospf_authentication.py
+++ tests/topotests/ospf_basic_functionality/test_ospf_authentication.py
@@ -29,6 +29,7 @@ import pytest
 from time import sleep
 from copy import deepcopy
 import json
+from lib.topotest import frr_unicode
 
 # Save the Current Working Directory to find configuration files.
 CWD = os.path.dirname(os.path.realpath(__file__))
@@ -318,7 +319,7 @@ def test_ospf_authentication_simple_pass_tc28_p1(request):
     topo_modify_change_ip = deepcopy(topo)
     intf_ip = topo_modify_change_ip["routers"]["r1"]["links"]["r2"]["ipv4"]
     topo_modify_change_ip["routers"]["r1"]["links"]["r2"]["ipv4"] = str(
-        IPv4Address(unicode(intf_ip.split("/")[0])) + 3
+        IPv4Address(frr_unicode(intf_ip.split("/")[0])) + 3
     ) + "/{}".format(intf_ip.split("/")[1])
 
     build_config_from_json(tgen, topo_modify_change_ip, save_bkup=False)
@@ -529,7 +530,7 @@ def test_ospf_authentication_md5_tc29_p1(request):
     intf_ip = topo_modify_change_ip["routers"]["r1"]["links"]["r2"]["ipv4"]
 
     topo_modify_change_ip["routers"]["r1"]["links"]["r2"]["ipv4"] = str(
-        IPv4Address(unicode(intf_ip.split("/")[0])) + 3
+        IPv4Address(frr_unicode(intf_ip.split("/")[0])) + 3
     ) + "/{}".format(intf_ip.split("/")[1])
 
     build_config_from_json(tgen, topo_modify_change_ip, save_bkup=False)
