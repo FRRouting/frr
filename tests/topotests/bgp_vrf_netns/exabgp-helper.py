5c8f30867b56ed83b16fe033a7d32d80ee0c86ea
David Schweizer <dschweizer@opensourcerouting.org>
2021-01-01 12:00:00 +0000

    tests: bgp_vrf_netns python3 migration and ExaBGP syntax update
    * Update of ExaBGP config from v3.x to v4.x syntax.
    
    * Various python3 migration fixes.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>
    Signed-off-by: David Schweizer <dschweizer@opensourcerouting.org>

---
+++ tests/topotests/bgp_vrf_netns/exabgp-helper.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from re import match
+from sys import argv, stdin, stdout
+from datetime import datetime
+
+#
+# Handle program arguments.
+#
+
+# 1st arg is peer number
+peer = int(argv[1])
+numRoutes = 10
+
+route_counter = 0
+log_fd = open("<log-file>", "w")
+
+
+#
+# Define helper functions.
+#
+
+
+def receive_update():
+    "Receive update from ExaBGP. The message received might be an UPDATE or a 'done' reply."
+    try:
+        line = stdin.readline()
+        if match(r"done", line) is not None:
+            return
+    except KeyboardInterrupt:
+        pass
+    except IOError:
+        # most likely a signal during readline
+        pass
+
+    # Log received messages to file.
+    timestamp = datetime.now().strftime("%Y%m%d_%H:%M:%S - ")
+    log_fd.write(timestamp + line)
+    log_fd.flush()
+
+    # Yep, we've got a route, account it.
+    if match(r"receive update announced", line):
+        global route_counter
+        route_counter += 1
+
+    # We've got a shutdown request, lets bail.
+    if match(r"shutdown", line):
+        exit(0)
+
+
+def send_route(route):
+    "Sends route and expect 'done' message back."
+    # Send route.
+    stdout.write(route)
+    stdout.flush()
+
+    # Expect to receive acknowledgement.
+    receive_update()
+
+
+#
+# Program main code.
+#
+
+for i in range(0, numRoutes):
+    stdout.write(
+        "announce route 10.201.%s.0/24 med 100 community %i:1 next-hop 10.0.%i.%i\n"
+        % (i, i, (((peer - 1) / 5) + 1), peer + 100)
+    )
+    stdout.flush()
+
+# Loop endlessly to allow ExaBGP to continue running
+while True:
+    receive_update()
