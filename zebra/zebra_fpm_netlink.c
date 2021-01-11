8852b81108aba7d19ceb1a168aedde479733435a
Ameya Dharkar <adharkar@vmware.com>
2021-01-11 11:08:43 -0800

    Zebra: FPM support for gateway IP overlay Index
    FPM sends VNI to the data plane with the EVPN prefix. For pure type-5 EVPN
    route, nexthop interface of EVPN prefix is L3VNI SVI. Thus, we encode L3VNI
    corresponding to the nexthop vrf with rtmsg for this prefix.
    
    For EVPN type-5 route with gateway IP overlay index, we supporting
    asymmetric IRB. Thus, nexthop interface is L2VNI SVI. So, instead of fetching
    vrf VNI, fetch VNI corresponding to the nexthop SVI and encode it in the rtmsg
    for EVPN prefix.
    
    Signed-off-by: Ameya Dharkar <adharkar@vmware.com>

--- zebra/zebra_fpm_netlink.c
+++ zebra/zebra_fpm_netlink.c
@@ -42,6 +42,7 @@
 
 #include "zebra/zebra_fpm_private.h"
 #include "zebra/zebra_vxlan_private.h"
+#include "zebra/interface.h"
 
 /*
  * af_addr_size
@@ -164,7 +165,10 @@ static int netlink_route_info_add_nh(struct netlink_route_info *ri,
 {
 	struct netlink_nh_info nhi;
 	union g_addr *src;
-	zebra_l3vni_t *zl3vni = NULL;
+	struct zebra_vrf *zvrf = NULL;
+	struct interface *ifp = NULL, *link_if = NULL;
+	struct zebra_if *zif = NULL;
+	vni_t vni = 0;
 
 	memset(&nhi, 0, sizeof(nhi));
 	src = NULL;
@@ -199,12 +203,29 @@ static int netlink_route_info_add_nh(struct netlink_route_info *ri,
 	if (re && CHECK_FLAG(re->flags, ZEBRA_FLAG_EVPN_ROUTE)) {
 		nhi.encap_info.encap_type = FPM_NH_ENCAP_VXLAN;
 
-		zl3vni = zl3vni_from_vrf(nexthop->vrf_id);
-		if (zl3vni && is_l3vni_oper_up(zl3vni)) {
-
-			/* Add VNI to VxLAN encap info */
-			nhi.encap_info.vxlan_encap.vni = zl3vni->vni;
+		/* Extract VNI id for the nexthop SVI interface */
+		zvrf = zebra_vrf_lookup_by_id(nexthop->vrf_id);
+		if (zvrf) {
+			ifp = if_lookup_by_index_per_ns(zvrf->zns,
+							nexthop->ifindex);
+			if (ifp) {
+				zif = (struct zebra_if *)ifp->info;
+				if (zif) {
+					if (IS_ZEBRA_IF_BRIDGE(ifp))
+						link_if = ifp;
+					else if (IS_ZEBRA_IF_VLAN(ifp))
+						link_if =
+						if_lookup_by_index_per_ns(
+							zvrf->zns,
+							zif->link_ifindex);
+					if (link_if)
+						vni = vni_id_from_svi(ifp,
+								      link_if);
+				}
+			}
 		}
+
+		nhi.encap_info.vxlan_encap.vni = vni;
 	}
 
 	/*
