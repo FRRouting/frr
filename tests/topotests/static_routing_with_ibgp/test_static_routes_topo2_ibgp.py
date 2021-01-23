efb62eeddf3a33483ca8ece0ee0635d020f292b6
Donald Sharp <sharpd@nvidia.com>
2021-01-23 18:49:05 -0500

    tests: Add pytest.mark.staticd to topotests
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- tests/topotests/static_routing_with_ibgp/test_static_routes_topo2_ibgp.py
+++ tests/topotests/static_routing_with_ibgp/test_static_routes_topo2_ibgp.py
@@ -75,6 +75,8 @@ from lib.bgp import verify_bgp_convergence, create_router_bgp, verify_bgp_rib
 from lib.topojson import build_topo_from_json, build_config_from_json
 from lib.topotest import version_cmp
 
+pytestmark = [pytest.mark.bgpd, pytest.mark.staticd]
+
 # Reading the data from JSON File for topology creation
 jsonFile = "{}/static_routes_topo2_ibgp.json".format(CWD)
 try:
