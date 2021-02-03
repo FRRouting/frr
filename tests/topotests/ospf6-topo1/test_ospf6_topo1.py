ecd32c7741687f34ec18565da5e4b5213e8819a4
Mark Stapp <mjs@voltanet.io>
2021-02-03 16:28:18 -0500

    tests: fix ospf6_topo1 missing ref files
    Only one of the four reference files was present; add the missing
    three. The test just silently passed if a ref file was missing:
    change that to a failure.
    
    Signed-off-by: Mark Stapp <mjs@voltanet.io>

--- tests/topotests/ospf6-topo1/test_ospf6_topo1.py
+++ tests/topotests/ospf6-topo1/test_ospf6_topo1.py
@@ -383,7 +383,15 @@ def test_linux_ipv6_kernel_routingTable():
                 "Linux Kernel IPv6 Routing Table verification failed for router r%s:\n%s"
                 % (i, diff)
             )
+        else:
+            logger.error(
+                "r{} failed - no nhid ref file: {}".format(i, refTableFile)
+            )
 
+            assert False, (
+                "Linux Kernel IPv6 Routing Table verification failed for router r%s\n"
+                % (i)
+            )
 
 def test_shutdown_check_stderr():
 
