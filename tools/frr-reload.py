b094c63ef001019ce4da0d3677df3e2c526a6f97
Chirag Shah <chirag@nvidia.com>
2021-02-26 08:31:07 -0800

    tools: Mark reload failure when no form cli fails
    if no form of the cli fails to execute, mark frr-reload
    as failure so return code can be nonzero.
    The similar approach is done for non no-form (add case) of the cli.
    
    Ticket:CM-33345
    Reviewed By:CCR-11287
    Testing Done:
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>
    
    (cherry picked from commit f26070fc07a279d3a616e8f2d5d05069625f6de5)

--- tools/frr-reload.py
+++ tools/frr-reload.py
@@ -1575,6 +1575,7 @@ if __name__ == '__main__':
 
                             if len(last_arg) <= 2:
                                 log.error('"%s" we failed to remove this command', ' -- '.join(original_cmd))
+                                reload_ok = False
                                 break
 
                             new_last_arg = last_arg[0:-1]
