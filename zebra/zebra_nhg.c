740ee99ae5cbd8b30ca8fe1851bc47c886c6dfff
Mark Stapp <mjs@voltanet.io>
2021-03-02 15:53:19 -0500

    zebra: reorg api used to add daemon-owned nhg
    Re-org/simplify the api used to add nhgs via zapi messages
    from daemons, using a temporary nhe so we can convey all nhe
    attributes.
    
    Signed-off-by: Mark Stapp <mjs@voltanet.io>

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -3798,10 +3798,10 @@ bool zebra_nhg_proto_nexthops_only(void)
 	return proto_nexthops_only;
 }
 
-/* Add NHE from upper level proto */
-struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
-					   uint16_t instance, uint32_t session,
-					   struct nexthop_group *nhg, afi_t afi)
+/*
+ * Add NHE from upper level proto/daemon
+ */
+struct nhg_hash_entry *zebra_nhe_proto_add(struct nhg_hash_entry *nhe)
 {
 	struct nhg_hash_entry lookup;
 	struct nhg_hash_entry *new, *old;
@@ -3809,11 +3809,15 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 	struct nexthop *newhop;
 	bool replace = false;
 	int ret = 0;
+	const struct nexthop_group *nhg;
 
-	if (!nhg->nexthop) {
+	if (nhe)
+		nhg = &nhe->nhg;
+
+	if (!nhe || !nhg->nexthop) {
 		if (IS_ZEBRA_DEBUG_NHG)
 			zlog_debug("%s: id %u, no nexthops passed to add",
-				   __func__, id);
+				   __func__, (nhe ? nhe->id : 0));
 		return NULL;
 	}
 
@@ -3830,7 +3834,7 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 			if (IS_ZEBRA_DEBUG_NHG)
 				zlog_debug(
 					"%s: id %u, backup nexthops not supported",
-					__func__, id);
+					__func__, nhe->id);
 			return NULL;
 		}
 
@@ -3838,7 +3842,7 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 			if (IS_ZEBRA_DEBUG_NHG)
 				zlog_debug(
 					"%s: id %u, blackhole nexthop not supported",
-					__func__, id);
+					__func__, nhe->id);
 			return NULL;
 		}
 
@@ -3846,7 +3850,7 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 			if (IS_ZEBRA_DEBUG_NHG)
 				zlog_debug(
 					"%s: id %u, nexthop without gateway not supported",
-					__func__, id);
+					__func__, nhe->id);
 			return NULL;
 		}
 
@@ -3854,19 +3858,19 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 			if (IS_ZEBRA_DEBUG_NHG)
 				zlog_debug(
 					"%s: id %u, nexthop without ifindex is not supported",
-					__func__, id);
+					__func__, nhe->id);
 			return NULL;
 		}
 		SET_FLAG(newhop->flags, NEXTHOP_FLAG_ACTIVE);
 	}
 
-	zebra_nhe_init(&lookup, afi, nhg->nexthop);
+	zebra_nhe_init(&lookup, nhe->afi, nhg->nexthop);
 	lookup.nhg.nexthop = nhg->nexthop;
 	lookup.nhg.nhgr = nhg->nhgr;
-	lookup.id = id;
-	lookup.type = type;
+	lookup.id = nhe->id;
+	lookup.type = nhe->type;
 
-	old = zebra_nhg_lookup_id(id);
+	old = zebra_nhg_lookup_id(nhe->id);
 
 	if (old) {
 		/*
@@ -3882,19 +3886,19 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 		zebra_nhg_release_all_deps(old);
 	}
 
-	new = zebra_nhg_rib_find_nhe(&lookup, afi);
+	new = zebra_nhg_rib_find_nhe(&lookup, nhe->afi);
 
 	if (!new) {
 		if (IS_ZEBRA_DEBUG_NHG)
-			zlog_debug("%s: zebra_nhg_rib_find_nhe failed for id %u", __func__, id);
+			zlog_debug("%s: zebra_nhg_rib_find_nhe failed for id %u", __func__, nhe->id);
 		return NULL;
 	}
 
 	zebra_nhg_increment_ref(new);
 
 	/* Capture zapi client info */
-	new->zapi_instance = instance;
-	new->zapi_session = session;
+	new->zapi_instance = nhe->zapi_instance;
+	new->zapi_session = nhe->zapi_session;
 
 	zebra_nhg_set_valid_if_active(new);
 
