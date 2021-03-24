0d770f497991eec67f8d3055a8b34868faf0dde9
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-03-24 09:40:10 -0300

    zebra: send BFD messages to staticd
    Add logic to allow `zebra` to exchange BFD messages with `staticd` too.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>
    (cherry picked from commit f85a359aa929264ec4f9a0888d0afb3481d0e619)
    
    PR=76508
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

--- zebra/zebra_ptm.h
+++ zebra/zebra_ptm.h
@@ -69,7 +69,8 @@ struct zebra_ptm_cb {
 	(protocol) == ZEBRA_ROUTE_OSPF6 || \
 	(protocol) == ZEBRA_ROUTE_ISIS || \
 	(protocol) == ZEBRA_ROUTE_PIM || \
-	(protocol) == ZEBRA_ROUTE_OPENFABRIC \
+	(protocol) == ZEBRA_ROUTE_OPENFABRIC || \
+	(protocol) == ZEBRA_ROUTE_STATIC \
 )
 
 void zebra_ptm_init(void);
