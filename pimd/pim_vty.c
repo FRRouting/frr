8c70c9e290211493f0a051f9d7ca84be90391072
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-05-03 12:30:40 -0300

    pimd: show MSDP configuration
    Display the MSDP peer configuration in `show running-config` so it can
    be saved on configuration write.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- pimd/pim_vty.c
+++ pimd/pim_vty.c
@@ -176,6 +176,7 @@ int pim_global_config_write_worker(struct pim_instance *pim, struct vty *vty)
 	else
 		snprintf(spaces, sizeof(spaces), "%s", " ");
 
+	writes += pim_msdp_peer_config_write(vty, pim, spaces);
 	writes += pim_msdp_config_write(pim, vty, spaces);
 
 	if (!pim->send_v6_secondary) {
