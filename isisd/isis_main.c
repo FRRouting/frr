6c458b0a9daf05ea214ab69d3b7d833ea53a84ec
Igor Ryzhov <iryzhov@nfware.com>
2021-05-09 16:43:29 +0300

    isisd: fix dangling instances
    We only need an instance when we have at least one area configured in a
    VRF. Currently we have the following issues:
    - instance for the default VRF is always created
    - instance is not removed after the last area config is removed
    
    This commit fixes both issues.
    
    Signed-off-by: Igor Ryzhov <iryzhov@nfware.com>

--- isisd/isis_main.c
+++ isisd/isis_main.c
@@ -265,9 +265,6 @@ int main(int argc, char **argv, char **envp)
 	lsp_init();
 	mt_init();
 
-	/* create the global 'isis' instance */
-	isis_global_instance_create(VRF_DEFAULT_NAME);
-
 	isis_zebra_init(master, instance);
 	isis_bfd_init(master);
 	isis_ldp_sync_init();
