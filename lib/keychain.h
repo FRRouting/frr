aae83ee3ed7ef001c031ddb16f61b9fbbf7ce6e3
Abhinay Ramesh <rabhinay@vmware.com>
2021-05-11 10:00:38 +0000

    lib: Changes to support hash algo in keychain.
    Problem Statement:
    ==================
    Currently there is no support for configuring hash algorithm in
    keychain. 
     
    RCA:
    ====
    Not implemented yet.
     
    Fix:
    ====
    Changes are done to configure hash algorithm as part of keychain.
    which will easy the configuration from modules using keychain.
     
    Risk:
    =====
    Low risk
     
    Tests Executed:
    ===============
    Have tested the configuration and unconfiguration flow for newly
    implemented CLI.
    
    !
    key chain abcd
     key 100
      key-string password
      cryptographic-algorithm sha1
     exit
     key 200
      key-string password
      cryptographic-algorithm sha256
     exit
    !
    
    Signed-off-by: Abhinay Ramesh <rabhinay@vmware.com>

--- lib/keychain.h
+++ lib/keychain.h
@@ -27,6 +27,47 @@
 extern "C" {
 #endif
 
+enum keychain_hash_algo {
+	KEYCHAIN_ALGO_NULL,
+	KEYCHAIN_ALGO_MD5,
+	KEYCHAIN_ALGO_HMAC_SHA1,
+	KEYCHAIN_ALGO_HMAC_SHA256,
+	KEYCHAIN_ALGO_HMAC_SHA384,
+	KEYCHAIN_ALGO_HMAC_SHA512,
+	KEYCHAIN_ALGO_MAX
+};
+
+#define KEYCHAIN_MD5_HASH_SIZE 16
+#define KEYCHAIN_HMAC_SHA1_HASH_SIZE 20
+#define KEYCHAIN_HMAC_SHA256_HASH_SIZE 32
+#define KEYCHAIN_HMAC_SHA384_HASH_SIZE 48
+#define KEYCHAIN_HMAC_SHA512_HASH_SIZE 64
+#define KEYCHAIN_MAX_HASH_SIZE 64
+
+#define KEYCHAIN_ALGO_MD5_INTERNAL_BLK_SIZE 16
+#define KEYCHAIN_ALGO_SHA1_INTERNAL_BLK_SIZE 64
+#define KEYCHAIN_ALGO_SHA256_INTERNAL_BLK_SIZE 64
+#define KEYCHAIN_ALGO_SHA384_INTERNAL_BLK_SIZE 128
+#define KEYCHAIN_ALGO_SHA512_INTERNAL_BLK_SIZE 128
+#define KEYCHAIN_ALGO_MAX_INTERNAL_BLK_SIZE 128
+
+struct keychain_algo_info {
+	enum keychain_hash_algo key;
+	const char *name;
+	uint32_t length;
+	uint32_t block;
+	const char *desc;
+};
+
+extern const struct keychain_algo_info algo_info[];
+uint32_t keychain_get_block_size(enum keychain_hash_algo key);
+uint32_t keychain_get_hash_len(enum keychain_hash_algo key);
+const char *keychain_get_description(enum keychain_hash_algo key);
+struct keychain_algo_info
+keychain_get_hash_algo_info(enum keychain_hash_algo key);
+enum keychain_hash_algo keychain_get_algo_id_by_name(const char *name);
+const char *keychain_get_algo_name_by_id(enum keychain_hash_algo key);
+
 struct keychain {
 	char *name;
 
@@ -47,7 +88,7 @@ struct key {
 	uint32_t index;
 
 	char *string;
-
+	enum keychain_hash_algo hash_algo;
 	struct key_range send;
 	struct key_range accept;
 
@@ -60,7 +101,7 @@ extern struct keychain *keychain_lookup(const char *);
 extern struct key *key_lookup_for_accept(const struct keychain *, uint32_t);
 extern struct key *key_match_for_accept(const struct keychain *, const char *);
 extern struct key *key_lookup_for_send(const struct keychain *);
-
+const char *keychain_algo_str(enum keychain_hash_algo hash_algo);
 #ifdef __cplusplus
 }
 #endif
