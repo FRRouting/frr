e7a8aeb5894bbdca37d33d8e633406e22713e382
anlancs <anlan_cs@tom.com>
2021-06-12 16:42:26 +0800

    vtysh: make hostname and domainname persistent
    Two difficults to make persistent:
    
    1) In integrated mode, watchfrr uses `vtysh -w` to save into configure file, but the new
    `vtysh` can't get these names from current using vtysh.
    
    2) When usesrs run `vtysh`, it will never reading /etf/frr/frr.conf, so
    `vtysh` can't get the exact names.
    
    To fixes this issue, need put these names saved into all daemons besides
    current vtysh.
    
    Signed-off-by: anlancs <anlan_cs@tom.com>

--- lib/command.c
+++ lib/command.c
@@ -2522,10 +2522,6 @@ void cmd_init(int terminal)
 		hash_cmd_init();
 	}
 
-	install_element(CONFIG_NODE, &hostname_cmd);
-	install_element(CONFIG_NODE, &no_hostname_cmd);
-	install_element(CONFIG_NODE, &domainname_cmd);
-	install_element(CONFIG_NODE, &no_domainname_cmd);
 
 	if (terminal > 0) {
 		full_cli = true;
@@ -2536,6 +2532,11 @@ void cmd_init(int terminal)
 		install_element(CONFIG_NODE, &no_password_cmd);
 		install_element(CONFIG_NODE, &enable_password_cmd);
 		install_element(CONFIG_NODE, &no_enable_password_cmd);
+
+		install_element(CONFIG_NODE, &hostname_cmd);
+		install_element(CONFIG_NODE, &no_hostname_cmd);
+		install_element(CONFIG_NODE, &domainname_cmd);
+		install_element(CONFIG_NODE, &no_domainname_cmd);
 
 		install_element(CONFIG_NODE, &service_password_encrypt_cmd);
 		install_element(CONFIG_NODE, &no_service_password_encrypt_cmd);
