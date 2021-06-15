6aafa32970f37bbc3ab108de8788778c8431d42f
Quentin Young <qlyoung@nvidia.com>
2021-06-15 19:49:19 -0400

    bgpd: apply cond-adv policy to update group
    The new outbound filter to apply conditional advertisement policy was
    not working properly due to complications with update groups. The two
    routemaps were properly copied into the update group peer filter but not
    the conditional advertisement state.
    
    Signed-off-by: Quentin Young <qlyoung@nvidia.com>

--- bgpd/bgp_conditional_adv.c
+++ bgpd/bgp_conditional_adv.c
@@ -258,6 +258,19 @@ static void bgp_conditional_adv_timer(struct thread *t)
 						? UPDATE_TYPE_WITHDRAW
 						: UPDATE_TYPE_ADVERTISE;
 
+			/*
+			 * Update condadv update type so
+			 * subgroup_announce_check() can properly apply
+			 * outbound policy according to advertisement state
+			 */
+			paf = peer_af_find(peer, afi, safi);
+			if (paf) {
+				SUBGRP_PEER(PAF_SUBGRP(paf))
+					->filter[afi][safi]
+					.advmap.update_type =
+					filter->advmap.update_type;
+			}
+
 			/* Send regular update as per the existing policy.
 			 * There is a change in route-map, match-rule, ACLs,
 			 * or route-map filter configuration on the same peer.
@@ -270,11 +283,14 @@ static void bgp_conditional_adv_timer(struct thread *t)
 						__func__, peer->host,
 						get_afi_safi_str(afi, safi,
 								 false));
-
-				paf = peer_af_find(peer, afi, safi);
 				if (paf) {
 					update_subgroup_split_peer(paf, NULL);
 					subgrp = paf->subgroup;
+					SUBGRP_PEER(PAF_SUBGRP(paf))
+						->filter[afi][safi]
+						.advmap.update_type =
+						filter->advmap.update_type;
+
 					if (subgrp && subgrp->update_group)
 						subgroup_announce_table(
 							paf->subgroup, NULL);
