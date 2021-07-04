4535b6113cf105287e060042b956251d4cbf4198
Donald Lee <dlqs@gmx.com>
2021-07-04 23:13:32 +0800

    tests: Add test for calling Lua function
    Signed-off-by: Donald Lee <dlqs@gmx.com>

--- tests/lib/script1.lua
+++ tests/lib/script1.lua
@@ -1,1 +1,20 @@
-a = a + b
+function foo(a, b)
+  a = a + b
+  return {
+    a = a,
+    b = b,
+  }
+end
+
+function fact(n)
+  function helper(m)
+    if m == 0 then
+      return 1
+    else
+      return m * helper(m - 1)
+    end
+  end
+  return {
+    n = helper(n)
+  }
+end
