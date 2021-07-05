142774530ade49f63997bd52c434457fe8979bdf
github login name <ranjany@vmware.com>
2021-07-05 04:07:25 -0700

    pimd: make pim interface hold interval range 1-630
    Making the interface holdtime range to 3.5 times the hello-time
    
    As per 7761, Section 4.11:
    The Holdtime in a Hello message should be set to
       (3.5 * Hello_Period), giving a default value of 105 seconds.
    
    Therefore providing the user also to configure max upto 3.5 times
    the hello timer interval.
    
    Signed-off-by: Mobashshera Rasool <mrasool@vmware.com>

--- pimd/pim_cmd.c
+++ pimd/pim_cmd.c
@@ -8767,7 +8767,7 @@ DEFUN (interface_no_ip_mroute,
 
 DEFUN (interface_ip_pim_hello,
        interface_ip_pim_hello_cmd,
-       "ip pim hello (1-180) [(1-180)]",
+       "ip pim hello (1-180) [(1-630)]",
        IP_STR
        PIM_STR
        IFACE_PIM_HELLO_STR
@@ -8802,7 +8802,7 @@ DEFUN (interface_ip_pim_hello,
 
 DEFUN (interface_no_ip_pim_hello,
        interface_no_ip_pim_hello_cmd,
-       "no ip pim hello [(1-180) [(1-180)]]",
+       "no ip pim hello [(1-180) [(1-630)]]",
        NO_STR
        IP_STR
        PIM_STR
