1ffa72a503ed91e9a6f6d403cfb6c28ec2b4b176
Donald Lee <dlqs@gmx.com>
2021-07-08 03:07:06 +0800

    tests: Add more examples to get_result
    Signed-off-by: Donald Lee <dlqs@gmx.com>

--- tests/lib/script1.lua
+++ tests/lib/script1.lua
@@ -1,12 +1,28 @@
+
+-- Positive testing
+
 function foo(a, b)
-  a = a + b
+  a = a + 1
+  b = b + 1
   return {
     a = a,
     b = b,
   }
 end
 
+function bar(a, b)
+  a = a + 1
+  b = b + 1
+  c = 303
+  return {
+    b = b,
+    c = c,
+  }
+end
+
 function fact(n)
+  -- outer function must return a table
+  -- inner functions can be used to recurse or as helpers
   function helper(m)
     if m == 0 then
       return 1
@@ -19,14 +35,20 @@ function fact(n)
   }
 end
 
+-- Negative testing
+
 function bad_return1()
 end
 
 function bad_return2()
-  return 1
+  return 123
 end
 
 function bad_return3()
+  return {}
+end
+
+function bad_return4()
   error("Something bad!")
 end
 
