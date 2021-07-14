b7e61d132a04edfc77faf12aca959987b6766c7d
Christian Hopps <chopps@labn.net>
2021-07-14 08:37:59 +0000

    tests: Update dir-locals
    - format with black in emacs
    - wrap text at 88 (black)
    - find the project pylint file
    
    Signed-off-by: Christian Hopps <chopps@labn.net>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,11 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-;;; Match project coding conventions
 
 ((c-mode . ((indent-tabs-mode . t)
             (show-trailing-whitespace . t)
-            (c-basic-offset . 8)
-            )))
+            (c-basic-offset . 8)))
+ (json-mode . ((js-indent-level 4)))
+ (python-mode . ((python-formatter . black)
+                 (eval (flycheck-select-checker 'python-pylint))
+                 (eval setf flycheck-pylintrc (flycheck-locate-config-file-ancestor-directories ".pylintrc" 'python-pylint))
+                 (python-fill-column . 88))))
