c8bcfb94f0c77949d259539e17980960eaef6cf3
Donald Lee <dlqs@gmx.com>
2021-08-02 02:59:02 +0800

    bootstrap.sh: untar lua
    Signed-off-by: Donald Lee <dlqs@gmx.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ ! -d "lua/" ] && tar -xf lua-5.3.6.tar.gz && mv lua-5.3.6/ lua/
+
 # This file exists to document the proper way to initialize autotools,
 # and so that those used to the presence of bootstrap.sh or autogen.sh
 # will have an eaiser time.
