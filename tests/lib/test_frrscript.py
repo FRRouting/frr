16cebc55c99dbf597446b3fd6ab52acdb94b1bb7
Donald Lee <dlqs@gmx.com>
2021-08-02 16:33:36 +0800

    tests: Remove SCRIPTING guard for tests
    Signed-off-by: Donald Lee <dlqs@gmx.com>

--- tests/lib/test_frrscript.py
+++ tests/lib/test_frrscript.py
@@ -1,14 +1,7 @@
 import frrtest
 import pytest
 
-if 'S["SCRIPTING_TRUE"]=""\n' not in open("../config.status").readlines():
-    class TestFrrscript:
-        @pytest.mark.skipif(True, reason="Test unsupported")
-        def test_exit_cleanly(self):
-            pass
-else:
+class TestFrrscript(frrtest.TestMultiOut):
+    program = "./test_frrscript"
 
-    class TestFrrscript(frrtest.TestMultiOut):
-        program = "./test_frrscript"
-
-    TestFrrscript.exit_cleanly()
+TestFrrscript.exit_cleanly()
