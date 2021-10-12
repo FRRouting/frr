FRRouting/frr

commit 1f7a68a2ff0ba1424131f30112e0cc1572f0bee3

lib, zebra: update interface name at netlink creation
the interface name was not present in the hook in charge of updating the
interface context to the registered hook service. For that, update the
name before informing it.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

lib/if.c
@@ -217,14 +217,16 @@ struct interface *if_create_name(const char *name, vrf_id_t vrf_id)
 	return ifp;
 }
 
-struct interface *if_create_ifindex(ifindex_t ifindex, vrf_id_t vrf_id)
+struct interface *if_create_ifindex(ifindex_t ifindex, vrf_id_t vrf_id,
+				    char *optional_name)
 {
 	struct interface *ifp;
 
 	ifp = if_new(vrf_id);
 
 	if_set_index(ifp, ifindex);
-
+	if (optional_name)
+		if_set_name(ifp, optional_name);
 	hook_call(if_add, ifp);
 	return ifp;
 }
@@ -554,7 +556,8 @@ struct interface *if_get_by_name(const char *name, vrf_id_t vrf_id)
 	return NULL;
 }
 
-struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id)
+struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id,
+				    char *optional_name)
 {
 	struct interface *ifp;
 
@@ -564,7 +567,7 @@ struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id)
 		ifp = if_lookup_by_ifindex(ifindex, vrf_id);
 		if (ifp)
 			return ifp;
-		return if_create_ifindex(ifindex, vrf_id);
+		return if_create_ifindex(ifindex, vrf_id, optional_name);
 	case VRF_BACKEND_VRF_LITE:
 		ifp = if_lookup_by_index_all_vrf(ifindex);
 		if (ifp) {
@@ -576,7 +579,7 @@ struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id)
 			if_update_to_new_vrf(ifp, vrf_id);
 			return ifp;
 		}
-		return if_create_ifindex(ifindex, vrf_id);
+		return if_create_ifindex(ifindex, vrf_id, optional_name);
 	}
 
 	return NULL;

zebra/if_netlink.c
@@ -711,10 +711,8 @@ static int netlink_interface(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 	 * back references on the slave interfaces is painful if not done
 	 * this way, i.e. by creating by ifindex.
 	 */
-	ifp = if_get_by_ifindex(ifi->ifi_index, vrf_id);
+	ifp = if_get_by_ifindex(ifi->ifi_index, vrf_id, name);
 	set_ifindex(ifp, ifi->ifi_index, zns); /* add it to ns struct */
-
-	if_set_name(ifp, name);
 
 	ifp->flags = ifi->ifi_flags & 0x0000fffff;
 	ifp->mtu6 = ifp->mtu = *(uint32_t *)RTA_DATA(tb[IFLA_MTU]);

lib/if.h
@@ -509,7 +509,8 @@ extern void if_update_to_new_vrf(struct interface *, vrf_id_t vrf_id);
 extern struct interface *if_create_name(const char *name, vrf_id_t vrf_id);
 
 /* Create new interface, adds to index list only */
-extern struct interface *if_create_ifindex(ifindex_t ifindex, vrf_id_t vrf_id);
+extern struct interface *if_create_ifindex(ifindex_t ifindex, vrf_id_t vrf_id,
+					   char *name);
 extern struct interface *if_lookup_by_index(ifindex_t, vrf_id_t vrf_id);
 extern struct interface *if_lookup_by_index_all_vrf(ifindex_t);
 extern struct interface *if_lookup_exact_address(const void *matchaddr,
@@ -524,8 +525,8 @@ size_t if_lookup_by_hwaddr(const uint8_t *hw_addr, size_t addrsz,
 extern struct interface *if_lookup_by_name_all_vrf(const char *ifname);
 extern struct interface *if_lookup_by_name(const char *ifname, vrf_id_t vrf_id);
 extern struct interface *if_get_by_name(const char *ifname, vrf_id_t vrf_id);
-extern struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id);
-
+extern struct interface *if_get_by_ifindex(ifindex_t ifindex, vrf_id_t vrf_id,
+					   char *optional_name);
 /* Sets the index and adds to index list */
 extern int if_set_index(struct interface *ifp, ifindex_t ifindex);
 /* Sets the name and adds to name list */

zebra/zebra_l2.c
@@ -110,7 +110,7 @@ void zebra_l2_map_slave_to_bond(struct zebra_l2info_bondslave *bond_slave,
 		bond_slave->bond_if = bond_if;
 	else
 		bond_slave->bond_if = if_create_ifindex(bond_slave->bond_ifindex,
-							vrf_id);
+							vrf_id, NULL);
 }
 
 void zebra_l2_unmap_slave_from_bond(struct zebra_l2info_bondslave *bond_slave)

commit cf4642cfff27abdb5fc92f222d98740042f8b1ea

zebra: update the interface name when if context is available
This commit addresses a case where at startup, a big chunk of
interfaces is created, and when the list of interfaces becomes
bigger, the interface context on zebra is somehow not read from
kernel. This looks as if the interface context from zebra was
unsynced with interface context from kernel.

The script to reproduce is given below, and creates many tuples
made up of 2 vlan interfaces, one vrf interface and one bonding
interface. Somehow, the 64th bonding interface with index 255
failed to be synced with kernel.

v=$1
m=$(($v + 1001))
j=100
k=120
/sbin/ip link set dev ntfp1 mtu 1566
/sbin/ip link set dev ntfp2 mtu 1566
/sbin/ip link set dev ntfp1 up
/sbin/ip link set dev ntfp2 up
for i in $(seq 1002 $m); do
    j=$(($j + 1))
    k=$(($k + 1))
    /sbin/ip link add vrf$i up type vrf table $i
    /sbin/ip link add ethgrp$i up master vrf$i  mtu 1566 type bond mode active-backup fail_over_mac none
    /sbin/ip link add link ntfp1 name bnet$j master ethgrp$i type vlan id $j
    /sbin/ip link add link ntfp2 name bnetbis$j master ethgrp$i type vlan id $j
    /sbin/ip link set dev ethgrp$i type bond miimon 0 primary_reselect always primary bnet$j
    /sbin/ip addr add dev ethgrp$i 192.168.123.$k/24
    /sbin/ip -6 addr add dev ethgrp$i 2000::$k/64
    /sbin/ip link set dev bnet$j up
    /sbin/ip link set dev bnetbis$j up
done

This is due to the fact that interface has already been created
by interface index, and the interface name has not been set. The
search for an already existing context per ifindex has been found,
but the interface name has been forgotten to be set. Rework the
fixed commit by externalizing the setting of the interface name.

As side effect of externalizing the call to if_set_name(), the order
of function calls has changed, and the interface add hooks put in
place in if_create_ifindex() will be called without the presence
of the interface name in interface context. To handle that case,
place the same add hook in the function in charge of setting the
interface name. The successive call of add hook is already a reality,
so changing the name should be not be a problem.

The part of code related to looking for already configured
interface is still necessary in that branch, since that function
is called upon new namespace created, and netlink events may come
after frr has loaded the configuration, with pre-configured interfaces.

Fixes: 1f7a68a2ff0b ("lib, zebra: update interface name at netlink creation")

lib/if.c
@@ -716,6 +716,7 @@ void if_set_name(struct interface *ifp, const char *name, struct vrf *vrf_param)
 
 	if (ifp->name[0] != '\0')
 		IFNAME_RB_INSERT(vrf, ifp);
+	hook_call(if_add, ifp);
 }
 
 /* Does interface up ? */

zebra/if_netlink.c
@@ -973,12 +973,25 @@ static int netlink_interface(struct nlmsghdr *h, ns_id_t ns_id, int startup)
 	}
 
 	/* Add interface.
-	 * We add by index first because in some cases such as the master
+	 * We first look to already user configured interfaces, indexed
+	 * by interface name, in case there are pre-configured interfaces.
+	 * This function can be called after config loaded, when vrf backend
+	 * is namespace based. Because zebra permits pre-configuring interfaces
+	 * from other namespaces, a check must be done here.
+	 * Then if interface with name is not found, we add by index; this
+	 * search must be done just after, because in some cases such as the master
 	 * interface, we have the index before we have the name. Fixing
 	 * back references on the slave interfaces is painful if not done
-	 * this way, i.e. by creating by ifindex.
+	 * this way, i.e. by creating by ifindex. Do not forget to set the
+	 * interface name.
 	 */
-	ifp = if_get_by_ifindex(ifi->ifi_index, vrf_id);
+	ifp = if_lookup_by_name(name, vrf_id);
+	if (ifp)
+		if_set_index(ifp, ifi->ifi_index);
+	else {
+		ifp = if_get_by_ifindex(ifi->ifi_index, vrf_id);
+		if_set_name(ifp, name, NULL);
+	}
 	set_ifindex(ifp, ifi->ifi_index, zns); /* add it to ns struct */
 
 	if_set_name(ifp, name, NULL);