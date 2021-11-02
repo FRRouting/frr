782f7f42d025fc22104c421e830ac9cca513aeac
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-11-02 18:54:23 -0300

    bgpd: fix BFD configuration update on TTL change
    When altering the TTL of a eBGP peer also update the BFD
    configuration. This was only working when the configuration happened
    after the peer connection had been established.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>
    (cherry picked from commit 4ba37eb691f9574e908ec7ed03e8f4cb4214f2d5)

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -4734,6 +4734,10 @@ int peer_ebgp_multihop_set(struct peer *peer, int ttl)
 						BGP_NOTIFY_CEASE_CONFIG_CHANGE);
 			else
 				bgp_session_reset(peer);
+
+			/* Reconfigure BFD peer with new TTL. */
+			if (peer->bfd_config)
+				bgp_peer_bfd_update_source(peer);
 		}
 	} else {
 		group = peer->group;
@@ -4748,6 +4752,10 @@ int peer_ebgp_multihop_set(struct peer *peer, int ttl)
 						BGP_NOTIFY_CEASE_CONFIG_CHANGE);
 			else
 				bgp_session_reset(peer);
+
+			/* Reconfigure BFD peer with new TTL. */
+			if (peer->bfd_config)
+				bgp_peer_bfd_update_source(peer);
 		}
 	}
 	return 0;
@@ -4781,6 +4789,10 @@ int peer_ebgp_multihop_unset(struct peer *peer)
 					BGP_NOTIFY_CEASE_CONFIG_CHANGE);
 		else
 			bgp_session_reset(peer);
+
+		/* Reconfigure BFD peer with new TTL. */
+		if (peer->bfd_config)
+			bgp_peer_bfd_update_source(peer);
 	} else {
 		group = peer->group;
 		for (ALL_LIST_ELEMENTS(group->peer, node, nnode, peer)) {
@@ -4797,6 +4809,10 @@ int peer_ebgp_multihop_unset(struct peer *peer)
 				else
 					bgp_session_reset(peer);
 			}
+
+			/* Reconfigure BFD peer with new TTL. */
+			if (peer->bfd_config)
+				bgp_peer_bfd_update_source(peer);
 		}
 	}
 	return 0;
