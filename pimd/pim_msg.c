3ab1f9acc99a217e44404dd2428ba30843ab673e
sarita patra <saritap@vmware.com>
2021-11-30 04:21:04 -0800

    pimd: Do not check for keep alive timer while sending SGRpt prune
    RCA: We were checking for keep alive timer running while sending SGRpt prune.
    THis was done to detect oif source was reachable in LHR. If SG was
    created due to pim join and not due to traffic, SGRpt prune will not sent.
    
    Fix: Kat timer check has to be done only on LHR. Other routers should not
    check for KAT timer while sending SG Rpt prune.
    So Identified LHR by checking if IGMP flag is set on parent and verifed kat
    timer only for those upstream.
    
    Signed-off-by: sarita patra <saritap@vmware.com>

--- pimd/pim_msg.c
+++ pimd/pim_msg.c
@@ -136,8 +136,12 @@ size_t pim_msg_get_jp_group_size(struct list *sources)
 				 */
 				if (child->rpf.source_nexthop.interface &&
 					!pim_rpf_is_same(&up->rpf,
-						&child->rpf) &&
-					(child->t_ka_timer)) {
+						&child->rpf)) {
+					if (PIM_UPSTREAM_FLAG_TEST_SRC_IGMP(
+								up->flags)
+					    && !(child->t_ka_timer))
+						continue;
+
 					size += sizeof(
 						struct pim_encoded_source_ipv4);
 					PIM_UPSTREAM_FLAG_SET_SEND_SG_RPT_PRUNE(
