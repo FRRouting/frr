04623de4df60d554e5dda1555082be4fa2178d1a
Mobashshera Rasool <mrasool@vmware.com>
2021-12-13 22:20:37 -0800

    pimd: Modifying in_addr to pim_addr in struct pim_ifchannel for IPv6.
    Changed struct in_addr ifassert_winner to pim_addr
    which will be used in both IPv4 and IPv6(Both MLD and IGMP).
    Reviewed-by: Sarita Patra <saritap@vmware.com>
    Signed-off-by: Mobashshera Rasool <mrasool@vmware.com>

--- pimd/pim_assert.h
+++ pimd/pim_assert.h
@@ -55,8 +55,7 @@ struct pim_assert_metric {
 #define PIM_ASSERT_ROUTE_METRIC_MAX      (0xFFFFFFFF)
 
 void pim_ifassert_winner_set(struct pim_ifchannel *ch,
-			     enum pim_ifassert_state new_state,
-			     struct in_addr winner,
+			     enum pim_ifassert_state new_state, pim_addr winner,
 			     struct pim_assert_metric winner_metric);
 
 int pim_assert_recv(struct interface *ifp, struct pim_neighbor *neigh,
