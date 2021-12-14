b0b7322a777cd5b7cb73d1a74ee23ceca43c56aa
Mobashshera Rasool <mrasool@vmware.com>
2021-12-13 22:31:39 -0800

    pimd: Modifying in_addr to pim_addr in struct pim_interface for IPv6
    Based on compiler option, pim_addr will be changed to in_addr
    or in6_addr for pimd and pim6d respectively.
    Reviewed-by: Sarita Patra <saritap@vmware.com>
    Signed-off-by: Mobashshera Rasool <mrasool@vmware.com>

--- pimd/pim_pim.c
+++ pimd/pim_pim.c
@@ -570,8 +570,8 @@ static int pim_msg_send_frame(int fd, char *buf, size_t len,
 	return 0;
 }
 
-int pim_msg_send(int fd, struct in_addr src, struct in_addr dst,
-		 uint8_t *pim_msg, int pim_msg_size, const char *ifname)
+int pim_msg_send(int fd, pim_addr src, struct in_addr dst, uint8_t *pim_msg,
+		 int pim_msg_size, const char *ifname)
 {
 	struct sockaddr_in to;
 	socklen_t tolen;
