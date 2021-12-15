e35befa52819ab6bed0e2ff37efcb254a8d80ec9
Philippe Guibert <philippe.guibert@6wind.com>
2021-12-15 21:54:51 +0100

    Revert "bfdd: forbid setting interface for multihop sessions"
    This reverts commit 55d9ae78dbc2403974227b78dbade01cba37324a.
    
    There has been discussions in the opensource community, whether
    it was necessary to support multihop sessions with interface
    parameter. IETF yang mode for BFD does not support ifname
    parameter, whereas yams releases permit configuring an interface
    attribute for tracker bfd sessions.
    
    Originally authorized on stable/frr/7.3, the upstream frr yang
    model does not permit configuring ifname attribute for multihop
    sessions. To maintain the usage of interface attribute in yams,
    the interface attribute in bfd sessions has been kept in frr
    
    Revert a previous pull request done on upstream to maintain
    interface attribute in bfd session.
    
    Link: https://github.com/FRRouting/frr/pull/8827/commits/55d9ae78dbc2403974227b78dbade01cba37324a
    
    PR=76508
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bfdd/bfdd_cli.c
+++ bfdd/bfdd_cli.c
@@ -134,13 +134,7 @@ DEFPY_YANG_NOSH(
 
 	if (multihop) {
 		if (!local_address_str) {
-			vty_out(vty,
-				"%% local-address is required when using multihop\n");
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-		if (ifname) {
-			vty_out(vty,
-				"%% interface is prohibited when using multihop\n");
+			vty_out(vty, "%% local-address is required when using multihop\n");
 			return CMD_WARNING_CONFIG_FAILED;
 		}
 		snprintf(source_str, sizeof(source_str), "[source-addr='%s']",
@@ -155,7 +149,7 @@ DEFPY_YANG_NOSH(
 	if (ifname)
 		slen += snprintf(xpath + slen, sizeof(xpath) - slen,
 				 "[interface='%s']", ifname);
-	else if (!multihop)
+	else
 		slen += snprintf(xpath + slen, sizeof(xpath) - slen,
 				 "[interface='*']");
 	if (vrf)
@@ -200,20 +194,10 @@ DEFPY_YANG(
 	char xpath[XPATH_MAXLEN];
 	char source_str[INET6_ADDRSTRLEN + 32];
 
-	if (multihop) {
-		if (!local_address_str) {
-			vty_out(vty,
-				"%% local-address is required when using multihop\n");
-			return CMD_WARNING_CONFIG_FAILED;
-		}
-		if (ifname) {
-			vty_out(vty,
-				"%% interface is prohibited when using multihop\n");
-			return CMD_WARNING_CONFIG_FAILED;
-		}
+	if (multihop)
 		snprintf(source_str, sizeof(source_str), "[source-addr='%s']",
 			 local_address_str);
-	} else
+	else
 		source_str[0] = 0;
 
 	slen = snprintf(xpath, sizeof(xpath),
@@ -223,7 +207,7 @@ DEFPY_YANG(
 	if (ifname)
 		slen += snprintf(xpath + slen, sizeof(xpath) - slen,
 				 "[interface='%s']", ifname);
-	else if (!multihop)
+	else
 		slen += snprintf(xpath + slen, sizeof(xpath) - slen,
 				 "[interface='*']");
 	if (vrf)
@@ -243,6 +227,7 @@ static void _bfd_cli_show_peer(struct vty *vty, struct lyd_node *dnode,
 			       bool mhop)
 {
 	const char *vrf = yang_dnode_get_string(dnode, "./vrf");
+	const char *ifname = yang_dnode_get_string(dnode, "./interface");
 
 	vty_out(vty, " peer %s",
 		yang_dnode_get_string(dnode, "./dest-addr"));
@@ -257,12 +242,8 @@ static void _bfd_cli_show_peer(struct vty *vty, struct lyd_node *dnode,
 	if (strcmp(vrf, VRF_DEFAULT_NAME))
 		vty_out(vty, " vrf %s", vrf);
 
-	if (!mhop) {
-		const char *ifname =
-			yang_dnode_get_string(dnode, "./interface");
-		if (strcmp(ifname, "*"))
-			vty_out(vty, " interface %s", ifname);
-	}
+	if (strcmp(ifname, "*"))
+		vty_out(vty, " interface %s", ifname);
 
 	vty_out(vty, "\n");
 }
