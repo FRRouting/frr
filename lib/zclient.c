FRRouting/frr

commit cff0de128d10f1f78144c8c0ca2d2251099d0241

lib: fix SA warning in vrf creation zapi handler
Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

lib/zclient.c
@@ -1724,11 +1724,11 @@ static int zclient_vrf_add(struct zclient *zclient, vrf_id_t vrf_id)
 	if (strlen(vrfname_tmp) == 0)
 		goto stream_failure;
 
-	/* Lookup/create vrf by vrf_id. */
+	/* Lookup/create vrf by name, then vrf_id. */
 	vrf = vrf_get(vrf_id, vrfname_tmp);
 
-	/* Maybe it already exists? */
-	if (!vrf && vrf_get(vrf_id, NULL) != NULL)
+	/* If there's already a VRF with this name, don't create vrf */
+	if (!vrf)
 		return 0;
 
 	vrf->data.l.table_id = data.l.table_id;

commit d4786bdd3c629881dceae8edcbea7fd79d3e0a6e

lib: prevent from configuring vrf with invalid pointer
add a log trace to indicate vrf could not be created.

Fixes: cff0de128d10 ("lib: fix SA warning in vrf creation zapi handler")
Old-PR: 67048
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
Acked-by: Alain Ritoux <alain.ritoux@6wind.com>
(cherry picked from commit 027f86709907d06fc397fa68dcb998b65e9881fa)
PR=76507
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
Acked-by: Louis Scalbert <louis.scalbert@6wind.com>

lib/zclient.c
@@ -1941,9 +1941,11 @@ static int zclient_vrf_add(struct zclient *zclient, vrf_id_t vrf_id)
 	vrf = vrf_get(vrf_id, vrfname_tmp);
 
 	/* If there's already a VRF with this name, don't create vrf */
-	if (!vrf)
+	if (!vrf) {
+		zlog_err("%s() no vrf created with id %u name %s",
+			 __func__, vrf_id, vrfname_tmp);
 		return 0;
-
+	}
 	vrf->data.l.table_id = data.l.table_id;
 	memcpy(vrf->data.l.netns_name, data.l.netns_name, NS_NAMSIZ);
 	/* overwrite default vrf */