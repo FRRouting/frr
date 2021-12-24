6WIND/quagga

bgpd: support TCP keepalive for BGP connection
TCP keepalive is enabled once BGP connection is established.

New vty commands:

  bgp tcp-keepalive <1-65535> <1-65535> <1-30>
  no bgp tcp-keepalive

Signed-off-by: Xiaofeng Liu <xiaofeng.liu@6wind.com>

bgpd/bgp_network.c
@@ -147,6 +147,22 @@ bgp_update_sock_send_buffer_size (int fd)
 }
 
 static void
+bgp_update_setsockopt_tcp_keepalive(int fd)
+{
+  if (bm->tcp_keepalive_idle != 0)
+    {
+      int ret;
+      ret = setsockopt_tcp_keepalive(fd, bm->tcp_keepalive_idle,
+                                     bm->tcp_keepalive_intvl,
+                                     bm->tcp_keepalive_probes);
+      if (ret < 0)
+        zlog_err ("Can't set TCP keepalive on socket %d, idle %u intvl %u probes %u",
+                 fd, bm->tcp_keepalive_idle, bm->tcp_keepalive_intvl,
+                 bm->tcp_keepalive_probes);
+    }
+}
+
+static void
 bgp_set_socket_ttl (struct peer *peer, int bgp_sock)
 {
   char buf[INET_ADDRSTRLEN];
@@ -221,6 +237,8 @@ bgp_accept (struct thread *thread)
 
   /* Set socket send buffer size */
   bgp_update_sock_send_buffer_size(bgp_sock);
+  /* Set TCP keepalive when TCP keepalive is enabled */
+  bgp_update_setsockopt_tcp_keepalive(bgp_sock);
 
   if (BGP_DEBUG (events, EVENTS))
     zlog_debug ("[Event] BGP connection from host %s", inet_sutop (&su, buf));
@@ -425,6 +443,8 @@ bgp_connect (struct peer *peer)
 
   /* Set socket send buffer size */
   bgp_update_sock_send_buffer_size(peer->fd);
+  /* Set TCP keepalive when TCP keepalive is enabled */
+  bgp_update_setsockopt_tcp_keepalive(peer->fd);
 
   bgp_set_socket_ttl (peer, peer->fd);
 

bgpd/bgp_vty.c
@@ -987,6 +987,38 @@ ALIAS (no_bgp_timers,
        "Keepalive interval\n"
        "Holdtime\n")
 
+DEFUN (bgp_tcp_keepalive,
+       bgp_tcp_keepalive_cmd,
+       "bgp tcp-keepalive <1-65535> <1-65535> <1-30>",
+       "BGP specific commands\n"
+       "TCP keepalive parameters\n"
+       "TCP keepalive idle time (seconds)\n"
+       "TCP keepalive interval (seconds)\n"
+       "TCP keepalive maxinum probes\n")
+{
+  u_int16_t idle, intvl, probes;
+
+  VTY_GET_INTEGER ("keepalive-idle", idle, argv[0]);
+  VTY_GET_INTEGER ("keepalive-interval", intvl, argv[1]);
+  VTY_GET_INTEGER ("keepalive-probes", probes, argv[2]);
+
+  bgp_tcp_keepalive_set(idle, intvl, probes);
+
+  return CMD_SUCCESS;
+}
+
+DEFUN (no_bgp_tcp_keepalive,
+       no_bgp_tcp_keepalive_cmd,
+       "no bgp tcp-keepalive",
+       NO_STR
+       "BGP specific commands\n"
+       "TCP keepalive parameters\n")
+{
+  bgp_tcp_keepalive_unset();
+
+  return CMD_SUCCESS;
+}
+
 DEFUN (bgp_client_to_client_reflection,
        bgp_client_to_client_reflection_cmd,
        "bgp client-to-client reflection",
@@ -10384,6 +10416,10 @@ bgp_vty_init (void)
   /* "bgp config-type" commands. */
   install_element (CONFIG_NODE, &bgp_config_type_cmd);
   install_element (CONFIG_NODE, &no_bgp_config_type_cmd);
+
+  /* "bgp tcp-keepalive" commands */
+  install_element (CONFIG_NODE, &bgp_tcp_keepalive_cmd);
+  install_element (CONFIG_NODE, &no_bgp_tcp_keepalive_cmd);
 
   /* Dummy commands (Currently not supported) */
   install_element (BGP_NODE, &no_synchronization_cmd);

bgpd/bgpd.c
@@ -302,6 +302,23 @@ bgp_timers_unset (struct bgp *bgp)
   return 0;
 }
 
+void
+bgp_tcp_keepalive_set (u_int16_t keepalive_idle, u_int16_t keepalive_intvl,
+                       u_int16_t keepalive_probes)
+{
+  bm->tcp_keepalive_idle = keepalive_idle;
+  bm->tcp_keepalive_intvl = keepalive_intvl;
+  bm->tcp_keepalive_probes = keepalive_probes;
+}
+
+void
+bgp_tcp_keepalive_unset (void)
+{
+  bm->tcp_keepalive_idle = 0;
+  bm->tcp_keepalive_intvl = 0;
+  bm->tcp_keepalive_probes = 0;
+}
+
 /* BGP confederation configuration.  */
 int
 bgp_confederation_id_set (struct bgp *bgp, as_t as)
@@ -5959,6 +5976,14 @@ bgp_config_write (struct vty *vty)
       write++;
     }
 
+  /* TCP keepalive configuration */
+  if (bm->tcp_keepalive_idle)
+    {
+      vty_out (vty, "bgp tcp-keepalive %u %u %u%s", bm->tcp_keepalive_idle,
+               bm->tcp_keepalive_intvl, bm->tcp_keepalive_probes, VTY_NEWLINE);
+      write++;
+    }
+
   /* BGP configuration. */
   for (ALL_LIST_ELEMENTS (bm->bgp, mnode, mnnode, bgp))
     {
@@ -6196,6 +6221,10 @@ bgp_master_init (void)
   bm->port = BGP_PORT_DEFAULT;
   bm->master = thread_master_create ();
   bm->start_time = bgp_clock ();
+
+  bm->tcp_keepalive_idle = 0;
+  bm->tcp_keepalive_intvl = 0;
+  bm->tcp_keepalive_probes = 0;
 
   qzc_init ();
   QZC_NODE_REG(bm, bgp_master)

bgpd/bgpd.h
@@ -75,6 +75,11 @@ struct bgp_master
   /* BGP start time.  */
   time_t start_time;
 
+  /* TCP keepalive parameters for BGP connection */
+  u_int16_t tcp_keepalive_idle;
+  u_int16_t tcp_keepalive_intvl;
+  u_int16_t tcp_keepalive_probes;
+
   /* Various BGP global configuration.  */
   u_char options;
 #define BGP_OPT_NO_FIB                   (1 << 0)
@@ -1037,6 +1042,9 @@ extern int bgp_confederation_peers_remove (struct bgp *, as_t);
 
 extern int bgp_timers_set (struct bgp *, u_int32_t keepalive, u_int32_t holdtime);
 extern int bgp_timers_unset (struct bgp *);
+
+extern void bgp_tcp_keepalive_set (u_int16_t, u_int16_t, u_int16_t);
+extern void bgp_tcp_keepalive_unset (void);
 
 extern int bgp_default_local_preference_set (struct bgp *, u_int32_t);
 extern int bgp_default_local_preference_unset (struct bgp *);

FRRouting/frr

Old-PR: 73323
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
Acked-by: Louis Scalbert <louis.scalbert@6wind.com>
(cherry picked from commit 2a60e2209e0a980d052719fffaecb18535ec4195)
Conflicts:
	bgpd/bgp_vty.c
	bgpd/bgpd.h
Link: https://github.com/FRRouting/frr/pull/9998
PR=76608

bgpd/bgp_network.c
@@ -226,6 +226,24 @@ int bgp_md5_set(struct peer *peer)
 	return bgp_md5_set_password(peer, peer->password);
 }
 
+static void
+bgp_update_setsockopt_tcp_keepalive(struct bgp *bgp, int fd)
+{
+	if (!bgp)
+		return;
+	if (bgp->tcp_keepalive_idle != 0) {
+		int ret;
+
+		ret = setsockopt_tcp_keepalive(fd, bgp->tcp_keepalive_idle,
+					       bgp->tcp_keepalive_intvl,
+					       bgp->tcp_keepalive_probes);
+		if (ret < 0)
+			zlog_err("Can't set TCP keepalive on socket %d, idle %u intvl %u probes %u",
+				 fd, bgp->tcp_keepalive_idle, bgp->tcp_keepalive_intvl,
+				 bgp->tcp_keepalive_probes);
+	}
+}
+
 int bgp_md5_unset(struct peer *peer)
 {
 	/* Unset the password from listen socket. */
@@ -434,6 +452,9 @@ static int bgp_accept(struct thread *thread)
 	}
 
 	bgp_socket_set_buffer_size(bgp_sock);
+
+	/* Set TCP keepalive when TCP keepalive is enabled */
+	bgp_update_setsockopt_tcp_keepalive(bgp, bgp_sock);
 
 	/* Check remote IP address */
 	peer1 = peer_lookup(bgp, &su);
@@ -732,6 +753,9 @@ int bgp_connect(struct peer *peer)
 		sockopt_tcp_mss_set(peer->fd, peer->tcp_mss);
 
 	bgp_socket_set_buffer_size(peer->fd);
+
+	/* Set TCP keepalive when TCP keepalive is enabled */
+	bgp_update_setsockopt_tcp_keepalive(peer->bgp, peer->fd);
 
 	if (bgp_set_socket_ttl(peer, peer->fd) < 0)
 		return -1;

bgpd/bgp_vty.c
@@ -2084,6 +2084,15 @@ void bgp_config_write_coalesce_time(struct vty *vty, struct bgp *bgp)
 		vty_out(vty, " coalesce-time %u\n", bgp->coalesce_time);
 }
 
+/* BGP TCP keepalive */
+static void bgp_config_tcp_keepalive(struct vty *vty, struct bgp *bgp)
+{
+	if (bgp->tcp_keepalive_idle) {
+		vty_out(vty, " bgp tcp-keepalive %u %u %u\n",
+			bgp->tcp_keepalive_idle, bgp->tcp_keepalive_intvl,
+			bgp->tcp_keepalive_probes);
+	}
+}
 
 DEFUN (bgp_coalesce_time,
        bgp_coalesce_time_cmd,
@@ -2273,6 +2282,44 @@ DEFUN (no_bgp_timers,
 	return CMD_SUCCESS;
 }
 
+
+DEFUN (bgp_tcp_keepalive,
+       bgp_tcp_keepalive_cmd,
+       "bgp tcp-keepalive (1-65535) (1-65535) (1-30)",
+       "BGP specific commands\n"
+       "TCP keepalive parameters\n"
+       "TCP keepalive idle time (seconds)\n"
+       "TCP keepalive interval (seconds)\n"
+       "TCP keepalive maximum probes\n")
+{
+	uint32_t idle, intvl, probes;
+	VTY_DECLVAR_CONTEXT(bgp, bgp);
+
+	idle = strtoul(argv[2]->arg, NULL, 10);
+	intvl = strtoul(argv[3]->arg, NULL, 10);
+	probes = strtoul(argv[4]->arg, NULL, 10);
+
+	bgp_tcp_keepalive_set(bgp, (uint16_t)idle, (uint16_t)intvl, (uint16_t)probes);
+
+	return CMD_SUCCESS;
+}
+
+DEFUN (no_bgp_tcp_keepalive,
+       no_bgp_tcp_keepalive_cmd,
+       "no bgp tcp-keepalive (1-65535) (1-65535) (1-30)",
+       NO_STR
+       "BGP specific commands\n"
+       "TCP keepalive parameters\n"
+       "TCP keepalive idle time (seconds)\n"
+       "TCP keepalive interval (seconds)\n"
+       "TCP keepalive maximum probes\n")
+{
+	VTY_DECLVAR_CONTEXT(bgp, bgp);
+
+	bgp_tcp_keepalive_unset(bgp);
+
+	return CMD_SUCCESS;
+}
 
 DEFUN (bgp_client_to_client_reflection,
        bgp_client_to_client_reflection_cmd,
@@ -16647,6 +16694,9 @@ int bgp_config_write(struct vty *vty)
 			if (CHECK_FLAG(bgp->flags, BGP_FLAG_GRACEFUL_SHUTDOWN))
 				vty_out(vty, " bgp graceful-shutdown\n");
 
+		/* BGP TCP keepalive */
+		bgp_config_tcp_keepalive(vty, bgp);
+
 		/* BGP graceful-restart. */
 		if (bgp->stalepath_time != BGP_DEFAULT_STALEPATH_TIME)
 			vty_out(vty,
@@ -18285,6 +18335,10 @@ void bgp_vty_init(void)
 	/* ttl_security commands */
 	install_element(BGP_NODE, &neighbor_ttl_security_cmd);
 	install_element(BGP_NODE, &no_neighbor_ttl_security_cmd);
+
+	/* "bgp tcp-keepalive" commands */
+	install_element(BGP_NODE, &bgp_tcp_keepalive_cmd);
+	install_element(BGP_NODE, &no_bgp_tcp_keepalive_cmd);
 
 	/* "show [ip] bgp memory" commands. */
 	install_element(VIEW_NODE, &show_bgp_memory_cmd);

bgpd/bgpd.c
@@ -547,6 +547,23 @@ void bgp_timers_unset(struct bgp *bgp)
 	bgp->default_delayopen = BGP_DEFAULT_DELAYOPEN;
 }
 
+void
+bgp_tcp_keepalive_set(struct bgp *bgp, uint16_t keepalive_idle,
+		      uint16_t keepalive_intvl, uint16_t keepalive_probes)
+{
+	bgp->tcp_keepalive_idle = keepalive_idle;
+	bgp->tcp_keepalive_intvl = keepalive_intvl;
+	bgp->tcp_keepalive_probes = keepalive_probes;
+}
+
+void
+bgp_tcp_keepalive_unset(struct bgp *bgp)
+{
+	bgp->tcp_keepalive_idle = 0;
+	bgp->tcp_keepalive_intvl = 0;
+	bgp->tcp_keepalive_probes = 0;
+}
+
 /* BGP confederation configuration.  */
 void bgp_confederation_id_set(struct bgp *bgp, as_t as)
 {
@@ -3174,6 +3191,7 @@ static struct bgp *bgp_create(as_t *as, const char *name,
 	bgp->default_local_pref = BGP_DEFAULT_LOCAL_PREF;
 	bgp->default_subgroup_pkt_queue_max =
 		BGP_DEFAULT_SUBGROUP_PKT_QUEUE_MAX;
+	bgp_tcp_keepalive_unset(bgp);
 	bgp_timers_unset(bgp);
 	bgp->restart_time = BGP_DEFAULT_RESTART_TIME;
 	bgp->stalepath_time = BGP_DEFAULT_STALEPATH_TIME;

bgpd/bgpd.h
@@ -141,6 +141,7 @@ struct bgp_master {
 
 	/* Various BGP global configuration.  */
 	uint8_t options;
+
 #define BGP_OPT_NO_FIB                   (1 << 0)
 #define BGP_OPT_NO_LISTEN                (1 << 1)
 #define BGP_OPT_NO_ZEBRA                 (1 << 2)
@@ -716,6 +717,11 @@ struct bgp {
 	/* BGP Conditional advertisement */
 	uint32_t condition_filter_count;
 	struct thread *t_condition_check;
+
+	/* TCP keepalive parameters for BGP connection */
+	uint16_t tcp_keepalive_idle;
+	uint16_t tcp_keepalive_intvl;
+	uint16_t tcp_keepalive_probes;
 
 	QOBJ_FIELDS;
 };
@@ -2070,6 +2076,9 @@ extern int peer_default_originate_set(struct peer *peer, afi_t afi, safi_t safi,
 				      const char *rmap,
 				      struct route_map *route_map);
 extern int peer_default_originate_unset(struct peer *, afi_t, safi_t);
+extern void bgp_tcp_keepalive_set(struct bgp *bgp, uint16_t idle,
+				  uint16_t interval, uint16_t probes);
+extern void bgp_tcp_keepalive_unset(struct bgp *bgp);
 
 extern void peer_port_set(struct peer *, uint16_t);
 extern void peer_port_unset(struct peer *);