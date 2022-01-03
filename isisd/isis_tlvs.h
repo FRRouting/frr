a007deeccb9f96585dd90f079a3acd3910824e5d
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-01-03 22:53:52 +0000

    isisd: refactor enum tail comma
    Many of the enum definitions defined in isis_tlvs.h
    are often extended at the end. The c/c++ allows
    commas at the end of a list. This commit simplifies
    the patching of later extensions.
    
    Signed-off-by: Hiroki Shirokura <hiroki.shirokura@linecorp.com>
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- isisd/isis_tlvs.h
+++ isisd/isis_tlvs.h
@@ -118,7 +118,7 @@ struct isis_spine_leaf {
 enum isis_threeway_state {
 	ISIS_THREEWAY_DOWN = 2,
 	ISIS_THREEWAY_INITIALIZING = 1,
-	ISIS_THREEWAY_UP = 0
+	ISIS_THREEWAY_UP = 0,
 };
 
 struct isis_threeway_adj {
@@ -325,7 +325,7 @@ enum isis_tlv_context {
 	ISIS_CONTEXT_SUBTLV_NE_REACH,
 	ISIS_CONTEXT_SUBTLV_IP_REACH,
 	ISIS_CONTEXT_SUBTLV_IPV6_REACH,
-	ISIS_CONTEXT_MAX
+	ISIS_CONTEXT_MAX,
 };
 
 struct isis_subtlvs {
@@ -410,7 +410,7 @@ enum isis_tlv_type {
 	ISIS_SUBTLV_AVA_BW = 38,
 	ISIS_SUBTLV_USE_BW = 39,
 
-	ISIS_SUBTLV_MAX = 40
+	ISIS_SUBTLV_MAX = 40,
 };
 
 /* subTLVs size for TE and SR */
@@ -441,7 +441,7 @@ enum ext_subtlv_size {
 	ISIS_SUBTLV_HDR_SIZE = 2,
 	ISIS_SUBTLV_DEF_SIZE = 4,
 
-	ISIS_SUBTLV_MAX_SIZE = 180
+	ISIS_SUBTLV_MAX_SIZE = 180,
 };
 
 /* Macros to manage the optional presence of EXT subTLVs */
