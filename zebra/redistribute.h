183a1a9d749d392d7ed2ebccc3f98a68e65a2d6d
Donald Sharp <sharpd@nvidia.com>
2022-01-14 11:30:31 -0500

    zebra: Fix redistribute.h up to our standards
    FRR must give variable names instead of not defining
    them in the .h file.  This just cleans up this
    problem for redistribute.h
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/redistribute.h
+++ zebra/redistribute.h
@@ -55,20 +55,20 @@ void redistribute_delete(const struct route_node *rn,
 			 const struct route_entry *old_re,
 			 const struct route_entry *new_re);
 
-extern void zebra_interface_up_update(struct interface *);
-extern void zebra_interface_down_update(struct interface *);
+extern void zebra_interface_up_update(struct interface *ifp);
+extern void zebra_interface_down_update(struct interface *ifp);
 
-extern void zebra_interface_add_update(struct interface *);
-extern void zebra_interface_delete_update(struct interface *);
+extern void zebra_interface_add_update(struct interface *ifp);
+extern void zebra_interface_delete_update(struct interface *ifp);
 
-extern void zebra_interface_address_add_update(struct interface *,
-					       struct connected *);
-extern void zebra_interface_address_delete_update(struct interface *,
+extern void zebra_interface_address_add_update(struct interface *ifp,
+					       struct connected *c);
+extern void zebra_interface_address_delete_update(struct interface *ifp,
 						  struct connected *c);
-extern void zebra_interface_parameters_update(struct interface *);
-extern void zebra_interface_vrf_update_del(struct interface *,
+extern void zebra_interface_parameters_update(struct interface *ifp);
+extern void zebra_interface_vrf_update_del(struct interface *ifp,
 					   vrf_id_t new_vrf_id);
-extern void zebra_interface_vrf_update_add(struct interface *,
+extern void zebra_interface_vrf_update_add(struct interface *ifp,
 					   vrf_id_t old_vrf_id);
 
 extern int zebra_import_table(afi_t afi, vrf_id_t vrf_id,
