a9338fa4490627df2263649de6dcec0774baad8e
David Lamparter <equinox@opensourcerouting.org>
2022-01-18 14:33:04 +0100

    pim6d: IPv6-adjust mroute code
    This is just hitting the pim_mroute code with a hammer until it doesn't
    print warnings anymore.  This is NOT quite tested or working yet, it
    just compiles.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- pimd/pim_upstream.c
+++ pimd/pim_upstream.c
@@ -1997,7 +1997,7 @@ static bool pim_upstream_kat_start_ok(struct pim_upstream *up)
 		return false;
 
 	pim_ifp = ifp->info;
-	if (pim_ifp->mroute_vif_index != c_oil->oil.mfcc_parent)
+	if (pim_ifp->mroute_vif_index != *oil_parent(c_oil))
 		return false;
 
 	if (pim_if_connected_to_source(up->rpf.source_nexthop.interface,
