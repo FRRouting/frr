9a7ea213c072a24aa2059e04cb51502a3e956705
Stephen Worley <sworley@nvidia.com>
2022-01-27 12:41:49 -0500

    pbrd: pbr route maps get addr family of nhgs
    When adding a nhg to a route map, make sure to specify the `family`
    of the rm by looking at the contents of the nhg. Installation in the
    kernel (for DSCP rules in particular) relies on this being specified in
    the netlink message.
    
    Signed-off-by: Wesley Coakley <wcoakley@nvidia.com>
    Signed-off-by: Stephen Worley <sworley@nvidia.com>

--- pbrd/pbr_vty.c
+++ pbrd/pbr_vty.c
@@ -506,7 +506,8 @@ DEFPY(pbr_map_nexthop_group, pbr_map_nexthop_group_cmd,
 	/* This is new/replacement config */
 	pbrms_clear_set_config(pbrms);
 
-	pbrms->nhgrp_name = XSTRDUP(MTYPE_TMP, name);
+	pbr_nht_set_seq_nhg(pbrms, name);
+
 	pbr_map_check(pbrms, true);
 
 	return CMD_SUCCESS;
