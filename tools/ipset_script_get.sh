f3751bf914d02d2d549c68f2327f12ded883dcd8
Louis Scalbert <louis.scalbert@6wind.com>
2022-02-01 12:36:33 +0100

    tools: ipset wrap script lookup in the correct netns
    FRR used to run in netns mode. There was a unique FRR instance that
    managed all the netns. One unique instance knows all the netns.
    
    Since the implementation of L3VRF in yams, L3VRF and netns can co-exist.
    L3VRFs are running within their own netns. One instance of FRR runs per
    netns. All of them know their L3VRFs.
    
    When FRR runs in a netns, the vtysh command "show pbr ipset" uses the
    wrap script tools/ipset_script_get.sh to get stats from each ipset. The
    issue is that the script look for the ipset in the main netns.
    
    Look for the ipset in the correct netns.
    
    PR=77631
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
    Acked-by: Philippe Guibert <philippe.guibert@6wind.com>

--- tools/ipset_script_get.sh
+++ tools/ipset_script_get.sh
@@ -2,7 +2,21 @@
 #input : 'ipset_script_get.sh --list <NAME>'
 #description: call 'fpcmd nf-ipset <NAME>'
 #output: similar output as with netfilter ipset --list <NAME> command
+
+netns=$(ip netns identify $$)
+if [ "$netns" == "" ]
+then
+	if command -v vrfctl >/dev/null 2>&1
+	then
+		netns_id=$(vrfctl list vrfname $netns | awk -F'vrf| |\t' '{print $2}')
+	else
+		echo Cannot get Netns ID: vrfctl not found. >&2
+	fi
+else
+	netns_id=0
+fi
+
 if [ $# -eq 2 ]
 then
-    fpcmd nf-ipset $2
+    fp-cli vrf-exec $netns_id nf-ipset $2
 fi
