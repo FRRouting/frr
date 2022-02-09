626dab7f3fc54843b34dffe6588dcf2a3b8263f5
Abhishek N R <abnr@vmware.com>
2022-02-08 20:32:14 -0800

    pim6d: Implementing "show ipv6 pim secondary" CLI
    Adding new show CLI to display pim secondary addresses.
    
    Signed-off-by: Abhishek N R <abnr@vmware.com>

--- pimd/pim6_cmd.c
+++ pimd/pim6_cmd.c
@@ -782,6 +782,35 @@ DEFPY (show_ipv6_pim_rpf_vrf_all,
 	return CMD_SUCCESS;
 }
 
+DEFPY (show_ipv6_pim_secondary,
+       show_ipv6_pim_secondary_cmd,
+       "show ipv6 pim [vrf NAME] secondary",
+       SHOW_STR
+       IPV6_STR
+       PIM_STR
+       VRF_CMD_HELP_STR
+       "PIM neighbor addresses\n")
+{
+	struct pim_instance *pim;
+	struct vrf *v;
+
+	v = vrf_lookup_by_name(vrf ? vrf : VRF_DEFAULT_NAME);
+
+	if (!v)
+		return CMD_WARNING;
+
+	pim = pim_get_pim_instance(v->vrf_id);
+
+	if (!pim) {
+		vty_out(vty, "%% Unable to find pim instance\n");
+		return CMD_WARNING;
+	}
+
+	pim_show_neighbors_secondary(pim, vty);
+
+	return CMD_SUCCESS;
+}
+
 void pim_cmd_init(void)
 {
 	if_cmd_init(pim_interface_config_write);
@@ -838,4 +867,5 @@ void pim_cmd_init(void)
 	install_element(VIEW_NODE, &show_ipv6_pim_rp_vrf_all_cmd);
 	install_element(VIEW_NODE, &show_ipv6_pim_rpf_cmd);
 	install_element(VIEW_NODE, &show_ipv6_pim_rpf_vrf_all_cmd);
+	install_element(VIEW_NODE, &show_ipv6_pim_secondary_cmd);
 }
