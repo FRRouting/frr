416df55976413759158c103b97697e6153986f8e
Rafael Zalamena <rzalamena@opensourcerouting.org>
2022-02-10 16:20:42 -0300

    lib,vtysh: show operational data with config
    Add option to merge configuration data in the operational data show
    command.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -2955,6 +2955,7 @@ DEFUN (show_yang_operational_data,
          [{\
 	   format <json|xml>\
 	   |translate WORD\
+	   |with-config\
 	 }]" DAEMONS_LIST,
        SHOW_STR
        "YANG information\n"
@@ -2965,6 +2966,7 @@ DEFUN (show_yang_operational_data,
        "Extensible Markup Language\n"
        "Translate operational data\n"
        "YANG module translator\n"
+       "Merge configuration data\n"
        DAEMONS_STR)
 {
 	return show_one_daemon(vty, argv, argc - 1, argv[argc - 1]->text);
