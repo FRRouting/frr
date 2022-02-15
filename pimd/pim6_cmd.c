b1a419baef33492752823f35fe8bdb0e68df151d
Abhishek N R <abnr@vmware.com>
2022-02-14 23:42:40 -0800

    pim6d: Implementing "show ipv6 pim upstream-rpf" CLI.
    Adding new show CLI to display pim RPF for upstreams.
    
    Signed-off-by: Abhishek N R <abnr@vmware.com>

--- pimd/pim6_cmd.c
+++ pimd/pim6_cmd.c
@@ -963,6 +963,37 @@ DEFPY (show_ipv6_pim_upstream_join_desired,
 	return CMD_SUCCESS;
 }
 
+DEFPY (show_ipv6_pim_upstream_rpf,
+       show_ipv6_pim_upstream_rpf_cmd,
+       "show ipv6 pim [vrf NAME] upstream-rpf [json$json]",
+       SHOW_STR
+       IPV6_STR
+       PIM_STR
+       VRF_CMD_HELP_STR
+       "PIM upstream source rpf\n"
+       JSON_STR)
+{
+	struct pim_instance *pim;
+	struct vrf *v;
+	bool uj = !!json;
+
+	v = vrf_lookup_by_name(vrf ? vrf : VRF_DEFAULT_NAME);
+
+	if (!v)
+		return CMD_WARNING;
+
+	pim = pim_get_pim_instance(v->vrf_id);
+
+	if (!pim) {
+		vty_out(vty, "%% Unable to find pim instance\n");
+		return CMD_WARNING;
+	}
+
+	pim_show_upstream_rpf(pim, vty, uj);
+
+	return CMD_SUCCESS;
+}
+
 void pim_cmd_init(void)
 {
 	if_cmd_init(pim_interface_config_write);
@@ -1024,4 +1055,5 @@ void pim_cmd_init(void)
 	install_element(VIEW_NODE, &show_ipv6_pim_upstream_cmd);
 	install_element(VIEW_NODE, &show_ipv6_pim_upstream_vrf_all_cmd);
 	install_element(VIEW_NODE, &show_ipv6_pim_upstream_join_desired_cmd);
+	install_element(VIEW_NODE, &show_ipv6_pim_upstream_rpf_cmd);
 }
--- pimd/pim_cmd.c
+++ pimd/pim_cmd.c
@@ -4540,9 +4540,9 @@ DEFPY (show_ip_pim_upstream_join_desired,
 	return CMD_SUCCESS;
 }
 
-DEFUN (show_ip_pim_upstream_rpf,
+DEFPY (show_ip_pim_upstream_rpf,
        show_ip_pim_upstream_rpf_cmd,
-       "show ip pim [vrf NAME] upstream-rpf [json]",
+       "show ip pim [vrf NAME] upstream-rpf [json$json]",
        SHOW_STR
        IP_STR
        PIM_STR
@@ -4550,14 +4550,23 @@ DEFUN (show_ip_pim_upstream_rpf,
        "PIM upstream source rpf\n"
        JSON_STR)
 {
-	int idx = 2;
-	struct vrf *vrf = pim_cmd_lookup_vrf(vty, argv, argc, &idx);
-	bool uj = use_json(argc, argv);
+	struct pim_instance *pim;
+	struct vrf *v;
+	bool uj = !!json;
 
-	if (!vrf)
+	v = vrf_lookup_by_name(vrf ? vrf : VRF_DEFAULT_NAME);
+
+	if (!v)
 		return CMD_WARNING;
 
-	pim_show_upstream_rpf(vrf->info, vty, uj);
+	pim = pim_get_pim_instance(v->vrf_id);
+
+	if (!pim) {
+		vty_out(vty, "%% Unable to find pim instance\n");
+		return CMD_WARNING;
+	}
+
+	pim_show_upstream_rpf(pim, vty, uj);
 
 	return CMD_SUCCESS;
 }
