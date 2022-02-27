3d128ccba88e8a84fe7d340fc8c3f39ad53bb176
David Lamparter <equinox@opensourcerouting.org>
2022-02-27 19:32:20 +0100

    zebra: use zlog_blk in route_entry_dump()
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- zebra/rib.h
+++ zebra/rib.h
@@ -346,8 +346,9 @@ int route_entry_update_nhe(struct route_entry *re,
 void rib_handle_nhg_replace(struct nhg_hash_entry *old_entry,
 			    struct nhg_hash_entry *new_entry);
 
-#define route_entry_dump(prefix, src, re) _route_entry_dump(__func__, prefix, src, re)
-extern void _route_entry_dump(const char *func, union prefixconstptr pp,
+#define route_entry_dump(prefix, src, re)                                      \
+	ZLOG_CALL_LOGBLK(_route_entry_dump, prefix, src, re)
+extern void _route_entry_dump(struct zlog_blk *blk, union prefixconstptr pp,
 			      union prefixconstptr src_pp,
 			      const struct route_entry *re);
 
