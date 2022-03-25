ce67e8fd10f0250c187bab529808457c74fc4b0c
Donald Sharp <sharpd@nvidia.com>
2022-03-24 20:02:33 -0400

    zebra: Fix use after deletion event in freebsd
    In the FreeBSD code if you delete the interface
    and it has no configuration, the ifp pointer will
    be deleted from the system *but* zebra continues
    to dereference the just freed pointer.
    
    ==58624== Invalid read of size 1
    ==58624==    at 0x48539F3: strlcpy (in /usr/local/libexec/valgrind/vgpreload_memcheck-amd64-freebsd.so)
    ==58624==    by 0x2B0565: ifreq_set_name (ioctl.c:48)
    ==58624==    by 0x2B0565: if_get_flags (ioctl.c:416)
    ==58624==    by 0x2B2D9E: ifan_read (kernel_socket.c:455)
    ==58624==    by 0x2B2D9E: kernel_read (kernel_socket.c:1403)
    ==58624==    by 0x499F46E: thread_call (thread.c:2002)
    ==58624==    by 0x495D2B7: frr_run (libfrr.c:1196)
    ==58624==    by 0x2B40B8: main (main.c:471)
    ==58624==  Address 0x6baa7f0 is 64 bytes inside a block of size 432 free'd
    ==58624==    at 0x484ECDC: free (in /usr/local/libexec/valgrind/vgpreload_memcheck-amd64-freebsd.so)
    ==58624==    by 0x4953A64: if_delete (if.c:283)
    ==58624==    by 0x2A93C1: if_delete_update (interface.c:874)
    ==58624==    by 0x2B2DF3: ifan_read (kernel_socket.c:453)
    ==58624==    by 0x2B2DF3: kernel_read (kernel_socket.c:1403)
    ==58624==    by 0x499F46E: thread_call (thread.c:2002)
    ==58624==    by 0x495D2B7: frr_run (libfrr.c:1196)
    ==58624==    by 0x2B40B8: main (main.c:471)
    ==58624==  Block was alloc'd at
    ==58624==    at 0x4851381: calloc (in /usr/local/libexec/valgrind/vgpreload_memcheck-amd64-freebsd.so)
    ==58624==    by 0x496A022: qcalloc (memory.c:116)
    ==58624==    by 0x49546BC: if_new (if.c:164)
    ==58624==    by 0x49546BC: if_create_name (if.c:218)
    ==58624==    by 0x49546BC: if_get_by_name (if.c:603)
    ==58624==    by 0x2B1295: ifm_read (kernel_socket.c:628)
    ==58624==    by 0x2A7FB6: interface_list (if_sysctl.c:129)
    ==58624==    by 0x2E99C8: zebra_ns_enable (zebra_ns.c:127)
    ==58624==    by 0x2E99C8: zebra_ns_init (zebra_ns.c:214)
    ==58624==    by 0x2B3FF2: main (main.c:401)
    ==58624==
    
    Zebra needs to pass back whether or not the ifp pointer
    was freed when if_delete_update is called and it should
    then check in ifan_read as well as ifm_read that the
    ifp pointer is still valid for use.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    (cherry picked from commit d0438da6b09333d2b77a9eac2e9fffbbae6e603b)

--- zebra/interface.h
+++ zebra/interface.h
@@ -474,7 +474,7 @@ extern void if_nbr_ipv6ll_to_ipv4ll_neigh_update(struct interface *ifp,
 						 struct in6_addr *address,
 						 int add);
 extern void if_nbr_ipv6ll_to_ipv4ll_neigh_del_all(struct interface *ifp);
-extern void if_delete_update(struct interface *ifp);
+extern void if_delete_update(struct interface **ifp);
 extern void if_add_update(struct interface *ifp);
 extern void if_up(struct interface *);
 extern void if_down(struct interface *);
