6fee2752ca7fc3d19b86775aacc1ce524eeb64b2
Louis Scalbert <louis.scalbert@6wind.com>
2022-04-06 18:40:32 +0200

    isisd: remove previous labels before fast-reroute
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
    (cherry picked from commit fed5e101bb218b2eeb8461cd5632f3f1cb48fee6)
    
    PR=78288
    Link: https://github.com/FRRouting/frr/pull/10962
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
    Acked-by: Philippe Guibert <philippe.guibert@6wind.com>

--- isisd/isis_route.c
+++ isisd/isis_route.c
@@ -459,6 +459,21 @@ void isis_route_delete(struct isis_area *area, struct route_node *rode,
 	route_unlock_node(rode);
 }
 
+static void isis_route_remove_previous_sid(struct isis_area *area,
+					   struct prefix *prefix,
+					   struct isis_route_info *route_info)
+{
+	/*
+	 * Explicitly uninstall previous Prefix-SID label if it has
+	 * changed or was removed.
+	 */
+	if (route_info->sr_previous.present &&
+	    (!route_info->sr.present ||
+	     route_info->sr_previous.label != route_info->sr.label))
+		isis_zebra_prefix_sid_uninstall(area, prefix, route_info,
+						&route_info->sr_previous);
+}
+
 static void isis_route_update(struct isis_area *area, struct prefix *prefix,
 			      struct prefix_ipv6 *src_p,
 			      struct isis_route_info *route_info)
@@ -467,17 +482,7 @@ static void isis_route_update(struct isis_area *area, struct prefix *prefix,
 		if (CHECK_FLAG(route_info->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED))
 			return;
 
-		/*
-		 * Explicitly uninstall previous Prefix-SID label if it has
-		 * changed or was removed.
-		 */
-		if (route_info->sr_previous.present
-		    && (!route_info->sr.present
-			|| route_info->sr_previous.label
-				   != route_info->sr.label))
-			isis_zebra_prefix_sid_uninstall(
-				area, prefix, route_info,
-				&route_info->sr_previous);
+		isis_route_remove_previous_sid(area, prefix, route_info);
 
 		/* Install route. */
 		isis_zebra_route_add_route(area->isis, prefix, src_p,
@@ -766,6 +771,7 @@ void isis_route_switchover_nexthop(struct isis_area *area,
 				       (const struct prefix **)&src_p);
 
 		/* Switchover route. */
+		isis_route_remove_previous_sid(area, prefix, rinfo);
 		UNSET_FLAG(rinfo->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED);
 		isis_route_update(area, prefix, src_p, rinfo->backup);
 
