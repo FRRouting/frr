b17d5444f46c55fb131de4e0ab260bb1bd7df93d
Donatas Abraitis <donatas@opensourcerouting.org>
2022-04-13 20:40:30 +0300

    bgpd: Pass non-transitive ext-communities between Route Server and RS clients
    https://datatracker.ietf.org/doc/html/rfc7947#section-2.2
    
    Optional recognized and unrecognized BGP attributes,
       whether transitive or non-transitive, SHOULD NOT be updated by the
       route server (unless enforced by local IXP operator configuration)
       and SHOULD be passed on to other route server clients.
    
    By default LB ext-community works with iBGP peers. When we receive a route
    from eBGP peer, we can send LB ext-community to iBGP peers.
    
    With this patch, allow sending LB ext-community to iBGP/eBGP peers if they
    are set as RS clients.
    
    FRR does not send non-transitive ext-communities to eBGP peers, but for
    example GoBGP sends and if it's set as RS client, we should pass those attributes
    towards another RS client.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -4177,9 +4177,14 @@ bgp_size_t bgp_packet_attribute(struct bgp *bgp, struct peer *peer,
 	if (CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_SEND_EXT_COMMUNITY)
 	    && (attr->flag & ATTR_FLAG_BIT(BGP_ATTR_EXT_COMMUNITIES))) {
 		struct ecommunity *ecomm = bgp_attr_get_ecommunity(attr);
+		bool transparent = CHECK_FLAG(peer->af_flags[afi][safi],
+					      PEER_FLAG_RSERVER_CLIENT) &&
+				   from &&
+				   CHECK_FLAG(from->af_flags[afi][safi],
+					      PEER_FLAG_RSERVER_CLIENT);
 
-		if (peer->sort == BGP_PEER_IBGP
-		    || peer->sort == BGP_PEER_CONFED) {
+		if (peer->sort == BGP_PEER_IBGP ||
+		    peer->sort == BGP_PEER_CONFED || transparent) {
 			if (ecomm->size * 8 > 255) {
 				stream_putc(s,
 					    BGP_ATTR_FLAG_OPTIONAL
