d565b673970b3081e1a27c8a58122daa78f29ddc
Louis Scalbert <louis.scalbert@6wind.com>
2022-04-22 18:08:08 +0200

    lib: add a function to get the VRF or loopback interface
    Add a function to find the VRF or the loopback interface: the loopback
    interface for the default VRF and the VRF master interface otherwise.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- lib/if.c
+++ lib/if.c
@@ -565,6 +565,20 @@ size_t if_lookup_by_hwaddr(const uint8_t *hw_addr, size_t addrsz,
 	return count;
 }
 
+/* Get the VRF loopback interface, i.e. the loopback on the default VRF
+ * or the VRF interface.
+ */
+struct interface *if_get_vrf_loopback(vrf_id_t vrf_id)
+{
+	struct interface *ifp = NULL;
+	struct vrf *vrf = vrf_lookup_by_id(vrf_id);
+
+	FOR_ALL_INTERFACES (vrf, ifp)
+		if (if_is_loopback(ifp))
+			return ifp;
+
+	return NULL;
+}
 
 /* Get interface by name if given name interface doesn't exist create
    one. */
