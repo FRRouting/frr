175a6dcd9e5cf14edd3408a04765fed34029219d
David Lamparter <equinox@opensourcerouting.org>
2022-04-25 13:53:17 +0200

    pimd: fix reversed logic for "debug ... detail"
    The normal flag was enabling detail messages, but what we want is the
    detail flag enabling normal messages.
    
    Remove the _ONLY macro while at it, it's only used for config print &
    that seems like a place where making the difference explicitly visible
    is helpful regardless.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- pimd/pim_vty.c
+++ pimd/pim_vty.c
@@ -70,18 +70,18 @@ int pim_debug_config_write(struct vty *vty)
 		++writes;
 	}
 
-	if (PIM_DEBUG_MROUTE) {
+	/* PIM_DEBUG_MROUTE catches _DETAIL too */
+	if (router->debugs & PIM_MASK_MROUTE) {
 		vty_out(vty, "debug mroute\n");
+		++writes;
+	}
+	if (PIM_DEBUG_MROUTE_DETAIL) {
+		vty_out(vty, "debug mroute detail\n");
 		++writes;
 	}
 
 	if (PIM_DEBUG_MTRACE) {
 		vty_out(vty, "debug mtrace\n");
-		++writes;
-	}
-
-	if (PIM_DEBUG_MROUTE_DETAIL_ONLY) {
-		vty_out(vty, "debug mroute detail\n");
 		++writes;
 	}
 
@@ -102,11 +102,12 @@ int pim_debug_config_write(struct vty *vty)
 		++writes;
 	}
 
-	if (PIM_DEBUG_PIM_TRACE) {
+	/* PIM_DEBUG_PIM_TRACE catches _DETAIL too */
+	if (router->debugs & PIM_MASK_PIM_TRACE) {
 		vty_out(vty, "debug pim trace\n");
 		++writes;
 	}
-	if (PIM_DEBUG_PIM_TRACE_DETAIL_ONLY) {
+	if (PIM_DEBUG_PIM_TRACE_DETAIL) {
 		vty_out(vty, "debug pim trace detail\n");
 		++writes;
 	}
