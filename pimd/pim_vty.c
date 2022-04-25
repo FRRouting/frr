2c57f5612b1bf6d4b7b6242cddf253cd53ccb245
David Lamparter <equinox@opensourcerouting.org>
2022-04-25 14:01:42 +0200

    pimd: add missing `debug igmp trace detail`
    The flag was defined but there was no command to actually enable it.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- pimd/pim_vty.c
+++ pimd/pim_vty.c
@@ -65,8 +65,13 @@ int pim_debug_config_write(struct vty *vty)
 		vty_out(vty, "debug igmp packets\n");
 		++writes;
 	}
-	if (PIM_DEBUG_IGMP_TRACE) {
+	/* PIM_DEBUG_IGMP_TRACE catches _DETAIL too */
+	if (router->debugs & PIM_MASK_IGMP_TRACE) {
 		vty_out(vty, "debug igmp trace\n");
+		++writes;
+	}
+	if (PIM_DEBUG_IGMP_TRACE_DETAIL) {
+		vty_out(vty, "debug igmp trace detail\n");
 		++writes;
 	}
 
