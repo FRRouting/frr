122c31403efb53087da4ad7a807b21c86f3a09a9
sarita patra <saritap@vmware.com>
2022-04-27 01:23:31 -0700

    pim6d: Adjust rpf_addr in pim_vxlan_orig_mr_up_add
    In this PR, we are handling the pim_vxlan_orig_mr_up_add()
    after rpf_addr modified from prefix to pim_addr.
    
    Signed-off-by: sarita patra <saritap@vmware.com>

--- pimd/pim_vxlan.c
+++ pimd/pim_vxlan.c
@@ -303,7 +303,6 @@ static void pim_vxlan_orig_mr_up_add(struct pim_vxlan_sg *vxlan_sg)
 	struct pim_upstream *up;
 	struct pim_interface *term_ifp;
 	int flags = 0;
-	struct prefix nht_p;
 	struct pim_instance *pim = vxlan_sg->pim;
 
 	if (vxlan_sg->up) {
@@ -353,9 +352,8 @@ static void pim_vxlan_orig_mr_up_add(struct pim_vxlan_sg *vxlan_sg)
 		 * iif
 		 */
 		if (!PIM_UPSTREAM_FLAG_TEST_STATIC_IIF(up->flags)) {
-			pim_addr_to_prefix(&nht_p, up->upstream_addr);
-			pim_delete_tracked_nexthop(vxlan_sg->pim, &nht_p, up,
-						   NULL);
+			pim_delete_tracked_nexthop(
+				vxlan_sg->pim, &up->upstream_addr, up, NULL);
 		}
 		/* We are acting FHR; clear out use_rpt setting if any */
 		pim_upstream_update_use_rpt(up, false /*update_mroute*/);
