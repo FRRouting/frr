2e2b72c061c5ff1d69023240a1adf4c6d13fa481
Louis Scalbert <louis.scalbert@6wind.com>
2022-04-29 19:41:57 +0200

    bgpd: move mp_nexthop_prefer_global boolean attribute to nh_flag
    Previous commits have introduced a new 8 bits nh_flag in the attr
    struct that has increased the memory footprint.
    
    Move the mp_nexthop_prefer_global boolean in the attr structure that
    takes 8 bits to the new nh_flag in order to go back to the previous
    memory utilization.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_snmp_bgp4v2.c
+++ bgpd/bgp_snmp_bgp4v2.c
@@ -704,7 +704,8 @@ static uint8_t *bgp4v2PathAttrTable(struct variable *v, oid name[],
 		case BGP_ATTR_NHLEN_IPV6_GLOBAL:
 			return SNMP_INTEGER(2);
 		case BGP_ATTR_NHLEN_IPV6_GLOBAL_AND_LL:
-			if (path->attr->mp_nexthop_prefer_global)
+			if (CHECK_FLAG(path->attr->nh_flag,
+				       BGP_ATTR_NH_MP_PREFER_GLOBAL))
 				return SNMP_INTEGER(2);
 			else
 				return SNMP_INTEGER(4);
@@ -718,7 +719,8 @@ static uint8_t *bgp4v2PathAttrTable(struct variable *v, oid name[],
 		case BGP_ATTR_NHLEN_IPV6_GLOBAL:
 			return SNMP_IP6ADDRESS(path->attr->mp_nexthop_global);
 		case BGP_ATTR_NHLEN_IPV6_GLOBAL_AND_LL:
-			if (path->attr->mp_nexthop_prefer_global)
+			if (CHECK_FLAG(path->attr->nh_flag,
+				       BGP_ATTR_NH_MP_PREFER_GLOBAL))
 				return SNMP_IP6ADDRESS(
 					path->attr->mp_nexthop_global);
 			else
