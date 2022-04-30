3806449f40dadaf313fcc01ff216c6042eaba953
Bruno Bernard <contact.brunobernard@gmail.com>
2022-04-30 23:36:54 +0400

    fix directory topotato

--- tests/topotato/topotato/doc/mkdocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-: "${PYTHON:=python3}"
-
-cd "`dirname $0`"
-"${PYTHON}" -m sphinx -a -q -b text -d "_build/.doctrees" "." "_build/text"
-"${PYTHON}" -m sphinx -q -b html -d "_build/.doctrees" "." "_build/html"
--- tests/topotato/topotato/doc/utils.rst
+++
@@ -1,5 +0,0 @@
-Utilities
-=========
-
-.. automodule:: topotato.utils
-   :members:
