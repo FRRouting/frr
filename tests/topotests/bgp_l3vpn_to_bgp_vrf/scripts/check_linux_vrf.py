a1d9f6f2f2d2db5980f7b11e3ac2be9f68f4b063
Louis Scalbert <louis.scalbert@6wind.com>
2022-05-12 14:57:17 +0200

    topotests: add VRF leak tests in bgp_l3vpn_to_bgp_vrf
    Check that route leaking between VRF within a router works properly.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- tests/topotests/bgp_l3vpn_to_bgp_vrf/scripts/check_linux_vrf.py
+++ tests/topotests/bgp_l3vpn_to_bgp_vrf/scripts/check_linux_vrf.py
@@ -72,3 +72,46 @@ luCommand(
     "wait",
     "CE4->PE4 ping",
 )
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 192.168.1.1 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "pass",
+    "R1(r1-cust5)->R1(r1-cust1 - r1-eth4) ping",
+)
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 192.168.1.2 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "wait",
+    "R1(r1-cust5)->CE1 ping",
+)
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 192.168.1.2 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "pass",
+    "R1(r1-cust5)->CE1 ping",
+)
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 99.0.0.1 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "pass",
+    "R1(r1-cust5)->CE1 (loopback) ping",
+)
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 5.1.0.1 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "wait",
+    "R1(r1-cust5)->CE1 (loopback) ping",
+    time=30,
+)
+luCommand(
+    "r1",
+    "ip vrf exec r1-cust5 ping 5.1.0.1 -I 29.0.0.1 -c 1",
+    " 0. packet loss",
+    "pass",
+    "R1(r1-cust5)->CE1 (loopback) ping",
+)
