FRRouting/frr

commit 636f76088d1148711f3cdbffd2b1c807b773d664

bgpd: router-id change reflect to vpn auto rd rt
VRF route leak auto RD and RT uses router-id,
when a router-id changes for a bgp instance, change
associated vpn RD and RT values. Withdraw
old RD/RT routes from vpn and with new
RD/RT values advertise new routes to vpn.

One of the sceanrio is restarting frr:
A router-id change may not have reflected
for bgp vrf instance X, while import vrf X
under bgp vrf instance Y.
Once router-id changes for bgp VRF X,
change RD and RTs from export VRF and
imported VRFs. Readvertise routes with new
values to VPN.

Ticket:CM-24149
Reviewed By:CCR-8394
Testing Done:

Validated via configured multiple bgp VRF instances
and enable route leaks among them, restart frr
and all instance received correct RD and RT values.
Checked 'show bgp vrf all ipv4 unicast route-leak'
and vpn table 'show bgp ipv4 vpn all' output.

Signed-off-by: Chirag Shah <chirag@cumulusnetworks.com>

bgpd/bgpd.c
@@ -253,6 +253,8 @@ static int bgp_router_id_set(struct bgp *bgp, const struct in_addr *id)
 	if (is_evpn_enabled())
 		bgp_evpn_handle_router_id_update(bgp, TRUE);
 
+	vpn_handle_router_id_update(bgp, TRUE);
+
 	IPV4_ADDR_COPY(&bgp->router_id, id);
 
 	/* Set all peer's local identifier with this value. */
@@ -269,6 +271,8 @@ static int bgp_router_id_set(struct bgp *bgp, const struct in_addr *id)
 	/* EVPN uses router id in RD, update them */
 	if (is_evpn_enabled())
 		bgp_evpn_handle_router_id_update(bgp, FALSE);
+
+	vpn_handle_router_id_update(bgp, FALSE);
 
 	return 0;
 }

bgpd/bgp_mplsvpn.c
@@ -1483,6 +1483,97 @@ static void vpn_policy_routemap_update(struct bgp *bgp, const char *rmap_name)
 	}
 }
 
+/* This API is used during router-id change, reflect VPNs
+ * auto RD and RT values and readvertise routes to VPN table.
+ */
+void vpn_handle_router_id_update(struct bgp *bgp, bool withdraw)
+{
+	afi_t afi;
+	int debug;
+	char *vname;
+	const char *export_name;
+	char buf[RD_ADDRSTRLEN];
+	struct bgp *bgp_import;
+	struct listnode *node;
+	struct ecommunity *ecom;
+	vpn_policy_direction_t idir, edir;
+
+	if (bgp->inst_type != BGP_INSTANCE_TYPE_DEFAULT
+	    && bgp->inst_type != BGP_INSTANCE_TYPE_VRF)
+		return;
+
+	export_name = bgp->name ? bgp->name : VRF_DEFAULT_NAME;
+	debug = (BGP_DEBUG(vpn, VPN_LEAK_TO_VRF) |
+		     BGP_DEBUG(vpn, VPN_LEAK_FROM_VRF));
+
+	idir = BGP_VPN_POLICY_DIR_FROMVPN;
+	edir = BGP_VPN_POLICY_DIR_TOVPN;
+
+	for (afi = 0; afi < AFI_MAX; ++afi) {
+		if (!vpn_leak_to_vpn_active(bgp, afi, NULL))
+			continue;
+
+		if (withdraw) {
+			vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN,
+					   afi, bgp_get_default(), bgp);
+			if (debug)
+				zlog_debug("%s: %s after to_vpn vpn_leak_prechange",
+					   __func__, export_name);
+
+			/* Remove import RT from VRFs */
+			ecom = bgp->vpn_policy[afi].rtlist[edir];
+			for (ALL_LIST_ELEMENTS_RO(bgp->vpn_policy[afi].
+						  export_vrf, node, vname)) {
+				bgp_import = bgp_lookup_by_name(vname);
+				if (!bgp_import)
+					continue;
+
+				ecommunity_del_val(bgp_import->vpn_policy[afi].
+						   rtlist[idir],
+					(struct ecommunity_val *)ecom->val);
+
+			}
+		} else {
+			/* New router-id derive auto RD and RT and export
+			 * to VPN
+			 */
+			form_auto_rd(bgp->router_id, bgp->vrf_rd_id,
+				     &bgp->vrf_prd_auto);
+			bgp->vpn_policy[afi].tovpn_rd = bgp->vrf_prd_auto;
+			prefix_rd2str(&bgp->vpn_policy[afi].tovpn_rd, buf,
+				      sizeof(buf));
+			bgp->vpn_policy[afi].rtlist[edir] =
+				ecommunity_str2com(buf,
+						   ECOMMUNITY_ROUTE_TARGET, 0);
+
+			/* Update import_vrf rt_list */
+			ecom = bgp->vpn_policy[afi].rtlist[edir];
+			for (ALL_LIST_ELEMENTS_RO(bgp->vpn_policy[afi].
+						  export_vrf, node, vname)) {
+				bgp_import = bgp_lookup_by_name(vname);
+				if (!bgp_import)
+					continue;
+				if (bgp_import->vpn_policy[afi].rtlist[idir])
+					bgp_import->vpn_policy[afi].rtlist[idir]
+						= ecommunity_merge(
+						bgp_import->vpn_policy[afi]
+						.rtlist[idir], ecom);
+				else
+					bgp_import->vpn_policy[afi].rtlist[idir]
+						= ecommunity_dup(ecom);
+
+			}
+			/* Update routes to VPN */
+			vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN,
+					    afi, bgp_get_default(),
+					    bgp);
+			if (debug)
+				zlog_debug("%s: %s after to_vpn vpn_leak_postchange",
+					   __func__, export_name);
+		}
+	}
+}
+
 void vpn_policy_routemap_event(const char *rmap_name)
 {
 	int debug = BGP_DEBUG(vpn, VPN_LEAK_RMAP_EVENT);
@@ -1508,10 +1599,14 @@ void vrf_import_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 	char buf[1000];
 	struct ecommunity *ecom;
 	bool first_export = false;
+	int debug;
 
 	export_name = to_bgp->name ? to_bgp->name : VRF_DEFAULT_NAME;
 	idir = BGP_VPN_POLICY_DIR_FROMVPN;
 	edir = BGP_VPN_POLICY_DIR_TOVPN;
+
+	debug = (BGP_DEBUG(vpn, VPN_LEAK_TO_VRF) |
+		     BGP_DEBUG(vpn, VPN_LEAK_FROM_VRF));
 
 	/*
 	 * Cross-ref both VRFs. Also, note if this is the first time
@@ -1555,6 +1650,23 @@ void vrf_import_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 		to_bgp->vpn_policy[afi].rtlist[idir] = ecommunity_dup(ecom);
 	SET_FLAG(to_bgp->af_flags[afi][safi], BGP_CONFIG_VRF_TO_VRF_IMPORT);
 
+	if (debug) {
+		const char *from_name;
+
+		from_name = from_bgp->name ? from_bgp->name :
+			VRF_DEFAULT_NAME;
+		zlog_debug("%s from %s to %s first_export %u import-rt %s export-rt %s",
+			   __func__, from_name, export_name, first_export,
+			   to_bgp->vpn_policy[afi].rtlist[idir] ?
+			   (ecommunity_ecom2str(to_bgp->vpn_policy[afi].
+						rtlist[idir],
+					ECOMMUNITY_FORMAT_ROUTE_MAP, 0)) : " ",
+			   to_bgp->vpn_policy[afi].rtlist[edir] ?
+			   (ecommunity_ecom2str(to_bgp->vpn_policy[afi].
+						rtlist[edir],
+					ECOMMUNITY_FORMAT_ROUTE_MAP, 0)) : " ");
+	}
+
 	/* Does "import_vrf" first need to export its routes or that
 	 * is already done and we just need to import those routes
 	 * from the global table?
@@ -1573,11 +1685,15 @@ void vrf_unimport_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 	char *vname;
 	struct ecommunity *ecom;
 	struct listnode *node;
+	int debug;
 
 	export_name = to_bgp->name ? to_bgp->name : VRF_DEFAULT_NAME;
 	tmp_name = from_bgp->name ? from_bgp->name : VRF_DEFAULT_NAME;
 	idir = BGP_VPN_POLICY_DIR_FROMVPN;
 	edir = BGP_VPN_POLICY_DIR_TOVPN;
+
+	debug = (BGP_DEBUG(vpn, VPN_LEAK_TO_VRF) |
+		     BGP_DEBUG(vpn, VPN_LEAK_FROM_VRF));
 
 	/* Were we importing from "import_vrf"? */
 	for (ALL_LIST_ELEMENTS_RO(to_bgp->vpn_policy[afi].import_vrf, node,
@@ -1595,6 +1711,9 @@ void vrf_unimport_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 	 */
 	if (!vname)
 		return;
+
+	if (debug)
+		zlog_debug("%s from %s to %s", __func__, tmp_name, export_name);
 
 	/* Remove "import_vrf" from our import list. */
 	listnode_delete(to_bgp->vpn_policy[afi].import_vrf, vname);

bgpd/bgp_mplsvpn.h
@@ -231,5 +231,6 @@ extern void vpn_policy_routemap_event(const char *rmap_name);
 extern vrf_id_t get_first_vrf_for_redirect_with_rt(struct ecommunity *eckey);
 
 extern void vpn_leak_postchange_all(void);
+extern void vpn_handle_router_id_update(struct bgp *bgp, bool withdraw);
 
 #endif /* _QUAGGA_BGP_MPLSVPN_H */

commit 9cf8d02f1fb26631612b79cde8aec36a613f02e8

bgpd: explicitly request rd and rt auto change behavior after router-id change
636f76088d ("bgpd: router-id change reflect to vpn auto rd rt") has
introduced an automatic configuration change of the `rd vpn export`
and `rt vpn export` attributes after a router-id modification with the
`bgp router-id` command.

For example, run the bgp_l3vpn_to_bgp_vrf topotest:
> python3 -m pytest bgp_l3vpn_to_bgp_vrf -v --pause -s --shell=r1

Configure the following to r1:
> ip link add r1-cust5 type vrf table 100
> ip link set r1-cust5 up
>
> vtysh
> conf t
> router bgp 5227 vrf r1-cust5
>  bgp router-id 192.168.1.1
>  !
>  address-family ipv4 unicast
>   rd vpn export 10:1
>   rt vpn both 52:100
>   export vpn
>   import vpn
>  exit-address-family
> exit

Then change the router-id:
> router bgp 5227 vrf r1-cust5
>  bgp router-id 192.168.1.11

Notice that the change `rd/rt vpn export` attributes:
> r1(config-router)# do sh run bgp
> router bgp 5227 vrf r1-cust5
>  bgp router-id 192.168.1.11
>  !
>  address-family ipv4 unicast
>   rd vpn export 192.168.1.11:3
>   rt vpn import 52:100
>   rt vpn export 192.168.1.11:3
>   export vpn
>   import vpn
>  exit-address-family
>

`rd/rt vpn export` attributes are in the following format:
> r1(config-router-af)# rt vpn export ?
>   RTLIST  Space separated route target list (A.B.C.D:MN|EF:OPQR|GHJK:MN)
> r1(config-router-af)# rd vpn export ?
>  ASN:NN_OR_IP-ADDRESS:NN  Route Distinguisher (<as-number>:<number> | <ip-address>:<number>)

The automatic change of the `rd/rt vpn export` attributes have only a
benefit in the case the attributes are in the `<router-id:number>`
format. It should not be the general case.

Add a `bgp auto-rd-rt` global option to explicitly request the
`rd/rt vpn export` attributes to be automatically replaced by the
`<router-id:number>` format after a `bgp router-id` modification.

Fixes: 636f76088d ("bgpd: router-id change reflect to vpn auto rd rt")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_vty.c
@@ -1724,6 +1724,22 @@ DEFUN (no_bgp_cluster_id,
 	return CMD_SUCCESS;
 }
 
+DEFPY (bgp_auto_rd_rt,
+       bgp_auto_rd_rt_cmd,
+       "[no] bgp auto-rd-rt",
+       NO_STR
+       BGP_STR
+       "Auto-update rd/rt export attributes to <router-id>:XX after a router-id change\n")
+{
+	if (no)
+		bgp_option_unset(BGP_OPT_AUTO_RD_RT);
+	else
+		bgp_option_set(BGP_OPT_AUTO_RD_RT);
+
+	return CMD_SUCCESS;
+}
+
+
 DEFPY (bgp_norib,
        bgp_norib_cmd,
        "bgp no-rib",
@@ -17099,6 +17115,9 @@ int bgp_config_write(struct vty *vty)
 	if (bgp_option_check(BGP_OPT_NO_FIB))
 		vty_out(vty, "bgp no-rib\n");
 
+	if (bgp_option_check(BGP_OPT_AUTO_RD_RT))
+		vty_out(vty, "bgp auto-rd-rt\n");
+
 	if (CHECK_FLAG(bm->flags, BM_FLAG_SEND_EXTRA_DATA_TO_ZEBRA))
 		vty_out(vty, "bgp send-extra-data zebra\n");
 
@@ -17805,6 +17824,9 @@ void bgp_vty_init(void)
 	/* "bgp cluster-id" commands. */
 	install_element(BGP_NODE, &bgp_cluster_id_cmd);
 	install_element(BGP_NODE, &no_bgp_cluster_id_cmd);
+
+	/* "bgp auto-rd-rt" commands. */
+	install_element(CONFIG_NODE, &bgp_auto_rd_rt_cmd);
 
 	/* "bgp no-rib" commands. */
 	install_element(CONFIG_NODE, &bgp_norib_cmd);

bgpd/bgpd.c
@@ -189,6 +189,7 @@ int bgp_option_set(int flag)
 	case BGP_OPT_NO_FIB:
 	case BGP_OPT_NO_LISTEN:
 	case BGP_OPT_NO_ZEBRA:
+	case BGP_OPT_AUTO_RD_RT:
 		SET_FLAG(bm->options, flag);
 		break;
 	default:
@@ -203,6 +204,7 @@ int bgp_option_unset(int flag)
 	/* Fall through.  */
 	case BGP_OPT_NO_ZEBRA:
 	case BGP_OPT_NO_FIB:
+	case BGP_OPT_AUTO_RD_RT:
 		UNSET_FLAG(bm->options, flag);
 		break;
 	default:
@@ -294,7 +296,8 @@ static int bgp_router_id_set(struct bgp *bgp, const struct in_addr *id,
 	if (is_evpn_enabled())
 		bgp_evpn_handle_router_id_update(bgp, true);
 
-	vpn_handle_router_id_update(bgp, true, is_config);
+	if (bgp_option_check(BGP_OPT_AUTO_RD_RT))
+		vpn_handle_router_id_update(bgp, true, is_config);
 
 	IPV4_ADDR_COPY(&bgp->router_id, id);
 
@@ -313,7 +316,8 @@ static int bgp_router_id_set(struct bgp *bgp, const struct in_addr *id,
 	if (is_evpn_enabled())
 		bgp_evpn_handle_router_id_update(bgp, false);
 
-	vpn_handle_router_id_update(bgp, false, is_config);
+	if (bgp_option_check(BGP_OPT_AUTO_RD_RT))
+		vpn_handle_router_id_update(bgp, false, is_config);
 
 	return 0;
 }

bgpd/bgpd.h
@@ -135,6 +135,7 @@ struct bgp_master {
 #define BGP_OPT_NO_FIB                   (1 << 0)
 #define BGP_OPT_NO_LISTEN                (1 << 1)
 #define BGP_OPT_NO_ZEBRA                 (1 << 2)
+#define BGP_OPT_AUTO_RD_RT               (1 << 3)
 
 	uint64_t updgrp_idspace;
 	uint64_t subgrp_idspace;