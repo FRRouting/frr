FRRouting/frr

commit 106d2fd572c18bebe3dc44031c6d5e56e7b9030f

2003-08-01 Cougar <cougar@random.ee>
	* lib/if.c: (if_cmp_func) new function, compare interface names in
	alphabetical order.
	(if_create) Take name as argument and add interface in sorted order.
	(if_get_by_name),(interface_cmd) fixup calls to if_create - see
	above.
	(if_init) register list comparison function.
	* lib/if.h: Add comparison function, modify if_create prototype.
	* lib/zclient.c: Modify call to if_create.
	* ospfd/ospf_interface.c: (ospf_vl_new) modify call to if_create.
	change sprintf to snprintf.
	* zebra/kernel_socket.c: (ifm_read) modify call to if_create.

lib/if.c
@@ -1,3 +1,4 @@
+
 /* 
  * Interface functions.
  * Copyright (C) 1997, 98 Kunihiro Ishiguro
@@ -46,6 +47,52 @@ struct if_master
   int (*if_delete_hook) (struct interface *);
 } if_master;
 
+/* Compare interface names */
+int
+if_cmp_func (struct interface *ifp1, struct interface *ifp2)
+{
+  unsigned int l1, l2;
+  long int x1, x2;
+  char *p1, *p2;
+  int res;
+
+  p1 = ifp1->name;
+  p2 = ifp2->name;
+
+  while (1) {
+    /* look up to any number */
+    l1 = strcspn(p1, "0123456789");
+    l2 = strcspn(p2, "0123456789");
+
+    /* name lengths are different -> compare names */
+    if (l1 != l2)
+      return (strcmp(p1, p2));
+
+    res = strncmp(p1, p2, l1);
+
+    /* names are different -> compare them */
+    if (res)
+      return res;
+
+    /* with identical name part, go to numeric part */
+
+    p1 += l1;
+    p2 += l1;
+
+    x1 = strtol(p1, &p1, 10);
+    x2 = strtol(p2, &p2, 10);
+
+    /* let's compare numbers now */
+    if (x1 < x2)
+      return -1;
+    if (x1 > x2)
+      return 1;
+
+    /* numbers were equal, lets do it again..
+    (it happens with name like "eth123.456:789") */
+  }
+}
+
 /* Create new interface structure. */
 struct interface *
 if_new ()
@@ -58,13 +105,17 @@ if_new ()
 }
 
 struct interface *
-if_create ()
+if_create (char *name, int namelen)
 {
   struct interface *ifp;
 
   ifp = if_new ();
   
-  listnode_add (iflist, ifp);
+  assert (name);
+  assert (namelen <= (INTERFACE_NAMSIZ + 1));
+  strncpy (ifp->name, name, namelen);
+  ifp->name[INTERFACE_NAMSIZ] = '\0';
+  listnode_add_sort (iflist, ifp);
   ifp->connected = list_new ();
   ifp->connected->del = (void (*) (void *)) connected_free;
 
@@ -248,10 +299,7 @@ if_get_by_name (char *name)
 
   ifp = if_lookup_by_name (name);
   if (ifp == NULL)
-    {
-      ifp = if_create ();
-      strncpy (ifp->name, name, IFNAMSIZ);
-    }
+    ifp = if_create (name, INTERFACE_NAMSIZ);
   return ifp;
 }
 
@@ -430,10 +478,7 @@ DEFUN (interface,
   ifp = if_lookup_by_name (argv[0]);
 
   if (ifp == NULL)
-    {
-      ifp = if_create ();
-      strncpy (ifp->name, argv[0], INTERFACE_NAMSIZ);
-    }
+    ifp = if_create (argv[0], INTERFACE_NAMSIZ);
   vty->index = ifp;
   vty->node = INTERFACE_NODE;
 
@@ -803,8 +848,10 @@ if_init ()
   iflist = list_new ();
   ifaddr_ipv4_table = route_table_init ();
 
-  if (iflist)
+  if (iflist) {
+    iflist->cmp = (int (*)(void *, void *))if_cmp_func;
     return;
+  }
 
   memset (&if_master, 0, sizeof if_master);
 }

lib/if.h
@@ -180,8 +180,9 @@ struct connected
 #endif /* IFF_LINK2 */
 
 /* Prototypes. */
+int if_cmp_func (struct interface *, struct interface *);
 struct interface *if_new (void);
-struct interface *if_create (void);
+struct interface *if_create (char *name, int namelen);
 struct interface *if_lookup_by_index (unsigned int);
 struct interface *if_lookup_by_name (char *);
 struct interface *if_lookup_exact_address (struct in_addr);

lib/zclient.c
@@ -555,10 +555,7 @@ zebra_interface_add_read (struct stream *s)
 
   /* If such interface does not exist, make new one. */
   if (! ifp)
-    {
-      ifp = if_create ();
-      strncpy (ifp->name, ifname_tmp, IFNAMSIZ);
-    }
+    ifp = if_create (ifname_tmp, INTERFACE_NAMSIZ);
 
   /* Read interface's index. */
   ifp->ifindex = stream_getl (s);

ospfd/ospf_interface.c
@@ -745,7 +745,8 @@ ospf_vl_new (struct ospf *ospf, struct ospf_vl_data *vl_data)
   if (IS_DEBUG_OSPF_EVENT)
     zlog_info ("ospf_vl_new(): creating pseudo zebra interface");
 
-  vi = if_create ();
+  snprintf (ifname, INTERFACE_NAMSIZ + 1, "VLINK%d", vlink_count);
+  vi = if_create (ifname, INTERFACE_NAMSIZ);
   co = connected_new ();
   co->ifp = vi;
   listnode_add (vi->connected, co);
@@ -769,10 +770,9 @@ ospf_vl_new (struct ospf *ospf, struct ospf_vl_data *vl_data)
   voi->ifp->mtu = OSPF_VL_MTU;
   voi->type = OSPF_IFTYPE_VIRTUALLINK;
 
-  sprintf (ifname, "VLINK%d", vlink_count++);
+  vlink_count++;
   if (IS_DEBUG_OSPF_EVENT)
     zlog_info ("ospf_vl_new(): Created name: %s", ifname);
-  strncpy (vi->name, ifname, IFNAMSIZ);
   if (IS_DEBUG_OSPF_EVENT)
     zlog_info ("ospf_vl_new(): set if->name to %s", vi->name);
 

zebra/kernel_socket.c
@@ -219,9 +219,8 @@ ifm_read (struct if_msghdr *ifm)
 	  return -1;
 	}
 
-      ifp = if_create ();
+      ifp = if_create (sdl->sdl_data, sdl->sdl_nlen);
 
-      strncpy (ifp->name, sdl->sdl_data, sdl->sdl_nlen);
       ifp->ifindex = ifm->ifm_index;
       ifp->flags = ifm->ifm_flags;
 #if defined(__bsdi__)

commit a373540ca95415b6ba6fde8feebeed09ec5f3296

lib/if: fix interface name comparison
Using strtol() to compare two strings is a bad idea.
Before the patch, if_cmp_name_func() may confuse foo001 and foo1.

Let's use strverscmp(), which classify foo2 before foo10, as expected.
Add strverscmp() implementation for BSD builds under lib/ folder.

Fixes: 106d2fd ("2003-08-01 Cougar <cougar@random.ee>")
Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

lib/if.c
@@ -77,57 +77,7 @@ static struct interface_master{
  */
 int if_cmp_name_func(const char *p1, const char *p2)
 {
-	unsigned int l1, l2;
-	long int x1, x2;
-	int res;
-
-	while (*p1 && *p2) {
-		/* look up to any number */
-		l1 = strcspn(p1, "0123456789");
-		l2 = strcspn(p2, "0123456789");
-
-		/* name lengths are different -> compare names */
-		if (l1 != l2)
-			return (strcmp(p1, p2));
-
-		/* Note that this relies on all numbers being less than all
-		 * letters, so
-		 * that de0 < del0.
-		 */
-		res = strncmp(p1, p2, l1);
-
-		/* names are different -> compare them */
-		if (res)
-			return res;
-
-		/* with identical name part, go to numeric part */
-		p1 += l1;
-		p2 += l1;
-
-		if (!*p1 && !*p2)
-			return 0;
-		if (!*p1)
-			return -1;
-		if (!*p2)
-			return 1;
-
-		x1 = strtol(p1, (char **)&p1, 10);
-		x2 = strtol(p2, (char **)&p2, 10);
-
-		/* let's compare numbers now */
-		if (x1 < x2)
-			return -1;
-		if (x1 > x2)
-			return 1;
-
-		/* numbers were equal, lets do it again..
-		(it happens with name like "eth123.456:789") */
-	}
-	if (*p1)
-		return 1;
-	if (*p2)
-		return -1;
-	return 0;
+	return strverscmp(p1, p2);
 }
 
 static int if_cmp_func(const struct interface *ifp1,

lib/strverscmp.c
@@ -1,0 +1,63 @@
+/* Compare strings while treating digits characters numerically.
+ * musl as a whole is licensed under the following standard MIT license:
+ * ----------------------------------------------------------------------
+ * Copyright © 2005-2020 Rich Felker, et al.
+
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ----------------------------------------------------------------------
+ */
+#include "zebra.h"
+
+#if defined(OPEN_BSD) || defined(__NetBSD__) || defined(__FreeBSD__)
+#include <ctype.h>
+#include <string.h>
+
+int strverscmp(const char *l0, const char *r0)
+{
+	const unsigned char *l = (const void *)l0;
+	const unsigned char *r = (const void *)r0;
+	size_t i, dp, j;
+	int z = 1;
+
+	/* Find maximal matching prefix and track its maximal digit
+	 * suffix and whether those digits are all zeros. */
+	for (dp=i=0; l[i]==r[i]; i++) {
+		int c = l[i];
+		if (!c) return 0;
+		if (!isdigit(c)) dp=i+1, z=1;
+		else if (c!='0') z=0;
+	}
+
+	if (l[dp]!='0' && r[dp]!='0') {
+		/* If we're not looking at a digit sequence that began
+		 * with a zero, longest digit string is greater. */
+		for (j=i; isdigit(l[j]); j++)
+			if (!isdigit(r[j])) return 1;
+		if (isdigit(r[j])) return -1;
+	} else if (z && dp<i && (isdigit(l[i]) || isdigit(r[i]))) {
+		/* Otherwise, if common prefix of digit sequence is
+		 * all zeros, digits order less than non-digits. */
+		return (unsigned char)(l[i]-'0') - (unsigned char)(r[i]-'0');
+	}
+
+	return l[i] - r[i];
+}
+
+#endif

lib/subdir.am
@@ -95,6 +95,7 @@ lib_libfrr_la_SOURCES = \
 	lib/strformat.c \
 	lib/strlcat.c \
 	lib/strlcpy.c \
+	lib/strverscmp.c \
 	lib/systemd.c \
 	lib/table.c \
 	lib/termtable.c \

lib/zebra.h
@@ -230,6 +230,8 @@ size_t strlcpy(char *__restrict dest,
 	       const char *__restrict src, size_t destsize);
 #endif
 
+int strverscmp(const char *s1, const char *s2);
+
 #if !defined(HAVE_STRUCT_MMSGHDR_MSG_HDR) || !defined(HAVE_SENDMMSG)
 /* avoid conflicts in case we have partial support */
 #define mmsghdr frr_mmsghdr