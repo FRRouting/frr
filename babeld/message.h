a76cf7e49e62b60d5aedd1e6d6f562cb90ed051b
Donald Sharp <sharpd@nvidia.com>
2022-06-08 09:01:31 -0400

    babeld: Ignore sub-tlv's with mandatory bit set
    RFC 8966 4.4
    
       If the mandatory bit is set, then the
       whole enclosing TLV MUST be silently ignored (except for updating the
       parser state by a Router-Id, Next Hop, or Update TLV, as described in
       the next section).
    
    Fixes: #11349
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- babeld/message.h
+++ babeld/message.h
@@ -51,6 +51,7 @@ THE SOFTWARE.
 #define SUBTLV_PADN 1
 #define SUBTLV_DIVERSITY 2 /* Also known as babelz. */
 #define SUBTLV_TIMESTAMP 3 /* Used to compute RTT. */
+#define SUBTLV_MANDATORY 0x80
 
 extern unsigned short myseqno;
 
