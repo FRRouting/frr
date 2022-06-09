cefa37cf41709bc23a1be296b6ed63097ea47350
Sai Gomathi N <nsaigomathi@vmware.com>
2022-06-09 03:23:28 -0700

    pim6d: Moving the common lines of pim_show_nexthop cli
    Moving the common lines of pim_show_nexthop cli
    in pim_cmd.c and pim6_cmd.c to pim_cmd_common.c file
    
    Signed-off-by: Sai Gomathi N <nsaigomathi@vmware.com>

--- pimd/pim_cmd_common.h
+++ pimd/pim_cmd_common.h
@@ -102,6 +102,7 @@ void pim_show_interfaces_single(struct pim_instance *pim, struct vty *vty,
 				json_object *json);
 void ip_pim_ssm_show_group_range(struct pim_instance *pim, struct vty *vty,
 				 bool uj);
+int pim_show_nexthop_cmd_helper(const char *vrf, struct vty *vty);
 void pim_show_nexthop(struct pim_instance *pim, struct vty *vty);
 int pim_show_neighbors_cmd_helper(const char *vrf, struct vty *vty,
 				  const char *json, const char *interface);
