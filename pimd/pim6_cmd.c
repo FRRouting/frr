1336f3e8d21aca952da337d495b98435a16dea5e
Abhishek N R <abnr@vmware.com>
2022-06-09 03:29:02 -0700

    pim6d: Moving reusable code to common api for "show pim state" command
    Signed-off-by: Abhishek N R <abnr@vmware.com>

--- pimd/pim6_cmd.c
+++ pimd/pim6_cmd.c
@@ -928,31 +928,7 @@ DEFPY (show_ipv6_pim_state,
        "Multicast address\n"
        JSON_STR)
 {
-	struct pim_instance *pim;
-	struct vrf *v;
-	json_object *json_parent = NULL;
-
-	v = vrf_lookup_by_name(vrf ? vrf : VRF_DEFAULT_NAME);
-
-	if (!v)
-		return CMD_WARNING;
-
-	pim = pim_get_pim_instance(v->vrf_id);
-
-	if (!pim) {
-		vty_out(vty, "%% Unable to find pim instance\n");
-		return CMD_WARNING;
-	}
-
-	if (json)
-		json_parent = json_object_new_object();
-
-	pim_show_state(pim, vty, s_or_g_str, g_str, json_parent);
-
-	if (json)
-		vty_json(vty, json_parent);
-
-	return CMD_SUCCESS;
+	return pim_show_state_helper(vrf, vty, s_or_g_str, g_str, !!json);
 }
 
 DEFPY (show_ipv6_pim_state_vrf_all,
@@ -967,27 +943,7 @@ DEFPY (show_ipv6_pim_state_vrf_all,
        "Multicast address\n"
        JSON_STR)
 {
-	struct vrf *vrf;
-	json_object *json_parent = NULL;
-	json_object *json_vrf = NULL;
-
-	if (json)
-		json_parent = json_object_new_object();
-
-	RB_FOREACH (vrf, vrf_name_head, &vrfs_by_name) {
-		if (!json)
-			vty_out(vty, "VRF: %s\n", vrf->name);
-		else
-			json_vrf = json_object_new_object();
-		pim_show_state(vrf->info, vty, s_or_g_str, g_str, json_vrf);
-		if (json)
-			json_object_object_add(json_parent, vrf->name,
-					       json_vrf);
-	}
-	if (json)
-		vty_json(vty, json_parent);
-
-	return CMD_SUCCESS;
+	return pim_show_state_vrf_all_helper(vty, s_or_g_str, g_str, !!json);
 }
 
 DEFPY (show_ipv6_pim_channel,
