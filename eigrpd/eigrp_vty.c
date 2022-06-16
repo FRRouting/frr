38f740782ec09a44f8eb1c16cadc22bf57f95dce
Donald Sharp <sharpd@nvidia.com>
2022-06-16 15:34:16 -0400

    eigrpd: vrf variable name hides a parameter of the same name
    vrf is a char * being passed in.  Rename the vrf to be something
    else.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- eigrpd/eigrp_vty.c
+++ eigrpd/eigrp_vty.c
@@ -240,14 +240,14 @@ DEFPY (show_ip_eigrp_interfaces,
 	struct eigrp *eigrp;
 
 	if (vrf && strncmp(vrf, "all", sizeof("all")) == 0) {
-		struct vrf *vrf;
+		struct vrf *v;
 
-		RB_FOREACH (vrf, vrf_name_head, &vrfs_by_name) {
-			eigrp = eigrp_lookup(vrf->vrf_id);
+		RB_FOREACH (v, vrf_name_head, &vrfs_by_name) {
+			eigrp = eigrp_lookup(v->vrf_id);
 			if (!eigrp)
 				continue;
 
-			vty_out(vty, "VRF %s:\n", vrf->name);
+			vty_out(vty, "VRF %s:\n", v->name);
 
 			eigrp_interface_helper(vty, eigrp, ifname, detail);
 		}
