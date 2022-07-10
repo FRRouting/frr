e5525edf12a40bf92e85e282576c5557ed07ac99
Donatas Abraitis <donatas@opensourcerouting.org>
2022-07-10 21:41:25 +0300

    Revert "isisd: apply fast-reroute on an adjacency failure"
    This reverts commit 71252973234e70ffa3cb5eec43d4efc1be3d3331.

--- isisd/isis_circuit.h
+++ isisd/isis_circuit.h
@@ -28,7 +28,6 @@
 #include "qobj.h"
 #include "prefix.h"
 #include "ferr.h"
-#include "nexthop.h"
 
 #include "isis_constants.h"
 #include "isis_common.h"
@@ -210,9 +209,6 @@ void isis_circuit_print_vty(struct isis_circuit *circuit, struct vty *vty,
 void isis_circuit_print_json(struct isis_circuit *circuit,
 			     struct json_object *json, char detail);
 size_t isis_circuit_pdu_size(struct isis_circuit *circuit);
-void isis_circuit_switchover_routes(struct isis_circuit *circuit, int family,
-				    union g_addr *nexthop_ip,
-				    ifindex_t ifindex);
 void isis_circuit_stream(struct isis_circuit *circuit, struct stream **stream);
 
 void isis_circuit_af_set(struct isis_circuit *circuit, bool ip_router,
