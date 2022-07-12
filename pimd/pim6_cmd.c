53f9908a0cfc2aaf7c72fdb2aa1b56a0e5f9b420
Sarita Patra <saritap@vmware.com>
2022-07-12 06:48:33 -0700

    pim6d: Add "[no] debug pimv6 bsm" cli
    The cli "[no] debug pimv6 bsm" is used to enable/
    disable bsm debugs for PIMV6
    
    Signed-off-by: Sarita Patra <saritap@vmware.com>

--- pimd/pim6_cmd.c
+++ pimd/pim6_cmd.c
@@ -1627,6 +1627,7 @@ DEFUN_NOSH (show_debugging_pimv6,
 	return CMD_SUCCESS;
 }
 
+<<<<<<< HEAD
 DEFPY (debug_mld,
        debug_mld_cmd,
        "[no] debug mld",
@@ -1708,6 +1709,22 @@ DEFPY (debug_mld_trace_detail,
 		PIM_DO_DEBUG_GM_TRACE_DETAIL;
 	else
 		PIM_DONT_DEBUG_GM_TRACE_DETAIL;
+
+	return CMD_SUCCESS;
+}
+
+DEFPY (debug_pimv6_bsm,
+       debug_pimv6_bsm_cmd,
+       "[no] debug pimv6 bsm",
+       NO_STR
+       DEBUG_STR
+       DEBUG_PIMV6_STR
+       DEBUG_PIMV6_BSM_STR)
+{
+	if (!no)
+		PIM_DO_DEBUG_BSM;
+	else
+		PIM_DONT_DEBUG_BSM;
 
 	return CMD_SUCCESS;
 }
@@ -1855,6 +1872,7 @@ void pim_cmd_init(void)
 	install_element(ENABLE_NODE, &debug_mld_packets_cmd);
 	install_element(ENABLE_NODE, &debug_mld_trace_cmd);
 	install_element(ENABLE_NODE, &debug_mld_trace_detail_cmd);
+	install_element(ENABLE_NODE, &debug_pimv6_bsm_cmd);
 
 	install_element(CONFIG_NODE, &debug_pimv6_cmd);
 	install_element(CONFIG_NODE, &debug_pimv6_nht_cmd);
@@ -1873,4 +1891,5 @@ void pim_cmd_init(void)
 	install_element(CONFIG_NODE, &debug_mld_packets_cmd);
 	install_element(CONFIG_NODE, &debug_mld_trace_cmd);
 	install_element(CONFIG_NODE, &debug_mld_trace_detail_cmd);
+	install_element(CONFIG_NODE, &debug_pimv6_bsm_cmd);
 }
