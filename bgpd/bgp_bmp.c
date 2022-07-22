2c1900e0d11007e43715723d0e33900d59d0cb9b
mxyns <mx.yns@outlook.fr>
2022-07-22 12:16:29 +0200

    bgpd: correct loc rib update queue cleanup
    empties out and free the locrib specific queue's memory on bmp_close call
    
    Signed-off-by: Maxence Younsi <mx.yns@outlook.fr>

--- bgpd/bgp_bmp.c
+++ bgpd/bgp_bmp.c
@@ -1638,6 +1638,9 @@ static void bmp_close(struct bmp *bmp)
 	while ((bqe = bmp_pull(bmp)))
 		if (!bqe->refcount)
 			XFREE(MTYPE_BMP_QUEUE, bqe);
+	while ((bqe = bmp_pull_locrib(bmp)))
+		if (!bqe->refcount)
+			XFREE(MTYPE_BMP_QUEUE, bqe);
 
 	EVENT_OFF(bmp->t_read);
 	pullwr_del(bmp->pullwr);
