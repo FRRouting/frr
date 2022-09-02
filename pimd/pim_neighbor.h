709b2de5e4a04f15215e74b84c7e3b1c91ad890c
Mobashshera Rasool <mrasool@vmware.com>
2022-09-02 09:50:28 -0700

    pimd: Removal of unused function pim_neighbor_find_prefix
    Signed-off-by: Mobashshera Rasool <mrasool@vmware.com>

--- pimd/pim_neighbor.h
+++ pimd/pim_neighbor.h
@@ -52,8 +52,6 @@ void pim_neighbor_timer_reset(struct pim_neighbor *neigh, uint16_t holdtime);
 void pim_neighbor_free(struct pim_neighbor *neigh);
 struct pim_neighbor *pim_neighbor_find(struct interface *ifp,
 				       pim_addr source_addr);
-struct pim_neighbor *pim_neighbor_find_prefix(struct interface *ifp,
-					      const struct prefix *src_prefix);
 struct pim_neighbor *pim_neighbor_find_by_secondary(struct interface *ifp,
 						    struct prefix *src);
 struct pim_neighbor *pim_neighbor_find_if(struct interface *ifp);
