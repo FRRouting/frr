11c243766a6f7ed13e0578b2f6cef24c4d618ca4
Donatas Abraitis <donatas@opensourcerouting.org>
2022-09-06 11:49:08 +0300

    bgpd: Do not forget to unlock bgp_dest from update_advertise_vni_routes
    If "unexpected" happens.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_evpn.c
+++ bgpd/bgp_evpn.c
@@ -3631,8 +3631,10 @@ static int update_advertise_vni_routes(struct bgp *bgp, struct bgpevpn *vpn)
 			    pi->type == ZEBRA_ROUTE_BGP
 			    && pi->sub_type == BGP_ROUTE_STATIC)
 				break;
-		if (!pi) /* unexpected */
+		if (!pi) {
+			bgp_dest_unlock_node(dest);
 			return 0;
+		}
 		attr = pi->attr;
 
 		global_dest = bgp_global_evpn_node_get(bgp->rib[afi][safi],
