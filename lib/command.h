5fd879ad177c6dd504de28d5baddcb86c539662f
Louis Scalbert <louis.scalbert@6wind.com>
2022-09-12 16:49:59 +0200

    isisd: dummy frrbot
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- lib/command.h
+++ lib/command.h
@@ -91,101 +91,101 @@ struct host {
 
 /* List of CLI nodes. Please remember to update the name array in command.c. */
 enum node_type {
-	AUTH_NODE,		 /* Authentication mode of vty interface. */
-	VIEW_NODE,		 /* View node. Default mode of vty interface. */
-	AUTH_ENABLE_NODE,	/* Authentication mode for change enable. */
-	ENABLE_NODE,		 /* Enable node. */
-	CONFIG_NODE,		 /* Config node. Default mode of config file. */
-	DEBUG_NODE,		 /* Debug node. */
-	VRF_DEBUG_NODE,		 /* Vrf Debug node. */
-	NORTHBOUND_DEBUG_NODE,	 /* Northbound Debug node. */
-	DEBUG_VNC_NODE,		 /* Debug VNC node. */
-	RMAP_DEBUG_NODE,         /* Route-map debug node */
-	RESOLVER_DEBUG_NODE,	 /* Resolver debug node */
-	AAA_NODE,		 /* AAA node. */
-	EXTLOG_NODE,		 /* RFC5424 & co. extended syslog */
-	KEYCHAIN_NODE,		 /* Key-chain node. */
-	KEYCHAIN_KEY_NODE,       /* Key-chain key node. */
-	IP_NODE,		 /* Static ip route node. */
-	VRF_NODE,		 /* VRF mode node. */
-	INTERFACE_NODE,		 /* Interface mode node. */
-	NH_GROUP_NODE,		 /* Nexthop-Group mode node. */
-	ZEBRA_NODE,		 /* zebra connection node. */
-	TABLE_NODE,		 /* rtm_table selection node. */
-	RIP_NODE,		 /* RIP protocol mode node. */
-	RIPNG_NODE,		 /* RIPng protocol mode node. */
-	BABEL_NODE,		 /* BABEL protocol mode node. */
-	EIGRP_NODE,		 /* EIGRP protocol mode node. */
-	BGP_NODE,		 /* BGP protocol mode which includes BGP4+ */
-	BGP_VPNV4_NODE,		 /* BGP MPLS-VPN PE exchange. */
-	BGP_VPNV6_NODE,		 /* BGP MPLS-VPN PE exchange. */
-	BGP_IPV4_NODE,		 /* BGP IPv4 unicast address family.  */
-	BGP_IPV4M_NODE,		 /* BGP IPv4 multicast address family.  */
-	BGP_IPV4L_NODE,		 /* BGP IPv4 labeled unicast address family.  */
-	BGP_IPV6_NODE,		 /* BGP IPv6 address family */
-	BGP_IPV6M_NODE,		 /* BGP IPv6 multicast address family. */
-	BGP_IPV6L_NODE,		 /* BGP IPv6 labeled unicast address family. */
-	BGP_VRF_POLICY_NODE,     /* BGP VRF policy */
-	BGP_VNC_DEFAULTS_NODE,   /* BGP VNC nve defaults */
-	BGP_VNC_NVE_GROUP_NODE,  /* BGP VNC nve group */
-	BGP_VNC_L2_GROUP_NODE,   /* BGP VNC L2 group */
-	RFP_DEFAULTS_NODE,       /* RFP defaults node */
-	BGP_EVPN_NODE,		 /* BGP EVPN node. */
-	BGP_SRV6_NODE,		 /* BGP SRv6 node. */
-	OSPF_NODE,		 /* OSPF protocol mode */
-	OSPF6_NODE,		 /* OSPF protocol for IPv6 mode */
-	LDP_NODE,		 /* LDP protocol mode */
-	LDP_IPV4_NODE,		 /* LDP IPv4 address family */
-	LDP_IPV6_NODE,		 /* LDP IPv6 address family */
-	LDP_IPV4_IFACE_NODE,     /* LDP IPv4 Interface */
-	LDP_IPV6_IFACE_NODE,     /* LDP IPv6 Interface */
-	LDP_L2VPN_NODE,		 /* LDP L2VPN node */
-	LDP_PSEUDOWIRE_NODE,     /* LDP Pseudowire node */
-	ISIS_NODE,		 /* ISIS protocol mode */
-	ISIS_FLEX_ALGO_NODE,	 /* ISIS Flex Algo mode */
-	ACCESS_NODE,		 /* Access list node. */
-	PREFIX_NODE,		 /* Prefix list node. */
-	ACCESS_IPV6_NODE,	/* Access list node. */
-	ACCESS_MAC_NODE,	 /* MAC access list node*/
-	PREFIX_IPV6_NODE,	/* Prefix list node. */
-	AS_LIST_NODE,		 /* AS list node. */
-	COMMUNITY_LIST_NODE,     /* Community list node. */
-	COMMUNITY_ALIAS_NODE, /* Community alias node. */
-	RMAP_NODE,		 /* Route map node. */
-	PBRMAP_NODE,		 /* PBR map node. */
-	SMUX_NODE,		 /* SNMP configuration node. */
-	DUMP_NODE,		 /* Packet dump node. */
-	FORWARDING_NODE,	 /* IP forwarding node. */
-	PROTOCOL_NODE,		 /* protocol filtering node */
-	MPLS_NODE,		 /* MPLS config node */
-	PW_NODE,		 /* Pseudowire config node */
-	SEGMENT_ROUTING_NODE,	 /* Segment routing root node */
-	SR_TRAFFIC_ENG_NODE,	 /* SR Traffic Engineering node */
-	SR_SEGMENT_LIST_NODE,	 /* SR segment list config node */
-	SR_POLICY_NODE,		 /* SR policy config node */
-	SR_CANDIDATE_DYN_NODE,	 /* SR dynamic candidate path config node */
-	PCEP_NODE,	 	 /* PCEP node */
-	PCEP_PCE_CONFIG_NODE,	 /* PCE shared configuration node */
-	PCEP_PCE_NODE,		 /* PCE configuration node */
-	PCEP_PCC_NODE,		 /* PCC configuration node */
-	SRV6_NODE,		 /* SRv6 node */
-	SRV6_LOCS_NODE,		 /* SRv6 locators node */
-	SRV6_LOC_NODE,		 /* SRv6 locator node */
-	VTY_NODE,		 /* Vty node. */
-	FPM_NODE,		 /* Dataplane FPM node. */
-	LINK_PARAMS_NODE,	/* Link-parameters node */
-	BGP_EVPN_VNI_NODE,       /* BGP EVPN VNI */
-	RPKI_NODE,     /* RPKI node for configuration of RPKI cache server
-			  connections.*/
-	BGP_FLOWSPECV4_NODE,	/* BGP IPv4 FLOWSPEC Address-Family */
-	BGP_FLOWSPECV6_NODE,	/* BGP IPv6 FLOWSPEC Address-Family */
-	BFD_NODE,		 /* BFD protocol mode. */
-	BFD_PEER_NODE,		 /* BFD peer configuration mode. */
-	BFD_PROFILE_NODE,	 /* BFD profile configuration mode. */
-	OPENFABRIC_NODE,	/* OpenFabric router configuration node */
-	VRRP_NODE,		 /* VRRP node */
-	BMP_NODE,		/* BMP config under router bgp */
-	NODE_TYPE_MAX, /* maximum */
+	AUTH_NODE,		/* Authentication mode of vty interface. */
+	VIEW_NODE,		/* View node. Default mode of vty interface. */
+	AUTH_ENABLE_NODE,       /* Authentication mode for change enable. */
+	ENABLE_NODE,		/* Enable node. */
+	CONFIG_NODE,		/* Config node. Default mode of config file. */
+	DEBUG_NODE,		/* Debug node. */
+	VRF_DEBUG_NODE,		/* Vrf Debug node. */
+	NORTHBOUND_DEBUG_NODE,  /* Northbound Debug node. */
+	DEBUG_VNC_NODE,		/* Debug VNC node. */
+	RMAP_DEBUG_NODE,	/* Route-map debug node */
+	RESOLVER_DEBUG_NODE,    /* Resolver debug node */
+	AAA_NODE,		/* AAA node. */
+	EXTLOG_NODE,		/* RFC5424 & co. extended syslog */
+	KEYCHAIN_NODE,		/* Key-chain node. */
+	KEYCHAIN_KEY_NODE,      /* Key-chain key node. */
+	IP_NODE,		/* Static ip route node. */
+	VRF_NODE,		/* VRF mode node. */
+	INTERFACE_NODE,		/* Interface mode node. */
+	NH_GROUP_NODE,		/* Nexthop-Group mode node. */
+	ZEBRA_NODE,		/* zebra connection node. */
+	TABLE_NODE,		/* rtm_table selection node. */
+	RIP_NODE,		/* RIP protocol mode node. */
+	RIPNG_NODE,		/* RIPng protocol mode node. */
+	BABEL_NODE,		/* BABEL protocol mode node. */
+	EIGRP_NODE,		/* EIGRP protocol mode node. */
+	BGP_NODE,		/* BGP protocol mode which includes BGP4+ */
+	BGP_VPNV4_NODE,		/* BGP MPLS-VPN PE exchange. */
+	BGP_VPNV6_NODE,		/* BGP MPLS-VPN PE exchange. */
+	BGP_IPV4_NODE,		/* BGP IPv4 unicast address family.  */
+	BGP_IPV4M_NODE,		/* BGP IPv4 multicast address family.  */
+	BGP_IPV4L_NODE,		/* BGP IPv4 labeled unicast address family.  */
+	BGP_IPV6_NODE,		/* BGP IPv6 address family */
+	BGP_IPV6M_NODE,		/* BGP IPv6 multicast address family. */
+	BGP_IPV6L_NODE,		/* BGP IPv6 labeled unicast address family. */
+	BGP_VRF_POLICY_NODE,    /* BGP VRF policy */
+	BGP_VNC_DEFAULTS_NODE,  /* BGP VNC nve defaults */
+	BGP_VNC_NVE_GROUP_NODE, /* BGP VNC nve group */
+	BGP_VNC_L2_GROUP_NODE,  /* BGP VNC L2 group */
+	RFP_DEFAULTS_NODE,      /* RFP defaults node */
+	BGP_EVPN_NODE,		/* BGP EVPN node. */
+	BGP_SRV6_NODE,		/* BGP SRv6 node. */
+	OSPF_NODE,		/* OSPF protocol mode */
+	OSPF6_NODE,		/* OSPF protocol for IPv6 mode */
+	LDP_NODE,		/* LDP protocol mode */
+	LDP_IPV4_NODE,		/* LDP IPv4 address family */
+	LDP_IPV6_NODE,		/* LDP IPv6 address family */
+	LDP_IPV4_IFACE_NODE,    /* LDP IPv4 Interface */
+	LDP_IPV6_IFACE_NODE,    /* LDP IPv6 Interface */
+	LDP_L2VPN_NODE,		/* LDP L2VPN node */
+	LDP_PSEUDOWIRE_NODE,    /* LDP Pseudowire node */
+	ISIS_NODE,		/* ISIS protocol mode */
+	ISIS_FLEX_ALGO_NODE,    /* ISIS Flex Algo mode */
+	ACCESS_NODE,		/* Access list node. */
+	PREFIX_NODE,		/* Prefix list node. */
+	ACCESS_IPV6_NODE,       /* Access list node. */
+	ACCESS_MAC_NODE,	/* MAC access list node*/
+	PREFIX_IPV6_NODE,       /* Prefix list node. */
+	AS_LIST_NODE,		/* AS list node. */
+	COMMUNITY_LIST_NODE,    /* Community list node. */
+	COMMUNITY_ALIAS_NODE,   /* Community alias node. */
+	RMAP_NODE,		/* Route map node. */
+	PBRMAP_NODE,		/* PBR map node. */
+	SMUX_NODE,		/* SNMP configuration node. */
+	DUMP_NODE,		/* Packet dump node. */
+	FORWARDING_NODE,	/* IP forwarding node. */
+	PROTOCOL_NODE,		/* protocol filtering node */
+	MPLS_NODE,		/* MPLS config node */
+	PW_NODE,		/* Pseudowire config node */
+	SEGMENT_ROUTING_NODE,   /* Segment routing root node */
+	SR_TRAFFIC_ENG_NODE,    /* SR Traffic Engineering node */
+	SR_SEGMENT_LIST_NODE,   /* SR segment list config node */
+	SR_POLICY_NODE,		/* SR policy config node */
+	SR_CANDIDATE_DYN_NODE,  /* SR dynamic candidate path config node */
+	PCEP_NODE,		/* PCEP node */
+	PCEP_PCE_CONFIG_NODE,   /* PCE shared configuration node */
+	PCEP_PCE_NODE,		/* PCE configuration node */
+	PCEP_PCC_NODE,		/* PCC configuration node */
+	SRV6_NODE,		/* SRv6 node */
+	SRV6_LOCS_NODE,		/* SRv6 locators node */
+	SRV6_LOC_NODE,		/* SRv6 locator node */
+	VTY_NODE,		/* Vty node. */
+	FPM_NODE,		/* Dataplane FPM node. */
+	LINK_PARAMS_NODE,       /* Link-parameters node */
+	BGP_EVPN_VNI_NODE,      /* BGP EVPN VNI */
+	RPKI_NODE,	   /* RPKI node for configuration of RPKI cache server
+				connections.*/
+	BGP_FLOWSPECV4_NODE, /* BGP IPv4 FLOWSPEC Address-Family */
+	BGP_FLOWSPECV6_NODE, /* BGP IPv6 FLOWSPEC Address-Family */
+	BFD_NODE,	    /* BFD protocol mode. */
+	BFD_PEER_NODE,       /* BFD peer configuration mode. */
+	BFD_PROFILE_NODE,    /* BFD profile configuration mode. */
+	OPENFABRIC_NODE,     /* OpenFabric router configuration node */
+	VRRP_NODE,	   /* VRRP node */
+	BMP_NODE,	    /* BMP config under router bgp */
+	NODE_TYPE_MAX,       /* maximum */
 };
 
 extern vector cmdvec;
