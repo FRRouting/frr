FRRouting/frr

commit 26b0598f6b6f55b33e225cb96012fa0e18b23e92

isisd: fix isis_route_merge_verify logic
This addresses two issues for L1L2 operation:

a) If an L1 route has ROUTE_ACTIVE unset and an L2 route for the same
destination has ROUTE_ACTIVE set, isisd would still put the L1 route
into the merged table. This causes the route for the destination to
get uninstalled from zebra until the next SPF run, which is incorrect.

To fix this, look at the ROUTE_ACTIVE flag and allow L2 routes to win
against L1 routes, when the L1 has ROUTE_ACTIVE unset.

b) If an L1 route wins against an existing L2 route, the ZEBRA_SYNCED
flag would remain on the L2 route. This leads to the problem that when
the L1 route disappears again, the L2 doesn't get reinstalled, since
isisd assumes it's already in the RIB because ZEBRA_SYNCED is set.

Solve this by clearing ZEBRA_SYNCED on L2 routes, if they lose against
an L1 route.

Signed-off-by: Christian Franke <chris@opensourcerouting.org>

isisd/isis_route.c
@@ -490,12 +490,37 @@ void isis_route_verify_merge(struct isis_area *area,
 	for (int level = ISIS_LEVEL1; level <= ISIS_LEVEL2; level++) {
 		for (rnode = route_top(tables[level - 1]); rnode;
 		     rnode = route_next(rnode)) {
-			if (rnode->info == NULL)
+			struct isis_route_info *rinfo = rnode->info;
+			if (!rinfo)
 				continue;
+
 			mrnode = route_node_get(merge, &rnode->p);
-			if (mrnode->info != NULL) {
+			struct isis_route_info *mrinfo = mrnode->info;
+			if (mrinfo) {
 				route_unlock_node(mrnode);
-				continue;
+				if (CHECK_FLAG(mrinfo->flag,
+					       ISIS_ROUTE_FLAG_ACTIVE)) {
+					/* Clear the ZEBRA_SYNCED flag on the
+					 * L2 route when L1 wins, otherwise L2
+					 * won't get reinstalled when L1
+					 * disappears.
+					 */
+					UNSET_FLAG(
+						rinfo->flag,
+						ISIS_ROUTE_FLAG_ZEBRA_SYNCED
+					);
+					continue;
+				} else {
+					/* Clear the ZEBRA_SYNCED flag on the L1
+					 * route when L2 wins, otherwise L1
+					 * won't get reinstalled when it
+					 * reappears.
+					 */
+					UNSET_FLAG(
+						mrinfo->flag,
+						ISIS_ROUTE_FLAG_ZEBRA_SYNCED
+					);
+				}
 			}
 			mrnode->info = rnode->info;
 		}

commit cce23f5e6ec7d1c8fee374bc1c9040b95f127def

isisd: fix merging flex-algo route nodes
As isis_route_verify_merge() builds a new route table, each (src,dst)
new node in the table is created by merging data from the same (src,dst)
pair present in all route tables.  However, as it iterates over those
route nodes and copies SR data into the newly created node, it repeatedly
overwrites the isis_route_info pointer.  This, in turn, pollutes the
isis_route_info structures in all of the other tables with partially
updated SR data.

After a route is deleted, this leaves pointers to freed memory in one
of the route trees.

Fixes: 26b0598f6b6f ("isisd: fix isis_route_merge_verify logic")
Signed-off-by: Eric Kinzie <ekinzie@labn.net>

isisd/isis_route.c
@@ -882,8 +882,9 @@ static void _isis_route_verify_merge(struct isis_area *area,
 						ISIS_ROUTE_FLAG_ZEBRA_SYNCED)) {
 					continue;
 				}
+			} else {
+				mrnode->info = rnode->info;
 			}
-			mrnode->info = rnode->info;
 		}
 	}
 