85521aaabd26d58939279440c0ebce5e69c84cae
Ryoga Saito <ryoga.saito@linecorp.com>
2022-09-20 18:24:50 +0900

    zebra: add default SRv6 Function length
    Add default SRv6 Function Length for usecases like SRv6 L3VPN. The
    default value (16) comes from the default Function length for SRv6
    L3VPN in BGPd.
    
    Signed-off-by: Ryoga Saito <ryoga.saito@linecorp.com>

--- zebra/zebra_srv6_vty.h
+++ zebra/zebra_srv6_vty.h
@@ -20,6 +20,10 @@
 #ifndef _ZEBRA_SRV6_VTY_H
 #define _ZEBRA_SRV6_VTY_H
 
+#define ZEBRA_SRV6_LOCATOR_BLOCK_LENGTH 40
+#define ZEBRA_SRV6_LOCATOR_NODE_LENGTH 24
+#define ZEBRA_SRV6_FUNCTION_LENGTH 16
+
 extern void zebra_srv6_vty_init(void);
 
 #endif /* _ZEBRA_SRV6_VTY_H */
