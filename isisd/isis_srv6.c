d52c6c88b25ff43034c9b585040c1c801eef4f22
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-11-28 10:34:10 +0100

    isisd: Add isis_srv6.[c, h] files
    Add `isis_srv6.c` and `isis_srv6.h` files needed to support SRv6 in IS-IS
    (as per RFC 9352).
    
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

---
+++ isisd/isis_srv6.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * This is an implementation of Segment Routing over IPv6 (SRv6) for IS-IS
+ * as per RFC 9352
+ * https://datatracker.ietf.org/doc/html/rfc9352
+ *
+ * Copyright (C) 2023 Carmine Scarpitta - University of Rome Tor Vergata
+ */
+
+#include <zebra.h>
---
+++ isisd/isis_srv6.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * This is an implementation of Segment Routing over IPv6 (SRv6) for IS-IS
+ * as per RFC 9352
+ * https://datatracker.ietf.org/doc/html/rfc9352
+ *
+ * Copyright (C) 2023 Carmine Scarpitta - University of Rome Tor Vergata
+ */
+
+#ifndef _FRR_ISIS_SRV6_H
+#define _FRR_ISIS_SRV6_H
+
+#endif /* _FRR_ISIS_SRV6_H */
