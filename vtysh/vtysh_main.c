8d472da36100a5b86f1d32d71b24c9163607daf8
Donald Sharp <sharpd@nvidia.com>
2022-12-21 08:04:34 -0500

    vtysh: Remove double retrieve of env VTYSH_HISTFILE
    The code is double checking the VTYSH_HISTFILE env variable,
    additionally clang-15 when running SA over it doesn't fully
    understand the code pattern.  Reduce the double check to
    one check to reduce program run-time (ha!) and make SA happy.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    (cherry picked from commit 99a9f25ce8c1e070f7f37c4cbd439c11bb7432eb)

--- vtysh/vtysh_main.c
+++ vtysh/vtysh_main.c
@@ -352,6 +352,7 @@ int main(int argc, char **argv, char **env)
 	const char *pathspace_arg = NULL;
 	char pathspace[MAXPATHLEN] = "";
 	const char *histfile = NULL;
+	const char *histfile_env = getenv("VTYSH_HISTFILE");
 
 	/* SUID: drop down to calling user & go back up when needed */
 	elevuid = geteuid();
@@ -609,10 +610,8 @@ int main(int argc, char **argv, char **env)
 	 * VTYSH_HISTFILE is preferred over command line
 	 * argument (-H/--histfile).
 	 */
-	if (getenv("VTYSH_HISTFILE")) {
-		const char *file = getenv("VTYSH_HISTFILE");
-
-		strlcpy(history_file, file, sizeof(history_file));
+	if (histfile_env) {
+		strlcpy(history_file, histfile_env, sizeof(history_file));
 	} else if (histfile) {
 		strlcpy(history_file, histfile, sizeof(history_file));
 	} else {
