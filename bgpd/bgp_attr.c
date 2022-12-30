FRRouting/frr

commit 722e8011e1e8e17e9f9c4c2dcf061ad4b1392982

bgpd: make bgp_packet_mpattr_start more prescriptive when using enum's
This function was just using default: case statements for
the encoding of nlri's to a peer.  Lay out all the different
cases and make things fail hard when a dev escape is found.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgp_attr.c
@@ -3876,7 +3876,10 @@ size_t bgp_packet_mpattr_start(struct stream *s, struct peer *peer, afi_t afi,
 				stream_putc(s, attr->mp_nexthop_len);
 				stream_put_ipv4(s, attr->nexthop.s_addr);
 			}
-		default:
+			break;
+		case SAFI_UNSPEC:
+		case SAFI_MAX:
+			assert(!"SAFI's UNSPEC or MAX being specified are a DEV ESCAPE");
 			break;
 		}
 		break;
@@ -3927,16 +3930,23 @@ size_t bgp_packet_mpattr_start(struct stream *s, struct peer *peer, afi_t afi,
 			break;
 		case SAFI_FLOWSPEC:
 			stream_putc(s, 0); /* no nexthop for flowspec */
-		default:
+			break;
+		case SAFI_UNSPEC:
+		case SAFI_MAX:
+			assert(!"SAFI's UNSPEC or MAX being specified are a DEV ESCAPE");
 			break;
 		}
 		break;
-	default:
+	case AFI_L2VPN:
 		if (safi != SAFI_FLOWSPEC)
 			flog_err(
 				EC_BGP_ATTR_NH_SEND_LEN,
 				"Bad nexthop when sending to %s, AFI %u SAFI %u nhlen %d",
 				peer->host, afi, safi, attr->mp_nexthop_len);
+		break;
+	case AFI_UNSPEC:
+	case AFI_MAX:
+		assert(!"DEV ESCAPE: AFI_UNSPEC or AFI_MAX should not be used here");
 		break;
 	}
 

commit 8d9492d645dbd96223ff7c1c022f3591e515cad3

bgpd: flowspec SAFI is never used with AFI_L2VPN
Remove code check about SAFI_FLOWSPEC when AFI is L2VPN,
as this case can never happen.

Fixes: 722e8011e1e8 ("Use switch statements without default for safi_t and afi_t enum's")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_attr.c
@@ -3951,11 +3951,10 @@ size_t bgp_packet_mpattr_start(struct stream *s, struct peer *peer, afi_t afi,
 		}
 		break;
 	case AFI_L2VPN:
-		if (safi != SAFI_FLOWSPEC)
-			flog_err(
-				EC_BGP_ATTR_NH_SEND_LEN,
-				"Bad nexthop when sending to %s, AFI %u SAFI %u nhlen %d",
-				peer->host, afi, safi, attr->mp_nexthop_len);
+		flog_err(
+			 EC_BGP_ATTR_NH_SEND_LEN,
+			 "Bad nexthop when sending to %s, AFI %u SAFI %u nhlen %d",
+			 peer->host, afi, safi, attr->mp_nexthop_len);
 		break;
 	case AFI_UNSPEC:
 	case AFI_MAX: