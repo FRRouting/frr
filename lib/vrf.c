30447578ec947516fda9eae79e9dc9e3bdfc3a12
Rafael Zalamena <rzalamena@opensourcerouting.org>
2023-01-19 10:42:01 -0300

    lib: remove dead logic code
    If we got inside the condition of `vrfp->status == VRF_ACTIVE` then
    don't make the same check again.
    
    Found by Coverity Scan (CID 1519760)
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- lib/vrf.c
+++ lib/vrf.c
@@ -1000,8 +1000,7 @@ lib_vrf_state_active_get_elem(struct nb_cb_get_elem_args *args)
 	struct vrf *vrfp = (struct vrf *)args->list_entry;
 
 	if (vrfp->status == VRF_ACTIVE)
-		return yang_data_new_bool(
-			args->xpath, vrfp->status == VRF_ACTIVE ? true : false);
+		return yang_data_new_bool(args->xpath, true);
 
 	return NULL;
 }
