c695bdbedb9b377755bc4fd7ca5215e8f9ba9758
David Lamparter <equinox@opensourcerouting.org>
2023-01-31 21:58:41 +0100

    lib: apply more `printf` attributes
    ... missed some functions in the earlier commits :(
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

--- lib/db.h
+++ lib/db.h
@@ -36,21 +36,24 @@
 #define _FRR_DB_H_
 #ifdef HAVE_SQLITE3
 
+#include "compiler.h"
 #include <sqlite3.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern int db_init(const char *path_fmt, ...);
+extern int db_init(const char *path_fmt, ...) PRINTFRR(1, 2);
 extern int db_close(void);
+/* WARNING: sqlite format string! not printf compatible! */
 extern int db_bindf(struct sqlite3_stmt *ss, const char *fmt, ...);
 extern struct sqlite3_stmt *db_prepare_len(const char *stmt, int stmtlen);
 extern struct sqlite3_stmt *db_prepare(const char *stmt);
 extern int db_run(struct sqlite3_stmt *ss);
+/* WARNING: sqlite format string! not scanf compatible! */
 extern int db_loadf(struct sqlite3_stmt *ss, const char *fmt, ...);
 extern void db_finalize(struct sqlite3_stmt **ss);
-extern int db_execute(const char *stmt_fmt, ...);
+extern int db_execute(const char *stmt_fmt, ...) PRINTFRR(1, 2);
 
 #ifdef __cplusplus
 }
