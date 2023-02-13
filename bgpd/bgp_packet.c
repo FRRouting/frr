bad2eb4c3e08dfc76651e5c397befe2e8e26ea6a
Maxence Younsi <mx.yns@outlook.fr>
2023-02-13 13:13:39 +0100

    bgpd, lib, doc: bmp adj-rib-out monitoring, bmp multipath support
    bgpd: changed bmp config to include bmp monitor rib-out
    bgpd: added rib-out pre-policy & post-policy monitoring
    bgpd: bmp sync for rib-out pre and post
    bgpd: added missing out-pre hook bump bmp sessions on bmp reconfigure to run sync immediately
    lib, bgpd: added pullwr_timeout, added bmp startup-delay command
    bgpd: bmp added bmp session state to bmp show
    doc: bmp updated documentation with new commands
    bgpd: bmp add rib-out post-policy stats
    bgpd: bmp add adj-rib-in stats
    bgpd: bmp add loc-rib stats
    
    bgpd: bmp multipath support for adj-rib-in pre-policy
    bgpd: bmp multipath support for adj-rib-out post-policy
    bgpd: bmp multipath support for adj-rib-in post-policy
    bgpd: bmp multipath support for loc-rib
    bgpd: bmp multipath support for rib-out-pre
    bgpd: bmp add multipath syncing, fix startup-delay cmd
    bgpd: bmp simplify bpi locking system, add show bmp locked
    bgpd: remove locked path in bqe, fix withdraw flag, fix lbpi hash & cmp
    bgpd: no addpath id in mpls vpn case
    bgpd: peer types and peer distinguisher in all bmp messages
    
    Signed-off-by: Maxence Younsi <mx.yns@outlook.fr>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -641,30 +641,10 @@ void bgp_keepalive_send(struct peer *peer)
 	bgp_writes_on(peer->connection);
 }
 
-/*
- * Creates a BGP Open packet and appends it to the peer's output queue.
- * Sets capabilities as necessary.
- */
-void bgp_open_send(struct peer_connection *connection)
+struct stream *bgp_open_make(struct peer *peer, uint16_t send_holdtime,
+				    as_t local_as, bool *ext_opt_params)
 {
-	struct stream *s;
-	uint16_t send_holdtime;
-	as_t local_as;
-	struct peer *peer = connection->peer;
-	bool ext_opt_params = false;
-
-	if (CHECK_FLAG(peer->flags, PEER_FLAG_TIMER))
-		send_holdtime = peer->holdtime;
-	else
-		send_holdtime = peer->bgp->default_holdtime;
-
-	/* local-as Change */
-	if (peer->change_local_as)
-		local_as = peer->change_local_as;
-	else
-		local_as = peer->local_as;
-
-	s = stream_new(BGP_STANDARD_MESSAGE_MAX_PACKET_SIZE);
+	struct stream *s = stream_new(BGP_STANDARD_MESSAGE_MAX_PACKET_SIZE);
 
 	/* Make open packet. */
 	bgp_packet_set_marker(s, BGP_MSG_OPEN);
@@ -678,17 +658,17 @@ void bgp_open_send(struct peer_connection *connection)
 
 	/* Set capabilities */
 	if (CHECK_FLAG(peer->flags, PEER_FLAG_EXTENDED_OPT_PARAMS)) {
-		ext_opt_params = true;
-		(void)bgp_open_capability(s, peer, ext_opt_params);
+		*ext_opt_params = true;
+		(void)bgp_open_capability(s, peer, *ext_opt_params);
 	} else {
 		struct stream *tmp = stream_new(STREAM_SIZE(s));
 
 		stream_copy(tmp, s);
-		if (bgp_open_capability(tmp, peer, ext_opt_params) >
+		if (bgp_open_capability(tmp, peer, *ext_opt_params) >
 		    BGP_OPEN_NON_EXT_OPT_LEN) {
 			stream_free(tmp);
-			ext_opt_params = true;
-			(void)bgp_open_capability(s, peer, ext_opt_params);
+			*ext_opt_params = true;
+			(void)bgp_open_capability(s, peer, *ext_opt_params);
 		} else {
 			stream_copy(s, tmp);
 			stream_free(tmp);
@@ -697,6 +677,34 @@ void bgp_open_send(struct peer_connection *connection)
 
 	/* Set BGP packet length. */
 	bgp_packet_set_size(s);
+
+	return s;
+}
+
+/*
+ * Creates a BGP Open packet and appends it to the peer's output queue.
+ * Sets capabilities as necessary.
+ */
+void bgp_open_send(struct peer_connection *connection)
+{
+	struct stream *s;
+	uint16_t send_holdtime;
+	as_t local_as;
+	bool ext_opt_params = false;
+	struct peer *peer = connection->peer;
+
+	if (CHECK_FLAG(peer->flags, PEER_FLAG_TIMER))
+		send_holdtime = peer->holdtime;
+	else
+		send_holdtime = peer->bgp->default_holdtime;
+
+	/* local-as Change */
+	if (peer->change_local_as)
+		local_as = peer->change_local_as;
+	else
+		local_as = peer->local_as;
+
+	s = bgp_open_make(peer, send_holdtime, local_as, &ext_opt_params);
 
 	if (bgp_debug_neighbor_events(peer))
 		zlog_debug("%pBP fd %d sending OPEN%s, version %d, my as %u, holdtime %d, id %pI4",
