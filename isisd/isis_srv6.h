057e9df2ccd3d43835a9cdf89d85849a583eba94
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-15 11:28:01 +0100

    isisd: Add SRv6 SID structure data structure
    Add `struct isis_srv6_sid_structure` data structure to represent an SRv6
    SID structure (as defined in RFC 8986 section #3.1).
    
    The struct has the following members:
    * loc_block_len: locator block length
    * loc_node_len: locator node length
    * func_len: function length
    * arg_len: argument length
    
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_srv6.h
+++ isisd/isis_srv6.h
@@ -18,6 +18,14 @@
 #define SRV6_MAX_H_ENCAPS 2
 #define SRV6_MAX_END_D 5
 
+/* SRv6 SID structure */
+struct isis_srv6_sid_structure {
+	uint8_t loc_block_len;
+	uint8_t loc_node_len;
+	uint8_t func_len;
+	uint8_t arg_len;
+};
+
 /* SRv6 SID not bound to any adjacency */
 struct isis_srv6_sid {
 	struct isis_srv6_sid *next;
