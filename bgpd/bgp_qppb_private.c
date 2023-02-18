753445f806dc422c2eaee0cca3d2df46d9e78caa
Volodymyr Huti <volodymyr.huti@gmail.com>
2023-02-18 17:11:43 +0200

    bgpd: Initial QPPB plugin for interacting with BPF mappings
    Check the documentation for more details:
    https://phabricator.vyos.net/T4180
    
    Signed-off-by: Volodymyr Huti <v.huti@vyos.io>

---
+++ bgpd/bgp_qppb_private.c
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * BGP QPPB support
+ * Copyright (C) 2023 VyOS Inc.
+ * Volodymyr Huti
+ */
+
+#include <zebra.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include "bgpd/bgpd.h"
+#include "log.h"
+#include "prefix.h"
+#include "privs.h"
+
+#define BPF_DSCP_MAP "dscp_map"
+#define BPF_PIN_DIR  "/sys/fs/bpf/"
+/*
+ * struct bpf_lpm_trie_key {
+ *      __u32 prefixlen;
+ *      __u8 data[0];
+ * };
+ * data[0] - stretchy buf, sizeof() doesn`t count for it
+ */
+#define BPF_LPM_KEY_SIZE (sizeof(struct bpf_lpm_trie_key) + sizeof(__u32))
+
+extern struct zebra_privs_t bgpd_privs;
+static int dscp_map_fd;
+
+static int open_bpf_map_file(const char *pin_dir, const char *mapname)
+{
+	char filename[PATH_MAX];
+	int len, fd;
+
+	len = snprintf(filename, PATH_MAX, "%s/%s", pin_dir, mapname);
+	if (len < 0) {
+		zlog_err("Failed constructing BPF map path");
+		return -1;
+	}
+
+	fd = bpf_obj_get(filename);
+	if (fd < 0)
+		zlog_err("Failed to open bpf map file [%s - err(%d):%s]",
+			 filename, errno, strerror(errno));
+	return fd;
+}
+
+static void bgp_qppb_map_init(void)
+{
+	const char *pin_dir = THIS_MODULE->load_args ?: BPF_PIN_DIR;
+
+	dscp_map_fd = open_bpf_map_file(pin_dir, BPF_DSCP_MAP);
+}
+
+static int bgp_qppb_mark_prefix(const struct prefix *p, uint8_t dscp, bool add)
+{
+	struct bpf_lpm_trie_key *key_ipv4;
+	int err = 0;
+
+	if (dscp_map_fd < 0)
+		return err;
+
+	key_ipv4 = alloca(BPF_LPM_KEY_SIZE);
+	key_ipv4->prefixlen = p->prefixlen;
+	memcpy(key_ipv4->data, &p->u.prefix4, sizeof(struct in_addr));
+
+	frr_with_privs (&bgpd_privs) {
+		err = add ? bpf_map_update_elem(dscp_map_fd, key_ipv4, &dscp, 0)
+			  : bpf_map_delete_elem(dscp_map_fd, key_ipv4);
+	}
+	zlog_info("QPPB %s prefix [%pFX| dscp %d, err %d]",
+		  add ? "mark" : "unmark", p, dscp, err);
+	return err;
+}
+
+static int bgp_qppb_module_init(void)
+{
+	bgp_qppb_map_init();
+	hook_register(bgp_qppb_mark_prefix, bgp_qppb_mark_prefix);
+	return 0;
+}
+
+FRR_MODULE_SETUP(.name = "bgp_vyos_qppb", .version = "0.0.1",
+		 .description = "bgp QPPB implementation for VyOS",
+		 .init = bgp_qppb_module_init);
