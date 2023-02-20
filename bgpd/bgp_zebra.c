8eb09e64d205ccb24223be5de254167ec96f61b6
Donatas Abraitis <donatas@opensourcerouting.org>
2023-02-20 17:34:26 +0200

    bgpd: Allow peering via 127.0.0.0/8
    There are some specific edge-cases when is a need to run FRR and another FRR
    and/or another BGP implementation on the same box. Relaxing 127.0.0.0/8 for
    this case might be reasonable.
    
    An example below peering via 127.0.0.0/8 between FRR and GoBGP:
    
    ```
    % ss -ntlp | grep 179
    LISTEN   0         4096              127.0.0.1:179              0.0.0.0:*
    LISTEN   0         128               127.0.0.2:179              0.0.0.0:*
    
    % grep 127.0.0.2 /etc/frr/daemons
    bgpd_options="   -A 127.0.0.1 -l 127.0.0.2"
    
    % grep local /etc/gobgp/config.toml
        local-address-list = ["127.0.0.1"]
    
    donatas-pc# sh ip bgp summary
    
    IPv4 Unicast Summary (VRF default):
    BGP router identifier 192.168.10.17, local AS number 65001 vrf-id 0
    BGP table version 0
    RIB entries 0, using 0 bytes of memory
    Peers 1, using 725 KiB of memory
    
    Neighbor        V         AS   MsgRcvd   MsgSent   TblVer  InQ OutQ  Up/Down State/PfxRcd   PfxSnt Desc
    127.0.0.1       4      65002         7         7        0    0    0 00:02:02            0        0 N/A
    
    Total number of neighbors 1
    donatas-pc#
    ```
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_zebra.c
+++ bgpd/bgp_zebra.c
@@ -834,6 +834,13 @@ bool bgp_zebra_nexthop_set(union sockunion *local, union sockunion *remote,
 						      peer->bgp->vrf_id);
 	}
 
+	/* Handle peerings via loopbacks. For instance, peer between
+	 * 127.0.0.1 and 127.0.0.2. In short, allow peering with self
+	 * via 127.0.0.0/8.
+	 */
+	if (!ifp && cmd_allow_reserved_ranges_get())
+		ifp = if_get_vrf_loopback(peer->bgp->vrf_id);
+
 	if (!ifp) {
 		/*
 		 * BGP views do not currently get proper data
