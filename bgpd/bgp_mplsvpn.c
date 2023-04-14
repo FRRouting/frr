bbd92c1cb61ae3ddb07f3ab74344f330806aee02
Philippe Guibert <philippe.guibert@6wind.com>
2023-04-14 13:50:31 +0200

    bgpd: notify bgp updates from default VRF upon SRv6 changes
    An L3VPN network can be configured on the main BGP instance,
    with an SRv6 SID. By declaring a network, the network is
    exported, and a VPNv6 update is created, but is not selected.
    
    The below BGP VPN configuration in the default VRF has been
    used:
    
    > router bgp 1
    >  segment-routing srv6
    >   locator loc1
    >  exit
    >  !
    >  address-family ipv6 unicast
    >   sid vpn export auto
    >   rd vpn export 1:30
    >   rt vpn both 77:77
    >   import vpn
    >   export vpn
    >   network 2001:7::/64
    >  exit-address-family
    
    The below BGP VPNv6 update is created, but is not selected.
    Also, no SRv6 SID is allocated.
    
    > # show bgp ipv6 vpn
    > [..]
    >    Network          Next Hop            Metric LocPrf Weight Path
    > Route Distinguisher: 1:30
    >    2001:7::/64      ::@0<                    0         32768 ?
    > # show bgp segment-routing srv6
    > [..]
    > bgps:
    > - name: default
    >   vpn_policy[AFI_IP].tovpn_sid: (null)
    >   vpn_policy[AFI_IP6].tovpn_sid: (null)
    >   per-vrf tovpn_sid: (null)
    >
    
    The SID in the main instance has not been updated, whereas
    in other BGP VRF instances, a SID is available. The update
    happens in the 'bgp_vpn_leak_postchange_all()' function.
    The main BGP instance is not parsed, so let us refresh the
    L3VPN updates of the default BGP instance too.
    
    This results in having a selected BGP VPNv6 update, with a
    SID.
    
    > # show bgp segment-routing srv6
    > [..]
    > bgps:
    > - name: default
    >   vpn_policy[AFI_IP].tovpn_sid: (null)
    >   vpn_policy[AFI_IP6].tovpn_sid: 2001:db8:1:1:100::
    >   per-vrf tovpn_sid: (null)
    
    Fixes: 3bd70bf8f37b ("bgpd: fix NULL dereference in vrf-vpn leak config if before default instance")
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -3746,7 +3746,8 @@ void vpn_leak_postchange_all(void)
 	/* First, do any exporting from VRFs to the single VPN RIB */
 	for (ALL_LIST_ELEMENTS_RO(bm->bgp, next, bgp)) {
 
-		if (bgp->inst_type != BGP_INSTANCE_TYPE_VRF)
+		if (bgp->inst_type != BGP_INSTANCE_TYPE_VRF &&
+		    bgp->inst_type != BGP_INSTANCE_TYPE_DEFAULT)
 			continue;
 
 		vpn_leak_postchange(
@@ -3765,7 +3766,8 @@ void vpn_leak_postchange_all(void)
 	/* Now, do any importing to VRFs from the single VPN RIB */
 	for (ALL_LIST_ELEMENTS_RO(bm->bgp, next, bgp)) {
 
-		if (bgp->inst_type != BGP_INSTANCE_TYPE_VRF)
+		if (bgp->inst_type != BGP_INSTANCE_TYPE_VRF &&
+		    bgp->inst_type != BGP_INSTANCE_TYPE_DEFAULT)
 			continue;
 
 		vpn_leak_postchange(
