00e94de9bea9f94d568f35cd237cec534efe2b94
David Lamparter <equinox@opensourcerouting.org>
2023-04-17 11:47:08 +0200

    pimd: fix null register before aging out reg-stop
    It looks like the code was trying to do this with the null_register
    parameter on pim_upstream_start_register_stop_timer(), but that didn't
    quite work right.  Restructure a bit to get it right.
    
    Signed-off-by: David Lamparter <equinox@opensourcerouting.org>
    (cherry picked from commit dce38da8061a7ac62c690dbb8a89cae7f9a758d6)
    
    # Conflicts:
    #	pimd/pim_upstream.c

--- pimd/pim_upstream.c
+++ pimd/pim_upstream.c
@@ -1693,7 +1693,13 @@ const char *pim_reg_state2str(enum pim_reg_state reg_state, char *state_str,
 	return state_str;
 }
 
+<<<<<<< HEAD
 static void pim_upstream_register_stop_timer(struct thread *t)
+=======
+static void pim_upstream_start_register_stop_timer(struct pim_upstream *up);
+
+static void pim_upstream_register_stop_timer(struct event *t)
+>>>>>>> dce38da80 (pimd: fix null register before aging out reg-stop)
 {
 	struct pim_interface *pim_ifp;
 	struct pim_instance *pim;
@@ -1739,7 +1745,7 @@ static void pim_upstream_register_stop_timer(struct thread *t)
 			return;
 		}
 		up->reg_state = PIM_REG_JOIN_PENDING;
-		pim_upstream_start_register_stop_timer(up, 1);
+		pim_upstream_start_register_stop_timer(up);
 
 		if (((up->channel_oil->cc.lastused / 100)
 		     > pim->keep_alive_time)
@@ -1757,25 +1763,15 @@ static void pim_upstream_register_stop_timer(struct thread *t)
 	}
 }
 
-void pim_upstream_start_register_stop_timer(struct pim_upstream *up,
-					    int null_register)
+static void pim_upstream_start_register_stop_timer(struct pim_upstream *up)
 {
 	uint32_t time;
 
 	THREAD_OFF(up->t_rs_timer);
 
-	if (!null_register) {
-		uint32_t lower = (0.5 * router->register_suppress_time);
-		uint32_t upper = (1.5 * router->register_suppress_time);
-		time = lower + (frr_weak_random() % (upper - lower + 1));
-		/* Make sure we don't wrap around */
-		if (time >= router->register_probe_time)
-			time -= router->register_probe_time;
-		else
-			time = 0;
-	} else
-		time = router->register_probe_time;
+	time = router->register_probe_time;
 
+<<<<<<< HEAD
 	if (PIM_DEBUG_PIM_TRACE) {
 		zlog_debug(
 			"%s: (S,G)=%s Starting upstream register stop timer %d",
@@ -1783,6 +1779,51 @@ void pim_upstream_start_register_stop_timer(struct pim_upstream *up,
 	}
 	thread_add_timer(router->master, pim_upstream_register_stop_timer, up,
 			 time, &up->t_rs_timer);
+=======
+	if (PIM_DEBUG_PIM_TRACE)
+		zlog_debug("%s: (S,G)=%s Starting upstream register stop timer %d",
+			   __func__, up->sg_str, time);
+	event_add_timer(router->master, pim_upstream_register_stop_timer, up,
+			time, &up->t_rs_timer);
+>>>>>>> dce38da80 (pimd: fix null register before aging out reg-stop)
+}
+
+static void pim_upstream_register_probe_timer(struct event *t)
+{
+	struct pim_upstream *up = EVENT_ARG(t);
+
+	if (!up->rpf.source_nexthop.interface ||
+	    !up->rpf.source_nexthop.interface->info) {
+		if (PIM_DEBUG_PIM_REG)
+			zlog_debug("cannot send Null register for %pSG, no path to RP",
+				   &up->sg);
+	} else
+		pim_null_register_send(up);
+
+	pim_upstream_start_register_stop_timer(up);
+}
+
+void pim_upstream_start_register_probe_timer(struct pim_upstream *up)
+{
+	uint32_t time;
+
+	EVENT_OFF(up->t_rs_timer);
+
+	uint32_t lower = (0.5 * router->register_suppress_time);
+	uint32_t upper = (1.5 * router->register_suppress_time);
+	time = lower + (frr_weak_random() % (upper - lower + 1));
+	/* Make sure we don't wrap around */
+	if (time >= router->register_probe_time)
+		time -= router->register_probe_time;
+	else
+		time = 0;
+
+	if (PIM_DEBUG_PIM_TRACE)
+		zlog_debug("%s: (S,G)=%s Starting upstream register stop null probe timer %d",
+			   __func__, up->sg_str, time);
+
+	event_add_timer(router->master, pim_upstream_register_probe_timer, up,
+			time, &up->t_rs_timer);
 }
 
 int pim_upstream_inherited_olist_decide(struct pim_instance *pim,
