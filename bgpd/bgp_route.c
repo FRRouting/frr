cd845be6a7e32d6b65bcf6fda64df03f688a4f54
Chirag Shah <chirag@nvidia.com>
2023-05-18 21:49:03 -0700

    bgpd: fix aggregate route best path select
    In ebgp+ ibgp deployment aggregate summary-only route
    selected path should always be locally originated
    summary route.
    When aggregate route summary-only config is removed
    The selected path is iBGP peer as its lower cost
    Upon reconfiguring aggregate route summary-only,
    the locally originated is not selected due to
    always choosing first path attribute and bailing
    out as no change in route update.
    
    Ticket:#3467890
    Issue:3467890
    Testing Done:
    
    Config:
    ------
    TORC11(config-router)#router bgp
    TORC11(config-router)# address-family ipv4 unicast
    TORC11(config-router-af)# aggregate-address 184.123.0.0/16
            summary-only
    TORC11(config-router-af)# no aggregate-address 184.123.0.0/16
            summary-only
    TORC11(config-router-af)# aggregate-address 184.123.0.0/16
            summary-only
    
    Before fix:
    -----------
    *> 184.123.0.0/16   ::(TORC11)               0         32768 i
    *                   uplink1                                0 4435 5546 i
    *                   uplink2                                0 4435 5546 i
    * i                 peerlink-3               0    100      0 i
    
    After fix:
    ----------
    *> 184.123.0.0/16   ::(TORC11)               0         32768 i
    * i                 peerlink-3               0    100      0 i
    *                   uplink2                                0 4435 5546 i
    *                   uplink1                                0 4435 5546 i
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -7477,8 +7477,8 @@ static void bgp_aggregate_install(
 		 * If the aggregate information has not changed
 		 * no need to re-install it again.
 		 */
-		if (bgp_aggregate_info_same(orig, origin, aspath, community,
-					    ecommunity, lcommunity)) {
+		if (pi && bgp_aggregate_info_same(pi, origin, aspath, community,
+						  ecommunity, lcommunity)) {
 			bgp_dest_unlock_node(dest);
 
 			if (aspath)
