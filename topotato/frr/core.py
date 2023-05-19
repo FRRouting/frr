628e3456f1640927c8257856f87c2a781fe8de84
David Lamparter <equinox@diac24.net>
2023-05-19 17:51:33 +0200

    topotato: split off frr.templating
    move TemplateUtils into .templating submodule.
    
    Signed-off-by: David Lamparter <equinox@diac24.net>

--- topotato/frr/core.py
+++ topotato/frr/core.py
@@ -17,27 +17,19 @@ import socket
 import struct
 import sys
 import time
-from dataclasses import dataclass
 import typing
 from typing import (
     Any,
-    Callable,
     ClassVar,
     Dict,
     FrozenSet,
     Generator,
     List,
-    Literal,
     Mapping,
     Optional,
-    Set,
-    Tuple,
-    Union,
-    cast,
 )
 
 import pytest
-from .. import jinlinja
 
 from ..defer import subprocess
 from ..utils import deindent, get_dir, EnvcheckResult
@@ -46,37 +38,13 @@ from .livelog import LiveLog
 from ..exceptions import TopotatoDaemonCrash
 from ..pcapng import Context
 from ..network import TopotatoNetwork
+from .templating import TemplateUtils, jenv
 
 if typing.TYPE_CHECKING:
     from .. import toponom
 
 
 logger = logging.getLogger("topotato")
-
-jenv = jinlinja.InlineEnv()
-
-# TBD: might be more accessible to just put these in a templates/ dir
-_templates = {
-    "boilerplate.conf": """
-        log record-priority
-        log timestamp precision 6
-        !
-        hostname {{ router.name }}
-        service advanced-vty
-        !
-        #% block main
-        #% endblock
-        !
-        line vty
-        !
-        """.replace(
-        "\n        ", "\n"
-    ).lstrip(
-        "\n"
-    ),
-}
-
-jenv.register_templates(_templates.items())
 
 
 class FRRSetupError(EnvironmentError):
@@ -290,59 +258,6 @@ class FRRConfigs(dict):
         self.frr = frr
         self.daemons = frr.daemons
 
-    @dataclass
-    class TemplateUtils:
-        configs: "FRRConfigs"
-        router: "toponom.Router"
-        daemon: str
-
-        def static_route_for(
-            self,
-            dst: "toponom.AnyNetwork",
-            *,
-            rtr_filter: Callable[["toponom.Router"], bool] = lambda nbr: True,
-        ):
-            """
-            Calculate and output a staticd route for given destination.
-
-            Current router is used as starting point.  Uses a simple
-            breath-first search, only one route will be output (no ECMP.)
-            If the destination is directly connected, output is a comment.
-            """
-            visited: Set["toponom.Router"] = set()
-            queue: List[
-                Tuple[
-                    "toponom.Router",
-                    List[Tuple["toponom.LinkIface", "toponom.LinkIface"]],
-                ]
-            ] = [(self.router, [])]
-
-            assert dst.version in [4, 6]
-            ipv = cast(Union[Literal[4], Literal[6]], dst.version)
-
-            while queue:
-                rtr, path = queue.pop(0)
-                if rtr in visited:
-                    continue
-                visited.add(rtr)
-                for addr in rtr.addrs(ipv):
-                    if dst == addr.network:
-                        if not path:
-                            return f"! {dst!s} is directly connected"
-                        if_self, if_other = path[0]
-                        if dst.version == 6:
-                            return (
-                                f"ipv6 route {dst!s} {if_other.ll6!s} {if_self.ifname}"
-                            )
-                        return (
-                            f"ip route {dst!s} {if_other.ip4[0].ip!s} {if_self.ifname}"
-                        )
-                for iface, nbr_iface, nbr in rtr.neighbors(rtr_filter=rtr_filter):
-                    nbr_path = path + [(iface, nbr_iface)]
-                    queue.append((nbr, nbr_path))
-
-            raise RuntimeError(f"no route for {dst!r} on {self.router!r}")
-
     def generate(self):
         """
         Render and fill in the actual templates.
@@ -366,7 +281,7 @@ class FRRConfigs(dict):
                         router=router,
                         routers=rtrmap,
                         topo=topo,
-                        frr=self.TemplateUtils(self, router, daemon),
+                        frr=TemplateUtils(router, daemon),
                     )
         return self
 
