56b895c17242a608731b6b03a4909f258805caee
Abhinay Ramesh <rabhinay@vmware.com>
2023-07-03 09:40:11 +0000

    bgpd: fix bgp evpn cli memory leaks.
    problem:
    In CLI config codeflow there are memory leaks in failure scenario
    
    Fix:
    Code changes are done to free ecommunity
    
    Signed-off-by: Abhinay Ramesh <rabhinay@vmware.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -8587,6 +8587,8 @@ DEFPY (neighbor_soo,
 		ecommunity_free(&peer->soo[afi][safi]);
 		peer->soo[afi][safi] = ecomm_soo;
 		peer_af_flag_unset(peer, afi, safi, PEER_FLAG_SOO);
+	} else {
+		ecommunity_free(&ecomm_soo);
 	}
 
 	return bgp_vty_return(vty,
