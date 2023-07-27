b48d04099998e59f6c209da29565749c0f6f0b0b
Rajesh Varatharaj <rvaratharaj@nvidia.com>
2023-07-26 23:57:04 -0700

    pimd: Fix for FHR mroute taking longer to age out
    Issue:
    When there is no traffic for a group, the LHR and RP take the default KAT+Join timer expiry of
    a maximum of 480 seconds to clear the S,G . However, in the FHR, we update the state from JOINED
    to NOT Joined, downstream state from PPto NOINFO.  This restarts the ET timer, causing S,G on FHR to
    take more than 10 minutes to age out.
    
    In other words,
    Consider a case where (S,G) is in Join state. When the traffic stops and the KAT (210) expires,
     the Join expiry timer restarts. At this time, if we receive a prune, the expectation is to set
     PPT to 0 (RFC 4601 sec 4.5.2).
     When the PPT expires, we move to the noinfo state and restart the expiry timer one more time. We remove the
     (S,G) entry only after ~10 minutes when there is no active traffic.
    
    Summary:
    KAT Join ET 210 + PP ET 210 + NOINFO ET 210.
    
    Solution:
    Delete the ifchannel when in noinfo state, and KAT is not running.
    
    Ticket: #13703
    
    Signed-off-by: Rajesh Varatharaj <rvaratharaj@nvidia.com>
    (cherry picked from commit afed39ea2be25bf30d50ac49b4edf424deadcb17)

--- pimd/pim_upstream.c
+++ pimd/pim_upstream.c
@@ -316,7 +316,7 @@ static void on_join_timer(struct thread *t)
 	}
 
 	/*
-	 * In the case of a HFR we will not ahve anyone to send this to.
+	 * In the case of a FHR we will not ahve anyone to send this to.
 	 */
 	if (PIM_UPSTREAM_FLAG_TEST_FHR(up->flags))
 		return;
