0e1bf005dc847b5a8fd329dccd6b5c2d5951ab52
G. Paul Ziemba <paulz@labn.net>
2023-07-30 11:52:57 -0700

    lib: pbr.h: remove unused PBR_FILTER_PROTO
    Signed-off-by: G. Paul Ziemba <paulz@labn.net>

--- lib/pbr.h
+++ lib/pbr.h
@@ -34,17 +34,17 @@ struct pbr_filter {
 #define PBR_FILTER_SRC_PORT       (1 << 2)
 #define PBR_FILTER_DST_PORT       (1 << 3)
 #define PBR_FILTER_FWMARK         (1 << 4)
-#define PBR_FILTER_PROTO          (1 << 5)
+#define PBR_FILTER_IP_PROTOCOL	  (1 << 5)
 #define PBR_FILTER_SRC_PORT_RANGE (1 << 6)
 #define PBR_FILTER_DST_PORT_RANGE (1 << 7)
 #define PBR_FILTER_DSFIELD        (1 << 8)
-#define PBR_FILTER_IP_PROTOCOL    (1 << 9)
 #define PBR_FILTER_PCP            (1 << 10)
 #define PBR_FILTER_VLAN_FLAGS     (1 << 11)
 #define PBR_FILTER_VLAN_ID        (1 << 12)
 
 #define PBR_DSFIELD_DSCP (0xfc) /* Upper 6 bits of DS field: DSCP */
 #define PBR_DSFIELD_ECN (0x03)	/* Lower 2 bits of DS field: BCN */
+
 #define PBR_PCP (0x07)		/* 3-bit value 0..7 for prioritization*/
 
 #define PBR_VLAN_FLAGS_NO_WILD	  0
