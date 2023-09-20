FRRouting/frr

commit d33bd631262d60a7c0a2d56a2fe143bff45e32f0

bgpd: fix coverity issue in bgpd
Should address this issue:
** CID 1566843:  Uninitialized variables  (UNINIT)
/bgpd/bgp_route.c: 6754 in bgp_static_set()
6748                            bgp_static->backdoor = backdoor;
6749                            bgp_static->valid = 0;
6750                            bgp_static->igpmetric = 0;
6751                            bgp_static->igpnexthop.s_addr = INADDR_ANY;
6752                            bgp_static->label_index = label_index;
6753                            bgp_static->label = label;
>>>     CID 1566843:  Uninitialized variables  (UNINIT)
>>>     Using uninitialized value prd.
6754                            bgp_static->prd = prd;
6755
6756                            if (rmap) {
6757                                    XFREE(MTYPE_ROUTE_MAP_NAME,
6758                                          bgp_static->rmap.name);
6759                                    route_map_counter_decrement(

Testing Done:
 build

Ticket: #NA
Signed-off-by: Rajesh Varatharaj <rvaratharaj@nvidia.com>

bgpd/bgp_route.c
@@ -6610,6 +6610,7 @@ int bgp_static_set(struct vty *vty, bool negate, const char *ip_str,
 	}
 
 	if (safi == SAFI_MPLS_VPN || safi == SAFI_EVPN) {
+		memset(&prd, 0, sizeof(prd));
 		ret = str2prefix_rd(rd_str, &prd);
 		if (!ret) {
 			vty_out(vty, "%% Malformed rd\n");

commit cbbdcee7a80a23bbb49ba0d3de5a1e0aa4a35fe1

bgpd: Initialise prd despite if it's safi-related or not
Fixes: d33bd631262d60a7c0a2d56a2fe143bff45e32f0 ("bgpd: fix coverity issue in bgpd")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_route.c
@@ -6597,7 +6597,7 @@ int bgp_static_set(struct vty *vty, bool negate, const char *ip_str,
 	int ret;
 	struct prefix p;
 	struct bgp_static *bgp_static;
-	struct prefix_rd prd;
+	struct prefix_rd prd = {};
 	struct bgp_dest *pdest;
 	struct bgp_dest *dest;
 	struct bgp_table *table;
@@ -6626,7 +6626,6 @@ int bgp_static_set(struct vty *vty, bool negate, const char *ip_str,
 	}
 
 	if (safi == SAFI_MPLS_VPN || safi == SAFI_EVPN) {
-		memset(&prd, 0, sizeof(prd));
 		ret = str2prefix_rd(rd_str, &prd);
 		if (!ret) {
 			vty_out(vty, "%% Malformed rd\n");