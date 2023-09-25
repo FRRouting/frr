5c91087850221229cbe725b11033c882ffe9bcba
Pooja Jagadeesh Doijode <pdoijode@nvidia.com>
2023-09-25 15:32:18 -0700

    bgpd: [EVPN GR] Unimport and delete stale l2vpn evpn route
    On GR helper, when the peer that has GR enabled goes down,
    BGP will mark all the L2VPN EVPN routes in default VRF as stale.
    
    If the peer doesn't comeup before GR restart timer expires, then
    the L2VPN EVPN routes in default VRF that were previously marked
    stale needs to be unimported from all the VRFs and VNIs (IP and MAC
    table) before deleting it in L2VPN EVPN table in default VRF.
    
    Signed-off-by: Pooja Jagadeesh Doijode <pdoijode@nvidia.com>

--- tools/frr_babeltrace.py
+++ tools/frr_babeltrace.py
@@ -203,6 +203,7 @@ def location_gr_client_not_found(field_val):
     elif field_val == 2:
         return "Stale route delete from table"
 
+
 ############################ common parsers - end #############################
 
 
