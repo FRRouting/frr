FRRouting/frr

commit 3c844979431822386bcc2c8728a83513528675b2

zebra: label manager should never return a reserved block
if the requested chunk size was less than 16 then a chunk
within the reserved block would be returned. Make sure that
we never return labels that are below MPLS_LABEL_UNRESERVED_MIN

Signed-off-by: Pat Ruddy <pat@voltanet.io>

zebra/label_manager.c
@@ -313,7 +313,7 @@ static struct label_manager_chunk *assign_label_chunk(uint8_t proto,
 {
 	struct label_manager_chunk *lmc;
 	struct listnode *node;
-	uint32_t prev_end = 0;
+	uint32_t prev_end = MPLS_LABEL_UNRESERVED_MIN;
 
 	/* handle chunks request with a specific base label */
 	if (base != MPLS_LABEL_BASE_ANY)

commit b71370e83fe999b089ac7a0eecf206ab60bcd55e

zebra: fix label allocation when room space before first chunk
After ISIS first allocates a label chunk at [1000;2000],
the '16' label value is not used when BGP tries to
allocate a label chunk in auto mode. This does not happen
when BGP is the only one to do the label allocation.

When a label chunk has been accepted, the next label
request checks if there is room space before the existing
label chunk, and uses the lower label value to 17, and not
16.

Fix this by changing the previous range end 'prev_end' label
value to 15 which is the end of the reserved MPLS label
range.

Fixes: 3c8449794318 ("zebra: label manager should never return a reserved block")
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/label_manager.c
@@ -314,7 +314,7 @@ assign_label_chunk(uint8_t proto, unsigned short instance, uint32_t session_id,
 {
 	struct label_manager_chunk *lmc;
 	struct listnode *node;
-	uint32_t prev_end = MPLS_LABEL_UNRESERVED_MIN;
+	uint32_t prev_end = MPLS_LABEL_UNRESERVED_MIN - 1;
 
 	/* handle chunks request with a specific base label
 	 * - static label requests: BGP hardset value, Pathd