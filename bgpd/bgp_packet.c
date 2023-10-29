b21e7b8fe8c4970f2f6f794cb78bcc93057af94f
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-29 22:44:45 +0200

    bgpd: Ignore handling NLRIs if we received MP_UNREACH_NLRI
    If we receive MP_UNREACH_NLRI, we should stop handling remaining NLRIs if
    no mandatory path attributes received.
    
    In other words, if MP_UNREACH_NLRI received, the remaining NLRIs should be handled
    as a new data, but without mandatory attributes, it's a malformed packet.
    
    In normal case, this MUST not happen at all, but to avoid crashing bgpd, we MUST
    handle that.
    
    Reported-by: Iggy Frankovic <iggyfran@amazon.com>
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>
    (cherry picked from commit c37119df45bbf4ef713bc10475af2ee06e12f3bf)

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -1951,7 +1951,12 @@ static int bgp_update_receive(struct peer *peer, bgp_size_t size)
 	/* Network Layer Reachability Information. */
 	update_len = end - stream_pnt(s);
 
-	if (update_len && attribute_len) {
+	/* If we received MP_UNREACH_NLRI attribute, but also NLRIs, then
+	 * NLRIs should be handled as a new data. Though, if we received
+	 * NLRIs without mandatory attributes, they should be ignored.
+	 */
+	if (update_len && attribute_len &&
+	    attr_parse_ret != BGP_ATTR_PARSE_MISSING_MANDATORY) {
 		/* Set NLRI portion to structure. */
 		nlris[NLRI_UPDATE].afi = AFI_IP;
 		nlris[NLRI_UPDATE].safi = SAFI_UNICAST;
