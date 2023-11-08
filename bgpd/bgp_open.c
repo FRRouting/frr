89e6b4f390c96083c9404de94ba9fb96f4bc61b1
Donatas Abraitis <donatas@opensourcerouting.org>
2023-11-08 15:29:32 +0200

    bgpd: Send software version capability for OAD EBGP peers
    We send this capability for iBGP peers by default. Recently OAD support was
    merged, and we should adopt sending the capability according to OAD as well.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_open.c
+++ bgpd/bgp_open.c
@@ -1938,7 +1938,7 @@ uint16_t bgp_open_capability(struct stream *s, struct peer *peer,
 	 */
 	if (peergroup_flag_check(peer, PEER_FLAG_CAPABILITY_SOFT_VERSION) ||
 	    CHECK_FLAG(peer->bgp->flags, BGP_FLAG_SOFT_VERSION_CAPABILITY) ||
-	    peer->sort == BGP_PEER_IBGP) {
+	    peer->sort == BGP_PEER_IBGP || peer->sub_sort == BGP_PEER_EBGP_OAD) {
 		SET_FLAG(peer->cap, PEER_CAP_SOFT_VERSION_ADV);
 		stream_putc(s, BGP_OPEN_OPT_CAP);
 		rcapp = stream_get_endp(s);
