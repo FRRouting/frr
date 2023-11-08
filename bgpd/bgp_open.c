6af683039f8d7dca0d09bb4c613c9b44e7c30b60
Donatas Abraitis <donatas@opensourcerouting.org>
2023-11-08 15:26:40 +0200

    bgpd: Set the software version capability received flag only after a validation
    We shouldn't set it blindly once the packet is received, but first we have to
    do some sanity checks.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_open.c
+++ bgpd/bgp_open.c
@@ -896,8 +896,6 @@ static int bgp_capability_software_version(struct peer *peer,
 	size_t end = stream_get_getp(s) + hdr->length;
 	uint8_t len;
 
-	SET_FLAG(peer->cap, PEER_CAP_SOFT_VERSION_RCV);
-
 	len = stream_getc(s);
 	if (stream_get_getp(s) + len > end) {
 		flog_warn(
@@ -906,6 +904,8 @@ static int bgp_capability_software_version(struct peer *peer,
 			__func__, peer->host);
 		return -1;
 	}
+
+	SET_FLAG(peer->cap, PEER_CAP_SOFT_VERSION_RCV);
 
 	if (len > BGP_MAX_SOFT_VERSION) {
 		flog_warn(EC_BGP_CAPABILITY_INVALID_LENGTH,
