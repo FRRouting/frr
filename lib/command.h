a22f5a5289a59ef01986616a34ee863fe24bd874
Igor Ryzhov <iryzhov@nfware.com>
2023-11-15 02:14:22 +0100

    mgmtd: add node for writing configuration
    Signed-off-by: Igor Ryzhov <iryzhov@nfware.com>

--- lib/command.h
+++ lib/command.h
@@ -176,6 +176,7 @@ enum node_type {
 	BMP_NODE,		/* BMP config under router bgp */
 	ISIS_SRV6_NODE,    /* ISIS SRv6 node */
 	ISIS_SRV6_NODE_MSD_NODE,    /* ISIS SRv6 Node MSDs node */
+	MGMTD_NODE,		 /* MGMTD node. */
 	NODE_TYPE_MAX, /* maximum */
 };
 /* clang-format on */
