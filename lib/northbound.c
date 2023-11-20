FRRouting/frr

commit 51a2a4b3f471eba2f4416865ed19a986a6aca9f3

lib: print all errors when validating a config
Signed-off-by: Igor Ryzhov <iryzhov@nfware.com>

lib/northbound.c
@@ -871,9 +871,14 @@ int nb_candidate_update(struct nb_config *candidate)
 int nb_candidate_validate_yang(struct nb_config *candidate, bool no_state,
 			       char *errmsg, size_t errmsg_len)
 {
-	if (lyd_validate_all(&candidate->dnode, ly_native_ctx,
-			     no_state ? LYD_VALIDATE_NO_STATE
-				      : LYD_VALIDATE_PRESENT,
+	uint32_t options = LYD_VALIDATE_MULTI_ERROR;
+
+	if (no_state)
+		SET_FLAG(options, LYD_VALIDATE_NO_STATE);
+	else
+		SET_FLAG(options, LYD_VALIDATE_PRESENT);
+
+	if (lyd_validate_all(&candidate->dnode, ly_native_ctx, options,
 			     NULL) != 0) {
 		yang_print_errors(ly_native_ctx, errmsg, errmsg_len);
 		return NB_ERR_VALIDATION;

commit 75a7b5b6b64fde187acdfde67e60396f7634664c

lib: use LYD_VALIDATE_MULTI_ERROR only if available
LYD_VALIDATE_MULTI_ERROR was added in libyang 2.1.36.  The currently
enforced minimum of libyang is 2.0.0.  Stick an #ifdef around it.

Fixes: 51a2a4b3f471 ("lib: print all errors when validating a config")
Cc: Igor Ryzhov <iryzhov@nfware.com>
Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

lib/northbound.c
@@ -871,7 +871,12 @@ int nb_candidate_update(struct nb_config *candidate)
 int nb_candidate_validate_yang(struct nb_config *candidate, bool no_state,
 			       char *errmsg, size_t errmsg_len)
 {
-	uint32_t options = LYD_VALIDATE_MULTI_ERROR;
+	uint32_t options = 0;
+
+#ifdef LYD_VALIDATE_MULTI_ERROR
+	/* libyang 2.1.36+ */
+	options |= LYD_VALIDATE_MULTI_ERROR;
+#endif
 
 	if (no_state)
 		SET_FLAG(options, LYD_VALIDATE_NO_STATE);