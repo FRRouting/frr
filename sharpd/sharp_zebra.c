FRRouting/frr

commit 5a9c0931aa9571b875db8658ced6f1dc6f10a2b1

sharpd: don't send invalid nexthop-groups to zebra
Ensure that there are valid (resolved) nexthops, and no
invalid backup nexthops, in nhgs sent to zebra for
installation.

Signed-off-by: Mark Stapp <mjs@voltanet.io>

sharpd/sharp_zebra.c
@@ -539,6 +539,7 @@ void nhg_add(uint32_t id, const struct nexthop_group *nhg,
 	struct zapi_nhg api_nhg = {};
 	struct zapi_nexthop *api_nh;
 	struct nexthop *nh;
+	bool is_valid = true;
 
 	api_nhg.id = id;
 	for (ALL_NEXTHOPS_PTR(nhg, nh)) {
@@ -549,10 +550,23 @@ void nhg_add(uint32_t id, const struct nexthop_group *nhg,
 			break;
 		}
 
+		/* Unresolved nexthops will lead to failure - only send
+		 * nexthops that zebra will consider valid.
+		 */
+		if (nh->ifindex == 0)
+			continue;
+
 		api_nh = &api_nhg.nexthops[api_nhg.nexthop_num];
 
 		zapi_nexthop_from_nexthop(api_nh, nh);
 		api_nhg.nexthop_num++;
+	}
+
+	if (api_nhg.nexthop_num == 0) {
+		zlog_debug("%s: nhg %u not sent: no valid nexthops",
+			   __func__, id);
+		is_valid = false;
+		goto done;
 	}
 
 	if (backup_nhg) {
@@ -563,6 +577,20 @@ void nhg_add(uint32_t id, const struct nexthop_group *nhg,
 					__func__);
 				break;
 			}
+
+			/* Unresolved nexthop: will be rejected by zebra.
+			 * That causes a problem, since the primary nexthops
+			 * rely on array indexing into the backup nexthops. If
+			 * that array isn't valid, the backup indexes won't be
+			 * valid.
+			 */
+			if (nh->ifindex == 0) {
+				zlog_debug("%s: nhg %u: invalid backup nexthop",
+					   __func__, id);
+				is_valid = false;
+				break;
+			}
+
 			api_nh = &api_nhg.backup_nexthops
 					  [api_nhg.backup_nexthop_num];
 
@@ -571,7 +599,9 @@ void nhg_add(uint32_t id, const struct nexthop_group *nhg,
 		}
 	}
 
-	zclient_nhg_send(zclient, ZEBRA_NHG_ADD, &api_nhg);
+done:
+	if (is_valid)
+		zclient_nhg_send(zclient, ZEBRA_NHG_ADD, &api_nhg);
 }
 
 void nhg_del(uint32_t id)

commit 82beaf6ae520f1c969755ae0ee82d2af2f150927

sharpd: fix deleting nhid when suppressing nexthop from nh group
When removing the last nexthop from a nexthop-group, the nexthop
group remains in the zebra contexts:

> ubuntu2204(config)# nexthop-group gdgd
> 2023/11/23 14:06:36 SHARP: [Q5NBA-GN1BG] NHG ID assigned: 179687502
> ubuntu2204(config-nh-group)# nexthop 192.0.2.7 loop1
> ubuntu2204(config-nh-group)# 2023/11/23 14:06:38 ZEBRA: [VNMVB-91G3G] _netlink_nexthop_build_group: ID (179687502): group 338
> 2023/11/23 14:06:38 ZEBRA: [R43C6-KYHWT] netlink_nexthop_msg_encode: RTM_NEWNEXTHOP, id=179687502
> 2023/11/23 14:06:38 ZEBRA: [HYEHE-CQZ9G] nl_batch_send: netlink-dp (NS 0), batch size=44, msg cnt=1
> 2023/11/23 14:06:38 SHARP: [JWRCN-N9K90] Installed nhg 179687502
>
> ubuntu2204(config-nh-group)# no nexthop 192.0.2.7 loop1
> 2023/11/23 14:06:47 SHARP: [Y2G2F-ZTW6M] nhg_add: nhg 179687502 not sent: no valid nexthops
> ubuntu2204(config-nh-group)# do show nexthop-group rib 179687502
> ID: 179687502 (sharp)
>      RefCnt: 1
>      Uptime: 00:00:23
>      VRF: default
>      Valid, Installed
>      Depends: (338)
>            via 192.0.2.7, loop1 (vrf default), weight 1

Fix this by sending an NHG_DEL message when no nexthops are attached,
and when the id was already installed.

Fixes: 5a9c0931aa95 ("sharpd: don't send invalid nexthop-groups to zebra")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

sharpd/sharp_zebra.c
@@ -563,9 +563,15 @@ void nhg_add(uint32_t id, const struct nexthop_group *nhg,
 	}
 
 	if (api_nhg.nexthop_num == 0) {
-		zlog_debug("%s: nhg %u not sent: no valid nexthops", __func__,
-			   id);
-		is_valid = false;
+		if (sharp_nhgroup_id_is_installed(id)) {
+			zlog_debug("%s: nhg %u: no nexthops, deleting nexthop group", __func__,
+				   id);
+			zclient_nhg_send(zclient, ZEBRA_NHG_DEL, &api_nhg);
+		} else {
+			zlog_debug("%s: nhg %u not sent: no valid nexthops", __func__,
+				   id);
+			is_valid = false;
+		}
 		goto done;
 	}
 