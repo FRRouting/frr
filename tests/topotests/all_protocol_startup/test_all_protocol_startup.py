FRRouting/frr

commit 35729f38fa5713b923782ca9921c893bb8d3bc25

zebra: Add a timer to nexthop group deletion
Before deleting nexthop groups, that are installed,
from the system, start a timer and hold the nexthop
group for that time.

Suppose you have this scenario

a) create a static route with 1 x ecmp
      creates a nhg with 1 x ecmp
b) create a static route with 2 x ecmp
      creates a nhg with 2 x ecmp
      deletes a's nhg
c) create a static route with 3 x ecmp
      creates a nhg with 3 x ecmp
      deletes b's nhg
d) create a different route with 1 x ecmp
      creates another 1 x ecmp ( since a's ecmp was deleted )
e) create a different route with 2 x ecmp
      creates another 2 x ecmp ( since b's ecmp was deleted )

If you don't delete the nhg, start a timer, the nhg's used
in steps a and b can be reused for steps d and e.  This reduces
overhead work with zebra <-> kernel interactions and improves
the speed of the system.

So modify the code to note that an installed nexthop group should
be kept around a bit and hopefully reused.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

zebra/zebra_nhg.c
@@ -1625,6 +1625,17 @@ void zebra_nhg_hash_free(void *p)
 	zebra_nhg_free((struct nhg_hash_entry *)p);
 }
 
+static void zebra_nhg_timer(struct thread *thread)
+{
+	struct nhg_hash_entry *nhe = THREAD_ARG(thread);
+
+	if (IS_ZEBRA_DEBUG_NHG_DETAIL)
+		zlog_debug("Nexthop Timer for nhe: %pNG", nhe);
+
+	if (nhe->refcnt == 1)
+		zebra_nhg_decrement_ref(nhe);
+}
+
 void zebra_nhg_decrement_ref(struct nhg_hash_entry *nhe)
 {
 	if (IS_ZEBRA_DEBUG_NHG_DETAIL)
@@ -1632,6 +1643,15 @@ void zebra_nhg_decrement_ref(struct nhg_hash_entry *nhe)
 			   nhe->refcnt, nhe->refcnt - 1);
 
 	nhe->refcnt--;
+
+	if (!zrouter.in_shutdown && nhe->refcnt <= 0 &&
+	    CHECK_FLAG(nhe->flags, NEXTHOP_GROUP_INSTALLED) &&
+	    !CHECK_FLAG(nhe->flags, NEXTHOP_GROUP_KEEP_AROUND)) {
+		nhe->refcnt = 1;
+		SET_FLAG(nhe->flags, NEXTHOP_GROUP_KEEP_AROUND);
+		thread_add_timer(zrouter.master, zebra_nhg_timer, nhe, 180,
+				 &nhe->timer);
+	}
 
 	if (!zebra_nhg_depends_is_empty(nhe))
 		nhg_connected_tree_decrement_ref(&nhe->nhg_depends);
@@ -1647,6 +1667,12 @@ void zebra_nhg_increment_ref(struct nhg_hash_entry *nhe)
 			   nhe->refcnt, nhe->refcnt + 1);
 
 	nhe->refcnt++;
+
+	if (thread_is_scheduled(nhe->timer)) {
+		THREAD_OFF(nhe->timer);
+		nhe->refcnt--;
+		UNSET_FLAG(nhe->flags, NEXTHOP_GROUP_KEEP_AROUND);
+	}
 
 	if (!zebra_nhg_depends_is_empty(nhe))
 		nhg_connected_tree_increment_ref(&nhe->nhg_depends);
@@ -3296,9 +3322,6 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 
 		rib_handle_nhg_replace(old, new);
 
-		/* if this != 1 at this point, we have a bug */
-		assert(old->refcnt == 1);
-
 		/* We have to decrement its singletons
 		 * because some might not exist in NEW.
 		 */
@@ -3310,6 +3333,7 @@ struct nhg_hash_entry *zebra_nhg_proto_add(uint32_t id, int type,
 
 		/* Dont call the dec API, we dont want to uninstall the ID */
 		old->refcnt = 0;
+		THREAD_OFF(old->timer);
 		zebra_nhg_free(old);
 		old = NULL;
 	}

zebra/zebra_nhg.h
@@ -105,6 +105,8 @@ struct nhg_hash_entry {
 	 */
 	struct nhg_connected_tree_head nhg_depends, nhg_dependents;
 
+	struct thread *timer;
+
 /*
  * Is this nexthop group valid, ie all nexthops are fully resolved.
  * What is fully resolved?  It's a nexthop that is either self contained
@@ -144,6 +146,15 @@ struct nhg_hash_entry {
  *
  */
 #define NEXTHOP_GROUP_PROTO_RELEASED (1 << 5)
+
+/*
+ * When deleting a NHG notice that it is still installed
+ * and if it is, slightly delay the actual removal to
+ * the future.  So that upper level protocols might
+ * be able to take advantage of some NHG's that
+ * are there
+ */
+#define NEXTHOP_GROUP_KEEP_AROUND (1 << 6)
 
 /*
  * Track FPM installation status..

zebra/zebra_vty.c
@@ -1433,13 +1433,21 @@ static void show_nexthop_group_out(struct vty *vty, struct nhg_hash_entry *nhe)
 	struct nhg_connected *rb_node_dep = NULL;
 	struct nexthop_group *backup_nhg;
 	char up_str[MONOTIME_STRLEN];
+	char time_left[MONOTIME_STRLEN];
 
 	uptime2str(nhe->uptime, up_str, sizeof(up_str));
 
 	vty_out(vty, "ID: %u (%s)\n", nhe->id, zebra_route_string(nhe->type));
-	vty_out(vty, "     RefCnt: %u\n", nhe->refcnt);
+	vty_out(vty, "     RefCnt: %u", nhe->refcnt);
+	if (thread_is_scheduled(nhe->timer))
+		vty_out(vty, " Time to Deletion: %s",
+			thread_timer_to_hhmmss(time_left, sizeof(time_left),
+					       nhe->timer));
+	vty_out(vty, "\n");
+
 	vty_out(vty, "     Uptime: %s\n", up_str);
 	vty_out(vty, "     VRF: %s\n", vrf_id_to_name(nhe->vrf_id));
+
 
 	if (CHECK_FLAG(nhe->flags, NEXTHOP_GROUP_VALID)) {
 		vty_out(vty, "     Valid");

commit 05ca03f9b57077cb2cfc305a5a17ba35f42d406c

zebra: remove proto nhid from system, when NHG_DEL is sent
When suppressing a nexthop group, the proto nhid is still present
in the system:
> ubuntu2204(config)# nexthop-group LL
> 2023/12/04 21:23:44 SHARP: [Q5NBA-GN1BG] NHG ID assigned: 179687510
> ubuntu2204(config-nh-group)# nexthop 192.0.2.43 loop1
> ubuntu2204(config-nh-group)# no  nexthop 192.0.2.43 loop1
> 2023/12/04 21:23:56 ZEBRA: [RY75W-058E9] zebra_nhg_proto_del: deleted nhe 0x55edc18eb870 (179687510[260]), vrf 0, type sharp
> 2023/12/04 21:23:56 ZEBRA: [WDEB1-93HCZ] zebra_nhg_decrement_ref: nhe 0x55edc18eb870 (179687510[260]) 1 => 0
> 2023/12/04 21:23:56 SHARP: [H3QKG-WH8ZV] Removed nhg 179687510
> ubuntu2204(config-nh-group)#
> ubuntu2204(config-nh-group)# exi
> ubuntu2204(config)# <Ctrl-D>
> # ip nexthop ls
> id 179687510 group 260 proto 194
> id 260 via 192.0.2.43 dev loop1 scope link proto 194

There is no need to keep nexthop group from protocol levels if there
are no references to that nexthop group. Fix this by forcing the system
removal only if it is a protocol nhid.

Fixes: 35729f38fa57 ("zebra: Add a timer to nexthop group deletion")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/zebra_nhg.c
@@ -1719,7 +1719,8 @@ void zebra_nhg_decrement_ref(struct nhg_hash_entry *nhe)
 
 	nhe->refcnt--;
 
-	if (!zebra_router_in_shutdown() && nhe->refcnt <= 0 &&
+	if (!PROTO_OWNED(nhe) && !zebra_router_in_shutdown() &&
+	    nhe->refcnt <= 0 &&
 	    CHECK_FLAG(nhe->flags, NEXTHOP_GROUP_INSTALLED) &&
 	    !CHECK_FLAG(nhe->flags, NEXTHOP_GROUP_KEEP_AROUND)) {
 		nhe->refcnt = 1;

tests/topotests/all_protocol_startup/test_all_protocol_startup.py
@@ -466,6 +466,22 @@ def verify_nexthop_group(nhg_id, recursive=False, ecmp=0):
         )
 
 
+def verify_nexthop_group_on_system(nhg_id, present):
+    # Verify nhgid presence on the system
+    net = get_topogen().net
+    output = net["r1"].cmd("ip nexthop show id %s" % nhg_id)
+    match = re.search(r"id %s" % nhg_id, output)
+    if present:
+        assert match is not None, (
+            "Nexthop Group ID=%d should be present on the system, but is not" % nhg_id
+        )
+    else:
+        assert match is None, (
+            "Nexthop Group ID=%d should not be present on the system, but is not"
+            % nhg_id
+        )
+
+
 def verify_route_nexthop_group(route_str, recursive=False, ecmp=0):
     # Verify route and that zebra created NHGs for and they are valid/installed
     nhg_id = route_get_nhg_id(route_str)
@@ -612,6 +628,22 @@ def test_nexthop_groups():
         "Route 6.6.6.1/32 with Nexthop Group ID=%d has wrong number of resolved nexthops"
         % nhg_id
     )
+
+    ## nexthop suppression
+
+    net["r1"].cmd(
+        'vtysh -c "c t" -c "nexthop-group suppress" -c "nexthop 192.168.0.165 r1-eth0"'
+    )
+    net["r1"].cmd('vtysh -c "sharp install routes 7.7.7.1 nexthop-group suppress 1"')
+    verify_route_nexthop_group("7.7.7.1/32")
+    nhg_id = route_get_nhg_id("7.7.7.1/32")
+    verify_nexthop_group_on_system(nhg_id, True)
+    net["r1"].cmd('vtysh -c "sharp remove routes 7.7.7.1 1"')
+    net["r1"].cmd(
+        'vtysh -c "c t" -c "nexthop-group suppress" -c "no nexthop 192.168.0.165 r1-eth0"'
+    )
+    net["r1"].cmd('vtysh -c "c t" -c "no nexthop-group suppress"')
+    verify_nexthop_group_on_system(nhg_id, False)
 
     ## Remove all NHG routes
 