3d1f0c467b3d8cd9c7cb0fd3ea81165748ee8af0
Donald Sharp <sharpd@nvidia.com>
2023-12-11 15:44:26 -0500

    zebra: On shutdown properly free up ns memory
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- lib/netns_linux.c
+++ lib/netns_linux.c
@@ -502,11 +502,18 @@ void ns_init_management(ns_id_t default_ns_id, ns_id_t internal_ns)
 void ns_terminate(void)
 {
 	struct ns *ns;
+	struct ns_map_nsid *ns_map;
 
 	while (!RB_EMPTY(ns_head, &ns_tree)) {
 		ns = RB_ROOT(ns_head, &ns_tree);
 
 		ns_delete(ns);
+	}
+
+	while (!RB_EMPTY(ns_map_nsid_head, &ns_map_nsid_list)) {
+		ns_map = RB_ROOT(ns_map_nsid_head, &ns_map_nsid_list);
+		RB_REMOVE(ns_map_nsid_head, &ns_map_nsid_list, ns_map);
+		XFREE(MTYPE_NS, ns_map);
 	}
 }
 
