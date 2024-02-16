c38ef653f332d0da3591d5c1397268210797e4dd
Donald Sharp <sharpd@nvidia.com>
2024-02-15 23:42:46 -0500

    zebra: Remove deprecated items
    Both zebra_evpn.c and zebra_vxlan.c have items that
    were deprecated a year ago.  Let's remove them.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_vxlan.c
+++ zebra/zebra_vxlan.c
@@ -769,10 +769,6 @@ static void zl3vni_print(struct zebra_l3vni *zl3vni, void **ctx)
 		json_evpn_list = json_object_new_array();
 		json_object_int_add(json, "vni", zl3vni->vni);
 		json_object_string_add(json, "type", "L3");
-#if CONFDATE > 20240210
-CPP_NOTICE("Drop `vrf` from JSON outputs")
-#endif
-		json_object_string_add(json, "vrf", zl3vni_vrf_name(zl3vni));
 		json_object_string_add(json, "tenantVrf",
 				       zl3vni_vrf_name(zl3vni));
 		json_object_string_addf(json, "localVtepIp", "%pI4",
