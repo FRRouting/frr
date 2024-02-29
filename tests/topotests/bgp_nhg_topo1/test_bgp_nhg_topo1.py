fcd56cc645f32ac05476dfc631a29a5af6719cff
Philippe Guibert <philippe.guibert@6wind.com>
2024-02-29 15:33:17 +0100

    topotests: bgp_nhg_zapi_scalability, add route update avoidance
    When using bgp nexthop groups, it is possible to avoid route
    updates, and this test ensures that the number of zapi route
    messages from BGP to ZEBRA can be reduced, for instance when
    a failover happens.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- tests/topotests/bgp_nhg_topo1/test_bgp_nhg_topo1.py
+++ tests/topotests/bgp_nhg_topo1/test_bgp_nhg_topo1.py
@@ -45,12 +45,9 @@ sys.path.append(os.path.join(CWD, "../"))
 # pylint: disable=C0413
 # Import topogen and topotest helpers
 from lib import topotest
+from lib.common_check import ip_check_path_selection, iproute2_check_path_selection
 from lib.common_config import step
-from lib.nexthopgroup import (
-    route_get_nhg_id,
-    verify_nexthop_group,
-    verify_route_nexthop_group,
-)
+from lib.nexthopgroup import route_check_nhg_id_is_protocol
 from lib.topogen import Topogen, TopoRouter, get_topogen
 from lib.topolog import logger
 
@@ -255,65 +252,6 @@ def iproute2_check_path_not_present(router, ipaddr_str):
             return f"The following entry is found: {nhid_entry['dst']}."
 
     return None
-
-
-def ip_check_path_selection(router, ipaddr_str, expected, vrf_name=None):
-    if vrf_name:
-        output = json.loads(
-            router.vtysh_cmd(f"show ip route vrf {vrf_name} {ipaddr_str} json")
-        )
-    else:
-        output = json.loads(router.vtysh_cmd(f"show ip route {ipaddr_str} json"))
-    ret = topotest.json_cmp(output, expected)
-    if ret is None:
-        num_nh_expected = len(expected[ipaddr_str][0]["nexthops"])
-        num_nh_observed = len(output[ipaddr_str][0]["nexthops"])
-        if num_nh_expected == num_nh_observed:
-            return ret
-        return "{}, prefix {} does not have the correct number of nexthops : observed {}, expected {}".format(
-            router.name, ipaddr_str, num_nh_observed, num_nh_expected
-        )
-    return ret
-
-
-def iproute2_check_path_selection(
-    router, ipaddr_str, expected, vrf_name=None, nhg_id=None
-):
-    if not topotest.iproute2_is_json_capable():
-        return None
-
-    if vrf_name:
-        output = json.loads(
-            router.run(f"ip -json route show vrf {vrf_name} {ipaddr_str}")
-        )
-    else:
-        output = json.loads(router.run(f"ip -json route show {ipaddr_str}"))
-    if output is None:
-        return "problem. iproute2 returns nothing"
-
-    for entry in output:
-        if "nhid" not in entry.keys():
-            return "problem. nhid not found"
-        if nhg_id is None:
-            if entry["nhid"] <= 75757550:
-                return f"problem: invalid nhid {entry['nhid']}"
-        elif entry["nhid"] != nhg_id:
-            return f"problem: invalid nhid {entry['nhid']}, expected {nhg_id}"
-
-    return topotest.json_cmp(output, expected)
-
-
-def route_check_nhg_id_is_protocol(ipaddr_str, rname, vrf_name=None, protocol="bgp"):
-    tgen = get_topogen()
-    nhg_id = route_get_nhg_id(ipaddr_str, rname, vrf_name=vrf_name)
-    output = tgen.gears["r1"].vtysh_cmd(
-        "show nexthop-group rib %d" % nhg_id,
-    )
-    assert f"ID: {nhg_id} ({protocol})" in output, (
-        "NHG %d not found in 'show nexthop-group rib ID json" % nhg_id
-    )
-
-    return nhg_id
 
 
 def test_bgp_ipv4_route_presence():
