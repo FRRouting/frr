FRRouting/frr

commit 641065d4fc0fd956252b988f8a5037b2917d9633

bgpd: VRF-Lite fix to clear stale leaked routes
Description:
Change is intended for fixing the issue related to
clearing of stale leaked routes:
- Whenever BGP goes down,
  after bringing down tcp connection and renegotiating capabilities,
  once we reestablish connection,
  we are not handling clear of VRF leaked route in the bgp_clear_stale_route.

- While bgp is clearing stale routes,
  we need to handle withdraw of routes for VRF route-leaking.

Co-authored-by: Kantesh Mundaragi <kmundaragi@vmware.com>
Signed-off-by: Iqra Siddiqui <imujeebsiddi@vmware.com>

bgpd/bgp_route.c
@@ -5379,6 +5379,17 @@ void bgp_clear_stale_route(struct peer *peer, afi_t afi, safi_t safi)
 							BGP_PATH_STALE))
 						break;
 
+					/*
+					 * If this is VRF leaked route
+					 * process for withdraw.
+					 */
+					if (pi->sub_type ==
+						    BGP_ROUTE_IMPORTED &&
+					    peer->bgp->inst_type ==
+						    BGP_INSTANCE_TYPE_DEFAULT)
+						vpn_leak_to_vrf_withdraw(
+							peer->bgp, pi);
+
 					bgp_rib_remove(rm, pi, peer, afi, safi);
 					break;
 				}
@@ -5398,6 +5409,15 @@ void bgp_clear_stale_route(struct peer *peer, afi_t afi, safi_t safi)
 					break;
 				if (!CHECK_FLAG(pi->flags, BGP_PATH_STALE))
 					break;
+				if (safi == SAFI_UNICAST &&
+				    (peer->bgp->inst_type ==
+					     BGP_INSTANCE_TYPE_VRF ||
+				     peer->bgp->inst_type ==
+					     BGP_INSTANCE_TYPE_DEFAULT))
+					vpn_leak_from_vrf_withdraw(
+						bgp_get_default(), peer->bgp,
+						pi);
+
 				bgp_rib_remove(dest, pi, peer, afi, safi);
 				break;
 			}

commit b633e0d574682412e6884885fa292d32f70391c0

bgpd: fix withdraw stale routes
Incoming MPLS VPN staled routes are not flushing imported
entries, upon expiration of the stalepath timer.

The incoming MPLS VPN staled routes that are received from
a given peer have a sub_type set to BGP_ROUTE_NORMAL. The
original patch is wrong as it filters out the locally
exported routes marked with the BGP_ROUTE_IMPORTED subtype.

Fix this by extending the test to all MPLS VPN routes
received from this peer.

Fixes: 641065d4fc ("bgpd: VRF-Lite fix to clear stale leaked routes")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_route.c
@@ -5886,8 +5886,7 @@ void bgp_clear_stale_route(struct peer *peer, afi_t afi, safi_t safi)
 					 * If this is VRF leaked route
 					 * process for withdraw.
 					 */
-					if (pi->sub_type ==
-						    BGP_ROUTE_IMPORTED &&
+					if (safi == SAFI_MPLS_VPN &&
 					    peer->bgp->inst_type ==
 						    BGP_INSTANCE_TYPE_DEFAULT)
 						vpn_leak_to_vrf_withdraw(pi);