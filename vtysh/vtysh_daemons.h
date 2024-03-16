d80f5e4151e2b53d25fcb2ec1684a1ef4d255d61
ponedo <2534164734@qq.com>
2024-03-16 04:40:05 +0000

    lib: Substitute poll APIs with epoll APIs in thread management routine
    This commit substitutes poll APIs with epoll APIs for better performace. Note that epoll APIs are only available for Linux platforms. For BSD platforms which do not support epoll APIs, poll APIs are still used.
    
    Signed-off-by: Kaifei Peng <2534164734@qq.com>

---
+++ vtysh/vtysh_daemons.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#define DAEMONS_LIST                                                           \
+	"<zebra|mgmtd|bgpd|ripd|ripngd|ospfd|ospf6d|isisd|fabricd|nhrpd|ldpd|babeld|eigrpd|sharpd|pimd|pim6d|pbrd|staticd|bfdd|vrrpd|pathd>"
+#define DAEMONS_STR                                                            \
+	"For the zebra daemon\nFor the mgmtd daemon\nFor the bgpd daemon\nFor the ripd daemon\nFor the ripngd daemon\nFor the ospfd daemon\nFor the ospf6d daemon\nFor the isisd daemon\nFor the fabricd daemon\nFor the nhrpd daemon\nFor the ldpd daemon\nFor the babeld daemon\nFor the eigrpd daemon\nFor the sharpd daemon\nFor the pimd daemon\nFor the pim6d daemon\nFor the pbrd daemon\nFor the staticd daemon\nFor the bfdd daemon\nFor the vrrpd daemon\nFor the pathd daemon\n"
