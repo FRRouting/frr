874242f129f1fc6cff48ffa581326735f1da66e1
Donatas Abraitis <donatas@opensourcerouting.org>
2024-03-21 16:46:58 +0200

    *: Add missing SPDX-License-Identifier for some .c/.h files
    Adding them as others: GPL-2.0-or-later
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_trace.c
+++ bgpd/bgp_trace.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #define TRACEPOINT_CREATE_PROBES
 #define TRACEPOINT_DEFINE
 
--- lib/routing_nb.h
+++ lib/routing_nb.h
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifndef _FRR_ROUTING_NB_H_
 #define _FRR_ROUTING_NB_H_
 
