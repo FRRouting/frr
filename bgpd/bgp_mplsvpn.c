FRRouting/frr

commit 2fceba1fa7dea4e5ca628273a5ab01e2abf32b2c

bgpd: add utility to check if a vrf is MPLS VPN
If a vrf is exporting to a vpn table and/or importing to a vpn
table then it is assumed t be a MPLS VPN vrf.

Signed-off-by: Pat Ruddy <pat@voltanet.io>

bgpd/bgp_mplsvpn.h
@@ -83,6 +83,21 @@ extern void vrf_import_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 void vrf_unimport_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 			   afi_t afi, safi_t safi);
 
+static inline bool is_bgp_vrf_mplsvpn(struct bgp *bgp)
+{
+	afi_t afi;
+
+	if (bgp->inst_type == BGP_INSTANCE_TYPE_VRF)
+		for (afi = 0; afi < AFI_MAX; ++afi) {
+			if (CHECK_FLAG(bgp->af_flags[afi][SAFI_UNICAST],
+				       BGP_CONFIG_VRF_TO_MPLSVPN_EXPORT)
+			    || CHECK_FLAG(bgp->af_flags[afi][SAFI_UNICAST],
+					  BGP_CONFIG_MPLSVPN_TO_VRF_IMPORT))
+				return true;
+		}
+	return false;
+}
+
 static inline int vpn_leak_to_vpn_active(struct bgp *bgp_vrf, afi_t afi,
 					 const char **pmsg)
 {

commit e42d2cd821a517dbbbc16727f6435eed476cccb7

bgpd: fix L3VPN cases on the default VRF
The BGP L3VPN code imports and exports IPv4/IPv6 prefixes
from the BGP main instance and from the BGP VRF instances.
However, in some scenarios, the BGP L3VPN routes may not be
refreshed properly.

The first change in the bgp_vpn_leak_unimport() function
is related to unimporting route entries when deleting a
BGP instance.

The second change in the is_bgp_vrf_mplsvpn() function is
is related to the visibility of the L3VPN MIB applied to
the default VRF instance too.

Fixes: ("9ecf931b13cf bgpd: no router bgp cleanup vrf leaked vpn routes")
Fixes: 2fceba1fa7de ("bgpd: add utility to check if a vrf is MPLS VPN")
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_mplsvpn.c
@@ -3804,7 +3804,8 @@ void bgp_vpn_leak_unimport(struct bgp *from_bgp)
 	bool is_vrf_leak_bind;
 	int debug;
 
-	if (from_bgp->inst_type != BGP_INSTANCE_TYPE_VRF)
+	if (from_bgp->inst_type != BGP_INSTANCE_TYPE_DEFAULT &&
+	    from_bgp->inst_type != BGP_INSTANCE_TYPE_VRF)
 		return;
 
 	debug = (BGP_DEBUG(vpn, VPN_LEAK_TO_VRF) |

bgpd/bgp_mplsvpn.h
@@ -101,7 +101,8 @@ static inline bool is_bgp_vrf_mplsvpn(struct bgp *bgp)
 {
 	afi_t afi;
 
-	if (bgp->inst_type == BGP_INSTANCE_TYPE_VRF)
+	if (bgp->inst_type == BGP_INSTANCE_TYPE_VRF ||
+	    bgp->inst_type == BGP_INSTANCE_TYPE_DEFAULT)
 		for (afi = 0; afi < AFI_MAX; ++afi) {
 			if (CHECK_FLAG(bgp->af_flags[afi][SAFI_UNICAST],
 				       BGP_CONFIG_VRF_TO_MPLSVPN_EXPORT)