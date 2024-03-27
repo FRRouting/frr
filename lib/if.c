FRRouting/frr

commit 0760a74d2fc29a89d1d0d252af98d674323b210a

lib: add utility to get the next index in a vrf
Add if_vrf_lookup_by_index_next to get the next ifindex in a vrf
given the previous ifindex or 0 for the first.

Signed-off-by: Pat Ruddy <pat@voltanet.io>

lib/if.c
@@ -351,6 +351,40 @@ struct interface *if_lookup_by_index(ifindex_t ifindex, vrf_id_t vrf_id)
 	return NULL;
 }
 
+/* Interface existance check by index. */
+struct interface *if_vrf_lookup_by_index_next(ifindex_t ifindex,
+					      vrf_id_t vrf_id)
+{
+	struct vrf *vrf = vrf_lookup_by_id(vrf_id);
+	struct interface *tmp_ifp;
+	bool found = false;
+
+	if (!vrf)
+		return NULL;
+
+	if (ifindex == 0) {
+		tmp_ifp = RB_MIN(if_index_head, &vrf->ifaces_by_index);
+		/* skip the vrf interface */
+		if (tmp_ifp && if_is_vrf(tmp_ifp))
+			ifindex = tmp_ifp->ifindex;
+		else
+			return tmp_ifp;
+	}
+
+	RB_FOREACH (tmp_ifp, if_index_head, &vrf->ifaces_by_index) {
+		if (found) {
+			/* skip the vrf interface */
+			if (tmp_ifp && if_is_vrf(tmp_ifp))
+				continue;
+			else
+				return tmp_ifp;
+		}
+		if (tmp_ifp->ifindex == ifindex)
+			found = true;
+	}
+	return NULL;
+}
+
 const char *ifindex2ifname(ifindex_t ifindex, vrf_id_t vrf_id)
 {
 	struct interface *ifp;

lib/if.h
@@ -513,6 +513,8 @@ extern struct interface *if_create_name(const char *name, vrf_id_t vrf_id);
 /* Create new interface, adds to index list only */
 extern struct interface *if_create_ifindex(ifindex_t ifindex, vrf_id_t vrf_id);
 extern struct interface *if_lookup_by_index(ifindex_t, vrf_id_t vrf_id);
+extern struct interface *if_vrf_lookup_by_index_next(ifindex_t ifindex,
+						     vrf_id_t vrf_id);
 extern struct interface *if_lookup_by_index_all_vrf(ifindex_t);
 extern struct interface *if_lookup_exact_address(const void *matchaddr,
 						 int family, vrf_id_t vrf_id);

commit f818dbda254c144b96950e4961128c9206f0ca65

lib: fix next vrf interface lookup avoid loopback interface
When the default vrf is submitted, the returned interface
may be the loopback interface, which may not be valid.
Actually, on vrfs, the vrf interface is bypassed. When
being on default vrf, the 'lo' interface should be
bypassed too.
Fix this by avoiding returning the lo interface too.

Fixes: 0760a74d2fc2 ("lib: add utility to get the next index in a vrf")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

lib/if.c
@@ -319,8 +319,8 @@ struct interface *if_vrf_lookup_by_index_next(ifindex_t ifindex,
 
 	if (ifindex == 0) {
 		tmp_ifp = RB_MIN(if_index_head, &vrf->ifaces_by_index);
-		/* skip the vrf interface */
-		if (tmp_ifp && if_is_vrf(tmp_ifp))
+		/* skip the vrf interface or the lo interface */
+		if (tmp_ifp && if_is_loopback(tmp_ifp))
 			ifindex = tmp_ifp->ifindex;
 		else
 			return tmp_ifp;
@@ -328,8 +328,8 @@ struct interface *if_vrf_lookup_by_index_next(ifindex_t ifindex,
 
 	RB_FOREACH (tmp_ifp, if_index_head, &vrf->ifaces_by_index) {
 		if (found) {
-			/* skip the vrf interface */
-			if (tmp_ifp && if_is_vrf(tmp_ifp))
+			/* skip the vrf interface or the lo interface */
+			if (tmp_ifp && if_is_loopback(tmp_ifp))
 				continue;
 			else
 				return tmp_ifp;

lib/vrf.h
@@ -149,6 +149,9 @@ static inline uint32_t vrf_interface_count(struct vrf *vrf)
 	struct interface *ifp;
 
 	RB_FOREACH (ifp, if_name_head, &vrf->ifaces_by_name) {
+		/* skip the lo interface */
+		if (if_is_loopback_exact(ifp))
+			continue;
 		/* skip the l3mdev */
 		if (strncmp(ifp->name, vrf->name, VRF_NAMSIZ) == 0)
 			continue;