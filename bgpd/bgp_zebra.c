c2e29941da92b8aed67677d2f202769a1b9b7ca4
Louis Scalbert <louis.scalbert@6wind.com>
2024-03-28 11:11:05 +0100

    bgpd: fix updating ipv6 global nexthop
    bgpd keeps on advertising IPv6 prefixes with a IPv6 link-local nexthop
    or a IPv4-mapped IPv6 after a valid IPv6 global appears.
    
    For example, at bgpd startup, the IPv6 global is announced by zebra
    after the link-local. Only the link-local is advertised. Clearing the
    BGP sessions make the global to to be announced.
    
    Update the nexthops with the global IPv6 when available. Do not override
    any previous global address that is still valid.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_zebra.c
+++ bgpd/bgp_zebra.c
@@ -351,9 +351,11 @@ static int bgp_interface_address_add(ZAPI_CALLBACK_ARGS)
 			 * into peer's v6_global and send updates out
 			 * with new nexthop addr.
 			 */
-			if ((peer->conf_if && (strcmp(peer->conf_if, ifc->ifp->name) == 0)) &&
-			    ((IS_MAPPED_IPV6(&peer->nexthop.v6_global)) ||
-			     IN6_IS_ADDR_LINKLOCAL(&peer->nexthop.v6_global))) {
+			if ((peer->nexthop.ifp == ifc->ifp ||
+			     (peer->conf_if && strcmp(peer->conf_if, ifc->ifp->name) == 0)) &&
+			    (IS_MAPPED_IPV6(&peer->nexthop.v6_global) ||
+			     IN6_IS_ADDR_LINKLOCAL(&peer->nexthop.v6_global) ||
+			     IN6_IS_ADDR_UNSPECIFIED(&peer->nexthop.v6_global))) {
 				if (bgp_debug_zebra(ifc->address)) {
 					zlog_debug("Update peer %pBP's current intf global addr from %pI6 to %pI6 and send updates",
 						   peer, &peer->nexthop.v6_global, &addr->u.prefix6);
