FRRouting/frr

commit 26f6acafc369f1c126870b4282d5e252cc830f26

isisd: add support for segment routing
This is an implementation of the IS-IS SR draft [1] for FRR.

The following features are supported:
* IPv4 and IPv6 Prefix-SIDs;
* IPv4 and IPv6 Adj-SIDs and LAN-Adj-SIDs;
* Index and absolute labels;
* The no-php and explicit-null Prefix-SID flags;
* Full integration with the Label Manager.

Known limitations:
* No support for Anycast-SIDs;
* No support for the SID/Label Binding TLV (required for LDP interop).
* No support for persistent Adj-SIDs;
* No support for multiple SRGBs.

[1] draft-ietf-isis-segment-routing-extensions-25

Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

isisd/isis_adjacency.c
@@ -93,6 +93,7 @@ struct isis_adjacency *isis_new_adj(const uint8_t *id, const uint8_t *snpa,
 				.last_dis_change = time(NULL);
 		}
 	}
+	adj->adj_sids = list_new();
 
 	return adj;
 }
@@ -122,6 +123,44 @@ struct isis_adjacency *isis_adj_lookup_snpa(const uint8_t *ssnpa,
 	return NULL;
 }
 
+bool isis_adj_exists(const struct isis_area *area, int level,
+		     const uint8_t *sysid)
+{
+	struct isis_circuit *circuit;
+	struct listnode *node;
+
+	for (ALL_LIST_ELEMENTS_RO(area->circuit_list, node, circuit)) {
+		struct isis_adjacency *adj;
+		struct listnode *anode;
+		struct list *adjdb;
+
+		switch (circuit->circ_type) {
+		case CIRCUIT_T_BROADCAST:
+			adjdb = circuit->u.bc.adjdb[level - 1];
+			if (!adjdb)
+				continue;
+
+			for (ALL_LIST_ELEMENTS_RO(adjdb, anode, adj)) {
+				if (!memcmp(adj->sysid, sysid, ISIS_SYS_ID_LEN))
+					return true;
+			}
+			break;
+		case CIRCUIT_T_P2P:
+			adj = circuit->u.p2p.neighbor;
+			if (!adj)
+				break;
+
+			if (!memcmp(adj->sysid, sysid, ISIS_SYS_ID_LEN))
+				return true;
+			break;
+		default:
+			break;
+		}
+	}
+
+	return false;
+}
+
 DEFINE_HOOK(isis_adj_state_change_hook, (struct isis_adjacency *adj), (adj))
 
 void isis_delete_adj(void *arg)
@@ -145,6 +184,7 @@ void isis_delete_adj(void *arg)
 	XFREE(MTYPE_ISIS_ADJACENCY_INFO, adj->ipv6_addresses);
 
 	adj_mt_finish(adj);
+	list_delete(&adj->adj_sids);
 
 	XFREE(MTYPE_ISIS_ADJACENCY, adj);
 	return;
@@ -441,6 +481,9 @@ void isis_adj_print_vty(struct isis_adjacency *adj, struct vty *vty,
 	}
 
 	if (detail == ISIS_UI_LEVEL_DETAIL) {
+		struct sr_adjacency *sra;
+		struct listnode *anode;
+
 		level = adj->level;
 		vty_out(vty, "\n");
 		if (adj->circuit)
@@ -528,6 +571,31 @@ void isis_adj_print_vty(struct isis_adjacency *adj, struct vty *vty,
 					  buf, sizeof(buf));
 				vty_out(vty, "      %s\n", buf);
 			}
+		}
+		for (ALL_LIST_ELEMENTS_RO(adj->adj_sids, anode, sra)) {
+			const char *adj_type;
+			const char *backup;
+			uint32_t sid;
+
+			switch (sra->adj->circuit->circ_type) {
+			case CIRCUIT_T_BROADCAST:
+				adj_type = "LAN Adjacency-SID";
+				sid = sra->u.ladj_sid->sid;
+				break;
+			case CIRCUIT_T_P2P:
+				adj_type = "Adjacency-SID";
+				sid = sra->u.adj_sid->sid;
+				break;
+			default:
+				continue;
+			}
+			backup = (sra->type == ISIS_SR_LAN_BACKUP) ? " (backup)"
+								   : "";
+
+			vty_out(vty, "    %s %s%s: %u\n",
+				(sra->nexthop.family == AF_INET) ? "IPv4"
+								 : "IPv6",
+				adj_type, backup, sid);
 		}
 		vty_out(vty, "\n");
 	}

isisd/isis_sr.h
@@ -1,0 +1,259 @@
+/*
+ * This is an implementation of Segment Routing for IS-IS
+ * as per draft draft-ietf-isis-segment-routing-extensions-25
+ *
+ * Copyright (C) 2019 Orange Labs http://www.orange.com
+ *
+ * Author: Olivier Dugeon <olivier.dugeon@orange.com>
+ * Contributor: Renato Westphal <renato@opensourcerouting.org> for NetDEF
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _FRR_ISIS_SR_H
+#define _FRR_ISIS_SR_H
+
+#include "lib/linklist.h"
+#include "lib/mpls.h"
+#include "lib/nexthop.h"
+#include "lib/typesafe.h"
+
+#include "isisd/isis_tlvs.h"
+
+/*
+ * Segment Routing information is transported through the following Sub-TLVs:
+ *
+ * Sub-TLV Name                         Value   TLVs
+ * ---------------------------------------------------------------------
+ * SID Label				 1
+ *
+ * Prefix Segment Identifier		 3	135, 235, 236 and 237
+ *
+ * Adjacency Segment Identifier		31	22, 23, 141, 222 and 223
+ * LAN Adjacency Segment Identifier	32	22, 23, 141, 222 and 223
+ *
+ * Segment Routing Capability		 2	242
+ * Segment Routing Algorithm		19	242
+ * Node Maximum Stack Depth (MSD)	23	242
+ *
+ * Sub-TLV definitions, serialization and de-serialization are defined
+ * in isis_tlvs.[c,h].
+ */
+
+#define SRGB_LOWER_BOUND               16000
+#define SRGB_UPPER_BOUND               23999
+
+PREDECL_RBTREE_UNIQ(tree_sr_node)
+PREDECL_RBTREE_UNIQ(tree_sr_node_prefix)
+PREDECL_RBTREE_UNIQ(tree_sr_area_prefix)
+PREDECL_RBTREE_UNIQ(tree_sr_prefix_cfg)
+
+/* SR Adj-SID type. */
+enum sr_adj_type {
+	ISIS_SR_ADJ_NORMAL = 0,
+	ISIS_SR_LAN_BACKUP,
+};
+
+/* SR Adjacency. */
+struct sr_adjacency {
+	/* Adjacency type. */
+	enum sr_adj_type type;
+
+	/* Adj-SID nexthop information. */
+	struct {
+		int family;
+		union g_addr address;
+		mpls_label_t label;
+	} nexthop;
+
+	/* (LAN-)Adj-SID Sub-TLV. */
+	union {
+		struct isis_adj_sid *adj_sid;
+		struct isis_lan_adj_sid *ladj_sid;
+	} u;
+
+	/* Back pointer to IS-IS adjacency. */
+	struct isis_adjacency *adj;
+};
+
+/* SR Prefix-SID type. */
+enum sr_prefix_type {
+	ISIS_SR_PREFIX_LOCAL = 0,
+	ISIS_SR_PREFIX_REMOTE,
+};
+
+/* SR Nexthop Information. */
+struct sr_nexthop_info {
+	mpls_label_t label;
+	time_t uptime;
+};
+
+/* SR Prefix-SID. */
+struct sr_prefix {
+	/* RB-tree entries. */
+	struct tree_sr_node_prefix_item node_entry;
+	struct tree_sr_area_prefix_item area_entry;
+
+	/* IP prefix. */
+	struct prefix prefix;
+
+	/* SID value, algorithm and flags. */
+	struct isis_prefix_sid sid;
+
+	/* Local label value. */
+	mpls_label_t local_label;
+
+	/* Prefix-SID type. */
+	enum sr_prefix_type type;
+	union {
+		struct {
+			/* Information about this local Prefix-SID. */
+			struct sr_nexthop_info info;
+		} local;
+		struct {
+			/* Route associated to this remote Prefix-SID. */
+			struct isis_route_info *rinfo;
+		} remote;
+	} u;
+
+	/* Backpointer to SR node. */
+	struct sr_node *srn;
+
+	/* Flags used while the LSPDB is being parsed. */
+	uint8_t parse_flags;
+#define F_ISIS_SR_PREFIX_SID_NEW	0x01
+#define F_ISIS_SR_PREFIX_SID_MODIFIED	0x02
+#define F_ISIS_SR_PREFIX_SID_UNCHANGED	0x04
+};
+
+/* SR node. */
+struct sr_node {
+	/* RB-tree entry. */
+	struct tree_sr_node_item entry;
+
+	/* IS-IS level: ISIS_LEVEL1 or ISIS_LEVEL2. */
+	int level;
+
+	/* IS-IS node identifier. */
+	uint8_t sysid[ISIS_SYS_ID_LEN];
+
+	/* IS-IS node capabilities (SRGB, SR Algorithms, etc). */
+	struct isis_router_cap cap;
+
+	/* List of Prefix-SIDs advertised by this node. */
+	struct tree_sr_node_prefix_head prefix_sids;
+
+	/* Backpointer to IS-IS area. */
+	struct isis_area *area;
+
+	/* Flags used while the LSPDB is being parsed. */
+	uint8_t parse_flags;
+#define F_ISIS_SR_NODE_NEW		0x01
+#define F_ISIS_SR_NODE_MODIFIED		0x02
+#define F_ISIS_SR_NODE_UNCHANGED	0x04
+};
+
+/* NOTE: these values must be in sync with the YANG module. */
+enum sr_sid_value_type {
+	SR_SID_VALUE_TYPE_INDEX = 0,
+	SR_SID_VALUE_TYPE_ABSOLUTE = 1,
+};
+
+/* NOTE: these values must be in sync with the YANG module. */
+enum sr_last_hop_behavior {
+	SR_LAST_HOP_BEHAVIOR_EXP_NULL = 0,
+	SR_LAST_HOP_BEHAVIOR_NO_PHP = 1,
+	SR_LAST_HOP_BEHAVIOR_PHP = 2,
+};
+
+/* SR Prefix-SID configuration. */
+struct sr_prefix_cfg {
+	/* RB-tree entry. */
+	struct tree_sr_prefix_cfg_item entry;
+
+	/* IP prefix. */
+	struct prefix prefix;
+
+	/* SID value. */
+	uint32_t sid;
+
+	/* SID value type. */
+	enum sr_sid_value_type sid_type;
+
+	/* SID last hop behavior. */
+	enum sr_last_hop_behavior last_hop_behavior;
+
+	/* Does this Prefix-SID refer to a loopback address (Node-SID)? */
+	bool node_sid;
+
+	/* Backpointer to IS-IS area. */
+	struct isis_area *area;
+};
+
+/* Per-area IS-IS Segment Routing information. */
+struct isis_sr_db {
+	/* Operational status of Segment Routing. */
+	bool enabled;
+
+	/* Adj-SIDs. */
+	struct list *adj_sids;
+
+	/* SR information from all nodes. */
+	struct tree_sr_node_head sr_nodes[ISIS_LEVELS];
+
+	/* Prefix-SIDs. */
+	struct tree_sr_area_prefix_head prefix_sids[ISIS_LEVELS];
+
+	/* Area SR configuration. */
+	struct {
+		/* Administrative status of Segment Routing. */
+		bool enabled;
+
+		/* Segment Routing Global Block lower & upper bound. */
+		uint32_t srgb_lower_bound;
+		uint32_t srgb_upper_bound;
+
+		/* Maximum SID Depth supported by the node. */
+		uint8_t msd;
+
+		/* Prefix-SID mappings. */
+		struct tree_sr_prefix_cfg_head prefix_sids;
+	} config;
+};
+
+/* Prototypes. */
+extern int isis_sr_cfg_srgb_update(struct isis_area *area, uint32_t lower_bound,
+				   uint32_t upper_bound);
+extern void isis_sr_cfg_msd_update(struct isis_area *area);
+extern struct sr_prefix_cfg *
+isis_sr_cfg_prefix_add(struct isis_area *area, const struct prefix *prefix);
+extern void isis_sr_cfg_prefix_del(struct sr_prefix_cfg *pcfg);
+extern struct sr_prefix_cfg *
+isis_sr_cfg_prefix_find(struct isis_area *area, union prefixconstptr prefix);
+extern void isis_sr_prefix_cfg2subtlv(const struct sr_prefix_cfg *pcfg,
+				      bool external,
+				      struct isis_prefix_sid *psid);
+extern void isis_sr_nexthop_update(struct sr_nexthop_info *srnh,
+				   mpls_label_t label);
+extern void isis_sr_nexthop_reset(struct sr_nexthop_info *srnh);
+extern void isis_area_verify_sr(struct isis_area *area);
+extern int isis_sr_start(struct isis_area *area);
+extern void isis_sr_stop(struct isis_area *area);
+extern void isis_sr_area_init(struct isis_area *area);
+extern void isis_sr_area_term(struct isis_area *area);
+extern void isis_sr_init(void);
+extern void isis_sr_term(void);
+
+#endif /* _FRR_ISIS_SR_H */

isisd/isis_zebra.c
@@ -50,8 +50,16 @@
 #include "isisd/isis_route.h"
 #include "isisd/isis_zebra.h"
 #include "isisd/isis_te.h"
+#include "isisd/isis_sr.h"
 
-struct zclient *zclient = NULL;
+struct zclient *zclient;
+static struct zclient *zclient_sync;
+
+/* List of chunks of labels externally assigned by zebra. */
+static struct list *label_chunk_list;
+static struct listnode *current_label_chunk;
+
+static void isis_zebra_label_manager_connect(void);
 
 /* Router-id update message from zebra. */
 static int isis_router_id_update_zebra(ZAPI_CALLBACK_ARGS)
@@ -245,6 +253,88 @@ void isis_zebra_route_del_route(struct prefix *prefix,
 	zclient_route_send(ZEBRA_ROUTE_DELETE, zclient, &api);
 }
 
+/* Install Prefix-SID in the forwarding plane. */
+void isis_zebra_install_prefix_sid(const struct sr_prefix *srp)
+{
+	struct zapi_labels zl;
+	struct zapi_nexthop_label *znh;
+	struct listnode *node;
+	struct isis_nexthop *nexthop;
+	struct interface *ifp;
+
+	/* Prepare message. */
+	memset(&zl, 0, sizeof(zl));
+	zl.type = ZEBRA_LSP_ISIS_SR;
+	zl.local_label = srp->local_label;
+
+	switch (srp->type) {
+	case ISIS_SR_PREFIX_LOCAL:
+		ifp = if_lookup_by_name("lo", VRF_DEFAULT);
+		if (!ifp) {
+			zlog_warn(
+				"%s: couldn't install Prefix-SID %pFX: loopback interface not found",
+				__func__, &srp->prefix);
+			return;
+		}
+
+		znh = &zl.nexthops[zl.nexthop_num++];
+		znh->type = NEXTHOP_TYPE_IFINDEX;
+		znh->ifindex = ifp->ifindex;
+		znh->label = MPLS_LABEL_IMPLICIT_NULL;
+		break;
+	case ISIS_SR_PREFIX_REMOTE:
+		/* Update route in the RIB too. */
+		SET_FLAG(zl.message, ZAPI_LABELS_FTN);
+		zl.route.prefix = srp->prefix;
+		zl.route.type = ZEBRA_ROUTE_ISIS;
+		zl.route.instance = 0;
+
+		for (ALL_LIST_ELEMENTS_RO(srp->u.remote.rinfo->nexthops, node,
+					  nexthop)) {
+			if (nexthop->sr.label == MPLS_INVALID_LABEL)
+				continue;
+
+			if (zl.nexthop_num >= MULTIPATH_NUM)
+				break;
+
+			znh = &zl.nexthops[zl.nexthop_num++];
+			znh->type = (srp->prefix.family == AF_INET)
+					    ? NEXTHOP_TYPE_IPV4_IFINDEX
+					    : NEXTHOP_TYPE_IPV6_IFINDEX;
+			znh->family = nexthop->family;
+			znh->address = nexthop->ip;
+			znh->ifindex = nexthop->ifindex;
+			znh->label = nexthop->sr.label;
+		}
+		break;
+	}
+
+	/* Send message to zebra. */
+	(void)zebra_send_mpls_labels(zclient, ZEBRA_MPLS_LABELS_REPLACE, &zl);
+}
+
+/* Uninstall Prefix-SID from the forwarding plane. */
+void isis_zebra_uninstall_prefix_sid(const struct sr_prefix *srp)
+{
+	struct zapi_labels zl;
+
+	/* Prepare message. */
+	memset(&zl, 0, sizeof(zl));
+	zl.type = ZEBRA_LSP_ISIS_SR;
+	zl.local_label = srp->local_label;
+
+	if (srp->type == ISIS_SR_PREFIX_REMOTE) {
+		/* Update route in the RIB too. */
+		SET_FLAG(zl.message, ZAPI_LABELS_FTN);
+		zl.route.prefix = srp->prefix;
+		zl.route.type = ZEBRA_ROUTE_ISIS;
+		zl.route.instance = 0;
+	}
+
+	/* Send message to zebra. */
+	(void)zebra_send_mpls_labels(zclient, ZEBRA_MPLS_LABELS_DELETE, &zl);
+}
+
 static int isis_zebra_read(ZAPI_CALLBACK_ARGS)
 {
 	struct zapi_route api;
@@ -302,13 +392,192 @@ void isis_zebra_redistribute_unset(afi_t afi, int type)
 				     type, 0, VRF_DEFAULT);
 }
 
+/* Label Manager Requests. */
+int isis_zebra_request_label_range(uint32_t base, uint32_t chunk_size)
+{
+	int ret;
+	uint32_t start, end;
+
+	if (zclient_sync->sock == -1)
+		isis_zebra_label_manager_connect();
+
+	ret = lm_get_label_chunk(zclient_sync, 0, base, chunk_size, &start,
+				 &end);
+	if (ret < 0) {
+		zlog_warn("%s: error getting label range!", __func__);
+		return -1;
+	}
+
+	return 0;
+}
+
+void isis_zebra_release_label_range(uint32_t start, uint32_t end)
+{
+	int ret;
+
+	if (zclient_sync->sock == -1)
+		isis_zebra_label_manager_connect();
+
+	ret = lm_release_label_chunk(zclient_sync, start, end);
+	if (ret < 0)
+		zlog_warn("%s: error releasing label range!", __func__);
+}
+
+static int isis_zebra_get_label_chunk(void)
+{
+	int ret;
+	uint32_t start, end;
+	struct label_chunk *new_label_chunk;
+
+	if (zclient_sync->sock == -1)
+		isis_zebra_label_manager_connect();
+
+	ret = lm_get_label_chunk(zclient_sync, 0, MPLS_LABEL_BASE_ANY,
+				 CHUNK_SIZE, &start, &end);
+	if (ret < 0) {
+		zlog_warn("%s: error getting label chunk!", __func__);
+		return -1;
+	}
+
+	new_label_chunk = calloc(1, sizeof(struct label_chunk));
+	if (!new_label_chunk) {
+		zlog_warn("%s: error trying to allocate label chunk %u - %u",
+			  __func__, start, end);
+		return -1;
+	}
+
+	new_label_chunk->start = start;
+	new_label_chunk->end = end;
+	new_label_chunk->used_mask = 0;
+
+	listnode_add(label_chunk_list, (void *)new_label_chunk);
+
+	/* let's update current if needed */
+	if (!current_label_chunk)
+		current_label_chunk = listtail(label_chunk_list);
+
+	return 0;
+}
+
+mpls_label_t isis_zebra_request_dynamic_label(void)
+{
+	struct label_chunk *label_chunk;
+	uint32_t i, size;
+	uint64_t pos;
+	uint32_t label = MPLS_INVALID_LABEL;
+
+	while (current_label_chunk) {
+		label_chunk = listgetdata(current_label_chunk);
+		if (!label_chunk)
+			goto end;
+
+		/* try to get next free label in currently used label chunk */
+		size = label_chunk->end - label_chunk->start + 1;
+		for (i = 0, pos = 1; i < size; i++, pos <<= 1) {
+			if (!(pos & label_chunk->used_mask)) {
+				label_chunk->used_mask |= pos;
+				label = label_chunk->start + i;
+				goto end;
+			}
+		}
+		current_label_chunk = listnextnode(current_label_chunk);
+	}
+
+end:
+	/*
+	 * we moved till the last chunk, or were not able to find a label, so
+	 * let's ask for another one.
+	 */
+	if (!current_label_chunk
+	    || current_label_chunk == listtail(label_chunk_list)
+	    || label == MPLS_INVALID_LABEL) {
+		if (isis_zebra_get_label_chunk() != 0)
+			zlog_warn("%s: error getting label chunk!", __func__);
+	}
+
+	return label;
+}
+
+static void isis_zebra_del_label_chunk(void *val)
+{
+	free(val);
+}
+
+static int isis_zebra_release_label_chunk(uint32_t start, uint32_t end)
+{
+	int ret;
+
+	ret = lm_release_label_chunk(zclient_sync, start, end);
+	if (ret < 0) {
+		zlog_warn("%s: error releasing label chunk!", __func__);
+		return -1;
+	}
+
+	return 0;
+}
+
+void isis_zebra_release_dynamic_label(mpls_label_t label)
+{
+	struct listnode *node;
+	struct label_chunk *label_chunk;
+	uint64_t pos;
+
+	for (ALL_LIST_ELEMENTS_RO(label_chunk_list, node, label_chunk)) {
+		if (!(label <= label_chunk->end && label >= label_chunk->start))
+			continue;
+
+		pos = 1ULL << (label - label_chunk->start);
+		label_chunk->used_mask &= ~pos;
+
+		/*
+		 * If nobody is using this chunk and it's not
+		 * current_label_chunk, then free it.
+		 */
+		if (!label_chunk->used_mask && (current_label_chunk != node)) {
+			if (isis_zebra_release_label_chunk(label_chunk->start,
+							   label_chunk->end)
+			    != 0)
+				zlog_warn("%s: error releasing label chunk!",
+					  __func__);
+			else {
+				listnode_delete(label_chunk_list, label_chunk);
+				isis_zebra_del_label_chunk(label_chunk);
+			}
+		}
+		break;
+	}
+}
+
+static void isis_zebra_label_manager_connect(void)
+{
+	/* Connect to label manager. */
+	while (zclient_socket_connect(zclient_sync) < 0) {
+		zlog_warn("%s: error connecting synchronous zclient!",
+			  __func__);
+		sleep(1);
+	}
+	set_nonblocking(zclient_sync->sock);
+	while (lm_label_manager_connect(zclient_sync, 0) != 0) {
+		zlog_warn("%s: error connecting to label manager!", __func__);
+		sleep(1);
+	}
+
+	label_chunk_list = list_new();
+	label_chunk_list->del = isis_zebra_del_label_chunk;
+	while (isis_zebra_get_label_chunk() != 0) {
+		zlog_warn("%s: error getting first label chunk!", __func__);
+		sleep(1);
+	}
+}
+
 static void isis_zebra_connected(struct zclient *zclient)
 {
 	zclient_send_reg_requests(zclient, VRF_DEFAULT);
 }
 
-void isis_zebra_init(struct thread_master *master)
+void isis_zebra_init(struct thread_master *master, int instance)
 {
+	/* Initialize asynchronous zclient. */
 	zclient = zclient_new(master, &zclient_options_default);
 	zclient_init(zclient, PROTO_TYPE, 0, &isisd_privs);
 	zclient->zebra_connected = isis_zebra_connected;
@@ -319,7 +588,12 @@ void isis_zebra_init(struct thread_master *master)
 	zclient->redistribute_route_add = isis_zebra_read;
 	zclient->redistribute_route_del = isis_zebra_read;
 
-	return;
+	/* Initialize special zclient for synchronous message exchanges. */
+	zclient_sync = zclient_new(master, &zclient_options_default);
+	zclient_sync->sock = -1;
+	zclient_sync->redist_default = ZEBRA_ROUTE_ISIS;
+	zclient_sync->instance = instance;
+	zclient_sync->privs = &isisd_privs;
 }
 
 void isis_zebra_stop(void)

isisd/isis_adjacency.h
@@ -69,6 +69,7 @@ struct isis_dis_record {
 };
 
 struct bfd_session;
+struct isis_area;
 
 struct isis_adjacency {
 	uint8_t snpa[ETH_ALEN];		    /* NeighbourSNPAAddress */
@@ -103,6 +104,7 @@ struct isis_adjacency {
 	uint16_t *mt_set;      /* Topologies this adjacency is valid for */
 	unsigned int mt_count; /* Number of entries in mt_set */
 	struct bfd_session *bfd_session;
+	struct list *adj_sids; /* Segment Routing Adj-SIDs. */
 };
 
 struct isis_threeway_adj;
@@ -111,6 +113,8 @@ struct isis_adjacency *isis_adj_lookup(const uint8_t *sysid,
 				       struct list *adjdb);
 struct isis_adjacency *isis_adj_lookup_snpa(const uint8_t *ssnpa,
 					    struct list *adjdb);
+bool isis_adj_exists(const struct isis_area *area, int level,
+		     const uint8_t *sysid);
 struct isis_adjacency *isis_new_adj(const uint8_t *id, const uint8_t *snpa,
 				    int level, struct isis_circuit *circuit);
 void isis_delete_adj(void *adj);

isisd/isis_cli.c
@@ -1480,7 +1480,7 @@ DEFPY (isis_sr_prefix_sid,
        isis_sr_prefix_sid_cmd,
        "segment-routing prefix\
           <A.B.C.D/M|X:X::X:X/M>$prefix\
-	  <absolute$sid_type (16000-1048575)$sid_value|index$sid_type (0-65535)$sid_value>\
+	  <absolute$sid_type (16-1048575)$sid_value|index$sid_type (0-65535)$sid_value>\
 	  [<no-php-flag|explicit-null>$lh_behavior]",
        SR_STR
        "Prefix SID\n"
@@ -1518,7 +1518,7 @@ DEFPY (isis_sr_prefix_sid,
 DEFPY (no_isis_sr_prefix_sid,
        no_isis_sr_prefix_sid_cmd,
        "no segment-routing prefix <A.B.C.D/M|X:X::X:X/M>$prefix\
-         [<absolute$sid_type (16000-1048575)|index (0-65535)> [<no-php-flag|explicit-null>]]",
+         [<absolute$sid_type (16-1048575)|index (0-65535)> [<no-php-flag|explicit-null>]]",
        NO_STR
        SR_STR
        "Prefix SID\n"

isisd/isis_errors.c
@@ -38,6 +38,12 @@ static struct log_ref ferr_isis_err[] = {
 		.suggestion = "Ensure configuration is correct"
 	},
 	{
+		.code = EC_ISIS_SID_OVERFLOW,
+		.title = "SID index overflow",
+		.description = "Isis has detected that a SID index falls outside of its associated SRGB range",
+		.suggestion = "Configure a larger SRGB"
+	},
+	{
 		.code = END_FERR,
 	}
 };

isisd/isis_errors.h
@@ -26,6 +26,7 @@
 enum isis_log_refs {
 	EC_ISIS_PACKET = ISIS_FERR_START,
 	EC_ISIS_CONFIG,
+	EC_ISIS_SID_OVERFLOW,
 };
 
 extern void isis_error_init(void);

commit 2e1be61b082b2cdeabf2483dd66c16317c9dd65a

isisd: fix mispelling with ISIS_SR_LAN_BACKUP
The ISIS_SR_LAN_BACKUP should be renamed to ISIS_SR_ADJ_BACKUP.

Fixes: 26f6acafc369 ("isisd: add support for segment routing")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

isisd/isis_adjacency.c
@@ -687,7 +687,7 @@ void isis_adj_print_json(struct isis_adjacency *adj, struct json_object *json,
 			default:
 				continue;
 			}
-			backup = (sra->type == ISIS_SR_LAN_BACKUP) ? " (backup)"
+			backup = (sra->type == ISIS_SR_ADJ_BACKUP) ? " (backup)"
 								   : "";
 
 			json_object_string_add(adj_sid_json, "nexthop",
@@ -862,7 +862,7 @@ void isis_adj_print_vty(struct isis_adjacency *adj, struct vty *vty,
 			default:
 				continue;
 			}
-			backup = (sra->type == ISIS_SR_LAN_BACKUP) ? " (backup)"
+			backup = (sra->type == ISIS_SR_ADJ_BACKUP) ? " (backup)"
 								   : "";
 
 			vty_out(vty, "    %s %s%s: %u\n",

isisd/isis_lfa.c
@@ -916,9 +916,8 @@ int isis_tilfa_check(struct isis_spftree *spftree_pc,
 
 		adj = isis_adj_find(spftree_pc->area, spftree_pc->level,
 				    vertex->N.id);
-		if (adj
-		    && isis_sr_adj_sid_find(adj, spftree_pc->family,
-					    ISIS_SR_LAN_BACKUP)) {
+		if (adj && isis_sr_adj_sid_find(adj, spftree_pc->family,
+						ISIS_SR_ADJ_BACKUP)) {
 			if (IS_DEBUG_LFA)
 				zlog_debug(
 					"ISIS-LFA: %s %s already covered by node protection",

isisd/isis_sr.c
@@ -462,8 +462,7 @@ void isis_area_delete_backup_adj_sids(struct isis_area *area, int level)
 	struct listnode *node, *nnode;
 
 	for (ALL_LIST_ELEMENTS(area->srdb.adj_sids, node, nnode, sra))
-		if (sra->type == ISIS_SR_LAN_BACKUP
-		    && (sra->adj->level & level))
+		if (sra->type == ISIS_SR_ADJ_BACKUP && (sra->adj->level & level))
 			sr_adj_sid_del(sra);
 }
 
@@ -689,7 +688,7 @@ void sr_adj_sid_add_single(struct isis_adjacency *adj, int family, bool backup,
 		circuit->ext = isis_alloc_ext_subtlvs();
 
 	sra = XCALLOC(MTYPE_ISIS_SR_INFO, sizeof(*sra));
-	sra->type = backup ? ISIS_SR_LAN_BACKUP : ISIS_SR_ADJ_NORMAL;
+	sra->type = backup ? ISIS_SR_ADJ_BACKUP : ISIS_SR_ADJ_NORMAL;
 	sra->input_label = input_label;
 	sra->nexthop.family = family;
 	sra->nexthop.address = nexthop;
@@ -819,7 +818,7 @@ static void sr_adj_sid_del(struct sr_adjacency *sra)
 		exit(1);
 	}
 
-	if (sra->type == ISIS_SR_LAN_BACKUP && sra->backup_nexthops) {
+	if (sra->type == ISIS_SR_ADJ_BACKUP && sra->backup_nexthops) {
 		sra->backup_nexthops->del =
 			(void (*)(void *))isis_nexthop_delete;
 		list_delete(&sra->backup_nexthops);

isisd/isis_sr.h
@@ -82,7 +82,7 @@ struct sr_local_block {
 /* Segment Routing Adjacency-SID type. */
 enum sr_adj_type {
 	ISIS_SR_ADJ_NORMAL = 0,
-	ISIS_SR_LAN_BACKUP,
+	ISIS_SR_ADJ_BACKUP,
 };
 
 /* Segment Routing Adjacency. */

isisd/isis_zebra.c
@@ -454,7 +454,7 @@ void isis_zebra_send_adjacency_sid(int cmd, const struct sr_adjacency *sra)
 	znh->labels[0] = MPLS_LABEL_IMPLICIT_NULL;
 
 	/* Set backup nexthops. */
-	if (sra->type == ISIS_SR_LAN_BACKUP) {
+	if (sra->type == ISIS_SR_ADJ_BACKUP) {
 		int count;
 
 		count = isis_zebra_add_nexthops(isis, sra->backup_nexthops,