c8e0ece39d046d5cd0aa18ff27bbb99a28f5a9cc
Donald Sharp <sharpd@nvidia.com>
2024-04-11 13:47:57 -0400

    bgpd: Convert int's to bool in a couple of spots
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_evpn.c
+++ bgpd/bgp_evpn.c
@@ -3862,7 +3862,7 @@ int bgp_evpn_route_entry_install_if_vrf_match(struct bgp *bgp_vrf,
  * Install or uninstall mac-ip routes are appropriate for this
  * particular VRF.
  */
-static int install_uninstall_routes_for_vrf(struct bgp *bgp_vrf, int install)
+static int install_uninstall_routes_for_vrf(struct bgp *bgp_vrf, bool install)
 {
 	afi_t afi;
 	safi_t safi;
@@ -3926,7 +3926,7 @@ static int install_uninstall_routes_for_vrf(struct bgp *bgp_vrf, int install)
  * particular VNI.
  */
 static int install_uninstall_routes_for_vni(struct bgp *bgp,
-					    struct bgpevpn *vpn, int install)
+					    struct bgpevpn *vpn, bool install)
 {
 	afi_t afi;
 	safi_t safi;
@@ -4013,7 +4013,7 @@ static int install_uninstall_routes_for_vni(struct bgp *bgp,
  */
 static int install_routes_for_vrf(struct bgp *bgp_vrf)
 {
-	install_uninstall_routes_for_vrf(bgp_vrf, 1);
+	install_uninstall_routes_for_vrf(bgp_vrf, true);
 	return 0;
 }
 
@@ -4028,13 +4028,13 @@ static int install_routes_for_vni(struct bgp *bgp, struct bgpevpn *vpn)
 	 * Install type-3 routes followed by type-2 routes - the ones applicable
 	 * for this VNI.
 	 */
-	return install_uninstall_routes_for_vni(bgp, vpn, 1);
+	return install_uninstall_routes_for_vni(bgp, vpn, true);
 }
 
 /* uninstall routes from l3vni vrf. */
 static int uninstall_routes_for_vrf(struct bgp *bgp_vrf)
 {
-	install_uninstall_routes_for_vrf(bgp_vrf, 0);
+	install_uninstall_routes_for_vrf(bgp_vrf, false);
 	return 0;
 }
 
@@ -4048,7 +4048,7 @@ static int uninstall_routes_for_vni(struct bgp *bgp, struct bgpevpn *vpn)
 	 * Uninstall type-2 routes followed by type-3 routes - the ones
 	 * applicable for this VNI.
 	 */
-	return install_uninstall_routes_for_vni(bgp, vpn, 0);
+	return install_uninstall_routes_for_vni(bgp, vpn, false);
 }
 
 /*
