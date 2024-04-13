d6da504af7d60f121e49bbac4b137060c23e5193
Donatas Abraitis <donatas@opensourcerouting.org>
2024-04-13 22:17:01 +0300

    bgpd: Drop newline in JSON output for `show bgp afi safi json detail`
    Before:
    
    ```
    {
     "vrfId": 0,
     "vrfName": "default",
     "tableVersion": 2,
     "routerId": "1.1.1.1",
     "defaultLocPrf": 100,
     "localAS": 65001,
     "routes": { "192.168.1.0/24": {
    "prefix": "192.168.1.0/24",
    "version": "1",
    
    "paths": [{"aspath":{"string":"Local","segments":[],"length":0},"origin":"IGP","metric":0,"weight":32768,"valid":true,"version":1,"sourced":true,"local":true,"bestpath":{"overall":true,"selectionReason":"First path received"},"lastUpdate":{"epoch":1713035588,"string":"Sat Apr 13 22:13:08 2024\n"},"nexthops":[{"ip":"0.0.0.0","hostname":"donatas.net","afi":"ipv4","metric":0,"accessible":true,"used":true}],"peer":{"peerId":"0.0.0.0","routerId":"1.1.1.1"}}]
    } ,"192.168.11.0/24": {
    "prefix": "192.168.11.0/24",
    "version": "2",
    
    "paths": [{"aspath":{"string":"Local","segments":[],"length":0},"origin":"IGP","metric":0,"weight":32768,"valid":true,"version":2,"sourced":true,"local":true,"bestpath":{"overall":true,"selectionReason":"First path received"},"lastUpdate":{"epoch":1713035588,"string":"Sat Apr 13 22:13:08 2024\n"},"nexthops":[{"ip":"0.0.0.0","hostname":"donatas.net","afi":"ipv4","metric":0,"accessible":true,"used":true}],"peer":{"peerId":"0.0.0.0","routerId":"1.1.1.1"}}]
    }  }  }
    ```
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>
    (cherry picked from commit 1dc28e1d735614715bab5d438bddcb2def6c8f3c)

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -11875,7 +11875,7 @@ void route_vty_out_detail_header(struct vty *vty, struct bgp *bgp,
 		} else {
 			if (incremental_print) {
 				vty_out(vty, "\"prefix\": \"%pFX\",\n", p);
-				vty_out(vty, "\"version\": \"%" PRIu64 "\",\n",
+				vty_out(vty, "\"version\": \"%" PRIu64 "\",",
 					dest->version);
 			} else {
 				json_object_string_addf(json, "prefix", "%pFX",
