9c9f9ce3c11c16c9b43ed719137b3a2a57b7eee8
Donatas Abraitis <donatas@opensourcerouting.org>
2024-04-21 14:18:39 +0300

    bgpd: Inherit `capability software-version` flag from the peer-group
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -1542,7 +1542,7 @@ struct peer *peer_new(struct bgp *bgp)
 		peer_flag_set(peer, PEER_FLAG_ENFORCE_FIRST_AS);
 
 	if (CHECK_FLAG(bgp->flags, BGP_FLAG_SOFT_VERSION_CAPABILITY))
-		SET_FLAG(peer->flags, PEER_FLAG_CAPABILITY_SOFT_VERSION);
+		peer_flag_set(peer, PEER_FLAG_CAPABILITY_SOFT_VERSION);
 
 	if (CHECK_FLAG(bgp->flags, BGP_FLAG_DYNAMIC_CAPABILITY))
 		SET_FLAG(peer->flags, PEER_FLAG_DYNAMIC_CAPABILITY);
