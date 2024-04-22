FRRouting/frr

commit 2566656a6f5082e62bafa4a5e42764f666499876

isisd: Add data structure for SRv6 Adjacency SIDs
An SRv6 adjacency SID is a SID that is associated with a particular
adjacency. Adjacency SIDs are advertised using the SRv6 End.X SID
Sub-TLV (RFC 9352 section #8.1) or SRv6 LAN End.X SID Sub-TLV (RFC 9352
section #8.2).

This commit defines a data structure `srv6_adjacency` for storing
information about an SRv6 Adjacency SID. This data structure will be
used to support SRv6 Adjacency SIDs functionalities in future commits.

Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

isisd/isis_srv6.h
@@ -69,6 +69,48 @@ struct isis_srv6_locator {
 	struct list *srv6_sid;
 };
 
+/* SRv6 Adjacency-SID type */
+enum srv6_adj_type {
+	ISIS_SRV6_ADJ_NORMAL = 0,
+	ISIS_SRV6_LAN_BACKUP,
+};
+
+/* SRv6 Adjacency. */
+struct srv6_adjacency {
+	/* Adjacency type */
+	enum srv6_adj_type type;
+
+	/* SID flags */
+	uint8_t flags;
+
+	/* SID value */
+	struct in6_addr sid;
+
+	/* Endpoint behavior bound to the SID */
+	enum srv6_endpoint_behavior_codepoint behavior;
+
+	/* SRv6 SID structure */
+	struct isis_srv6_sid_structure structure;
+
+	/* Parent SRv6 locator */
+	struct srv6_locator_chunk *locator;
+
+	/* Adjacency-SID nexthop information */
+	struct in6_addr nexthop;
+
+	/* End.X SID TI-LFA backup nexthops */
+	struct list *backup_nexthops;
+
+	/* SRv6 (LAN) End.X SID Sub-TLV */
+	union {
+		struct isis_srv6_endx_sid_subtlv *endx_sid;
+		struct isis_srv6_lan_endx_sid_subtlv *lendx_sid;
+	} u;
+
+	/* Back pointer to IS-IS adjacency. */
+	struct isis_adjacency *adj;
+};
+
 /* Per-area IS-IS SRv6 Data Base (SRv6 DB) */
 struct isis_srv6_db {
 

commit ad874c7d3bb5de8a5615795b83cf955d578a3faf

isisd: fix renaming backup define for ENDX SID
The define has inheritated from a wrong naming in sr-mpls.
Fix this by renaming ISIS_SRV6_LAN_BACKUP to ISIS_SRV6_ADJ_BACKUP.

Fixes: 2566656a6f50 ("isisd: Add data structure for SRv6 Adjacency SIDs")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

isisd/isis_srv6.c
@@ -363,7 +363,7 @@ void isis_area_delete_backup_srv6_endx_sids(struct isis_area *area, int level)
 	struct listnode *node, *nnode;
 
 	for (ALL_LIST_ELEMENTS(area->srv6db.srv6_endx_sids, node, nnode, sra))
-		if (sra->type == ISIS_SRV6_LAN_BACKUP &&
+		if (sra->type == ISIS_SRV6_ADJ_BACKUP &&
 		    (sra->adj->level & level))
 			srv6_endx_sid_del(sra);
 }
@@ -420,7 +420,7 @@ void srv6_endx_sid_add_single(struct isis_adjacency *adj, bool backup,
 			   : SRV6_ENDPOINT_BEHAVIOR_END_X;
 
 	sra = XCALLOC(MTYPE_ISIS_SRV6_INFO, sizeof(*sra));
-	sra->type = backup ? ISIS_SRV6_LAN_BACKUP : ISIS_SRV6_ADJ_NORMAL;
+	sra->type = backup ? ISIS_SRV6_ADJ_BACKUP : ISIS_SRV6_ADJ_NORMAL;
 	sra->behavior = behavior;
 	sra->locator = chunk;
 	sra->structure.loc_block_len = chunk->block_bits_length;
@@ -538,7 +538,7 @@ void srv6_endx_sid_del(struct srv6_adjacency *sra)
 		exit(1);
 	}
 
-	if (sra->type == ISIS_SRV6_LAN_BACKUP && sra->backup_nexthops) {
+	if (sra->type == ISIS_SRV6_ADJ_BACKUP && sra->backup_nexthops) {
 		sra->backup_nexthops->del =
 			(void (*)(void *))isis_nexthop_delete;
 		list_delete(&sra->backup_nexthops);

isisd/isis_srv6.h
@@ -68,7 +68,7 @@ struct isis_srv6_locator {
 /* SRv6 Adjacency-SID type */
 enum srv6_adj_type {
 	ISIS_SRV6_ADJ_NORMAL = 0,
-	ISIS_SRV6_LAN_BACKUP,
+	ISIS_SRV6_ADJ_BACKUP,
 };
 
 /* SRv6 Adjacency. */