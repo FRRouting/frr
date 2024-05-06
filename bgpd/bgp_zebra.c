FRRouting/frr

commit 94d12dc490d7c4109828e7d4421e7ae0d84c3448

bgpd: update route leak when vrf appears
If the VRF is not yet created and a BGP instance is created for the
VRF, dependent leaked routes are inactive, which is normal. However,
when the VRF interface appears, they remains inactive.

Update route leak when a VRF interface appears. Note that routes to a
deleted VRF are already removed by zebra.

Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_zebra.c
@@ -3160,6 +3160,15 @@ static int bgp_ifp_create(struct interface *ifp)
 
 	bgp_update_interface_nbrs(bgp, ifp, ifp);
 	hook_call(bgp_vrf_status_changed, bgp, ifp);
+
+	if (bgp_get_default() && if_is_loopback(ifp)) {
+		vpn_leak_zebra_vrf_label_update(bgp, AFI_IP);
+		vpn_leak_zebra_vrf_label_update(bgp, AFI_IP6);
+		vpn_leak_zebra_vrf_sid_update(bgp, AFI_IP);
+		vpn_leak_zebra_vrf_sid_update(bgp, AFI_IP6);
+		vpn_leak_postchange_all();
+	}
+
 	return 0;
 }
 

commit c3f942de6e66cac7971d8f9bbac80e393cc36f71

bgpd: fix avoid calling VPN processing at VRF interface creation
At VRF interface creation, the vpn_leak*() functions are called,
whereas at creation, the VRF interface may not be yet operational
because it is not up.

Calling the VPN processing functions is useless compared to
before interface processing. Also, betting on interface up event
now is not justified, as a separate notification will be sent
later.

Fix this by not triggering changes for VRF interface creation.

Fixes: 94d12dc490d7 ("bgpd: update route leak when vrf appears")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_zebra.c
@@ -3338,7 +3338,7 @@ static int bgp_ifp_create(struct interface *ifp)
 	bgp_update_interface_nbrs(bgp, ifp, ifp);
 	hook_call(bgp_vrf_status_changed, bgp, ifp);
 
-	if (bgp_get_default() && if_is_loopback(ifp)) {
+	if (bgp_get_default() && if_is_loopback_exact(ifp)) {
 		vpn_leak_zebra_vrf_label_update(bgp, AFI_IP);
 		vpn_leak_zebra_vrf_label_update(bgp, AFI_IP6);
 		vpn_leak_zebra_vrf_sid_update(bgp, AFI_IP);