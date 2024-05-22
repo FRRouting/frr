FRRouting/frr

commit f185005b2f2f17d5bde3cc20c1f6b4799a46c0b2

isisd: fix the display topology command
The unconfigured flexible algorithm should not be displayed.

Before:
```
anlan# show isis topology algorithm
Area A: Algorithm 128

Area A:Area A:Area A:Area A:Area A:Area A:Area A:Area A:Area A:Area ...
......
anlan#
```

After:
```
anlan# show isis topology algorithm
Area A: Algorithm 128

anlan#
```

Signed-off-by: anlan_cs <anlan_cs@tom.com>

isisd/isis_spf.c
@@ -2343,9 +2343,6 @@ static void show_isis_topology_common(struct vty *vty, int levels,
 		return;
 
 	for (ALL_LIST_ELEMENTS_RO(isis->area_list, node, area)) {
-		vty_out(vty,
-			"Area %s:", area->area_tag ? area->area_tag : "null");
-
 #ifndef FABRICD
 		/*
 		 * The shapes of the flex algo spftree 2-dimensional array
@@ -2360,6 +2357,9 @@ static void show_isis_topology_common(struct vty *vty, int levels,
 			fa_data = (struct isis_flex_algo_data *)fa->data;
 		} else
 			fa_data = NULL;
+
+		vty_out(vty,
+			"Area %s:", area->area_tag ? area->area_tag : "null");
 
 		if (algo != SR_ALGORITHM_SPF)
 			vty_out(vty, " Algorithm %hhu\n", algo);

commit c2058bb0a0a657d61bb16917ee9b8f90d0dda503

isisd: fix show isis topology display
Fix "Area X:" display for fabricd

Fixes: f185005b2f ("isisd: fix the display topology command")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

isisd/isis_spf.c
@@ -2357,10 +2357,12 @@ static void show_isis_topology_common(struct vty *vty, int levels,
 			fa_data = (struct isis_flex_algo_data *)fa->data;
 		} else
 			fa_data = NULL;
+#endif /* ifndef FABRICD */
 
 		vty_out(vty,
 			"Area %s:", area->area_tag ? area->area_tag : "null");
 
+#ifndef FABRICD
 		if (algo != SR_ALGORITHM_SPF)
 			vty_out(vty, " Algorithm %hhu\n", algo);
 		else