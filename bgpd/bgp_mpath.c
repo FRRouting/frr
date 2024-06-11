FRRouting/frr

commit 7dc9d4e4e3609804aee5c4665ed998b570bca8f2

bgp may add multiple path entries with the same nexthop
Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>
Reviewed-by:   Donald Sharp <sharpd@cumulusnetworks.com>

Ticket: CM-8129

- We have 14 paths for each prefix, 7 are from ipv4 peers and 7 are from ipv6 peers

- There are 7 unique nexthops

- When comparing the exact same path from an v4 peer vs. a v6 peer the path from
  the v4 peer wins.  This is due to the "lowest neighbor IP" check in the decision
  algorithm.  For example below we learn NEXTHOP 210.2.4.2 from 210.2.4.2 and
  2001:20:4::2 but only the one from the v4 peer is flagged as multipath.

- The problem is when our bestpath is from a v6 peer, 2001:20:2::2 in this case
  (see line 85). 2001:20:2::2 sent us 210.2.2.2 so we will install that nexthop
  because it is from our bestpath, the problem is we flag the path from 210.2.2.2
  (line 37) as multipath which causes us to install two paths with nexthop 210.2.2.2

  1 superm-redxp-05# show ip bgp 2.23.24.192/28
  2 BGP routing table entry for 2.23.24.192/28
  3 Paths: (14 available, best #14, table Default-IP-Routing-Table)
  4   Advertised to non peer-group peers:
  5   210.2.0.2 210.2.1.2 210.2.2.2 210.2.3.2 210.2.4.2 210.2.5.2 210.2.6.2 210.4.1.4 2001:20::2 2001:20:1::2 2001:20:2::2 2001:20:3::2 2001:20:4::2 2001:2
  6   205 200 300 790 90 80 2334 544 56 67 889 3111 777 8 879 900 88 7654 3211 113 43434 666 343 4534 667 7688
  7     210.2.4.2 from 210.2.4.2 (10.0.0.2)
  8       Origin IGP, localpref 100, valid, external, multipath
  9       Community: 0:100 0:200 0:300 0:324 0:2938 0:3344 0:3545 0:4466 0:5445 0:5754
 10       Last update: Wed Nov 11 20:54:57 2015
 11
 12   204 200 300 790 90 80 2334 544 56 67 889 3111 777 8 879 900 88 7654 3211 113 43434 666 343 4534 667 7688
 13     210.2.3.2 from 210.2.3.2 (10.0.0.2)
 14       Origin IGP, localpref 100, valid, external, multipath
 15       Community: 0:100 0:200 0:300 0:324 0:2938 0:3344 0:3545 0:4466 0:5445 0:5754

bgpd/bgp_mpath.c
@@ -103,7 +103,7 @@ bgp_maximum_paths_unset (struct bgp *bgp, afi_t afi, safi_t safi,
  * or greater than zero if bi1 is respectively less than, equal to,
  * or greater than bi2.
  */
-static int
+int
 bgp_info_nexthop_cmp (struct bgp_info *bi1, struct bgp_info *bi2)
 {
   struct attr_extra *ae1, *ae2;

bgpd/bgp_route.c
@@ -1637,12 +1637,20 @@ bgp_best_selection (struct bgp *bgp, struct bgp_node *rn,
             if (ri->peer->status != Established)
               continue;
 
+          if (!bgp_info_nexthop_cmp (ri, new_select))
+            {
+              if (debug)
+                zlog_debug("%s: path %s has the same nexthop as the bestpath, skip it",
+                           pfx_buf, ri->peer->host);
+              continue;
+            }
+
           bgp_info_cmp (bgp, ri, new_select, &paths_eq, mpath_cfg, debug, pfx_buf);
 
           if (paths_eq)
             {
               if (debug)
-                zlog_debug("%s: %s path is equivalent to the bestpath, add to the multipath list",
+                zlog_debug("%s: path %s is equivalent to the bestpath, add to the multipath list",
                            pfx_buf, ri->peer->host);
 	      bgp_mp_list_add (&mp_list, ri);
             }

bgpd/bgp_mpath.h
@@ -59,6 +59,7 @@ extern int bgp_maximum_paths_unset (struct bgp *, afi_t, safi_t, int);
 /* Functions used by bgp_best_selection to record current
  * multipath selections
  */
+extern int bgp_info_nexthop_cmp (struct bgp_info *bi1, struct bgp_info *bi2);
 extern void bgp_mp_list_init (struct list *);
 extern void bgp_mp_list_clear (struct list *);
 extern void bgp_mp_list_add (struct list *, struct bgp_info *);

bgpd/bgpd.c
@@ -6899,10 +6899,10 @@ bgp_config_write (struct vty *vty)
             {
               vty_out (vty, " bgp bestpath as-path multipath-relax as-set%s", VTY_NEWLINE);
             }
-        }
-     else
-        {
-	  vty_out (vty, " no bgp bestpath as-path multipath-relax%s", VTY_NEWLINE);
+          else
+            {
+              vty_out (vty, " bgp bestpath as-path multipath-relax%s", VTY_NEWLINE);
+            }
         }
 
       if (bgp_flag_check (bgp, BGP_FLAG_RR_ALLOW_OUTBOUND_POLICY)) {

commit 9fb7d677d3584eadd1d4568bedd542eb880afcd7

bgpd: fix do not skip paths with same nexthop
Under a setup where two BGP prefixes are available from multiple sources,
if one of the two prefixes is recursive over the other BGP prefix, then
it will not be considered as multipath. The below output shows the two
prefixes 192.0.2.24/32 and 192.0.2.21/32. The 192.0.2.[5,6,8] are the
known IP addresses visible from the IGP.

> # show bgp ipv4 192.0.2.24/32
> *>i 192.0.2.24/32    192.0.2.21               0    100      0 i
> * i                  192.0.2.21               0    100      0 i
> * i                  192.0.2.21               0    100      0 i
> # show bgp ipv4 192.0.2.21/32
>  *>i 192.0.2.21/32    192.0.2.5                0    100      0 i
>  *=i                  192.0.2.6                0    100      0 i
>  *=i                  192.0.2.8                0    100      0 i

The bgp best selection algorithm refuses to consider the paths to
'192.0.2.24/32' as multipath, whereas the BGP paths which use the
BGP peer as nexthop are considered multipath.

> ... has the same nexthop as the bestpath, skip it ...

Previously, this condition has been added to prevent ZEBRA from
installing routes with same nexthop:

>     Here you can see the two paths with nexthop 210.2.2.2
>     superm-redxp-05# show ip route 2.23.24.192/28
>     Routing entry for 2.23.24.192/28
>       Known via "bgp", distance 20, metric 0, best
>       Last update 00:32:12 ago
>       * 210.2.2.2, via swp3
>       * 210.2.0.2, via swp1
>       * 210.2.1.2, via swp2
>       * 210.2.2.2, via swp3
> [..]

But today, ZEBRA knows how to handle it. When receiving incoming routes,
nexthop groups are used. At creation, duplicated nexthops are
identified, and will not be installed. The below output illustrate the
duplicate paths to 172.16.0.200 received by an other peer.

> r1# show ip route 172.18.1.100 nexthop-group
> Routing entry for 172.18.1.100/32
>   Known via "bgp", distance 200, metric 0, best
>   Last update 00:03:03 ago
>   Nexthop Group ID: 75757580
>     172.16.0.200 (recursive), weight 1
>   *   172.31.0.3, via r1-eth1, label 16055, weight 1
>   *   172.31.2.4, via r1-eth2, label 16055, weight 1
>   *   172.31.0.3, via r1-eth1, label 16006, weight 1
>   *   172.31.2.4, via r1-eth2, label 16006, weight 1
>   *   172.31.8.7, via r1-eth4, label 16008, weight 1
>     172.16.0.200 (duplicate nexthop removed) (recursive), weight 1
>       172.31.0.3, via r1-eth1 (duplicate nexthop removed), label 16055, weight 1
>       172.31.2.4, via r1-eth2 (duplicate nexthop removed), label 16055, weight 1
>       172.31.0.3, via r1-eth1 (duplicate nexthop removed), label 16006, weight 1
>       172.31.2.4, via r1-eth2 (duplicate nexthop removed), label 16006, weight 1
>       172.31.8.7, via r1-eth4 (duplicate nexthop removed), label 16008, weight 1

Fix this by proposing to let ZEBRA handle this duplicate decision.

Fixes: 7dc9d4e4e360 ("bgp may add multiple path entries with the same nexthop")

bgpd/bgp_mpath.c
@@ -605,31 +605,43 @@ void bgp_path_info_mpath_update(struct bgp *bgp, struct bgp_dest *dest,
 		if (mp_node && (listgetdata(mp_node) == cur_mpath)) {
 			list_delete_node(mp_list, mp_node);
 			bgp_path_info_mpath_dequeue(cur_mpath);
-			if ((mpath_count < maxpaths)
-			    && prev_mpath
-			    && bgp_path_info_nexthop_cmp(prev_mpath,
-							 cur_mpath)) {
+			if ((mpath_count < maxpaths) && prev_mpath) {
+				mpath_count++;
+				if (bgp_path_info_nexthop_cmp(prev_mpath,
+							      cur_mpath)) {
+					if (ecommunity_linkbw_present(
+						    bgp_attr_get_ecommunity(
+							    cur_mpath->attr),
+						    &bwval) ||
+					    ecommunity_linkbw_present(
+						    bgp_attr_get_ipv6_ecommunity(
+							    cur_mpath->attr),
+						    &bwval))
+						cum_bw += bwval;
+					else
+						all_paths_lb = false;
+					if (debug) {
+						bgp_path_info_path_with_addpath_rx_str(
+							cur_mpath, path_buf,
+							sizeof(path_buf));
+						zlog_debug("%pBD: %s is still multipath, cur count %d",
+							   dest, path_buf,
+							   mpath_count);
+					}
+				} else {
+					if (debug) {
+						bgp_path_info_path_with_addpath_rx_str(
+							cur_mpath, path_buf,
+							sizeof(path_buf));
+						zlog_debug("%pBD: nexthop equal, however add mpath %s nexthop %pI4, cur count %d",
+							   dest, path_buf,
+							   &cur_mpath->attr->nexthop,
+							   mpath_count);
+					}
+				}
 				bgp_path_info_mpath_enqueue(prev_mpath,
 							    cur_mpath);
 				prev_mpath = cur_mpath;
-				mpath_count++;
-				if (ecommunity_linkbw_present(bgp_attr_get_ecommunity(
-								      cur_mpath->attr),
-							      &bwval) ||
-				    ecommunity_linkbw_present(
-					    bgp_attr_get_ipv6_ecommunity(
-						    cur_mpath->attr),
-					    &bwval))
-					cum_bw += bwval;
-				else
-					all_paths_lb = false;
-				if (debug) {
-					bgp_path_info_path_with_addpath_rx_str(
-						cur_mpath, path_buf,
-						sizeof(path_buf));
-					zlog_debug("%pBD: %s is still multipath, cur count %d",
-						   dest, path_buf, mpath_count);
-				}
 			} else {
 				mpath_changed = 1;
 				if (debug) {
@@ -693,35 +705,50 @@ void bgp_path_info_mpath_update(struct bgp *bgp, struct bgp_dest *dest,
 			list_delete_node(mp_list, mp_node);
 			assert(new_mpath);
 			assert(prev_mpath);
-			if ((mpath_count < maxpaths) && (new_mpath != new_best)
-			    && bgp_path_info_nexthop_cmp(prev_mpath,
-							 new_mpath)) {
+			if ((mpath_count < maxpaths) && (new_mpath != new_best)) {
+				/* keep duplicate nexthop */
 				bgp_path_info_mpath_dequeue(new_mpath);
 
 				bgp_path_info_mpath_enqueue(prev_mpath,
 							    new_mpath);
-				prev_mpath = new_mpath;
 				mpath_changed = 1;
 				mpath_count++;
-				if (ecommunity_linkbw_present(bgp_attr_get_ecommunity(
-								      new_mpath->attr),
-							      &bwval) ||
-				    ecommunity_linkbw_present(
-					    bgp_attr_get_ipv6_ecommunity(
-						    new_mpath->attr),
-					    &bwval))
-					cum_bw += bwval;
-				else
-					all_paths_lb = false;
-				if (debug) {
-					bgp_path_info_path_with_addpath_rx_str(
-						new_mpath, path_buf,
-						sizeof(path_buf));
-					zlog_debug("%pBD: add mpath %s nexthop %pI4, cur count %d",
-						   dest, path_buf,
-						   &new_mpath->attr->nexthop,
-						   mpath_count);
+				if (bgp_path_info_nexthop_cmp(prev_mpath,
+							      new_mpath)) {
+					if (ecommunity_linkbw_present(
+						    bgp_attr_get_ecommunity(
+							    new_mpath->attr),
+						    &bwval) ||
+					    ecommunity_linkbw_present(
+						    bgp_attr_get_ipv6_ecommunity(
+							    new_mpath->attr),
+						    &bwval))
+						cum_bw += bwval;
+					else
+						all_paths_lb = false;
+					if (debug) {
+						bgp_path_info_path_with_addpath_rx_str(
+							new_mpath, path_buf,
+							sizeof(path_buf));
+						zlog_debug("%pBD: add mpath %s nexthop %pI4, cur count %d",
+							   dest, path_buf,
+							   &new_mpath->attr
+								    ->nexthop,
+							   mpath_count);
+					}
+				} else {
+					if (debug) {
+						bgp_path_info_path_with_addpath_rx_str(
+							new_mpath, path_buf,
+							sizeof(path_buf));
+						zlog_debug("%pBD: nexthop equal, however add mpath %s nexthop %pI4, cur count %d",
+							   dest, path_buf,
+							   &new_mpath->attr
+								    ->nexthop,
+							   mpath_count);
+					}
 				}
+				prev_mpath = new_mpath;
 			}
 			mp_node = mp_next_node;
 		}

bgpd/bgp_route.c
@@ -3256,15 +3256,6 @@ void bgp_best_selection(struct bgp *bgp, struct bgp_dest *dest,
 				if (!peer_established(pi->peer->connection))
 					continue;
 
-			if (!bgp_path_info_nexthop_cmp(pi, new_select)) {
-				if (debug)
-					zlog_debug(
-						"%pBD(%s): %s has the same nexthop as the bestpath, skip it",
-						dest, bgp->name_pretty,
-						path_buf);
-				continue;
-			}
-
 			bgp_path_info_cmp(bgp, pi, new_select, &paths_eq,
 					  mpath_cfg, debug, pfx_buf, afi, safi,
 					  &dest->reason);