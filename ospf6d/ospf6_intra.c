426b9c8cddce71226a8648b614e6c46e0c3fae98
Andrew Cooks <acooks.at.bda@gmail.com>
2024-06-11 12:08:34 +1000

    let ospf6_router_lsa_get_nbr_id handle E-Router-LSA
    Signed-off-by: Andrew Cooks <acooks.at.bda@gmail.com>

--- ospf6d/ospf6_intra.c
+++ ospf6d/ospf6_intra.c
@@ -83,6 +83,15 @@ static char *ospf6_router_lsa_get_nbr_id(struct ospf6_lsa *lsa, char *buf,
 	if (!lsa || !buf)
 		return NULL;
 
+	if (lsa->header->type == OSPF6_LSTYPE_E_ROUTER)
+		handler.tlv_type = OSPF6_TLV_ROUTER_LINK;
+	else
+		/*
+		 * Router LSA doesn't need a tlv_type;
+		 * Other types are not handled.
+		 */
+		handler.tlv_type = OSPF6_TLV_RESERVED;
+
 	foreach_lsdesc(lsa->header, &handler);
 
 	/* found it? */
