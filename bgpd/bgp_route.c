3b98ddf5018cf7526b50c15018cbaf71a38fa752
Donatas Abraitis <donatas@opensourcerouting.org>
2024-06-24 20:16:16 +0300

    bgpd: Relax OAD (One-Administration-Domain) for RFC8212
    RFC 8212 defines leak prevention for eBGP peers, but BGP-OAD defines a new
    peering type One Administrative Domain (OAD), where multiple ASNs could be used
    inside a single administrative domain. OAD allows sending non-transitive attributes,
    so this prevention should be relaxed too.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -6333,7 +6333,7 @@ void bgp_set_stale_route(struct peer *peer, afi_t afi, safi_t safi)
 
 bool bgp_outbound_policy_exists(struct peer *peer, struct bgp_filter *filter)
 {
-	if (peer->sort == BGP_PEER_IBGP)
+	if (peer->sort == BGP_PEER_IBGP || peer->sub_sort == BGP_PEER_EBGP_OAD)
 		return true;
 
 	if (peer->sort == BGP_PEER_EBGP &&
@@ -6346,7 +6346,7 @@ bool bgp_outbound_policy_exists(struct peer *peer, struct bgp_filter *filter)
 
 bool bgp_inbound_policy_exists(struct peer *peer, struct bgp_filter *filter)
 {
-	if (peer->sort == BGP_PEER_IBGP)
+	if (peer->sort == BGP_PEER_IBGP || peer->sub_sort == BGP_PEER_EBGP_OAD)
 		return true;
 
 	if (peer->sort == BGP_PEER_EBGP
