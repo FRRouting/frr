1f80213fca6eabb6590a08427f2f9494148bc048
Philippe Guibert <philippe.guibert@6wind.com>
2024-06-24 08:33:58 +0200

    sharpd: add 'use-nexthop-group' keyword for route install
    Some CP daemons may add a route pointing to a non installed
    nexthop-group. Add the 'use-nexthop-group' keyword to allow
    the usage of the nexthop-group identifier.
    
    > ubuntu2204(config)# nexthop-group ABCD
    > ubuntu2204(config-nh-group)# nexthop 5.5.6.6
    > ubuntu2204(config-nh-group)# end
    > ubuntu2204# sharp install routes 3.4.3.3 nexthop-group ABCD 1 use-nexthop-group
    >
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- sharpd/sharp_zebra.c
+++ sharpd/sharp_zebra.c
@@ -239,10 +239,9 @@ static bool route_add(const struct prefix *p, vrf_id_t vrf_id, uint8_t instance,
 	api.flags = flags;
 	SET_FLAG(api.message, ZAPI_MESSAGE_NEXTHOP);
 
-	/* Only send via ID if nhgroup has been successfully installed */
-	if (nhgid && sharp_nhgroup_id_is_installed(nhgid)) {
+	if (nhgid)
 		zapi_route_set_nhg_id(&api, &nhgid);
-	} else {
+	else {
 		for (ALL_NEXTHOPS_PTR(nhg, nh)) {
 			/* Check if we set a VNI label */
 			if (nh->nh_label &&
