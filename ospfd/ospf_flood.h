4571625cbb44a8d95ac4f620791a281bb5ac1525
Acee Lindem <acee@lindem.com>
2024-07-05 20:44:30 +0000

    ospfd: Fix several problems with direct acknowledgments and improved delay acks.
       1. On P2MP interfaces, direct ack would include the same LSA multiple times
          multiple packets are processed before the OSPF interfae direct LSA
          acknowledgment event is processed. Now duplicates LSA in the same event
          are suppressed.
       2. On non-broadcast interfaces, direct acks for multiple neighbors would be
          unicast to the same neighbor due to the multiple OSPF LS Update packets
          being process prior to the OSPF interface direct ack event. Now, separate
          direct acks are unicast to the neighbors requiring them.
       3. The interface delayed acknowledgment timer runs would run continously
          (every second as long as the interace is up). Now, the timer is set
          when delayed acknowledgments are queued and all queued delayed
          acknowledges are sent when it fires.
       4. For non-broadcast interface delayed acknowledgments, the logic to send
          to multiple neighbors wasn't working because the list was emptied while
          building the packet for the first neighbor.
    
    Signed-off-by: Acee Lindem <acee@lindem.com>
    (cherry picked from commit ed480148844259b7e9e30ed92489cdf44085457e)
    
    # Conflicts:
    #	ospfd/ospf_flood.h
    #	ospfd/ospf_packet.h

--- ospfd/ospf_flood.h
+++ ospfd/ospf_flood.h
@@ -7,6 +7,42 @@
 #ifndef _ZEBRA_OSPF_FLOOD_H
 #define _ZEBRA_OSPF_FLOOD_H
 
+<<<<<<< HEAD
+=======
+#include "typesafe.h"
+
+/*
+ * OSPF Temporal LSA List
+ */
+PREDECL_DLIST(ospf_lsa_list);
+
+struct ospf_lsa_list_entry {
+	/* Linkage for LSA List */
+	struct ospf_lsa_list_item list_linkage;
+
+	union {
+		/*
+		 * Time associated with the list entry. For example, for a
+		 * neigbhor link retransmission list, this is the
+		 * retransmission time.
+		 */
+		struct timeval list_entry_timeval;
+
+		/*
+		 * Destanation address specific to the LSA list. For example,
+		 * the distination for an associated direct LS acknowledgment.
+		 */
+		struct in_addr list_entry_dst_addr;
+	} u;
+
+	struct ospf_lsa *lsa;
+};
+#define list_entry_time u.list_entry_timeval
+#define list_entry_dst	u.list_entry_dst_addr
+
+DECLARE_DLIST(ospf_lsa_list, struct ospf_lsa_list_entry, list_linkage);
+
+>>>>>>> ed48014884 (ospfd: Fix several problems with direct acknowledgments and improved delay acks.)
 extern int ospf_flood(struct ospf *, struct ospf_neighbor *, struct ospf_lsa *,
 		      struct ospf_lsa *);
 extern int ospf_flood_through(struct ospf *, struct ospf_neighbor *,
