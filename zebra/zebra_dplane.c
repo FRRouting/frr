FRRouting/frr

commit 27805e74f02c9e0b4f0086e8727135788cf9fe2b

zebra: Properly set NEXTHOP_FLAG_FIB when skipping install
When the dataplane detects that we have no need to
reinstall the same route, setup the NEXTHOP_FLAG_FIB
appropriately.

Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

zebra/zebra_dplane.c
@@ -2368,10 +2368,24 @@ dplane_route_update_internal(struct route_node *rn,
 		if ((dplane_ctx_get_type(ctx) == dplane_ctx_get_old_type(ctx))
 		    && (dplane_ctx_get_nhe_id(ctx)
 			== dplane_ctx_get_old_nhe_id(ctx))) {
+			struct nexthop *nexthop;
+
 			if (IS_ZEBRA_DEBUG_DPLANE)
 				zlog_debug(
 					"%s: Ignoring Route exactly the same",
 					__func__);
+
+			for (ALL_NEXTHOPS_PTR(dplane_ctx_get_ng(ctx),
+					      nexthop)) {
+				if (CHECK_FLAG(nexthop->flags,
+					       NEXTHOP_FLAG_RECURSIVE))
+					continue;
+
+				if (CHECK_FLAG(nexthop->flags,
+					       NEXTHOP_FLAG_ACTIVE))
+					SET_FLAG(nexthop->flags,
+						 NEXTHOP_FLAG_FIB);
+			}
 
 			return ZEBRA_DPLANE_REQUEST_SUCCESS;
 		}

commit 763cee506d45e8adc6d796c306df1ec0f41b1d7e

zebra: fix update nhg's nexthop FIB flag when skipping route install
When updating a route with a protocol nexthop-group, the route and
nexthop-group contexts have the fib flag value lost.

configuration used:

> configure terminal
> ip route 6.6.6.0/24 192.168.1.44
> ip route 6.6.6.0/24 192.168.1.47
> nexthop-group A
> allow-recursion
> nexthop 6.6.6.22
> ! fib flag is correctly set on nhid 181818168
> end
> sharp install routes 9.9.9.9 nexthop-group A 1
> ! fib flag is correctly set on route 9.9.9.9 nhid 181818168
> configure terminal
> ip route 6.6.6.0/24 192.168.1.49
> end
> ! re-install route to 9.9.9.9 to trigger update
> sharp install routes 9.9.9.9 nexthop-group A 1

observed:

> ubuntu2204# do show ip route 9.9.9.9 json
> {
>   "9.9.9.9/32":[
>     {
>       "prefix":"9.9.9.9/32",
> [..]
>       "nexthops":[
>         {
> [..]
>           "recursive":true,
>         },
>         {
>           "flags":1,
>           "ip":"192.168.1.44",
>           "afi":"ipv4",
>           "interfaceIndex":3,
>           "interfaceName":"loop1",
>           "resolver":true,
>           "active":true,
>           "weight":1
>         },
>         {
>           "flags":1,
>           "ip":"192.168.1.47",
>           "afi":"ipv4",
>           "interfaceIndex":3,
>           "interfaceName":"loop1",
>           "resolver":true,
>           "active":true,
>           "weight":1
>         },
>         {
>           "flags":1,
>           "ip":"192.168.1.49",
>           "afi":"ipv4",
>           "interfaceIndex":3,
>           "interfaceName":"loop1",
>           "resolver":true,
>           "active":true,
>           "weight":1
>         }
>       ]
>     }
>   ]
> }
>
> ubuntu2204# show nexthop-group rib 181818168 json
> {
>   "181818168":{
>     "type":"sharp",
>     "refCount":2,
>     "uptime":"00:06:35",
>     "vrf":"default",
>     "valid":true,
>     "installed":true,
>     "depends":[
>       324
>     ],
>     "nexthops":[
>       {
>         "flags":5,
>         "ip":"6.6.6.22",
>         "afi":"ipv4",
>         "vrf":"default",
>         "active":true,
>         "recursive":true,
>         "weight":1
>       },
>       {
>         "flags":1,
>         "ip":"192.168.1.44",
>         "afi":"ipv4",
>         "interfaceIndex":3,
>         "interfaceName":"loop1",
>         "resolver":true,
>         "vrf":"default",
>         "active":true,
>         "weight":1
>       },
>       {
>         "flags":1,
>         "ip":"192.168.1.47",
>         "afi":"ipv4",
>         "interfaceIndex":3,
>         "interfaceName":"loop1",
>         "resolver":true,
>         "vrf":"default",
>         "active":true,
>         "weight":1
>       },
>       {
>         "flags":1,
>         "ip":"192.168.1.49",
>         "afi":"ipv4",
>         "interfaceIndex":3,
>         "interfaceName":"loop1",
>         "resolver":true,
>         "vrf":"default",
>         "active":true,
>         "weight":1
>       }
>     ]
>   }
> }

This case happens when the route to update is the same and the nhg is
a protocol nexthop-group. Fix this by updating the nexthop flags of the
original nhg instead of the nhg from the passed dataplane nhg context.

Fixes: 27805e74f02c ("zebra: Properly set NEXTHOP_FLAG_FIB when skipping install")

zebra/zebra_dplane.c
@@ -4246,6 +4246,7 @@ dplane_route_update_internal(struct route_node *rn,
 	enum zebra_dplane_result result = ZEBRA_DPLANE_REQUEST_FAILURE;
 	int ret = EINVAL;
 	struct zebra_dplane_ctx *ctx = NULL;
+	struct nhg_hash_entry *nhe;
 
 	/* Obtain context block */
 	ctx = dplane_ctx_alloc();
@@ -4308,8 +4309,8 @@ dplane_route_update_internal(struct route_node *rn,
 					"%s: Ignoring Route exactly the same",
 					__func__);
 
-			for (ALL_NEXTHOPS_PTR(dplane_ctx_get_ng(ctx),
-					      nexthop)) {
+			nhe = zebra_nhg_lookup_id(dplane_ctx_get_nhe_id(ctx));
+			for (ALL_NEXTHOPS_PTR(&nhe->nhg, nexthop)) {
 				if (CHECK_FLAG(nexthop->flags,
 					       NEXTHOP_FLAG_RECURSIVE))
 					continue;