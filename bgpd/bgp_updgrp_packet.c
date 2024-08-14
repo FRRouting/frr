FRRouting/frr

commit fc5a738409eac9ca938cbb398872ea77d9cc5023

bgpd: set ipv4-mapped ipv6 for ipv4 with ipv6 nexthop
The code was expected that no IPv6 global address was present but the
previous commit was replacing nexthop.v6global by the link-local address
instead of un-setting it in case of removal of the IPv6 global.

Set also ipv4-mapped ipv6 address as nexthop when a link-local is found
and it is an ipv4 prefix over ipv6 nexthop.

Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_updgrp_packet.c
@@ -523,11 +523,13 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 			gnh_modified = 1;
 		}
 
-		if (IN6_IS_ADDR_UNSPECIFIED(mod_v6nhg)) {
-			if (peer->nexthop.v4.s_addr != INADDR_ANY) {
-				ipv4_to_ipv4_mapped_ipv6(mod_v6nhg,
-							 peer->nexthop.v4);
-			}
+		if (peer->nexthop.v4.s_addr != INADDR_ANY &&
+		    (IN6_IS_ADDR_UNSPECIFIED(mod_v6nhg) ||
+		     (IN6_IS_ADDR_LINKLOCAL(mod_v6nhg) &&
+		      peer->connection->su.sa.sa_family == AF_INET6 &&
+		      paf->afi == AFI_IP))) {
+			ipv4_to_ipv4_mapped_ipv6(mod_v6nhg, peer->nexthop.v4);
+			gnh_modified = 1;
 		}
 
 		if (IS_MAPPED_IPV6(&peer->nexthop.v6_global)) {

commit d5064178cf772c8a282a9be0a4d2aba982bd30b5

bgpd: Do not use mapped IPv4/IPv6 addresses for unnumbered peering
Before:

```
router bgp 4283023031
 bgp router-id 100.83.23.31
 neighbor leafs peer-group
 neighbor leafs remote-as external
 neighbor eth1 interface peer-group leafs
 !
 address-family ipv4 unicast
  network 100.83.23.31/32
 exit-address-family
 !
exit
```

```
leaf2# show ip bgp 100.83.23.31/32
…
  4283023031
    ::ffff:6453:171f (inaccessible) from node31-h23-osl3(swp32) (100.83.23.31)
    (fe80::a6bf:1ff:fe2d:689a) (used)
      Origin IGP, metric 0, invalid, external
      Last update: Tue Aug 13 08:36:46 2024
```

Fixes: fc5a738409eac9ca938cbb398872ea77d9cc5023 ("bgpd: set ipv4-mapped ipv6 for ipv4 with ipv6 nexthop")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_updgrp_packet.c
@@ -534,7 +534,7 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 			gnh_modified = 1;
 		}
 
-		if (peer->nexthop.v4.s_addr != INADDR_ANY &&
+		if (!peer->conf_if && peer->nexthop.v4.s_addr != INADDR_ANY &&
 		    (IN6_IS_ADDR_UNSPECIFIED(mod_v6nhg) ||
 		     (IN6_IS_ADDR_LINKLOCAL(mod_v6nhg) &&
 		      ((peer->connection->su.sa.sa_family == AF_INET6 &&