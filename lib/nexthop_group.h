0bc79f5e51827fad611d714da5e926da2bd123a7
Donald Sharp <sharpd@nvidia.com>
2024-10-01 14:31:08 -0400

    lib: nexthop code should use uint16_t for nexthop counting
    It's possible to specify via the cli and configure how many
    nexthops that are allowed on the system.  If you happen to
    have > 255 then things are about to get interesting otherwise.
    
    Let's allow up to 65k nexthops (ha!)
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- lib/nexthop_group.h
+++ lib/nexthop_group.h
@@ -149,9 +149,8 @@ extern void nexthop_group_json_nexthop(json_object *j,
 				       const struct nexthop *nh);
 
 /* Return the number of nexthops in this nhg */
-extern uint8_t nexthop_group_nexthop_num(const struct nexthop_group *nhg);
-extern uint8_t
-nexthop_group_active_nexthop_num(const struct nexthop_group *nhg);
+extern uint16_t nexthop_group_nexthop_num(const struct nexthop_group *nhg);
+extern uint16_t nexthop_group_active_nexthop_num(const struct nexthop_group *nhg);
 
 extern bool nexthop_group_has_label(const struct nexthop_group *nhg);
 
