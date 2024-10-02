FRRouting/frr

commit 0435b31bb8ed55377f83d0e19bc085abc3c71b44

bgpd: Allow bgp to specify if it will allow v6 routing with v4 nexthops
Add a `--v6-with-v4-nexthop` cli to bgp to allow it to peer with
neighbors in the configuration where the interface has no v6 addresses
at all and there is a v4 address that is usable as a v4 address
embedded in a v6 address.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgp_main.c
@@ -57,15 +57,17 @@
 
 /* bgpd options, we use GNU getopt library. */
 static const struct option longopts[] = {
-	{"bgp_port", required_argument, NULL, 'p'},
-	{"listenon", required_argument, NULL, 'l'},
-	{"no_kernel", no_argument, NULL, 'n'},
-	{"skip_runas", no_argument, NULL, 'S'},
-	{"ecmp", required_argument, NULL, 'e'},
-	{"int_num", required_argument, NULL, 'I'},
-	{"no_zebra", no_argument, NULL, 'Z'},
-	{"socket_size", required_argument, NULL, 's'},
-	{0}};
+	{ "bgp_port", required_argument, NULL, 'p' },
+	{ "listenon", required_argument, NULL, 'l' },
+	{ "no_kernel", no_argument, NULL, 'n' },
+	{ "skip_runas", no_argument, NULL, 'S' },
+	{ "ecmp", required_argument, NULL, 'e' },
+	{ "int_num", required_argument, NULL, 'I' },
+	{ "no_zebra", no_argument, NULL, 'Z' },
+	{ "socket_size", required_argument, NULL, 's' },
+	{ "v6-with-v4-nexthops", no_argument, NULL, 'v' },
+	{ 0 }
+};
 
 /* signal definitions */
 void sighup(void);
@@ -387,16 +389,16 @@ int main(int argc, char **argv)
 	addresses->cmp = (int (*)(void *, void *))strcmp;
 
 	frr_preinit(&bgpd_di, argc, argv);
-	frr_opt_add(
-		"p:l:SnZe:I:s:" DEPRECATED_OPTIONS, longopts,
-		"  -p, --bgp_port     Set BGP listen port number (0 means do not listen).\n"
-		"  -l, --listenon     Listen on specified address (implies -n)\n"
-		"  -n, --no_kernel    Do not install route to kernel.\n"
-		"  -Z, --no_zebra     Do not communicate with Zebra.\n"
-		"  -S, --skip_runas   Skip capabilities checks, and changing user and group IDs.\n"
-		"  -e, --ecmp         Specify ECMP to use.\n"
-		"  -I, --int_num      Set instance number (label-manager)\n"
-		"  -s, --socket_size  Set BGP peer socket send buffer size\n");
+	frr_opt_add("p:l:SnZe:I:s:" DEPRECATED_OPTIONS, longopts,
+		    "  -p, --bgp_port           Set BGP listen port number (0 means do not listen).\n"
+		    "  -l, --listenon           Listen on specified address (implies -n)\n"
+		    "  -n, --no_kernel          Do not install route to kernel.\n"
+		    "  -Z, --no_zebra           Do not communicate with Zebra.\n"
+		    "  -S, --skip_runas         Skip capabilities checks, and changing user and group IDs.\n"
+		    "  -e, --ecmp               Specify ECMP to use.\n"
+		    "  -I, --int_num            Set instance number (label-manager)\n"
+		    "  -s, --socket_size        Set BGP peer socket send buffer size\n"
+		    "    , --v6-with-v4-nexthop Allow BGP to form v6 neighbors using v4 nexthops\n");
 
 	/* Command line argument treatment. */
 	while (1) {
@@ -457,6 +459,9 @@ int main(int argc, char **argv)
 			break;
 		case 's':
 			buffer_size = atoi(optarg);
+			break;
+		case 'v':
+			bm->v6_with_v4_nexthops = true;
 			break;
 		default:
 			frr_help_exit(1);

commit 0495cac837ad0f6ff1082746c37e4a48c1068035

bgpd: Actually make ` --v6-with-v4-nexthops` it work
It was using `-v` which is actually a _version_.

Fixes: 0435b31bb8ed55377f83d0e19bc085abc3c71b44 ("bgpd: Allow bgp to specify if it will allow v6 routing with v4 nexthops")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_main.c
@@ -63,18 +63,16 @@ DEFINE_HOOK(bgp_hook_vrf_update, (struct vrf *vrf, bool enabled),
 	    (vrf, enabled));
 
 /* bgpd options, we use GNU getopt library. */
-static const struct option longopts[] = {
-	{ "bgp_port", required_argument, NULL, 'p' },
-	{ "listenon", required_argument, NULL, 'l' },
-	{ "no_kernel", no_argument, NULL, 'n' },
-	{ "skip_runas", no_argument, NULL, 'S' },
-	{ "ecmp", required_argument, NULL, 'e' },
-	{ "int_num", required_argument, NULL, 'I' },
-	{ "no_zebra", no_argument, NULL, 'Z' },
-	{ "socket_size", required_argument, NULL, 's' },
-	{ "v6-with-v4-nexthops", no_argument, NULL, 'v' },
-	{ 0 }
-};
+static const struct option longopts[] = { { "bgp_port", required_argument, NULL, 'p' },
+					  { "listenon", required_argument, NULL, 'l' },
+					  { "no_kernel", no_argument, NULL, 'n' },
+					  { "skip_runas", no_argument, NULL, 'S' },
+					  { "ecmp", required_argument, NULL, 'e' },
+					  { "int_num", required_argument, NULL, 'I' },
+					  { "no_zebra", no_argument, NULL, 'Z' },
+					  { "socket_size", required_argument, NULL, 's' },
+					  { "v6-with-v4-nexthops", no_argument, NULL, 'x' },
+					  { 0 } };
 
 /* signal definitions */
 void sighup(void);
@@ -424,11 +422,12 @@ int main(int argc, char **argv)
 	int buffer_size = BGP_SOCKET_SNDBUF_SIZE;
 	char *address;
 	struct listnode *node;
+	bool v6_with_v4_nexthops = false;
 
 	addresses->cmp = (int (*)(void *, void *))strcmp;
 
 	frr_preinit(&bgpd_di, argc, argv);
-	frr_opt_add("p:l:SnZe:I:s:" DEPRECATED_OPTIONS, longopts,
+	frr_opt_add("p:l:SnZe:I:s:x" DEPRECATED_OPTIONS, longopts,
 		    "  -p, --bgp_port           Set BGP listen port number (0 means do not listen).\n"
 		    "  -l, --listenon           Listen on specified address (implies -n)\n"
 		    "  -n, --no_kernel          Do not install route to kernel.\n"
@@ -437,7 +436,7 @@ int main(int argc, char **argv)
 		    "  -e, --ecmp               Specify ECMP to use.\n"
 		    "  -I, --int_num            Set instance number (label-manager)\n"
 		    "  -s, --socket_size        Set BGP peer socket send buffer size\n"
-		    "    , --v6-with-v4-nexthop Allow BGP to form v6 neighbors using v4 nexthops\n");
+		    "  -x, --v6-with-v4-nexthop Allow BGP to form v6 neighbors using v4 nexthops\n");
 
 	/* Command line argument treatment. */
 	while (1) {
@@ -499,8 +498,8 @@ int main(int argc, char **argv)
 		case 's':
 			buffer_size = atoi(optarg);
 			break;
-		case 'v':
-			bm->v6_with_v4_nexthops = true;
+		case 'x':
+			v6_with_v4_nexthops = true;
 			break;
 		default:
 			frr_help_exit(1);
@@ -513,6 +512,7 @@ int main(int argc, char **argv)
 	bgp_master_init(frr_init(), buffer_size, addresses);
 	bm->startup_time = monotime(NULL);
 	bm->port = bgp_port;
+	bm->v6_with_v4_nexthops = v6_with_v4_nexthops;
 	if (bgp_port == 0)
 		bgp_option_set(BGP_OPT_NO_LISTEN);
 	if (no_fib_flag || no_zebra_flag)