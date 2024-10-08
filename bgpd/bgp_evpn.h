3ec60f20e2ecd198f62553217d9279ba47f757e2
Chirag Shah <chirag@nvidia.com>
2024-10-07 19:04:43 -0700

    bgpd: fix evpn mh esi flap remove local routes
    In symmetric routing, when local ESI is down,
    the MH peer learnt local mac-ip
    prefix is installed into teannt vrf (given l3vni).
    
    When ESI is back up and associated to evi/vni then
    remove the local synced mac-ip imported routes from the
    tenant vrf as local neigh/arp is present.
    
    Ticket: #3878699
    Testing:
    
    peer advertised mac-ip route:
    *> [2]:[0]:[48]:[aa:aa:aa:00:00:01]:[32]:[45.0.0.51] RD 27.0.0.4:9
                        27.0.0.4 (spine-1)
                                                               0 64435 65016 i
                        ESI:03:44:38:39:ff:ff:01:00:00:01
                        RT:65016:1000 RT:65016:4000 ET:8 Rmac:44:38:39:ff:ff:16
    
    When local ESI is flapped
    torm-11:# ip neigh show 45.0.0.51
    45.0.0.51 dev vlan1000 lladdr aa:aa:aa:00:00:01 REACHABLE proto zebra
    
    Before fix:
    (The imported route remained in tenant-vrf)
    torm-11:# ip route show vrf vrf1 45.0.0.51
    45.0.0.51 nhid 257 proto bgp metric 20
    
    After fix:
    
    torm-11# ip route show vrf vrf1 45.0.0.51
    torm-11#
    
    trace:
    2024/10/11 18:19:29 BGP: [JMP3T-178G8] route [2]:[0]:[48]:[00:02:00:00:00:08]:[32]:[21.1.0.5]
    is matched on local esi 03:00:00:00:77:01:04:00:00:0e, uninstall from VRF tenant1 route table
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>
    (cherry picked from commit 3f00709a39ab2fe6f4bbae3d8f4b17baaab2e3dd)

--- bgpd/bgp_evpn.h
+++ bgpd/bgp_evpn.h
@@ -195,4 +195,8 @@ extern enum zclient_send_status
 evpn_zebra_uninstall(struct bgp *bgp, struct bgpevpn *vpn,
 		     const struct prefix_evpn *p, struct bgp_path_info *pi,
 		     bool is_sync);
+bool bgp_evpn_skip_vrf_import_of_local_es(struct bgp *bgp_vrf, const struct prefix_evpn *evp,
+					  struct bgp_path_info *pi, int install);
+int uninstall_evpn_route_entry_in_vrf(struct bgp *bgp_vrf, const struct prefix_evpn *evp,
+				      struct bgp_path_info *parent_pi);
 #endif /* _QUAGGA_BGP_EVPN_H */
