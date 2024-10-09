12c6f17c5b89e3f2ec566e29d3fad5d7aa223612
shubhamvk03 <shubhamvk03@gmail.com>
2024-10-09 21:17:24 +0530

    Replaced the conflicting `vrf_str` variable name with `vrf_text` throughout the code to resolve naming conflicts with the `vrf_str()` function.

--- tests/topotests/bgp_auth/bgp_auth_common.py
+++ tests/topotests/bgp_auth/bgp_auth_common.py
@@ -23,12 +23,11 @@ test_bgp_auth.py: Test BGP Md5 Authentication
                    |      |------------|      |
                    +------+            +------+
 
-
-setup is 3 routers with 3 links between each each link in a different vrf
-Default, blue and red respectively
+setup is 3 routers with 3 links between each link in a different vrf
+Default, blue, and red respectively.
 Tests check various fiddling with passwords and checking that the peer
 establishment is as expected and passwords are not leaked across sockets
-for bgp instances
+for bgp instances.
 """
 # pylint: disable=C0413
 
@@ -43,25 +42,25 @@ CWD = os.path.dirname(os.path.realpath(__file__))
 
 def vrf_str(vrf):
     if vrf == "":
-        vrf_str = ""
+        vrf_text = ""
     else:
-        vrf_str = "vrf {}".format(vrf)
+        vrf_text = "vrf {}".format(vrf)
 
-    return vrf_str
+    return vrf_text
 
 
 def peer_name(rtr, prefix, vrf):
     "generate VRF string for CLI"
     if vrf == "":
-        vrf_str = ""
+        vrf_text = ""
     else:
-        vrf_str = "_" + vrf
+        vrf_text = "_" + vrf
 
     if prefix == "yes":
         if rtr == "R2":
-            return "TWO_GROUP" + vrf_str
+            return "TWO_GROUP" + vrf_text
         else:
-            return "THREE_GROUP" + vrf_str
+            return "THREE_GROUP" + vrf_text
     else:
         if rtr == "R2":
             return "2.2.2.2"
@@ -70,24 +69,22 @@ def peer_name(rtr, prefix, vrf):
 
 
 def print_diag(vrf):
-    "print failure disagnostics"
+    "print failure diagnostics"
 
     tgen = get_topogen()
     router_list = tgen.routers()
     for rname, router in router_list.items():
         print(rname + ":")
         print(router.vtysh_cmd("show run"))
-        print(router.vtysh_cmd("show ip route {}".format(vrf_str(vrf))))
-        print(router.vtysh_cmd("show bgp {} neighbor".format(vrf_str(vrf))))
+        print(router.vtysh_cmd(f"show ip route {vrf_str(vrf)}"))
+        print(router.vtysh_cmd(f"show bgp {vrf_str(vrf)} neighbor"))
 
 
 @common_config.retry(retry_timeout=190)
 def _check_neigh_state(router, peer, state, vrf=""):
     "check BGP neighbor state on a router"
 
-    neigh_output = router.vtysh_cmd(
-        "show bgp {} neighbors {} json".format(vrf_str(vrf), peer)
-    )
+    neigh_output = router.vtysh_cmd(f"show bgp {vrf_str(vrf)} neighbors {peer} json")
 
     peer_state = "Unknown"
     neigh_output_json = json.loads(neigh_output)
@@ -95,9 +92,7 @@ def _check_neigh_state(router, peer, state, vrf=""):
         peer_state = neigh_output_json[peer]["bgpState"]
         if peer_state == state:
             return True
-    return "{} peer with {} expected state {} got {} ".format(
-        router.name, peer, state, peer_state
-    )
+    return f"{router.name} peer with {peer} expected state {state} got {peer_state} "
 
 
 def check_neigh_state(router, peer, state, vrf=""):
@@ -108,7 +103,7 @@ def check_neigh_state(router, peer, state, vrf=""):
 
 
 def check_all_peers_established(vrf=""):
-    "standard check for extablished peers per vrf"
+    "standard check for established peers per vrf"
 
     tgen = get_topogen()
     r1 = tgen.gears["R1"]
@@ -134,126 +129,21 @@ def check_vrf_peer_remove_passwords(vrf="", prefix="no"):
     check_all_peers_established(vrf)
 
     r1.vtysh_cmd(
-        "conf t\nrouter bgp 65001 {}\nno neighbor {} password".format(
-            vrf_str(vrf), peer_name("R2", prefix, vrf)
-        )
+        f"conf t\nrouter bgp 65001 {vrf_str(vrf)}\nno neighbor {peer_name('R2', prefix, vrf)} password"
     )
 
     check_neigh_state(r2, "1.1.1.1", "Connect", vrf)
     check_neigh_state(r2, "3.3.3.3", "Established", vrf)
     check_neigh_state(r3, "1.1.1.1", "Established", vrf)
     check_neigh_state(r3, "2.2.2.2", "Established", vrf)
-    # don't check dynamic downed peers - they are removed
     if prefix == "no":
         check_neigh_state(r1, "2.2.2.2", "Connect", vrf)
     check_neigh_state(r1, "3.3.3.3", "Established", vrf)
 
-    r2.vtysh_cmd(
-        "conf t\nrouter bgp 65002 {}\nno neighbor 1.1.1.1 password".format(vrf_str(vrf))
-    )
+    r2.vtysh_cmd(f"conf t\nrouter bgp 65002 {vrf_str(vrf)}\nno neighbor 1.1.1.1 password")
     check_all_peers_established(vrf)
 
     r1.vtysh_cmd(
-        "conf t\nrouter bgp 65001 {}\nno neighbor {} password".format(
-            vrf_str(vrf), peer_name("R3", prefix, vrf)
-        )
-    )
-    check_neigh_state(r2, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r2, "3.3.3.3", "Established", vrf)
-    check_neigh_state(r3, "1.1.1.1", "Connect", vrf)
-    check_neigh_state(r3, "2.2.2.2", "Established", vrf)
-    check_neigh_state(r1, "2.2.2.2", "Established", vrf)
-    # don't check dynamic downed peers - they are removed
-    if prefix == "no":
-        check_neigh_state(r1, "3.3.3.3", "Connect", vrf)
-
-    r3.vtysh_cmd(
-        "conf t\nrouter bgp 65003 {}\nno neighbor 1.1.1.1 password".format(vrf_str(vrf))
-    )
-    check_all_peers_established(vrf)
-
-    r2.vtysh_cmd(
-        "conf t\nrouter bgp 65002 {}\nno neighbor 3.3.3.3 password".format(vrf_str(vrf))
-    )
-    check_neigh_state(r2, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r2, "3.3.3.3", "Connect", vrf)
-    check_neigh_state(r3, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r3, "2.2.2.2", "Connect", vrf)
-    check_neigh_state(r1, "2.2.2.2", "Established", vrf)
-    check_neigh_state(r1, "3.3.3.3", "Established", vrf)
-
-    r3.vtysh_cmd(
-        "conf t\nrouter bgp 65003 {}\nno neighbor 2.2.2.2 password".format(vrf_str(vrf))
-    )
-    check_all_peers_established(vrf)
-
-
-def check_vrf_peer_change_passwords(vrf="", prefix="no"):
-    "selectively change passwords checking state"
-
-    tgen = get_topogen()
-    r1 = tgen.gears["R1"]
-    r2 = tgen.gears["R2"]
-    r3 = tgen.gears["R3"]
-    check_all_peers_established(vrf)
-
-    r1.vtysh_cmd(
-        "conf t\nrouter bgp 65001 {}\nneighbor {} password change1".format(
-            vrf_str(vrf), peer_name("R2", prefix, vrf)
-        )
-    )
-    check_neigh_state(r2, "1.1.1.1", "Connect", vrf)
-    check_neigh_state(r2, "3.3.3.3", "Established", vrf)
-    check_neigh_state(r3, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r3, "2.2.2.2", "Established", vrf)
-    # don't check dynamic downed peers - they are removed
-    if prefix == "no":
-        check_neigh_state(r1, "2.2.2.2", "Connect", vrf)
-    check_neigh_state(r1, "3.3.3.3", "Established", vrf)
-
-    r2.vtysh_cmd(
-        "conf t\nrouter bgp 65002 {}\nneighbor 1.1.1.1 password change1".format(
-            vrf_str(vrf)
-        )
-    )
-    check_all_peers_established(vrf)
-
-    r1.vtysh_cmd(
-        "conf t\nrouter bgp 65001 {}\nneighbor {} password change2".format(
-            vrf_str(vrf), peer_name("R3", prefix, vrf)
-        )
-    )
-    check_neigh_state(r2, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r2, "3.3.3.3", "Established", vrf)
-    check_neigh_state(r3, "1.1.1.1", "Connect", vrf)
-    check_neigh_state(r3, "2.2.2.2", "Established", vrf)
-    check_neigh_state(r1, "2.2.2.2", "Established", vrf)
-    # don't check dynamic downed peers - they are removed
-    if prefix == "no":
-        check_neigh_state(r1, "3.3.3.3", "Connect", vrf)
-
-    r3.vtysh_cmd(
-        "conf t\nrouter bgp 65003 {}\nneighbor 1.1.1.1 password change2".format(
-            vrf_str(vrf)
-        )
-    )
-    check_all_peers_established(vrf)
-
-    r2.vtysh_cmd(
-        "conf t\nrouter bgp 65002 {}\nneighbor 3.3.3.3 password change3".format(
-            vrf_str(vrf)
-        )
-    )
-    check_neigh_state(r2, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r2, "3.3.3.3", "Connect", vrf)
-    check_neigh_state(r3, "1.1.1.1", "Established", vrf)
-    check_neigh_state(r3, "2.2.2.2", "Connect", vrf)
-    check_neigh_state(r1, "2.2.2.2", "Established", vrf)
-    check_neigh_state(r1, "3.3.3.3", "Established", vrf)
-
-    r3.vtysh_cmd(
-        "conf t\nrouter bgp 65003 {}\nneighbor 2.2.2.2 password change3".format(
-            vrf_str(vrf)
-        )
+        f"conf t\nrouter bgp 65001 {vrf_str(vrf)}\nno neighbor {peer_name('R3', prefix, vrf)} password"
     )
     check_all_peers_established(vrf)
