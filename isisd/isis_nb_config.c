909e6dd6218eeda2ebb2a09e8be775c995a1bfab
Philippe Guibert <philippe.guibert@6wind.com>
2024-10-25 10:23:54 +0200

    isisd: fix change flex-algorithm number from uint32 to uint8
    The algorithm number is encoded on 8 bits and does not require
    an unsigned 32 bit value to store the value.
    
    Fixes: cc4926c1284e ("isisd,yang: add algorithm-prefix-sid configuration tree")
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    (cherry picked from commit 8e861a75e8b426f0944157387699fe1ab322fe8e)

--- isisd/isis_nb_config.c
+++ isisd/isis_nb_config.c
@@ -2633,14 +2633,14 @@ int isis_instance_segment_routing_algorithm_prefix_sid_create(
 	struct isis_area *area;
 	struct prefix prefix;
 	struct sr_prefix_cfg *pcfg;
-	uint32_t algorithm;
+	uint8_t algorithm;
 
 	if (args->event != NB_EV_APPLY)
 		return NB_OK;
 
 	area = nb_running_get_entry(args->dnode, NULL, true);
 	yang_dnode_get_prefix(&prefix, args->dnode, "prefix");
-	algorithm = yang_dnode_get_uint32(args->dnode, "algo");
+	algorithm = yang_dnode_get_uint8(args->dnode, "algo");
 
 	pcfg = isis_sr_cfg_prefix_add(area, &prefix, algorithm);
 	pcfg->algorithm = algorithm;
