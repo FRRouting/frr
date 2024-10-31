188ba91082956b14633e9857a36f7481b6082e66
Philippe Guibert <philippe.guibert@6wind.com>
2024-10-31 09:31:04 +0100

    bgpd, topotests: bmp, fix wrong peer type for peer up/down events
    When running the bgp_bmp_2 vrf test, peer up/down events from the pre
    and post policy are received with a wrong peer type value
    
    > {"peer_type": "global instance", "policy": "pre-policy", "ipv6": false,
    > "peer_ip": "192.168.0.2", "peer_distinguisher": "0:0", "peer_asn": 65502,
    > "peer_bgp_id": "192.168.0.2", "timestamp": "2024-10-16 21:59:53.111962",
    > "bmp_log_type": "peer up", "local_ip": "192.168.0.1", "local_port": 179,
    > "remote_port": 50710, "seq": 4}
    
    RFC7854 defines RD instance peer type, and later in 4.2 requests that
    the peer distinguisher value be set to non zero value when the peer type
    is not global. This is the case for peer vrf instances.
    
    Fix this by modifying the BMP client, update the peer type
    value by updating the peer type value when sending peer up/down messages.
    
    Add a check in the bgp_bmp_2 test to ensure that peer type is correctly
    set.
    
    > {"peer_type": "route distinguisher instance", "policy": "pre-policy",
    > "ipv6": true, "peer_ip": "192:168::2", "peer_distinguisher": "0:0",
    > "peer_asn": 65502, "peer_bgp_id": "192.168.0.2", "timestamp":
    > "2024-10-16 21:59:53.111962", "bmp_log_type": "peer up", "local_ip":
    > "192:168::1", "local_port": 179, "remote_port": 50836, "seq": 5}
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_bmp.c
+++ bgpd/bgp_bmp.c
@@ -474,12 +474,6 @@ static struct stream *bmp_peerstate(struct peer *peer, bool down)
 	peer_type = bmp_get_peer_type(peer);
 	if (peer_type == BMP_PEER_TYPE_LOC_RIB_INSTANCE)
 		is_locrib = true;
-	else
-		/* TODO: remove this when other RD and local instances supported */
-		peer_type = BMP_PEER_TYPE_GLOBAL_INSTANCE;
-
-	if (is_locrib == false)
-		peer_type = BMP_PEER_TYPE_GLOBAL_INSTANCE;
 
 	if (bmp_get_peer_distinguisher(peer->bgp, AFI_UNSPEC, peer_type, &peer_distinguisher)) {
 		zlog_warn("skipping bmp message for peer %s: can't get peer distinguisher",
