FRRouting/frr

commit 1f5611c06d1c243b42279748788f0627793ead9c

zebra: Allow zebra cli to accept v6 routes with v4 nexthops
add --v6-with-v4-nexthop cli to zebra to allow operator to
specify that this functionality is allowed.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

zebra/main.c
@@ -71,22 +71,25 @@ uint32_t rcvbufsize = 128 * 1024;
 
 #define OPTION_V6_RR_SEMANTICS 2000
 #define OPTION_ASIC_OFFLOAD    2001
+#define OPTION_V6_WITH_V4_NEXTHOP 2002
 
 /* Command line options. */
 const struct option longopts[] = {
-	{"batch", no_argument, NULL, 'b'},
-	{"allow_delete", no_argument, NULL, 'a'},
-	{"socket", required_argument, NULL, 'z'},
-	{"ecmp", required_argument, NULL, 'e'},
-	{"retain", no_argument, NULL, 'r'},
-	{"graceful_restart", required_argument, NULL, 'K'},
-	{"asic-offload", optional_argument, NULL, OPTION_ASIC_OFFLOAD},
+	{ "batch", no_argument, NULL, 'b' },
+	{ "allow_delete", no_argument, NULL, 'a' },
+	{ "socket", required_argument, NULL, 'z' },
+	{ "ecmp", required_argument, NULL, 'e' },
+	{ "retain", no_argument, NULL, 'r' },
+	{ "graceful_restart", required_argument, NULL, 'K' },
+	{ "asic-offload", optional_argument, NULL, OPTION_ASIC_OFFLOAD },
+	{ "v6-with-v4-nexthops", no_argument, NULL, OPTION_V6_WITH_V4_NEXTHOP },
 #ifdef HAVE_NETLINK
-	{"vrfwnetns", no_argument, NULL, 'n'},
-	{"nl-bufsize", required_argument, NULL, 's'},
-	{"v6-rr-semantics", no_argument, NULL, OPTION_V6_RR_SEMANTICS},
+	{ "vrfwnetns", no_argument, NULL, 'n' },
+	{ "nl-bufsize", required_argument, NULL, 's' },
+	{ "v6-rr-semantics", no_argument, NULL, OPTION_V6_RR_SEMANTICS },
 #endif /* HAVE_NETLINK */
-	{0}};
+	{ 0 }
+};
 
 zebra_capabilities_t _caps_p[] = {ZCAP_NET_ADMIN, ZCAP_SYS_ADMIN,
 				  ZCAP_NET_RAW,
@@ -287,6 +290,7 @@ int main(int argc, char **argv)
 	struct sockaddr_storage dummy;
 	socklen_t dummylen;
 	bool asic_offload = false;
+	bool v6_with_v4_nexthop = false;
 	bool notify_on_ack = true;
 
 	graceful_restart = 0;
@@ -294,26 +298,26 @@ int main(int argc, char **argv)
 
 	frr_preinit(&zebra_di, argc, argv);
 
-	frr_opt_add(
-		"baz:e:rK:s:"
+	frr_opt_add("baz:e:rK:s:"
 #ifdef HAVE_NETLINK
-		"n"
+		    "n"
 #endif
-		,
-		longopts,
-		"  -b, --batch              Runs in batch mode\n"
-		"  -a, --allow_delete       Allow other processes to delete zebra routes\n"
-		"  -z, --socket             Set path of zebra socket\n"
-		"  -e, --ecmp               Specify ECMP to use.\n"
-		"  -r, --retain             When program terminates, retain added route by zebra.\n"
-		"  -K, --graceful_restart   Graceful restart at the kernel level, timer in seconds for expiration\n"
-		"  -A, --asic-offload       FRR is interacting with an asic underneath the linux kernel\n"
+		    ,
+		    longopts,
+		    "  -b, --batch               Runs in batch mode\n"
+		    "  -a, --allow_delete        Allow other processes to delete zebra routes\n"
+		    "  -z, --socket              Set path of zebra socket\n"
+		    "  -e, --ecmp                Specify ECMP to use.\n"
+		    "  -r, --retain              When program terminates, retain added route by zebra.\n"
+		    "  -K, --graceful_restart    Graceful restart at the kernel level, timer in seconds for expiration\n"
+		    "  -A, --asic-offload        FRR is interacting with an asic underneath the linux kernel\n"
+		    "      --v6-with-v4-nexthops Underlying dataplane supports v6 routes with v4 nexthops"
 #ifdef HAVE_NETLINK
-		"  -s, --nl-bufsize         Set netlink receive buffer size\n"
-		"  -n, --vrfwnetns          Use NetNS as VRF backend\n"
-		"      --v6-rr-semantics    Use v6 RR semantics\n"
+		    "  -s, --nl-bufsize          Set netlink receive buffer size\n"
+		    "  -n, --vrfwnetns           Use NetNS as VRF backend\n"
+		    "      --v6-rr-semantics     Use v6 RR semantics\n"
 #else
-		"  -s,                      Set kernel socket receive buffer size\n"
+		    "  -s,                       Set kernel socket receive buffer size\n"
 #endif /* HAVE_NETLINK */
 	);
 
@@ -383,6 +387,9 @@ int main(int argc, char **argv)
 				notify_on_ack = true;
 			asic_offload = true;
 			break;
+		case OPTION_V6_WITH_V4_NEXTHOP:
+			v6_with_v4_nexthop = true;
+			break;
 #endif /* HAVE_NETLINK */
 		default:
 			frr_help_exit(1);
@@ -392,7 +399,7 @@ int main(int argc, char **argv)
 	zrouter.master = frr_init();
 
 	/* Zebra related initialize. */
-	zebra_router_init(asic_offload, notify_on_ack);
+	zebra_router_init(asic_offload, notify_on_ack, v6_with_v4_nexthop);
 	zserv_init();
 	rib_init();
 	zebra_if_init();

zebra/zebra_router.c
@@ -255,7 +255,8 @@ bool zebra_router_notify_on_ack(void)
 	return !zrouter.asic_offloaded || zrouter.notify_on_ack;
 }
 
-void zebra_router_init(bool asic_offload, bool notify_on_ack)
+void zebra_router_init(bool asic_offload, bool notify_on_ack,
+		       bool v6_with_v4_nexthop)
 {
 	zrouter.sequence_num = 0;
 
@@ -310,7 +311,7 @@ void zebra_router_init(bool asic_offload, bool notify_on_ack)
 
 	zrouter.asic_offloaded = asic_offload;
 	zrouter.notify_on_ack = notify_on_ack;
-
+	zrouter.v6_with_v4_nexthop = v6_with_v4_nexthop;
 	/*
 	 * If you start using asic_notification_nexthop_control
 	 * come talk to the FRR community about what you are doing

zebra/zebra_router.h
@@ -207,6 +207,7 @@ struct zebra_router {
 	 */
 	bool asic_offloaded;
 	bool notify_on_ack;
+	bool v6_with_v4_nexthop;
 
 	/*
 	 * If the asic is notifying us about successful nexthop
@@ -237,7 +238,8 @@ struct zebra_router {
 extern struct zebra_router zrouter;
 extern uint32_t rcvbufsize;
 
-extern void zebra_router_init(bool asic_offload, bool notify_on_ack);
+extern void zebra_router_init(bool asic_offload, bool notify_on_ack,
+			      bool v6_with_v4_nexthop);
 extern void zebra_router_cleanup(void);
 extern void zebra_router_terminate(void);
 

zebra/zebra_vty.c
@@ -4033,6 +4033,9 @@ DEFUN (show_zebra,
 	ttable_add_row(table, "VRF|Not Available");
 #endif
 
+	ttable_add_row(table, "v6 with v4 nexthop|%s",
+		       zrouter.v6_with_v4_nexthop ? "Used" : "Unavaliable");
+
 	ttable_add_row(table, "ASIC offload|%s",
 		       zrouter.asic_offloaded ? "Used" : "Unavailable");
 

commit 25ae643996d338b8230fb15a9064843fe85de224

zebra: Add missing new line for help string
```
  -A, --asic-offload        FRR is interacting with an asic underneath the linux kernel
      --v6-with-v4-nexthops Underlying dataplane supports v6 routes with v4 nexthops  -s, --nl-bufsize          Set netlink receive buffer size
```

Fixes: 1f5611c06d1c243b42279748788f0627793ead9c ("zebra: Allow zebra cli to accept v6 routes with v4 nexthops")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

zebra/main.c
@@ -341,7 +341,7 @@ int main(int argc, char **argv)
 		    "  -e, --ecmp                Specify ECMP to use.\n"
 		    "  -r, --retain              When program terminates, retain added route by zebra.\n"
 		    "  -A, --asic-offload        FRR is interacting with an asic underneath the linux kernel\n"
-		    "      --v6-with-v4-nexthops Underlying dataplane supports v6 routes with v4 nexthops"
+		    "      --v6-with-v4-nexthops Underlying dataplane supports v6 routes with v4 nexthops\n"
 #ifdef HAVE_NETLINK
 		    "  -s, --nl-bufsize          Set netlink receive buffer size\n"
 		    "  -n, --vrfwnetns           Use NetNS as VRF backend\n"