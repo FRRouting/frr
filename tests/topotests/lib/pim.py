13c0722b5ccc53b00d07388f7a2bddd44f05b17b
Nathan Bahr <nbahr@atcorp.com>
2024-11-01 19:15:52 +0000

    tests: PIM AutoRP tests expanded
    Now with a full AutoRP implementation, we can test AutoRP in a full network setup
    beginning with candidate RP announcements all the way through discovery and active RP
    selection.
    
    Signed-off-by: Nathan Bahr <nbahr@atcorp.com>

--- tests/topotests/lib/pim.py
+++ tests/topotests/lib/pim.py
@@ -1720,26 +1720,26 @@ def verify_pim_rp_info(
                         )
                         return errormsg
 
-            if not iamRP:
-                if rp_json["iAmRP"] == False:
-                    logger.info(
-                        "[DUT %s]: Verifying group "
-                        "and iAmNotRP [PASSED]!!"
-                        " Found Expected: (%s, %s:%s)",
-                        dut,
-                        grp_addr,
-                        "iAmRP",
-                        rp_json["iAmRP"],
-                    )
-                else:
-                    errormsg = (
-                        "[DUT %s]: Verifying group"
-                        "%s and iAmRP [FAILED]!! "
-                        "Expected: (iAmRP: %s),"
-                        " Found: (iAmRP: %s)"
-                        % (dut, grp_addr, "false", rp_json["iAmRP"])
-                    )
-                    return errormsg
+                if not iamRP:
+                    if rp_json["iAmRP"] == False:
+                        logger.info(
+                            "[DUT %s]: Verifying group "
+                            "and iAmNotRP [PASSED]!!"
+                            " Found Expected: (%s, %s:%s)",
+                            dut,
+                            grp_addr,
+                            "iAmRP",
+                            rp_json["iAmRP"],
+                        )
+                    else:
+                        errormsg = (
+                            "[DUT %s]: Verifying group"
+                            "%s and iAmRP [FAILED]!! "
+                            "Expected: (iAmRP: %s),"
+                            " Found: (iAmRP: %s)"
+                            % (dut, grp_addr, "false", rp_json["iAmRP"])
+                        )
+                        return errormsg
 
     logger.debug("Exiting lib API: {}".format(sys._getframe().f_code.co_name))
     return True
