afd9d3f924133ec4ae54014a1f44467946a71466
Carmine Scarpitta <cscarpit@cisco.com>
2024-11-03 08:45:03 +0100

    zebra: Fix wrong debug macro in `release_srv6_sid_func_dynamic`
    `ZEBRA_DEBUG_SRV6` is not the correct macro to evaluate if SRv6 debug is enabled or not.
    
    The correct macro is `IS_ZEBRA_DEBUG_SRV6`.
    
    Fix this by replacing `ZEBRA_DEBUG_SRV6` with `IS_ZEBRA_DEBUG_SRV6`.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- zebra/zebra_srv6.c
+++ zebra/zebra_srv6.c
@@ -2107,7 +2107,7 @@ static int release_srv6_sid_func_dynamic(struct zebra_srv6_sid_block *block,
 		}
 	}
 
-	if (ZEBRA_DEBUG_SRV6)
+	if (IS_ZEBRA_DEBUG_SRV6)
 		zlog_debug("%s: released dynamic SRv6 SID function %u from block %pFX",
 			   __func__, sid_func, &block->prefix);
 
