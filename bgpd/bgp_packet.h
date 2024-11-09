y-bharath14/frr_y_bharath

doc, tests, bgpd: address PR#14847 review
fixed nits & styling issues
fixed bad conditions in bmp_bpi_(un)lock
added bgp_peer_get_send_holdtime
added bgp_peer_get_local_as

Signed-off-by: Maxence Younsi <mx.yns@outlook.fr>

bgpd/bgp_packet.h
@@ -43,9 +43,10 @@ DECLARE_HOOK(bgp_packet_send,
 
 /* Packet send and receive function prototypes. */
 extern void bgp_keepalive_send(struct peer *peer);
-extern struct stream *bgp_open_make(struct peer *peer,
-					   uint16_t send_holdtime,
-					   as_t local_as);
+
+extern uint16_t bgp_peer_get_local_as(struct peer *peer);
+extern uint16_t bgp_peer_get_send_holdtime(struct peer *peer);
+extern struct stream *bgp_open_make(struct peer *peer);
 extern void bgp_open_send(struct peer_connection *connection);
 extern void bgp_notify_send(struct peer_connection *connection, uint8_t code,
 			    uint8_t sub_code);

FRRouting/frr

bgpd/bgp_packet.h
@@ -43,7 +43,9 @@ DECLARE_HOOK(bgp_packet_send,
 
 /* Packet send and receive function prototypes. */
 extern void bgp_keepalive_send(struct peer *peer);
-extern struct stream *bgp_open_make(struct peer *peer, uint16_t send_holdtime, as_t local_as);
+extern as_t bgp_peer_get_local_as(struct peer *peer);
+extern uint16_t bgp_peer_get_send_holdtime(struct peer *peer);
+extern struct stream *bgp_open_make(struct peer *peer);
 extern void bgp_open_send(struct peer_connection *connection);
 extern void bgp_notify_send(struct peer_connection *connection, uint8_t code,
 			    uint8_t sub_code);