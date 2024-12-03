f023329bf81135490385f4349741c490c1d91801
Philippe Guibert <philippe.guibert@6wind.com>
2024-12-03 09:05:45 +0100

    bgpd: bmp, handle imported bgp instances in bmp end of rib
    Modify the bmp_eor() function to send end of rib messages for each peer
    of the current BGP instance.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_trace.h
+++ bgpd/bgp_trace.h
@@ -135,12 +135,13 @@ TRACEPOINT_LOGLEVEL(frr_bgp, bmp_mirror_packet, TRACE_INFO)
 TRACEPOINT_EVENT(
 	frr_bgp,
 	bmp_eor,
-	TP_ARGS(afi_t, afi, safi_t, safi, uint8_t, flags, uint8_t, peer_type_flag),
+	TP_ARGS(afi_t, afi, safi_t, safi, uint8_t, flags, uint8_t, peer_type_flag, bgp),
 	TP_FIELDS(
 		ctf_integer(afi_t, afi, afi)
 		ctf_integer(safi_t, safi, safi)
 		ctf_integer(uint8_t, flags, flags)
 		ctf_integer(uint8_t, peer_type_flag, peer_type_flag)
+		ctf_string(bgp, bgp->name_pretty)
 	)
 )
 
