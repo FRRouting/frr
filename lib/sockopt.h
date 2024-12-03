d5615fd6f84e643f194be8c3e91343b35585eb9c
Donald Sharp <sharpd@nvidia.com>
2024-12-03 18:38:59 -0500

    lib: Allow setsockopt functions to return size set
    When finding a send/receive buffer size that is usable
    let's report how big we were able to set it.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- lib/sockopt.h
+++ lib/sockopt.h
@@ -12,8 +12,8 @@
 extern "C" {
 #endif
 
-extern void setsockopt_so_recvbuf(int sock, int size);
-extern void setsockopt_so_sendbuf(const int sock, int size);
+extern int setsockopt_so_recvbuf(int sock, int size);
+extern int setsockopt_so_sendbuf(const int sock, int size);
 extern int getsockopt_so_sendbuf(const int sock);
 extern int getsockopt_so_recvbuf(const int sock);
 
