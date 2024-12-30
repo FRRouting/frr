bb8d36f61e30359c372f28a841882a4572e62102
Louis Scalbert <louis.scalbert@6wind.com>
2024-12-30 15:03:30 +0100

    bgpd: filter non matching rt in show advertised-routes
    Filter out non matching route-target based on route-target constraint in
    show bgp <afi> <safi> neighbor advertised-routes.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -18495,6 +18495,8 @@ static void show_adj_route(struct vty *vty, struct peer *peer, struct bgp_table 
 				(*output_count)++;
 			}
 		} else if (type == bgp_show_adj_route_advertised) {
+			struct ecommunity *ecom;
+
 			RB_FOREACH (adj, bgp_adj_out_rb, &dest->adj_out)
 				SUBGRP_FOREACH_PEER (adj->subgroup, paf) {
 					if (paf->peer != peer || !adj->attr)
@@ -18513,6 +18515,14 @@ static void show_adj_route(struct vty *vty, struct peer *peer, struct bgp_table 
 								  rmap_name);
 
 					if (ret == RMAP_DENY) {
+						(*filtered_count)++;
+						bgp_attr_flush(&attr);
+						continue;
+					}
+
+					ecom = bgp_attr_get_ecommunity(&attr);
+					if (ecom &&
+					    bgp_rtc_filter(peer, ecom, NULL) == RTC_PREFIX_DENY) {
 						(*filtered_count)++;
 						bgp_attr_flush(&attr);
 						continue;
