b7fd45c5ca2e2e7b61a7ffd9b35c64e194cad267
Philippe Guibert <philippe.guibert@6wind.com>
2025-01-06 17:03:26 +0100

    topotests: bgp_vpnv4_noretain, check presence of locpref in adj-rib-out
    Add a test that check that the detailed command of show bgp advertised
    neighbors 10.125.0.2 displays the locpref value.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- tests/topotests/bgp_vpnv4_noretain/test_bgp_vpnv4_noretain.py
+++ tests/topotests/bgp_vpnv4_noretain/test_bgp_vpnv4_noretain.py
@@ -218,6 +218,29 @@ def check_show_bgp_ipv4_vpn(rname, json_file):
     assert result is None, assertmsg
 
 
+def check_show_bgp_ipv4_vpn_peer_advertised_routes(rname, peer, json_file):
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+    router = tgen.gears[rname]
+
+    logger.info(
+        "Checking VPNv4 advertised routes for on {} for peer {}".format(rname, peer)
+    )
+
+    json_file = "{}/{}/{}".format(CWD, router.name, json_file)
+    expected = json.loads(open(json_file).read())
+    test_func = partial(
+        topotest.router_json_cmp,
+        router,
+        "show bgp ipv4 vpn neighbors {} advertised-routes detail json".format(peer),
+        expected,
+    )
+    _, result = topotest.run_and_expect(test_func, None, count=10, wait=0.5)
+    assertmsg = '"{}" JSON output mismatches'.format(router.name)
+    assert result is None, assertmsg
+
+
 def check_show_bgp_vrf_ipv4(rname, json_file):
     tgen = get_topogen()
     if tgen.routers_have_failure():
@@ -561,6 +584,21 @@ router bgp 65500
     check_show_bgp_ipv4_vpn("r2", "ipv4_vpn_routes_all.json")
 
     check_show_bgp_vrf_ipv4(rname, "ipv4_vrf_all_routes_init.json")
+
+
+def test_bgp_advertised_routes_step13():
+    """
+    Dump advertised routes from r1 to r2
+    Check that the localpref attribute is set on the show command
+    """
+
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    check_show_bgp_ipv4_vpn_peer_advertised_routes(
+        "r1", "10.125.0.2", "ipv4_vpn_routes_advertised_10_125_0_2.json"
+    )
 
 
 def test_memory_leak():
