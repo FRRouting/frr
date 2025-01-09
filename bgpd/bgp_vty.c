FRRouting/frr

commit ef4a9215b912c885498715614ee01b43dc861c1a

bgpd: Reuse defined constants for BGP timers
Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -95,15 +95,15 @@ FRR_CFG_DEFAULT_BOOL(BGP_DETERMINISTIC_MED,
 );
 FRR_CFG_DEFAULT_ULONG(BGP_CONNECT_RETRY,
 	{ .val_ulong = 10, .match_profile = "datacenter", },
-	{ .val_ulong = 120 },
+	{ .val_ulong = BGP_DEFAULT_CONNECT_RETRY },
 );
 FRR_CFG_DEFAULT_ULONG(BGP_HOLDTIME,
 	{ .val_ulong = 9, .match_profile = "datacenter", },
-	{ .val_ulong = 180 },
+	{ .val_ulong = BGP_DEFAULT_KEEPALIVE },
 );
 FRR_CFG_DEFAULT_ULONG(BGP_KEEPALIVE,
 	{ .val_ulong = 3, .match_profile = "datacenter", },
-	{ .val_ulong = 60 },
+	{ .val_ulong = BGP_DEFAULT_KEEPALIVE },
 );
 FRR_CFG_DEFAULT_BOOL(BGP_EBGP_REQUIRES_POLICY,
 	{ .val_bool = false, .match_profile = "datacenter", },

commit 76fc75de9eac4c5c70d1928e4d89ea5e5db52335

bgpd: Fix showing default `timers bgp x y`
Fixes: ef4a9215b912c885498715614ee01b43dc861c1a ("bgpd: Reuse defined constants for BGP timers")
Fixes: ab3535fbcf37b59ec02332fa021142c5b7d6dd3e ("bgpd: Implement connect retry backoff")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -93,18 +93,6 @@ FRR_CFG_DEFAULT_BOOL(BGP_DETERMINISTIC_MED,
 	{ .val_bool = true, .match_profile = "datacenter", },
 	{ .val_bool = false },
 );
-FRR_CFG_DEFAULT_ULONG(BGP_CONNECT_RETRY,
-	{ .val_ulong = 10, .match_profile = "datacenter", },
-	{ .val_ulong = BGP_DEFAULT_CONNECT_RETRY },
-);
-FRR_CFG_DEFAULT_ULONG(BGP_HOLDTIME,
-	{ .val_ulong = 9, .match_profile = "datacenter", },
-	{ .val_ulong = BGP_DEFAULT_KEEPALIVE },
-);
-FRR_CFG_DEFAULT_ULONG(BGP_KEEPALIVE,
-	{ .val_ulong = 3, .match_profile = "datacenter", },
-	{ .val_ulong = BGP_DEFAULT_KEEPALIVE },
-);
 FRR_CFG_DEFAULT_BOOL(BGP_EBGP_REQUIRES_POLICY,
 	{ .val_bool = false, .match_profile = "datacenter", },
 	{ .val_bool = false, .match_version = "< 7.4", },

bgpd/bgp_vty.h
@@ -10,6 +10,19 @@
 #include "stream.h"
 struct bgp;
 
+FRR_CFG_DEFAULT_ULONG(BGP_KEEPALIVE,
+	{ .val_ulong = 3, .match_profile = "datacenter", },
+	{ .val_ulong = BGP_DEFAULT_KEEPALIVE },
+);
+FRR_CFG_DEFAULT_ULONG(BGP_HOLDTIME,
+	{ .val_ulong = 9, .match_profile = "datacenter", },
+	{ .val_ulong = BGP_DEFAULT_HOLDTIME },
+);
+FRR_CFG_DEFAULT_ULONG(BGP_CONNECT_RETRY,
+	{ .val_ulong = 10, .match_profile = "datacenter", },
+	{ .val_ulong = BGP_DEFAULT_CONNECT_RETRY },
+);
+
 #define BGP_INSTANCE_HELP_STR "BGP view\nBGP VRF\nView/VRF name\n"
 #define BGP_INSTANCE_ALL_HELP_STR "BGP view\nBGP VRF\nAll Views/VRFs\n"
 

bgpd/bgpd.c
@@ -583,9 +583,9 @@ void bgp_timers_set(struct vty *vty, struct bgp *bgp, uint32_t keepalive,
 /* mostly for completeness - CLI uses its own defaults */
 void bgp_timers_unset(struct bgp *bgp)
 {
-	bgp->default_keepalive = BGP_DEFAULT_KEEPALIVE;
-	bgp->default_holdtime = BGP_DEFAULT_HOLDTIME;
-	bgp->default_connect_retry = BGP_DEFAULT_CONNECT_RETRY;
+	bgp->default_keepalive = DFLT_BGP_KEEPALIVE;
+	bgp->default_holdtime = DFLT_BGP_HOLDTIME;
+	bgp->default_connect_retry = DFLT_BGP_CONNECT_RETRY;
 	bgp->default_delayopen = BGP_DEFAULT_DELAYOPEN;
 }
 