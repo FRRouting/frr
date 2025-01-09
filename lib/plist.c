FRRouting/frr

commit 856ca177c4bb8f857fd60a1d93ac99fbd1a76484

Added json formating support to show-...-neighbors-... bgp commands.
Ticket: CM-6789
Reviewed By: CCR-3263
Testing Done: Manual Testing and smoke tests

Whenever some sort of output is encountered, added a json version with
proper logic as well.

lib/plist.c
@@ -20,6 +20,7 @@
  */
 
 #include <zebra.h>
+#include "lib/json.h"
 
 #include "prefix.h"
 #include "command.h"
@@ -2559,10 +2560,13 @@ prefix_bgp_orf_remove_all (char *name)
 
 /* return prefix count */
 int
-prefix_bgp_show_prefix_list (struct vty *vty, afi_t afi, char *name)
+prefix_bgp_show_prefix_list (struct vty *vty, afi_t afi, char *name, u_char use_json)
 {
   struct prefix_list *plist;
   struct prefix_list_entry *pentry;
+  json_object *json = NULL;
+  json_object *json_prefix = NULL;
+  json_object *json_list = NULL;
 
   plist = prefix_list_lookup (AFI_ORF_PREFIX, name);
   if (! plist)
@@ -2571,26 +2575,65 @@ prefix_bgp_show_prefix_list (struct vty *vty, afi_t afi, char *name)
   if (! vty)
     return plist->count;
 
-  vty_out (vty, "ip%s prefix-list %s: %d entries%s",
-	   afi == AFI_IP ? "" : "v6",
-	   plist->name, plist->count, VTY_NEWLINE);
-
-  for (pentry = plist->head; pentry; pentry = pentry->next)
+  if(use_json)
     {
-      struct prefix *p = &pentry->prefix;
-      char buf[BUFSIZ];
+      json = json_object_new_object();
+      json_prefix = json_object_new_object();
+      json_list = json_object_new_object();
 
-      vty_out (vty, "   seq %d %s %s/%d", pentry->seq,
-	       prefix_list_type_str (pentry),
-	       inet_ntop (p->family, &p->u.prefix, buf, BUFSIZ),
-	       p->prefixlen);
+      json_object_int_add(json_prefix, "prefixListCounter", plist->count);
+      json_object_string_add(json_prefix, "prefixListName", plist->name);
 
-      if (pentry->ge)
-	vty_out (vty, " ge %d", pentry->ge);
-      if (pentry->le)
-	vty_out (vty, " le %d", pentry->le);
+      for (pentry = plist->head; pentry; pentry = pentry->next)
+        {
+          struct prefix *p = &pentry->prefix;
+          char buf_a[BUFSIZ];
+          char buf_b[BUFSIZ];
 
-      vty_out (vty, "%s", VTY_NEWLINE);
+          sprintf(buf_a, "%s/%d", inet_ntop (p->family, &p->u.prefix, buf_b, BUFSIZ),
+                            p->prefixlen);
+
+          json_object_int_add(json_list, "seq", pentry->seq);
+          json_object_string_add(json_list, "seqPrefixListType", prefix_list_type_str (pentry));
+
+          if (pentry->ge)
+            json_object_int_add(json_list, "ge", pentry->ge);
+          if (pentry->le)
+            json_object_int_add(json_list, "le", pentry->le);
+
+          json_object_object_add(json_prefix, buf_a, json_list);
+        }
+      if (afi == AFI_IP)
+        json_object_object_add(json, "ipPrefixList", json_prefix);
+      else
+        json_object_object_add(json, "ipv6PrefixList", json_prefix);
+
+      vty_out (vty, "%s%s", json_object_to_json_string(json), VTY_NEWLINE);
+      json_object_free(json);
+    }
+  else
+    {
+      vty_out (vty, "ip%s prefix-list %s: %d entries%s",
+               afi == AFI_IP ? "" : "v6",
+               plist->name, plist->count, VTY_NEWLINE);
+
+      for (pentry = plist->head; pentry; pentry = pentry->next)
+        {
+          struct prefix *p = &pentry->prefix;
+          char buf[BUFSIZ];
+
+          vty_out (vty, "   seq %d %s %s/%d", pentry->seq,
+                   prefix_list_type_str (pentry),
+                   inet_ntop (p->family, &p->u.prefix, buf, BUFSIZ),
+                   p->prefixlen);
+
+          if (pentry->ge)
+            vty_out (vty, " ge %d", pentry->ge);
+          if (pentry->le)
+            vty_out (vty, " le %d", pentry->le);
+
+          vty_out (vty, "%s", VTY_NEWLINE);
+        }
     }
   return plist->count;
 }

bgpd/bgp_bfd.c
@@ -33,6 +33,7 @@
 #include "zclient.h"
 #include "vty.h"
 #include "bfd.h"
+#include "lib/json.h"
 #include "bgpd/bgpd.h"
 #include "bgp_fsm.h"
 #include "bgpd/bgp_bfd.h"
@@ -383,22 +384,40 @@ bgp_bfd_peer_config_write(struct vty *vty, struct peer *peer, char *addr)
  * bgp_bfd_show_info - Show the peer BFD information.
  */
 void
-bgp_bfd_show_info(struct vty *vty, struct peer *peer)
+bgp_bfd_show_info(struct vty *vty, struct peer *peer, u_char use_json, json_object *json_neigh)
 {
   struct bfd_info *bfd_info;
+  json_object *json_bfd = NULL;
 
   if (!peer->bfd_info)
     return;
 
+  if (use_json)
+    json_bfd = json_object_new_object();
+
   bfd_info = (struct bfd_info *)peer->bfd_info;
 
-  vty_out (vty, "  BFD: Multi-hop: %s%s",
-       (bgp_bfd_is_peer_multihop(peer)) ? "yes" : "no", VTY_NEWLINE);
-  vty_out (vty, "    Detect Mul: %d, Min Rx interval: %d,"
-                " Min Tx interval: %d%s",
-                    bfd_info->detect_mult, bfd_info->required_min_rx,
-                    bfd_info->desired_min_tx, VTY_NEWLINE);
-  vty_out (vty, "%s", VTY_NEWLINE);
+  if (use_json)
+    {
+      if (bgp_bfd_is_peer_multihop(peer))
+        json_object_string_add(json_bfd, "bfdMultiHop", "yes");
+      else
+        json_object_string_add(json_bfd, "bfdMultiHop", "no");
+      json_object_int_add(json_bfd, "detectMultiplier", bfd_info->detect_mult);
+      json_object_int_add(json_bfd, "rxMinInterval", bfd_info->required_min_rx);
+      json_object_int_add(json_bfd, "txMinInterval", bfd_info->desired_min_tx);
+      json_object_object_add(json_neigh, "peerBfdInfo", json_bfd);
+    }
+  else
+    {
+      vty_out (vty, "  BFD: Multi-hop: %s%s",
+               (bgp_bfd_is_peer_multihop(peer)) ? "yes" : "no", VTY_NEWLINE);
+      vty_out (vty, "    Detect Mul: %d, Min Rx interval: %d,"
+               " Min Tx interval: %d%s",
+               bfd_info->detect_mult, bfd_info->required_min_rx,
+               bfd_info->desired_min_tx, VTY_NEWLINE);
+      vty_out (vty, "%s", VTY_NEWLINE);
+    }
 }
 
 DEFUN (neighbor_bfd,

bgpd/bgp_bfd.h
@@ -40,6 +40,6 @@ extern void
 bgp_bfd_peer_config_write(struct vty *vty, struct peer *peer, char *addr);
 
 extern void
-bgp_bfd_show_info(struct vty *vty, struct peer *peer);
+bgp_bfd_show_info(struct vty *vty, struct peer *peer, u_char use_json, json_object *json_neigh);
 
 #endif /* _QUAGGA_BGP_BFD_H */

bgpd/bgp_damp.c
@@ -543,10 +543,11 @@ bgp_config_write_damp (struct vty *vty)
 }
 
 static const char *
-bgp_get_reuse_time (unsigned int penalty, char *buf, size_t len)
+bgp_get_reuse_time (unsigned int penalty, char *buf, size_t len, u_char use_json, json_object *json)
 {
   time_t reuse_time = 0;
   struct tm *tm = NULL;
+  int time_store = 0;
 
   if (penalty > damp->reuse_limit)
     {
@@ -564,16 +565,45 @@ bgp_get_reuse_time (unsigned int penalty, char *buf, size_t len)
 #define ONE_DAY_SECOND 60*60*24
 #define ONE_WEEK_SECOND 60*60*24*7
   if (reuse_time == 0)
-    snprintf (buf, len, "00:00:00");
+    {
+      if (use_json)
+        json_object_int_add(json, "reuseTimerMsecs", 0);
+      else
+        snprintf (buf, len, "00:00:00");
+    }
   else if (reuse_time < ONE_DAY_SECOND)
-    snprintf (buf, len, "%02d:%02d:%02d", 
-              tm->tm_hour, tm->tm_min, tm->tm_sec);
+    {
+      if (use_json)
+        {
+          time_store = (3600000 * tm->tm_hour) + (60000 * tm->tm_min) + (1000 * tm->tm_sec);
+          json_object_int_add(json, "reuseTimerMsecs", time_store);
+        }
+      else
+        snprintf (buf, len, "%02d:%02d:%02d",
+                tm->tm_hour, tm->tm_min, tm->tm_sec);
+    }
   else if (reuse_time < ONE_WEEK_SECOND)
-    snprintf (buf, len, "%dd%02dh%02dm", 
-              tm->tm_yday, tm->tm_hour, tm->tm_min);
+    {
+      if (use_json)
+        {
+          time_store = (86400000 * tm->tm_yday) + (3600000 * tm->tm_hour) + (60000 * tm->tm_min) + (1000 * tm->tm_sec);
+          json_object_int_add(json, "reuseTimerMsecs", time_store);
+        }
+      else
+        snprintf (buf, len, "%dd%02dh%02dm",
+                tm->tm_yday, tm->tm_hour, tm->tm_min);
+    }
   else
-    snprintf (buf, len, "%02dw%dd%02dh", 
-              tm->tm_yday/7, tm->tm_yday - ((tm->tm_yday/7) * 7), tm->tm_hour); 
+    {
+      if (use_json)
+        {
+          time_store = (604800000 * tm->tm_yday/7) + (86400000 * (tm->tm_yday - ((tm->tm_yday/7) * 7))) + (3600000 * tm->tm_hour) + (60000 * tm->tm_min) + (1000 * tm->tm_sec);
+          json_object_int_add(json, "reuseTimerMsecs", time_store);
+        }
+      else
+        snprintf (buf, len, "%02dw%dd%02dh",
+                tm->tm_yday/7, tm->tm_yday - ((tm->tm_yday/7) * 7), tm->tm_hour);
+    }
 
   return buf;
 }
@@ -607,26 +637,22 @@ bgp_damp_info_vty (struct vty *vty, struct bgp_info *binfo,
     {
       json_object_int_add(json_path, "dampeningPenalty", penalty);
       json_object_int_add(json_path, "dampeningFlapCount", bdi->flap);
-      json_object_string_add(json_path, "dampeningFlapPeriod",
-                              peer_uptime (bdi->start_time, timebuf, BGP_UPTIME_LEN));
+      peer_uptime (bdi->start_time, timebuf, BGP_UPTIME_LEN, 1, json_path);
 
       if (CHECK_FLAG (binfo->flags, BGP_INFO_DAMPED)
           && ! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
-        {
-          json_object_string_add(json_path, "dampeningReuseIn",
-                                 bgp_get_reuse_time (penalty, timebuf, BGP_UPTIME_LEN));
-        }
+        bgp_get_reuse_time (penalty, timebuf, BGP_UPTIME_LEN, 1, json_path);
     }
   else
     {
       vty_out (vty, "      Dampinfo: penalty %d, flapped %d times in %s",
                penalty, bdi->flap,
-	       peer_uptime (bdi->start_time, timebuf, BGP_UPTIME_LEN));
+	       peer_uptime (bdi->start_time, timebuf, BGP_UPTIME_LEN, 0, json_path));
 
       if (CHECK_FLAG (binfo->flags, BGP_INFO_DAMPED)
           && ! CHECK_FLAG (binfo->flags, BGP_INFO_HISTORY))
         vty_out (vty, ", reuse in %s",
-	       bgp_get_reuse_time (penalty, timebuf, BGP_UPTIME_LEN));
+	       bgp_get_reuse_time (penalty, timebuf, BGP_UPTIME_LEN, 0, json_path));
 
       vty_out (vty, "%s", VTY_NEWLINE);
     }
@@ -634,7 +660,7 @@ bgp_damp_info_vty (struct vty *vty, struct bgp_info *binfo,
 
 const char *
 bgp_damp_reuse_time_vty (struct vty *vty, struct bgp_info *binfo,
-                         char *timebuf, size_t len)
+                         char *timebuf, size_t len, u_char use_json, json_object *json)
 {
   struct bgp_damp_info *bdi;
   time_t t_now, t_diff;
@@ -656,5 +682,5 @@ bgp_damp_reuse_time_vty (struct vty *vty, struct bgp_info *binfo,
   t_diff = t_now - bdi->t_updated;
   penalty = bgp_damp_decay (t_diff, bdi->penalty);
 
-  return  bgp_get_reuse_time (penalty, timebuf, len);
+  return  bgp_get_reuse_time (penalty, timebuf, len, use_json, json);
 }

bgpd/bgp_damp.h
@@ -21,8 +21,6 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 #ifndef _QUAGGA_BGP_DAMP_H
 #define _QUAGGA_BGP_DAMP_H
 
-#include "lib/json.h"
-
 /* Structure maintained on a per-route basis. */
 struct bgp_damp_info
 {
@@ -144,6 +142,6 @@ extern int bgp_damp_decay (time_t, int);
 extern void bgp_config_write_damp (struct vty *);
 extern void bgp_damp_info_vty (struct vty *, struct bgp_info *, json_object *json_path);
 extern const char * bgp_damp_reuse_time_vty (struct vty *, struct bgp_info *,
-                                             char *, size_t);
+                                             char *, size_t, u_char, json_object *);
 
 #endif /* _QUAGGA_BGP_DAMP_H */

bgpd/bgp_fsm.c
@@ -33,6 +33,7 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 #include "workqueue.h"
 #include "queue.h"
 
+#include "lib/json.h"
 #include "bgpd/bgpd.h"
 #include "bgpd/bgp_attr.h"
 #include "bgpd/bgp_debug.h"

bgpd/bgp_open.c
@@ -29,6 +29,7 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 #include "memory.h"
 #include "queue.h"
 
+#include "lib/json.h"
 #include "bgpd/bgpd.h"
 #include "bgpd/bgp_attr.h"
 #include "bgpd/bgp_debug.h"
@@ -49,12 +50,16 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
    inforation at each peer. */
 
 void
-bgp_capability_vty_out (struct vty *vty, struct peer *peer)
+bgp_capability_vty_out (struct vty *vty, struct peer *peer, u_char use_json, json_object *json_neigh)
 {
   char *pnt;
   char *end;
   struct capability_mp_data mpc;
   struct capability_header *hdr;
+  json_object *json_cap = NULL;
+
+  if (use_json)
+    json_cap = json_object_new_object();
 
   pnt = peer->notify.data;
   end = pnt + peer->notify.length;
@@ -72,46 +77,89 @@ bgp_capability_vty_out (struct vty *vty, struct peer *peer)
 
       if (hdr->code == CAPABILITY_CODE_MP)
 	{
-	  vty_out (vty, "  Capability error for: Multi protocol ");
-
-	  switch (ntohs (mpc.afi))
-	    {
-	    case AFI_IP:
-	      vty_out (vty, "AFI IPv4, ");
-	      break;
-	    case AFI_IP6:
-	      vty_out (vty, "AFI IPv6, ");
-	      break;
-	    default:
-	      vty_out (vty, "AFI Unknown %d, ", ntohs (mpc.afi));
-	      break;
-	    }
-	  switch (mpc.safi)
-	    {
-	    case SAFI_UNICAST:
-	      vty_out (vty, "SAFI Unicast");
-	      break;
-	    case SAFI_MULTICAST:
-	      vty_out (vty, "SAFI Multicast");
-	      break;
-	    case SAFI_MPLS_LABELED_VPN:
-	      vty_out (vty, "SAFI MPLS-labeled VPN");
-	      break;
-	    default:
-	      vty_out (vty, "SAFI Unknown %d ", mpc.safi);
-	      break;
-	    }
-	  vty_out (vty, "%s", VTY_NEWLINE);
-	}
+          if (use_json)
+            {
+              switch (ntohs (mpc.afi))
+                {
+                  case AFI_IP:
+                    json_object_string_add(json_cap, "capabilityErrorMultiProtocolAfi", "IPv4");
+                  break;
+                  case AFI_IP6:
+                    json_object_string_add(json_cap, "capabilityErrorMultiProtocolAfi", "IPv6");
+                  break;
+                  default:
+                    json_object_int_add(json_cap, "capabilityErrorMultiProtocolAfiUnknown", ntohs (mpc.afi));
+                  break;
+                }
+              switch (mpc.safi)
+                {
+                  case SAFI_UNICAST:
+                    json_object_string_add(json_cap, "capabilityErrorMultiProtocolSafi", "unicast");
+                  break;
+                  case SAFI_MULTICAST:
+                    json_object_string_add(json_cap, "capabilityErrorMultiProtocolSafi", "multicast");
+                  break;
+                  case SAFI_MPLS_LABELED_VPN:
+                    json_object_string_add(json_cap, "capabilityErrorMultiProtocolSafi", "MPLS-labeled VPN");
+                  break;
+                  default:
+                    json_object_int_add(json_cap, "capabilityErrorMultiProtocolSafiUnknown", mpc.safi);
+                  break;
+                }
+            }
+          else
+            {
+              vty_out (vty, "  Capability error for: Multi protocol ");
+              switch (ntohs (mpc.afi))
+                {
+                  case AFI_IP:
+                    vty_out (vty, "AFI IPv4, ");
+                  break;
+                  case AFI_IP6:
+                    vty_out (vty, "AFI IPv6, ");
+                  break;
+                  default:
+                    vty_out (vty, "AFI Unknown %d, ", ntohs (mpc.afi));
+                  break;
+                }
+              switch (mpc.safi)
+                {
+                  case SAFI_UNICAST:
+                    vty_out (vty, "SAFI Unicast");
+                  break;
+                  case SAFI_MULTICAST:
+                    vty_out (vty, "SAFI Multicast");
+                  break;
+                 case SAFI_MPLS_LABELED_VPN:
+                    vty_out (vty, "SAFI MPLS-labeled VPN");
+                  break;
+                  default:
+                    vty_out (vty, "SAFI Unknown %d ", mpc.safi);
+                  break;
+                }
+              vty_out (vty, "%s", VTY_NEWLINE);
+            }
+        }
       else if (hdr->code >= 128)
-	vty_out (vty, "  Capability error: vendor specific capability code %d",
-		 hdr->code);
+        {
+          if (use_json)
+            json_object_int_add(json_cap, "capabilityErrorVendorSpecificCapabilityCode", hdr->code);
+          else
+            vty_out (vty, "  Capability error: vendor specific capability code %d",
+                     hdr->code);
+        }
       else
-	vty_out (vty, "  Capability error: unknown capability code %d", 
-		 hdr->code);
-
+        {
+          if (use_json)
+            json_object_int_add(json_cap, "capabilityErrorUnknownCapabilityCode", hdr->code);
+          else
+            vty_out (vty, "  Capability error: unknown capability code %d",
+                     hdr->code);
+        }
       pnt += hdr->length + 2;
     }
+  if (use_json)
+    json_object_object_add(json_neigh, "capabilityErrors", json_cap);
 }
 
 static void 

commit 8ccf60921b85893d301186a0f8156fb702da379f

bgpd: fix crash in displaying json orf prefix-list
bgpd crashes when there is several entries in the prefix-list. No
backtrace is provided because the issue was catched from a code review.

Fixes: 856ca177c4 ("Added json formating support to show-...-neighbors-... bgp commands.")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

lib/plist.c
@@ -1536,7 +1536,6 @@ int prefix_bgp_show_prefix_list(struct vty *vty, afi_t afi, char *name,
 	if (use_json) {
 		json = json_object_new_object();
 		json_prefix = json_object_new_object();
-		json_list = json_object_new_object();
 
 		json_object_int_add(json_prefix, "prefixListCounter",
 				    plist->count);
@@ -1544,6 +1543,8 @@ int prefix_bgp_show_prefix_list(struct vty *vty, afi_t afi, char *name,
 				       plist->name);
 
 		for (pentry = plist->head; pentry; pentry = pentry->next) {
+			json_list = json_object_new_object();
+
 			json_object_int_add(json_list, "seq", pentry->seq);
 			json_object_string_add(json_list, "seqPrefixListType",
 					       prefix_list_type_str(pentry));