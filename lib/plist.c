9d3b32857b671a73ddd3a2afa1ee4db2b4d810c0
Louis Scalbert <louis.scalbert@6wind.com>
2025-01-09 18:28:53 +0100

    bgpd: fix crash in displaying json orf prefix-list
    bgpd crashes when there is several entries in the prefix-list. No
    backtrace is provided because the issue was catched from a code review.
    
    Fixes: 856ca177c4 ("Added json formating support to show-...-neighbors-... bgp commands.")
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>
    (cherry picked from commit 8ccf60921b85893d301186a0f8156fb702da379f)

--- lib/plist.c
+++ lib/plist.c
@@ -1504,7 +1504,6 @@ int prefix_bgp_show_prefix_list(struct vty *vty, afi_t afi, char *name,
 	if (use_json) {
 		json = json_object_new_object();
 		json_prefix = json_object_new_object();
-		json_list = json_object_new_object();
 
 		json_object_int_add(json_prefix, "prefixListCounter",
 				    plist->count);
@@ -1512,6 +1511,8 @@ int prefix_bgp_show_prefix_list(struct vty *vty, afi_t afi, char *name,
 				       plist->name);
 
 		for (pentry = plist->head; pentry; pentry = pentry->next) {
+			json_list = json_object_new_object();
+
 			json_object_int_add(json_list, "seq", pentry->seq);
 			json_object_string_add(json_list, "seqPrefixListType",
 					       prefix_list_type_str(pentry));
