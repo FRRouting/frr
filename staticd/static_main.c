73fdae9bea086006f164c689ebd0f0f623808f28
Yuqing Zhao <galadriel.zyq@alibaba-inc.com>
2025-01-13 11:04:38 +0100

    staticd: Initialize/cleanup SRv6
    Signed-off-by: Yuqing Zhao <galadriel.zyq@alibaba-inc.com>

--- staticd/static_main.c
+++ staticd/static_main.c
@@ -26,6 +26,7 @@
 #include "static_zebra.h"
 #include "static_debug.h"
 #include "static_nb.h"
+#include "static_srv6.h"
 
 #include "mgmt_be_client.h"
 
@@ -76,6 +77,10 @@ static void sigint(void)
 	static_vrf_terminate();
 
 	static_zebra_stop();
+
+	/* clean up SRv6 data structures */
+	static_srv6_cleanup();
+
 	frr_fini();
 
 	exit(0);
@@ -160,6 +165,9 @@ int main(int argc, char **argv, char **envp)
 
 	static_debug_init();
 	static_vrf_init();
+
+	/* initialize SRv6 data structures */
+	static_srv6_init();
 
 	static_zebra_init();
 	static_vty_init();
