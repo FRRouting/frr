c58cbb2c44c7ae78f6246a07fb3c269008fa7770
wumu.zsl <wumu.zsl@alibaba-inc.com>
2025-01-21 06:32:47 +0000

    bfdd: add sbfd state machine functions
    Signed-off-by: wumu.zsl <wumu.zsl@alibaba-inc.com>

--- bfdd/event.c
+++ bfdd/event.c
@@ -58,6 +58,73 @@ void bfd_echo_recvtimer_update(struct bfd_session *bs)
 			   &bs->echo_recvtimer_ev);
 }
 
+void sbfd_init_recvtimer_update(struct bfd_session *bs)
+{
+	struct timeval tv = { .tv_sec = 0, .tv_usec = bs->detect_TO };
+
+	/* Remove previous schedule if any. */
+	sbfd_init_recvtimer_delete(bs);
+
+	/* Don't add event if peer is deactivated. */
+	if (CHECK_FLAG(bs->flags, BFD_SESS_FLAG_SHUTDOWN) || bs->sock == -1)
+		return;
+
+	tv_normalize(&tv);
+#ifdef BFD_EVENT_DEBUG
+	log_debug("%s: sec = %ld, usec = %ld", __func__, tv.tv_sec, tv.tv_usec);
+#endif /* BFD_EVENT_DEBUG */
+
+	event_add_timer_tv(master, sbfd_init_recvtimer_cb, bs, &tv, &bs->recvtimer_ev);
+}
+
+void sbfd_echo_recvtimer_update(struct bfd_session *bs)
+{
+	struct timeval tv = { .tv_sec = 0, .tv_usec = bs->echo_detect_TO };
+
+	/* Remove previous schedule if any. */
+	sbfd_echo_recvtimer_delete(bs);
+
+	/* Don't add event if peer is deactivated. */
+	if (CHECK_FLAG(bs->flags, BFD_SESS_FLAG_SHUTDOWN) || bs->sock == -1)
+		return;
+
+	tv_normalize(&tv);
+
+	event_add_timer_tv(master, sbfd_echo_recvtimer_cb, bs, &tv, &bs->echo_recvtimer_ev);
+}
+
+void sbfd_init_xmttimer_update(struct bfd_session *bs, uint64_t jitter)
+{
+	struct timeval tv = { .tv_sec = 0, .tv_usec = jitter };
+
+	/* Remove previous schedule if any. */
+	sbfd_init_xmttimer_delete(bs);
+
+	/* Don't add event if peer is deactivated. */
+	if (CHECK_FLAG(bs->flags, BFD_SESS_FLAG_SHUTDOWN) || bs->sock == -1)
+		return;
+
+	tv_normalize(&tv);
+
+	event_add_timer_tv(master, sbfd_init_xmt_cb, bs, &tv, &bs->xmttimer_ev);
+}
+
+void sbfd_echo_xmttimer_update(struct bfd_session *bs, uint64_t jitter)
+{
+	struct timeval tv = { .tv_sec = 0, .tv_usec = jitter };
+
+	/* Remove previous schedule if any. */
+	sbfd_echo_xmttimer_delete(bs);
+
+	/* Don't add event if peer is deactivated. */
+	if (CHECK_FLAG(bs->flags, BFD_SESS_FLAG_SHUTDOWN) || bs->sock == -1)
+		return;
+
+	tv_normalize(&tv);
+
+	event_add_timer_tv(master, sbfd_echo_xmt_cb, bs, &tv, &bs->echo_xmttimer_ev);
+}
+
 void bfd_xmttimer_update(struct bfd_session *bs, uint64_t jitter)
 {
 	struct timeval tv = {.tv_sec = 0, .tv_usec = jitter};
@@ -109,6 +176,26 @@ void bfd_xmttimer_delete(struct bfd_session *bs)
 }
 
 void bfd_echo_xmttimer_delete(struct bfd_session *bs)
+{
+	EVENT_OFF(bs->echo_xmttimer_ev);
+}
+
+void sbfd_init_recvtimer_delete(struct bfd_session *bs)
+{
+	EVENT_OFF(bs->recvtimer_ev);
+}
+
+void sbfd_echo_recvtimer_delete(struct bfd_session *bs)
+{
+	EVENT_OFF(bs->echo_recvtimer_ev);
+}
+
+void sbfd_init_xmttimer_delete(struct bfd_session *bs)
+{
+	EVENT_OFF(bs->xmttimer_ev);
+}
+
+void sbfd_echo_xmttimer_delete(struct bfd_session *bs)
 {
 	EVENT_OFF(bs->echo_xmttimer_ev);
 }
