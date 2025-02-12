457e721938f0d04b1f6a1ed644ad853f492c1f0a
Louis Scalbert <louis.scalbert@6wind.com>
2025-02-12 13:09:37 +0100

    bgpd: fix process_queue when un-hiding
    bgp_process_queue_init() is not called in bgp_create() when leaving the
    BGP instance hidden state because of the following goto:
    
    >	if (hidden) {
    >		bgp = bgp_old;
    >		goto peer_init;
    >	}
    
    Upon reconfiguration of the default instance, the prefixes are never set
    into a meta queue by mq_add_handler(). They are never processed for
    zebra RIB installation and announcements of update/withdraw.
    
    Do not delete the BGP process_queue when hiding.
    
    Fixes: 4d0e7a49cf ("bgpd: VRF-Lite fix default bgp delete")
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -4265,12 +4265,11 @@ int bgp_delete(struct bgp *bgp)
 			bgp_set_evpn(bgp_get_default());
 	}
 
-	if (bgp->process_queue)
-		work_queue_free_and_null(&bgp->process_queue);
-
-	if (!IS_BGP_INSTANCE_HIDDEN(bgp))
+	if (!IS_BGP_INSTANCE_HIDDEN(bgp)) {
+		if (bgp->process_queue)
+			work_queue_free_and_null(&bgp->process_queue);
 		bgp_unlock(bgp); /* initial reference */
-	else {
+	} else {
 		for (afi = AFI_IP; afi < AFI_MAX; afi++) {
 			enum vpn_policy_direction dir;
 
