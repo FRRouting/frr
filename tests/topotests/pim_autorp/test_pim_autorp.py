12e211463ef1129e06c6e809615023dad70e4367
Nathan Bahr <nbahr@atcorp.com>
2025-02-21 17:59:04 +0000

    pim: Fix autorp group joins
    Group joining got broken when moving the autorp socket to open/close
    as needed. This fixes it so autorp group joining is properly handled
    as part of opening the socket.
    
    Signed-off-by: Nathan Bahr <nbahr@atcorp.com>
    (cherry picked from commit d840560b74e3a6117aa1e4b1203dcdd8fb254ef6)
    
    # Conflicts:
    #	pimd/pim_autorp.c
    #	tests/topotests/pim_autorp/test_pim_autorp.py

--- tests/topotests/pim_autorp/test_pim_autorp.py
+++ tests/topotests/pim_autorp/test_pim_autorp.py
@@ -133,8 +133,304 @@ def test_pim_autorp_discovery_single_rp(request):
     assert result is True, "Testcase {} :Failed \n Error: {}".format(tc_name, result)
 
 
+<<<<<<< HEAD
 def test_pim_autorp_discovery_multiple_rp(request):
     "Test PIM AutoRP Discovery with multiple RP's"
+=======
+def test_pim_autorp_disable_enable(request):
+    "Test PIM AutoRP disable and re-enable works properly"
+    tgen = get_topogen()
+    tc_name = request.node.name
+    write_test_header(tc_name)
+
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    step("Ensure AutoRP groups are joined on all routers")
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        expected = {
+            f"{rtr}-eth0": {
+                "name": f"{rtr}-eth0",
+                "224.0.1.39": "*",
+                "224.0.1.40": "*",
+            },
+            f"{rtr}-eth1": {
+                "name": f"{rtr}-eth1",
+                "224.0.1.39": "*",
+                "224.0.1.40": "*",
+            },
+        }
+
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip igmp sources json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct autorp groups joined".format(
+            rtr
+        )
+
+    step("Disable AutoRP on all routers")
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        tgen.routers()[rtr].vtysh_cmd(
+            """
+            conf
+            router pim
+              no autorp discovery
+            """
+        )
+
+    step("Ensure AutoRP groups are no longer joined on all routers")
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        expected = {f"{rtr}-eth0": None, f"{rtr}-eth1": None}
+
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip igmp sources json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct autorp groups joined".format(
+            rtr
+        )
+
+    step("Re-enable AutoRP on all routers")
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        tgen.routers()[rtr].vtysh_cmd(
+            """
+            conf
+            router pim
+              autorp discovery
+            """
+        )
+
+    step("Ensure AutoRP groups are re-joined on all routers")
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        expected = {
+            f"{rtr}-eth0": {
+                "name": f"{rtr}-eth0",
+                "224.0.1.39": "*",
+                "224.0.1.40": "*",
+            },
+            f"{rtr}-eth1": {
+                "name": f"{rtr}-eth1",
+                "224.0.1.39": "*",
+                "224.0.1.40": "*",
+            },
+        }
+
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip igmp sources json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct autorp groups joined".format(
+            rtr
+        )
+
+
+def test_pim_autorp_no_mapping_agent_rp(request):
+    "Test PIM AutoRP candidate with no mapping agent"
+    tgen = get_topogen()
+    tc_name = request.node.name
+    write_test_header(tc_name)
+
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    # Start a candidate RP on r2
+    tgen.routers()["r2"].vtysh_cmd(
+        """
+        conf
+         router pim
+          autorp announce 10.0.0.2 224.0.0.0/4
+          autorp announce scope 31 interval 1 holdtime 5
+        """
+    )
+
+    # Without a mapping agent, we should still have no RP
+    step("Verify no RP without mapping agent")
+    expected = json.loads(
+        """
+        {
+          "10.0.3.4":[
+            {
+              "rpAddress":"10.0.3.4",
+              "group":"224.0.1.0/24",
+              "source":"Static"
+            }
+          ]
+        }"""
+    )
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip pim rp-info json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct rp-info".format(rtr)
+
+    step("Verify candidate RP in AutoRP on R2")
+    expected = json.loads(
+        """
+        {
+          "discovery":{
+            "enabled": true
+          },
+          "announce": {
+              "enabled":true,
+              "scope":31,
+              "interval":1,
+              "holdtime":5,
+              "rpList":[
+                {
+                  "rpAddress":"10.0.0.2",
+                  "groupRange":"224.0.0.0/4",
+                  "prefixList":"-"
+                }
+              ]
+          },
+          "mapping-agent": {
+            "enabled":false
+          }
+        }"""
+    )
+    test_func = partial(
+        topotest.router_json_cmp, tgen.gears["r2"], "show ip pim autorp json", expected
+    )
+    _, result = topotest.run_and_expect(test_func, None)
+    assert result is None, "{} does not have correct autorp configuration".format("r2")
+
+
+def test_pim_autorp_discovery_rp(request):
+    "Test PIM AutoRP candidate advertised by mapping agent"
+    tgen = get_topogen()
+    tc_name = request.node.name
+    write_test_header(tc_name)
+
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    # Start the mapping agent on R1
+    tgen.routers()["r1"].vtysh_cmd(
+        """
+        conf
+         router pim
+          autorp send-rp-discovery source interface r1-eth0
+          autorp send-rp-discovery scope 31 interval 1 holdtime 5
+        """
+    )
+
+    step("Verify rp-info of the only candidate RP")
+    expected = json.loads(
+        """
+        {
+          "10.0.3.4":[
+            {
+              "rpAddress":"10.0.3.4",
+              "group":"224.0.1.0/24",
+              "source":"Static"
+            }
+          ],
+          "10.0.0.2":[
+            {
+              "rpAddress":"10.0.0.2",
+              "group":"224.0.0.0/4",
+              "source":"AutoRP"
+            }
+          ]
+        }"""
+    )
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip pim rp-info json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct rp-info".format(rtr)
+
+    step("Verify mapping-agent in AutoRP on R1")
+    expected = json.loads(
+        """
+        {
+          "announce": {
+            "enabled":false
+          },
+          "mapping-agent": {
+            "enabled":true,
+            "active":true,
+            "scope":31,
+            "interval":1,
+            "holdtime":5,
+            "source":"interface",
+            "interface":"r1-eth0",
+            "address":"10.0.0.1",
+            "rpList":{
+              "10.0.0.2":{
+                "rpAddress":"10.0.0.2",
+                "groupRanges":[
+                  {
+                    "negative":false,
+                    "prefix":"224.0.0.0/4"
+                  }
+                ]
+              }
+            }
+          }
+        }"""
+    )
+    test_func = partial(
+        topotest.router_json_cmp, tgen.gears["r1"], "show ip pim autorp json", expected
+    )
+    _, result = topotest.run_and_expect(test_func, None)
+    assert result is None, "{} does not have correct autorp configuration".format("r1")
+
+    step("Verify AutoRP discovery RP's")
+    expected = json.loads(
+        """
+        {
+          "discovery":{
+            "enabled": true,
+            "rpList":{
+              "10.0.0.2":{
+                "rpAddress":"10.0.0.2",
+                "holdtime":5,
+                "groupRanges":[
+                  {
+                    "negative":false,
+                    "prefix":"224.0.0.0/4"
+                  }
+                ]
+              }
+            }
+          }
+        }"""
+    )
+    for rtr in ["r1", "r2", "r3", "r4"]:
+        test_func = partial(
+            topotest.router_json_cmp,
+            tgen.gears[rtr],
+            "show ip pim autorp json",
+            expected,
+        )
+        _, result = topotest.run_and_expect(test_func, None)
+        assert result is None, "{} does not have correct autorp configuration".format(
+            rtr
+        )
+
+
+def test_pim_autorp_discovery_multiple_rp_same(request):
+    "Test PIM AutoRP Discovery with multiple RP's for same group prefix"
+>>>>>>> d840560b7 (pim: Fix autorp group joins)
     tgen = get_topogen()
     tc_name = request.node.name
     write_test_header(tc_name)
