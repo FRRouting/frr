3b0dfb12eb6010765ce0db046cd05a215aa70685
Carmine Scarpitta <cscarpit@cisco.com>
2025-02-26 16:08:33 +0100

    tests: Add test case to verify that SIDs can be re-added
    Add a test case to verify that staticd is able to re-install all SIDs
    after disabling and re-enabling SRv6.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- tests/topotests/static_srv6_sids/test_static_srv6_sids.py
+++ tests/topotests/static_srv6_sids/test_static_srv6_sids.py
@@ -287,6 +287,53 @@ def test_srv6_static_sids_srv6_disable():
     check_srv6_static_sids(router, "expected_srv6_sids_srv6_disable.json")
 
 
+def test_srv6_static_sids_srv6_reenable():
+    """
+    Re-enable SRv6
+    """
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+    router = tgen.gears["r1"]
+
+    def _check_srv6_static_sids(router, expected_route_file):
+        logger.info("checking zebra srv6 static sids")
+        output = json.loads(router.vtysh_cmd("show ipv6 route static json"))
+        expected = open_json_file("{}/{}".format(CWD, expected_route_file))
+        return topotest.json_cmp(output, expected)
+
+    def check_srv6_static_sids(router, expected_file):
+        func = functools.partial(_check_srv6_static_sids, router, expected_file)
+        _, result = topotest.run_and_expect(func, None, count=15, wait=1)
+        assert result is None, "Failed"
+
+    router.vtysh_cmd(
+        """
+        configure terminal
+         segment-routing
+          srv6
+           locators
+            locator MAIN
+             prefix fcbb:bbbb:1::/48 block-len 32 node-len 16 func-bits 16
+            !
+           !
+           static-sids
+            sid fcbb:bbbb:1::/48 locator MAIN behavior uN
+            sid fcbb:bbbb:1:fe10::/64 locator MAIN behavior uDT4 vrf Vrf10
+            sid fcbb:bbbb:1:fe20::/64 locator MAIN behavior uDT6 vrf Vrf20
+            sid fcbb:bbbb:1:fe30::/64 locator MAIN behavior uDT46 vrf Vrf30
+            sid fcbb:bbbb:1:fe40::/64 locator MAIN behavior uA interface sr0 nexthop 2001::2
+        """
+    )
+
+    # FOR DEVELOPER:
+    # If you want to stop some specific line and start interactive shell,
+    # please use tgen.mininet_cli() to start it.
+
+    logger.info("Test for srv6 sids configuration")
+    check_srv6_static_sids(router, "expected_srv6_sids.json")
+
+
 if __name__ == "__main__":
     args = ["-s"] + sys.argv[1:]
     sys.exit(pytest.main(args))
