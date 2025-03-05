d8fb17f5a85f9fee71ee3afa64b0684e328612d4
Carmine Scarpitta <cscarpit@cisco.com>
2025-03-05 13:17:08 +0100

    staticd: Fix crash when allocating SIDs without parent locator
    In same cases, it may happen staticd attempts to allocate a SID when it
    does not know about the parent locator yet.
    In this case, the locator pointer stored inside the SID data structure
    is NULL. When `static_zebra_request_srv6_sid` attempts to dereference
    the locator pointer, we get a NULL pointer dereference crash.
    
    This commit adds a check to prevent the crash from happening.
    
    If the locator pointer is NULL, it means that staticd is not aware
    about the parent locator yet. Then, staticd should call
    `static_zebra_srv6_manager_get_locator()` to get the locator from SRv6
    Manager. Once staticd receives the requested locator, the existing
    callback `static_zebra_process_srv6_locator_internal()` will take care
    of retrying to allocate the SID.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- staticd/static_zebra.c
+++ staticd/static_zebra.c
@@ -992,6 +992,11 @@ extern void static_zebra_request_srv6_sid(struct static_srv6_sid *sid)
 	if (!sid || !static_zebra_sid_locator_block_check(sid))
 		return;
 
+	if (!sid->locator) {
+		static_zebra_srv6_manager_get_locator(sid->locator_name);
+		return;
+	}
+
 	/* convert `srv6_endpoint_behavior_codepoint` to `seg6local_action_t` */
 	switch (sid->behavior) {
 	case SRV6_ENDPOINT_BEHAVIOR_END:
