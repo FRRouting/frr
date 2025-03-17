fc63fbf722dbb77f75342d06bf68776525c6008f
Tuetuopay <tuetuopay@me.com>
2025-03-17 15:08:15 +0100

    bgpd: fix evpn attributes being dropped on input
    All assignments of the EVPN attributes (ESI and Gateway IP) are gated
    behind the peer being set up for inbound soft-reconfiguration.
    
    There are no actual reasons for this limitation, so let's perform the
    EVPN attribute assignment no matter what when soft reconfiguration is
    not enabled.
    
    Fixes: 6e076ba5231 ("bgpd: Fix for ain->attr corruption during path update")
    Signed-off-by: Tuetuopay <tuetuopay@me.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -5186,7 +5186,8 @@ void bgp_update(struct peer *peer, const struct prefix *p, uint32_t addpath_id,
 	 * attr->evpn_overlay with evpn directly. Instead memcpy
 	 * evpn to new_atr.evpn_overlay before it is interned.
 	 */
-	if (soft_reconfig && evpn && afi == AFI_L2VPN) {
+	if (evpn && afi == AFI_L2VPN &&
+	    (soft_reconfig || !CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_SOFT_RECONFIG))) {
 		bgp_attr_set_evpn_overlay(&new_attr, evpn);
 		p_evpn = NULL;
 	}
