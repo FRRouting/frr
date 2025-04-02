FRRouting/frr

commit 8384d41144496019725c1e250abd0ceea854341f

lib: Return duplicate prefix-list entry test
If we do e.g.:

ip prefix-list PL_LoopbackV4 permit 10.1.0.32/32
ip prefix-list PL_LoopbackV4 permit 10.1.0.32/32
ip prefix-list PL_LoopbackV4 permit 10.1.0.32/32

We end up, having duplicate records with a different sequence number only.

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

lib/filter_cli.c
@@ -1206,10 +1206,14 @@ DEFPY_YANG(
 	snprintf(xpath, sizeof(xpath),
 		 "/frr-filter:lib/prefix-list[type='ipv4'][name='%s']", name);
 	if (seq_str == NULL) {
-		/* Use XPath to find the next sequence number. */
-		sseq = acl_get_seq(vty, xpath, false);
-		if (sseq < 0)
-			return CMD_WARNING_CONFIG_FAILED;
+		if (plist_is_dup(vty->candidate_config->dnode, &pda))
+			sseq = pda.pda_seq;
+		else {
+			/* Use XPath to find the next sequence number. */
+			sseq = acl_get_seq(vty, xpath, false);
+			if (sseq < 0)
+				return CMD_WARNING_CONFIG_FAILED;
+		}
 
 		snprintfrr(xpath_entry, sizeof(xpath_entry),
 			   "%s/entry[sequence='%" PRId64 "']", xpath, sseq);

commit 24ae7cd30a055dc17fc9d75762320e1359e005b2

lib: Return duplicate ipv6 prefix-list entry test
Fixes: 8384d41144496019725c1e250abd0ceea854341f ("lib: Return duplicate prefix-list entry test")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

lib/filter_cli.c
@@ -1400,10 +1400,14 @@ DEFPY_YANG(
 	snprintf(xpath, sizeof(xpath),
 		 "/frr-filter:lib/prefix-list[type='ipv6'][name='%s']", name);
 	if (seq_str == NULL) {
-		/* Use XPath to find the next sequence number. */
-		sseq = acl_get_seq(vty, xpath, false);
-		if (sseq < 0)
-			return CMD_WARNING_CONFIG_FAILED;
+		if (plist_is_dup(vty->candidate_config->dnode, &pda))
+			sseq = pda.pda_seq;
+		else {
+			/* Use XPath to find the next sequence number. */
+			sseq = acl_get_seq(vty, xpath, false);
+			if (sseq < 0)
+				return CMD_WARNING_CONFIG_FAILED;
+		}
 
 		snprintfrr(xpath_entry, sizeof(xpath_entry),
 			   "%s/entry[sequence='%" PRId64 "']", xpath, sseq);