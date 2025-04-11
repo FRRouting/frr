FRRouting/frr

commit f80ec7e3d630cbe3adfbb393c2fc7b11ee73d5fd

zebra: handle iptable list of interfaces
Upon reception of an iptable_add or iptable_del, a list of interface
indexes may be passed in the zapi interface. The list is converted in
interface name so that it is ready to be passed to be programmed to the
underlying system.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/zebra_pbr.c
@@ -23,10 +23,16 @@
 
 #include <jhash.h>
 #include <hash.h>
+#include <memory.h>
 
 #include "zebra/zebra_pbr.h"
 #include "zebra/rt.h"
 #include "zebra/zapi_msg.h"
+#include "zebra/zebra_memory.h"
+#include "zebra_pbr.h"
+
+/* definitions */
+DEFINE_MTYPE_STATIC(ZEBRA, PBR_IPTABLE_IFNAME, "PBR interface list")
 
 /* definitions */
 static const struct message ipset_type_msg[] = {
@@ -246,9 +252,17 @@ int zebra_pbr_ipset_entry_hash_equal(const void *arg1, const void *arg2)
 void zebra_pbr_iptable_free(void *arg)
 {
 	struct zebra_pbr_iptable *iptable;
+	struct listnode *node, *nnode;
+	char *name;
 
 	iptable = (struct zebra_pbr_iptable *)arg;
 
+	for (ALL_LIST_ELEMENTS(iptable->interface_name_list,
+					node, nnode, name)) {
+		XFREE(MTYPE_PBR_IPTABLE_IFNAME, name);
+		list_delete_node(iptable->interface_name_list,
+				 node);
+	}
 	XFREE(MTYPE_TMP, iptable);
 }
 
@@ -548,16 +562,26 @@ void zebra_pbr_add_iptable(struct zebra_ns *zns,
 void zebra_pbr_del_iptable(struct zebra_ns *zns,
 			   struct zebra_pbr_iptable *iptable)
 {
-	struct zebra_pbr_ipset_entry *lookup;
+	struct zebra_pbr_iptable *lookup;
 
 	lookup = hash_lookup(zns->iptable_hash, iptable);
 	/* TODO:
 	 * - call netlink layer
 	 * - detach from iptable list
 	 */
-	if (lookup)
+	if (lookup) {
+		struct listnode *node, *nnode;
+		char *name;
+
+		hash_release(zns->iptable_hash, lookup);
+		for (ALL_LIST_ELEMENTS(iptable->interface_name_list,
+				       node, nnode, name)) {
+			XFREE(MTYPE_PBR_IPTABLE_IFNAME, name);
+			list_delete_node(iptable->interface_name_list,
+					 node);
+		}
 		XFREE(MTYPE_TMP, lookup);
-	else
+	} else
 		zlog_warn("%s: IPTable being deleted we know nothing about",
 			  __PRETTY_FUNCTION__);
 }
@@ -881,4 +905,23 @@ void zebra_pbr_show_iptable(struct vty *vty)
 
 	hash_walk(zns->iptable_hash, zebra_pbr_show_iptable_walkcb,
 		  &env);
+}
+
+void zebra_pbr_iptable_update_interfacelist(struct stream *s,
+					    struct zebra_pbr_iptable *zpi)
+{
+	uint32_t i = 0, index;
+	struct interface *ifp;
+	char *name;
+
+	for (i = 0; i < zpi->nb_interface; i++) {
+		STREAM_GETL(s, index);
+		ifp = if_lookup_by_index(index, zpi->vrf_id);
+		if (!ifp)
+			continue;
+		name = XSTRDUP(MTYPE_PBR_IPTABLE_IFNAME, ifp->name);
+		listnode_add(zpi->interface_name_list, name);
+	}
+stream_failure:
+	return;
 }

zebra/zapi_msg.c
@@ -2929,6 +2929,7 @@ static inline void zread_iptable(ZAPI_HANDLER_ARGS)
 
 	memset(&zpi, 0, sizeof(zpi));
 
+	zpi.interface_name_list = list_new();
 	zpi.sock = client->sock;
 	zpi.vrf_id = zvrf->vrf->vrf_id;
 	STREAM_GETL(s, zpi.unique);
@@ -2937,6 +2938,8 @@ static inline void zread_iptable(ZAPI_HANDLER_ARGS)
 	STREAM_GETL(s, zpi.action);
 	STREAM_GETL(s, zpi.fwmark);
 	STREAM_GET(&zpi.ipset_name, s, ZEBRA_IPSET_NAME_SIZE);
+	STREAM_GETL(s, zpi.nb_interface);
+	zebra_pbr_iptable_update_interfacelist(s, &zpi);
 
 	if (hdr->command == ZEBRA_IPTABLE_ADD)
 		zebra_pbr_add_iptable(zvrf->zns, &zpi);

zebra/zebra_pbr.h
@@ -131,6 +131,10 @@ struct zebra_pbr_iptable {
 
 	uint32_t action;
 
+	uint32_t nb_interface;
+
+	struct list *interface_name_list;
+
 	char ipset_name[ZEBRA_IPSET_NAME_SIZE];
 };
 
@@ -219,5 +223,7 @@ extern int zebra_pbr_iptable_hash_equal(const void *arg1, const void *arg2);
 extern void zebra_pbr_init(void);
 extern void zebra_pbr_show_ipset_list(struct vty *vty, char *ipsetname);
 extern void zebra_pbr_show_iptable(struct vty *vty);
+extern void zebra_pbr_iptable_update_interfacelist(struct stream *s,
+				   struct zebra_pbr_iptable *zpi);
 
 #endif /* _ZEBRA_PBR_H */

commit 92cddedffdf0681577f785743a19e9f79721144f

zebra: fix pbr_iptable memory leak
We are obviously doing deleting on wrong object.

> Direct leak of 40 byte(s) in 1 object(s) allocated from:
>     #0 0x7fcf718b4a57 in __interceptor_calloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:154
>     #1 0x7fcf7126f8dd in qcalloc lib/memory.c:105
>     #2 0x7fcf7124401a in list_new lib/linklist.c:49
>     #3 0x55771621d86d in pbr_iptable_alloc_intern zebra/zebra_pbr.c:1015
>     #4 0x7fcf71217d79 in hash_get lib/hash.c:147
>     #5 0x55771621dad3 in zebra_pbr_add_iptable zebra/zebra_pbr.c:1030
>     #6 0x55771614d00c in zread_iptable zebra/zapi_msg.c:4131
>     #7 0x55771614e586 in zserv_handle_commands zebra/zapi_msg.c:4424
>     #8 0x5577162dae2c in zserv_process_messages zebra/zserv.c:521
>     #9 0x7fcf7137798e in event_call lib/event.c:2011
>     #10 0x7fcf71242ff1 in frr_run lib/libfrr.c:1216
>     #11 0x5577160e4d6d in main zebra/main.c:540
>     #12 0x7fcf70c29d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
>
> Indirect leak of 24 byte(s) in 1 object(s) allocated from:
>     #0 0x7fcf718b4a57 in __interceptor_calloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:154
>     #1 0x7fcf7126f8dd in qcalloc lib/memory.c:105
>     #2 0x7fcf71244129 in listnode_new lib/linklist.c:71
>     #3 0x7fcf71244238 in listnode_add lib/linklist.c:92
>     #4 0x55771621d938 in pbr_iptable_alloc_intern zebra/zebra_pbr.c:1019
>     #5 0x7fcf71217d79 in hash_get lib/hash.c:147
>     #6 0x55771621dad3 in zebra_pbr_add_iptable zebra/zebra_pbr.c:1030
>     #7 0x55771614d00c in zread_iptable zebra/zapi_msg.c:4131
>     #8 0x55771614e586 in zserv_handle_commands zebra/zapi_msg.c:4424
>     #9 0x5577162dae2c in zserv_process_messages zebra/zserv.c:521
>     #10 0x7fcf7137798e in event_call lib/event.c:2011
>     #11 0x7fcf71242ff1 in frr_run lib/libfrr.c:1216
>     #12 0x5577160e4d6d in main zebra/main.c:540
>     #13 0x7fcf70c29d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58

Fixes: f80ec7e3d6 ("zebra: handle iptable list of interfaces")

zebra/zebra_pbr.c
@@ -1047,13 +1047,13 @@ void zebra_pbr_del_iptable(struct zebra_pbr_iptable *iptable)
 		char *name;
 
 		hash_release(zrouter.iptable_hash, lookup);
-		for (ALL_LIST_ELEMENTS(iptable->interface_name_list,
+		for (ALL_LIST_ELEMENTS(lookup->interface_name_list,
 				       node, nnode, name)) {
 			XFREE(MTYPE_PBR_IPTABLE_IFNAME, name);
-			list_delete_node(iptable->interface_name_list,
+			list_delete_node(lookup->interface_name_list,
 					 node);
 		}
-		list_delete(&iptable->interface_name_list);
+		list_delete(&lookup->interface_name_list);
 		XFREE(MTYPE_PBR_IPTABLE, lookup);
 	} else
 		zlog_debug("%s: IPTable being deleted we know nothing about",