aafc484114e8e2b0c521eb57fcd66b7c988b16b8
Donatas Abraitis <donatas@opensourcerouting.org>
2025-05-09 10:23:39 +0300

    bgpd: Add `neighbor X bfd strict` command
    This command is used to keep BGP up/down state strictly related to BFD state.
    
    If BFD state is down, then BGP is kept down too.
    
    This is something partially of https://datatracker.ietf.org/doc/html/draft-ietf-idr-bgp-bfd-strict-mode,
    but it's not yet defined completely, so adding kinda the same, but without a
    capability for now and without FSM changes.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>
    (cherry picked from commit 4169f27085e8548e81e0f0db61a444ee5678bcc0)
    
    # Conflicts:
    #	bgpd/bgpd.c
    #	bgpd/bgpd.h

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -4694,6 +4694,7 @@ bool peer_active(struct peer_connection *connection)
 
 	if (peer->bfd_config) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 		if (peer_established(peer->connection) && bfd_session_is_down(peer->bfd_config->session))
 			return false;
 =======
@@ -4703,6 +4704,15 @@ bool peer_active(struct peer_connection *connection)
 			 bfd_session_is_down(peer->bfd_config->session))
 			return BGP_PEER_BFD_DOWN;
 >>>>>>> f9400457e (bgpd: Make sure BGP stays in Idle if BFD profile is admin shutdown)
+=======
+		/* Only if `neighbor X bfd strict` is enabled */
+		if (peergroup_flag_check(peer, PEER_FLAG_BFD_STRICT)) {
+			if (bfd_session_is_admin_down(peer->bfd_config->session))
+				return BGP_PEER_BFD_ADMIN_DOWN;
+			else if (bfd_session_is_down(peer->bfd_config->session))
+				return BGP_PEER_BFD_DOWN;
+		}
+>>>>>>> 4169f2708 (bgpd: Add `neighbor X bfd strict` command)
 	}
 
 	if (peer->afc[AFI_IP][SAFI_UNICAST] || peer->afc[AFI_IP][SAFI_MULTICAST]
@@ -4857,6 +4867,11 @@ static const struct peer_flag_action peer_flag_action_list[] = {
 	{PEER_FLAG_EXTENDED_LINK_BANDWIDTH, 0, peer_change_none},
 	{PEER_FLAG_LONESOUL, 0, peer_change_reset_out},
 	{PEER_FLAG_TCP_MSS, 0, peer_change_none},
+<<<<<<< HEAD
+=======
+	{PEER_FLAG_CAPABILITY_LINK_LOCAL, 0, peer_change_none},
+	{PEER_FLAG_BFD_STRICT, 0, peer_change_none},
+>>>>>>> 4169f2708 (bgpd: Add `neighbor X bfd strict` command)
 	{0, 0, 0}};
 
 static const struct peer_flag_action peer_af_flag_action_list[] = {
