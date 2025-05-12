609a35e3d4ee54748119f4510f666c2c0998b9eb
Eugene Crosser <crosser@average.org>
2025-05-12 17:08:43 +0200

    bgpd: log error if no label found in path_info
    Signed-off-by: Eugene Crosser <crosser@average.org>

--- bgpd/bgp_evpn.c
+++ bgpd/bgp_evpn.c
@@ -8158,8 +8158,13 @@ vni_t bgp_evpn_path_info_get_l3vni(const struct bgp_path_info *pi)
 
 	mpls_label_t *label = bgp_evpn_path_info_labels_get_l3vni(pi->extra->labels->label,
 								  pi->extra->labels->num_labels);
-	if (!label)
+	if (!label) {
+		/* Shouldn't happen: label not found in pi->extra */
+		flog_err(EC_BGP_PATH_WITHOUT_LABEL,
+			 "%s: path_info without label stack, label=%p, num_labels=%d", __func__,
+			 pi->extra->labels->label, pi->extra->labels->num_labels);
 		return 0;
+	}
 
 	return label2vni(label);
 }
