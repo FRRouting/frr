FRRouting/frr

commit 5323ad0d1fec3f7b0751e325cd4d34ebf8a9f56c

staticd: Add SRv6 encap behavior in show running-config
This commit extends the output of `show running-config` to display the
configured SRv6 encapsulation behavior.

```
router# show running-config
[...]
ip route 192.0.2.2/32 sr0 segments fcbb:bbbb:1:2:3:4:5:6/fcbb:bbbb:7:8:fe00:: encap-behavior H_Encaps
ip route 192.0.2.3/32 sr0 segments fcbb:bbbb:1:2:3:4:5:6/fcbb:bbbb:7:8:fe00:: encap-behavior H_Encaps_Red
ipv6 route 2001:db8:1:1::2/128 sr0 segments fcbb:bbbb:1:2:3:4:5:6/fcbb:bbbb:7:8:fe00:: encap-behavior H_Encaps
ipv6 route 2001:db8:1:1::3/128 sr0 segments fcbb:bbbb:1:2:3:4:5:6/fcbb:bbbb:7:8:fe00:: encap-behavior H_Encaps_Red
[...]
```

Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

staticd/static_vty.c
@@ -1518,6 +1518,8 @@ static void nexthop_cli_show(struct vty *vty, const struct lyd_node *route,
 	uint8_t distance;
 	struct mpls_label_iter iter;
 	struct srv6_seg_iter seg_iter;
+	enum srv6_headend_behavior srv6_encap_behavior = SRV6_HEADEND_BEHAVIOR_H_ENCAPS;
+	const char *srv6_encap_behavior_str;
 	const char *nexthop_vrf;
 	uint32_t table_id;
 	struct prefix src_prefix;
@@ -1599,6 +1601,19 @@ static void nexthop_cli_show(struct vty *vty, const struct lyd_node *route,
 	seg_iter.first = true;
 	yang_dnode_iterate(srv6_seg_iter_cb, &seg_iter, nexthop,
 			   "./srv6-segs-stack/entry");
+
+	if (yang_dnode_exists(nexthop, "./srv6-segs-stack/encap-behavior")) {
+		srv6_encap_behavior_str = yang_dnode_get_string(nexthop,
+								"./srv6-segs-stack/encap-behavior");
+		if (strmatch(srv6_encap_behavior_str, "ietf-srv6-types:H.Encaps"))
+			srv6_encap_behavior = SRV6_HEADEND_BEHAVIOR_H_ENCAPS;
+		else if (strmatch(srv6_encap_behavior_str, "ietf-srv6-types:H.Encaps.Red"))
+			srv6_encap_behavior = SRV6_HEADEND_BEHAVIOR_H_ENCAPS_RED;
+
+		if (srv6_encap_behavior != SRV6_HEADEND_BEHAVIOR_H_ENCAPS || show_defaults)
+			vty_out(vty, " encap-behavior %s",
+				srv6_headend_behavior2str(srv6_encap_behavior));
+	}
 
 	nexthop_vrf = yang_dnode_get_string(nexthop, "vrf");
 	if (strcmp(vrf, nexthop_vrf))

commit d9adf193979b0d36e7af1638cff378f1f2b7b129

staticd, bgp: fix srv6 encap-value displayed with _ instead of .
When configuring an srv6 route with encap option, the displayed option
in running config differs:

> ubuntu2404(config)# ipv6 route 5::6/128 loop0 segments 1::1 encap-behavior H_Encaps_Red
> ubuntu2404(config)# do show ipv6 route
> [..]
> S>* 5::6/128 [1/0] is directly connected, loop0, seg6 1::1, encap behavior H.Encaps.Red, weight 1, 00:00:02
> ubuntu2404(config)# do show running-config
> [..]
> ipv6 route 5::6/128 loop0 segments 1::1 encap-behavior H.Encaps.Red

Modify output in running-config wherever the encap-behavior is used. Use
_ instead.

Fixes: 5323ad0d1fec ("staticd: Add SRv6 encap behavior in show running-config")
Fixes: dbd77b184f1a ("bgpd: add encap red support to L3VPN seg6 routes")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_vty.c
@@ -20113,7 +20113,7 @@ int bgp_config_write(struct vty *vty)
 					bgp->srv6_locator_name);
 			if (bgp->srv6_encap_behavior != SRV6_HEADEND_BEHAVIOR_H_ENCAPS)
 				vty_out(vty, "  encap-behavior %s\n",
-					srv6_headend_behavior2str(bgp->srv6_encap_behavior));
+					srv6_headend_behavior2str(bgp->srv6_encap_behavior, true));
 			vty_endframe(vty, " exit\n");
 		}
 

lib/nexthop.c
@@ -1398,8 +1398,9 @@ void nexthop_json_helper(json_object *json_nexthop,
 							 ->seg[0]);
 			json_object_object_add(json_nexthop, "seg6", json_seg6);
 			json_object_string_add(json_nexthop, "srv6EncapBehavior",
-					       srv6_headend_behavior2str(
-						       nexthop->nh_srv6->seg6_segs->encap_behavior));
+					       srv6_headend_behavior2str(nexthop->nh_srv6->seg6_segs
+										 ->encap_behavior,
+									 false));
 		} else {
 			if (nexthop->nh_srv6->seg6_segs) {
 				json_segs = json_object_new_array();
@@ -1417,9 +1418,10 @@ void nexthop_json_helper(json_object *json_nexthop,
 				json_object_object_add(json_nexthop, "seg6",
 						       json_segs);
 				json_object_string_add(json_nexthop, "srv6EncapBehavior",
-						       srv6_headend_behavior2str(
-							       nexthop->nh_srv6->seg6_segs
-								       ->encap_behavior));
+						       srv6_headend_behavior2str(nexthop->nh_srv6
+											 ->seg6_segs
+											 ->encap_behavior,
+										 false));
 			}
 		}
 	}
@@ -1553,8 +1555,9 @@ void nexthop_vty_helper(struct vty *vty, const struct nexthop *nexthop,
 			if (nexthop->nh_srv6->seg6_segs->encap_behavior !=
 			    SRV6_HEADEND_BEHAVIOR_H_ENCAPS)
 				vty_out(vty, ", encap behavior %s",
-					srv6_headend_behavior2str(
-						nexthop->nh_srv6->seg6_segs->encap_behavior));
+					srv6_headend_behavior2str(nexthop->nh_srv6->seg6_segs
+									  ->encap_behavior,
+								  false));
 		}
 	}
 

lib/srv6.h
@@ -380,19 +380,20 @@ struct srv6_sid_ctx {
 	ifindex_t ifindex;
 };
 
-static inline const char *srv6_headend_behavior2str(enum srv6_headend_behavior behavior)
+static inline const char *srv6_headend_behavior2str(enum srv6_headend_behavior behavior,
+						    bool running_conf)
 {
 	switch (behavior) {
 	case SRV6_HEADEND_BEHAVIOR_H_INSERT:
-		return "H.Insert";
+		return running_conf ? "H_Insert" : "H.Insert";
 	case SRV6_HEADEND_BEHAVIOR_H_ENCAPS:
-		return "H.Encaps";
+		return running_conf ? "H_Encaps" : "H.Encaps";
 	case SRV6_HEADEND_BEHAVIOR_H_ENCAPS_RED:
-		return "H.Encaps.Red";
+		return running_conf ? "H_Encaps_Red" : "H.Encaps.Red";
 	case SRV6_HEADEND_BEHAVIOR_H_ENCAPS_L2:
-		return "H.Encaps.L2";
+		return running_conf ? "H_Encaps_L2" : "H.Encaps.L2";
 	case SRV6_HEADEND_BEHAVIOR_H_ENCAPS_L2_RED:
-		return "H.Encaps.L2.Red";
+		return running_conf ? "H_Encaps_L2_Red" : "H.Encaps.L2.Red";
 	}
 
 	return "unknown";

staticd/static_vty.c
@@ -1612,7 +1612,7 @@ static void nexthop_cli_show(struct vty *vty, const struct lyd_node *route,
 
 		if (srv6_encap_behavior != SRV6_HEADEND_BEHAVIOR_H_ENCAPS || show_defaults)
 			vty_out(vty, " encap-behavior %s",
-				srv6_headend_behavior2str(srv6_encap_behavior));
+				srv6_headend_behavior2str(srv6_encap_behavior, true));
 	}
 
 	nexthop_vrf = yang_dnode_get_string(nexthop, "vrf");