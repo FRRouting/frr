FRRouting/frr

commit 1115feedc3f878853e2e67df9efdd0f5e5e1f997

bgpd: add some counters not displayed yet
Add some counters to keep track how often stuff is done.
This is mainly for us developers.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgp_route.c
@@ -745,6 +745,8 @@ int bgp_path_info_cmp(struct bgp *bgp, struct bgp_path_info *new,
 	struct bgp_path_info *bpi_ultimate;
 	struct peer *peer_new, *peer_exist;
 
+	bgp->bestpath_runs++;
+
 	*paths_eq = 0;
 
 	/* 0. Null check. */
@@ -4119,8 +4121,10 @@ static void bgp_process_internal(struct bgp *bgp, struct bgp_dest *dest,
 	}
 
 	/* already scheduled for processing? */
-	if (CHECK_FLAG(dest->flags, BGP_NODE_PROCESS_SCHEDULED))
+	if (CHECK_FLAG(dest->flags, BGP_NODE_PROCESS_SCHEDULED)) {
+		bgp->node_already_on_queue++;
 		return;
+	}
 
 	/* If the flag BGP_NODE_SELECT_DEFER is set, do not add route to
 	 * the workqueue
@@ -4129,6 +4133,7 @@ static void bgp_process_internal(struct bgp *bgp, struct bgp_dest *dest,
 		if (BGP_DEBUG(update, UPDATE_OUT))
 			zlog_debug("BGP_NODE_SELECT_DEFER set for route %p",
 				   dest);
+		bgp->node_deferred_on_queue++;
 		return;
 	}
 
@@ -13095,6 +13100,9 @@ DEFUN(show_ip_bgp_afi_safi_statistics, show_ip_bgp_afi_safi_statistics_cmd,
 		json = json_object_new_object();
 		json_object_object_add(json, get_afi_safi_str(afi, safi, true),
 				       json_afi_safi);
+		json_object_int_add(json, "bgpBestPathCalls", bgp->bestpath_runs);
+		json_object_int_add(json, "bgpNodeOnQueue", bgp->node_already_on_queue);
+		json_object_int_add(json, "bgpNodeDeferredOnQueue", bgp->node_deferred_on_queue);
 		vty_json(vty, json);
 	}
 	return ret;

bgpd/bgpd.h
@@ -864,6 +864,10 @@ struct bgp {
 	/* BGP route flap dampening configuration */
 	struct bgp_damp_config damp[AFI_MAX][SAFI_MAX];
 
+	uint64_t bestpath_runs;
+	uint64_t node_already_on_queue;
+	uint64_t node_deferred_on_queue;
+
 	QOBJ_FIELDS;
 };
 DECLARE_QOBJ_TYPE(bgp);

commit 16ba374b9795783c79d44e4393fc6ed3d5e69996

bgpd: Fix crash when fetching statistics for bgp instance
When running `show bgp view all ipv4 unicast statistics json` and bgp is NULL,
do not crash it and instead iterate over the all instances and put stats
accordingly.

Fixes: https://github.com/FRRouting/frr/issues/18982
Fixes: 1115feedc3f878853e2e67df9efdd0f5e5e1f997 ("bgpd: add some counters not displayed yet")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_route.c
@@ -13837,11 +13837,7 @@ DEFUN(show_ip_bgp_afi_safi_statistics, show_ip_bgp_afi_safi_statistics_cmd,
 
 	if (uj) {
 		json = json_object_new_object();
-		json_object_object_add(json, get_afi_safi_str(afi, safi, true),
-				       json_afi_safi);
-		json_object_int_add(json, "bgpBestPathCalls", bgp->bestpath_runs);
-		json_object_int_add(json, "bgpNodeOnQueue", bgp->node_already_on_queue);
-		json_object_int_add(json, "bgpNodeDeferredOnQueue", bgp->node_deferred_on_queue);
+		json_object_object_add(json, get_afi_safi_str(afi, safi, true), json_afi_safi);
 		vty_json(vty, json);
 	}
 	return ret;
@@ -14705,6 +14701,12 @@ static int bgp_table_stats_single(struct vty *vty, struct bgp *bgp, afi_t afi,
 			get_afi_safi_str(afi, safi, false), bgp->name_pretty);
 	else
 		json_object_string_add(json, "instance", bgp->name_pretty);
+
+	if (json) {
+		json_object_int_add(json, "bgpBestPathCalls", bgp->bestpath_runs);
+		json_object_int_add(json, "bgpNodeOnQueue", bgp->node_already_on_queue);
+		json_object_int_add(json, "bgpNodeDeferredOnQueue", bgp->node_deferred_on_queue);
+	}
 
 	/* labeled-unicast routes live in the unicast table */
 	if (safi == SAFI_LABELED_UNICAST)