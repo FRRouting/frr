33267a12a033ccc17e580fc21a887b82651f9397
Philippe Guibert <philippe.guibert@6wind.com>
2025-06-25 10:36:48 +0200

    bgpd: add srv6 locator per vrf support
    The srv6 locator can be configured per vrf, but are never taken into
    account in the SID allocation logic.
    
    > r1# show running-config
    > router bgp 1
    >  segment-routing srv6
    >   locator loc1
    >  exit
    > exit
    > router bgp 1 vrf vrf20
    >  segment-routing srv6
    >   locator loc2
    >  exit
    > exit
    > segment-routing
    >  srv6
    >   locators
    >    locator loc1
    >     prefix 2001:db8:1:1::/64
    >    exit
    >    !
    >    locator BBB
    >     prefix 2002:db8:1:1::/64
    >    exit
    > [..]
    > r1# do show segment-routing srv6 sid
    >  SID               Behavior    Context        Daemon/Instance    Locator    AllocationType
    >  ----------------------------  -------------  -----------------  ---------  ----------------
    >  2001:db8:1:1:1::  End.DT4     VRF 'vrf10'    bgp(0)             loc1       dynamic
    >  2001:db8:1:1:2::  End.DT6     VRF 'vrf10'    bgp(0)             loc1       dynamic
    >  2001:db8:1:1:3::  End.DT6     VRF 'vrf20'    bgp(0)             loc1       dynamic
    
    Add BGP zebra support to update bgp->srv6_locator for each VRF.
    Enforce the logic control of SID: use the VRF locator if available,
    otherwise fallback to the BGP default one.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -801,7 +801,7 @@ void ensure_vrf_tovpn_sid_per_af(struct bgp *bgp_vpn, struct bgp *bgp_vrf,
 	if (!bgp_vpn)
 		return;
 
-	locator_bgp = bgp_srv6_locator_lookup(bgp_vpn);
+	locator_bgp = bgp_srv6_locator_lookup(bgp_vrf, bgp_vpn);
 	/*
 	 * skip when bgp srv6 locator isn't allocated
 	 */
@@ -878,7 +878,7 @@ void ensure_vrf_tovpn_sid_per_vrf(struct bgp *bgp_vpn, struct bgp *bgp_vrf)
 	if (!bgp_vpn)
 		return;
 
-	locator_bgp = bgp_srv6_locator_lookup(bgp_vpn);
+	locator_bgp = bgp_srv6_locator_lookup(bgp_vrf, bgp_vpn);
 	/*
 	 * skip when bgp srv6 locator isn't allocated
 	 */
@@ -919,7 +919,7 @@ void ensure_vrf_tovpn_sid_per_vrf(struct bgp *bgp_vpn, struct bgp *bgp_vrf)
 	}
 
 	if (!tovpn_sid_auto && !is_tovpn_sid_explicit) {
-		if (!srv6_sid_compose(&tovpn_sid, bgp_srv6_locator_lookup(bgp_vpn),
+		if (!srv6_sid_compose(&tovpn_sid, bgp_srv6_locator_lookup(bgp_vrf, bgp_vpn),
 				      bgp_vrf->tovpn_sid_index)) {
 			zlog_err("%s: failed to compose new sid for vrf %s",
 				 __func__, bgp_vrf->name_pretty);
@@ -1162,7 +1162,9 @@ static bool leak_update_nexthop_valid(struct bgp *to_bgp, struct bgp_dest *bn,
 	 * If the SID per VRF is not available, also consider the rib as
 	 * invalid.
 	 */
-	if (bgp_srv6_locator_is_configured(to_bgp) && nh_valid)
+	if ((bgp_srv6_locator_is_configured(to_bgp) ||
+	     bgp_srv6_locator_is_configured(bgp_nexthop)) &&
+	    nh_valid)
 		nh_valid = is_pi_srv6_valid(bpi, bgp_nexthop, afi, safi);
 
 	if (debug)
