7075edeb9b7e8806a79bf417d1158be700d5c6e2
Philippe Guibert <philippe.guibert@6wind.com>
2025-06-30 17:01:59 +0200

    bgpd: fix explicit-sid with func_bits over 20 bits
    When using the 'sid vpn per-vrf export explicit' command, there is no
    control to know if the sid func part is corretly encoded in the BGP
    update. If the func part is over the 20 bit value, then there is missing
    information, and summing up the label and the sid info is not enough.
    
    For instance, the value cece:2222 has to be encoded in the 32 bit func
    part.
    
    > segment-routing
    >  srv6
    >   locators
    >    locator loc1
    >     prefix fc00:0:5::/48 block-len 32 node-len 16 func-bits 32
    >    !
    >   !
    >  !
    > !
    > router bgp 5 vrf vrf10
    >  address-family ipv6 unicast
    >   sid vpn export explicit fc00:0:5:cece:2222::
    > [..]
    
    But BGP does not know how to encode the whole SID information.
    
    > rt5# show bgp ipv6 vpn 2001:db9:10::/64
    > BGP routing table entry for 5:10:2001:db9:10::/64, version 1
    > not allocated
    > Paths: (1 available, best #1)
    >   Advertised to peers:
    >   fc00:0:1::1
    >   Local
    >     :: from :: (5.5.5.5) vrf vrf10(7) announce-nh-self
    >       Origin incomplete, metric 0, weight 32768, valid, sourced, local, best (First path received)
    >       Extended Community: RT:99:99
    >       Originator: 5.5.5.5
    >       Remote label: 0
    >       Remote SID: fc00:0:5::
    >       Last update: Mon Jun 30 17:15:50 2025
    
    In the case, the function bit length of the locator is over 20 bits,
    consider encoding the SID in the prefix SID TLV, and do not use the
    label value.
    
    > rt1# show bgp ipv6 vpn 2001:db9:10::/64
    > BGP routing table entry for 5:10:2001:db9:10::/64, version 1
    > not allocated
    > Paths: (1 available, best #1)
    >   Advertised to peers:
    >   fc00:0:5::1 fc00:0:6::1
    >   5
    >     fc00:0:5::1 (metric 20) from fc00:0:5::1 (5.5.5.5)
    >       Origin incomplete, metric 0, valid, external, best (First path received)
    >       Extended Community: RT:99:99
    >       Remote label: 3
    >       Remote SID: fc00:0:5:cece:2222::
    >       Last update: Mon Jun 30 17:00:05 2025
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_mplsvpn.c
+++ bgpd/bgp_mplsvpn.c
@@ -2002,19 +2002,22 @@ void vpn_leak_from_vrf_update(struct bgp *to_bgp,	     /* to */
 		static_attr.srv6_l3vpn->arg_len =
 			from_bgp->vpn_policy[afi]
 				.tovpn_sid_locator->argument_bits_length;
-		static_attr.srv6_l3vpn->transposition_len =
-			from_bgp->vpn_policy[afi]
-				.tovpn_sid_locator->function_bits_length;
-		static_attr.srv6_l3vpn->transposition_offset =
-			from_bgp->vpn_policy[afi]
-				.tovpn_sid_locator->block_bits_length +
-			from_bgp->vpn_policy[afi]
-				.tovpn_sid_locator->node_bits_length;
-		;
-		memcpy(&static_attr.srv6_l3vpn->sid,
-		       &from_bgp->vpn_policy[afi]
-				.tovpn_sid_locator->prefix.prefix,
-		       sizeof(struct in6_addr));
+		if (from_bgp->vpn_policy[afi].tovpn_sid_locator->function_bits_length >
+		    BGP_PREFIX_SID_SRV6_MAX_FUNCTION_LENGTH) {
+			static_attr.srv6_l3vpn->transposition_len = 0;
+			static_attr.srv6_l3vpn->transposition_offset = 0;
+			memcpy(&static_attr.srv6_l3vpn->sid, from_bgp->vpn_policy[afi].tovpn_sid,
+			       sizeof(struct in6_addr));
+		} else {
+			static_attr.srv6_l3vpn->transposition_len =
+				from_bgp->vpn_policy[afi].tovpn_sid_locator->function_bits_length;
+			static_attr.srv6_l3vpn->transposition_offset =
+				from_bgp->vpn_policy[afi].tovpn_sid_locator->block_bits_length +
+				from_bgp->vpn_policy[afi].tovpn_sid_locator->node_bits_length;
+			memcpy(&static_attr.srv6_l3vpn->sid,
+			       &from_bgp->vpn_policy[afi].tovpn_sid_locator->prefix.prefix,
+			       sizeof(struct in6_addr));
+		}
 	} else if (from_bgp->tovpn_sid_locator) {
 		struct srv6_locator *locator = from_bgp->tovpn_sid_locator;
 
@@ -2035,14 +2038,22 @@ void vpn_leak_from_vrf_update(struct bgp *to_bgp,	     /* to */
 			from_bgp->tovpn_sid_locator->function_bits_length;
 		static_attr.srv6_l3vpn->arg_len =
 			from_bgp->tovpn_sid_locator->argument_bits_length;
-		static_attr.srv6_l3vpn->transposition_len =
-			from_bgp->tovpn_sid_locator->function_bits_length;
-		static_attr.srv6_l3vpn->transposition_offset =
-			from_bgp->tovpn_sid_locator->block_bits_length +
-			from_bgp->tovpn_sid_locator->node_bits_length;
-		memcpy(&static_attr.srv6_l3vpn->sid,
-		       &from_bgp->tovpn_sid_locator->prefix.prefix,
-		       sizeof(struct in6_addr));
+		if (from_bgp->tovpn_sid_locator->function_bits_length >
+		    BGP_PREFIX_SID_SRV6_MAX_FUNCTION_LENGTH) {
+			static_attr.srv6_l3vpn->transposition_len = 0;
+			static_attr.srv6_l3vpn->transposition_offset = 0;
+			memcpy(&static_attr.srv6_l3vpn->sid, from_bgp->tovpn_sid,
+			       sizeof(struct in6_addr));
+		} else {
+			static_attr.srv6_l3vpn->transposition_len =
+				from_bgp->tovpn_sid_locator->function_bits_length;
+			static_attr.srv6_l3vpn->transposition_offset =
+				from_bgp->tovpn_sid_locator->block_bits_length +
+				from_bgp->tovpn_sid_locator->node_bits_length;
+			memcpy(&static_attr.srv6_l3vpn->sid,
+			       &from_bgp->tovpn_sid_locator->prefix.prefix,
+			       sizeof(struct in6_addr));
+		}
 	}
 
 
