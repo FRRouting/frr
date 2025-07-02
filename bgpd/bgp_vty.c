FRRouting/frr

commit 051f9b9731bf206582d580ec5a538278509fbe96

bgpd: add neighbor ip-transparent
Implement a per‑neighbor flag that sets IP_TRANSPARENT for the
underlying TCP socket. With this flag bgpd can accept or initiate a
session to/from an address that is not present on the host.

Typical use‑cases:
  - running bgpd inside a container without configuring the router
    loopback address inside that netns.
  - hitless switchover of a keepalived/VRRP VIP: the standby bgpd
    can pre‑bind and come up instantly after takeover.
  - BGP speakers when the IP address is not set (transparent
    firewall).
  - others...

It is safeguarded by a CAP_NET_ADMIN.

Signed-off-by: Vincent Jardin <vjardin@free.fr>

bgpd/bgp_vty.c
@@ -18427,6 +18427,34 @@ DEFUN(no_neighbor_tcp_mss, no_neighbor_tcp_mss_cmd,
 	return peer_tcp_mss_vty(vty, argv[peer_index]->arg, NULL);
 }
 
+DEFPY(neighbor_ip_transparent,
+      neighbor_ip_transparent_cmd,
+      "[no$no] neighbor <A.B.C.D|X:X::X:X|WORD>$neighbor ip-transparent",
+      NO_STR
+      NEIGHBOR_STR
+      NEIGHBOR_ADDR_STR2
+      "Enable IP_TRANSPARENT on the BGP TCP socket\n")
+{
+	struct peer *peer;
+	int ret;
+
+	peer = peer_and_group_lookup_vty(vty, neighbor);
+	if (!peer)
+		return CMD_WARNING_CONFIG_FAILED;
+
+	if (!peergroup_flag_check(peer, PEER_FLAG_UPDATE_SOURCE)) {
+		vty_out(vty, "%% Missing update-source\n");
+		return CMD_WARNING_CONFIG_FAILED;
+	}
+
+	if (no)
+		ret = peer_flag_unset_vty(vty, neighbor, PEER_FLAG_IP_TRANSPARENT);
+	else
+		ret = peer_flag_set_vty(vty, neighbor, PEER_FLAG_IP_TRANSPARENT);
+
+	return bgp_vty_return(vty, ret);
+}
+
 DEFPY(bgp_retain_route_target, bgp_retain_route_target_cmd,
       "[no$no] bgp retain route-target all",
       NO_STR BGP_STR
@@ -19026,6 +19054,10 @@ static void bgp_config_write_peer_global(struct vty *vty, struct bgp *bgp,
 			vty_out(vty, " neighbor %s update-source %s\n", addr,
 				peer->update_if);
 	}
+
+	/* ip-transparent on/off */
+	if (peergroup_flag_check(peer, PEER_FLAG_IP_TRANSPARENT))
+		vty_out(vty, " neighbor %s ip-transparent\n", addr);
 
 	/* advertisement-interval */
 	if (peergroup_flag_check(peer, PEER_FLAG_ROUTEADV))
@@ -22219,6 +22251,8 @@ void bgp_vty_init(void)
 	/* tcp-mss command */
 	install_element(BGP_NODE, &neighbor_tcp_mss_cmd);
 	install_element(BGP_NODE, &no_neighbor_tcp_mss_cmd);
+
+	install_element(BGP_NODE, &neighbor_ip_transparent_cmd);
 
 	/* srv6 commands */
 	install_element(VIEW_NODE, &show_bgp_srv6_cmd);

bgpd/bgp_network.c
@@ -828,6 +828,14 @@ enum connect_result bgp_connect(struct peer_connection *connection)
 	if (CHECK_FLAG(peer->flags, PEER_FLAG_TCP_MSS))
 		sockopt_tcp_mss_set(connection->fd, peer->tcp_mss);
 
+	/* Neighbor's bind() as a source address when ip transparent is used */
+	if (CHECK_FLAG(peer->flags, PEER_FLAG_IP_TRANSPARENT) &&
+	    CHECK_FLAG(peer->flags, PEER_FLAG_UPDATE_SOURCE)) {
+		frr_with_privs (&bgpd_privs) {
+			sockopt_ip_transparent(connection->fd);
+		}
+	}
+
 	bgp_socket_set_buffer_size(connection->fd);
 
 	/* Set TCP keepalive when TCP keepalive is enabled */

bgpd/bgpd.c
@@ -4979,6 +4979,7 @@ static const struct peer_flag_action peer_flag_action_list[] = {
 	{PEER_FLAG_CAPABILITY_LINK_LOCAL, 0, peer_change_none},
 	{PEER_FLAG_BFD_STRICT, 0, peer_change_none},
 	{PEER_FLAG_SEND_NHC_ATTRIBUTE, 0, peer_change_none},
+	{PEER_FLAG_IP_TRANSPARENT, 0, peer_change_reset},
 	{0, 0, 0}};
 
 static const struct peer_flag_action peer_af_flag_action_list[] = {

bgpd/bgpd.h
@@ -1703,6 +1703,7 @@ struct peer {
 #define PEER_FLAG_BFD_STRICT (1ULL << 43)
 /* https://datatracker.ietf.org/doc/html/draft-ietf-idr-entropy-label */
 #define PEER_FLAG_SEND_NHC_ATTRIBUTE (1ULL << 44)
+#define PEER_FLAG_IP_TRANSPARENT     (1ULL << 45) /* ip-transparent */
 
 	/*
 	 *GR-Disabled mode means unset PEER_FLAG_GRACEFUL_RESTART

commit ecb8f95437682fb8f956b8d8a7dd4097da385bef

bgpd: Fix 'no' form for 'neighbor X ip-transparent' command
```
donatas(config)# router bgp
donatas(config-router)# neighbor 127.0.0.1 update-source lo
donatas(config-router)# neighbor 127.0.0.1 ip-transparent
donatas(config-router)# no neighbor 127.0.0.1 update-source lo
donatas(config-router)# no neighbor 127.0.0.1 ip-transparent
% Missing update-source
donatas(config-router)#
```

Fixes: 051f9b9731bf206582d580ec5a538278509fbe96 ("bgpd: add neighbor ip-transparent")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -18504,7 +18504,7 @@ DEFPY(neighbor_ip_transparent,
 	if (!peer)
 		return CMD_WARNING_CONFIG_FAILED;
 
-	if (!peergroup_flag_check(peer, PEER_FLAG_UPDATE_SOURCE)) {
+	if (!no && !peergroup_flag_check(peer, PEER_FLAG_UPDATE_SOURCE)) {
 		vty_out(vty, "%% Missing update-source\n");
 		return CMD_WARNING_CONFIG_FAILED;
 	}