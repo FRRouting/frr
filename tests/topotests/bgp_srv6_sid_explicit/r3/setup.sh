f7fbcc95fa5cd7afed1f3b827e0a3eebcef4eee0
Philippe Guibert <philippe.guibert@6wind.com>
2025-07-07 15:25:12 +0200

    topotests: bgp_srv6_sid_explicit, get wide func idx from 32 bit func-bits
    Add a test that demonstrates that using a 32 bit func-bits locator, and
    using the index allocation mechanism, the obtained SID value is ok.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- tests/topotests/bgp_srv6_sid_explicit/r3/setup.sh
+++ tests/topotests/bgp_srv6_sid_explicit/r3/setup.sh
@@ -5,5 +5,8 @@ sysctl -w net.vrf.strict_mode=1
 
 ip link add Vrf10 type vrf table 10
 ip link set Vrf10 up
+ip link add Vrf20 type vrf table 20
+ip link set Vrf20 up
 
 ip link set eth1 master Vrf10
+ip link set eth2 master Vrf20
