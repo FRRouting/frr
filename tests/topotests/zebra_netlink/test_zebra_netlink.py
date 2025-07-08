ffe44c87c321f90dba9e8533550bbaa284acb257
Gabriel Goller <g.goller@proxmox.com>
2025-07-08 19:30:28 +0200

    tests: add topotest for zebra altnames
    Adds two altnames to an interface and checks if zebra recognizes and
    shows them. Then add ip addrs to the same interface using the real name
    and an altname.
    
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>

--- tests/topotests/zebra_netlink/test_zebra_netlink.py
+++ tests/topotests/zebra_netlink/test_zebra_netlink.py
@@ -103,6 +103,47 @@ def test_zebra_netlink_batching(tgen):
     r1.vtysh_cmd("sharp remove routes 2.1.3.7 " + str(count))
 
 
+def test_zebra_altnames(tgen):
+    "Test zebra altnames."
+    logger.info("Test zebra altnames")
+
+    r1 = tgen.gears["r1"]
+    r1.run("ip link property add dev r1-eth0 altname eno1")
+
+    entry = {"r1-eth0": {"altNames": ["eno1"]}}
+    ok = topotest.router_json_cmp_retry(r1, "show int json", entry, False, 30)
+    assert ok, "Altname 1 not visible"
+
+    r1.run("ip link property add dev r1-eth0 altname thisaltnameislongerthan15chars")
+
+    entry = {"r1-eth0": {"altNames": ["eno1", "thisaltnameislongerthan15chars"]}}
+    ok = topotest.router_json_cmp_retry(r1, "show int json", entry, False, 30)
+    assert ok, "Altname 2 not visible"
+
+@pytest.mark.skip(reason="--enable-transparent-altnames needs to be enabled for this to work")
+def test_zebra_altnames_lookup(tgen):
+    "Test zebra altnames lookup."
+    logger.info("Test zebra altnames")
+
+    r1 = tgen.gears["r1"]
+    # altnames should already exist from the previous test, but ensure they are here
+    r1.run("ip link property add dev r1-eth0 altname eno1")
+    r1.run("ip link property add dev r1-eth0 altname thisaltnameislongerthan15chars")
+
+    r1.vtysh_cmd(
+        """
+          configure terminal
+            interface r1-eth0
+              ip address 172.16.0.5/24
+            interface thisaltnameislongerthan15chars
+              ip address 172.16.0.6/24
+        """
+    )
+
+    entry = {"r1-eth0": {"ipAddresses": [{"address":"172.16.0.5/24"},{"address":"172.16.0.6/24"}]}}
+    ok = topotest.router_json_cmp_retry(r1, "show int json", entry, False, 30)
+    assert ok, "Zebra altname lookup not working"
+
 if __name__ == "__main__":
     args = ["-s"] + sys.argv[1:]
     sys.exit(pytest.main(args))
