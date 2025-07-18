FRRouting/frr

commit fb4bae192a4dd0c6ffb4d98e27923f1a708fe4b9

bgpd: fix allow to reconfigure advertise-all-vni on non default vrf
When running 'no advertise-all-vni' command on the evpn bgp instance,
then reconfiguring it is not possible:

> r2(config)# router bgp 65000 vrf vrf-evpn
> r2(config-router)# address-family l2vpn evpn
> r2(config-router-af)#
> r2(config-router-af)# no advertise-all-vni
> r2(config-router-af)# advertise-all-vni
> % Please unconfigure EVPN in VRF default
> r2(config-router-af)#

Actually, the evpn bgp instance is not the default one. Fix this by not
setting the bgp evpn instance to the default BGP instance, when removing
the 'advertise-all-vni' command.

Fixes: e2f3a930c54c ("bgpd: Allow non-default instance to be EVPN one")
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_evpn_vty.c
@@ -3516,7 +3516,7 @@ static void evpn_set_advertise_all_vni(struct bgp *bgp)
 static void evpn_unset_advertise_all_vni(struct bgp *bgp)
 {
 	bgp->advertise_all_vni = 0;
-	bgp_set_evpn(bgp_get_default());
+	bgp_set_evpn(NULL);
 	bgp_zebra_advertise_all_vni(bgp, bgp->advertise_all_vni);
 	bgp_evpn_cleanup_on_disable(bgp);
 }
@@ -3739,9 +3739,14 @@ DEFUN (no_bgp_evpn_advertise_all_vni,
        "Advertise All local VNIs\n")
 {
 	struct bgp *bgp = VTY_GET_CONTEXT(bgp);
+	struct bgp *bgp_evpn = NULL;
 
 	if (!bgp)
 		return CMD_WARNING;
+	bgp_evpn = bgp_get_evpn();
+	if (!bgp_evpn || bgp_evpn != bgp)
+		return CMD_SUCCESS;
+
 	evpn_unset_advertise_all_vni(bgp);
 	return CMD_SUCCESS;
 }

commit 1120bdc2afc0763e1ef4a7998a357d35141d1e2a

bgpd: fix EVPN show commands on non-VTEP speakers
This is a partial revert of fb4bae19 and 4656368f.

Having the EVPN BGP pointer unset unless `advertise-all-vni` is set
prevents BGP speakers that are not VTEPs (e.g. route-reflectors) from
showing the EVPN RIB from vtysh.

This can easily be seen even on an empty bgpd, expected output:

    route-reflector# show bgp l2vpn evpn
    No prefixes displayed, 0 exist
    route-reflector#

Actual output:

    route-reflector# show bgp l2vpn evpn
    route-reflector#

Of course, when there are actual prefixes to show, this is even more
dramatic.

As for the intention behind 4656368f, a workaround is available, and is
to configure the EVPN BGP instance before the default BGP instance:

    router bgp 65000 vrf vrf-ul
      ! ...
      address-family l2vpn evpn
        advertise-all-vni
    !
    router bgp 65000
      ! ...

If, however, this needs to be done on a live router, an explicit
`no advertise-all-vni` command can be issued in the default VRF to force
EVPN to be moved around. See the topotest `test_evpn_reconfigure_evpn`
for more details.

Fixes: fb4bae19 ("bgpd: fix allow to reconfigure advertise-all-vni on non default vrf")
Fixes: 4656368f ("bgpd: do not set default bgp instance at bgp evpn at creation")

Signed-off-by: Tuetuopay <tuetuopay@me.com>

bgpd/bgp_evpn_vty.c
@@ -3520,8 +3520,17 @@ static void evpn_set_advertise_all_vni(struct bgp *bgp)
  */
 static void evpn_unset_advertise_all_vni(struct bgp *bgp)
 {
+	bgp_set_evpn(bgp_get_default());
+
+	/* When setting back the EVPN instance to be the default instance, we effectively block
+	 * other VRFs from begin the EVPN VRF.
+	 * Let's make it so that `no advertise-all-vni` in the default BGP instance that did not
+	 * really have it "unlocks" EVPN for other VRFs by setting the EVPN pointer to NULL.
+	 */
+	if (bgp->inst_type == BGP_INSTANCE_TYPE_DEFAULT && !bgp->advertise_all_vni)
+		bgp_set_evpn(NULL);
+
 	bgp->advertise_all_vni = 0;
-	bgp_set_evpn(NULL);
 	bgp_zebra_advertise_all_vni(bgp, bgp->advertise_all_vni);
 	bgp_evpn_cleanup_on_disable(bgp);
 }

bgpd/bgpd.c
@@ -3541,6 +3541,12 @@ static struct bgp *bgp_create(as_t *as, const char *name,
 				   name, bgp->as_pretty);
 	}
 
+	/* Default the EVPN VRF to the default one */
+	if (inst_type == BGP_INSTANCE_TYPE_DEFAULT && !bgp_master.bgp_evpn) {
+		bgp_lock(bgp);
+		bm->bgp_evpn = bgp;
+	}
+
 	bgp_lock(bgp);
 
 	bgp->allow_martian = false;

tests/topotests/bgp_evpn_rt5/test_bgp_evpn.py
@@ -347,7 +347,8 @@ def test_bgp_vrf_routes():
 def test_evpn_unconfigure_evpn():
     """
     Unconfigure R2 with advertise-all-vni on vrf-evpn instance
-    Ensure that local L2VPN EVPN entries are removed
+    Ensure that local L2VPN EVPN entries are removed, but we keep those from
+    other BGP speakers.
     """
     tgen = get_topogen()
     if tgen.routers_have_failure():
@@ -365,7 +366,12 @@ def test_evpn_unconfigure_evpn():
         exit
         """
     )
-    expected = {}
+    # show bgp l2vpn evpn routes is broken for vrfs when not vtep, we can't see the prefixes
+    if R2_VRF_UNDERLAY:
+        expected = {"numPrefix": 0, "totalPrefix": 0}
+    else:
+        json_file = "{}/r2/bgp_l2vpn_evpn_routes_no_local.json".format(CWD)
+        expected = json.loads(open(json_file).read())
     test_func = partial(
         topotest.router_json_cmp,
         router,
@@ -388,17 +394,25 @@ def test_evpn_reconfigure_evpn():
         pytest.skip(tgen.errors)
     logger.info("==== r2, reconfigure advertise-all-vni from BGP EVPN instance")
     router = tgen.gears["r2"]
-    vrf_to_write = f" vrf {R2_VRF_UNDERLAY}" if R2_VRF_UNDERLAY else ""
-    router.vtysh_cmd(
-        f"""
-        configure terminal
-        router bgp 65000{vrf_to_write}
-        address-family l2vpn evpn
-        advertise-all-vni
-        exit-address-family
-        exit
+    if R2_VRF_UNDERLAY:
+        # we first have to unconfigure EVPN in the default VRF to move it to another VRF
+        cmd = f"""
+        conf t
+          router bgp 65000
+            address-family l2vpn evpn
+              no advertise-all-vni
+          router bgp 65000 vrf {R2_VRF_UNDERLAY}
+            address-family l2vpn evpn
+              advertise-all-vni
         """
-    )
+    else:
+        cmd = f"""
+        conf t
+          router bgp 65000
+            address-family l2vpn evpn
+              advertise-all-vni
+        """
+    router.vtysh_cmd(cmd)
     json_file = "{}/r2/bgp_l2vpn_evpn_routes.json".format(CWD)
     expected = json.loads(open(json_file).read())
     test_func = partial(