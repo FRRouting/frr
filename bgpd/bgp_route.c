FRRouting/frr

commit 5f8ad4045c2069aded37ca2aa087ee0041890008

bgpd: Show scope as link-local if using LL Next Hop capability
Fixes: db853cc97eafee8742cd391aaa2b5bc58a6751ae ("bgpd: Implement Link-Local Next Hop capability")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_route.c
@@ -9727,6 +9727,9 @@ void route_vty_out(struct vty *vty, const struct prefix *p,
 	char *nexthop_hostname =
 		bgp_nexthop_hostname(path->peer, path->nexthop);
 	char esi_buf[ESI_STR_LEN];
+	bool ll_nexthop_only = attr->mp_nexthop_len == BGP_ATTR_NHLEN_IPV6_GLOBAL &&
+			       !!CHECK_FLAG(path->peer->cap, PEER_CAP_LINK_LOCAL_ADV) &&
+			       !!CHECK_FLAG(path->peer->cap, PEER_CAP_LINK_LOCAL_RCV);
 
 	if (json_paths)
 		json_path = json_object_new_object();
@@ -9953,7 +9956,7 @@ void route_vty_out(struct vty *vty, const struct prefix *p,
 			json_object_string_add(json_nexthop_global, "afi",
 					       "ipv6");
 			json_object_string_add(json_nexthop_global, "scope",
-					       "global");
+					       ll_nexthop_only ? "link-local" : "global");
 			json_object_int_add(json_nexthop_global, "length", attr->mp_nexthop_len);
 
 			/* We display both LL & GL if both have been
@@ -10834,6 +10837,9 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 	struct bgp_path_info *bpi_ultimate =
 		bgp_get_imported_bpi_ultimate(path);
 	struct bgp_route_evpn *bre = bgp_attr_get_evpn_overlay(attr);
+	bool ll_nexthop_only = attr->mp_nexthop_len == BGP_ATTR_NHLEN_IPV6_GLOBAL &&
+			       !!CHECK_FLAG(path->peer->cap, PEER_CAP_LINK_LOCAL_ADV) &&
+			       !!CHECK_FLAG(path->peer->cap, PEER_CAP_LINK_LOCAL_RCV);
 
 	if (json_paths) {
 		json_path = json_object_new_object();
@@ -11088,6 +11094,8 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 					       "ipv4");
 	} else {
 		if (json_paths) {
+			bool ll_nexthop = IN6_IS_ADDR_LINKLOCAL(&attr->mp_nexthop_global);
+
 			json_object_string_addf(json_nexthop_global, "ip",
 						"%pI6",
 						&attr->mp_nexthop_global);
@@ -11100,7 +11108,9 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 			json_object_string_add(json_nexthop_global, "afi",
 					       "ipv6");
 			json_object_string_add(json_nexthop_global, "scope",
-					       "global");
+					       ll_nexthop ? "link-local" : "global");
+			json_object_boolean_add(json_nexthop_global, "linkLocalOnly",
+						ll_nexthop_only);
 			json_object_int_add(json_nexthop_global, "length", attr->mp_nexthop_len);
 		} else {
 			if (nexthop_hostname)

commit 55e15cb30af8e6a380549e3eae47f4bbf90864f6

bgpd: Show `linkLocalOnly` JSON only if it's really link-local next-hop
Fixes: 5f8ad4045c2069aded37ca2aa087ee0041890008 ("bgpd: Show scope as link-local if using LL Next Hop capability")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_route.c
@@ -10387,7 +10387,7 @@ void route_vty_out(struct vty *vty, const struct prefix *p,
 			json_object_string_add(json_nexthop_global, "scope",
 					       ll_nexthop ? "link-local" : "global");
 			json_object_boolean_add(json_nexthop_global, "linkLocalOnly",
-						ll_nexthop_only);
+						ll_nexthop && ll_nexthop_only);
 			json_object_int_add(json_nexthop_global, "length", attr->mp_nexthop_len);
 
 			/* We display both LL & GL if both have been
@@ -11544,7 +11544,7 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 			json_object_string_add(json_nexthop_global, "scope",
 					       ll_nexthop ? "link-local" : "global");
 			json_object_boolean_add(json_nexthop_global, "linkLocalOnly",
-						ll_nexthop_only);
+						ll_nexthop && ll_nexthop_only);
 			json_object_int_add(json_nexthop_global, "length", attr->mp_nexthop_len);
 		} else {
 			if (nexthop_hostname)