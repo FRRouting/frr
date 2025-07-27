FRRouting/frr

commit 95d9a338f28ccda62b6c5cfafc45c5288e88ef35

bgpd: Do not send link-local address which is empty (::)
When next-hop length is set to 16 bytes, and link-local capability is
negotiated, we should send only link-local address and don't send GUA or an
empty one (::).

This was spotted using a datacenter profile.

Fixes: 0050f82b8db8d0ea2775fd1fd0b9aadab6844453 ("bgpd: Send LL only if LL Next Hop capability is negotiated")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_updgrp_packet.c
@@ -537,7 +537,7 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 		}
 
 		if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL_AND_LL ||
-		    nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL || ll_nexthop_only) {
+		    nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL) {
 			stream_get_from(&v6nhlocal, s, offset_nhlocal,
 					IPV6_MAX_BYTELEN);
 			if (IN6_IS_ADDR_UNSPECIFIED(&v6nhlocal)) {
@@ -546,7 +546,14 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 			}
 		}
 
-		if (lnh_modified && ll_nexthop_only) {
+		/* If link-local next-hop capability is negotiated, then
+		 * we have to ensure that the link-local next-hop is set
+		 * to the peer's link-local address, and not the `::`.
+		 * Here it comes as nhlen == 16 (not 32).
+		 */
+		if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL &&
+		    IN6_IS_ADDR_LINKLOCAL(&peer->nexthop.v6_local) && ll_nexthop_only) {
+			mod_v6nhl = &peer->nexthop.v6_local;
 			stream_put_in6_addr_at(s, offset_nhlocal, mod_v6nhl);
 		} else {
 			if (gnh_modified)
@@ -557,7 +564,7 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 
 		if (bgp_debug_update(peer, NULL, NULL, 0)) {
 			if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL_AND_LL ||
-			    nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL || ll_nexthop_only)
+			    nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL)
 				zlog_debug(
 					"u%" PRIu64 ":s%" PRIu64
 					" %s send UPDATE w/ mp_nexthops %pI6, %pI6%s",
@@ -567,6 +574,11 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 					(nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL
 						 ? " and RD"
 						 : ""));
+			else if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL && ll_nexthop_only)
+				zlog_debug("u%" PRIu64 ":s%" PRIu64
+					   " %s send UPDATE w/ mp_nexthop (link-local only) %pI6",
+					   PAF_SUBGRP(paf)->update_group->id, PAF_SUBGRP(paf)->id,
+					   peer->host, mod_v6nhl);
 			else
 				zlog_debug(
 					"u%" PRIu64 ":s%" PRIu64

commit 244397af9733dafcc5ee63650d602dcd791152bd

bgpd: Fix logging when IPv6 Link-Local only next-hop is used
Fixes: 95d9a338f28ccda62b6c5cfafc45c5288e88ef35 ("bgpd: Do not send link-local address which is empty (::)")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_updgrp_packet.c
@@ -551,8 +551,11 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 		 * to the peer's link-local address, and not the `::`.
 		 * Here it comes as nhlen == 16 (not 32).
 		 */
-		if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL &&
-		    IN6_IS_ADDR_LINKLOCAL(&peer->nexthop.v6_local) && ll_nexthop_only) {
+		bool use_ll_nexthop_only = (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL &&
+					    IN6_IS_ADDR_LINKLOCAL(&peer->nexthop.v6_local) &&
+					    ll_nexthop_only);
+
+		if (use_ll_nexthop_only) {
 			mod_v6nhl = &peer->nexthop.v6_local;
 			stream_put_in6_addr_at(s, offset_nhlocal, mod_v6nhl);
 		} else {
@@ -574,7 +577,7 @@ struct stream *bpacket_reformat_for_peer(struct bpacket *pkt,
 					(nhlen == BGP_ATTR_NHLEN_VPNV6_GLOBAL_AND_LL
 						 ? " and RD"
 						 : ""));
-			else if (nhlen == BGP_ATTR_NHLEN_IPV6_GLOBAL && ll_nexthop_only)
+			else if (use_ll_nexthop_only)
 				zlog_debug("u%" PRIu64 ":s%" PRIu64
 					   " %s send UPDATE w/ mp_nexthop (link-local only) %pI6",
 					   PAF_SUBGRP(paf)->update_group->id, PAF_SUBGRP(paf)->id,