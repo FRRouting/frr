858d39988e352fe59cacc53bc28bf6948db3b15e
Loïc Sang <loic.sang@6wind.com>
2025-08-07 12:06:51 +0200

    bgpd: move bgp_attr_srv6_l3vpn to bgp_attr_srv6.h
    Move bgp_attr_srv6_l3vpn to bgp_attr_srv6.h and rename it to
    bgp_attr_srv6_l3service.
    
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>

--- bgpd/rfapi/rfapi_vty.c
+++ bgpd/rfapi/rfapi_vty.c
@@ -421,19 +421,18 @@ void rfapi_vty_out_vncinfo(struct vty *vty, const struct prefix *p,
 				decode_label(&bpi->extra->labels->label[0]));
 	}
 
-	if (bpi->attr->srv6_l3vpn || bpi->attr->srv6_vpn) {
-		struct in6_addr *sid_tmp =
-			bpi->attr->srv6_l3vpn ? (&bpi->attr->srv6_l3vpn->sid)
-					      : (&bpi->attr->srv6_vpn->sid);
+	if (bpi->attr->srv6_l3service || bpi->attr->srv6_vpn) {
+		struct in6_addr *sid_tmp = bpi->attr->srv6_l3service
+						   ? (&bpi->attr->srv6_l3service->sid)
+						   : (&bpi->attr->srv6_vpn->sid);
 		vty_out(vty, " sid=%pI6", sid_tmp);
 
-		if (bpi->attr->srv6_l3vpn &&
-		    bpi->attr->srv6_l3vpn->loc_block_len != 0) {
+		if (bpi->attr->srv6_l3service && bpi->attr->srv6_l3service->loc_block_len != 0) {
 			vty_out(vty, " sid_structure=[%d,%d,%d,%d]",
-				bpi->attr->srv6_l3vpn->loc_block_len,
-				bpi->attr->srv6_l3vpn->loc_node_len,
-				bpi->attr->srv6_l3vpn->func_len,
-				bpi->attr->srv6_l3vpn->arg_len);
+				bpi->attr->srv6_l3service->loc_block_len,
+				bpi->attr->srv6_l3service->loc_node_len,
+				bpi->attr->srv6_l3service->func_len,
+				bpi->attr->srv6_l3service->arg_len);
 		}
 	}
 
