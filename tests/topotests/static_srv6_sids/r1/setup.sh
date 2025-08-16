eceafe4973f92a5d208f995c49e70195d78bb79c
Carmine Scarpitta <cscarpit@cisco.com>
2025-08-16 08:03:59 +0000

    tests: Verify SRv6 SID installation for default VRF in staticd
    Add an SRv6 SID for the default VRF to the FRR config and verify the SID
    is installed in the RIB.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- tests/topotests/static_srv6_sids/r1/setup.sh
+++ tests/topotests/static_srv6_sids/r1/setup.sh
@@ -10,4 +10,9 @@ ip link set Vrf20 up
 ip link add Vrf30 type vrf table 30
 ip link set Vrf30 up
 
+# VRF associated with main routing table,
+# required for SRv6 uDT4/uDT46 SIDs
+ip link add vrfdefault type vrf table main
+ip link set vrfdefault up
+
 sysctl -w net.vrf.strict_mode=1
