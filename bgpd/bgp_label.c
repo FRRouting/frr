e85876c36d115669b7c64a03cabe490af53d8cc9
Donald Sharp <sharpd@nvidia.com>
2025-08-21 13:29:08 -0400

    bgpd: Release label from dest copy once freed
    When bgp is using a labelpool and is registering
    to use a label, it requests one from the pool and then
    stores the label directly on the dest for easier
    lookup.  When a clear is issued( or really ) anything
    that would cause the dest to have no path_info's anymore
    the label was released to the label pool, but the label
    value was still being stored on the dest.  Thus when
    a new path_info is received the labelpool code would
    notice that the dest has a label attached to it already
    and reuse that one again.
    
    Modify the code such that when a label is released
    to the labelpool again the copy that is on the dest
    is also cleared.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_label.c
+++ bgpd/bgp_label.c
@@ -408,6 +408,7 @@ void bgp_reg_dereg_for_label(struct bgp_dest *dest, struct bgp_path_info *pi,
 	} else {
 		UNSET_FLAG(dest->flags, BGP_NODE_LABEL_REQUESTED);
 		bgp_lp_release(LP_TYPE_BGP_LU, dest, label);
+		bgp_unset_valid_label(&dest->local_label);
 	}
 
 	bgp_send_fec_register_label_msg(
