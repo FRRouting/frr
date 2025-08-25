bca12a0e4e3a11223868b918f422a9a4c1136c02
Loïc Sang <loic.sang@6wind.com>
2025-08-25 10:21:19 +0200

    bgpd: register route for srv6 unicast
    Register/unregister route for srv6 unicast.
    
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>

--- bgpd/bgp_srv6.c
+++ bgpd/bgp_srv6.c
@@ -199,3 +199,79 @@ void bgp_srv6_unicast_sid_update(struct bgp *bgp, afi_t afi)
 	bgp_srv6_unicast_sid_endpoint(bgp, afi, ifp, true);
 }
 
+void bgp_srv6_unicast_unregister_route(struct bgp_dest *dest)
+{
+	XFREE(MTYPE_BGP_SRV6_L3SERVICE, dest->srv6_unicast);
+	dest->srv6_unicast = NULL;
+}
+
+void bgp_srv6_unicast_register_route(struct bgp *bgp, afi_t afi, struct bgp_dest *dest,
+				     struct bgp_path_info *bpi)
+{
+	struct attr attr_tmp;
+	const struct prefix *p;
+	struct route_map *rmap;
+	route_map_result_t ret;
+	struct bgp_path_info info;
+	struct srv6_locator *locator;
+
+	if (!bpi) {
+		if (dest->srv6_unicast)
+			bgp_srv6_unicast_unregister_route(dest);
+
+		return;
+	}
+
+	if (bpi->attr->srv6_l3service)
+		return;
+
+	if (!bgp->srv6_unicast[afi].sid_locator)
+		return;
+
+	if (bgp->srv6_unicast[afi].rmap_name) {
+		rmap = route_map_lookup_by_name(bgp->srv6_unicast[afi].rmap_name);
+		if (rmap) {
+			attr_tmp = *bpi->attr;
+			info.attr = &attr_tmp;
+			info.peer = bgp->peer_self;
+			memset(&info, 0, sizeof(info));
+			p = bgp_dest_get_prefix(bpi->net);
+
+			ret = route_map_apply(rmap, p, &info);
+
+			if (ret == RMAP_DENYMATCH) {
+				if (dest->srv6_unicast)
+					bgp_srv6_unicast_unregister_route(dest);
+
+				if (BGP_DEBUG(update, UPDATE_OUT))
+					zlog_debug("srv6 unicast prefix %pBD denied", dest);
+
+				return;
+			}
+		} else {
+			zlog_warn("route-map %s was no found, ignored",
+				  bgp->srv6_unicast[afi].rmap_name);
+		}
+	}
+
+	if (dest->srv6_unicast && sid_same(bgp->srv6_unicast[afi].sid, &dest->srv6_unicast->sid))
+		return;
+
+	locator = bgp->srv6_unicast[afi].sid_locator;
+	dest->srv6_unicast = XCALLOC(MTYPE_BGP_SRV6_L3SERVICE,
+				     sizeof(struct bgp_attr_srv6_l3service));
+	dest->srv6_unicast->sid_flags = 0x00;
+	dest->srv6_unicast->endpoint_behavior =
+			afi == AFI_IP ? (CHECK_FLAG(locator->flags, SRV6_LOCATOR_USID)
+						 ? SRV6_ENDPOINT_BEHAVIOR_END_DT4_USID
+						 : SRV6_ENDPOINT_BEHAVIOR_END_DT4)
+				      : (CHECK_FLAG(locator->flags, SRV6_LOCATOR_USID)
+						 ? SRV6_ENDPOINT_BEHAVIOR_END_DT6_USID
+						 : SRV6_ENDPOINT_BEHAVIOR_END_DT6);
+	dest->srv6_unicast->loc_block_len = locator->block_bits_length;
+	dest->srv6_unicast->loc_node_len = locator->node_bits_length;
+	dest->srv6_unicast->func_len = locator->function_bits_length;
+	dest->srv6_unicast->arg_len = locator->argument_bits_length;
+	memcpy(&dest->srv6_unicast->sid, bgp->srv6_unicast[afi].sid,
+	       sizeof(struct in6_addr));
+}
