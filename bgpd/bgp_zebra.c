c8828b0876427fbea0a301775deacf088c2abd50
Manpreet Kaur <manpreetk@nvidia.com>
2025-09-10 02:53:02 -0700

    bgpd: EVPNv6 bgp TP v4_v6 aware
    Testing:
    ```
    2025-09-18T11:09:40.964 frr_bgp:evpn_local_vni_add_zrecv {'vni': 104001, 'vtep': '2006:20:20::2', 'mc_grp': '0.0.}
    2025-09-18T11:09:40.965 frr_bgp:evpn_local_l3vni_add_zrecv {'vni': 104001, 'vrf': 12, 'svi_rmac': '00:01:00:00:00,
        'vrr_rmac': '00:01:00:00:02:08', 'vtep': '2006:20:20::2', 'filter': 0, 'svi_ifindex': 21, 'anycast_mac': 'no'}
    ```
    
    Signed-off-by: Manpreet Kaur <manpreetk@nvidia.com>

--- bgpd/bgp_zebra.c
+++ bgpd/bgp_zebra.c
@@ -3290,9 +3290,8 @@ static int bgp_zebra_process_local_l3vni(ZAPI_CALLBACK_ARGS)
 				   filter ? "prefix-routes-only" : "none",
 				   svi_ifindex);
 
-		frrtrace(8, frr_bgp, evpn_local_l3vni_add_zrecv, l3vni, vrf_id,
-			 &svi_rmac, &vrr_rmac, filter, originator_ip,
-			 svi_ifindex, is_anycast_mac);
+		frrtrace(8, frr_bgp, evpn_local_l3vni_add_zrecv, l3vni, vrf_id, &svi_rmac,
+			 &vrr_rmac, filter, &originator_ip, svi_ifindex, is_anycast_mac);
 
 		bgp_evpn_local_l3vni_add(l3vni, vrf_id, &svi_rmac, &vrr_rmac,
 					 &originator_ip, filter, svi_ifindex,
@@ -3350,8 +3349,8 @@ static int bgp_zebra_process_local_vni(ZAPI_CALLBACK_ARGS)
 	}
 
 	if (cmd == ZEBRA_VNI_ADD) {
-		frrtrace(4, frr_bgp, evpn_local_vni_add_zrecv, vni, vtep_ip,
-			 tenant_vrf_id, mcast_grp);
+		frrtrace(4, frr_bgp, evpn_local_vni_add_zrecv, vni, &vtep_ip, tenant_vrf_id,
+			 mcast_grp);
 
 		return bgp_evpn_local_vni_add(
 			bgp, vni,
