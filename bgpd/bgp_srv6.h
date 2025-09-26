bd6a4a437220ae2666183522fbd3d541e3ffac17
Loïc Sang <loic.sang@6wind.com>
2025-09-26 11:21:08 +0200

    bgpd: add srv6 unicast per-vrf support
    Introduce the "sid per-vrf export ..." command to support srv6 unicast.
    This installs a DT46 action for both IPv4 and IPv6 address families.
    
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>

--- bgpd/bgp_srv6.h
+++ bgpd/bgp_srv6.h
@@ -8,9 +8,20 @@
 #ifndef _BGP_SRV6_H_
 #define _BGP_SRV6_H_
 
+static inline bool is_srv6_unicast_vrf_enabled(struct bgp *bgp)
+{
+	if (CHECK_FLAG(bgp->vrf_flags, BGP_CONFIG_SRV6_UNICAST_SID_AUTO) ||
+	    bgp->srv6_unicast_vrf.sid_explicit || bgp->srv6_unicast_vrf.sid_index)
+		return true;
+
+	return false;
+}
+
 static inline bool is_srv6_unicast_afi_enabled(struct bgp *bgp, afi_t afi)
 {
-	if (CHECK_FLAG(bgp->af_flags[afi][SAFI_UNICAST], BGP_CONFIG_SRV6_UNICAST_SID_AUTO)
+	if (afi == AFI_UNSPEC && is_srv6_unicast_vrf_enabled(bgp))
+		return true;
+	else if (CHECK_FLAG(bgp->af_flags[afi][SAFI_UNICAST], BGP_CONFIG_SRV6_UNICAST_SID_AUTO)
 	    || bgp->srv6_unicast[afi].sid_explicit || bgp->srv6_unicast[afi].sid_index)
 		return true;
 
@@ -19,6 +30,9 @@ static inline bool is_srv6_unicast_afi_enabled(struct bgp *bgp, afi_t afi)
 
 static inline bool is_srv6_unicast_enabled(struct bgp *bgp)
 {
+	if (is_srv6_unicast_vrf_enabled(bgp))
+		return true;
+
 	if (is_srv6_unicast_afi_enabled(bgp, AFI_IP) ||
 	    is_srv6_unicast_afi_enabled(bgp, AFI_IP6))
 		return true;
@@ -29,6 +43,7 @@ static inline bool is_srv6_unicast_enabled(struct bgp *bgp)
 int bgp_srv6_configure(struct vty *vty, struct bgp *bgp, afi_t afi, bool sid_auto,
 			   uint32_t sid_idx, bool sid_explicit,
 			   struct in6_addr sid_value, const char *rmap_str, bool no);
+struct srv6_policy *get_srv6_policy(struct bgp *bgp, afi_t afi);
 void bgp_srv6_unicast_ensure_afi_sid(struct bgp *bgp, afi_t afi);
 void bgp_srv6_unicast_sid_withdraw(struct bgp *bgp, afi_t afi);
 void bgp_srv6_unicast_sid_update(struct bgp *bgp, afi_t afi);
@@ -37,7 +52,7 @@ void bgp_srv6_unicast_sid_endpoint(struct bgp *bgp, afi_t afi,
 				   struct interface *ifp, bool install);
 void bgp_srv6_unicast_unregister_route(struct bgp_dest *dest);
 void bgp_srv6_unicast_register_route(struct bgp *bgp, afi_t afi, struct bgp_dest *dest,
-				     struct bgp_path_info *bpi);
+				     struct srv6_policy *srv6_policy, struct bgp_path_info *bpi);
 void bgp_srv6_unicast_announce(struct bgp *bgp, afi_t afi);
 void bgp_srv6_unicast_withdraw(struct bgp *bgp, afi_t afi);
 
