da8f139294bbe6fbb057b67b2cf8fd4d63cbb416
Donald Sharp <sharpd@nvidia.com>
2025-10-01 08:48:46 -0400

    lib, zebra: vrf_configure_backend return code is never used
    Let's remove the vrf_configure_backend return code as that it is never
    used.   Additionally when sending the vrf_backend_type via zapi
    use GETC/putc to encode it directly to the enum type.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- lib/vrf.h
+++ lib/vrf.h
@@ -293,7 +293,7 @@ extern void vrf_install_commands(void);
 /*
  * API for configuring VRF backend
  */
-extern int vrf_configure_backend(enum vrf_backend_type backend);
+extern void vrf_configure_backend(enum vrf_backend_type backend);
 extern enum vrf_backend_type vrf_get_backend(void);
 extern int vrf_is_backend_netns(void);
 
