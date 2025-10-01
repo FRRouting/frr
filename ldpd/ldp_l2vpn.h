c5d56f45dfb8d1769acf7c1e180947aec8f53d61
Philippe Guibert <philippe.guibert@6wind.com>
2025-10-01 13:36:02 +0200

    yang, ldpd: add ldp l2vpn yang data model
    The l2vpn entries configured in LDP are a sub-type of the so called
    l2vpn components described in ch.3.1 of RFC4664. The series of commits
    aims at extracting a data model, first from LDP, then extending the
    model, in order to apply the L2VPN model to other instances like BGP.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

---
+++ ldpd/ldp_l2vpn.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Route map function.
+ * Copyright (C) 1998 Kunihiro Ishiguro
+ */
+
+#ifndef _LDP_L2VPN_H
+#define _LDP_L2VPN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct frr_yang_module_info frr_ldp_l2vpn;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LDP_L2VPN_H */
