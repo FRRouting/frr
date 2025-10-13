ef88662651e0e19db31b04432cf331b821f321bd
Donald Sharp <sharpd@nvidia.com>
2025-10-13 16:32:55 +0300

    bgpd: Do not override a specified rd
    If the router-id is learned after a bgp vrf has
    already received a specified rd, it will auto-derive
    a value and override the specified on the command line.
    
    Do not let this happen.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -9809,10 +9809,12 @@ DEFPY (af_rd_vpn_export,
 		bgp->vpn_policy[afi].tovpn_rd = prd;
 		SET_FLAG(bgp->vpn_policy[afi].flags,
 			 BGP_VPN_POLICY_TOVPN_RD_SET);
+		SET_FLAG(bgp->vpn_policy[afi].flags, BGP_VPN_POLICY_TOVPN_RD_CLI_SET);
 	} else {
 		XFREE(MTYPE_BGP_NAME, bgp->vpn_policy[afi].tovpn_rd_pretty);
 		UNSET_FLAG(bgp->vpn_policy[afi].flags,
 			   BGP_VPN_POLICY_TOVPN_RD_SET);
+		UNSET_FLAG(bgp->vpn_policy[afi].flags, BGP_VPN_POLICY_TOVPN_RD_CLI_SET);
 	}
 
 	/* post-change: re-export vpn routes */
