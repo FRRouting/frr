004f29d6217fa92b62f8ca99ac484072d61fb8c9
Donald Sharp <sharpd@nvidia.com>
2025-10-14 20:13:47 -0400

    tests: clear_mroute_verify was always waiting 125 seconds
    The function was not:
    
    a) checking the right key for the oil
    b) Stopping once it found the mroute back in the way it should be.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    (cherry picked from commit 6f7803b94a68474688f134d874326835752b999a)

--- tests/topotests/lib/pim.py
+++ tests/topotests/lib/pim.py
@@ -2512,7 +2512,7 @@ def clear_mroute_verify(tgen, dut, expected=True):
     # RFC 3376: 8.2. Query Interval - Default: 125 seconds
     # So waiting for maximum 130 sec to get the igmp report
     for _ in range(1, 26):
-        logger.info("[DUT: %s]: Waiting for 2 sec for mroutes" " to come up", dut)
+        logger.info("[DUT: %s]: Waiting for 5 sec for mroutes to come up", dut)
         sleep(5)
         keys_json1 = mroute_json_1.keys()
         mroute_json_2 = run_frr_cmd(rnode, "show ip mroute json", isjson=True)
@@ -2520,20 +2520,27 @@ def clear_mroute_verify(tgen, dut, expected=True):
         if bool(mroute_json_2):
             keys_json2 = mroute_json_2.keys()
 
+            all_groups_ready = True
             for group in mroute_json_2.keys():
-                flag = False
+                group_ready = False
                 for key in mroute_json_2[group].keys():
-                    if "oil" not in mroute_json_2[group]:
+                    if "oil" not in mroute_json_2[group][key]:
                         continue
 
                     for _key, _value in mroute_json_2[group][key]["oil"].items():
                         if _key != "pimreg" and keys_json1 == keys_json2:
-                            flag = True
+                            group_ready = True
                             break
-            if flag:
+                    if group_ready:
+                        break
+
+                if not group_ready:
+                    all_groups_ready = False
+                    break
+
+            if all_groups_ready:
+                logger.info("[DUT: %s]: Mroutes repopulated, exiting early!", dut)
                 break
-            else:
-                continue
 
     for group in mroute_json_2.keys():
         mroute_after_clear[group] = {}
