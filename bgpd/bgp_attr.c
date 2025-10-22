FRRouting/frr

commit d311a698deb84cf249fc1f258ce639d48565b110

bgpd: Implement BGP Next Hop Dependent Characteristics Attribute
https://datatracker.ietf.org/doc/html/draft-ietf-idr-entropy-label-16
https://datatracker.ietf.org/doc/html/draft-wang-idr-next-next-hop-nodes-02

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_nhc.c
@@ -1,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright 2025, Donatas Abraitis <donatas@opensourcerouting.org>
+ */
+
+#include "bgp_nhc.h"
+
+void bgp_nhc_tlv_add(struct bgp_nhc *nhc, struct bgp_nhc_tlv *tlv)
+{
+	struct bgp_nhc_tlv *last;
+
+	if (!tlv)
+		return;
+
+	for (last = nhc->tlvs; last && last->next; last = last->next)
+		;
+
+	if (last)
+		last->next = tlv;
+	else
+		nhc->tlvs = tlv;
+
+	nhc->tlvs_length += tlv->length + BGP_NHC_TLV_MIN_LEN;
+}
+
+struct bgp_nhc_tlv *bgp_nhc_tlv_find(struct bgp_nhc *nhc, uint16_t code)
+{
+	struct bgp_nhc_tlv *tlv = NULL;
+
+	if (!nhc)
+		return tlv;
+
+	for (tlv = nhc->tlvs; tlv; tlv = tlv->next) {
+		if (tlv->code == code)
+			return tlv;
+	}
+
+	return tlv;
+}
+
+void bgp_nhc_tlv_free(struct bgp_nhc_tlv *tlv)
+{
+	if (!tlv)
+		return;
+
+	if (tlv->value)
+		XFREE(MTYPE_BGP_NHC_TLV_VAL, tlv->value);
+
+	XFREE(MTYPE_BGP_NHC_TLV, tlv);
+}
+
+void bgp_nhc_tlvs_free(struct bgp_nhc_tlv *tlv)
+{
+	struct bgp_nhc_tlv *next;
+
+	while (tlv) {
+		next = tlv->next;
+		bgp_nhc_tlv_free(tlv);
+		tlv = next;
+	}
+}
+
+void bgp_nhc_free(struct bgp_nhc *nhc)
+{
+	bgp_nhc_tlvs_free(nhc->tlvs);
+	XFREE(MTYPE_BGP_NHC, nhc);
+}

bgpd/bgp_nhc.h
@@ -1,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright 2025, Donatas Abraitis <donatas@opensourcerouting.org>
+ */
+
+#ifndef _FRR_BGP_NHC_H
+#define _FRR_BGP_NHC_H
+
+#include "zebra.h"
+#include <bgpd/bgpd.h>
+#include <bgpd/bgp_attr.h>
+
+struct bgp_nhc_tlv {
+	struct bgp_nhc_tlv *next;
+	uint16_t code;
+	uint16_t length;
+	uint8_t *value;
+};
+
+struct bgp_nhc {
+	unsigned long refcnt;
+	uint16_t afi;
+	uint8_t safi;
+	uint8_t nh_length;
+	struct in_addr nh_ipv4;
+	struct in6_addr nh_ipv6;
+	uint16_t tlvs_length;
+	struct bgp_nhc_tlv *tlvs;
+};
+
+/* 4 => Characteristic Code + Characteristic Length */
+#define BGP_NHC_TLV_MIN_LEN sizeof(uint16_t) + sizeof(uint16_t)
+/*
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |   Address Family Identifier   |     SAFI      | Next Hop Len  |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * ~             Network Address of Next Hop (variable)            ~
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |      Characteristic Code      |      Characteristic Length    |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * ~                Characteristic Value (variable)                ~
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+#define BGP_NHC_MIN_LEN	     12
+#define BGP_NHC_MIN_IPV6_LEN 24
+
+/* TLV values: */
+/* draft-wang-idr-next-next-hop-nodes */
+#define BGP_ATTR_NHC_TLV_NNHN 2
+
+extern void bgp_nhc_tlv_add(struct bgp_nhc *nhc, struct bgp_nhc_tlv *tlv);
+extern struct bgp_nhc_tlv *bgp_nhc_tlv_find(struct bgp_nhc *nhc, uint16_t code);
+extern void bgp_nhc_tlv_free(struct bgp_nhc_tlv *tlv);
+extern void bgp_nhc_tlvs_free(struct bgp_nhc_tlv *tlv);
+extern void bgp_nhc_free(struct bgp_nhc *bnc);
+
+#endif /* _FRR_BGP_NHC_H */

bgpd/bgp_route.c
@@ -31,6 +31,7 @@
 #include "frrdistance.h"
 
 #include "bgpd/bgpd.h"
+#include "bgpd/bgp_nhc.h"
 #include "bgpd/bgp_table.h"
 #include "bgpd/bgp_route.h"
 #include "bgpd/bgp_attr.h"
@@ -12270,6 +12271,40 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 				path, path->extra, attr);
 			vty_out(vty, "      flags net: 0x%u, path: 0x%u, attr: 0x%" PRIu64 "\n",
 				path->net->flags, path->flags, attr->flag);
+		}
+	}
+
+	/* Display NHC attributes */
+	if (CHECK_FLAG(attr->flag, ATTR_FLAG_BIT(BGP_ATTR_NHC))) {
+		struct bgp_nhc *nhc = bgp_attr_get_nhc(attr);
+		struct bgp_nhc_tlv *tlv = NULL;
+
+		if (nhc) {
+			for (tlv = nhc->tlvs; tlv; tlv = tlv->next) {
+				if (tlv->code == BGP_ATTR_NHC_TLV_NNHN) {
+					json_object *json_nhc_nnhn = NULL;
+
+					if (!json_paths)
+						vty_out(vty, "      Next-next Hop Nodes:\n");
+					else
+						json_nhc_nnhn = json_object_new_array();
+
+					for (i = 0; i < tlv->length; i += IPV4_MAX_BYTELEN) {
+						if (!json_paths) {
+							vty_out(vty, "       %pI4\n",
+								(struct in_addr *)&tlv->value[i]);
+						} else {
+							json_array_string_addf(json_nhc_nnhn, "%pI4",
+									       (struct in_addr *)&tlv
+										       ->value[i]);
+						}
+					}
+
+					if (json_paths)
+						json_object_object_add(json_path, "nextNextHopNodes",
+								       json_nhc_nnhn);
+				}
+			}
 		}
 	}
 

bgpd/bgp_attr.h
@@ -318,6 +318,9 @@ struct attr {
 
 	/* AIGP Metric */
 	uint64_t aigp_metric;
+
+	/* Next-hop characteristics */
+	struct bgp_nhc *nhc;
 };
 
 /* rmap_change_flags definition */
@@ -391,7 +394,7 @@ extern bgp_size_t bgp_packet_attribute(struct bgp *bgp, struct peer *peer, struc
 				       struct prefix *p, afi_t afi, safi_t safi, struct peer *from,
 				       struct prefix_rd *prd, mpls_label_t *label,
 				       uint8_t num_labels, bool addpath_capable,
-				       uint32_t addpath_tx_id);
+				       uint32_t addpath_tx_id, struct bgp_path_info *bpi);
 extern void bgp_dump_routes_attr(struct stream *s, struct bgp_path_info *bpi,
 				 const struct prefix *p);
 extern bool attrhash_cmp(const void *arg1, const void *arg2);
@@ -582,6 +585,21 @@ static inline void bgp_attr_set_transit(struct attr *attr,
 					struct transit *transit)
 {
 	attr->transit = transit;
+}
+
+static inline struct bgp_nhc *bgp_attr_get_nhc(const struct attr *attr)
+{
+	return attr->nhc;
+}
+
+static inline void bgp_attr_set_nhc(struct attr *attr, struct bgp_nhc *bnc)
+{
+	attr->nhc = bnc;
+
+	if (bnc)
+		SET_FLAG(attr->flag, ATTR_FLAG_BIT(BGP_ATTR_NHC));
+	else
+		UNSET_FLAG(attr->flag, ATTR_FLAG_BIT(BGP_ATTR_NHC));
 }
 
 #define AIGP_TRANSMIT_ALLOWED(peer)                                                                \

bgpd/bgp_bmp.c
@@ -1139,7 +1139,7 @@ static struct stream *bmp_update(const struct prefix *p, struct prefix_rd *prd,
 
 	/* 5: Encode all the attributes, except MP_REACH_NLRI attr. */
 	total_attr_len = bgp_packet_attribute(NULL, peer, s, attr, &vecarr, NULL, afi, safi, peer,
-					      NULL, NULL, 0, 0, 0);
+					      NULL, NULL, 0, 0, 0, NULL);
 
 	/* space check? */
 

bgpd/bgp_debug.c
@@ -35,6 +35,7 @@
 #include "bgpd/bgp_vty.h"
 #include "bgpd/bgp_flowspec.h"
 #include "bgpd/bgp_packet.h"
+#include "bgpd/bgp_nhc.h"
 
 #include "bgpd/bgp_debug_clippy.c"
 
@@ -495,6 +496,16 @@ bool bgp_dump_attr(struct attr *attr, char *buf, size_t size)
 		if (attr->label_index != BGP_INVALID_LABEL_INDEX)
 			snprintf(buf + strlen(buf), size - strlen(buf),
 				 ", label-index %u", attr->label_index);
+	}
+
+	if (CHECK_FLAG(attr->flag, ATTR_FLAG_BIT(BGP_ATTR_NHC))) {
+		struct bgp_nhc_tlv *tlv;
+		struct bgp_nhc *nhc = bgp_attr_get_nhc(attr);
+
+		for (tlv = nhc->tlvs; tlv; tlv = tlv->next)
+			snprintf(buf + strlen(buf), size - strlen(buf),
+				 ", NHC TLV code %d length %d value %p", tlv->code, tlv->length,
+				 tlv->value);
 	}
 
 	if (strlen(buf) > 1)

bgpd/bgp_memory.c
@@ -137,3 +137,7 @@ DEFINE_MTYPE(BGPD, BGP_NOTIFICATION, "BGP Notification Message");
 DEFINE_MTYPE(BGPD, BGP_SOFT_VERSION, "Software Version");
 
 DEFINE_MTYPE(BGPD, BGP_EVPN_OVERLAY, "BGP EVPN Overlay");
+
+DEFINE_MTYPE(BGPD, BGP_NHC, "BGP NHC");
+DEFINE_MTYPE(BGPD, BGP_NHC_TLV, "BGP NHC TLV");
+DEFINE_MTYPE(BGPD, BGP_NHC_TLV_VAL, "BGP NHC TLV value");

commit a6244e56f00c0050b6ec675e66f954f765d3d43f

bgpd: Put local BGP ID when sending NNHN TLV for NH characteristic
draft-ietf-idr-next-next-hop-nodes (adopted by IDR WG) defines that local BGP ID
should be sent as well before encoding Next-next BGP IDs.

Fixes: d311a698de ("bgpd: Implement BGP Next Hop Dependent Characteristics Attribute")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_attr.c
@@ -3767,7 +3767,7 @@ static int bgp_attr_nhc(struct bgp_attr_parser_args *args)
 			zlog_debug("%pBP rcvd BGP NHC TLV code %d, length %d, value %p", peer,
 				   tlv->code, tlv->length, tlv->value);
 
-		/* draft-wang-idr-next-next-hop-nodes */
+		/* draft-ietf-idr-next-next-hop-nodes-00 */
 		if (tlv->code == BGP_ATTR_NHC_TLV_NNHN) {
 			if (tlv->length % IPV4_MAX_BYTELEN != 0) {
 				zlog_err("%pBP rcvd BGP NHC (NNHN TLV) length %d not a multiple of %d",
@@ -4640,6 +4640,7 @@ static void bgp_packet_nhc(struct stream *s, struct peer *peer, afi_t afi, safi_
 	prefix = bgp_dest_get_prefix(bpi->net);
 
 	total = bgp_path_info_mpath_count(bpi) * IPV4_MAX_BYTELEN;
+	total += IPV4_MAX_BYTELEN; /* Next-hop BGP ID */
 
 	stream_putc(s, BGP_ATTR_FLAG_OPTIONAL | BGP_ATTR_FLAG_TRANS);
 	stream_putc(s, BGP_ATTR_NHC);
@@ -4671,12 +4672,22 @@ static void bgp_packet_nhc(struct stream *s, struct peer *peer, afi_t afi, safi_
 	/* Put TLVs */
 
 	/* Begin NNHN TLV */
+	/*
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 * |    Characteristic Code = 2    |Characteristic Length(variable)|
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 * |                    Next-hop BGP ID                            |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 * |               Next-next-hop BGP IDs (variable)                |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	 */
 	if (bgp_path_info_mpath_count(bpi) > 1) {
 		if (bgp_debug_update(peer, NULL, NULL, 1))
 			zlog_debug("%pBP: Sending NHC TLV (%d) for %pFX", peer,
 				   BGP_ATTR_NHC_TLV_NNHN, prefix);
 		stream_putw(s, BGP_ATTR_NHC_TLV_NNHN);
 		stream_putw(s, total);
+		stream_put_ipv4(s, bpi->peer->local_id.s_addr);
 		stream_put_ipv4(s, bpi->peer->remote_id.s_addr);
 
 		for (exists = bgp_path_info_mpath_first(bpi); exists;

bgpd/bgp_nhc.c
@@ -87,8 +87,11 @@ uint64_t bgp_nhc_nnhn_count(struct bgp_nhc *nhc)
 
 	for (tlv = nhc->tlvs; tlv; tlv = tlv->next) {
 		if (tlv->code == BGP_ATTR_NHC_TLV_NNHN) {
-			/* BGP Identifier is always 4-bytes (yet...) */
-			count = tlv->length / IPV4_MAX_BYTELEN;
+			/* BGP Identifier is always 4-bytes (yet...).
+			 * -1 is to exclude the next-hop BGP ID.
+			 * We care only about Next-next hops here.
+			 */
+			count = (tlv->length / IPV4_MAX_BYTELEN) - 1;
 			break;
 		}
 	}

bgpd/bgp_nhc.h
@@ -45,7 +45,7 @@ struct bgp_nhc {
 #define BGP_NHC_MIN_IPV6_LEN 24
 
 /* TLV values: */
-/* draft-wang-idr-next-next-hop-nodes */
+/* draft-ietf-idr-next-next-hop-nodes-00 */
 #define BGP_ATTR_NHC_TLV_NNHN 2
 /* draft-ietf-idr-entropy-label */
 #define BGP_ATTR_NHC_TLV_BGPID 3

bgpd/bgp_route.c
@@ -12660,7 +12660,9 @@ void route_vty_out_detail(struct vty *vty, struct bgp *bgp, struct bgp_dest *bn,
 					else
 						json_nhc_nnhn = json_object_new_array();
 
-					for (i = 0; i < tlv->length; i += IPV4_MAX_BYTELEN) {
+					/* First is Next-hop BGP ID, skip it. */
+					for (i = IPV4_MAX_BYTELEN; i < tlv->length;
+					     i += IPV4_MAX_BYTELEN) {
 						if (!json_paths) {
 							vty_out(vty, "       %pI4\n",
 								(struct in_addr *)&tlv->value[i]);