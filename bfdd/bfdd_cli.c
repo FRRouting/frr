FRRouting/frr

commit 5578a14d949d89e25ec3e6136158603049e5a2dd

python: clidef.py
Adds "DEFPY()" which invokes an additional layer of preprocessing, so
that we get pre-parsed and named function arguments for the CLI.

Signed-off-by: David Lamparter <equinox@opensourcerouting.org>

python/clidef.py
@@ -1,0 +1,254 @@
+# FRR CLI preprocessor (DEFPY)
+#
+# Copyright (C) 2017  David Lamparter for NetDEF, Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; see the file COPYING; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+import clippy, traceback, sys, os
+from collections import OrderedDict
+from functools import reduce
+from pprint import pprint
+from string import Template
+from io import StringIO
+
+# the various handlers generate output C code for a particular type of
+# CLI token, choosing the most useful output C type.
+
+class RenderHandler(object):
+    def __init__(self, token):
+        pass
+    def combine(self, other):
+        if type(self) == type(other):
+            return other
+        return StringHandler(None)
+
+    deref = ''
+    drop_str = False
+
+class StringHandler(RenderHandler):
+    argtype = 'const char *'
+    decl = Template('const char *$varname = NULL;')
+    code = Template('$varname = argv[_i]->arg;')
+    drop_str = True
+
+class LongHandler(RenderHandler):
+    argtype = 'long'
+    decl = Template('long $varname = 0;')
+    code = Template('''\
+char *_end;
+$varname = strtol(argv[_i]->arg, &_end, 10);
+_fail = (_end == argv[_i]->arg) || (*_end != '\\0');''')
+
+# A.B.C.D/M (prefix_ipv4) and
+# X:X::X:X/M (prefix_ipv6) are "compatible" and can merge into a
+# struct prefix:
+
+class PrefixBase(RenderHandler):
+    def combine(self, other):
+        if type(self) == type(other):
+            return other
+        if type(other) in [Prefix4Handler, Prefix6Handler, PrefixGenHandler]:
+            return PrefixGenHandler(None)
+        return StringHandler(None)
+    deref = '&'
+class Prefix4Handler(PrefixBase):
+    argtype = 'const struct prefix_ipv4 *'
+    decl = Template('struct prefix_ipv4 $varname = { };')
+    code = Template('_fail = !str2prefix_ipv4(argv[_i]->arg, &$varname);')
+class Prefix6Handler(PrefixBase):
+    argtype = 'const struct prefix_ipv6 *'
+    decl = Template('struct prefix_ipv6 $varname = { };')
+    code = Template('_fail = !str2prefix_ipv6(argv[_i]->arg, &$varname);')
+class PrefixGenHandler(PrefixBase):
+    argtype = 'const struct prefix *'
+    decl = Template('struct prefix $varname = { };')
+    code = Template('_fail = !str2prefix(argv[_i]->arg, &$varname);')
+
+# same for IP addresses.  result is union sockunion.
+class IPBase(RenderHandler):
+    def combine(self, other):
+        if type(self) == type(other):
+            return other
+        if type(other) in [IP4Handler, IP6Handler, IPGenHandler]:
+            return IPGenHandler(None)
+        return StringHandler(None)
+class IP4Handler(IPBase):
+    argtype = 'struct in_addr'
+    decl = Template('struct in_addr $varname = { INADDR_ANY };')
+    code = Template('_fail = !inet_aton(argv[_i]->arg, &$varname);')
+class IP6Handler(IPBase):
+    argtype = 'struct in6_addr'
+    decl = Template('struct in6_addr $varname = IN6ADDR_ANY_INIT;')
+    code = Template('_fail = !inet_pton(AF_INET6, argv[_i]->arg, &$varname);')
+class IPGenHandler(IPBase):
+    argtype = 'const union sockunion *'
+    decl = Template('''union sockunion s__$varname = { .sa.sa_family = AF_UNSPEC }, *$varname = NULL;''')
+    code = Template('''\
+if (argv[_i]->text[0] == 'X') {
+	s__$varname.sa.sa_family = AF_INET6;
+	_fail = !inet_pton(AF_INET6, argv[_i]->arg, &s__$varname.sin6.sin6_addr);
+	$varname = &s__$varname;
+} else {
+	s__$varname.sa.sa_family = AF_INET;
+	_fail = !inet_aton(argv[_i]->arg, &s__$varname.sin.sin_addr);
+	$varname = &s__$varname;
+}''')
+
+def mix_handlers(handlers):
+    def combine(a, b):
+        if a is None:
+            return b
+        return a.combine(b)
+    return reduce(combine, handlers, None)
+
+handlers = {
+    'WORD_TKN':         StringHandler,
+    'VARIABLE_TKN':     StringHandler,
+    'RANGE_TKN':        LongHandler,
+    'IPV4_TKN':         IP4Handler,
+    'IPV4_PREFIX_TKN':  Prefix4Handler,
+    'IPV6_TKN':         IP6Handler,
+    'IPV6_PREFIX_TKN':  Prefix6Handler,
+}
+
+# core template invoked for each occurence of DEFPY.
+templ = Template('''/* $fnname => "$cmddef" */
+DEFUN_CMD_FUNC_DECL($fnname)
+#define funcdecl_$fnname static int ${fnname}_magic(\\
+	const struct cmd_element *self __attribute__ ((unused)),\\
+	struct vty *vty __attribute__ ((unused)),\\
+	int argc __attribute__ ((unused)),\\
+	struct cmd_token *argv[] __attribute__ ((unused))$argdefs)
+funcdecl_$fnname;
+DEFUN_CMD_FUNC_TEXT($fnname)
+{
+	int _i;
+	unsigned _fail = 0, _failcnt = 0;
+$argdecls
+	for (_i = 0; _i < argc; _i++) {
+		if (!argv[_i]->varname)
+			continue;
+		_fail = 0;$argblocks
+		if (_fail)
+			vty_out (vty, "%% invalid input for %s: %s%s",
+				argv[_i]->varname, argv[_i]->arg, VTY_NEWLINE);
+		_failcnt += _fail;
+	}
+	if (_failcnt)
+		return CMD_WARNING;
+	return ${fnname}_magic(self, vty, argc, argv$arglist);
+}
+
+''')
+
+# invoked for each named parameter
+argblock = Template('''
+		if (!strcmp(argv[_i]->varname, \"$varname\")) {$strblock
+			$code
+		}''')
+
+def process_file(fn, ofd, dumpfd, all_defun):
+    filedata = clippy.parse(fn)
+
+    for entry in filedata['data']:
+        if entry['type'] == 'DEFPY' or (all_defun and entry['type'].startswith('DEFUN')):
+            cmddef = entry['args'][2]
+            for i in cmddef:
+                assert i.startswith('"') and i.endswith('"')
+            cmddef = ''.join([i[1:-1] for i in cmddef])
+
+            graph = clippy.Graph(cmddef)
+            args = OrderedDict()
+            for token, depth in clippy.graph_iterate(graph):
+                if token.type not in handlers:
+                    continue
+                if token.varname is None:
+                    continue
+                arg = args.setdefault(token.varname, [])
+                arg.append(handlers[token.type](token))
+
+            #print('-' * 76)
+            #pprint(entry)
+            #clippy.dump(graph)
+            #pprint(args)
+
+            params = { 'cmddef': cmddef, 'fnname': entry['args'][0][0] }
+            argdefs = []
+            argdecls = []
+            arglist = []
+            argblocks = []
+            doc = []
+
+            def do_add(handler, varname, attr = ''):
+                argdefs.append(',\\\n\t%s %s%s' % (handler.argtype, varname, attr))
+                argdecls.append('\t%s\n' % (handler.decl.substitute({'varname': varname}).replace('\n', '\n\t')))
+                arglist.append(', %s%s' % (handler.deref, varname))
+                if attr == '':
+                    at = handler.argtype
+                    if not at.startswith('const '):
+                        at = '. . . ' + at
+                    doc.append('\t%-26s %s' % (at, varname))
+
+            for varname in args.keys():
+                handler = mix_handlers(args[varname])
+                #print(varname, handler)
+                if handler is None: continue
+                do_add(handler, varname)
+                code = handler.code.substitute({'varname': varname}).replace('\n', '\n\t\t\t')
+                strblock = ''
+                if not handler.drop_str:
+                    do_add(StringHandler(None), '%s_str' % (varname), ' __attribute__ ((unused))')
+                    strblock = '\n\t\t\t%s_str = argv[_i]->arg;' % (varname)
+                argblocks.append(argblock.substitute({'varname': varname, 'strblock': strblock, 'code': code}))
+
+            if dumpfd is not None:
+                if len(arglist) > 0:
+                    dumpfd.write('"%s":\n%s\n\n' % (cmddef, '\n'.join(doc)))
+                else:
+                    dumpfd.write('"%s":\n\t---- no magic arguments ----\n\n' % (cmddef))
+
+            params['argdefs'] = ''.join(argdefs)
+            params['argdecls'] = ''.join(argdecls)
+            params['arglist'] = ''.join(arglist)
+            params['argblocks'] = ''.join(argblocks)
+            ofd.write(templ.substitute(params))
+
+if __name__ == '__main__':
+    import argparse
+
+    argp = argparse.ArgumentParser(description = 'FRR CLI preprocessor in Python')
+    argp.add_argument('--all-defun', action = 'store_const', const = True,
+            help = 'process DEFUN() statements in addition to DEFPY()')
+    argp.add_argument('--show', action = 'store_const', const = True,
+            help = 'print out list of arguments and types for each definition')
+    argp.add_argument('-o', type = str, metavar = 'OUTFILE',
+            help = 'output C file name')
+    argp.add_argument('cfile', type = str)
+    args = argp.parse_args()
+
+    dumpfd = None
+    if args.o is not None:
+        ofd = StringIO()
+        if args.show:
+            dumpfd = sys.stdout
+    else:
+        ofd = sys.stdout
+        if args.show:
+            dumpfd = sys.stderr
+
+    process_file(args.cfile, ofd, dumpfd, args.all_defun)
+
+    if args.o is not None:
+        clippy.wrdiff(args.o, ofd)

.gitignore
@@ -61,6 +61,7 @@ debian/quagga.prerm.debhelper
 debian/quagga.substvars
 debian/quagga/
 debian/tmp/
+*.pyc
 *.swp
 cscope.*
 *.pb.h

Makefile.am
@@ -9,7 +9,9 @@ SUBDIRS = lib qpb fpm @ZEBRA@ @LIBRFP@ @RFPTEST@ \
 DIST_SUBDIRS = lib qpb fpm zebra bgpd ripd ripngd ospfd ospf6d ldpd \
 	  isisd watchfrr vtysh ospfclient doc m4 pkgsrc redhat tests \
 	  solaris pimd nhrpd eigrpd @LIBRFP@ @RFPTEST@ tools snapcraft \
-	  babeld
+	  babeld \
+	  python \
+	  # end
 
 EXTRA_DIST = aclocal.m4 SERVICES REPORTING-BUGS \
 	update-autotools \

common.am
@@ -3,6 +3,17 @@
 # tree.
 #
 
+AM_V_CLIPPY = $(am__v_CLIPPY_$(V))
+am__v_CLIPPY_ = $(am__v_CLIPPY_$(AM_DEFAULT_VERBOSITY))
+am__v_CLIPPY_0 = @echo "  CLIPPY  " $@;
+am__v_CLIPPY_1 =
+
+SUFFIXES = _clippy.c
+.c_clippy.c:
+	$(AM_V_at)$(MAKE) -C $(top_builddir)/$(CLIPPYDIR) clippy
+	$(AM_V_CLIPPY)$(top_builddir)/$(CLIPPYDIR)/clippy $(top_srcdir)/python/clidef.py $< > $@.tmp
+	@{ test -f $@ && diff $@.tmp $@ >/dev/null 2>/dev/null; } && rm $@.tmp || mv $@.tmp $@
+
 if HAVE_PROTOBUF
 
 # Uncomment to use an non-system version of libprotobuf-c.

configure.ac
@@ -1983,6 +1983,7 @@ AC_CONFIG_FILES([Makefile lib/Makefile qpb/Makefile zebra/Makefile ripd/Makefile
 	  redhat/Makefile
 	  tools/Makefile
 	  pkgsrc/Makefile
+	  python/Makefile
 	  fpm/Makefile
 	  redhat/frr.spec 
 	  snapcraft/Makefile

lib/command.h
@@ -207,6 +207,10 @@ struct cmd_node
      int argc __attribute__ ((unused)), \
      struct cmd_token *argv[] __attribute__ ((unused)) )
 
+#define DEFPY(funcname, cmdname, cmdstr, helpstr) \
+  DEFUN_CMD_ELEMENT(funcname, cmdname, cmdstr, helpstr, 0, 0) \
+  funcdecl_##funcname
+
 #define DEFUN(funcname, cmdname, cmdstr, helpstr) \
   DEFUN_CMD_FUNC_DECL(funcname) \
   DEFUN_CMD_ELEMENT(funcname, cmdname, cmdstr, helpstr, 0, 0) \
@@ -274,6 +278,9 @@ struct cmd_node
 #define ALIAS_SH_DEPRECATED(daemon, funcname, cmdname, cmdstr, helpstr) \
   DEFUN_CMD_ELEMENT(funcname, cmdname, cmdstr, helpstr, CMD_ATTR_DEPRECATED, daemon)
 
+#else /* VTYSH_EXTRACT_PL */
+#define DEFPY(funcname, cmdname, cmdstr, helpstr) \
+  DEFUN(funcname, cmdname, cmdstr, helpstr)
 #endif /* VTYSH_EXTRACT_PL */
 
 /* Some macroes */

python/Makefile.am
@@ -1,0 +1,3 @@
+EXTRA_DIST = \
+	clidef.py \
+	clippy/__init__.py

python/clippy/__init__.py
@@ -1,0 +1,64 @@
+# FRR CLI preprocessor
+#
+# Copyright (C) 2017  David Lamparter for NetDEF, Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; see the file COPYING; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+import _clippy
+from _clippy import parse, Graph, GraphNode
+
+def graph_iterate(graph):
+    '''iterator yielding all nodes of a graph
+
+    nodes arrive in input/definition order, graph circles are avoided.
+    '''
+
+    queue = [(graph.first(), frozenset(), 0)]
+    while len(queue) > 0:
+        node, stop, depth = queue.pop(0)
+        yield node, depth
+
+        join = node.join()
+        if join is not None:
+            queue.insert(0, (join, stop.union(frozenset([node])), depth))
+            join = frozenset([join])
+
+        stop = join or stop
+        nnext = node.next()
+        for n in reversed(nnext):
+            if n not in stop and n is not node:
+                queue.insert(0, (n, stop, depth + 1))
+
+def dump(graph):
+    '''print out clippy.Graph'''
+
+    for i, depth in graph_iterate(graph):
+        print('\t%s%s %r' % ('  ' * (depth * 2), i.type, i.text))
+
+def wrdiff(filename, buf):
+    '''write buffer to file if contents changed'''
+
+    expl = ''
+    if hasattr(buf, 'getvalue'):
+        buf = buf.getvalue()
+    old = None
+    try:    old = open(filename, 'r').read()
+    except: pass
+    if old == buf:
+        # sys.stderr.write('%s unchanged, not written\n' % (filename))
+        return
+    with open('.new.' + filename, 'w') as out:
+        out.write(buf)
+    os.rename('.new.' + filename, filename)

commit 84b9142abe33fe745e3eabd4527e984f32b7315d

*: fix add unsigned long support to RANGE token CLI
The original problem came with a DEFPY command that uses RANGE token
with a big value. To illustrate, tha passed argument is not the expected
one on i386 platforms.

> # config:
> router bgp 65003 vrf Vrf20
>  bgp router-id 192.0.2.3
>  sid vpn per-vrf export 4294442578
> # show running-config
> router bgp 65003 vrf Vrf20
>  bgp router-id 192.0.2.3
>  sid vpn per-vrf export 2147483647

The 'long' size on an i386 platform is 32 bits, which differs from most
of other platforms (64 bits). Propose to extend the RANGE to int64_t
values by converting the RANGE from long to int64_t.
Add the necessary changes on places where RANGE is used and where
internal attribute type differs from passed argument.

Fixes: 5578a14d949d ("python: clidef.py")

Signed-off-by: Dmytro Shytyi <dmytro.shytyi@6wind.com>
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bfdd/bfdd_cli.c
@@ -834,7 +834,7 @@ DEFPY_YANG(
 {
 	char value[32];
 
-	snprintf(value, sizeof(value), "%ld", interval * 1000);
+	snprintf(value, sizeof(value), "%" PRId64, interval * 1000);
 	nb_cli_enqueue_change(vty, "./required-receive-interval", no ? NB_OP_DESTROY : NB_OP_MODIFY,
 			      value);
 
@@ -858,7 +858,7 @@ DEFPY_YANG(
 {
 	char value[32];
 
-	snprintf(value, sizeof(value), "%ld", interval * 1000);
+	snprintf(value, sizeof(value), "%" PRId64, interval * 1000);
 	nb_cli_enqueue_change(vty, "./desired-transmission-interval",
 			      no ? NB_OP_DESTROY : NB_OP_MODIFY, value);
 
@@ -922,7 +922,7 @@ DEFPY_YANG(
 		return CMD_WARNING_CONFIG_FAILED;
 	}
 
-	snprintf(value, sizeof(value), "%ld", interval * 1000);
+	snprintf(value, sizeof(value), "%" PRId64, interval * 1000);
 	nb_cli_enqueue_change(vty, "./desired-echo-transmission-interval",
 			      no ? NB_OP_DESTROY : NB_OP_MODIFY, value);
 	nb_cli_enqueue_change(vty, "./required-echo-receive-interval",
@@ -947,7 +947,7 @@ DEFPY_YANG(
 		return CMD_WARNING_CONFIG_FAILED;
 	}
 
-	snprintf(value, sizeof(value), "%ld", interval * 1000);
+	snprintf(value, sizeof(value), "%" PRId64, interval * 1000);
 	nb_cli_enqueue_change(vty, "./desired-echo-transmission-interval",
 			      no ? NB_OP_DESTROY : NB_OP_MODIFY, value);
 
@@ -983,7 +983,7 @@ DEFPY_YANG(
 	if (disabled)
 		snprintf(value, sizeof(value), "0");
 	else
-		snprintf(value, sizeof(value), "%ld", interval * 1000);
+		snprintf(value, sizeof(value), "%" PRId64, interval * 1000);
 
 	nb_cli_enqueue_change(vty, "./required-echo-receive-interval",
 			      no ? NB_OP_DESTROY : NB_OP_MODIFY, value);

bgpd/bgp_rpki.c
@@ -1927,8 +1927,7 @@ DEFPY(rpki_cache_tcp, rpki_cache_tcp_cmd,
 	for (ALL_LIST_ELEMENTS_RO(rpki_vrf->cache_list, cache_node,
 				  current_cache)) {
 		if (current_cache->preference == preference) {
-			vty_out(vty,
-				"Cache with preference %ld is already configured\n",
+			vty_out(vty, "Cache with preference %" PRId64 " is already configured\n",
 				preference);
 			return CMD_WARNING;
 		}
@@ -1986,8 +1985,7 @@ DEFPY(rpki_cache_ssh, rpki_cache_ssh_cmd,
 	for (ALL_LIST_ELEMENTS_RO(rpki_vrf->cache_list, cache_node,
 				  current_cache)) {
 		if (current_cache->preference == preference) {
-			vty_out(vty,
-				"Cache with preference %ld is already configured\n",
+			vty_out(vty, "Cache with preference %" PRId64 " is already configured\n",
 				preference);
 			return CMD_WARNING;
 		}
@@ -2049,8 +2047,7 @@ DEFPY (no_rpki_cache,
 	cache_list = rpki_vrf->cache_list;
 	cache_p = find_cache(preference, cache_list);
 	if (!rpki_vrf || !cache_p) {
-		vty_out(vty, "Could not find cache with preference %ld\n",
-			preference);
+		vty_out(vty, "Could not find cache with preference %" PRId64 "\n", preference);
 		return CMD_WARNING;
 	}
 
@@ -2059,8 +2056,7 @@ DEFPY (no_rpki_cache,
 	} else if (is_running(rpki_vrf)) {
 		if (rtr_mgr_remove_group(rpki_vrf->rtr_config, preference) ==
 		    RTR_ERROR) {
-			vty_out(vty,
-				"Could not remove cache with preference %ld\n",
+			vty_out(vty, "Could not remove cache with preference %" PRId64 "\n",
 				preference);
 			return CMD_WARNING;
 		}

bgpd/bgp_vty.c
@@ -10417,7 +10417,7 @@ DEFPY (af_sid_vpn_export,
 	} else if (sid_idx != 0) {
 		/* SID allocation index-mode */
 		if (debug)
-			zlog_debug("%s: idx %ld sid alloc.", __func__, sid_idx);
+			zlog_debug("%s: idx %" PRId64 " sid alloc.", __func__, sid_idx);
 		bgp->vpn_policy[afi].tovpn_sid_index = sid_idx;
 	} else if (sid_explicit) {
 		/* SID allocation explicit-mode */
@@ -10521,8 +10521,7 @@ DEFPY (bgp_sid_vpn_export,
 	} else if (sid_idx != 0) {
 		/* SID allocation index-mode */
 		if (debug)
-			zlog_debug("%s: idx %ld per-vrf sid alloc.", __func__,
-				   sid_idx);
+			zlog_debug("%s: idx %" PRId64 " per-vrf sid alloc.", __func__, sid_idx);
 		bgp->tovpn_sid_index = sid_idx;
 	} else if (sid_explicit) {
 		/* SID allocation explicit-mode */
@@ -18489,8 +18488,7 @@ DEFPY(bgp_redistribute_ipv6_table, bgp_redistribute_ipv6_table_cmd,
 		return CMD_WARNING_CONFIG_FAILED;
 	}
 	if (table_id == RT_TABLE_MAIN || table_id == RT_TABLE_LOCAL) {
-		vty_out(vty,
-			"%% 'table-direct', can not use %lu routing table\n",
+		vty_out(vty, "%% 'table-direct', can not use %" PRIu64 " routing table\n",
 			table_id);
 		return CMD_WARNING_CONFIG_FAILED;
 	}
@@ -18525,8 +18523,7 @@ DEFPY(no_bgp_redistribute_ipv6_table, no_bgp_redistribute_ipv6_table_cmd,
 		return CMD_WARNING_CONFIG_FAILED;
 	}
 	if (table_id == RT_TABLE_MAIN || table_id == RT_TABLE_LOCAL) {
-		vty_out(vty,
-			"%% 'table-direct', can not use %lu routing table\n",
+		vty_out(vty, "%% 'table-direct', can not use %" PRIu64 " routing table\n",
 			table_id);
 		return CMD_WARNING_CONFIG_FAILED;
 	}

isisd/isis_cli.c
@@ -3719,14 +3719,13 @@ DEFPY_YANG_NOSH(flex_algo, flex_algo_cmd, "flex-algo (128-255)$algorithm",
 	int ret;
 	char xpath[XPATH_MAXLEN + 37];
 
-	snprintf(xpath, sizeof(xpath),
-		 "%s/flex-algos/flex-algo[flex-algo='%ld']", VTY_CURR_XPATH,
-		 algorithm);
+	snprintf(xpath, sizeof(xpath), "%s/flex-algos/flex-algo[flex-algo='%" PRId64 "']",
+		 VTY_CURR_XPATH, algorithm);
 
 	nb_cli_enqueue_change(vty, ".", NB_OP_CREATE, NULL);
 
-	ret = nb_cli_apply_changes(
-		vty, "./flex-algos/flex-algo[flex-algo='%ld']", algorithm);
+	ret = nb_cli_apply_changes(vty, "./flex-algos/flex-algo[flex-algo='%" PRId64 "']",
+				   algorithm);
 	if (ret == CMD_SUCCESS)
 		VTY_PUSH_XPATH(ISIS_FLEX_ALGO_NODE, xpath);
 
@@ -3740,18 +3739,18 @@ DEFPY_YANG(no_flex_algo, no_flex_algo_cmd, "no flex-algo (128-255)$algorithm",
 {
 	char xpath[XPATH_MAXLEN + 37];
 
-	snprintf(xpath, sizeof(xpath),
-		 "%s/flex-algos/flex-algo[flex-algo='%ld']", VTY_CURR_XPATH,
-		 algorithm);
+	snprintf(xpath, sizeof(xpath), "%s/flex-algos/flex-algo[flex-algo='%'" PRId64 "']",
+		 VTY_CURR_XPATH, algorithm);
 
 	if (!yang_dnode_exists(vty->candidate_config->dnode, xpath)) {
-		vty_out(vty, "ISIS flex-algo %ld isn't exist.\n", algorithm);
+		vty_out(vty, "ISIS flex-algo %" PRId64 " isn't exist.\n", algorithm);
 		return CMD_ERR_NO_MATCH;
 	}
 
 	nb_cli_enqueue_change(vty, ".", NB_OP_DESTROY, NULL);
-	return nb_cli_apply_changes_clear_pending(
-		vty, "./flex-algos/flex-algo[flex-algo='%ld']", algorithm);
+	return nb_cli_apply_changes_clear_pending(vty,
+						  "./flex-algos/flex-algo[flex-algo='%" PRId64 "']",
+						  algorithm);
 }
 
 DEFPY_YANG(advertise_definition, advertise_definition_cmd,

lib/routemap_cli.c
@@ -39,8 +39,8 @@ DEFPY_YANG_NOSH(
 		 "/frr-route-map:lib/route-map[name='%s']", name);
 	nb_cli_enqueue_change(vty, xpath, NB_OP_CREATE, NULL);
 
-	snprintf(xpath_index, sizeof(xpath_index), "%s/entry[sequence='%lu']",
-		 xpath, sequence);
+	snprintf(xpath_index, sizeof(xpath_index), "%s/entry[sequence='%" PRIu64 "']", xpath,
+		 sequence);
 	nb_cli_enqueue_change(vty, xpath_index, NB_OP_CREATE, NULL);
 
 	snprintf(xpath_action, sizeof(xpath_action), "%s/action", xpath_index);
@@ -79,8 +79,8 @@ DEFPY_YANG(
 	char xpath[XPATH_MAXLEN];
 
 	snprintf(xpath, sizeof(xpath),
-		 "/frr-route-map:lib/route-map[name='%s']/entry[sequence='%lu']",
-		 name, sequence);
+		 "/frr-route-map:lib/route-map[name='%s']/entry[sequence='%" PRIu64 "']", name,
+		 sequence);
 
 	nb_cli_enqueue_change(vty, xpath, NB_OP_DESTROY, NULL);
 
@@ -572,7 +572,7 @@ DEFPY_YANG(
 	nb_cli_enqueue_change(vty, xpath, NB_OP_CREATE, NULL);
 	snprintf(xpath_value, sizeof(xpath_value),
 		 "%s/rmap-match-condition/tag", xpath);
-	snprintf(value, sizeof(value), "%lu", tagged ? tagged : 0);
+	snprintf(value, sizeof(value), "%" PRIu64, tagged ? tagged : 0);
 	nb_cli_enqueue_change(vty, xpath_value, NB_OP_MODIFY, value);
 
 	return nb_cli_apply_changes(vty, NULL);
@@ -1140,7 +1140,7 @@ DEFPY_YANG(
 	nb_cli_enqueue_change(vty, xpath, NB_OP_CREATE, NULL);
 	snprintf(xpath_value, sizeof(xpath_value), "%s/rmap-set-action/tag",
 		 xpath);
-	snprintf(value, sizeof(value), "%lu", tagged ? tagged : 0);
+	snprintf(value, sizeof(value), "%" PRIu64, tagged ? tagged : 0);
 	nb_cli_enqueue_change(vty, xpath_value, NB_OP_MODIFY, value);
 
 	return nb_cli_apply_changes(vty, NULL);

python/clidef.py
@@ -40,13 +40,13 @@ class StringHandler(RenderHandler):
     canassert = True
 
 
-class LongHandler(RenderHandler):
-    argtype = "long"
-    decl = Template("long $varname = 0;")
+class Int64Handler(RenderHandler):
+    argtype = "int64_t"
+    decl = Template("int64_t $varname = 0;")
     code = Template(
         """\
 char *_end;
-$varname = strtol(argv[_i]->arg, &_end, 10);
+$varname = strtoll(argv[_i]->arg, &_end, 10);
 _fail = (_end == argv[_i]->arg) || (*_end != '\\0');"""
     )
 
@@ -151,7 +151,7 @@ def mix_handlers(handlers):
 handlers = {
     "WORD_TKN": StringHandler,
     "VARIABLE_TKN": StringHandler,
-    "RANGE_TKN": LongHandler,
+    "RANGE_TKN": Int64Handler,
     "IPV4_TKN": IP4Handler,
     "IPV4_PREFIX_TKN": Prefix4Handler,
     "IPV6_TKN": IP6Handler,

vrrpd/vrrp_vty.c
@@ -30,7 +30,7 @@
 #define VRRP_IP_STR "Virtual Router IP address\n"
 #define VRRP_VERSION_STR "VRRP protocol version\n"
 
-#define VRRP_XPATH_ENTRY VRRP_XPATH "[virtual-router-id='%ld']"
+#define VRRP_XPATH_ENTRY VRRP_XPATH "[virtual-router-id='%" PRId64 "']"
 
 /* clang-format off */
 
@@ -48,7 +48,7 @@ DEFPY_YANG(vrrp_vrid,
 {
 	char valbuf[20];
 
-	snprintf(valbuf, sizeof(valbuf), "%ld", version ? version : vd.version);
+	snprintf(valbuf, sizeof(valbuf), "%" PRId64, version ? version : vd.version);
 
 	if (no)
 		nb_cli_enqueue_change(vty, ".", NB_OP_DESTROY, NULL);
@@ -152,7 +152,7 @@ DEFPY_YANG(vrrp_advertisement_interval,
 
 	/* all internal computations are in centiseconds */
 	advertisement_interval /= CS2MS;
-	snprintf(val, sizeof(val), "%ld", advertisement_interval);
+	snprintf(val, sizeof(val),  "%" PRId64, advertisement_interval);
 	nb_cli_enqueue_change(vty, "./advertisement-interval", NB_OP_MODIFY,
 			      val);
 

zebra/label_manager.c
@@ -240,7 +240,8 @@ DEFPY(mpls_label_dynamic_block, mpls_label_dynamic_block_cmd,
 	}
 	if (start > end) {
 		vty_out(vty,
-			"%% label dynamic-block, wrong range (%ld > %ld), aborting\n",
+			"%% label dynamic-block, wrong range ( %" PRId64 " >  %" PRId64
+			"), aborting\n",
 			start, end);
 		return CMD_WARNING_CONFIG_FAILED;
 	}

zebra/zebra_cli.c
@@ -1318,9 +1318,9 @@ DEFPY_YANG (ipv6_nd_ra_interval,
 
 	if (!no) {
 		if (sec)
-			snprintf(value, sizeof(value), "%lu", sec * 1000);
+			snprintf(value, sizeof(value), "%" PRIu64, sec * 1000);
 		else
-			snprintf(value, sizeof(value), "%lu", msec);
+			snprintf(value, sizeof(value), "%" PRIu64, msec);
 
 		nb_cli_enqueue_change(vty,
 				      "./frr-zebra:zebra/ipv6-router-advertisements/max-rtr-adv-interval",
@@ -2463,7 +2463,8 @@ DEFPY_YANG (vni_mapping,
 	} else {
 		if (vty->node == CONFIG_NODE) {
 			if (yang_dnode_existsf(vty->candidate_config->dnode,
-					       "/frr-vrf:lib/vrf[name='%s']/frr-zebra:zebra[l3vni-id='%lu']",
+					       "/frr-vrf:lib/vrf[name='%s']/frr-zebra:zebra[l3vni-id='%" PRIu64
+					       "']",
 					       VRF_DEFAULT_NAME, vni))
 				nb_cli_enqueue_change(vty, "./frr-zebra:zebra/l3vni-id",
 						      NB_OP_DESTROY, NULL);
@@ -2473,7 +2474,8 @@ DEFPY_YANG (vni_mapping,
 				vrf = yang_dnode_get_string(dnode, "name");
 
 				if (yang_dnode_existsf(vty->candidate_config->dnode,
-						       "/frr-vrf:lib/vrf[name='%s']/frr-zebra:zebra[l3vni-id='%lu']",
+						       "/frr-vrf:lib/vrf[name='%s']/frr-zebra:zebra[l3vni-id='%" PRIu64
+						       "']",
 						       vrf, vni))
 					nb_cli_enqueue_change(vty, "./frr-zebra:zebra/l3vni-id",
 							      NB_OP_DESTROY, NULL);

zebra/zebra_srv6_vty.c
@@ -943,7 +943,8 @@ DEFPY (locator_prefix,
 
 	if (prefix->prefixlen + func_bit_len + 0 > 128) {
 		vty_out(vty,
-			"%% prefix-len + function-len + arg-len (%ld) cannot be greater than 128\n",
+			"%% prefix-len + function-len + arg-len (%" PRId64
+			") cannot be greater than 128\n",
 			prefix->prefixlen + func_bit_len + 0);
 		return CMD_WARNING_CONFIG_FAILED;
 	}

zebra/zebra_vty.c
@@ -3657,12 +3657,14 @@ DEFPY (ip_zebra_import_table_distance,
 		distance = ZEBRA_TABLE_DISTANCE_DEFAULT;
 
 	if (!is_zebra_valid_kernel_table(table_id)) {
-		vty_out(vty, "Invalid routing table ID, %ld. Must be in range 1-252\n", table_id);
+		vty_out(vty, "Invalid routing table ID, %" PRId64 ". Must be in range 1-252\n",
+			table_id);
 		return CMD_WARNING;
 	}
 
 	if (is_zebra_main_routing_table(table_id)) {
-		vty_out(vty, "Invalid routing table ID, %ld. Must be non-default table\n", table_id);
+		vty_out(vty, "Invalid routing table ID, %" PRId64 ". Must be non-default table\n",
+			table_id);
 		return CMD_WARNING;
 	}
 
@@ -3747,7 +3749,8 @@ DEFPY (no_ip_zebra_import_table,
 	}
 
 	if (is_zebra_main_routing_table(table_id)) {
-		vty_out(vty, "Invalid routing table ID, %ld. Must be non-default table\n", table_id);
+		vty_out(vty, "Invalid routing table ID, %" PRId64 ". Must be non-default table\n",
+			table_id);
 		return CMD_WARNING;
 	}
 