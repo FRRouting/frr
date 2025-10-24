0fe593f1ee4be571a92dc1656166bbb405b78ed3
Philippe Guibert <philippe.guibert@6wind.com>
2025-10-24 16:48:04 +0200

    zebra, lib: add support for ip6gre and ip6gretap interface
    Add new defines to detect ip6gre and ip6gretap interfaces.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- lib/if.h
+++ lib/if.h
@@ -90,7 +90,9 @@ enum zebra_iftype {
 	ZEBRA_IF_VETH,	    /* VETH interface*/
 	ZEBRA_IF_BOND,	    /* Bond */
 	ZEBRA_IF_GRE,	    /* GRE interface */
+	ZEBRA_IF_IP6GRE,    /* IP6GRE interface */
 	ZEBRA_IF_GRETAP,    /* GRETAP interface */
+	ZEBRA_IF_IP6GRETAP, /* IP6GRETAP interface */
 	ZEBRA_IF_DUMMY,	    /* Dummy interface */
 };
 
