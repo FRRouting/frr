791b4e68660b27975c33c55de338e9f4d030eb2e
Harini Subramanian <hnattamaisub@penta-01.mvlab.labs.mlnx>
2025-10-30 20:28:48 -0700

    zebra: Coverity issue (Null pointer derefence(CID 71721))
    This commit addresses null pointer dereference in zebra/zapi_msg.c
    
    CID 71721: Dereference null return value (NULL_RETURNS)
    dereference: Dereferencing a pointer that might be NULL rn when calling route_notify_internal.
    
    Description:
    Handled null check accordingly.
    
    Signed-off-by: Harini Subramanian <hnattamaisub@nvidia.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -820,6 +820,15 @@ int zsend_route_notify_owner_ctx(const struct zebra_dplane_ctx *ctx,
 	int result;
 	struct route_node *rn = rib_find_rn_from_ctx(ctx);
 
+	if (!rn) {
+		if (IS_ZEBRA_DEBUG_PACKET) {
+			zlog_debug("%s: route node not found for ctx vrf %u table %u afi %u safi %u",
+				   __func__, dplane_ctx_get_vrf(ctx), dplane_ctx_get_table(ctx),
+				   dplane_ctx_get_afi(ctx), dplane_ctx_get_safi(ctx));
+		}
+		return -1;
+	}
+
 	result = route_notify_internal(
 		rn, dplane_ctx_get_type(ctx), dplane_ctx_get_instance(ctx),
 		dplane_ctx_get_vrf(ctx), dplane_ctx_get_table(ctx), note,
