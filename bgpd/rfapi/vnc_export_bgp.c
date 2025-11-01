6961a385ec285404692e005e34d8f200e68fad1e
Donald Sharp <sharpd@nvidia.com>
2025-11-01 10:17:57 -0400

    bgpd: fix BGP_ATTR_LOCAL_PREF being set appropriately
    The BGP_ATTR_XXX flags are all using ATTR_FLAG_BIT to
    find the right spot in the bitfield of the flags
    variable.  From looking at 004f9909c866ddf8d04e680db00120071dd22344
    I decided to go looking at the rest of them.  This
    is where I found it wrong too.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>
    (cherry picked from commit 1812bc263b45e5f2cc62b3f7f5de3612fe054753)
    
    # Conflicts:
    #	bgpd/rfapi/vnc_export_bgp.c

--- bgpd/rfapi/vnc_export_bgp.c
+++ bgpd/rfapi/vnc_export_bgp.c
@@ -96,7 +96,13 @@ static void encap_attr_export_ce(struct attr *new, struct attr *orig,
 	 *          neighbor NEIGHBOR attribute-unchanged med
 	 */
 	if (!CHECK_FLAG(new->flag, BGP_ATTR_MULTI_EXIT_DISC)) {
+<<<<<<< HEAD
 		if (CHECK_FLAG(new->flag, BGP_ATTR_LOCAL_PREF)) {
+=======
+		uint32_t med = 255;
+
+		if (CHECK_FLAG(new->flag, ATTR_FLAG_BIT(BGP_ATTR_LOCAL_PREF))) {
+>>>>>>> 1812bc263 (bgpd: fix BGP_ATTR_LOCAL_PREF being set appropriately)
 			if (new->local_pref > 255)
 				new->med = 0;
 			else
@@ -642,7 +648,13 @@ encap_attr_export(struct attr *new, struct attr *orig,
 	 *          neighbor NEIGHBOR attribute-unchanged med
 	 */
 	if (!CHECK_FLAG(new->flag, BGP_ATTR_MULTI_EXIT_DISC)) {
+<<<<<<< HEAD
 		if (CHECK_FLAG(new->flag, BGP_ATTR_LOCAL_PREF)) {
+=======
+		uint32_t med = 255;
+
+		if (CHECK_FLAG(new->flag, ATTR_FLAG_BIT(BGP_ATTR_LOCAL_PREF))) {
+>>>>>>> 1812bc263 (bgpd: fix BGP_ATTR_LOCAL_PREF being set appropriately)
 			if (new->local_pref > 255)
 				new->med = 0;
 			else
