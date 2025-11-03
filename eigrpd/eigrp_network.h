6d99350c8963424d06fd3a48a4ed62f08cef2625
Donald Sharp <sharpd@nvidia.com>
2025-11-03 13:23:19 -0500

    eigrpd: Declarations should have variable names
    Our coding standard is that all function declarations
    should have actual function names.  Let's try to clean
    up all that is missing this in eigrpd.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- eigrpd/eigrp_network.h
+++ eigrpd/eigrp_network.h
@@ -16,19 +16,18 @@
 /* Prototypes */
 
 extern int eigrp_sock_init(struct vrf *vrf);
-extern int eigrp_if_ipmulticast(struct eigrp *, struct prefix *, unsigned int);
+extern int eigrp_if_ipmulticast(struct eigrp *top, struct prefix *p, unsigned int ifindex);
 extern int eigrp_network_set(struct eigrp *eigrp, struct prefix *p);
 extern int eigrp_network_unset(struct eigrp *eigrp, struct prefix *p);
 extern void eigrp_network_free(struct eigrp *eigrp, struct route_table *table);
 
 extern void eigrp_hello_timer(struct event *event);
-extern void eigrp_if_update(struct interface *);
-extern int eigrp_if_add_allspfrouters(struct eigrp *, struct prefix *,
-				      unsigned int);
+extern void eigrp_if_update(struct interface *ifp);
+extern int eigrp_if_add_allspfrouters(struct eigrp *top, struct prefix *p, unsigned int ifindex);
 extern int eigrp_if_drop_allspfrouters(struct eigrp *top, struct prefix *p,
 				       unsigned int ifindex);
-extern void eigrp_adjust_sndbuflen(struct eigrp *, unsigned int);
+extern void eigrp_adjust_sndbuflen(struct eigrp *eigrp, unsigned int buflen);
 
-extern void eigrp_external_routes_refresh(struct eigrp *, int);
+extern void eigrp_external_routes_refresh(struct eigrp *eigrp, int type);
 
 #endif /* EIGRP_NETWORK_H_ */
