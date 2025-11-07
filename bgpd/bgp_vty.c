FRRouting/frr

commit 8e6bb4a070785dad72ce6667917df68e85013075

bgpd: add neighbor <encapsulation-srv6|encapsulation-mpls> command
RFC9252 mentions:
"Implementations supporting this specification MUST provide a mechanism
to control the advertisement of SRv6-based BGP service routes on a
per-neighbor basis".

There is no control on how to filter out BGP VPN updates per peer, and
some peers may be unable to handle incoming BGP SRv6 updates, and may
see the BGP update as MPLS packet.

Fix this by proposing a config command under the ipv4-vpn and the
ipv6-vpn addres families:

> dut# neighbor A:B::C:D <encapsulation-srv6|encapsulation-mpls>

Without those commands, there is no filtering and if both MPLS and SRv6
paths are exported: without add-path, only the MPLS VPN path will be
advertised; with add-path, both the MPLS VPN path and the SRv6 VPN path
will be advertised.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_vty.c
@@ -9440,6 +9440,46 @@ DEFUN (no_neighbor_ttl_security,
 	return bgp_vty_return(vty, peer_ttl_security_hops_unset(peer));
 }
 
+/* "neighbor encapsulation-srv6|encapsulation-mpls" */
+DEFPY (neighbor_encapsulation_srv6_or_mpls,
+       neighbor_encapsulation_srv6_or_mpls_cmd,
+       "[no] neighbor <A.B.C.D|X:X::X:X|WORD>$peer_str <encapsulation-srv6$srv6|encapsulation-mpls$mpls>",
+       NO_STR
+       NEIGHBOR_STR
+       NEIGHBOR_ADDR_STR2
+       "Distribute L3VPN updates with SRv6 prefix SID\n"
+       "Distribute L3VPN updates with MPLS prefix SID\n")
+{
+	struct peer *peer;
+	afi_t afi = bgp_node_afi(vty);
+	safi_t safi = bgp_node_safi(vty);
+	int ret;
+
+	peer = peer_and_group_lookup_vty(vty, peer_str);
+	if (!peer)
+		return CMD_WARNING_CONFIG_FAILED;
+
+	if (srv6) {
+		if (no)
+			ret = peer_af_flag_unset_vty(vty, peer_str, afi, safi,
+						     PEER_FLAG_CONFIG_ENCAPSULATION_SRV6);
+		else
+			ret = peer_af_flag_set_vty(vty, peer_str, afi, safi,
+						   PEER_FLAG_CONFIG_ENCAPSULATION_SRV6);
+		return ret;
+	}
+	if (mpls) {
+		if (no)
+			ret = peer_af_flag_unset_vty(vty, peer_str, afi, safi,
+						     PEER_FLAG_CONFIG_ENCAPSULATION_MPLS);
+		else
+			ret = peer_af_flag_set_vty(vty, peer_str, afi, safi,
+						   PEER_FLAG_CONFIG_ENCAPSULATION_MPLS);
+		return ret;
+	}
+	return CMD_WARNING_CONFIG_FAILED;
+}
+
 /* disable-addpath-rx */
 DEFUN(neighbor_disable_addpath_rx,
       neighbor_disable_addpath_rx_cmd,
@@ -19596,6 +19636,12 @@ static void bgp_config_write_peer_af(struct vty *vty, struct bgp *bgp,
 		vty_out(vty, "  neighbor %s weight %lu\n", addr,
 			peer->weight[afi][safi]);
 
+	/* encapsulation-srv6|encapsulation-mpls */
+	if (peergroup_af_flag_check(peer, afi, safi, PEER_FLAG_CONFIG_ENCAPSULATION_SRV6))
+		vty_out(vty, "  neighbor %s encapsulation-srv6\n", addr);
+	else if (peergroup_af_flag_check(peer, afi, safi, PEER_FLAG_CONFIG_ENCAPSULATION_MPLS))
+		vty_out(vty, "  neighbor %s encapsulation-mpls\n", addr);
+
 	/* Filter. */
 	bgp_config_write_filter(vty, peer, afi, safi);
 
@@ -21877,6 +21923,10 @@ void bgp_vty_init(void)
 	install_element(BGP_VPNV4_NODE, &no_neighbor_weight_cmd);
 	install_element(BGP_VPNV6_NODE, &neighbor_weight_cmd);
 	install_element(BGP_VPNV6_NODE, &no_neighbor_weight_cmd);
+
+	/* "neighbor encapsulation-srv6|encapsulation-mpls" commands. */
+	install_element(BGP_VPNV4_NODE, &neighbor_encapsulation_srv6_or_mpls_cmd);
+	install_element(BGP_VPNV6_NODE, &neighbor_encapsulation_srv6_or_mpls_cmd);
 
 	/* "neighbor override-capability" commands. */
 	install_element(BGP_NODE, &neighbor_override_capability_cmd);

bgpd/bgp_route.c
@@ -2620,6 +2620,20 @@ bool subgroup_announce_check(struct bgp_dest *dest, struct bgp_path_info *pi,
 		}
 	}
 
+	if (safi == SAFI_MPLS_VPN &&
+	    CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_CONFIG_ENCAPSULATION_SRV6) &&
+	    !CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_CONFIG_ENCAPSULATION_MPLS) &&
+	    !pi->attr->srv6_l3vpn && !pi->attr->srv6_vpn)
+		/* MPLS update not advertised if SRv6 is autorised, but not MPLS */
+		return false;
+
+	if (safi == SAFI_MPLS_VPN &&
+	    CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_CONFIG_ENCAPSULATION_MPLS) &&
+	    !CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_CONFIG_ENCAPSULATION_SRV6) &&
+	    (pi->attr->srv6_l3vpn || pi->attr->srv6_vpn))
+		/* SRv6 update not advertised if MPLS is autorised, but not SRv6 */
+		return false;
+
 	bgp_peer_remove_private_as(bgp, afi, safi, peer, attr);
 	bgp_peer_as_override(bgp, afi, safi, peer, attr);
 

bgpd/bgp_updgrp.h
@@ -39,17 +39,16 @@
 #define PEER_UPDGRP_FLAGS                                                      \
 	(PEER_FLAG_LOCAL_AS_NO_PREPEND | PEER_FLAG_LOCAL_AS_REPLACE_AS)
 
-#define PEER_UPDGRP_AF_FLAGS                                                   \
-	(PEER_FLAG_SEND_COMMUNITY | PEER_FLAG_SEND_EXT_COMMUNITY |             \
-	 PEER_FLAG_SEND_EXT_COMMUNITY_RPKI | PEER_FLAG_SEND_LARGE_COMMUNITY |  \
-	 PEER_FLAG_DEFAULT_ORIGINATE | PEER_FLAG_REFLECTOR_CLIENT |            \
-	 PEER_FLAG_RSERVER_CLIENT | PEER_FLAG_NEXTHOP_SELF |                   \
-	 PEER_FLAG_NEXTHOP_UNCHANGED | PEER_FLAG_FORCE_NEXTHOP_SELF |          \
-	 PEER_FLAG_AS_PATH_UNCHANGED | PEER_FLAG_MED_UNCHANGED |               \
-	 PEER_FLAG_NEXTHOP_LOCAL_UNCHANGED | PEER_FLAG_REMOVE_PRIVATE_AS |     \
-	 PEER_FLAG_REMOVE_PRIVATE_AS_ALL |                                     \
-	 PEER_FLAG_REMOVE_PRIVATE_AS_REPLACE |                                 \
-	 PEER_FLAG_REMOVE_PRIVATE_AS_ALL_REPLACE | PEER_FLAG_AS_OVERRIDE)
+#define PEER_UPDGRP_AF_FLAGS                                                                      \
+	(PEER_FLAG_SEND_COMMUNITY | PEER_FLAG_SEND_EXT_COMMUNITY |                                \
+	 PEER_FLAG_SEND_EXT_COMMUNITY_RPKI | PEER_FLAG_SEND_LARGE_COMMUNITY |                     \
+	 PEER_FLAG_DEFAULT_ORIGINATE | PEER_FLAG_REFLECTOR_CLIENT | PEER_FLAG_RSERVER_CLIENT |    \
+	 PEER_FLAG_NEXTHOP_SELF | PEER_FLAG_NEXTHOP_UNCHANGED | PEER_FLAG_FORCE_NEXTHOP_SELF |    \
+	 PEER_FLAG_AS_PATH_UNCHANGED | PEER_FLAG_MED_UNCHANGED |                                  \
+	 PEER_FLAG_NEXTHOP_LOCAL_UNCHANGED | PEER_FLAG_REMOVE_PRIVATE_AS |                        \
+	 PEER_FLAG_REMOVE_PRIVATE_AS_ALL | PEER_FLAG_REMOVE_PRIVATE_AS_REPLACE |                  \
+	 PEER_FLAG_REMOVE_PRIVATE_AS_ALL_REPLACE | PEER_FLAG_AS_OVERRIDE |                        \
+	 PEER_FLAG_CONFIG_ENCAPSULATION_SRV6 | PEER_FLAG_CONFIG_ENCAPSULATION_MPLS)
 
 #define PEER_UPDGRP_CAP_FLAGS (PEER_CAP_AS4_RCV)
 

bgpd/bgpd.c
@@ -5053,6 +5053,8 @@ static const struct peer_flag_action peer_af_flag_action_list[] = {
 	{ PEER_FLAG_ACCEPT_OWN, 0, peer_change_reset },
 	{ PEER_FLAG_SEND_EXT_COMMUNITY_RPKI, 1, peer_change_reset_out },
 	{ PEER_FLAG_ADDPATH_RX_PATHS_LIMIT, 0, peer_change_none },
+	{ PEER_FLAG_CONFIG_ENCAPSULATION_SRV6, 1, peer_change_reset_out },
+	{ PEER_FLAG_CONFIG_ENCAPSULATION_MPLS, 1, peer_change_reset_out },
 	{ 0, 0, 0 }
 };
 

bgpd/bgpd.h
@@ -1768,6 +1768,8 @@ struct peer {
 #define PEER_FLAG_SEND_EXT_COMMUNITY_RPKI (1ULL << 29)
 #define PEER_FLAG_ADDPATH_RX_PATHS_LIMIT (1ULL << 30)
 #define PEER_FLAG_CONFIG_DAMPENING (1ULL << 31)
+#define PEER_FLAG_CONFIG_ENCAPSULATION_SRV6	(1ULL << 32)
+#define PEER_FLAG_CONFIG_ENCAPSULATION_MPLS	(1ULL << 33)
 #define PEER_FLAG_ACCEPT_OWN (1ULL << 63)
 
 	enum bgp_addpath_strat addpath_type[AFI_MAX][SAFI_MAX];

tests/bgpd/test_peer_attr.c
@@ -422,6 +422,18 @@ static struct test_peer_attr test_peer_attrs[] = {
 			| PEER_FLAG_MED_UNCHANGED,
 	},
 	{
+		.cmd = "encapsulation-mpls",
+		.families[0] = {.afi = AFI_IP, .safi = SAFI_MPLS_VPN},
+		.families[1] = {.afi = AFI_IP6, .safi = SAFI_MPLS_VPN},
+		.u.flag = PEER_FLAG_CONFIG_ENCAPSULATION_MPLS,
+	},
+	{
+		.cmd = "encapsulation-srv6",
+		.families[0] = {.afi = AFI_IP, .safi = SAFI_MPLS_VPN},
+		.families[1] = {.afi = AFI_IP6, .safi = SAFI_MPLS_VPN},
+		.u.flag = PEER_FLAG_CONFIG_ENCAPSULATION_SRV6,
+	},
+	{
 		.cmd = "capability orf prefix-list send",
 		.u.flag = PEER_FLAG_ORF_PREFIX_SM,
 	},

tests/bgpd/test_peer_attr.py
@@ -68,6 +68,10 @@ TestFlag.okfail("peer\\ipv4-unicast\\attribute-unchanged as-path next-hop med")
 TestFlag.okfail("peer\\ipv4-multicast\\attribute-unchanged as-path next-hop med")
 TestFlag.okfail("peer\\ipv6-unicast\\attribute-unchanged as-path next-hop med")
 TestFlag.okfail("peer\\ipv6-multicast\\attribute-unchanged as-path next-hop med")
+TestFlag.okfail("peer\\ipv4-vpn\\encapsulation-mpls")
+TestFlag.okfail("peer\\ipv6-vpn\\encapsulation-mpls")
+TestFlag.okfail("peer\\ipv4-vpn\\encapsulation-srv6")
+TestFlag.okfail("peer\\ipv6-vpn\\encapsulation-srv6")
 TestFlag.okfail("peer\\ipv4-unicast\\capability orf prefix-list send")
 TestFlag.okfail("peer\\ipv4-multicast\\capability orf prefix-list send")
 TestFlag.okfail("peer\\ipv6-unicast\\capability orf prefix-list send")

commit f7871679e2f5e161d88aeaab84cf1a37635d4973

bgpd: fix show running-config encapsulation-[mpls/srv6]
When a VPN neighbor is configured as both MPLS and SRv6, the show
running-config is wrong:

> router bgp 1
> address-family ipv4 vpn
> neighbor 1::1 encapsulation-srv6

Fixes: 8e6bb4a07078 ("bgpd: add neighbor <encapsulation-srv6|encapsulation-mpls> command")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_vty.c
@@ -19889,7 +19889,7 @@ static void bgp_config_write_peer_af(struct vty *vty, struct bgp *bgp,
 	else if (peergroup_af_flag_check(peer, afi, safi,
 					 PEER_FLAG_CONFIG_ENCAPSULATION_SRV6_RELAX))
 		vty_out(vty, "  neighbor %s encapsulation-srv6-relax\n", addr);
-	else if (peergroup_af_flag_check(peer, afi, safi, PEER_FLAG_CONFIG_ENCAPSULATION_MPLS))
+	if (peergroup_af_flag_check(peer, afi, safi, PEER_FLAG_CONFIG_ENCAPSULATION_MPLS))
 		vty_out(vty, "  neighbor %s encapsulation-mpls\n", addr);
 
 	/* Filter. */