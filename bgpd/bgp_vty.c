FRRouting/frr

commit f8dafef42b694de723df256765df37f8405ef48e

bgpd: avoid socket activation for VRF instances
Refactor `bgp_need_listening()` to prevent unnecessary reactivation of
BGP listening sockets for non default vrf instances that are already
active.

This function now checks whether a listener is already associated with
the BGP instance before attempting to open a new socket.

Also modify `bgp_get()` to skip socket setup for VRF instances, which
are now handled more appropriately elsewhere based on configuration
changes like `neighbor` or `listen-range`.

Signed-off-by: Francois Dumontet <francois.dumontet@6wind.com>

bgpd/bgp_vty.c
@@ -4718,6 +4718,24 @@ static struct peer_group *listen_range_exists(struct bgp *bgp,
 	return NULL;
 }
 
+static void bgp_need_listening(struct bgp *bgp, struct vty *vty)
+{
+	struct listnode *node;
+	struct bgp_listener *listener;
+
+	for (ALL_LIST_ELEMENTS_RO(bm->listen_sockets, node, listener)) {
+		if (listener->bgp == bgp)
+			break;
+	}
+	if (listener == NULL) {
+		struct vrf *vrf;
+
+		SET_FLAG(bgp->flags, BGP_FLAG_VRF_MAY_LISTEN);
+		vrf = bgp_vrf_lookup_by_instance_type(bgp);
+		bgp_handle_socket(bgp, vrf, VRF_UNKNOWN, true);
+	}
+}
+
 DEFUN (bgp_listen_range,
        bgp_listen_range_cmd,
        "bgp listen range <A.B.C.D/M|X:X::X:X/M> peer-group PGNAME",
@@ -4783,6 +4801,9 @@ DEFUN (bgp_listen_range,
 		vty_out(vty, "%% Configure the peer-group first\n");
 		return CMD_WARNING_CONFIG_FAILED;
 	}
+
+	/* if need start listening */
+	bgp_need_listening(bgp, vty);
 
 	ret = peer_group_listen_range_add(group, &range);
 	return bgp_vty_return(vty, ret);
@@ -4935,6 +4956,8 @@ static int peer_remote_as_vty(struct vty *vty, const char *peer_str,
 					"%% Create the peer-group or interface first\n");
 				return CMD_WARNING_CONFIG_FAILED;
 			}
+			/* if need start listening */
+			bgp_need_listening(bgp, vty);
 			return CMD_SUCCESS;
 		}
 	} else {
@@ -4943,6 +4966,9 @@ static int peer_remote_as_vty(struct vty *vty, const char *peer_str,
 				"%% Can not configure the local system as neighbor\n");
 			return CMD_WARNING_CONFIG_FAILED;
 		}
+
+		/* if need start listening */
+		bgp_need_listening(bgp, vty);
 		ret = peer_remote_as(bgp, &su, NULL, &as, as_type, as_str);
 	}
 

bgpd/bgpd.h
@@ -667,6 +667,7 @@ struct bgp {
 #define BGP_FLAG_L3VNI_SCHEDULE_FOR_INSTALL (1ULL << 41)
 #define BGP_FLAG_L3VNI_SCHEDULE_FOR_DELETE  (1ULL << 42)
 #define BGP_FLAG_LINK_LOCAL_CAPABILITY	    (1ULL << 43)
+#define BGP_FLAG_VRF_MAY_LISTEN		    (1ULL << 44)
 
 	/* BGP default address-families.
 	 * New peers inherit enabled afi/safis from bgp instance.

commit df7b1910abeedc822343643ac626b6fc84021f9d

bgpd: Activate listening socket for a default VRF when created
The intention was to stop/start listening sockets for VRF instances, but not
for a default VRF. This is especially useful for VRF leaking, when non-default
VRFs do not have any peers configured.

Fixes: f8dafef42b694de723df256765df37f8405ef48e ("bgpd: avoid socket activation for VRF instances")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -1469,6 +1469,24 @@ static int peer_flag_unset_vty(struct vty *vty, const char *ip_str,
 	return peer_flag_modify_vty(vty, ip_str, flag, 0);
 }
 
+static void bgp_need_listening(struct bgp *bgp, struct vty *vty)
+{
+	struct listnode *node;
+	struct bgp_listener *listener;
+
+	for (ALL_LIST_ELEMENTS_RO(bm->listen_sockets, node, listener)) {
+		if (listener->bgp == bgp)
+			break;
+	}
+	if (listener == NULL) {
+		struct vrf *vrf;
+
+		SET_FLAG(bgp->flags, BGP_FLAG_VRF_MAY_LISTEN);
+		vrf = bgp_vrf_lookup_by_instance_type(bgp);
+		bgp_handle_socket(bgp, vrf, VRF_UNKNOWN, true);
+	}
+}
+
 #include "bgpd/bgp_vty_clippy.c"
 
 DEFUN_HIDDEN (bgp_local_mac,
@@ -1666,8 +1684,10 @@ DEFUN_NOSH (router_bgp,
 		 * any pending VRF-VPN leaking that was configured via
 		 * earlier "router bgp X vrf FOO" blocks.
 		 */
-		if (inst_type == BGP_INSTANCE_TYPE_DEFAULT)
+		if (inst_type == BGP_INSTANCE_TYPE_DEFAULT) {
+			bgp_need_listening(bgp, vty);
 			vpn_leak_postchange_all();
+		}
 
 		if (inst_type == BGP_INSTANCE_TYPE_VRF || IS_BGP_INSTANCE_HIDDEN(bgp)) {
 			bgp_vpn_leak_export(bgp);
@@ -4852,25 +4872,6 @@ static void bgp_may_stop_listening(struct bgp *bgp, struct vty *vty)
 	vrf = bgp_vrf_lookup_by_instance_type(bgp);
 	bgp_handle_socket(bgp, vrf, VRF_UNKNOWN, false);
 	UNSET_FLAG(bgp->flags, BGP_FLAG_VRF_MAY_LISTEN);
-}
-
-
-static void bgp_need_listening(struct bgp *bgp, struct vty *vty)
-{
-	struct listnode *node;
-	struct bgp_listener *listener;
-
-	for (ALL_LIST_ELEMENTS_RO(bm->listen_sockets, node, listener)) {
-		if (listener->bgp == bgp)
-			break;
-	}
-	if (listener == NULL) {
-		struct vrf *vrf;
-
-		SET_FLAG(bgp->flags, BGP_FLAG_VRF_MAY_LISTEN);
-		vrf = bgp_vrf_lookup_by_instance_type(bgp);
-		bgp_handle_socket(bgp, vrf, VRF_UNKNOWN, true);
-	}
 }
 
 DEFUN (bgp_listen_range,