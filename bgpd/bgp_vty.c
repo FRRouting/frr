5d5e22360a68816c249c9ad7e28f5398f0a81c37
Loïc Sang <loic.sang@6wind.com>
2025-11-12 13:30:39 +0100

    bgpd: fix labeled-unicast output
    "show bgp neighbors xxxx" always returns "0 accepted prefixes":
    ...
    > For address family: IPv4 Labeled Unicast
    >  Update group 1, subgroup 1
    >  Packet Queue length 0
    >  Inbound soft reconfiguration allowed
    >  Community attribute sent to this neighbor(all)
    >  0 accepted prefixes
    ...
    
    Received prefixes live in unicast table.
    Update topotest.
    
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>
    (cherry picked from commit 9c2b9189ecc844a49885d333b623bf135aac1a88)
    
    # Conflicts:
    #	tests/topotests/bgp_lu_topo1/test_bgp_lu.py

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -13604,6 +13604,7 @@ static void bgp_show_peer_afi(struct vty *vty, struct peer *p, afi_t afi,
 			      safi_t safi, bool use_json,
 			      json_object *json_neigh)
 {
+	int pfx_rcd_safi;
 	struct bgp_filter *filter;
 	struct peer_af *paf;
 	char orf_pfx_name[BUFSIZ];
@@ -13612,6 +13613,11 @@ static void bgp_show_peer_afi(struct vty *vty, struct peer *p, afi_t afi,
 	json_object *json_prefA = NULL;
 	json_object *json_addr = NULL;
 	json_object *json_advmap = NULL;
+
+	if (safi == SAFI_LABELED_UNICAST)
+		pfx_rcd_safi = SAFI_UNICAST;
+	else
+		pfx_rcd_safi = safi;
 
 	if (use_json) {
 		json_addr = json_object_new_object();
@@ -13895,7 +13901,7 @@ static void bgp_show_peer_afi(struct vty *vty, struct peer *p, afi_t afi,
 
 		/* Receive prefix count */
 		json_object_int_add(json_addr, "acceptedPrefixCounter",
-				    p->pcount[afi][safi]);
+				    p->pcount[afi][pfx_rcd_safi]);
 		if (paf && PAF_SUBGRP(paf))
 			json_object_int_add(json_addr, "sentPrefixCounter",
 						(PAF_SUBGRP(paf))->scount);
@@ -14198,10 +14204,9 @@ static void bgp_show_peer_afi(struct vty *vty, struct peer *p, afi_t afi,
 		paf = peer_af_find(p, afi, safi);
 		if (paf && PAF_SUBGRP(paf))
 			vty_out(vty, "  %u accepted, %u sent prefixes\n",
-				p->pcount[afi][safi], PAF_SUBGRP(paf)->scount);
+				p->pcount[afi][pfx_rcd_safi], PAF_SUBGRP(paf)->scount);
 		else
-			vty_out(vty, "  %u accepted prefixes\n",
-				p->pcount[afi][safi]);
+			vty_out(vty, "  %u accepted prefixes\n", p->pcount[afi][pfx_rcd_safi]);
 
 		/* maximum-prefix-out */
 		if (CHECK_FLAG(p->af_flags[afi][safi],
