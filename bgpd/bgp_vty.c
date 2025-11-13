FRRouting/frr

commit 4c80ab09dfd7f83abaf92c7fc3643aa32b4a07ad

bgpd: add srv6-only command per bgp instance
Today, if SRv6 is used with the 'segment-routing srv6 / locator' command
under BGP, then it is not possible to export BGP VPN prefixes other than
SRv6 ones. For instance, when configuring a VRF with MPLS exportation,
the resulting VPN paths will not be valid, as the nexthop tracking
only considers SRv6 VPN paths, due to 'segment-routing srv6 / locator'
config.

Add a command that does autorise to export VPN updates which have no
SRv6 options when an srv6 locator is configured. This change will permit
to benefit from both MPLS and SRv6 updates coming from the same VRF.

Note that the command has been done to handle backward compat; in
further changes, having 'no srv6-only' command will be mandatory if any
user wants to support both MPLS and SRv6 VPN paths to co-exist together.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_vty.c
@@ -11213,6 +11213,29 @@ DEFPY (bgp_srv6_locator,
 	return CMD_SUCCESS;
 }
 
+DEFPY (bgp_srv6_only,
+       bgp_srv6_only_cmd,
+       "[no] srv6-only",
+       NO_STR
+       "Only allow SRv6 and disallow MPLS routes\n")
+{
+	VTY_DECLVAR_CONTEXT(bgp, bgp);
+
+	if (!no == bgp->srv6_only)
+		return CMD_SUCCESS;
+
+	/* pre-change */
+	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
+	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
+
+	bgp->srv6_only = !no;
+
+	/* post-change */
+	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
+	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
+	return CMD_SUCCESS;
+}
+
 DEFPY (no_bgp_srv6_locator,
        no_bgp_srv6_locator_cmd,
        "no locator NAME$name",
@@ -20236,7 +20259,7 @@ int bgp_config_write(struct vty *vty)
 		if (bgp->fast_convergence)
 			vty_out(vty, " bgp fast-convergence\n");
 
-		if (bgp_srv6_locator_is_configured(bgp) ||
+		if (bgp_srv6_locator_is_configured(bgp) || bgp->srv6_only == false ||
 		    bgp->srv6_encap_behavior != SRV6_HEADEND_BEHAVIOR_H_ENCAPS) {
 			vty_frame(vty, " !\n segment-routing srv6\n");
 			if (strlen(bgp->srv6_locator_name))
@@ -20245,6 +20268,9 @@ int bgp_config_write(struct vty *vty)
 			if (bgp->srv6_encap_behavior != SRV6_HEADEND_BEHAVIOR_H_ENCAPS)
 				vty_out(vty, "  encap-behavior %s\n",
 					srv6_headend_behavior2str(bgp->srv6_encap_behavior, true));
+			if (bgp->srv6_only == false)
+				vty_out(vty, "  no srv6-only\n");
+
 			vty_endframe(vty, " exit\n");
 		}
 
@@ -22370,6 +22396,7 @@ void bgp_vty_init(void)
 	install_element(BGP_NODE, &no_bgp_segment_routing_srv6_cmd);
 	install_element(BGP_SRV6_NODE, &bgp_srv6_locator_cmd);
 	install_element(BGP_SRV6_NODE, &no_bgp_srv6_locator_cmd);
+	install_element(BGP_SRV6_NODE, &bgp_srv6_only_cmd);
 	install_element(BGP_SRV6_NODE, &bgp_srv6_encap_behavior_cmd);
 	install_element(BGP_IPV4_NODE, &af_sid_vpn_export_cmd);
 	install_element(BGP_IPV6_NODE, &af_sid_vpn_export_cmd);

bgpd/bgp_mplsvpn.h
@@ -365,7 +365,7 @@ static inline bool is_pi_srv6_valid(struct bgp_path_info *pi, struct bgp *bgp_ne
 				    safi_t safi)
 {
 	if (!pi->attr->srv6_l3vpn && !pi->attr->srv6_vpn)
-		return false;
+		return !bgp_nexthop->srv6_only;
 
 	/* imported paths from VPN: srv6 enabled and nht reachability
 	 * are enough to know if that path is valid

bgpd/bgpd.c
@@ -1519,6 +1519,7 @@ static void bgp_srv6_init(struct bgp *bgp)
 	bgp->srv6_locator_chunks->del = srv6_locator_chunk_list_free;
 	bgp->srv6_functions = list_new();
 	bgp->srv6_functions->del = (void (*)(void *))srv6_function_free;
+	bgp->srv6_only = true;
 }
 
 static void bgp_srv6_cleanup(struct bgp *bgp)

bgpd/bgpd.h
@@ -961,6 +961,7 @@ struct bgp {
 	struct srv6_locator *tovpn_sid_locator;
 	uint32_t tovpn_sid_transpose_label;
 	struct in6_addr *tovpn_zebra_vrf_sid_last_sent;
+	bool srv6_only;
 
 	/* TCP keepalive parameters for BGP connection */
 	uint16_t tcp_keepalive_idle;

commit 4271530dff3019cd86fa6cfc176b4717be5df37c

bgpd: fix srv6-only command defaulted when 'no segment-routing ipv6'
When the 'no segment-routing ipv6' command is applied, the current
srv6-only configuration is still used; this makes think that the
previous command is ineffective. Fix this by defaulting the value to
false.

Fixes: 4c80ab09dfd7 ("bgpd: add srv6-only command per bgp instance")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

bgpd/bgp_vty.c
@@ -11411,6 +11411,20 @@ static void bgp_segment_routing_srv6_hencaps_refresh(struct bgp *bgp)
 		bgp_zebra_update_srv6_encap_routes(bgp_inst, AFI_IP6, bgp, true);
 	}
 }
+
+static void bgp_srv6_only_change(struct bgp *bgp, bool enable)
+{
+	/* pre-change */
+	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
+	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
+
+	bgp->srv6_only = enable;
+
+	/* post-change */
+	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
+	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
+}
+
 DEFUN (no_bgp_segment_routing_srv6,
        no_bgp_segment_routing_srv6_cmd,
        "no segment-routing srv6",
@@ -11428,6 +11442,9 @@ DEFUN (no_bgp_segment_routing_srv6,
 		bgp->srv6_encap_behavior = SRV6_HEADEND_BEHAVIOR_H_ENCAPS;
 		bgp_segment_routing_srv6_hencaps_refresh(bgp);
 	}
+
+	if (bgp->srv6_only)
+		bgp_srv6_only_change(bgp, false);
 
 	return CMD_SUCCESS;
 }
@@ -11505,16 +11522,7 @@ DEFPY (bgp_srv6_only,
 
 	if ((!no && bgp->srv6_only) || (no && !bgp->srv6_only))
 		return CMD_SUCCESS;
-
-	/* pre-change */
-	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
-	vpn_leak_prechange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
-
-	bgp->srv6_only = !no;
-
-	/* post-change */
-	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP, bgp_get_default(), bgp);
-	vpn_leak_postchange(BGP_VPN_POLICY_DIR_TOVPN, AFI_IP6, bgp_get_default(), bgp);
+	bgp_srv6_only_change(bgp, !no);
 	return CMD_SUCCESS;
 }
 