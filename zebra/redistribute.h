fad011074d71eb74d82dad447bee24acd588af8f
hashemi_me <hashemi_me@office.zharfpouyan.net>
2025-11-15 11:22:05 -0600

    zebra: fix the bugs and add json format
    json format has been added.
    Bugs have been fixed.
    
    Signed-off-by: awsomcoder <mehranstock1383@gmail.com>

--- zebra/redistribute.h
+++ zebra/redistribute.h
@@ -69,8 +69,7 @@ extern int zebra_import_table_config(struct vty *, vrf_id_t vrf_id);
 
 extern void zebra_import_table_rm_update(const char *rmap);
 
-bool zebra_redistribute_check(const struct route_node *rn,
-			      const struct route_entry *re,
+bool zebra_redistribute_check(const struct route_node *rn, const struct route_entry *re,
 			      struct zserv *client);
 
 #ifdef __cplusplus
