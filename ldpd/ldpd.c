9ca6a21b1e102a07b7d377ab4b1548e2eef4d723
Philippe Guibert <philippe.guibert@6wind.com>
2025-12-09 17:57:01 +0100

    ldpd: duplicate l2vpn_del() for ldp use cases
    L2vpn objects will be handled either in the shared library for common
    config, or in LDP code for its own purpose. The memory management of
    both objects will be separate. This commit introduces a new ldp_l2vpn_del()
    command inherited from l2vpn_del() commmand, and will be used for ldp
    purpose only.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- ldpd/ldpd.c
+++ ldpd/ldpd.c
@@ -1292,6 +1292,33 @@ void ldp_config_reset_l2vpns(struct l2vpn_head *conf_l2vpn_tree)
 	}
 }
 
+static void ldp_l2vpn_del(struct l2vpn *l2vpn)
+{
+	struct l2vpn_if *lif;
+	struct l2vpn_pw *pw;
+
+	while (!RB_EMPTY(l2vpn_if_head, &l2vpn->if_tree)) {
+		lif = RB_ROOT(l2vpn_if_head, &l2vpn->if_tree);
+
+		RB_REMOVE(l2vpn_if_head, &l2vpn->if_tree, lif);
+		free(lif);
+	}
+	while (!RB_EMPTY(l2vpn_pw_head, &l2vpn->pw_tree)) {
+		pw = RB_ROOT(l2vpn_pw_head, &l2vpn->pw_tree);
+
+		RB_REMOVE(l2vpn_pw_head, &l2vpn->pw_tree, pw);
+		free(pw);
+	}
+	while (!RB_EMPTY(l2vpn_pw_head, &l2vpn->pw_inactive_tree)) {
+		pw = RB_ROOT(l2vpn_pw_head, &l2vpn->pw_inactive_tree);
+
+		RB_REMOVE(l2vpn_pw_head, &l2vpn->pw_inactive_tree, pw);
+		free(pw);
+	}
+
+	free(l2vpn);
+}
+
 void
 ldp_clear_config(struct ldpd_conf *xconf)
 {
@@ -1325,7 +1352,7 @@ ldp_clear_config(struct ldpd_conf *xconf)
 		l2vpn = RB_ROOT(l2vpn_head, &xconf->l2vpn_tree);
 
 		RB_REMOVE(l2vpn_head, &xconf->l2vpn_tree, l2vpn);
-		l2vpn_del(l2vpn);
+		ldp_l2vpn_del(l2vpn);
 	}
 
 	free(xconf);
@@ -1813,7 +1840,7 @@ void merge_l2vpns(struct l2vpn_head *dst, struct l2vpn_head *src)
 				break;
 			}
 			RB_REMOVE(l2vpn_head, dst, l2vpn);
-			l2vpn_del(l2vpn);
+			ldp_l2vpn_del(l2vpn);
 		}
 	}
 	RB_FOREACH_SAFE (xl, l2vpn_head, src, ltmp) {
