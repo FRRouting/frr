FRRouting/frr

commit b90cb00974ef84beff603fb0e91a7a38b3a1b6a4

lib: Add ZAPI command `ZEBRA_SRV6_SID_NOTIFY`
Add a new ZAPI command `ZEBRA_SRV6_SID_NOTIFY` used by zebra to send
asynchronous SRv6 SIDs notifications to zclients.

Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

lib/zclient.c
@@ -2133,6 +2133,30 @@ stream_failure:
 	return false;
 }
 
+bool zapi_srv6_sid_notify_decode(struct stream *s, struct srv6_sid_ctx *ctx,
+				 struct in6_addr *sid_value, uint32_t *func,
+				 uint32_t *wide_func,
+				 enum zapi_srv6_sid_notify *note)
+{
+	uint32_t f, wf;
+
+	STREAM_GET(note, s, sizeof(*note));
+	STREAM_GET(ctx, s, sizeof(struct srv6_sid_ctx));
+	STREAM_GET(sid_value, s, sizeof(struct in6_addr));
+	STREAM_GETL(s, f);
+	STREAM_GETL(s, wf);
+
+	if (func)
+		*func = f;
+	if (wide_func)
+		*wide_func = wf;
+
+	return true;
+
+stream_failure:
+	return false;
+}
+
 struct nexthop *nexthop_from_zapi_nexthop(const struct zapi_nexthop *znh)
 {
 	struct nexthop *n = nexthop_new();

lib/log.c
@@ -464,7 +464,8 @@ static const struct zebra_desc_table command_types[] = {
 	DESC_ENTRY(ZEBRA_TC_CLASS_DELETE),
 	DESC_ENTRY(ZEBRA_TC_FILTER_ADD),
 	DESC_ENTRY(ZEBRA_TC_FILTER_DELETE),
-	DESC_ENTRY(ZEBRA_OPAQUE_NOTIFY)
+	DESC_ENTRY(ZEBRA_OPAQUE_NOTIFY),
+	DESC_ENTRY(ZEBRA_SRV6_SID_NOTIFY)
 };
 #undef DESC_ENTRY
 

lib/zclient.h
@@ -238,6 +238,7 @@ typedef enum {
 	ZEBRA_TC_FILTER_ADD,
 	ZEBRA_TC_FILTER_DELETE,
 	ZEBRA_OPAQUE_NOTIFY,
+	ZEBRA_SRV6_SID_NOTIFY,
 } zebra_message_types_t;
 /* Zebra message types. Please update the corresponding
  * command_types array with any changes!
@@ -764,6 +765,13 @@ enum zapi_iptable_notify_owner {
 	ZAPI_IPTABLE_FAIL_REMOVE,
 };
 
+enum zapi_srv6_sid_notify {
+	ZAPI_SRV6_SID_FAIL_ALLOC = 0,
+	ZAPI_SRV6_SID_ALLOCATED,
+	ZAPI_SRV6_SID_RELEASED,
+	ZAPI_SRV6_SID_FAIL_RELEASE,
+};
+
 enum zclient_send_status {
 	ZCLIENT_SEND_FAILURE = -1,
 	ZCLIENT_SEND_SUCCESS = 0,
@@ -810,6 +818,28 @@ zapi_rule_notify_owner2str(enum zapi_rule_notify_owner note)
 		break;
 	case ZAPI_RULE_REMOVED:
 		ret = "ZAPI_RULE_REMOVED";
+		break;
+	}
+
+	return ret;
+}
+
+static inline const char *zapi_srv6_sid_notify2str(enum zapi_srv6_sid_notify note)
+{
+	const char *ret = "UNKNOWN";
+
+	switch (note) {
+	case ZAPI_SRV6_SID_FAIL_ALLOC:
+		ret = "ZAPI_SRV6_SID_FAIL_ALLOC";
+		break;
+	case ZAPI_SRV6_SID_ALLOCATED:
+		ret = "ZAPI_SRV6_SID_ALLOCATED";
+		break;
+	case ZAPI_SRV6_SID_FAIL_RELEASE:
+		ret = "ZAPI_SRV6_SID_FAIL_RELEASE";
+		break;
+	case ZAPI_SRV6_SID_RELEASED:
+		ret = "ZAPI_SRV6_SID_RELEASED";
 		break;
 	}
 
@@ -1144,6 +1174,10 @@ bool zapi_rule_notify_decode(struct stream *s, uint32_t *seqno,
 bool zapi_ipset_notify_decode(struct stream *s,
 			      uint32_t *unique,
 			     enum zapi_ipset_notify_owner *note);
+bool zapi_srv6_sid_notify_decode(struct stream *s, struct srv6_sid_ctx *ctx,
+				 struct in6_addr *sid_value, uint32_t *func,
+				 uint32_t *wide_func,
+				 enum zapi_srv6_sid_notify *note);
 
 /* Nexthop-group message apis */
 extern enum zclient_send_status

commit 04d79f1c86a321f399c5341db5c04d1741dc0ee8

lib: fix add \0 to the end of the locator
Two consecutive SID notifications for the same L3VRF, but with locators
with similar naming; the second notification may fail. The below srv6
configuration:

> segment-routing
>  srv6
>   locators
>    locator dmytro
>     prefix 2002:db8:1:1::/64
>    exit
>    !
>    locator dmytro2
>     prefix 2004:db8:1:1::/64
>    exit

SID retrieval from BGP may result in a flooding of allocation/releases

> 2025/11/25 21:29:55 BGP: [Q7Z3T-FD5F4] SRv6 SID 2002:db8:1:1:1:: End.DT4 vrf_id 7 (Vrf20) : locator prefix mismatch (dmytro2), releasing it (pfx 2004:db8:1:1::/64).
> 2025/11/25 21:29:55 BGP: [Q7Z3T-FD5F4] SRv6 SID 2002:db8:1:1:1:: End.DT4 vrf_id 7 (Vrf20) : locator prefix mismatch (dmytro2), releasing it (pfx 2004:db8:1:1::/64).
> 2025/11/25 21:29:55 BGP: [Q7Z3T-FD5F4] SRv6 SID 2002:db8:1:1:1:: End.DT4 vrf_id 7 (Vrf20) : locator prefix mismatch (dmytro2), releasing it (pfx 2004:db8:1:1::/64).
> 2025/11/25 21:29:55 BGP: [Q7Z3T-FD5F4] SRv6 SID 2002:db8:1:1:1:: End.DT4 vrf_id 7 (Vrf20) : locator prefix mismatch (dmytro2), releasing it (pfx 2004:db8:1:1::/64).
> 2025/11/25 21:29:55 BGP: [Q7Z3T-FD5F4] SRv6 SID 2002:db8:1:1:1:: End.DT4 vrf_id 7 (Vrf20) : locator prefix mismatch (dmytro2), releasing it (pfx 2004:db8:1:1::/64).

Actually, dmytro2 is bound to 2004:db8, not 2002:db8. The static memory
zone where locator name is stored has not been appended with a
termination character. Fix this.

Fixes: b90cb00974ef8 ("lib: Add ZAPI command `ZEBRA_SRV6_SID_NOTIFY`")

Signed-off-by: Dmytro Shytyi <dmytro.shytyi@6wind.com>
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

lib/zclient.c
@@ -2370,6 +2370,7 @@ bool zapi_srv6_sid_notify_decode(struct stream *s, struct srv6_sid_ctx *ctx,
 			*p_locator_name = NULL;
 		else {
 			STREAM_GET(locator_name, s, len);
+			locator_name[len] = '\0';
 			*p_locator_name = locator_name;
 		}
 	}