40894852404b4f3b463c572b922ea64cb6e8322b
Louis Scalbert <louis.scalbert@6wind.com>
2025-12-15 13:13:54 +0100

    bgpd: fix no vrf import command
    The following adds a BGP VRF context if it was not present:
    
    > r2# show bgp vrfs
    > Type  Id     routerId          #PeersCfg  #PeersEstb  Name
    >              L3-VNI            RouterMAC              Interface
    > DFLT  0      10.0.0.2          2          2           default
    >              0                 00:00:00:00:00:00      unknown
    >
    > Total number of VRFs (including default): 2
    > r2# conf t
    > r2(config)# router bgp 65002
    > r2(config-router)# address-family ipv4 unicast
    > r2(config-router-af)# no import vrf vrf20
    > r2(config-router-af)# do show bgp vrfs
    > Type  Id     routerId          #PeersCfg  #PeersEstb  Name
    >              L3-VNI            RouterMAC              Interface
    > DFLT  0      10.0.0.2          2          2           default
    >              0                 00:00:00:00:00:00      unknown
    >  VRF  8      192.168.0.1       0          0           vrf10
    >              10                ce:48:e6:20:a5:45      br10
    >  VRF  -1     0.0.0.0           0          0           vrf20
    >              0                 00:00:00:00:00:00      unknown
    >
    > Total number of VRFs (including default): 3
    
    Do not add a new VRF context when trying to remove a VRF import. Just
    remove the import from the VRF list and unimport the prefix if the
    context was existing.
    
    Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

--- bgpd/bgp_mplsvpn.h
+++ bgpd/bgp_mplsvpn.h
@@ -91,7 +91,7 @@ extern void transpose_sid(struct in6_addr *sid, uint32_t label, uint8_t offset,
 			  uint8_t size);
 extern void vrf_import_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
 				afi_t afi, safi_t safi);
-void vrf_unimport_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp,
+void vrf_unimport_from_vrf(struct bgp *to_bgp, struct bgp *from_bgp, const char *import_name,
 			   afi_t afi, safi_t safi);
 
 static inline bool is_bgp_vrf_mplsvpn(struct bgp *bgp)
