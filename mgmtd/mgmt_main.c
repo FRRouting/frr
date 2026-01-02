935e39222926013304cc0a2d94e2d7c0d8de5e71
Christian Hopps <chopps@labn.net>
2026-01-02 07:14:37 +0000

    mgmtd: vty_terminate() before client cleanup
    - Need to vty_terminate() (i.e., vty_close our vtys) before we cleanup the mgmt
    client sessions associated with those vtys. vty_terminate() is normally done in
    frr_fini(), but we also need to do our client cleanup before calling frr_fini()
    as we need the event system still initialized and frr_fini() cleans it up.
    
    Signed-off-by: Christian Hopps <chopps@labn.net>
    (cherry picked from commit 3d4aa9659ea48baaa547de8d15695896407dfe54)
    
    # Conflicts:
    #	mgmtd/mgmt_main.c

--- mgmtd/mgmt_main.c
+++ mgmtd/mgmt_main.c
@@ -114,6 +114,14 @@ static FRR_NORETURN void mgmt_exit(int status)
 	/* it only makes sense for this to be called on a clean exit */
 	assert(status == 0);
 
+<<<<<<< HEAD
+=======
+	/* frr_fini() calls this but we need our vtys close before we terminate the client */
+	vty_terminate();
+
+	vty_mgmt_terminate();
+
+>>>>>>> 3d4aa9659 (mgmtd: vty_terminate() before client cleanup)
 	frr_early_fini();
 
 	/* stop pthreads (if any) */
