ee7012ecdb6d3a36f62b68094943fdf60e902392
Kyrylo Yatsenko <hedrok@gmail.com>
2026-01-06 21:37:28 +0200

    Revert "tools: Allow deleting of interfaces"
    This reverts commit f1a1703a0a2c2ecd81f50ccd384dcdb7e0c1a793.
    
    The commit may have fixed some issues when deleting all interface
    configuration for removed interface, but it introduced new issue: when
    interface is still active, frr-reload.py tries to run 'no interface' for
    it when all interface configuration is removed, which results in
    "only inactive interfaces can be deleted" error.
    
    Signed-off-by: Kyrylo Yatsenko <hedrok@gmail.com>

--- tools/frr-reload.py
+++ tools/frr-reload.py
@@ -1799,13 +1799,12 @@ def compare_context_objects(newconf, running):
                 delete_bgpd = True
                 lines_to_del.append((running_ctx_keys, None))
 
-            elif running_ctx_keys[0].startswith("interface"):
-                lines_to_del.append((running_ctx_keys, None))
-
-            # We cannot do 'no vrf' in FRR, and so deal with it
-            elif running_ctx_keys[0].startswith("vrf") or running_ctx_keys[
-                0
-            ].startswith("router pim"):
+            # We cannot do 'no interface' or 'no vrf' in FRR, and so deal with it
+            elif (
+                running_ctx_keys[0].startswith("interface")
+                or running_ctx_keys[0].startswith("vrf")
+                or running_ctx_keys[0].startswith("router pim")
+            ):
                 for line in running_ctx.lines:
                     lines_to_del.append((running_ctx_keys, line))
 
