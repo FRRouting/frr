FRRouting/frr

commit c951ee6eeeace451f89bfafbbf2ce9e9d554c22f

isisd: add support for Topology Independent LFA (TI-LFA)
TI-LFA is a modern fast-reroute (FRR) solution that leverages Segment
Routing to pre-compute backup nexthops for all destinations in the
network, helping to reduce traffic restoration times whenever a
failure occurs. The backup nexthops are expected to be installed
in the FIB so that they can be activated as soon as a failure
is detected, making sub-50ms recovery possible (assuming an
hierarchical FIB).

TI-LFA is a huge step forward compared to prior IP-FRR solutions,
like classic LFA and Remote LFA, as it guarantees 100% coverage
for all destinations. This is possible thanks to the source routing
capabilities of SR, which allows the backup nexthops to steer traffic
around the failures (using as many SIDs as necessary). In addition
to that, the repair paths always follow the post-convergence SPF
tree, which prevents transient congestions and suboptimal routing
from happening.

Deploying TI-LFA is very simple as it only requires a single
configuration command for each interface that needs to be protected
(both link protection and node protection are available). In addition
to IPv4 and IPv6 routes, SR Prefix-SIDs and Adj-SIDs are also
protected by the backup nexthops computed by the TI-LFA algorithms.

Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

isisd/isis_route.c
@@ -76,8 +76,9 @@ static struct isis_nexthop *isis_nexthop_create(int family, union g_addr *ip,
 	return nexthop;
 }
 
-static void isis_nexthop_delete(struct isis_nexthop *nexthop)
+void isis_nexthop_delete(struct isis_nexthop *nexthop)
 {
+	XFREE(MTYPE_ISIS_NEXTHOP_LABELS, nexthop->label_stack);
 	XFREE(MTYPE_ISIS_NEXTHOP, nexthop);
 }
 
@@ -115,8 +116,9 @@ static struct isis_nexthop *nexthoplookup(struct list *nexthops, int family,
 	return NULL;
 }
 
-static void adjinfo2nexthop(int family, struct list *nexthops,
-			    struct isis_adjacency *adj)
+void adjinfo2nexthop(int family, struct list *nexthops,
+		     struct isis_adjacency *adj,
+		     struct mpls_label_stack *label_stack)
 {
 	struct isis_nexthop *nh;
 	union g_addr ip = {};
@@ -132,6 +134,7 @@ static void adjinfo2nexthop(int family, struct list *nexthops,
 					AF_INET, &ip,
 					adj->circuit->interface->ifindex);
 				memcpy(nh->sysid, adj->sysid, sizeof(nh->sysid));
+				nh->label_stack = label_stack;
 				listnode_add(nexthops, nh);
 				break;
 			}
@@ -147,6 +150,7 @@ static void adjinfo2nexthop(int family, struct list *nexthops,
 					AF_INET6, &ip,
 					adj->circuit->interface->ifindex);
 				memcpy(nh->sysid, adj->sysid, sizeof(nh->sysid));
+				nh->label_stack = label_stack;
 				listnode_add(nexthops, nh);
 				break;
 			}
@@ -160,13 +164,15 @@ static void adjinfo2nexthop(int family, struct list *nexthops,
 }
 
 static void isis_route_add_dummy_nexthops(struct isis_route_info *rinfo,
-					  const uint8_t *sysid)
+					  const uint8_t *sysid,
+					  struct mpls_label_stack *label_stack)
 {
 	struct isis_nexthop *nh;
 
 	nh = XCALLOC(MTYPE_ISIS_NEXTHOP, sizeof(struct isis_nexthop));
 	memcpy(nh->sysid, sysid, sizeof(nh->sysid));
 	isis_sr_nexthop_reset(&nh->sr);
+	nh->label_stack = label_stack;
 	listnode_add(rinfo->nexthops, nh);
 }
 
@@ -186,13 +192,15 @@ static struct isis_route_info *isis_route_info_new(struct prefix *prefix,
 	for (ALL_LIST_ELEMENTS_RO(adjacencies, node, vadj)) {
 		struct isis_spf_adj *sadj = vadj->sadj;
 		struct isis_adjacency *adj = sadj->adj;
+		struct mpls_label_stack *label_stack = vadj->label_stack;
 
 		/*
 		 * Create dummy nexthops when running SPF on a testing
 		 * environment.
 		 */
 		if (CHECK_FLAG(im->options, F_ISIS_UNIT_TEST)) {
-			isis_route_add_dummy_nexthops(rinfo, sadj->id);
+			isis_route_add_dummy_nexthops(rinfo, sadj->id,
+						      label_stack);
 			continue;
 		}
 
@@ -219,7 +227,8 @@ static struct isis_route_info *isis_route_info_new(struct prefix *prefix,
 				 prefix->family);
 			exit(1);
 		}
-		adjinfo2nexthop(prefix->family, rinfo->nexthops, adj);
+		adjinfo2nexthop(prefix->family, rinfo->nexthops, adj,
+				label_stack);
 	}
 
 	rinfo->cost = cost;
@@ -237,6 +246,12 @@ static void isis_route_info_delete(struct isis_route_info *route_info)
 	}
 
 	XFREE(MTYPE_ISIS_ROUTE_INFO, route_info);
+}
+
+void isis_route_node_cleanup(struct route_table *table, struct route_node *node)
+{
+	if (node->info)
+		isis_route_info_delete(node->info);
 }
 
 static int isis_route_info_same(struct isis_route_info *new,
@@ -412,6 +427,7 @@ static void isis_route_update(struct isis_area *area, struct prefix *prefix,
 
 static void _isis_route_verify_table(struct isis_area *area,
 				     struct route_table *table,
+				     struct route_table *table_backup,
 				     struct route_table **tables)
 {
 	struct route_node *rnode, *drnode;
@@ -432,6 +448,19 @@ static void _isis_route_verify_table(struct isis_area *area,
 		srcdest_rnode_prefixes(rnode,
 				       (const struct prefix **)&dst_p,
 				       (const struct prefix **)&src_p);
+
+		/* Link primary route to backup route. */
+		if (table_backup) {
+			struct route_node *rnode_bck;
+
+			rnode_bck = srcdest_rnode_lookup(table_backup, dst_p,
+							 src_p);
+			if (rnode_bck) {
+				rinfo->backup = rnode_bck->info;
+				UNSET_FLAG(rinfo->flag,
+					   ISIS_ROUTE_FLAG_ZEBRA_SYNCED);
+			}
+		}
 
 #ifdef EXTREME_DEBUG
 		if (IS_DEBUG_RTE_EVENTS) {
@@ -490,9 +519,10 @@ static void _isis_route_verify_table(struct isis_area *area,
 	}
 }
 
-void isis_route_verify_table(struct isis_area *area, struct route_table *table)
+void isis_route_verify_table(struct isis_area *area, struct route_table *table,
+			     struct route_table *table_backup)
 {
-	_isis_route_verify_table(area, table, NULL);
+	_isis_route_verify_table(area, table, table_backup, NULL);
 }
 
 /* Function to validate route tables for L1L2 areas. In this case we can't use
@@ -507,9 +537,13 @@ void isis_route_verify_table(struct isis_area *area, struct route_table *table)
  * to the RIB with different zebra route types and let RIB handle this? */
 void isis_route_verify_merge(struct isis_area *area,
 			     struct route_table *level1_table,
-			     struct route_table *level2_table)
+			     struct route_table *level1_table_backup,
+			     struct route_table *level2_table,
+			     struct route_table *level2_table_backup)
 {
-	struct route_table *tables[] = { level1_table, level2_table };
+	struct route_table *tables[] = {level1_table, level2_table};
+	struct route_table *tables_backup[] = {level1_table_backup,
+					       level2_table_backup};
 	struct route_table *merge;
 	struct route_node *rnode, *mrnode;
 
@@ -519,6 +553,8 @@ void isis_route_verify_merge(struct isis_area *area,
 		for (rnode = route_top(tables[level - 1]); rnode;
 		     rnode = srcdest_route_next(rnode)) {
 			struct isis_route_info *rinfo = rnode->info;
+			struct route_node *rnode_bck;
+
 			if (!rinfo)
 				continue;
 
@@ -528,6 +564,16 @@ void isis_route_verify_merge(struct isis_area *area,
 			srcdest_rnode_prefixes(rnode,
 					       (const struct prefix **)&prefix,
 					       (const struct prefix **)&src_p);
+
+			/* Link primary route to backup route. */
+			rnode_bck = srcdest_rnode_lookup(
+				tables_backup[level - 1], prefix, src_p);
+			if (rnode_bck) {
+				rinfo->backup = rnode_bck->info;
+				UNSET_FLAG(rinfo->flag,
+					   ISIS_ROUTE_FLAG_ZEBRA_SYNCED);
+			}
+
 			mrnode = srcdest_rnode_get(merge, prefix, src_p);
 			struct isis_route_info *mrinfo = mrnode->info;
 			if (mrinfo) {
@@ -566,7 +612,7 @@ void isis_route_verify_merge(struct isis_area *area,
 		}
 	}
 
-	_isis_route_verify_table(area, merge, tables);
+	_isis_route_verify_table(area, merge, NULL, tables);
 	route_table_finish(merge);
 }
 
@@ -580,6 +626,14 @@ void isis_route_invalidate_table(struct isis_area *area,
 			continue;
 		rinfo = rode->info;
 
+		if (rinfo->backup) {
+			rinfo->backup = NULL;
+			/*
+			 * For now, always force routes that have backup
+			 * nexthops to be reinstalled.
+			 */
+			UNSET_FLAG(rinfo->flag, ISIS_ROUTE_FLAG_ZEBRA_SYNCED);
+		}
 		UNSET_FLAG(rinfo->flag, ISIS_ROUTE_FLAG_ACTIVE);
 	}
 }

isisd/isis_circuit.h
@@ -141,6 +141,8 @@ struct isis_circuit {
 	bool disable_threeway_adj;
 	struct bfd_info *bfd_info;
 	struct ldp_sync_info *ldp_sync_info;
+	bool tilfa_protection[ISIS_LEVELS];
+	bool tilfa_node_protection[ISIS_LEVELS];
 	/*
 	 * Counters as in 10589--11.2.5.9
 	 */

isisd/isis_lfa.h
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2020  NetDEF, Inc.
+ *                     Renato Westphal
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _FRR_ISIS_LFA_H
+#define _FRR_ISIS_LFA_H
+
+enum isis_tilfa_sid_type {
+	TILFA_SID_PREFIX = 1,
+	TILFA_SID_ADJ,
+};
+
+struct isis_tilfa_sid {
+	enum isis_tilfa_sid_type type;
+	union {
+		uint32_t index;
+		mpls_label_t label;
+	} value;
+};
+
+RB_HEAD(isis_spf_nodes, isis_spf_node);
+RB_PROTOTYPE(isis_spf_nodes, isis_spf_node, entry, isis_spf_node_compare)
+struct isis_spf_node {
+	RB_ENTRY(isis_spf_node) entry;
+
+	/* Node's System ID. */
+	uint8_t sysid[ISIS_SYS_ID_LEN];
+
+	/* Local adjacencies over which this node is reachable. */
+	struct list *adjacencies;
+
+	/* Best metric of all adjacencies used to reach this node. */
+	uint32_t best_metric;
+
+	struct {
+		/* Node's forward SPT. */
+		struct isis_spftree *spftree;
+
+		/* Node's reverse SPT. */
+		struct isis_spftree *spftree_reverse;
+
+		/* Node's P-space. */
+		struct isis_spf_nodes p_space;
+	} lfa;
+};
+
+enum lfa_protection_type {
+	LFA_LINK_PROTECTION = 1,
+	LFA_NODE_PROTECTION,
+};
+
+struct lfa_protected_resource {
+	/* The protection type. */
+	enum lfa_protection_type type;
+
+	/* The protected adjacency (might be a pseudonode). */
+	uint8_t adjacency[ISIS_SYS_ID_LEN + 1];
+
+	/* List of nodes reachable over the protected interface. */
+	struct isis_spf_nodes nodes;
+};
+
+/* Forward declaration(s). */
+struct isis_vertex;
+
+/* Prototypes. */
+void isis_spf_node_list_init(struct isis_spf_nodes *nodes);
+void isis_spf_node_list_clear(struct isis_spf_nodes *nodes);
+struct isis_spf_node *isis_spf_node_new(struct isis_spf_nodes *nodes,
+					const uint8_t *sysid);
+struct isis_spf_node *isis_spf_node_find(const struct isis_spf_nodes *nodes,
+					 const uint8_t *sysid);
+bool isis_lfa_excise_adj_check(const struct isis_spftree *spftree,
+			       const uint8_t *id);
+bool isis_lfa_excise_node_check(const struct isis_spftree *spftree,
+				const uint8_t *id);
+struct isis_spftree *isis_spf_reverse_run(const struct isis_spftree *spftree);
+int isis_spf_run_neighbors(struct isis_spftree *spftree);
+void isis_spf_run_lfa(struct isis_area *area, struct isis_spftree *spftree);
+int isis_lfa_check(struct isis_spftree *spftree, struct isis_vertex *vertex);
+struct isis_spftree *
+isis_tilfa_compute(struct isis_area *area, struct isis_spftree *spftree,
+		   struct isis_spftree *spftree_reverse,
+		   struct lfa_protected_resource *protected_resource);
+
+#endif /* _FRR_ISIS_LFA_H */

isisd/isis_memory.c
@@ -39,6 +39,7 @@ DEFINE_MTYPE(ISISD, ISIS_SPFTREE, "ISIS SPFtree")
 DEFINE_MTYPE(ISISD, ISIS_VERTEX, "ISIS vertex")
 DEFINE_MTYPE(ISISD, ISIS_ROUTE_INFO, "ISIS route info")
 DEFINE_MTYPE(ISISD, ISIS_NEXTHOP, "ISIS nexthop")
+DEFINE_MTYPE(ISISD, ISIS_NEXTHOP_LABELS, "ISIS nexthop MPLS labels")
 DEFINE_MTYPE(ISISD, ISIS_DICT, "ISIS dictionary")
 DEFINE_MTYPE(ISISD, ISIS_DICT_NODE, "ISIS dictionary node")
 DEFINE_MTYPE(ISISD, ISIS_EXT_ROUTE, "ISIS redistributed route")

isisd/isis_memory.h
@@ -38,6 +38,7 @@ DECLARE_MTYPE(ISIS_SPFTREE)
 DECLARE_MTYPE(ISIS_VERTEX)
 DECLARE_MTYPE(ISIS_ROUTE_INFO)
 DECLARE_MTYPE(ISIS_NEXTHOP)
+DECLARE_MTYPE(ISIS_NEXTHOP_LABELS)
 DECLARE_MTYPE(ISIS_DICT)
 DECLARE_MTYPE(ISIS_DICT_NODE)
 DECLARE_MTYPE(ISIS_EXT_ROUTE)

isisd/isis_nb_config.c
@@ -43,6 +43,7 @@
 #include "isisd/isis_csm.h"
 #include "isisd/isis_adjacency.h"
 #include "isisd/isis_spf.h"
+#include "isisd/isis_spf_private.h"
 #include "isisd/isis_te.h"
 #include "isisd/isis_memory.h"
 #include "isisd/isis_mt.h"
@@ -2945,14 +2946,23 @@ int lib_interface_isis_mpls_holddown_destroy(struct nb_cb_destroy_args *args)
 int lib_interface_isis_fast_reroute_level_1_ti_lfa_enable_modify(
 	struct nb_cb_modify_args *args)
 {
-	switch (args->event) {
-	case NB_EV_VALIDATE:
-	case NB_EV_PREPARE:
-	case NB_EV_ABORT:
-	case NB_EV_APPLY:
-		/* TODO: implement me. */
-		break;
+	struct isis_area *area;
+	struct isis_circuit *circuit;
+
+	if (args->event != NB_EV_APPLY)
+		return NB_OK;
+
+	circuit = nb_running_get_entry(args->dnode, NULL, true);
+	circuit->tilfa_protection[0] = yang_dnode_get_bool(args->dnode, NULL);
+	if (circuit->tilfa_protection[0])
+		circuit->area->lfa_protected_links[0]++;
+	else {
+		assert(circuit->area->lfa_protected_links[0] > 0);
+		circuit->area->lfa_protected_links[0]--;
 	}
+
+	area = circuit->area;
+	lsp_regenerate_schedule(area, area->is_type, 0);
 
 	return NB_OK;
 }
@@ -2964,14 +2974,18 @@ int lib_interface_isis_fast_reroute_level_1_ti_lfa_enable_modify(
 int lib_interface_isis_fast_reroute_level_1_ti_lfa_node_protection_modify(
 	struct nb_cb_modify_args *args)
 {
-	switch (args->event) {
-	case NB_EV_VALIDATE:
-	case NB_EV_PREPARE:
-	case NB_EV_ABORT:
-	case NB_EV_APPLY:
-		/* TODO: implement me. */
-		break;
-	}
+	struct isis_area *area;
+	struct isis_circuit *circuit;
+
+	if (args->event != NB_EV_APPLY)
+		return NB_OK;
+
+	circuit = nb_running_get_entry(args->dnode, NULL, true);
+	circuit->tilfa_node_protection[0] =
+		yang_dnode_get_bool(args->dnode, NULL);
+
+	area = circuit->area;
+	lsp_regenerate_schedule(area, area->is_type, 0);
 
 	return NB_OK;
 }
@@ -2983,14 +2997,23 @@ int lib_interface_isis_fast_reroute_level_1_ti_lfa_node_protection_modify(
 int lib_interface_isis_fast_reroute_level_2_ti_lfa_enable_modify(
 	struct nb_cb_modify_args *args)
 {
-	switch (args->event) {
-	case NB_EV_VALIDATE:
-	case NB_EV_PREPARE:
-	case NB_EV_ABORT:
-	case NB_EV_APPLY:
-		/* TODO: implement me. */
-		break;
+	struct isis_area *area;
+	struct isis_circuit *circuit;
+
+	if (args->event != NB_EV_APPLY)
+		return NB_OK;
+
+	circuit = nb_running_get_entry(args->dnode, NULL, true);
+	circuit->tilfa_protection[1] = yang_dnode_get_bool(args->dnode, NULL);
+	if (circuit->tilfa_protection[1])
+		circuit->area->lfa_protected_links[1]++;
+	else {
+		assert(circuit->area->lfa_protected_links[1] > 0);
+		circuit->area->lfa_protected_links[1]--;
 	}
+
+	area = circuit->area;
+	lsp_regenerate_schedule(area, area->is_type, 0);
 
 	return NB_OK;
 }
@@ -3002,14 +3025,18 @@ int lib_interface_isis_fast_reroute_level_2_ti_lfa_enable_modify(
 int lib_interface_isis_fast_reroute_level_2_ti_lfa_node_protection_modify(
 	struct nb_cb_modify_args *args)
 {
-	switch (args->event) {
-	case NB_EV_VALIDATE:
-	case NB_EV_PREPARE:
-	case NB_EV_ABORT:
-	case NB_EV_APPLY:
-		/* TODO: implement me. */
-		break;
-	}
+	struct isis_area *area;
+	struct isis_circuit *circuit;
+
+	if (args->event != NB_EV_APPLY)
+		return NB_OK;
+
+	circuit = nb_running_get_entry(args->dnode, NULL, true);
+	circuit->tilfa_node_protection[1] =
+		yang_dnode_get_bool(args->dnode, NULL);
+
+	area = circuit->area;
+	lsp_regenerate_schedule(area, area->is_type, 0);
 
 	return NB_OK;
 }

isisd/isis_route.h
@@ -33,6 +33,7 @@ struct isis_nexthop {
 	union g_addr ip;
 	uint8_t sysid[ISIS_SYS_ID_LEN];
 	struct sr_nexthop_info sr;
+	struct mpls_label_stack *label_stack;
 };
 
 struct isis_route_info {
@@ -43,6 +44,7 @@ struct isis_route_info {
 	uint32_t cost;
 	uint32_t depth;
 	struct list *nexthops;
+	struct isis_route_info *backup;
 };
 
 DECLARE_HOOK(isis_route_update_hook,
@@ -50,6 +52,10 @@ DECLARE_HOOK(isis_route_update_hook,
 	      struct isis_route_info *route_info),
 	     (area, prefix, route_info))
 
+void isis_nexthop_delete(struct isis_nexthop *nexthop);
+void adjinfo2nexthop(int family, struct list *nexthops,
+		     struct isis_adjacency *adj,
+		     struct mpls_label_stack *label_stack);
 struct isis_route_info *isis_route_create(struct prefix *prefix,
 					  struct prefix_ipv6 *src_p,
 					  uint32_t cost,
@@ -60,16 +66,22 @@ struct isis_route_info *isis_route_create(struct prefix *prefix,
 
 /* Walk the given table and install new routes to zebra and remove old ones.
  * route status is tracked using ISIS_ROUTE_FLAG_ACTIVE */
-void isis_route_verify_table(struct isis_area *area,
-			     struct route_table *table);
+void isis_route_verify_table(struct isis_area *area, struct route_table *table,
+			     struct route_table *table_backup);
 
 /* Same as isis_route_verify_table, but merge L1 and L2 routes before */
 void isis_route_verify_merge(struct isis_area *area,
 			     struct route_table *level1_table,
-			     struct route_table *level2_table);
+			     struct route_table *level1_table_backup,
+			     struct route_table *level2_table,
+			     struct route_table *level2_table_backup);
 
 /* Unset ISIS_ROUTE_FLAG_ACTIVE on all routes. Used before running spf. */
 void isis_route_invalidate_table(struct isis_area *area,
 				 struct route_table *table);
+
+/* Cleanup route node when freeing routing table. */
+void isis_route_node_cleanup(struct route_table *table,
+			     struct route_node *node);
 
 #endif /* _ZEBRA_ISIS_ROUTE_H */

commit 4afd1012fd6d8a097d40aad69821c982a4366602

isisd: fix memory issues in LFA backup route handling
Fix two related memory management issues:

1/ Double-free in adjinfo2nexthop(): The function was storing direct
pointers to label_stack and srv6_seg_stack from the vertex adjacency
list without copying. When the adjacency list was deleted after backup
route creation, these pointers became dangling. Fix by using
label_stack_dup() and srv6_seg_stack_dup() to create independent copies.

2/ Memory leak in isis_rlfa_activate(): When RLFA is reactivated for
the same vertex (e.g., receiving updated LDP responses), a new
label_stack was allocated without freeing the existing one. Fix by
freeing the old label_stack before allocating a new one.

Fixes: c951ee6eeeac ("isisd: add support for Topology Independent LFA (TI-LFA)")
Signed-off-by: Vincent Jardin <vjardin@free.fr>

isisd/isis_lfa.c
@@ -1723,6 +1723,9 @@ int isis_rlfa_activate(struct isis_spftree *spftree, struct rlfa *rlfa,
 		if (vadj->sr.present && vadj->sr.label != MPLS_LABEL_IMPLICIT_NULL)
 			num_labels++;
 
+		/* Free any existing label stack before allocating new one. */
+		XFREE(MTYPE_ISIS_NEXTHOP_LABELS, vadj->label_stack);
+
 		/* Allocate label stack. */
 		label_stack = XCALLOC(MTYPE_ISIS_NEXTHOP_LABELS,
 				      sizeof(struct mpls_label_stack) +

isisd/isis_route.c
@@ -191,8 +191,8 @@ void adjinfo2nexthop(int family, struct list *nexthops, struct isis_adjacency *a
 				memcpy(nh->sysid, adj->sysid, sizeof(nh->sysid));
 				if (sr)
 					nh->sr = *sr;
-				nh->label_stack = label_stack;
-				nh->srv6_seg_stack = srv6_seg_stack;
+				nh->label_stack = label_stack_dup(label_stack);
+				nh->srv6_seg_stack = srv6_seg_stack_dup(srv6_seg_stack);
 				listnode_add(nexthops, nh);
 				break;
 			}
@@ -209,8 +209,8 @@ void adjinfo2nexthop(int family, struct list *nexthops, struct isis_adjacency *a
 				memcpy(nh->sysid, adj->sysid, sizeof(nh->sysid));
 				if (sr)
 					nh->sr = *sr;
-				nh->label_stack = label_stack;
-				nh->srv6_seg_stack = srv6_seg_stack;
+				nh->label_stack = label_stack_dup(label_stack);
+				nh->srv6_seg_stack = srv6_seg_stack_dup(srv6_seg_stack);
 				listnode_add(nexthops, nh);
 				break;
 			}
@@ -224,14 +224,16 @@ void adjinfo2nexthop(int family, struct list *nexthops, struct isis_adjacency *a
 
 static void isis_route_add_dummy_nexthops(struct isis_route_info *rinfo, const uint8_t *sysid,
 					  struct isis_sr_psid_info *sr,
-					  struct mpls_label_stack *label_stack)
+					  struct mpls_label_stack *label_stack,
+					  struct isis_srv6_seg_stack *srv6_seg_stack)
 {
 	struct isis_nexthop *nh;
 
 	nh = XCALLOC(MTYPE_ISIS_NEXTHOP, sizeof(struct isis_nexthop));
 	memcpy(nh->sysid, sysid, sizeof(nh->sysid));
 	nh->sr = *sr;
-	nh->label_stack = label_stack;
+	nh->label_stack = label_stack_dup(label_stack);
+	nh->srv6_seg_stack = srv6_seg_stack_dup(srv6_seg_stack);
 	listnode_add(rinfo->nexthops, nh);
 }
 
@@ -259,7 +261,8 @@ static struct isis_route_info *isis_route_info_new(struct prefix *prefix,
 		 * environment.
 		 */
 		if (CHECK_FLAG(im->options, F_ISIS_UNIT_TEST)) {
-			isis_route_add_dummy_nexthops(rinfo, sadj->id, vsr, label_stack);
+			isis_route_add_dummy_nexthops(rinfo, sadj->id, vsr, label_stack,
+						      srv6_seg_stack);
 			if (!allow_ecmp)
 				break;
 			continue;