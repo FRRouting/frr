8756f2d79642ec9034751dcb21269c4e83847cff
Donald Sharp <sharpd@nvidia.com>
2026-01-15 11:54:02 -0500

    zebra: Add a basic context passing structure for the sysmgr
    Add a initial context passing structure for the new sysmgr
    code.  This is not really used at this point in time.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_sysmgr.c
+++ zebra/zebra_sysmgr.c
@@ -7,16 +7,117 @@
 #include <zebra.h>
 
 #include "lib/frr_pthread.h"
+#include "memory.h"
 #include "frrevent.h"
 
 #include "zebra/zebra_sysmgr.h"
+#include "zebra/zebra_memory.h"
 
 static struct zebra_sysmgr_globals {
 	_Atomic uint32_t run;
 	struct frr_pthread *pthread;
 	struct event_loop *master;
 	struct event *t_work;
+	struct zebra_sysmgr_ctx_q in_q;
+	struct zebra_sysmgr_ctx_q out_q;
 } zsysmgr;
+
+DEFINE_MTYPE_STATIC(ZEBRA, SYSMGR_CTX, "System manager context");
+
+struct zebra_sysmgr_ctx {
+	enum sysmgr_op_e op;
+	enum zebra_sysmgr_result status;
+	uint64_t corr_id;
+	union {
+		struct {
+			uint32_t flags;
+		} generic;
+	} u;
+
+	struct sysmgr_ctx_list_item entries;
+};
+
+DECLARE_DLIST(sysmgr_ctx_list, struct zebra_sysmgr_ctx, entries);
+
+struct zebra_sysmgr_ctx *zebra_sysmgr_ctx_alloc(void)
+{
+	struct zebra_sysmgr_ctx *ctx;
+
+	ctx = XCALLOC(MTYPE_SYSMGR_CTX, sizeof(*ctx));
+	ctx->status = ZEBRA_SYSMGR_STATUS_NONE;
+
+	return ctx;
+}
+
+void zebra_sysmgr_ctx_reset(struct zebra_sysmgr_ctx *ctx)
+{
+	if (!ctx)
+		return;
+
+	memset(ctx, 0, sizeof(*ctx));
+}
+
+void zebra_sysmgr_ctx_fini(struct zebra_sysmgr_ctx **pctx)
+{
+	if (!pctx || !*pctx)
+		return;
+
+	XFREE(MTYPE_SYSMGR_CTX, *pctx);
+	*pctx = NULL;
+}
+
+void zebra_sysmgr_ctx_q_init(struct zebra_sysmgr_ctx_q *q)
+{
+	pthread_mutex_init(&q->mutex, NULL);
+	sysmgr_ctx_list_init(&q->list);
+}
+
+void zebra_sysmgr_ctx_q_fini(struct zebra_sysmgr_ctx_q *q)
+{
+	pthread_mutex_destroy(&q->mutex);
+}
+
+void zebra_sysmgr_ctx_q_enqueue(struct zebra_sysmgr_ctx_q *q, struct zebra_sysmgr_ctx *ctx)
+{
+	pthread_mutex_lock(&q->mutex);
+	sysmgr_ctx_list_add_tail(&q->list, ctx);
+	pthread_mutex_unlock(&q->mutex);
+}
+
+struct zebra_sysmgr_ctx *zebra_sysmgr_ctx_q_dequeue(struct zebra_sysmgr_ctx_q *q)
+{
+	struct zebra_sysmgr_ctx *ctx;
+
+	pthread_mutex_lock(&q->mutex);
+	ctx = sysmgr_ctx_list_pop(&q->list);
+	pthread_mutex_unlock(&q->mutex);
+
+	return ctx;
+}
+
+void zebra_sysmgr_ctx_q_append(struct zebra_sysmgr_ctx_q *to, struct sysmgr_ctx_list_head *from)
+{
+	pthread_mutex_lock(&to->mutex);
+	while (true) {
+		struct zebra_sysmgr_ctx *ctx = sysmgr_ctx_list_pop(from);
+
+		if (!ctx)
+			break;
+		sysmgr_ctx_list_add_tail(&to->list, ctx);
+	}
+	pthread_mutex_unlock(&to->mutex);
+}
+
+uint32_t zebra_sysmgr_ctx_q_count(struct zebra_sysmgr_ctx_q *q)
+{
+	uint32_t count;
+
+	pthread_mutex_lock(&q->mutex);
+	count = sysmgr_ctx_list_count(&q->list);
+	pthread_mutex_unlock(&q->mutex);
+
+	return count;
+}
 
 static void sysmgr_thread_loop(struct event *event)
 {
@@ -29,6 +130,8 @@ static void sysmgr_thread_loop(struct event *event)
 void zebra_sysmgr_init(void)
 {
 	memset(&zsysmgr, 0, sizeof(zsysmgr));
+	zebra_sysmgr_ctx_q_init(&zsysmgr.in_q);
+	zebra_sysmgr_ctx_q_init(&zsysmgr.out_q);
 }
 
 void zebra_sysmgr_start(void)
@@ -62,5 +165,6 @@ void zebra_sysmgr_stop(void)
 
 void zebra_sysmgr_finish(void)
 {
-	/* Nothing to free yet; queues will be torn down here later. */
+	zebra_sysmgr_ctx_q_fini(&zsysmgr.in_q);
+	zebra_sysmgr_ctx_q_fini(&zsysmgr.out_q);
 }
--- zebra/zebra_sysmgr.h
+++ zebra/zebra_sysmgr.h
@@ -5,6 +5,7 @@
  */
 
 #include <zebra.h>
+#include "lib/typesafe.h"
 
 #ifndef _ZEBRA_SYSMGR_H
 #define _ZEBRA_SYSMGR_H 1
@@ -31,5 +32,63 @@ void zebra_sysmgr_stop(void);
  * all module cleanup is complete.
  */
 void zebra_sysmgr_finish(void);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Operations that the system manager can process.
+ */
+enum sysmgr_op_e {
+	SM_OP_NONE = 0,
+};
+
+/*
+ * Result codes returned to the zebra main context.
+ */
+enum zebra_sysmgr_result {
+	ZEBRA_SYSMGR_STATUS_NONE = 0,
+	ZEBRA_SYSMGR_REQUEST_QUEUED,
+	ZEBRA_SYSMGR_REQUEST_SUCCESS,
+	ZEBRA_SYSMGR_REQUEST_FAILURE,
+};
+
+/*
+ * Context blocks are exchanged between zebra and the system manager thread.
+ */
+PREDECL_DLIST(sysmgr_ctx_list);
+
+struct zebra_sysmgr_ctx;
+
+/* Allocate a context object */
+struct zebra_sysmgr_ctx *zebra_sysmgr_ctx_alloc(void);
+
+/*
+ * Reset an allocated context object for re-use.
+ */
+void zebra_sysmgr_ctx_reset(struct zebra_sysmgr_ctx *ctx);
+
+/*
+ * Free a context object and set the caller's pointer to NULL.
+ */
+void zebra_sysmgr_ctx_fini(struct zebra_sysmgr_ctx **pctx);
+
+/* Queue helper with internal mutex */
+struct zebra_sysmgr_ctx_q {
+	pthread_mutex_t mutex;
+	struct sysmgr_ctx_list_head list;
+};
+
+void zebra_sysmgr_ctx_q_init(struct zebra_sysmgr_ctx_q *q);
+void zebra_sysmgr_ctx_q_fini(struct zebra_sysmgr_ctx_q *q);
+void zebra_sysmgr_ctx_q_enqueue(struct zebra_sysmgr_ctx_q *q, struct zebra_sysmgr_ctx *ctx);
+struct zebra_sysmgr_ctx *zebra_sysmgr_ctx_q_dequeue(struct zebra_sysmgr_ctx_q *q);
+void zebra_sysmgr_ctx_q_append(struct zebra_sysmgr_ctx_q *to, struct sysmgr_ctx_list_head *from);
+uint32_t zebra_sysmgr_ctx_q_count(struct zebra_sysmgr_ctx_q *q);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _ZEBRA_SYSMGR_H */
