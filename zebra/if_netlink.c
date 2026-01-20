FRRouting/frr

commit 2e06e5b729e4a32b3841a4ef185e8d31fdbf9b7d

zebra: Allow tentative IPv6 addresses on DOWN interfaces
On DOWN interfaces, DAD cannot complete, so IPv6 addresses remain
tentative indefinitely. Modify address filtering to skip tentative
addresses only on UP interfaces (where DAD is in progress).

This allows daemons to receive address notifications for DOWN
interfaces to maintain consistent interface state.

Ticket: #
Signed-off-by: Rajesh Varatharaj <rvaratharaj@nvidia.com>

zebra/if_netlink.c
@@ -1226,11 +1226,18 @@ int netlink_interface_addr_dplane(struct nlmsghdr *h, ns_id_t ns_id,
 		/* Only consider valid addresses; we'll not get a kernel
 		 * notification till IPv6 DAD has completed, but at init
 		 * time, FRR does query for and will receive all addresses.
+		 *
+		 * Exception: If the interface is operationally down, we allow
+		 * tentative addresses because DAD cannot run on a down link.
+		 * The address will remain tentative indefinitely until the
+		 * interface comes up. Daemons may need to track interface
+		 * addresses even when the link is down.
 		 */
 		if (h->nlmsg_type == RTM_NEWADDR
-		    && (kernel_flags & (IFA_F_DADFAILED | IFA_F_TENTATIVE))) {
+		    && (kernel_flags & IFA_F_DADFAILED)) {
+			/* DAD failed - always skip these addresses */
 			if (IS_ZEBRA_DEBUG_KERNEL)
-				zlog_debug("%s: %s: Invalid/tentative addr",
+				zlog_debug("%s: %s: DAD failed addr",
 					   __func__,
 					   nl_msg_type_to_str(h->nlmsg_type));
 
@@ -1238,6 +1245,30 @@ int netlink_interface_addr_dplane(struct nlmsghdr *h, ns_id_t ns_id,
 				 0, 5);
 
 			return 0;
+		}
+
+		if (h->nlmsg_type == RTM_NEWADDR
+		    && (kernel_flags & IFA_F_TENTATIVE)) {
+			/* Tentative address - check if interface is down */
+			struct interface *ifp = if_lookup_by_index_per_ns(
+				zebra_ns_lookup(ns_id), ifa->ifa_index);
+
+			if (ifp && if_is_operative(ifp)) {
+				/* Interface is UP - skip tentative (DAD in progress) */
+				if (IS_ZEBRA_DEBUG_KERNEL)
+					zlog_debug("%s: %s: Tentative addr on UP interface, skipping",
+						   __func__,
+						   nl_msg_type_to_str(h->nlmsg_type));
+
+				frrtrace(3, frr_zebra, netlink_msg_err,
+					 nl_msg_type_to_str(h->nlmsg_type), 0, 5);
+				return 0;
+			}
+			/* Interface is DOWN - process tentative address */
+			if (IS_ZEBRA_DEBUG_KERNEL)
+				zlog_debug("%s: %s: Tentative addr on DOWN interface - accepted (DAD cannot run)",
+					   __func__,
+					   nl_msg_type_to_str(h->nlmsg_type));
 		}
 	}
 

commit df00a58f7aa5ecef2b15bde309ffc395086e9db8

zebra: Fix tentative address handling to respect dplane data model
The original commit accessed interface pointers in the dplane pthread
via if_lookup_by_index_per_ns(), breaking the data model. Fix by
passing IFA_F_TENTATIVE through the dplane context and moving the
interface operative check to zebra_if_addr_update_ctx() in the
master pthread.

Fixes: 2e06e5b729e4 ("zebra: Allow tentative IPv6 addresses on DOWN interfaces")
Ref: https://github.com/FRRouting/frr/pull/20526

Signed-off-by: Rajesh Varatharaj <rvaratharaj@nvidia.com>

zebra/if_netlink.c
@@ -1226,12 +1226,6 @@ int netlink_interface_addr_dplane(struct nlmsghdr *h, ns_id_t ns_id,
 		/* Only consider valid addresses; we'll not get a kernel
 		 * notification till IPv6 DAD has completed, but at init
 		 * time, FRR does query for and will receive all addresses.
-		 *
-		 * Exception: If the interface is operationally down, we allow
-		 * tentative addresses because DAD cannot run on a down link.
-		 * The address will remain tentative indefinitely until the
-		 * interface comes up. Daemons may need to track interface
-		 * addresses even when the link is down.
 		 */
 		if (h->nlmsg_type == RTM_NEWADDR
 		    && (kernel_flags & IFA_F_DADFAILED)) {
@@ -1246,30 +1240,9 @@ int netlink_interface_addr_dplane(struct nlmsghdr *h, ns_id_t ns_id,
 
 			return 0;
 		}
-
-		if (h->nlmsg_type == RTM_NEWADDR
-		    && (kernel_flags & IFA_F_TENTATIVE)) {
-			/* Tentative address - check if interface is down */
-			struct interface *ifp = if_lookup_by_index_per_ns(
-				zebra_ns_lookup(ns_id), ifa->ifa_index);
-
-			if (ifp && if_is_operative(ifp)) {
-				/* Interface is UP - skip tentative (DAD in progress) */
-				if (IS_ZEBRA_DEBUG_KERNEL)
-					zlog_debug("%s: %s: Tentative addr on UP interface, skipping",
-						   __func__,
-						   nl_msg_type_to_str(h->nlmsg_type));
-
-				frrtrace(3, frr_zebra, netlink_msg_err,
-					 nl_msg_type_to_str(h->nlmsg_type), 0, 5);
-				return 0;
-			}
-			/* Interface is DOWN - process tentative address */
-			if (IS_ZEBRA_DEBUG_KERNEL)
-				zlog_debug("%s: %s: Tentative addr on DOWN interface - accepted (DAD cannot run)",
-					   __func__,
-					   nl_msg_type_to_str(h->nlmsg_type));
-		}
+		/* Note: IFA_F_TENTATIVE is passed via context to main pthread
+		 * for handling - we cannot access interface pointers here.
+		 */
 	}
 
 	/* logic copied from iproute2/ip/ipaddress.c:print_addrinfo() */
@@ -1346,6 +1319,9 @@ int netlink_interface_addr_dplane(struct nlmsghdr *h, ns_id_t ns_id,
 
 	if (kernel_flags & IFA_F_NOPREFIXROUTE)
 		dplane_ctx_intf_set_noprefixroute(ctx);
+
+	if (kernel_flags & IFA_F_TENTATIVE)
+		dplane_ctx_intf_set_tentative(ctx);
 
 	/* Label */
 	if (tb[IFA_LABEL]) {

zebra/interface.c
@@ -1275,6 +1275,25 @@ static void zebra_if_addr_update_ctx(struct zebra_dplane_ctx *ctx,
 			   dplane_op2str(dplane_ctx_get_op(ctx)), ifp->name,
 			   ifp->ifindex, addr);
 
+	/* Handle tentative IPv6 addresses:
+	 * - On UP interfaces: skip tentative addresses (DAD in progress)
+	 * - On DOWN interfaces: accept tentative addresses (DAD cannot run)
+	 */
+	if (op == DPLANE_OP_INTF_ADDR_ADD && addr->family == AF_INET6 &&
+	    dplane_ctx_intf_is_tentative(ctx)) {
+		if (if_is_operative(ifp)) {
+			if (IS_ZEBRA_DEBUG_KERNEL)
+				zlog_debug("%s: %s: Tentative addr %pFX on UP interface %s, skipping",
+					   __func__,
+					   dplane_op2str(op), addr, ifp->name);
+			return;
+		}
+		if (IS_ZEBRA_DEBUG_KERNEL)
+			zlog_debug("%s: %s: Tentative addr %pFX on DOWN interface %s - accepted (DAD cannot run)",
+				   __func__, dplane_op2str(op), addr,
+				   ifp->name);
+	}
+
 	/* Is there a peer or broadcast address? */
 	dest = dplane_ctx_get_intf_dest(ctx);
 	if (dest->prefixlen == 0)

zebra/zebra_dplane.c
@@ -248,6 +248,7 @@ struct dplane_intf_info {
 #define DPLANE_INTF_HAS_DEST    DPLANE_INTF_CONNECTED
 #define DPLANE_INTF_HAS_LABEL   (1 << 4)
 #define DPLANE_INTF_NOPREFIXROUTE (1 << 5)
+#define DPLANE_INTF_TENTATIVE   (1 << 6) /* IPv6 address is tentative (DAD) */
 
 	/* Interface address/prefix */
 	struct prefix prefix;
@@ -2685,6 +2686,20 @@ void dplane_ctx_intf_set_broadcast(struct zebra_dplane_ctx *ctx)
 	DPLANE_CTX_VALID(ctx);
 
 	ctx->u.intf.flags |= DPLANE_INTF_BROADCAST;
+}
+
+bool dplane_ctx_intf_is_tentative(const struct zebra_dplane_ctx *ctx)
+{
+	DPLANE_CTX_VALID(ctx);
+
+	return (ctx->u.intf.flags & DPLANE_INTF_TENTATIVE);
+}
+
+void dplane_ctx_intf_set_tentative(struct zebra_dplane_ctx *ctx)
+{
+	DPLANE_CTX_VALID(ctx);
+
+	ctx->u.intf.flags |= DPLANE_INTF_TENTATIVE;
 }
 
 const struct prefix *dplane_ctx_get_intf_addr(

zebra/zebra_dplane.h
@@ -705,6 +705,8 @@ bool dplane_ctx_intf_is_noprefixroute(const struct zebra_dplane_ctx *ctx);
 void dplane_ctx_intf_set_noprefixroute(struct zebra_dplane_ctx *ctx);
 bool dplane_ctx_intf_is_broadcast(const struct zebra_dplane_ctx *ctx);
 void dplane_ctx_intf_set_broadcast(struct zebra_dplane_ctx *ctx);
+bool dplane_ctx_intf_is_tentative(const struct zebra_dplane_ctx *ctx);
+void dplane_ctx_intf_set_tentative(struct zebra_dplane_ctx *ctx);
 const struct prefix *dplane_ctx_get_intf_addr(
 	const struct zebra_dplane_ctx *ctx);
 const struct in6_addr *