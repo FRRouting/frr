3815975dd19668a3dfad800c55b40c3b838bd545
Rafael Zalamena <rzalamena@opensourcerouting.org>
2026-01-28 13:32:02 -0300

    tests: apply python code styling
    Fix python code styling.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- tests/topotests/pim_timers/test_pim_timers.py
+++ tests/topotests/pim_timers/test_pim_timers.py
@@ -35,9 +35,7 @@ pytestmark = [pytest.mark.pimd]
 def setup_module(mod):
     "Sets up the pytest environment"
 
-    topodef = {
-        "s1": ("r1")
-    }
+    topodef = {"s1": ("r1")}
 
     tgen = Topogen(topodef, mod.__name__)
     tgen.start_topology()
@@ -59,164 +57,192 @@ def test_pim_join_prune():
     tgen = get_topogen()
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
-    
+
     # Check the default value
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 60
-        }
-    }) is None, "invalid default join-prune interval"
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 60}}) is None
+    ), "invalid default join-prune interval"
 
     # Check global variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     router pim
      join-prune-interval 123
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 123
-        }
-    }) is None, "invalid global join-prune interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 123}}) is None
+    ), "invalid global join-prune interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ip pim join-prune-interval 134
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 134
-        }
-    }) is None, "invalid interface join-prune interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 134}}) is None
+    ), "invalid interface join-prune interval"
 
 
 def test_pim_assert_interval():
     tgen = get_topogen()
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
-    
+
     # Check the default value
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 180000
-        }
-    }) is None, "invalid default assert interval"
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"assertInterval": 180000}}) is None
+    ), "invalid default assert interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ip pim assert-interval 190000
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 190000
-        }
-    }) is None, "invalid interface assert interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"assertInterval": 190000}}) is None
+    ), "invalid interface assert interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ip pim assert-override-interval 3500
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ip pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 190000,
-            "assertOverrideInterval": 3500
-        }
-    }) is None, "invalid interface assert override interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ip pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(
+            output,
+            {"r1-eth0": {"assertInterval": 190000, "assertOverrideInterval": 3500}},
+        )
+        is None
+    ), "invalid interface assert override interval"
 
 
 def test_pim6_join_prune():
     tgen = get_topogen()
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
-    
+
     # Check the default value
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 60
-        }
-    }) is None, "invalid default join-prune interval"
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 60}}) is None
+    ), "invalid default join-prune interval"
 
     # Check global variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     router pim6
      join-prune-interval 123
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 123
-        }
-    }) is None, "invalid global join-prune interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 123}}) is None
+    ), "invalid global join-prune interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ipv6 pim join-prune-interval 134
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "joinPruneInterval": 134
-        }
-    }) is None, "invalid interface join-prune interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(output, {"r1-eth0": {"joinPruneInterval": 134}}) is None
+    ), "invalid interface join-prune interval"
 
 
 def test_pim6_assert_interval():
     tgen = get_topogen()
     if tgen.routers_have_failure():
         pytest.skip(tgen.errors)
-    
+
     # Check the default value
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 180000,
-            "assertOverrideInterval": 3000
-        }
-    }) is None, "invalid default assert interval"
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(
+            output,
+            {"r1-eth0": {"assertInterval": 180000, "assertOverrideInterval": 3000}},
+        )
+        is None
+    ), "invalid default assert interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ipv6 pim assert-interval 190000
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 190000,
-            "assertOverrideInterval": 3133
-        }
-    }) is None, "invalid interface assert interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(
+            output,
+            {"r1-eth0": {"assertInterval": 190000, "assertOverrideInterval": 3133}},
+        )
+        is None
+    ), "invalid interface assert interval"
 
     # Check interface variable change
-    tgen.gears['r1'].vtysh_cmd("""
+    tgen.gears["r1"].vtysh_cmd(
+        """
     configure terminal
     interface r1-eth0
      ipv6 pim assert-override-interval 3500
-    """)
-    output = tgen.gears['r1'].vtysh_cmd('show ipv6 pim interface r1-eth0 json', isjson=True)
-    assert topotest.json_cmp(output, {
-        "r1-eth0": {
-            "assertInterval": 190000,
-            "assertOverrideInterval": 3500
-        }
-    }) is None, "invalid interface assert override interval"
+    """
+    )
+    output = tgen.gears["r1"].vtysh_cmd(
+        "show ipv6 pim interface r1-eth0 json", isjson=True
+    )
+    assert (
+        topotest.json_cmp(
+            output,
+            {"r1-eth0": {"assertInterval": 190000, "assertOverrideInterval": 3500}},
+        )
+        is None
+    ), "invalid interface assert override interval"
 
 
 def test_memory_leak():
