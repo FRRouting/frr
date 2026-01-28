0988ab14b31501a486db7403b8d4b40b08798b45
Donald Sharp <sharpd@nvidia.com>
2026-01-28 08:14:38 -0500

    tests: Show that a redistribute connected passes reserved ranges
    The previous commit added the ability for zebra to pass the reserved
    ranges up to interested protocols.  BGP is the only one that currently
    accepts these values as far as I can tell at the moment.  Test that
    redistribute X works with reserved ranges.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- tests/topotests/zebra_reserved_ranges/test_zebra_reserved_ranges.py
+++ tests/topotests/zebra_reserved_ranges/test_zebra_reserved_ranges.py
@@ -18,8 +18,9 @@ sys.path.append(os.path.join(CWD, "../"))
 # pylint: disable=C0413
 from lib import topotest
 from lib.topogen import Topogen, get_topogen, TopoRouter
+from lib.topolog import logger
 
-pytestmark = [pytest.mark.mgmtd]
+pytestmark = [pytest.mark.mgmtd, pytest.mark.bgpd]
 
 
 def build_topo(tgen):
@@ -39,6 +40,7 @@ def setup_module(mod):
             [
                 (TopoRouter.RD_ZEBRA, None),
                 (TopoRouter.RD_MGMTD, None),
+                (TopoRouter.RD_BGP, None),
             ],
         )
 
@@ -79,9 +81,82 @@ def test_zebra_reserved_ranges():
         }
         return topotest.json_cmp(output, expected)
 
+    logger.info("Testing that Zebra sees the reserved ranges")
     test_func = functools.partial(_check_interfaces)
     _, result = topotest.run_and_expect(test_func, None, count=20, wait=1)
     assert result is None, "Can't see reserved IP ranges assigned for r1-eth0 interface"
+
+
+def test_redisted_routes_received():
+    tgen = get_topogen()
+
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    r1 = tgen.gears["r1"]
+
+    def _check_bgp_routes():
+        output = json.loads(r1.vtysh_cmd("show bgp ipv4 uni json"))
+        expected = {
+            "routes": {
+                "0.1.2.0/24": [
+                    {
+                        "valid": True,
+                        "bestpath": True,
+                        "selectionReason": "First path received",
+                        "pathFrom": "external",
+                        "prefix": "0.1.2.0",
+                        "prefixLen": 24,
+                        "network": "0.1.2.0/24",
+                        "metric": 0,
+                        "weight": 32768,
+                        "peerId": "(unspec)",
+                        "path": "",
+                        "origin": "incomplete",
+                        "nexthops": [
+                            {
+                                "ip": "0.0.0.0",
+                                "hostname": "r1",
+                                "afi": "ipv4",
+                                "used": True,
+                            }
+                        ],
+                    }
+                ],
+                "240.1.2.0/24": [
+                    {
+                        "valid": True,
+                        "bestpath": True,
+                        "selectionReason": "First path received",
+                        "pathFrom": "external",
+                        "prefix": "240.1.2.0",
+                        "prefixLen": 24,
+                        "network": "240.1.2.0/24",
+                        "metric": 0,
+                        "weight": 32768,
+                        "peerId": "(unspec)",
+                        "path": "",
+                        "origin": "incomplete",
+                        "nexthops": [
+                            {
+                                "ip": "0.0.0.0",
+                                "hostname": "r1",
+                                "afi": "ipv4",
+                                "used": True,
+                            }
+                        ],
+                    }
+                ],
+            },
+            "totalRoutes": 2,
+            "totalPaths": 2,
+        }
+        return topotest.json_cmp(output, expected)
+
+    logger.info("Testing that bgp sees the 0.1.2.0/24 and the 240.1.2.0/24 routes")
+    test_func = functools.partial(_check_bgp_routes)
+    _, result = topotest.run_and_expect(test_func, None, count=20, wait=1)
+    assert result is None, "Missing redistributed routes in BGP"
 
 
 def test_memory_leak():
