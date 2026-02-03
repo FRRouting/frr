FRRouting/frr

commit 2ca00075cc2c01e51cd720733b9a2c47a289c5bc

isisd: insert every route with the onlink flag
In order to allow true unnumbered interfaces (without copying the
loopback address to the interface) we need to insert the isis routes
using `onlink`. This is because the nexthop check will fail because the
peer interface doesn't have an address either.

Signed-off-by: Gabriel Goller <g.goller@proxmox.com>

isisd/isis_zebra.c
@@ -173,8 +173,7 @@ static int isis_zebra_add_nexthops(struct isis *isis, struct list *nexthops,
 
 		zapi_nexthop_init(api_nh);
 
-		if (fabricd)
-			SET_FLAG(api_nh->flags, ZAPI_NEXTHOP_FLAG_ONLINK);
+		SET_FLAG(api_nh->flags, ZAPI_NEXTHOP_FLAG_ONLINK);
 		api_nh->vrf_id = isis->vrf_id;
 
 		switch (nexthop->family) {

commit 8594c2a52afccc970ff51b158c0ded5887fd3cd7

isisd: fix avoid route with IPv4 prefix and nexthop same
The following route is now displayed:
> I>* 10.254.0.3/32 [115/20] via 10.254.0.3, r1-eth1 onlink, weight 1, 00:00:00

This is not clear having both prefix and nexthop same. Avoid this use
case, and replace the route with a directly connected one.

I>* 10.254.0.3/32 [115/20] is directly connected, r1-eth1, weight 1, 00:00:08

Fixes: 2ca00075cc2c ("isisd: insert every route with the onlink flag")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

isisd/isis_zebra.c
@@ -156,8 +156,8 @@ enum isis_zebra_nexthop_type {
 
 static int isis_zebra_add_nexthops(struct isis *isis, struct list *nexthops,
 				   struct zapi_nexthop zapi_nexthops[],
-				   enum isis_zebra_nexthop_type type,
-				   bool mpls_lsp, uint8_t backup_nhs)
+				   enum isis_zebra_nexthop_type type, bool mpls_lsp,
+				   uint8_t backup_nhs, struct prefix *prefix)
 {
 	struct isis_nexthop *nexthop;
 	struct listnode *node;
@@ -173,15 +173,21 @@ static int isis_zebra_add_nexthops(struct isis *isis, struct list *nexthops,
 
 		zapi_nexthop_init(api_nh);
 
-		SET_FLAG(api_nh->flags, ZAPI_NEXTHOP_FLAG_ONLINK);
+		if (fabricd)
+			SET_FLAG(api_nh->flags, ZAPI_NEXTHOP_FLAG_ONLINK);
 		api_nh->vrf_id = isis->vrf_id;
 
 		switch (nexthop->family) {
 		case AF_INET:
 			/* FIXME: can it be ? */
 			if (nexthop->ip.ipv4.s_addr != INADDR_ANY) {
-				api_nh->type = NEXTHOP_TYPE_IPV4_IFINDEX;
-				api_nh->gate.ipv4 = nexthop->ip.ipv4;
+				if (prefix && prefix->prefixlen == IPV4_MAX_BITLEN &&
+				    prefix->u.prefix4.s_addr == nexthop->ip.ipv4.s_addr)
+					api_nh->type = NEXTHOP_TYPE_IFINDEX;
+				else {
+					api_nh->type = NEXTHOP_TYPE_IPV4_IFINDEX;
+					api_nh->gate.ipv4 = nexthop->ip.ipv4;
+				}
 			} else {
 				api_nh->type = NEXTHOP_TYPE_IFINDEX;
 			}
@@ -279,9 +285,9 @@ void isis_zebra_route_add_route(struct isis *isis, struct prefix *prefix,
 
 	/* Add backup nexthops first. */
 	if (route_info->backup) {
-		count = isis_zebra_add_nexthops(
-			isis, route_info->backup->nexthops, api.backup_nexthops,
-			ISIS_NEXTHOP_BACKUP, false, 0);
+		count = isis_zebra_add_nexthops(isis, route_info->backup->nexthops,
+						api.backup_nexthops, ISIS_NEXTHOP_BACKUP, false, 0,
+						prefix);
 		if (count > 0) {
 			SET_FLAG(api.message, ZAPI_MESSAGE_BACKUP_NEXTHOPS);
 			api.backup_nexthop_num = count;
@@ -289,9 +295,8 @@ void isis_zebra_route_add_route(struct isis *isis, struct prefix *prefix,
 	}
 
 	/* Add primary nexthops. */
-	count = isis_zebra_add_nexthops(isis, route_info->nexthops,
-					api.nexthops, ISIS_NEXTHOP_MAIN, false,
-					count);
+	count = isis_zebra_add_nexthops(isis, route_info->nexthops, api.nexthops,
+					ISIS_NEXTHOP_MAIN, false, count, prefix);
 	if (!count)
 		return;
 	api.nexthop_num = count;
@@ -369,10 +374,9 @@ void isis_zebra_prefix_sid_install(struct isis_area *area,
 	} else {
 		/* Add backup nexthops first. */
 		if (psid->nexthops_backup) {
-			count = isis_zebra_add_nexthops(
-				area->isis, psid->nexthops_backup,
-				zl.backup_nexthops, ISIS_NEXTHOP_BACKUP, true,
-				0);
+			count = isis_zebra_add_nexthops(area->isis, psid->nexthops_backup,
+							zl.backup_nexthops, ISIS_NEXTHOP_BACKUP,
+							true, 0, NULL);
 			if (count > 0) {
 				SET_FLAG(zl.message, ZAPI_LABELS_HAS_BACKUPS);
 				zl.backup_nexthop_num = count;
@@ -380,9 +384,8 @@ void isis_zebra_prefix_sid_install(struct isis_area *area,
 		}
 
 		/* Add primary nexthops. */
-		count = isis_zebra_add_nexthops(area->isis, psid->nexthops,
-						zl.nexthops, ISIS_NEXTHOP_MAIN,
-						true, count);
+		count = isis_zebra_add_nexthops(area->isis, psid->nexthops, zl.nexthops,
+						ISIS_NEXTHOP_MAIN, true, count, NULL);
 		if (!count)
 			return;
 		zl.nexthop_num = count;
@@ -458,9 +461,8 @@ void isis_zebra_send_adjacency_sid(int cmd, const struct sr_adjacency *sra)
 	if (sra->type == ISIS_SR_ADJ_BACKUP) {
 		int count;
 
-		count = isis_zebra_add_nexthops(isis, sra->backup_nexthops,
-						zl.backup_nexthops,
-						ISIS_NEXTHOP_BACKUP, true, 0);
+		count = isis_zebra_add_nexthops(isis, sra->backup_nexthops, zl.backup_nexthops,
+						ISIS_NEXTHOP_BACKUP, true, 0, NULL);
 		if (count > 0) {
 			SET_FLAG(zl.message, ZAPI_LABELS_HAS_BACKUPS);
 			zl.backup_nexthop_num = count;