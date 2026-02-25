b447a31245aefe353c7ab5f22cc97126dd868983
harini <hnattamaisub@nvidia.com>
2026-02-25 07:44:07 -0800

    zebra: show nexthop-group rib brief json
    Commands supported:
    1)show nexthop-group rib brief json
    2)show nexthop-group rib 117 brief json
    3)show nexthop-group rib singleton ip brief json
    4)show nexthop-group rib singleton ipv6 brief json
    5)show nexthop-group rib zebra brief json
    
    Signed-off-by: harini <hnattamaisub@nvidia.com>

--- lib/nexthop.h
+++ lib/nexthop.h
@@ -292,9 +292,8 @@ extern bool nexthop_is_blackhole(const struct nexthop *nh);
 int nexthop_str2backups(const char *str, int *num_backups,
 			uint8_t *backups);
 
-void nexthop_json_helper(struct json_object *json_nexthop,
-			 const struct nexthop *nexthop, bool display_vrfid,
-			 uint8_t rn_family);
+void nexthop_json_helper(struct json_object *json_nexthop, const struct nexthop *nexthop,
+			 bool display_vrfid, uint8_t rn_family, bool brief);
 void nexthop_vty_helper(struct vty *vty, const struct nexthop *nexthop,
 			bool display_vrfid, uint8_t rn_family);
 
