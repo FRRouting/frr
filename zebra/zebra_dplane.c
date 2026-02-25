c633ce9e1f402fbe36fbcfc440c2c614933c522c
Mark Stapp <mjs@cisco.com>
2026-02-25 16:05:14 -0500

    zebra: bump dplane minor version for 10.7
    Bump the dplane version number for the ongoing master/10.7
    release.
    
    Signed-off-by: Mark Stapp <mjs@cisco.com>

--- zebra/zebra_dplane.c
+++ zebra/zebra_dplane.c
@@ -48,7 +48,7 @@ DEFINE_MTYPE(ZEBRA, VLAN_CHANGE_ARR, "Vlan Change Array");
  * are made. The minor version (at least) should be updated when new APIs
  * are introduced.
  */
-static uint32_t zdplane_version = MAKE_FRRVERSION(3, 0, 0);
+static uint32_t zdplane_version = MAKE_FRRVERSION(3, 1, 0);
 
 /* Control for collection of extra interface info with route updates; a plugin
  * can enable the extra info via a dplane api.
