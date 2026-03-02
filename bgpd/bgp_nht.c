FRRouting/frr

commit d1252ec2a6a8a7878447166db94cd316b3de52c1

bgpd: On interface up/down events allow for more interfaces to be affected
Current code in BGP only examines the bnc->ifindex_ipv6_ll to decide if
evaluate_paths should be used.  Let's broaden this scope a tiny bit
to allow for matches where the number of nexthops is 1 and the
nexthop->ifindex is the same as the interfaces ifindex for the
up/down event.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgp_nht.c
@@ -808,7 +808,9 @@ static void bgp_nht_ifp_table_handle(struct bgp *bgp,
 	}
 
 	frr_each (bgp_nexthop_cache, table, bnc) {
-		if (bnc->ifindex_ipv6_ll != ifp->ifindex)
+		if ((bnc->nexthop_num == 1 && bnc->nexthop &&
+		     bnc->nexthop->ifindex != ifp->ifindex) &&
+		    (bnc->ifindex_ipv6_ll != ifp->ifindex))
 			continue;
 
 		bnc->last_update = monotime(NULL);

commit 5de395fc8ac476cb17bbc911f527ead60e841fb4

bgpd: Fix condition when evaluating paths
Original commit said:

Let's broaden this scope a tiny bit to allow for matches where the number of
nexthops is 1 and the nexthop->ifindex is the same as the interfaces ifindex
for the up/down event.

But the condition was incorrect according to the definition.

That leads to routes being flushed incorrectly.

When nexthop_num > 1, the first clause is false, so the whole continue is
skipped, and the DOWN processing runs BGP_NEXTHOP_VALID is unset, nexthop_num
is set to 0, evaluate_paths() is called, which means paths are withdrawn...

Fixes: d1252ec ("bgpd: On interface up/down events allow for more interfaces to be affected")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_nht.c
@@ -808,8 +808,8 @@ static void bgp_nht_ifp_table_handle(struct bgp *bgp,
 	}
 
 	frr_each (bgp_nexthop_cache, table, bnc) {
-		if ((bnc->nexthop_num == 1 && bnc->nexthop &&
-		     bnc->nexthop->ifindex != ifp->ifindex) &&
+		if (!(bnc->nexthop_num == 1 && bnc->nexthop &&
+		      bnc->nexthop->ifindex == ifp->ifindex) &&
 		    (bnc->ifindex_ipv6_ll != ifp->ifindex))
 			continue;
 