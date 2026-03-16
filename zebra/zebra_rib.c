cb191a3cc7c2ce6685e5130d7dd3c68756f0de61
Robin Jarry <robin@jarry.cc>
2026-03-16 18:11:16 +0100

    zebra: route FDB reads through the dataplane
    Add DPLANE_OP_FDB_READ and convert all FDB read call sites to use the
    new dplane enqueue functions instead of calling netlink helpers from
    the main thread.
    
    All needed interface properties (bridge VLAN-awareness, VxLAN type)
    are captured at enqueue time so that the dplane handler never accesses
    zebra main-thread objects like zebra_ns or struct interface. Rework the
    netlink_macfdb_read* and netlink_request_specific_mac functions to take
    a struct nlsock pointer and plain values instead of struct zebra_ns and
    struct interface pointers. The dplane handler kernel_read_macfdb()
    calls them directly. Read results flow back through the existing
    notification path via netlink_macfdb_change().
    
    Remove the old macfdb_read wrappers from rt.h, rtread_netlink.c and
    rtread_sysctl.c since they are no longer used.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -5188,6 +5188,9 @@ static void rib_process_dplane_results(struct event *event)
 			case DPLANE_OP_NEIGH_DISCOVER:
 				zebra_neigh_dplane_update(ctx);
 				break;
+
+			case DPLANE_OP_FDB_READ:
+				break;
 			} /* Dispatch by op code */
 
 			dplane_ctx_fini(&ctx);
--- zebra/zebra_script.c
+++ zebra/zebra_script.c
@@ -428,6 +428,7 @@ void lua_pushzebra_dplane_ctx(lua_State *L, const struct zebra_dplane_ctx *ctx)
 	case DPLANE_OP_NONE:
 	case DPLANE_OP_STARTUP_STAGE:
 	case DPLANE_OP_VLAN_INSTALL:
+	case DPLANE_OP_FDB_READ:
 		break;
 	} /* Dispatch by op code */
 }
