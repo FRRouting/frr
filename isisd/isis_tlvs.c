a5f101daa9505817a3d2745598f0b54577c6788f
Mark Stapp <mjs@cisco.com>
2026-04-16 15:16:53 -0400

    isisd: continue hardening SRV6 tlv parsing
    Add more validation for SRV6 END.X and LAN_END.X subsubtlvs.
    Ensure allocated subsubtlv pointer is freed in error cleanup paths.
    
    Signed-off-by: Mark Stapp <mjs@cisco.com>
    Reported-by: Bronson Yen <bronson@calif.io>
    (cherry picked from commit 2978a02efd29bd4a10cbde5c7d1fb01f7eaeaca4)
    
    # Conflicts:
    #	isisd/isis_tlvs.c

--- isisd/isis_tlvs.c
+++ isisd/isis_tlvs.c
@@ -2147,15 +2147,33 @@ static int unpack_item_ext_subtlvs(uint16_t mtid, uint8_t len, struct stream *s,
 				stream_get(&adj->sid, s, IPV6_MAX_BYTELEN);
 				subsubtlv_len = stream_getc(s);
 
+				if (subsubtlv_len >
+				    subtlv_len - ISIS_SUBTLV_SRV6_ENDX_SID_SIZE) {
+					TLV_SIZE_MISMATCH(log, indent,
+							  "SRv6 End.X SID subsubtlvs");
+					XFREE(MTYPE_ISIS_SUBTLV, adj);
+					stream_forward_getp(
+						s,
+						subtlv_len - ISIS_SUBTLV_SRV6_ENDX_SID_SIZE);
+					break;
+				}
+
 				adj->subsubtlvs = isis_alloc_subsubtlvs(
 					ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID);
 
 				bool unpacked_known_tlvs = false;
+<<<<<<< HEAD
 				if (unpack_tlvs(
 					    ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID,
 					    subsubtlv_len, s, log,
 					    adj->subsubtlvs, indent + 4,
 					    &unpacked_known_tlvs)) {
+=======
+				if (unpack_tlvs(ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID,
+						subsubtlv_len, s, log, adj->subsubtlvs, indent + 4,
+						&unpacked_known_tlvs)) {
+					isis_free_subsubtlvs(adj->subsubtlvs);
+>>>>>>> 2978a02ef (isisd: continue hardening SRV6 tlv parsing)
 					XFREE(MTYPE_ISIS_SUBTLV, adj);
 					break;
 				}
@@ -2191,15 +2209,33 @@ static int unpack_item_ext_subtlvs(uint16_t mtid, uint8_t len, struct stream *s,
 				stream_get(&lan->sid, s, IPV6_MAX_BYTELEN);
 				subsubtlv_len = stream_getc(s);
 
+				if (subsubtlv_len >
+				    subtlv_len - ISIS_SUBTLV_SRV6_LAN_ENDX_SID_SIZE) {
+					TLV_SIZE_MISMATCH(log, indent,
+							  "SRv6 LAN End.X SID subsubtlvs");
+					XFREE(MTYPE_ISIS_SUBTLV, lan);
+					stream_forward_getp(
+						s,
+						subtlv_len - ISIS_SUBTLV_SRV6_LAN_ENDX_SID_SIZE);
+					break;
+				}
+
 				lan->subsubtlvs = isis_alloc_subsubtlvs(
 					ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID);
 
 				bool unpacked_known_tlvs = false;
+<<<<<<< HEAD
 				if (unpack_tlvs(
 					    ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID,
 					    subsubtlv_len, s, log,
 					    lan->subsubtlvs, indent + 4,
 					    &unpacked_known_tlvs)) {
+=======
+				if (unpack_tlvs(ISIS_CONTEXT_SUBSUBTLV_SRV6_ENDX_SID,
+						subsubtlv_len, s, log, lan->subsubtlvs, indent + 4,
+						&unpacked_known_tlvs)) {
+					isis_free_subsubtlvs(lan->subsubtlvs);
+>>>>>>> 2978a02ef (isisd: continue hardening SRV6 tlv parsing)
 					XFREE(MTYPE_ISIS_SUBTLV, lan);
 					break;
 				}
