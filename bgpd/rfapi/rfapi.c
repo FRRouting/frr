84981371a0960e2b484c1be4dc2a7162860bec63
Donald Sharp <sharpd@nvidia.com>
2026-05-06 08:37:42 -0400

    bgpd: Use lookup not get for VNC pdest on attr change add in rfapi
    Use a lookup to avoid the creation/deletion of a node in the removal
    case.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/rfapi/rfapi.c
+++ bgpd/rfapi/rfapi.c
@@ -987,16 +987,18 @@ void add_vnc_route(struct rfapi_descriptor *rfd, /* cookie, VPN UN addr, peer */
 
 
 			if (safi == SAFI_MPLS_VPN) {
-				struct bgp_dest *pdest = NULL;
-				struct bgp_table *table = NULL;
+				struct bgp_dest *pdest;
+				struct bgp_table *table;
 
-				pdest = bgp_node_get(bgp->rib[afi][safi],
-						     (struct prefix *)prd);
-				table = bgp_dest_get_bgp_table_info(pdest);
-				if (table)
-					vnc_import_bgp_add_vnc_host_route_mode_resolve_nve(
-						bgp, prd, table, p, bpi);
-				bgp_dest_unlock_node(pdest);
+				pdest = bgp_node_lookup(bgp->rib[afi][safi],
+							(struct prefix *)prd);
+				if (pdest) {
+					table = bgp_dest_get_bgp_table_info(pdest);
+					if (table)
+						vnc_import_bgp_add_vnc_host_route_mode_resolve_nve(
+							bgp, prd, table, p, bpi);
+					bgp_dest_unlock_node(pdest);
+				}
 			}
 
 			/* Process change. */
