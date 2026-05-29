FRRouting/frr

commit 2f63758b51c3b8f8be182fcb680d7798df6af4d3

bgpd: export local rt2 mac+ip entries to unicast
Local EVPN MAC+IP RT2 entries are currently not exported to the
unicast RIB of their own VRF, nor to other VRFs. Only remote RT2
entries may be exported to other VRFs, if configuration allows.

This can lead to sub-optimal routing when VTEPs are also connected
via IPv4 unicast. In such cases, bgpd may lack /32 host routes for
locally learned EVPN endpoints.

Example:
  - r1 peers via IPv4 unicast with r2 and r3 (VTEPs)
  - r2 and r3 peer via EVPN
  - 10.0.0.0/24 is in vrf10, with h2 behind r2 and h3 behind r3

         +---+  +-----------+
        /| r2|--|h2 10.0.0.2|
+---+  / +---+  +-----------+
| r1| /    |
+---+ \    |
       \ +---+  +-----------+
        \| r3|--|h3 10.0.0.3|
         +---+  +-----------+

r1, r2, and r3 all peer in VRF default. r1 receives three routes within
10.0.0.0/24:
 - 10.0.0.0/24 via r3 (or r2, depending on best path)
 - 10.0.0.2/32 via r3 (received by r3 from r2, then exported to default
   VRF)
 - 10.0.0.3/32 via r2

The path from r1 to h2 becomes:
 - r1 -> r3 -> r2 -> h2,
 - instead of the optimal r1 -> r2 -> h2.

This is due to missing /32 unicast routes.

Fix by exporting local EVPN RT2 MAC+IP entries to the unicast
table of their own VRF, and optionally to other VRFs if configured.
The result is:

  - 10.0.0.0/24 via r3 (or r2, depending on best path)
  - 10.0.0.2/32 via r2
  - 10.0.0.3/32 via r3

Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_evpn.c
@@ -2446,6 +2446,11 @@ static int update_evpn_route(struct bgp *bgp, struct bgpevpn *vpn,
 			NULL /* ip */, 1, &global_pi, flags, seq,
 			false /* setup_sync */, NULL /* old_is_sync */);
 
+		if (p->prefix.route_type == BGP_EVPN_MAC_IP_ROUTE && !mac_only) {
+			SET_FLAG(global_pi->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP);
+			bgp_evpn_import_route(bgp, afi, safi, bgp_dest_get_prefix(dest), global_pi);
+		}
+
 		/* Schedule for processing and unlock node. */
 		bgp_process(bgp, dest, global_pi, afi, safi);
 		bgp_dest_unlock_node(dest);
@@ -2550,8 +2555,13 @@ static int delete_evpn_route(struct bgp *bgp, struct bgpevpn *vpn,
 		/* Schedule for processing - withdraws to peers happen from
 		 * this table.
 		 */
-		if (pi)
+		if (pi) {
 			bgp_process(bgp, global_dest, pi, afi, safi);
+			if (p->prefix.route_type == BGP_EVPN_MAC_IP_ROUTE &&
+			    !is_evpn_prefix_ipaddr_none(p))
+				bgp_evpn_unimport_route(bgp, afi, safi,
+							bgp_dest_get_prefix(global_dest), pi);
+		}
 		bgp_dest_unlock_node(global_dest);
 	}
 
@@ -3110,6 +3120,8 @@ bgp_create_evpn_bgp_path_info(struct bgp_path_info *parent_pi,
 	/* Create new route with its attribute. */
 	pi = info_make(parent_pi->type, BGP_ROUTE_IMPORTED, 0, parent_pi->peer,
 		       attr_new, dest);
+	if (CHECK_FLAG(parent_pi->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP))
+		SET_FLAG(pi->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP);
 	SET_FLAG(pi->flags, BGP_PATH_VALID);
 	bgp_path_info_extra_get(pi);
 	if (!pi->extra->vrfleak)

bgpd/bgp_mplsvpn.c
@@ -1411,6 +1411,9 @@ leak_update(struct bgp *to_bgp, struct bgp_dest *bn,
 	new = info_make(ZEBRA_ROUTE_BGP, BGP_ROUTE_IMPORTED, 0,
 			to_bgp->peer_self, new_attr, bn);
 
+	if (CHECK_FLAG(source_bpi->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP))
+		SET_FLAG(new->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP);
+
 	bgp_path_info_extra_get(new);
 	if (!new->extra->vrfleak)
 		new->extra->vrfleak =

bgpd/bgp_route.h
@@ -352,6 +352,7 @@ struct bgp_path_info {
  * the actual ecmp path.
  */
 #define BGP_PATH_MULTIPATH_NEW (1 << 20)
+#define BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP (1 << 21)
 
 	/* BGP route type.  This can be static, RIP, OSPF, BGP etc.  */
 	uint8_t type;

bgpd/bgp_zebra.c
@@ -2045,6 +2045,8 @@ void bgp_zebra_route_install(struct bgp_dest *dest, struct bgp_path_info *info,
 	table = bgp_dest_table(dest);
 	if (table && table->afi == AFI_L2VPN && table->safi == SAFI_EVPN)
 		is_evpn = true;
+	else if (CHECK_FLAG(info->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP))
+		return;
 
 	/*
 	 * BGP is installing this route and bgp has been configured

commit a93881d7cbdfcf19ac6ff2e0e08f4cfe16d8018c

bgpd: fix export local rt2 leak rd race condition
If bgpd receives the route-id value from zebra after creating a local
MAC-IP RT2 entry, the entry in VNI tables refers to the stale entry in
the previous route-distinguisher (RD) table.

The RD value is derived from the router-id.

Un-import local RT2 mac+ip entries on their deletion in the global EVPN
table. Import them on update of local RT2 mac+ip entries.

Fixes: 2f63758b51 ("bgpd: export local rt2 mac+ip entries to unicast")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

bgpd/bgp_evpn.c
@@ -2834,8 +2834,12 @@ static void delete_global_type2_routes(struct bgp *bgp, struct bgpevpn *vpn)
 				continue;
 
 			pi = delete_evpn_route_entry(bgp, afi, safi, dest, NULL, 0);
-			if (pi)
+			if (pi) {
 				bgp_process(bgp, dest, pi, afi, safi);
+				if (!is_evpn_prefix_ipaddr_none(evp))
+					bgp_evpn_unimport_route(bgp, afi, safi,
+								(struct prefix *)evp, pi);
+			}
 		}
 
 		/* Unlock RD node. */
@@ -4749,7 +4753,7 @@ static void update_advertise_vni_route(struct bgp *bgp, struct bgpevpn *vpn,
 	struct attr *attr;
 	afi_t afi = AFI_L2VPN;
 	safi_t safi = SAFI_EVPN;
-
+	bool mac_ip = false;
 	struct prefix_evpn tmp_evp;
 	const struct prefix_evpn *evp =
 		(const struct prefix_evpn *)bgp_dest_get_prefix(dest);
@@ -4781,6 +4785,7 @@ static void update_advertise_vni_route(struct bgp *bgp, struct bgpevpn *vpn,
 			evpn_type2_prefix_global_copy(
 				&tmp_evp, evp, evpn_type2_path_info_get_mac(pi),
 				NULL /* ip */);
+			mac_ip = true;
 		}
 	} else {
 		memcpy(&tmp_evp, evp, sizeof(tmp_evp));
@@ -4801,6 +4806,11 @@ static void update_advertise_vni_route(struct bgp *bgp, struct bgpevpn *vpn,
 			NULL /* ip */, 1, &global_pi, 0,
 			mac_mobility_seqnum(attr), false /* setup_sync */,
 			NULL /* old_is_sync */);
+		if (mac_ip) {
+			SET_FLAG(global_pi->flags, BGP_PATH_LOCAL_IMPORT_EVPN_RT2_MACIP);
+			bgp_evpn_import_route(bgp, afi, safi, (struct prefix *)&tmp_evp, global_pi);
+		}
+
 	} else {
 		/* Type-1 route */
 		struct bgp_evpn_es *es;