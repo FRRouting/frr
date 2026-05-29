bf63dec8e112cfc92f972291b10806aac31f07f6
Jafar Al-Gharaibeh <jafar@atcorp.com>
2026-05-29 14:17:43 -0500

    pimd: fix inverted PIM socket receive failure counter
    The pim_sock_read refactor incremented pim_ifstat_hello_recvfail on
    successful reads instead of failures.
    
    Signed-off-by: Jafar Al-Gharaibeh <jafar@atcorp.com>
    (cherry picked from commit 321c6873911aa8248754713509a4e07bd5f43bff)

--- pimd/pim_pim.c
+++ pimd/pim_pim.c
@@ -524,7 +524,7 @@ static void pim_sock_read(struct event *t)
 
 	pim_ifp = ifp->info;
 
-	if (pim_sock_read_helper(fd, pim_ifp->pim, true) == 0)
+	if (pim_sock_read_helper(fd, pim_ifp->pim, true) != 0)
 		++pim_ifp->pim_ifstat_hello_recvfail;
 
 	pim_sock_read_on(ifp);
