d4e324c2286d5a99ff213c659ad286f55ba3a990
Jafar Al-Gharaibeh <jafar@atcorp.com>
2026-05-29 21:47:22 -0500

    pimd: drop duplicate rec_type from IGMPv3 report debug log
    The record type was already printed in the message prefix, so remove the
    redundant type=%d field from the per-record debug line.
    
    Signed-off-by: Jafar Al-Gharaibeh <jafar@atcorp.com>

--- pimd/pim_igmpv3.c
+++ pimd/pim_igmpv3.c
@@ -1899,9 +1899,9 @@ int igmp_v3_recv_report(struct gm_sock *igmp, struct in_addr from, char *igmp_ms
 		       sizeof(struct in_addr));
 
 		if (PIM_DEBUG_GM_PACKETS) {
-			zlog_debug("    Recv IGMP report v3 (type %d) from %pI4s on %s: record=%d type=%d auxdatalen=%d sources=%d group=%pI4",
-				   rec_type, &from, ifp->name, i, rec_type, rec_auxdatalen,
-				   rec_num_sources, &rec_group);
+			zlog_debug("    Recv IGMP report v3 (type %d) from %pI4s on %s: record=%d auxdatalen=%d sources=%d group=%pI4",
+				   rec_type, &from, ifp->name, i, rec_auxdatalen, rec_num_sources,
+				   &rec_group);
 		}
 
 		/* Scan sources */
