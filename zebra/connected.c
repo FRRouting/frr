FRRouting/frr

commit d4aa24ba7df066ee8a9f4bab5c1b4e9ff8474392

*: Introduce Local Host Routes to FRR
Create Local routes in FRR:

S   0.0.0.0/0 [1/0] via 192.168.119.1, enp39s0, weight 1, 00:03:46
K>* 0.0.0.0/0 [0/100] via 192.168.119.1, enp39s0, 00:03:51
O   192.168.119.0/24 [110/100] is directly connected, enp39s0, weight 1, 00:03:46
C>* 192.168.119.0/24 is directly connected, enp39s0, 00:03:51
L>* 192.168.119.224/32 is directly connected, enp39s0, 00:03:51
O   192.168.119.229/32 [110/100] via 0.0.0.0, enp39s0 inactive, weight 1, 00:03:46
C>* 192.168.119.229/32 is directly connected, enp39s0, 00:03:46

Create ability to redistribute local routes.

Modify tests to support this change.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

zebra/connected.c
@@ -182,7 +182,7 @@ static void connected_update(struct interface *ifp, struct connected *ifc)
 void connected_up(struct interface *ifp, struct connected *ifc)
 {
 	afi_t afi;
-	struct prefix p;
+	struct prefix p, plocal;
 	struct nexthop nh = {
 		.type = NEXTHOP_TYPE_IFINDEX,
 		.ifindex = ifp->ifindex,
@@ -194,6 +194,7 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 	uint32_t count = 0;
 	struct listnode *cnode;
 	struct connected *c;
+	bool install_local = true;
 
 	zvrf = ifp->vrf->info;
 	if (!zvrf) {
@@ -210,6 +211,7 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 	UNSET_FLAG(ifc->conf, ZEBRA_IFC_DOWN);
 
 	prefix_copy(&p, CONNECTED_PREFIX(ifc));
+	prefix_copy(&plocal, ifc->address);
 
 	/* Apply mask to the network. */
 	apply_mask(&p);
@@ -224,6 +226,8 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 		 */
 		if (prefix_ipv4_any((struct prefix_ipv4 *)&p))
 			return;
+
+		plocal.prefixlen = IPV4_MAX_BITLEN;
 		break;
 	case AFI_IP6:
 #ifndef GNU_LINUX
@@ -231,6 +235,11 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 		if (IN6_IS_ADDR_UNSPECIFIED(&p.u.prefix6))
 			return;
 #endif
+
+		if (IN6_IS_ADDR_LINKLOCAL(&plocal.u.prefix6))
+			install_local = false;
+
+		plocal.prefixlen = IPV6_MAX_BITLEN;
 		break;
 	case AFI_UNSPEC:
 	case AFI_L2VPN:
@@ -283,6 +292,15 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 	rib_add(afi, SAFI_MULTICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_CONNECT, 0,
 		flags, &p, NULL, &nh, 0, zvrf->table_id, metric, 0, 0, 0,
 		false);
+
+	if (install_local) {
+		rib_add(afi, SAFI_UNICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_LOCAL,
+			0, flags, &plocal, NULL, &nh, 0, zvrf->table_id, 0, 0,
+			0, 0, false);
+		rib_add(afi, SAFI_MULTICAST, zvrf->vrf->vrf_id,
+			ZEBRA_ROUTE_LOCAL, 0, flags, &plocal, NULL, &nh, 0,
+			zvrf->table_id, 0, 0, 0, 0, false);
+	}
 
 	/* Schedule LSP forwarding entries for processing, if appropriate. */
 	if (zvrf->vrf->vrf_id == VRF_DEFAULT) {
@@ -368,7 +386,7 @@ void connected_add_ipv4(struct interface *ifp, int flags,
 void connected_down(struct interface *ifp, struct connected *ifc)
 {
 	afi_t afi;
-	struct prefix p;
+	struct prefix p, plocal;
 	struct nexthop nh = {
 		.type = NEXTHOP_TYPE_IFINDEX,
 		.ifindex = ifp->ifindex,
@@ -378,6 +396,7 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 	uint32_t count = 0;
 	struct listnode *cnode;
 	struct connected *c;
+	bool remove_local = true;
 
 	zvrf = ifp->vrf->info;
 	if (!zvrf) {
@@ -403,6 +422,7 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 	}
 
 	prefix_copy(&p, CONNECTED_PREFIX(ifc));
+	prefix_copy(&plocal, ifc->address);
 
 	/* Apply mask to the network. */
 	apply_mask(&p);
@@ -417,10 +437,18 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 		 */
 		if (prefix_ipv4_any((struct prefix_ipv4 *)&p))
 			return;
+
+		plocal.prefixlen = IPV4_MAX_BITLEN;
 		break;
 	case AFI_IP6:
 		if (IN6_IS_ADDR_UNSPECIFIED(&p.u.prefix6))
 			return;
+
+		plocal.prefixlen = IPV6_MAX_BITLEN;
+
+		if (IN6_IS_ADDR_LINKLOCAL(&plocal.u.prefix6))
+			remove_local = false;
+
 		break;
 	case AFI_UNSPEC:
 	case AFI_L2VPN:
@@ -462,6 +490,16 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 
 	rib_delete(afi, SAFI_MULTICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_CONNECT,
 		   0, 0, &p, NULL, &nh, 0, zvrf->table_id, 0, 0, false);
+
+	if (remove_local) {
+		rib_delete(afi, SAFI_UNICAST, zvrf->vrf->vrf_id,
+			   ZEBRA_ROUTE_LOCAL, 0, 0, &plocal, NULL, &nh, 0,
+			   zvrf->table_id, 0, 0, false);
+
+		rib_delete(afi, SAFI_MULTICAST, zvrf->vrf->vrf_id,
+			   ZEBRA_ROUTE_LOCAL, 0, 0, &plocal, NULL, &nh, 0,
+			   zvrf->table_id, 0, 0, false);
+	}
 
 	/* Schedule LSP forwarding entries for processing, if appropriate. */
 	if (zvrf->vrf->vrf_id == VRF_DEFAULT) {

lib/log.c
@@ -547,6 +547,8 @@ int proto_redistnum(int afi, const char *s)
 			return ZEBRA_ROUTE_KERNEL;
 		else if (strmatch(s, "connected"))
 			return ZEBRA_ROUTE_CONNECT;
+		else if (strmatch(s, "local"))
+			return ZEBRA_ROUTE_LOCAL;
 		else if (strmatch(s, "static"))
 			return ZEBRA_ROUTE_STATIC;
 		else if (strmatch(s, "rip"))
@@ -581,6 +583,8 @@ int proto_redistnum(int afi, const char *s)
 			return ZEBRA_ROUTE_KERNEL;
 		else if (strmatch(s, "connected"))
 			return ZEBRA_ROUTE_CONNECT;
+		else if (strmatch(s, "local"))
+			return ZEBRA_ROUTE_LOCAL;
 		else if (strmatch(s, "static"))
 			return ZEBRA_ROUTE_STATIC;
 		else if (strmatch(s, "ripng"))

lib/zclient.h
@@ -865,7 +865,7 @@ int zclient_neigh_ip_encode(struct stream *s, uint16_t cmd, union sockunion *in,
 	((uint32_t)250000000) /* Bottom 28 bits then rounded down */
 #define ZEBRA_NHG_PROTO_SPACING (ZEBRA_NHG_PROTO_UPPER / ZEBRA_ROUTE_MAX)
 #define ZEBRA_NHG_PROTO_LOWER                                                  \
-	(ZEBRA_NHG_PROTO_SPACING * (ZEBRA_ROUTE_CONNECT + 1))
+	(ZEBRA_NHG_PROTO_SPACING * (ZEBRA_ROUTE_LOCAL + 1))
 
 extern uint32_t zclient_get_nhg_start(uint32_t proto);
 

tests/topotests/all_protocol_startup/r1/ipv4_routes.ref
@@ -8,6 +8,16 @@ C>* 192.168.6.0/26 is directly connected, r1-eth6, XX:XX:XX
 C>* 192.168.7.0/26 is directly connected, r1-eth7, XX:XX:XX
 C>* 192.168.8.0/26 is directly connected, r1-eth8, XX:XX:XX
 C>* 192.168.9.0/26 is directly connected, r1-eth9, XX:XX:XX
+L>* 192.168.0.1/32 is directly connected, r1-eth0, XX:XX:XX
+L>* 192.168.1.1/32 is directly connected, r1-eth1, XX:XX:XX
+L>* 192.168.2.1/32 is directly connected, r1-eth2, XX:XX:XX
+L>* 192.168.3.1/32 is directly connected, r1-eth3, XX:XX:XX
+L>* 192.168.4.1/32 is directly connected, r1-eth4, XX:XX:XX
+L>* 192.168.5.1/32 is directly connected, r1-eth5, XX:XX:XX
+L>* 192.168.6.1/32 is directly connected, r1-eth6, XX:XX:XX
+L>* 192.168.7.1/32 is directly connected, r1-eth7, XX:XX:XX
+L>* 192.168.8.1/32 is directly connected, r1-eth8, XX:XX:XX
+L>* 192.168.9.1/32 is directly connected, r1-eth9, XX:XX:XX
 O   192.168.0.0/24 [110/10] is directly connected, r1-eth0, weight 1, XX:XX:XX
 O   192.168.3.0/26 [110/10] is directly connected, r1-eth3, weight 1, XX:XX:XX
 S>* 1.1.1.1/32 [1/0] is directly connected, r1-eth1, weight 1, XX:XX:XX

tests/topotests/all_protocol_startup/r1/ipv6_routes.ref
@@ -19,6 +19,16 @@ C * fe80::/64 is directly connected, r1-eth6, XX:XX:XX
 C * fe80::/64 is directly connected, r1-eth7, XX:XX:XX
 C * fe80::/64 is directly connected, r1-eth8, XX:XX:XX
 C * fe80::/64 is directly connected, r1-eth9, XX:XX:XX
+L>* fc00:0:0:1::1/128 is directly connected, r1-eth1, XX:XX:XX
+L>* fc00:0:0:2::1/128 is directly connected, r1-eth2, XX:XX:XX
+L>* fc00:0:0:3::1/128 is directly connected, r1-eth3, XX:XX:XX
+L>* fc00:0:0:4::1/128 is directly connected, r1-eth4, XX:XX:XX
+L>* fc00:0:0:5::1/128 is directly connected, r1-eth5, XX:XX:XX
+L>* fc00:0:0:6::1/128 is directly connected, r1-eth6, XX:XX:XX
+L>* fc00:0:0:7::1/128 is directly connected, r1-eth7, XX:XX:XX
+L>* fc00:0:0:8::1/128 is directly connected, r1-eth8, XX:XX:XX
+L>* fc00:0:0:9::1/128 is directly connected, r1-eth9, XX:XX:XX
+L>* fc00::1/128 is directly connected, r1-eth0, XX:XX:XX
 O   fc00:0:0:4::/64 [110/10] is directly connected, r1-eth4, weight 1, XX:XX:XX
 S>* 4:5::6:10/128 [1/0] via fc00::2, r1-eth0, weight 1, XX:XX:XX
 S>* 4:5::6:11/128 [1/0] via fc00::2, r1-eth0, weight 1, XX:XX:XX

tests/topotests/ospf_multi_vrf_bgp_route_leak/r1/frr.conf
@@ -5,12 +5,18 @@ log file /tmp/r1-frr.log
 !
 interface r1-eth0
  ip address 10.0.1.1/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 interface r1-eth1
  ip address 10.0.20.1/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 interface r1-eth2 vrf neno
  ip address 10.0.30.1/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 ip forwarding
 !

tests/topotests/ospf_multi_vrf_bgp_route_leak/r2/frr.conf
@@ -5,14 +5,20 @@ log file /tmp/r2-frr.log
 !
 interface r2-eth0
  ip address 10.0.2.2/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 interface r2-eth1
  ip address 10.0.20.2/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 ip route 0.0.0.0/0 10.0.20.1
 !
 interface r2-eth2 vrf ray
  ip address 10.0.40.2/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 ip forwarding
 !

tests/topotests/ospf_multi_vrf_bgp_route_leak/r3/frr.conf
@@ -5,9 +5,13 @@ log file /tmp/r3-frr.log
 !
 interface r3-eth0
  ip address 10.0.3.3/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 interface r3-eth1
  ip address 10.0.30.3/24
+ ip ospf hello-interval 1
+ ip ospf dead-interval 4
 !
 ip forwarding
 !

commit 2611a893976672a34ba96fa9f3b62ef481e6c015

zebra: fix keep all local addresses for a given interface
On a setup with 4.4.4.4/24 address present, adding 4.4.4.40/24 address
results in unsyncing local entry 4.4.4.40/32.

The output on vtysh differs from the output on shell.

> # vtysh -c "show ip route" | grep 4.4.4.
> C>* 4.4.4.0/24 is directly connected, loo1, weight 1, 00:00:08
> L>* 4.4.4.4/32 is directly connected, loo1, weight 1, 00:00:08

> # ip route show table 255 | grep 4.4.4.
> local 4.4.4.4 dev loo1 proto kernel scope host src 4.4.4.4
> local 4.4.4.40 dev loo1 proto kernel scope host src 4.4.4.4
> broadcast 4.4.4.255 dev loo1 proto kernel scope link src 4.4.4.4

Consequently, using the secondary address for routing purposes may be
mishandled by ZEBRA. Fix this by keeping all the local entries.

Fixes: d4aa24ba7df0 ("*: Introduce Local Host Routes to FRR")

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/connected.c
@@ -236,6 +236,7 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 	uint32_t count = 0;
 	struct connected *c;
 	bool install_local = true;
+	bool install_prefix = true;
 
 	zvrf = ifp->vrf->info;
 	if (!zvrf) {
@@ -323,11 +324,13 @@ void connected_up(struct interface *ifp, struct connected *ifc)
 		    !CHECK_FLAG(c->conf, ZEBRA_IFC_DOWN))
 			count++;
 
-		if (count >= 2)
-			return;
+		if (count >= 2) {
+			install_prefix = false;
+			break;
+		}
 	}
 
-	if (!CHECK_FLAG(ifc->flags, ZEBRA_IFA_NOPREFIXROUTE)) {
+	if (!CHECK_FLAG(ifc->flags, ZEBRA_IFA_NOPREFIXROUTE) && install_prefix) {
 		connected_remove_kernel_for_connected(afi, SAFI_UNICAST, zvrf, &p, &nh);
 
 		rib_add(afi, SAFI_UNICAST, zvrf->vrf->vrf_id, ZEBRA_ROUTE_CONNECT, 0, flags, &p,
@@ -440,6 +443,7 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 	uint32_t count = 0;
 	struct connected *c;
 	bool remove_local = true;
+	bool remove_prefix = true;
 
 	zvrf = ifp->vrf->info;
 	if (!zvrf) {
@@ -521,15 +525,17 @@ void connected_down(struct interface *ifp, struct connected *ifc)
 		    !CHECK_FLAG(c->conf, ZEBRA_IFC_DOWN))
 			count++;
 
-		if (count >= 1)
-			return;
+		if (count >= 1) {
+			remove_prefix = false;
+			break;
+		}
 	}
 
 	/*
 	 * Same logic as for connected_up(): push the changes into the
 	 * head.
 	 */
-	if (!CHECK_FLAG(ifc->flags, ZEBRA_IFA_NOPREFIXROUTE)) {
+	if (!CHECK_FLAG(ifc->flags, ZEBRA_IFA_NOPREFIXROUTE) && remove_prefix) {
 		rib_delete(afi, SAFI_UNICAST, zvrf->vrf->vrf_id,
 			   ZEBRA_ROUTE_CONNECT, 0, 0, &p, NULL, &nh, 0,
 			   zvrf->table_id, 0, 0, false);