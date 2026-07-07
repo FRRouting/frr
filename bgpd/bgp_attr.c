732b55d1d9718e3198c795bc387b4a80c3165933
Carmine Scarpitta <cscarpit@cisco.com>
2026-07-07 19:17:02 +0200

    bgpd: Move Prefix-SID TLV length accounting
    Compute the Prefix-SID TLV size once and use it for the bounds check
    against the enclosing attribute before dispatching to the TLV parser.
    
    Account for the TLV immediately after that check. This makes the later
    post-dispatch overflow check redundant because TLVs that would exceed
    the Prefix-SID attribute length are rejected before dispatch.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>
    (cherry picked from commit 07ba73a08c2e228d698b33190df86d382feb5a55)

--- bgpd/bgp_attr.c
+++ bgpd/bgp_attr.c
@@ -3836,6 +3836,7 @@ enum bgp_attr_parse_ret bgp_attr_prefix_sid(struct bgp_attr_parser_args *args)
 	uint8_t type;
 	uint16_t length;
 	size_t headersz = sizeof(type) + sizeof(length);
+	size_t tlv_total_len;
 	size_t psid_parsed_length = 0;
 
 	if (peer->discard_attrs[args->type] || peer->withdraw_attrs[args->type])
@@ -3853,8 +3854,9 @@ enum bgp_attr_parse_ret bgp_attr_prefix_sid(struct bgp_attr_parser_args *args)
 
 		type = stream_getc(connection->curr);
 		length = stream_getw(connection->curr);
+		tlv_total_len = headersz + length;
 
-		if (((size_t)length + headersz + psid_parsed_length > (size_t)args->length) ||
+		if ((tlv_total_len + psid_parsed_length > (size_t)args->length) ||
 		    STREAM_READABLE(connection->curr) < length) {
 			flog_err(EC_BGP_ATTR_LEN,
 				 "Malformed Prefix SID attribute - insufficient data (need %hu for attribute body, have %zu remaining in UPDATE)",
@@ -3864,22 +3866,12 @@ enum bgp_attr_parse_ret bgp_attr_prefix_sid(struct bgp_attr_parser_args *args)
 						  args->total);
 		}
 
+		psid_parsed_length += tlv_total_len;
+
 		ret = bgp_attr_psid_sub(type, length, args);
 
 		if (ret != BGP_ATTR_PARSE_PROCEED)
 			return ret;
-
-		psid_parsed_length += length + headersz;
-
-		if (psid_parsed_length > args->length) {
-			flog_err(
-				EC_BGP_ATTR_LEN,
-				"Malformed Prefix SID attribute - TLV overflow by attribute (need %zu for TLV length, have %zu overflowed in UPDATE)",
-				length + headersz, psid_parsed_length - (length + headersz));
-			return bgp_attr_malformed(
-				args, BGP_NOTIFY_UPDATE_ATTR_LENG_ERR,
-				args->total);
-		}
 	}
 
 	bgp_attr_set(attr, BGP_ATTR_PREFIX_SID);
