82bfaf2e603f3741d6e7c35abcfcde07d44bb8c8
Evelyn0828 <jxshuibei@gmail.com>
2026-07-15 16:45:45 +0800

    ripd: allow neighbor updates with network IFNAME
    RIP accepts both prefix and interface-name forms of the network command.
    
    The neighbor unicast update path only checked the prefix-form table.
    
    That skipped updates when the outgoing interface was enabled only with network IFNAME.
    
    Reuse a single connected-interface enable helper that accepts either network form.
    
    Signed-off-by: Evelyn0828 <jxshuibei@gmail.com>

--- ripd/ripd.c
+++ ripd/ripd.c
@@ -2506,14 +2506,13 @@ static void rip_update_process(struct rip *rip, int route_type)
 			continue;
 		}
 
-		/* If unicast is used, but `network X.Y.Z.W` is not defined
-		 * for this interface, we SHOULD NOT send an update to this
-		 * neighbor.
+		/* If unicast is used, but no `network` statement enables
+		 * this interface, we SHOULD NOT send an update to this neighbor.
 		 * If RIP is configured on such an interface, the redistribution
 		 * of route(s) from another routing protocol into RIP, received
 		 * through that interface, does not work.
 		 */
-		if (rip_enable_network_lookup2(connected) < 0) {
+		if (rip_enable_network_connected_lookup(connected) < 0) {
 			if (RIP_DEBUG_SEND)
 				zlog_debug("Neighbor %pI4 is not in any `network` statement!",
 					   &p->u.prefix4);
