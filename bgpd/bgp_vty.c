FRRouting/frr

commit 14da03c5c008e5667b869df7aa28101cf8c50e63

bgpd: Free temporary memory after using argv_concat()
Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -8893,7 +8893,7 @@ DEFPY(neighbor_path_attribute_discard,
 {
 	struct peer *peer;
 	int idx = 0;
-	const char *discard_attrs = NULL;
+	char *discard_attrs = NULL;
 
 	peer = peer_and_group_lookup_vty(vty, neighbor);
 	if (!peer)
@@ -8904,6 +8904,8 @@ DEFPY(neighbor_path_attribute_discard,
 		discard_attrs = argv_concat(argv, argc, idx);
 
 	bgp_path_attribute_discard_vty(vty, peer, discard_attrs, true);
+
+	XFREE(MTYPE_TMP, discard_attrs);
 
 	return CMD_SUCCESS;
 }
@@ -8920,7 +8922,7 @@ DEFPY(no_neighbor_path_attribute_discard,
 {
 	struct peer *peer;
 	int idx = 0;
-	const char *discard_attrs = NULL;
+	char *discard_attrs = NULL;
 
 	peer = peer_and_group_lookup_vty(vty, neighbor);
 	if (!peer)
@@ -8931,6 +8933,8 @@ DEFPY(no_neighbor_path_attribute_discard,
 		discard_attrs = argv[idx]->arg;
 
 	bgp_path_attribute_discard_vty(vty, peer, discard_attrs, false);
+
+	XFREE(MTYPE_TMP, discard_attrs);
 
 	return CMD_SUCCESS;
 }
@@ -8946,7 +8950,7 @@ DEFPY(neighbor_path_attribute_treat_as_withdraw,
 {
 	struct peer *peer;
 	int idx = 0;
-	const char *withdraw_attrs = NULL;
+	char *withdraw_attrs = NULL;
 
 	peer = peer_and_group_lookup_vty(vty, neighbor);
 	if (!peer)
@@ -8957,6 +8961,8 @@ DEFPY(neighbor_path_attribute_treat_as_withdraw,
 		withdraw_attrs = argv_concat(argv, argc, idx);
 
 	bgp_path_attribute_withdraw_vty(vty, peer, withdraw_attrs, true);
+
+	XFREE(MTYPE_TMP, withdraw_attrs);
 
 	return CMD_SUCCESS;
 }
@@ -8973,7 +8979,7 @@ DEFPY(no_neighbor_path_attribute_treat_as_withdraw,
 {
 	struct peer *peer;
 	int idx = 0;
-	const char *withdraw_attrs = NULL;
+	char *withdraw_attrs = NULL;
 
 	peer = peer_and_group_lookup_vty(vty, neighbor);
 	if (!peer)
@@ -8984,6 +8990,8 @@ DEFPY(no_neighbor_path_attribute_treat_as_withdraw,
 		withdraw_attrs = argv_concat(argv, argc, idx);
 
 	bgp_path_attribute_withdraw_vty(vty, peer, withdraw_attrs, false);
+
+	XFREE(MTYPE_TMP, withdraw_attrs);
 
 	return CMD_SUCCESS;
 }

commit a953323a4c7d85273ffd6e9fa6b6648904bfb1e4

bgpd: Fix crash for `no neighbor X path-attribute discard ...`
Use the same pattern as no_neighbor_path_attribute_treat_as_withdraw_cmd has.

This one was missed somehow.

Fixes: 14da03c5c0 ("bgpd: Free temporary memory after using argv_concat()")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_vty.c
@@ -10315,7 +10315,7 @@ DEFPY(no_neighbor_path_attribute_discard,
 
 	argv_find(argv, argc, "(1-255)", &idx);
 	if (idx)
-		discard_attrs = argv[idx]->arg;
+		discard_attrs = argv_concat(argv, argc, idx);
 
 	bgp_path_attribute_discard_vty(vty, peer, discard_attrs, false);
 