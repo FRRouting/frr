12ec75eeb4bbe60755a6c08f96a7a5e96e844376
Philippe Guibert <philippe.guibert@6wind.com>
2026-07-31 11:17:24 +0200

    bgpd: add addpath-tx-best-per-encapsulation
    Exported ECMP entries result in duplicating VPN entries; if both SRv6
    and MPLS are set, then the number of VPN entries are duplicated too.
    Propose to add a new addpath service that filters advertised BGP updates
    based on their unique encapsulation.
    
    In vtysh,
    > conf terminal
    > router bgp 65500
    >  bgp encapsulation-selection
    >  address-family ipv4 vpn
    >  neighbor 100.100.100.100 addpath-tx-bestpath-per-encapsulation
    >  neighbor 2002:db8:5:: addpath-tx-bestpath-per-encapsulation
    >  exit-address-family
    
    This results in the DUT selecting the best route among the list of ECMP prefixes, sorted by encapsulation.
    > pe1-vm# show bgp ipv4 vpn 111.111.111.111/32
    > BGP routing table entry for 1.1.1.1:1:111.111.111.111/32, version 32
    > not allocated
    > Paths: (4 available, best #4)
    > [..]
    >   Local
    >     11.11.100.2 from 0.0.0.0 (1.1.1.1) vrf vrfA(14) announce-nh-self
    >       Origin IGP, metric 0, localpref 100, valid, sourced, local, multipath, bestpath-from-Encapsulation srv6
    >       Extended Community: RT:1:1
    >       Originator: 1.1.1.1
    >       Remote label: 917552
    >       Remote SID: 2002:db8:1::, sid structure=[32 16 16 0 16 48]
    >       AddPath ID: RX 0, TX-All 10 TX-Best-Per-AS 0 TX-Best-Selected 0 TX-Best-Per-Encapsulation 8
    >       Advertised to: 2002:db8:5::
    >       Last update: Mon Aug  3 11:02:15 2026
    >   Local
    >     11.11.100.2 from 0.0.0.0 (1.1.1.1) vrf vrfA(14) announce-nh-self
    >       Origin IGP, metric 0, localpref 100, valid, sourced, local, multipath, bestpath-from-Encapsulation mpls, best (Nothing left to compare)
    >       Extended Community: RT:1:1
    >       Originator: 1.1.1.1
    >       Remote label: 19
    >       AddPath ID: RX 0, TX-All 11 TX-Best-Per-AS 0 TX-Best-Selected 0 TX-Best-Per-Encapsulation 9
    >       Advertised to: 100.100.100.100
    >       Last update: Mon Aug  3 11:22:21 2026
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -3968,6 +3968,97 @@ void bgp_best_selection(struct bgp *bgp, struct bgp_dest *dest,
 	bgp_path_info_mpath_update(bgp, dest, new_select, old_select, num_candidates, mpath_cfg);
 	bgp_path_info_mpath_aggregate_update(new_select, old_select);
 
+
+	struct bgp_path_info *pi_mpls = NULL, *pi_srv6 = NULL, *pi_tmp = NULL;
+
+	if (safi != SAFI_MPLS_VPN)
+		goto end_bgp_best_selection;
+
+	if (!CHECK_FLAG(bgp->flags, BGP_FLAG_ENCAPSULATION_SELECTION) || new_select == NULL) {
+		for (pi1 = bgp_dest_get_bgp_path_info(dest); pi1; pi1 = pi1->next)
+			if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_unset_flag(dest, pi1,
+							 BGP_PATH_ENCAPSULATION_SELECTED);
+		goto end_bgp_best_selection;
+	}
+
+	/* Mark BGP_PATH_ENCAPSULATION_SELECTED for best paths with flag PATH_SELECTED */
+	if (new_select) {
+		if (bgp_attr_get_srv6_l3service(new_select->attr) ||
+		    bgp_attr_get_srv6_vpn(new_select->attr))
+			pi_srv6 = new_select;
+		else if (bgp_mplsvpn_path_uses_valid_mpls_label(new_select))
+			pi_mpls = new_select;
+
+		if (pi_mpls || pi_srv6) {
+			if (!CHECK_FLAG(new_select->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_set_flag(dest, new_select,
+						       BGP_PATH_ENCAPSULATION_SELECTED);
+		}
+	}
+
+	if (pi_mpls == NULL && pi_srv6 == NULL) {
+		for (pi1 = bgp_dest_get_bgp_path_info(dest); pi1; pi1 = pi1->next)
+			if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_unset_flag(dest, pi1,
+							 BGP_PATH_ENCAPSULATION_SELECTED);
+		goto end_bgp_best_selection;
+	}
+
+	/* Unmark BGP_PATH_ENCAPSULATION_SELECTED :
+	 * - for other paths with flag not set to PATH_MULTIPATH
+	 * - for other paths with flag set to PATH_MULTIPATH but redundant with SELECTED pi dataplane
+	 * mark BGP_PATH_ENCAPSULATION_SELECTED :
+	 * - if no other matching pi has the same mark (for deterministically having same PI
+	 * - reuse existing mark when available
+	 */
+	for (pi1 = bgp_dest_get_bgp_path_info(dest); pi1; pi1 = pi1->next) {
+		if (pi1 == new_select)
+			continue;
+
+		if (!CHECK_FLAG(pi1->flags, BGP_PATH_MULTIPATH)) {
+			if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_unset_flag(dest, pi1,
+							 BGP_PATH_ENCAPSULATION_SELECTED);
+			continue;
+		}
+
+		if (!bgp_mplsvpn_path_uses_valid_mpls_label(pi1) &&
+		    !bgp_attr_get_srv6_l3service(pi1->attr) && !bgp_attr_get_srv6_vpn(pi1->attr)) {
+			if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_unset_flag(dest, pi1,
+							 BGP_PATH_ENCAPSULATION_SELECTED);
+			continue;
+		}
+
+		if ((pi_mpls && pi_mpls != pi1 && bgp_mplsvpn_path_uses_valid_mpls_label(pi1)) ||
+		    (pi_srv6 && pi_srv6 != pi1 &&
+		     (bgp_attr_get_srv6_l3service(pi1->attr) || bgp_attr_get_srv6_vpn(pi1->attr)))) {
+			if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+				bgp_path_info_unset_flag(dest, pi1,
+							 BGP_PATH_ENCAPSULATION_SELECTED);
+			continue;
+		}
+
+		/* DATAPLANE_SELECTED is present */
+		if (CHECK_FLAG(pi1->flags, BGP_PATH_ENCAPSULATION_SELECTED)) {
+			if (pi_mpls)
+				pi_srv6 = pi1;
+			else if (pi_srv6)
+				pi_mpls = pi1;
+			break;
+		}
+		/* remember first available pi1 */
+		pi_tmp = pi1;
+	}
+
+	if (!pi_mpls && pi_tmp && !CHECK_FLAG(pi_tmp->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+		bgp_path_info_set_flag(dest, pi_tmp, BGP_PATH_ENCAPSULATION_SELECTED);
+
+	if (!pi_srv6 && pi_tmp && !CHECK_FLAG(pi_tmp->flags, BGP_PATH_ENCAPSULATION_SELECTED))
+		bgp_path_info_set_flag(dest, pi_tmp, BGP_PATH_ENCAPSULATION_SELECTED);
+
+end_bgp_best_selection:
 	bgp_addpath_update_ids(bgp, dest, afi, safi);
 
 	result->old = old_select;
@@ -14316,6 +14407,19 @@ skip_nexthop:
 				vty_out(vty, ", bestpath-from-AS %u", first_as);
 			else
 				vty_out(vty, ", bestpath-from-AS Local");
+		}
+	}
+
+	if (CHECK_FLAG(path->flags, BGP_PATH_ENCAPSULATION_SELECTED)) {
+		bool is_mpls = bgp_mplsvpn_path_uses_valid_mpls_label(path);
+
+		if (json_paths) {
+			if (!json_bestpath)
+				json_bestpath = json_object_new_object();
+			json_object_string_add(json_bestpath, "bestpathFromEncapsulation",
+					       is_mpls ? "mpls" : "srv6");
+		} else {
+			vty_out(vty, ", bestpath-from-Encapsulation %s", is_mpls ? "mpls" : "srv6");
 		}
 	}
 
