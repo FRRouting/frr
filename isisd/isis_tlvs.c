5e1f2b13fae80d848e72dc0f5d11822d58ed29f1
Carmine Scarpitta <cscarpit@cisco.com>
2026-08-01 16:15:50 +0200

    isisd: Use subTLV header size in Router Capability parsing
    Use ISIS_SUBTLV_HDR_SIZE for Router Capability subTLV header
    calculations instead of the literal value 2.
    
    This keeps the existing parser behavior while making it clear that the
    value accounts for the subTLV Type/Length header.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>
    (cherry picked from commit 9e3672f4813e2ef804c24859ce244f2177b26a01)
    
    # Conflicts:
    #	isisd/isis_tlvs.c

--- isisd/isis_tlvs.c
+++ isisd/isis_tlvs.c
@@ -5481,7 +5481,7 @@ static int unpack_tlv_router_cap(enum isis_tlv_context context,
 
 	/* Parse remaining part of the TLV if present */
 	subtlv_len = tlv_len - ISIS_ROUTER_CAP_SIZE;
-	while (subtlv_len > 2) {
+	while (subtlv_len > ISIS_SUBTLV_HDR_SIZE) {
 #ifndef FABRICD
 		struct isis_router_cap_fad *fad;
 		uint8_t subsubtlvs_len;
@@ -5491,10 +5491,16 @@ static int unpack_tlv_router_cap(enum isis_tlv_context context,
 		type = stream_getc(s);
 		length = stream_getc(s);
 
+<<<<<<< HEAD
 		if (length > STREAM_READABLE(s) || length > subtlv_len - 2) {
 			sbuf_push(
 				log, indent,
 				"WARNING: Router Capability subTLV length too large compared to expected size\n");
+=======
+		if (length > STREAM_READABLE(s) || length > subtlv_len - ISIS_SUBTLV_HDR_SIZE) {
+			sbuf_push(log, indent,
+				  "WARNING: Router Capability subTLV length too large compared to expected size\n");
+>>>>>>> 9e3672f48 (isisd: Use subTLV header size in Router Capability parsing)
 			stream_forward_getp(s, STREAM_READABLE(s));
 			return 0;
 		}
@@ -5850,7 +5856,7 @@ static int unpack_tlv_router_cap(enum isis_tlv_context context,
 			stream_forward_getp(s, length);
 			break;
 		}
-		subtlv_len = subtlv_len - length - 2;
+		subtlv_len -= length + ISIS_SUBTLV_HDR_SIZE;
 	}
 	return 0;
 }
