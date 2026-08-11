46a76f3cfe41a432dd5d8273ed723f8ea09f1c18
Donald Sharp <sharpd@nvidia.com>
2026-08-11 12:48:17 -0400

    bgpd: expose BGP extcommunity attribute to Lua match scripts
    Round-trip extended communities as opaque strings (RT/SoO/etc.).
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_script.c
+++ bgpd/bgp_script.c
@@ -221,6 +221,81 @@ static void bgp_attr_script_apply_lcommunity(struct attr *dst, struct attr *src)
 	bgp_attr_set_lcommunity(src, NULL);
 }
 
+
+static void lua_push_ecommunity_field(lua_State *L, struct ecommunity *ecom,
+				      const char *key)
+{
+	/*
+	 * Use community-list form ("rt 100:1") so ecommunity_str2com(..., 0, 1)
+	 * can round-trip. ecommunity_str() is DISPLAY form ("RT:100:1"), which
+	 * the parser does not accept.
+	 */
+	if (ecom) {
+		char *str = ecommunity_ecom2str(ecom,
+						ECOMMUNITY_FORMAT_COMMUNITY_LIST,
+						0);
+
+		lua_pushstring(L, str ? str : "");
+		XFREE(MTYPE_ECOMMUNITY_STR, str);
+	} else
+		lua_pushnil(L);
+	lua_setfield(L, -2, key);
+}
+
+static void lua_decode_ecommunity_field(lua_State *L, int idx, struct attr *attr)
+{
+	struct ecommunity *new_ecom = NULL;
+	struct ecommunity *old;
+	const char *str;
+
+	lua_getfield(L, idx, "extcommunity");
+	if (lua_isnil(L, -1)) {
+		bgp_attr_set_ecommunity(attr, NULL);
+		lua_pop(L, 1);
+		return;
+	}
+
+	str = lua_tostring(L, -1);
+	old = bgp_attr_get_ecommunity(attr);
+	if (str && old) {
+		char *cur = ecommunity_ecom2str(old,
+						ECOMMUNITY_FORMAT_COMMUNITY_LIST,
+						0);
+		int same = cur && strcmp(cur, str) == 0;
+
+		XFREE(MTYPE_ECOMMUNITY_STR, cur);
+		if (same) {
+			lua_pop(L, 1);
+			return;
+		}
+	}
+
+	if (str && *str)
+		new_ecom = ecommunity_str2com(str, 0, 1);
+	if (!new_ecom && str && *str) {
+		lua_pop(L, 1);
+		return;
+	}
+
+	bgp_attr_set_ecommunity(attr, new_ecom);
+	lua_pop(L, 1);
+}
+
+static void bgp_attr_script_apply_ecommunity(struct attr *dst, struct attr *src)
+{
+	struct ecommunity *old = bgp_attr_get_ecommunity(dst);
+	struct ecommunity *new = bgp_attr_get_ecommunity(src);
+
+	if (old == new)
+		return;
+
+	if (old && old->refcnt == 0)
+		ecommunity_free(&old);
+
+	bgp_attr_set_ecommunity(dst, new);
+	bgp_attr_set_ecommunity(src, NULL);
+}
+
 static void bgp_attr_script_apply_aspath(struct attr *dst, struct attr *src)
 {
 	if (dst->aspath == src->aspath)
@@ -286,6 +361,7 @@ void bgp_attr_script_apply(struct attr *dst, struct attr *src)
 
 	bgp_attr_script_apply_community(dst, src);
 	bgp_attr_script_apply_lcommunity(dst, src);
+	bgp_attr_script_apply_ecommunity(dst, src);
 	bgp_attr_script_apply_aspath(dst, src);
 }
 
@@ -310,6 +386,15 @@ void bgp_attr_script_discard(struct attr *working, const struct attr *orig)
 		if (lcom && lcom != olcom && lcom->refcnt == 0)
 			lcommunity_free(&lcom);
 		bgp_attr_set_lcommunity(working, NULL);
+	}
+
+	{
+		struct ecommunity *ecom = bgp_attr_get_ecommunity(working);
+		struct ecommunity *oecom = bgp_attr_get_ecommunity(orig);
+
+		if (ecom && ecom != oecom && ecom->refcnt == 0)
+			ecommunity_free(&ecom);
+		bgp_attr_set_ecommunity(working, NULL);
 	}
 }
 
@@ -490,6 +575,8 @@ void lua_pushattr(lua_State *L, const struct attr *attr)
 	lua_push_community_field(L, bgp_attr_get_community(attr), "community");
 	lua_push_lcommunity_field(L, bgp_attr_get_lcommunity(attr),
 				  "large_community");
+	lua_push_ecommunity_field(L, bgp_attr_get_ecommunity(attr),
+				  "extcommunity");
 }
 
 void lua_decode_attr(lua_State *L, int idx, struct attr *attr)
@@ -584,6 +671,7 @@ void lua_decode_attr(lua_State *L, int idx, struct attr *attr)
 
 	lua_decode_community_field(L, idx, attr);
 	lua_decode_lcommunity_field(L, idx, attr);
+	lua_decode_ecommunity_field(L, idx, attr);
 
 	/* pop the attributes table */
 	lua_pop(L, 1);
