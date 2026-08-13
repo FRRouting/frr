db3d4316cd053ff9a0348efa32ece11b284c05c3
Rafael Zalamena <rzalamena@opensourcerouting.org>
2026-08-13 17:10:53 +0000

    pimd: remove duplicated code
    Reuse the function to set socket non blocking instead of rewritting it.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- pimd/pim_sock.h
+++ pimd/pim_sock.h
@@ -16,9 +16,7 @@
 #define PIM_SOCK_ERR_TTL     (-4) /* TTL option */
 #define PIM_SOCK_ERR_LOOP    (-5) /* Loopback option */
 #define PIM_SOCK_ERR_IFACE   (-6) /* Outgoing interface option */
-#define PIM_SOCK_ERR_DSTADDR (-7) /* Outgoing interface option */
-#define PIM_SOCK_ERR_NONBLOCK_GETFL (-8) /* Get O_NONBLOCK */
-#define PIM_SOCK_ERR_NONBLOCK_SETFL (-9) /* Set O_NONBLOCK */
+#define PIM_SOCK_ERR_DSTADDR	    (-7) /* Outgoing interface option */
 #define PIM_SOCK_ERR_NAME    (-10) /* Socket name (getsockname) */
 #define PIM_SOCK_ERR_BIND    (-11) /* Can't bind to interface */
 #define PIM_SOCK_ERR_PKTINFO	    (-12) /* Can't set IP_PKTINFO */
