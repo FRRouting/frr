FRRouting/frr

commit 84bb1de14a373e57ad9057848fec1c0c243af64a

bgpd: fix bugs with suppress-duplicates
When determining whether the attribute of a new update is the same as
that of the preivous update, currently a hash value is calculated from
the attribute and is then compared with the hash value stored for the
previous update.

Two issues are identified and fixed in this patch:

1) The comparison of the hash value alone is insufficient.

   That's not correct when two different attributes end up with an
   identical hash value. It would result in missing updates.

   The fix is to compare the attributes fully by intern'ing the new
   attribute first.

2) The queued withdraw is not accounted for when suppressing duplicate
   updates. This can result in missing updates with rapid route changes,
   as reported in the following commit that implements a workaround:

   commit 1eed79229e7c63d7353993df2fa78222ee062980
   Author: Donatas Abraitis <donatas@opensourcerouting.org>
   Date:   Thu Jun 5 11:21:48 2025 +0300
   bgpd: Force adj-rib-out updates if MRAI is kicked in

   The issue is fixed in this patch by removing the queued withdraw
   when supressing duplicate updates.

Signed-off-by: Enke Chen <enchen@paloaltonetworks.com>

bgpd/bgp_updgrp_adv.c
@@ -544,7 +544,7 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 			      struct update_subgroup *subgrp, struct attr *attr,
 			      struct bgp_path_info *path)
 {
-	struct bgp_adj_out *adj = NULL;
+	struct bgp_adj_out *adj;
 	struct bgp_advertise *adv;
 	struct peer *peer;
 	afi_t afi;
@@ -552,7 +552,8 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 	struct peer *adv_peer;
 	struct peer_af *paf;
 	struct bgp *bgp;
-	uint32_t attr_hash = 0;
+	struct attr *attr_new;
+	bool debug;
 
 	peer = SUBGRP_PEER(subgrp);
 	afi = SUBGRP_AFI(subgrp);
@@ -561,6 +562,8 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 
 	if (DISABLE_BGP_ANNOUNCE)
 		return false;
+
+	debug = BGP_DEBUG(update, UPDATE_OUT);
 
 	/* Look for adjacency information. */
 	adj = adj_lookup(
@@ -586,15 +589,18 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 	 * at egress, neighbors will see duplicate UPDATES despite
 	 * the route wasn't changed actually.
 	 * Do not suppress BGP UPDATES for route-refresh.
+	 *
+	 * It's a duplicate UPDATE if the new attribute is the same as
+	 * the one that was sent out, or the one that has been queued.
 	 */
-	if (likely(CHECK_FLAG(bgp->flags, BGP_FLAG_SUPPRESS_DUPLICATES)))
-		attr_hash = attrhash_key_make(attr);
+	attr_new = bgp_attr_intern(attr);
 
-	if (!CHECK_FLAG(subgrp->sflags, SUBGRP_STATUS_FORCE_UPDATES) &&
-	    attr_hash && adj->attr_hash == attr_hash &&
-	    bgp_labels_cmp(path->extra ? path->extra->labels : NULL,
-			   adj->labels)) {
-		if (BGP_DEBUG(update, UPDATE_OUT)) {
+	if (CHECK_FLAG(bgp->flags, BGP_FLAG_SUPPRESS_DUPLICATES) &&
+	    !CHECK_FLAG(subgrp->sflags, SUBGRP_STATUS_FORCE_UPDATES) &&
+	    ((attr_new == adj->attr) ||
+	     (adj->adv && adj->adv->baa && (attr_new == adj->adv->baa->attr))) &&
+	    bgp_labels_cmp(path->extra ? path->extra->labels : NULL, adj->labels)) {
+		if (debug) {
 			char attr_str[BUFSIZ] = {0};
 
 			bgp_dump_attr(attr, attr_str, sizeof(attr_str));
@@ -602,6 +608,22 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 			zlog_debug("%s suppress UPDATE %pBD w/ attr: %s, afi=%s, safi=%s",
 				   peer->host, dest, attr_str, afi2str(afi), safi2str(safi));
 		}
+
+		/*
+		 * If this is a duplicate of the update that was sent out,
+		 * clean up the queued update, regardless whether it is a
+		 * withdraw or an advertisement.
+		 */
+		if (adj->adv && (attr_new == adj->attr)) {
+			if (debug) {
+				zlog_debug("%s delete queued UPDATE %pBD, afi=%s, safi=%s",
+					   peer->host, dest, afi2str(afi), safi2str(safi));
+			}
+
+			bgp_advertise_clean_subgroup(subgrp, adj);
+		}
+
+		bgp_attr_unintern(&attr_new);
 
 		/*
 		 * If BGP is skipping sending this value to it's peers
@@ -632,9 +654,8 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 	/* bgp_path_info adj_out reference */
 	adv->pathi = bgp_path_info_lock(path);
 
-	adv->baa = bgp_advertise_attr_intern(subgrp->hash, attr);
+	adv->baa = bgp_advertise_attr_intern(subgrp->hash, attr_new);
 	adv->adj = adj;
-	adj->attr_hash = attr_hash;
 	if (path->extra)
 		adj->labels = bgp_labels_intern(path->extra->labels);
 	else
@@ -642,6 +663,11 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 
 	/* Add new advertisement to advertisement attribute list. */
 	bgp_advertise_add(adv->baa, adv);
+
+	if (debug) {
+		zlog_debug("%s queue UPDATE %pBD, afi=%s, safi=%s", peer->host, dest, afi2str(afi),
+			   safi2str(safi));
+	}
 
 	/*
 	 * If the update adv list is empty, trigger the member peers'
@@ -686,6 +712,9 @@ void bgp_adj_out_unset_subgroup(struct bgp_dest *dest,
 	struct bgp_adj_out *adj;
 	struct bgp_advertise *adv;
 	bool trigger_write;
+	afi_t afi;
+	safi_t safi;
+	struct peer *peer;
 
 	if (DISABLE_BGP_ANNOUNCE)
 		return;
@@ -719,6 +748,15 @@ void bgp_adj_out_unset_subgroup(struct bgp_dest *dest,
 			/* Add to synchronization entry for withdraw
 			 * announcement.  */
 			bgp_adv_fifo_add_tail(&subgrp->sync->withdraw, adv);
+
+			if (BGP_DEBUG(update, UPDATE_OUT)) {
+				peer = SUBGRP_PEER(subgrp);
+				afi = SUBGRP_AFI(subgrp);
+				safi = SUBGRP_SAFI(subgrp);
+
+				zlog_debug("%s queue UPDATE (withdraw) %pBD, afi=%s, safi=%s",
+					   peer->host, dest, afi2str(afi), safi2str(safi));
+			}
 
 			if (trigger_write)
 				subgroup_trigger_write(subgrp);

bgpd/bgp_advertise.c
@@ -103,7 +103,8 @@ struct bgp_advertise_attr *bgp_advertise_attr_intern(struct hash *hash,
 	struct bgp_advertise_attr ref;
 	struct bgp_advertise_attr *baa;
 
-	ref.attr = bgp_attr_intern(attr);
+	/* The attr is already intern'ed */
+	ref.attr = attr;
 	baa = (struct bgp_advertise_attr *)hash_get(
 		hash, &ref, bgp_advertise_attr_hash_alloc);
 	baa->refcnt++;

bgpd/bgp_advertise.h
@@ -69,9 +69,6 @@ struct bgp_adj_out {
 
 	uint32_t addpath_tx_id;
 
-	/* Attribute hash */
-	uint32_t attr_hash;
-
 	/* Advertised attribute.  */
 	struct attr *attr;
 

commit 56c0b8ccd91c2f6a9439aca7afb65b0a752c8903

bgpd: fix crash when 'debug bgp updates out' enabled
The following crash happens.

> #2  __GI___pthread_kill (threadid=<optimized out>, signo=signo@entry=11) at ./nptl/pthread_kill.c:89
> #3  0x000073e57164527e in __GI_raise (sig=sig@entry=11) at ../sysdeps/posix/raise.c:26
> #4  0x000073e571b170ca in core_handler (signo=11, siginfo=0x7ffcb67b8ff0, context=<optimized out>) at lib/sigevent.c:267
> #5  <signal handler called>
> #6  bgp_dump_attr (attr=attr@entry=0x7ffcb67bb5d0, buf=buf@entry=0x7ffcb67b9550 "nexthop 0.0.0.0, metric 0, extcommunity RT:77:77, originator 192.0.2.1, path ", size=size@entry=8192) at bgpd/bgp_debug.c:506
> #7  0x00005e8134cf1254 in bgp_adj_out_set_subgroup (dest=dest@entry=0x5e8153c50b60, subgrp=subgrp@entry=0x5e8153c564d0, attr=attr@entry=0x7ffcb67bb5d0, path=path@entry=0x5e8153c50c10) at bgpd/bgp_updgrp_adv.c:605
> #8  0x00005e8134cb3759 in subgroup_process_announce_selected (subgrp=subgrp@entry=0x5e8153c564d0, selected=0x5e8153c50c10, dest=0x5e8153c50b60, afi=afi@entry=AFI_IP, safi=safi@entry=SAFI_MPLS_VPN, addpath_tx_id=0)
>     at bgpd/bgp_route.c:4131
> #9  0x00005e8134cf08ab in group_announce_route_walkcb (updgrp=<optimized out>, arg=0x7ffcb67bb860) at bgpd/bgp_updgrp_adv.c:280
> #10 0x000073e571ab96a2 in hash_walk (hash=0x5e8153c71b90, func=func@entry=0x5e8134ceb650 <update_group_walkcb>, arg=arg@entry=0x7ffcb67bb7b0) at lib/hash.c:270
> #11 0x00005e8134cef8b5 in update_group_af_walk (bgp=bgp@entry=0x5e8153c5e540, afi=<optimized out>, safi=<optimized out>, cb=cb@entry=0x5e8134cf0750 <group_announce_route_walkcb>, ctx=ctx@entry=0x7ffcb67bb860) at bgpd/bgp_updgrp.c:2182
> #12 0x00005e8134cf246d in group_announce_route (bgp=bgp@entry=0x5e8153c5e540, afi=afi@entry=AFI_IP, safi=safi@entry=SAFI_MPLS_VPN, dest=dest@entry=0x5e8153c50b60, pi=pi@entry=0x5e8153c50c10) at bgpd/bgp_updgrp_adv.c:1195
> #13 0x00005e8134cd5a75 in bgp_process_main_one (bgp=0x5e8153c5e540, dest=dest@entry=0x5e8153c50b60, afi=AFI_IP, safi=SAFI_MPLS_VPN) at bgpd/bgp_route.c:4693
> #14 0x00005e8134cd6d36 in process_subq_other_route (dest=<optimized out>) at bgpd/bgp_route.c:5206
> #15 process_subq (qindex=<optimized out>, subq=<optimized out>) at bgpd/bgp_route.c:5253
> #16 meta_queue_process (dummy=<optimized out>, data=0x5e8153c5c8e0) at bgpd/bgp_route.c:5289
> #17 0x000073e571b363b3 in work_queue_run (event=0x7ffcb67bbb80) at lib/workqueue.c:279
> #18 0x000073e571b2b3fe in event_call (event=event@entry=0x7ffcb67bbb80) at lib/event.c:2744
> #19 0x000073e571ac8668 in frr_run (loop=0x5e8153290fc0) at lib/libfrr.c:1258

Attr_new should be used, because a recent commit used another attr
by means of attr_intern() function. Actually, the old attr pointer
was freed.

Fixes: 84bb1de14 ("bgpd: fix bugs with suppress-duplicates")

bgpd/bgp_updgrp_adv.c
@@ -602,7 +602,7 @@ bool bgp_adj_out_set_subgroup(struct bgp_dest *dest,
 		if (debug) {
 			char attr_str[BUFSIZ] = {0};
 
-			bgp_dump_attr(attr, attr_str, sizeof(attr_str));
+			bgp_dump_attr(attr_new, attr_str, sizeof(attr_str));
 
 			zlog_debug("%s suppress UPDATE %pBD w/ attr: %s, afi=%s, safi=%s",
 				   peer->host, dest, attr_str, afi2str(afi), safi2str(safi));