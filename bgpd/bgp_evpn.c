FRRouting/frr

commit f7242a44b300da1aa9a93140a8fb650c561ca9c9

bgpd: Fix missing `advertise-pip`
If you enter this:

router bgp 32939 vrf BLUE
 address-family l2vpn evpn
   advertise-pip

show run does not display `advertise-pip`:
!
router bgp 39239 vrf BLUE
exit
!

Fix it.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgp_evpn_vty.c
@@ -7778,7 +7778,8 @@ void bgp_config_write_evpn_info(struct vty *vty, struct bgp *bgp, afi_t afi,
 							buf, sizeof(buf)));
 				}
 				vty_out(vty, "\n");
-			}
+			} else
+				vty_out(vty, "  advertise-pip\n");
 		}
 	}
 	if (CHECK_FLAG(bgp->vrf_flags, BGP_VRF_RD_CFGD))

commit 1225328e1d43f1b7031b898ae736d6bdef1f4904

bgpd: Do not print `advertise-pip` if it has a default value
Fixes: f7242a4 ("bgpd: Fix missing advertise-pip")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_evpn.c
@@ -8544,11 +8544,11 @@ void bgp_evpn_init(struct bgp *bgp)
 		bgp->evpn_info->dad_freeze_time = 0;
 		/* Initialize zebra vxlan */
 		bgp_zebra_dup_addr_detection(bgp);
-		/* Enable PIP feature by default for bgp vrf instance */
+
 		if (bgp->inst_type == BGP_INSTANCE_TYPE_VRF) {
 			struct bgp *bgp_default;
 
-			bgp->evpn_info->advertise_pip = true;
+			bgp->evpn_info->advertise_pip = DFLT_BGP_EVPN_ADVERTISE_PIP;
 			bgp_default = bgp_get_default();
 			if (bgp_default) {
 				SET_IPADDR_V4(&bgp->evpn_info->pip_ip);

bgpd/bgp_evpn_private.h
@@ -12,6 +12,10 @@
 #include "bgpd/bgpd.h"
 #include "bgpd/bgp_ecommunity.h"
 
+FRR_CFG_DEFAULT_BOOL(BGP_EVPN_ADVERTISE_PIP,
+	{ .val_bool = true },
+);
+
 /* It's not clear how "28" was derived.
  * Worst Case length for <local_admin>:<global_admin> is IPv4, i.e.
  * "255.255.255.255:65535" = 15 + 1 + 5

bgpd/bgp_evpn_vty.c
@@ -4799,7 +4799,9 @@ DEFPY (bgp_evpn_advertise_pip_ip_mac,
 			if (IPV4_ADDR_SAME(&ip, &bgp_vrf->evpn_info->pip_ip_static.ipaddr_v4))
 				return CMD_SUCCESS;
 
+			SET_IPADDR_V4(&bgp_vrf->evpn_info->pip_ip_static);
 			bgp_vrf->evpn_info->pip_ip_static.ipaddr_v4 = ip;
+			SET_IPADDR_V4(&bgp_vrf->evpn_info->pip_ip);
 			bgp_vrf->evpn_info->pip_ip.ipaddr_v4 = ip;
 		} else {
 			bgp_vrf->evpn_info->pip_ip_static.ipaddr_v4.s_addr = INADDR_ANY;
@@ -8204,22 +8206,20 @@ void bgp_config_write_evpn_info(struct vty *vty, struct bgp *bgp, afi_t afi, saf
 		vty_out(vty, "  default-originate ipv6\n");
 
 	if (bgp->inst_type == BGP_INSTANCE_TYPE_VRF) {
-		if (!bgp->evpn_info->advertise_pip)
-			vty_out(vty, "  no advertise-pip\n");
-		if (bgp->evpn_info->advertise_pip) {
-			if (bgp->evpn_info->pip_ip_static.ipaddr_v4.s_addr != INADDR_ANY) {
-				vty_out(vty, "  advertise-pip ip %pIA",
-					&bgp->evpn_info->pip_ip_static);
-				if (!is_zero_mac(&(bgp->evpn_info->pip_rmac_static))) {
-					char buf[ETHER_ADDR_STRLEN];
+		if (bgp->evpn_info->advertise_pip &&
+		    bgp->evpn_info->pip_ip_static.ipaddr_v4.s_addr != INADDR_ANY) {
+			vty_out(vty, "  advertise-pip ip %pIA", &bgp->evpn_info->pip_ip_static);
+			if (!is_zero_mac(&(bgp->evpn_info->pip_rmac_static))) {
+				char buf[ETHER_ADDR_STRLEN];
 
-					vty_out(vty, " mac %s",
-						prefix_mac2str(&bgp->evpn_info->pip_rmac, buf,
-							       sizeof(buf)));
-				}
-				vty_out(vty, "\n");
-			} else
-				vty_out(vty, "  advertise-pip\n");
+				vty_out(vty, " mac %s",
+					prefix_mac2str(&bgp->evpn_info->pip_rmac, buf,
+						       sizeof(buf)));
+			}
+			vty_out(vty, "\n");
+		} else if (bgp->evpn_info->advertise_pip != SAVE_BGP_EVPN_ADVERTISE_PIP) {
+			vty_out(vty, "  %sadvertise-pip\n",
+				bgp->evpn_info->advertise_pip ? "" : "no ");
 		}
 	}
 	if (CHECK_FLAG(bgp->vrf_flags, BGP_VRF_RD_CFGD))