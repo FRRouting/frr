FRRouting/frr

commit d1a9f41030cff796c5b82beafc9e8c66f8df7c37

bgpd: Do not establish session for dynamic peers if BFD strict is enabled
If we have BFD strict mode enabled from the other side, and dynamic peering
is used, we shouldn't allow the session going UP before BFD comes up.

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_bfd.c
@@ -54,6 +54,49 @@ static void bgp_bfd_strict_holdtime_expire(struct event *event)
 	BGP_EVENT_ADD(peer->connection, BGP_Stop);
 }
 
+bool bgp_bfd_strict_hold_start(struct peer *peer)
+{
+	struct peer_connection *connection = peer->connection;
+
+	if (!peer->bfd_config || !CHECK_FLAG(peer->flags, PEER_FLAG_BFD_STRICT))
+		return false;
+
+	if (bfd_session_is_up(peer->bfd_config->session))
+		return false;
+
+	if (bgp_debug_neighbor_events(peer))
+		zlog_debug("%pBP BFD strict mode, holding incoming connection until BFD is up for %s",
+			   peer, bgp_peer_get_connection_direction_string(connection));
+
+	SET_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD);
+
+	event_cancel(&connection->t_connect);
+	event_cancel(&peer->bfd_config->t_hold_timer);
+	event_add_timer(bm->master, bgp_bfd_strict_holdtime_expire, peer,
+			peer->bfd_config->hold_time, &peer->bfd_config->t_hold_timer);
+
+	return true;
+}
+
+void bgp_bfd_strict_hold_release(struct peer *peer)
+{
+	struct peer_connection *connection = peer->connection;
+
+	UNSET_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD);
+
+	if (peer->bfd_config)
+		event_cancel(&peer->bfd_config->t_hold_timer);
+
+	if (bgp_debug_neighbor_events(peer))
+		zlog_debug("%pBP BFD is up, releasing connection held by BFD strict mode for %s",
+			   peer, bgp_peer_get_connection_direction_string(connection));
+
+	if (CHECK_FLAG(peer->flags, PEER_FLAG_TIMER_DELAYOPEN))
+		BGP_EVENT_ADD(connection, TCP_connection_open_w_delay);
+	else
+		BGP_EVENT_ADD(connection, TCP_connection_open);
+}
+
 static void bfd_session_status_update(struct bfd_session_params *bsp,
 				      const struct bfd_session_status *bss,
 				      void *arg)
@@ -120,7 +163,12 @@ static void bfd_session_status_update(struct bfd_session_params *bsp,
 	    !peer_established(peer->connection)) {
 		if (!BGP_PEER_START_SUPPRESSED(peer)) {
 			bgp_fsm_nht_update(peer->connection, peer, true);
-			BGP_EVENT_ADD(peer->connection, BGP_Start);
+
+			/* Held connections sit in Active, BGP_Start is ignored there. */
+			if (CHECK_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD))
+				bgp_bfd_strict_hold_release(peer);
+			else
+				BGP_EVENT_ADD(peer->connection, BGP_Start);
 		}
 	}
 }
@@ -175,6 +223,10 @@ void bgp_peer_config_apply(struct peer *p, struct peer_group *pg)
 				  gconfig->bfd_config->cbit);
 	else
 		bfd_sess_set_cbit(p->bfd_config->session, p->bfd_config->cbit);
+
+	/* If no hold time was specified in peer, then use the group one. */
+	if (p->bfd_config->hold_time == BFD_DEF_STRICT_HOLD_TIME)
+		p->bfd_config->hold_time = gconfig->bfd_config->hold_time;
 
 	/* If no profile was specified in peer, then use the group profile. */
 	if (p->bfd_config->profile[0] == 0)

bgpd/bgp_bfd.h
@@ -60,4 +60,15 @@ extern void bgp_peer_remove_bfd_config(struct peer *p);
  */
 extern void bgp_peer_bfd_update_source(struct peer *p);
 
+/**
+ * Keep an accepted connection in Active while `bfd strict` is configured and
+ * BFD is not up yet. Arms the strict mode hold timer.
+ */
+extern bool bgp_bfd_strict_hold_start(struct peer *p);
+
+/**
+ * Let a held connection proceed.
+ */
+extern void bgp_bfd_strict_hold_release(struct peer *p);
+
 #endif /* _QUAGGA_BGP_BFD_H */

bgpd/bgp_fsm.c
@@ -408,9 +408,12 @@ void bgp_timer_set(struct peer_connection *connection)
 		/* Active is waiting connection from remote peer.  And if
 		   connect timer is expired, change status to Connect. */
 		event_cancel(&connection->t_start);
-		/* If peer is passive mode, do not set connect timer. */
-		if (CHECK_FLAG(peer->flags, PEER_FLAG_PASSIVE)
-		    || CHECK_FLAG(peer->sflags, PEER_STATUS_NSF_WAIT)) {
+		/* If peer is passive mode or held by BFD strict mode, do not set
+		 * connect timer.
+		 */
+		if (CHECK_FLAG(peer->flags, PEER_FLAG_PASSIVE) ||
+		    CHECK_FLAG(peer->sflags, PEER_STATUS_NSF_WAIT) ||
+		    CHECK_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD)) {
 			event_cancel(&connection->t_connect);
 		} else {
 			if (CHECK_FLAG(peer->flags, PEER_FLAG_TIMER_DELAYOPEN))
@@ -2265,6 +2268,8 @@ enum bgp_fsm_state_progress bgp_stop(struct peer_connection *connection)
 	event_cancel(&connection->t_routeadv);
 	event_cancel(&connection->t_delayopen);
 
+	UNSET_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD);
+
 	/* Clear input and output buffer.  */
 	frr_with_mutex (&connection->io_mtx) {
 		if (connection->ibuf)
@@ -3147,7 +3152,8 @@ void bgp_fsm_nht_update(struct peer_connection *connection, struct peer *peer,
 		}
 		break;
 	case Active:
-		if (has_valid_nexthops) {
+		/* Do not disturb a connection held by BFD strict mode. */
+		if (has_valid_nexthops && !CHECK_FLAG(peer->sflags, PEER_STATUS_BFD_STRICT_HOLD)) {
 			event_cancel(&connection->t_connect);
 			BGP_EVENT_ADD(connection, ConnectRetry_timer_expired);
 		}

bgpd/bgp_network.c
@@ -24,6 +24,7 @@
 #include "nexthop.h"
 
 #include "bgpd/bgpd.h"
+#include "bgpd/bgp_bfd.h"
 #include "bgpd/bgp_open.h"
 #include "bgpd/bgp_fsm.h"
 #include "bgpd/bgp_attr.h"
@@ -559,7 +560,11 @@ static void bgp_accept(struct event *event)
 			bgp_fsm_change_status(incoming, Active);
 			event_cancel(&incoming->t_start);
 
-			if (peer_active(incoming) == BGP_PEER_ACTIVE) {
+			/* Rejecting would delete the peer (because it's dynamic) and
+			 * deregister BFD, hold instead.
+			 */
+			if (peer_active(incoming) == BGP_PEER_ACTIVE &&
+			    !bgp_bfd_strict_hold_start(dynamic_peer)) {
 				if (CHECK_FLAG(dynamic_peer->flags, PEER_FLAG_TIMER_DELAYOPEN))
 					BGP_EVENT_ADD(incoming, TCP_connection_open_w_delay);
 				else

bgpd/bgp_vty.c
@@ -18408,6 +18408,9 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, uint16_t sh_flags, bo
 			json_object_boolean_add(json_neigh, "bfdHoldTimerExpired",
 						!!CHECK_FLAG(p->sflags,
 							     PEER_STATUS_BFD_STRICT_HOLD_TIME_EXPIRED));
+			json_object_boolean_add(json_neigh, "bfdStrictHold",
+						!!CHECK_FLAG(p->sflags,
+							     PEER_STATUS_BFD_STRICT_HOLD));
 		}
 		if (event_is_scheduled(p->connection->t_start))
 			json_object_int_add(json_neigh,
@@ -18474,6 +18477,10 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, uint16_t sh_flags, bo
 			vty_out(vty, "BFD Hold Time (interval %u) timer expires in %ld seconds\n",
 				p->bfd_config->hold_time,
 				event_timer_remain_second(p->bfd_config->t_hold_timer));
+
+		if (CHECK_FLAG(p->sflags, PEER_STATUS_BFD_STRICT_HOLD))
+			vty_out(vty,
+				"Incoming connection held until BFD session is up (BFD strict mode)\n");
 
 		if (p->password)
 			vty_out(vty, "Peer Authentication Enabled\n");

bgpd/bgpd.h
@@ -2015,6 +2015,7 @@ struct peer {
 #define PEER_STATUS_EXT_OPT_PARAMS_LENGTH	 (1U << 5)
 #define PEER_STATUS_BFD_STRICT_HOLD_TIME_EXPIRED (1U << 6) /* BFD strict hold time expired */
 #define PEER_STATUS_COND_ADV_PENDING		 (1U << 7) /* conditional advertisement pending */
+#define PEER_STATUS_BFD_STRICT_HOLD		 (1U << 8) /* BFD strict mode holds the connection */
 
 	/* Peer status af flags (reset in bgp_stop) */
 	uint16_t af_sflags[AFI_MAX][SAFI_MAX];

lib/bfd.c
@@ -1337,6 +1337,11 @@ int bfd_nht_update(const struct prefix *match, const struct zapi_route *route)
 	return 0;
 }
 
+bool bfd_session_is_up(const struct bfd_session_params *session)
+{
+	return session->bss.state == BSS_UP;
+}
+
 bool bfd_session_is_down(const struct bfd_session_params *session)
 {
 	return session->bss.state == BSS_DOWN ||

lib/bfd.h
@@ -463,6 +463,7 @@ extern bool bfd_protocol_integration_shutting_down(void);
 extern int bfd_nht_update(const struct prefix *match,
 			  const struct zapi_route *route);
 
+extern bool bfd_session_is_up(const struct bfd_session_params *session);
 extern bool bfd_session_is_down(const struct bfd_session_params *session);
 extern bool bfd_session_is_admin_down(const struct bfd_session_params *session);
 

commit b382aae7686e1f2e142f811f165a2bfc6c1e116a

bgpd: Stop BFD hold timer before freeing BFD config
Fixes: d1a9f41030cff796c5b82beafc9e8c66f8df7c37 ("bgpd: Do not establish session for dynamic peers if BFD strict is enabled")

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgp_bfd.c
@@ -505,6 +505,7 @@ static void bgp_group_remove_bfd(struct peer *p)
 		return;
 
 	/* Free configuration and point to `NULL`. */
+	event_cancel(&p->bfd_config->t_hold_timer);
 	XFREE(MTYPE_BFD_CONFIG, p->bfd_config);
 
 	/* Now that it is `NULL` recalculate configuration for all peers. */