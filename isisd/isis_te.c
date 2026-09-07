bc06f60cac090aba0a469822e09c42db9d2c2815
Carmine Scarpitta <cscarpit@cisco.com>
2026-09-07 18:08:58 +0200

    isisd: Reject oversized TE vertex identifiers
    The parser copies the vertex identifier into the 255-byte `sysid` buffer
    with `strlcpy()`. Identifiers longer than 254 bytes are truncated.
    
    For example, if I execute:
    
    ```
    show isis mpls-te database vertex A...A.00
    ```
    
    where `"A...A"` represents 255 `'A'` characters, the identifier is
    258 bytes long, but `sysid` contains only the first 254 characters.
    For this input, the parser performs these operations:
    
    ```
    pos = id + strlen(id) - 3;
    ...
    sysid[pos - id - 1] = '\0';
    ```
    
    Here, `pos - id - 1` evaluates to 255, so the statement writes to
    `sysid[255]`. This is one byte past the end of the buffer and causes a
    stack-buffer-overflow.
    
    Fix this by rejecting identifiers that are too large for `sysid` before
    copying or parsing them.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>
    (cherry picked from commit d323af11eab6fb2f1f7e7515d737b84535f63f19)

--- isisd/isis_te.c
+++ isisd/isis_te.c
@@ -1936,8 +1936,13 @@ static struct ls_vertex *vertex_for_arg(struct ls_ted *ted, const char *id,
 	uint8_t lspid[ISIS_SYS_ID_LEN + 2] = {0};
 	struct isis_dynhn *dynhn;
 	uint64_t key = 0;
+	size_t id_len;
 
 	if (!id)
+		return NULL;
+
+	id_len = strlen(id);
+	if (id_len >= sizeof(sysid))
 		return NULL;
 
 	/*
@@ -1950,8 +1955,8 @@ static struct ls_vertex *vertex_for_arg(struct ls_ted *ted, const char *id,
 	 * xxxx.xxxx.xxxx
 	 */
 	strlcpy(sysid, id, sizeof(sysid));
-	if (strlen(id) > 3) {
-		pos = id + strlen(id) - 3;
+	if (id_len > 3) {
+		pos = id + id_len - 3;
 		if (strncmp(pos, "-", 1) == 0) {
 			memcpy(number, ++pos, 2);
 			lspid[ISIS_SYS_ID_LEN + 1] =
