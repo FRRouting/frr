3966d7fd77631c39e823f1e97c872be2f24219b7
Andreas Karis <ak.karis@gmail.com>
2026-09-18 17:14:50 +0200

    zebra: SRv6 prefix command, validate first before assigning prefix
    Before we assign the prefix to the locator, make sure that it
    passes validation, first. Therefore, move the existing
    locator->prefix = *prefix assignment from before the validation
    to after.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- zebra/zebra_srv6_vty.c
+++ zebra/zebra_srv6_vty.c
@@ -894,7 +894,6 @@ DEFPY (locator_prefix,
 	int idx = 0;
 	bool node_bit_not_conf = false;
 
-	locator->prefix = *prefix;
 	/* Only set default if func_bit_len was not provided in command */
 	if (func_bit_len == 0 && !argv_find(argv, argc, "func-bits", &idx))
 		func_bit_len = ZEBRA_SRV6_FUNCTION_LENGTH;
@@ -952,6 +951,7 @@ DEFPY (locator_prefix,
 		return CMD_WARNING_CONFIG_FAILED;
 	}
 
+	locator->prefix = *prefix;
 	locator->block_bits_length = block_bit_len;
 	locator->node_bits_length = node_bit_len;
 	locator->function_bits_length = func_bit_len;
